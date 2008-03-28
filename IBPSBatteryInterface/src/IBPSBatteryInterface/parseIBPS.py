71d18c6327ad094edb287462841b45f7b65b3597
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-03-28 01:20:50 +0000

    basic ros node calling parseIBPS to read from batteries
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@52 f5854215-dd47-0410-b2c4-cdd35faa7885

--- IBPSBatteryInterface/src/IBPSBatteryInterface/parseIBPS.py
+++ IBPSBatteryInterface/src/IBPSBatteryInterface/parseIBPS.py
@@ -104,139 +104,140 @@ class robotPower:
 
 
 
+def main():
 
-os.system('stty 19200 </dev/ttyUSB0')
-os.system('stty 19200 </dev/ttyUSB1')
-os.system('stty 19200 </dev/ttyUSB2')
-os.system('stty 19200 </dev/ttyUSB3')
+    os.system('stty 19200 </dev/ttyUSB0')
+    os.system('stty 19200 </dev/ttyUSB1')
+    os.system('stty 19200 </dev/ttyUSB2')
+    os.system('stty 19200 </dev/ttyUSB3')
 
-    
+
 #f= open('testfile.txt','r')
-f0 = open('/dev/ttyUSB0','r')
-f1 = open('/dev/ttyUSB1','r')
-f2 = open('/dev/ttyUSB2','r')
-f3 = open('/dev/ttyUSB3','r')
+    f0 = open('/dev/ttyUSB0','r')
+    f1 = open('/dev/ttyUSB1','r')
+    f2 = open('/dev/ttyUSB2','r')
+    f3 = open('/dev/ttyUSB3','r')
 # split on %  to string and checksum
 # check checksum
 
-myPow = robotPower()
+    myPow = robotPower()
 
-start_time = time.time()
-last_time = start_time
-while True:
-    current, blah, blah2 = select.select([f0,f1,f2,f3],[],[],1)
-    for f in current:
-        if f == f0:
-            port = 0;
-            port_string = 'f0'
-        if f == f1:
-            port = 1;
-            port_string = 'f1'
-        if f == f2:
-            port = 2;
-            port_string = 'f2'
-        if f == f3:
-            port = 3;
-            port_string = 'f3'
-        line = f.readline()
-        halves = line.split('%')
-        if len(halves) != 2:
-            #            print 'I did not split on \% correctly'
-            continue
-        halves[1]=halves[1].strip()
-        halves[0]=halves[0].lstrip('$')
+    start_time = time.time()
+    last_time = start_time
+    while True:
+        current, blah, blah2 = select.select([f0,f1,f2,f3],[],[],1)
+        for f in current:
+            if f == f0:
+                port = 0;
+                port_string = 'f0'
+            if f == f1:
+                port = 1;
+                port_string = 'f1'
+            if f == f2:
+                port = 2;
+                port_string = 'f2'
+            if f == f3:
+                port = 3;
+                port_string = 'f3'
+            line = f.readline()
+            halves = line.split('%')
+            if len(halves) != 2:
+                #            print 'I did not split on \% correctly'
+                continue
+            halves[1]=halves[1].strip()
+            halves[0]=halves[0].lstrip('$')
 
 #        print line
 #    print halves
 #    print len(halves)
 #    print checksum256(halves[0])
 #    print hex2dec(halves[1])
-        #    print hex(255)
+            #    print hex(255)
 
 
-        # read first char and switch
-        message = halves[0]
+            # read first char and switch
+            message = halves[0]
 
-        # case C controller
+            # case C controller
 
-        # split on commas
-        # first element is controller number
-        # for each pair
-        # read index  01-07
-        # record value
-        if len(message) < 2:
-            print "error message too short: \"%s\" from \"%s\""%(message, line)
-            print "This often indicates a misconfigured serial port check port:"
-            print f
-            continue
-        if message[0] == 'C':
-            controller_number = int(message[1])
-            #print 'Controller on port %s says:'%(port_string)
-            splitmessage = message.split(',')
+            # split on commas
+            # first element is controller number
+            # for each pair
+            # read index  01-07
+            # record value
+            if len(message) < 2:
+                print "error message too short: \"%s\" from \"%s\""%(message, line)
+                print "This often indicates a misconfigured serial port check port:"
+                print f
+                continue
+            if message[0] == 'C':
+                controller_number = int(message[1])
+                #print 'Controller on port %s says:'%(port_string)
+                splitmessage = message.split(',')
 
-            #print 'batteries present'
-            #print  readmask(splitmessage[2])
-            mask = readmask(splitmessage[2])
-            for i in range(0,8):
-                myPow.controllers[port].batteries[i].present = mask[i]
-            #print 'batteries charging'
-            #print  readmask(splitmessage[4])
-            mask = readmask(splitmessage[2])
-            for i in range(0,8):
-                myPow.controllers[port].batteries[i].charging = mask[i]
-            #print 'batteries supplying power to system'
-            #print  readmask(splitmessage[6])
-            mask = readmask(splitmessage[6])
-            for i in range(0,8):
-                myPow.controllers[port].batteries[i].supplying_power = mask[i]
-            #print 'batteries which have charge power present'
-            #print  readmask(splitmessage[10])
-            mask = readmask(splitmessage[10])
-            for i in range(0,8):
-                myPow.controllers[port].batteries[i].w_charge_power = mask[i]
-            #print 'batteries with "Power no good"'
-            #print  readmask(splitmessage[12])
-            mask = readmask(splitmessage[12])
-            for i in range(0,8):
-                myPow.controllers[port].batteries[i].power_no_good = mask[i]
-            #print 'batteries charge inhibited'
-            #print  readmask(splitmessage[14])
-            mask = readmask(splitmessage[14])
-            for i in range(0,8):
-                myPow.controllers[port].batteries[i].charge_inhibited = mask[i]
+                #print 'batteries present'
+                #print  readmask(splitmessage[2])
+                mask = readmask(splitmessage[2])
+                for i in range(0,8):
+                    myPow.controllers[port].batteries[i].present = mask[i]
+                #print 'batteries charging'
+                #print  readmask(splitmessage[4])
+                mask = readmask(splitmessage[2])
+                for i in range(0,8):
+                    myPow.controllers[port].batteries[i].charging = mask[i]
+                #print 'batteries supplying power to system'
+                #print  readmask(splitmessage[6])
+                mask = readmask(splitmessage[6])
+                for i in range(0,8):
+                    myPow.controllers[port].batteries[i].supplying_power = mask[i]
+                #print 'batteries which have charge power present'
+                #print  readmask(splitmessage[10])
+                mask = readmask(splitmessage[10])
+                for i in range(0,8):
+                    myPow.controllers[port].batteries[i].w_charge_power = mask[i]
+                #print 'batteries with "Power no good"'
+                #print  readmask(splitmessage[12])
+                mask = readmask(splitmessage[12])
+                for i in range(0,8):
+                    myPow.controllers[port].batteries[i].power_no_good = mask[i]
+                #print 'batteries charge inhibited'
+                #print  readmask(splitmessage[14])
+                mask = readmask(splitmessage[14])
+                for i in range(0,8):
+                    myPow.controllers[port].batteries[i].charge_inhibited = mask[i]
 
-        else:
-            pass
+            else:
+                pass
 
-        if message[0] == 'B':
-            controller_number = int(message[1])
-            battery_number = int(message[2])
+            if message[0] == 'B':
+                controller_number = int(message[1])
+                battery_number = int(message[2])
 
-            #print 'Battery %d on Controller %d'%(battery_number, controller_number)
+                #print 'Battery %d on Controller %d'%(battery_number, controller_number)
 
-            splitmessage = message.split(',')
+                splitmessage = message.split(',')
 
-            for i in range(0,(len(splitmessage)-1)/2):
-                key = splitmessage[i*2+1]
-                value = splitmessage[i*2+2]
+                for i in range(0,(len(splitmessage)-1)/2):
+                    key = splitmessage[i*2+1]
+                    value = splitmessage[i*2+2]
 #                print 'key %s , value %s'%(splitmessage[i*2+1],splitmessage[i*2+2])
 #                print 'key %s , value dec %s'%(splitmessage[i*2+1],hex2dec(splitmessage[i*2+2]))
 
 
 
-                if key == '09':
-                    #print 'voltage is %f'%(float(hex2dec(value))/1000.0)
-                    myPow.controllers[port].batteries[battery_number].voltage = float(hex2dec(value))/1000.0 
+                    if key == '09':
+                        #print 'voltage is %f'%(float(hex2dec(value))/1000.0)
+                        myPow.controllers[port].batteries[battery_number].voltage = float(hex2dec(value))/1000.0 
 
 
-                if key == '0A':
-                    intval = float(hex2dec(value))
-                    if intval < 32767:
-                        val = intval
-                    else:
-                        val = (intval - 65636.0)
-                    #print 'current is %f'%(val/1000.0)
-                    myPow.controllers[port].batteries[battery_number].current = val/1000.0
+                    if key == '0A':
+                        intval = float(hex2dec(value))
+                        if intval < 32767:
+                            val = intval
+                        else:
+                            val = (intval - 65636.0)
+                        #print 'current is %f'%(val/1000.0)
+                        myPow.controllers[port].batteries[battery_number].current = val/1000.0
 
 
 # case B battery
@@ -247,32 +248,35 @@ while True:
 # record value cast properly so pages 13 to 35
 
 #case S system data
-        if message[0] == 'S':
-            splitmessage = message.split(',')
-            #print 'System Message'
-            for i in range(0,(len(splitmessage)-1)/2):
-                key = splitmessage[i*2+1]
-                value = splitmessage[i*2+2]
+            if message[0] == 'S':
+                splitmessage = message.split(',')
+                #print 'System Message'
+                for i in range(0,(len(splitmessage)-1)/2):
+                    key = splitmessage[i*2+1]
+                    value = splitmessage[i*2+2]
 #            print 'key %s , value %s'%(key,value)
 #            print 'key %s , value dec %s'%(key,hex2dec(value))
 
-                if key == '01':
-                    #print 'time until exhaustion = %s minutes'%hex2dec(value)
-                    myPow.controllers[port].time_remaining = int(hex2dec(value))
-                if key == '02':
-                    #reserved
-                    pass
+                    if key == '01':
+                        #print 'time until exhaustion = %s minutes'%hex2dec(value)
+                        myPow.controllers[port].time_remaining = int(hex2dec(value))
+                    if key == '02':
+                        #reserved
+                        pass
 
-                if key == '03':
-                    #print 'text message to the system %s'%value
-                    myPow.controllers[port].new_system_message(value)
+                    if key == '03':
+                        #print 'text message to the system %s'%value
+                        myPow.controllers[port].new_system_message(value)
 
-                if key == '04':
-                    #print 'average charge percent %d'% hex2dec(value)
-                    myPow.controllers[port].average_charge = int(hex2dec(value))
+                    if key == '04':
+                        #print 'average charge percent %d'% hex2dec(value)
+                        myPow.controllers[port].average_charge = int(hex2dec(value))
 
-    #print time.time()
-    increment = 1.0
-    if time.time() - last_time > increment:
-        last_time = last_time + increment
-        myPow.print_remaining()
+        #print time.time()
+        increment = 1.0
+        if time.time() - last_time > increment:
+            last_time = last_time + increment
+            myPow.print_remaining()
+
+
+#main()
