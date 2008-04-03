6e1e4b21fa33a0a0a039c998cac7eb34e98048df
morgan_quigley <morgan_quigley@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-04-03 04:41:59 +0000

    cleaner use of the namespace for string_utils
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@80 f5854215-dd47-0410-b2c4-cdd35faa7885

--- string_utils/src/libstring_utils/string_utils.cpp
+++ string_utils/src/libstring_utils/string_utils.cpp
@@ -1,10 +1,7 @@
 #include "string_utils/string_utils.h"
 using namespace std;
 
-namespace string_utils
-{
-
-void split(const string &s, vector<string> &t, const string &d)
+void string_utils::split(const string &s, vector<string> &t, const string &d)
 {
   t.clear();
   size_t start = 0, end;
@@ -16,4 +13,3 @@ void split(const string &s, vector<string> &t, const string &d)
   t.push_back(s.substr(start));
 }
 
-}
