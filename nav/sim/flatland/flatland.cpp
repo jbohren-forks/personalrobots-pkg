002a0fbdb0df3a3c516a6e2495ce979307ef9286
morgan_quigley <morgan_quigley@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-04-23 21:32:17 +0000

    adding simple simulator from ros core svn
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/branches/rosbus@168 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ nav/sim/flatland/flatland.cpp
@@ -0,0 +1,97 @@
+#include <unistd.h>
+#include <math.h>
+#include "ros/node.h"
+#include "rosrand/rosrand.h"
+#include "std_msgs/MsgPose2DFloat32.h"
+
+class FlatlandRobot
+{
+public:
+  double x, y, th;
+  double odom_x, odom_y, odom_th;
+  double v, w; // linear and angular velocity
+  double v_bias, w_bias;
+  FlatlandRobot() : x(0), y(0), th(0), v(0), w(0),
+    odom_x(0), odom_y(0), odom_th(0)
+  {
+    v_bias = ros::rand::gaussian(0, 0.001);
+    w_bias = ros::rand::gaussian(0, 0.001);
+  }
+  inline double clamp(double d, double l, double u)
+  {
+    return (d > u ? u : (d < l ? l : d));
+  }
+  double normalize_angle(double a)
+  {
+    if (a >= -M_PI && a < M_PI)
+      return a;
+    a -= floor(a / 2*M_PI) * 2 * M_PI;
+    if (a >= M_PI)
+      a -= 2 * M_PI;
+    else if (a < -M_PI)
+      a += 2 * M_PI;
+    return a;
+  }
+  double tic(double dt)
+  {
+    double xn = x + dt * v * cos(th);
+    double yn = y + dt * v * sin(th);
+    double vn = (fabs(v) > 0.001 ? v * (1.0 + v_bias) : v);
+    double wn = (fabs(w) > 0.001 ? w * (1.0 + w_bias) : w);
+    v_bias += ros::rand::uniform(-0.0001, 0.0001);
+    w_bias += ros::rand::uniform(-0.0001, 0.0001);
+    // clamp to sane values
+    v_bias = clamp(v_bias, -0.1, 0.1);
+    w_bias = clamp(w_bias, -0.1, 0.1);
+    // todo: check against wall collisions like switchyard flatland
+    x = xn;
+    y = yn;
+    odom_x += dt * vn * cos(odom_th);
+    odom_y += dt * vn * sin(odom_th);
+    th = normalize_angle(th + dt * w);
+    odom_th = normalize_angle(odom_th + dt * wn);
+  }
+};
+
+class Flatland : public ros::node
+{
+public:
+  MsgPose2DFloat32 odom;
+  FlatlandRobot robot;
+  double last_odom_t, last_t;
+
+  Flatland() : ros::node("flatland"), last_odom_t(0), last_t(0)
+  {
+    advertise("odom", odom);
+  }
+  void tic()
+  {
+    robot.v = 0.1; robot.w = 0.01;
+    double t = clock.time();
+    double dt = t - last_t;
+    last_t = t;
+    robot.tic(dt);
+    if (t > last_odom_t + 0.01)
+    {
+      // send odom message
+      odom.x  = robot.odom_x;
+      odom.y  = robot.odom_y;
+      odom.th = robot.odom_th;
+      publish("odom", odom);
+      last_odom_t = t;
+    }
+  }
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv);
+  Flatland flatland;
+  while (flatland.ok())
+  {
+    usleep(1000);
+    flatland.tic();
+  }
+  return 0;
+}
+
