175799c39ee50d9bb15cf99d120c4d9cb0e05a09
morgan_quigley <morgan_quigley@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-04-23 21:33:34 +0000

    input devices (just joysticks for now) coming over from ros core svn. I anticipate this to include things like pedals, spaceball, mice, keyboard, etc., anything you might want to ship onto the botnet to control stuff
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/branches/rosbus@169 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ input/joy_view/joy_view.cpp
@@ -0,0 +1,60 @@
+#include <unistd.h>
+#include <math.h>
+#include <GL/gl.h>
+#include "ros/node.h"
+#include "joy/MsgJoy.h"
+#include "sdlgl/sdlgl.h"
+
+using namespace ros;
+
+class JoyView : public node, public SDLGL
+{
+public:
+  MsgJoy joy;
+
+  JoyView() : node("joy_view")
+  {
+    subscribe("joy", joy, &JoyView::joy_cb);
+    init_gui(640, 480, "nav view");
+  }
+  void joy_cb()
+  {
+    request_render();
+  }
+  void render()
+  {
+    glClearColor(0.2,0.2,0.2,0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+    joy.lock();
+    glLineWidth(5);
+    glBegin(GL_LINES);
+      glColor3f(1, 0, 0);
+      glVertex2f(-1,0);
+      glVertex2f(-1 + joy.x1, joy.y1);
+
+      glColor3f(0, 1, 0);
+      glVertex2f(1, 0);
+      glVertex2f(1 + joy.x2, joy.y2);
+    glEnd();
+    joy.unlock();
+    SDL_GL_SwapBuffers();
+  }
+  void set_view_params(int width, int height)
+  {
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-2, 2, -2, 2, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    request_render();
+  }
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv);
+  JoyView view;
+  view.main_loop();
+  return 0;
+}
+
