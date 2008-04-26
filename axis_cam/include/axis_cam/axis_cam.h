fbff62bf34e86a491f9d4e8a5369ac03dd2863a7
morgan_quigley <morgan_quigley@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-04-26 01:03:39 +0000

    deleting thread_utils package because it has been subsumed by the rosthread package, which provides more stuff (e.g. semaphore)
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/branches/rosbus@183 f5854215-dd47-0410-b2c4-cdd35faa7885

--- axis_cam/include/axis_cam/axis_cam.h
+++ axis_cam/include/axis_cam/axis_cam.h
@@ -33,7 +33,7 @@
 #include <curl/curl.h>
 #include <string>
 #include <sstream>
-#include "thread_utils/mutex.h"
+#include "rosthread/mutex.h"
 using namespace std;
 
 class AxisCam
@@ -65,7 +65,7 @@ private:
   CURL *jpeg_curl, *getptz_curl, *setptz_curl;
   char *image_url, *ptz_url;
   stringstream ptz_ss; // need to mutex this someday...
-  ThreadUtils::Mutex ptz_ss_mutex;
+  ros::thread::mutex ptz_ss_mutex;
   inline double clamp(double d, double low, double high)
   { 
     return (d < low ? low : (d > high ? high : d)); 
