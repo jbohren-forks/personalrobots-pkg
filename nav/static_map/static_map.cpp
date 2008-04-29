a2553e6459b75ec7558c0f9c398a5e4aba417f32
morgan_quigley <morgan_quigley@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-04-29 21:34:20 +0000

    strawman of what I want the client usage to look like
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/branches/rosbus@224 f5854215-dd47-0410-b2c4-cdd35faa7885

--- nav/static_map/static_map.cpp
+++ nav/static_map/static_map.cpp
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include "ros/node.h"
+#include "std_rpc/map.h"
 
 using namespace ros;
 
@@ -8,6 +9,23 @@ class StaticMap : public node
 public:
   StaticMap() : node("static_map")
   {
+    advertise("map", &StaticMap::map_cb);
+  }
+  void map_cb(rpcMap &rpc)
+  {
+    // this code must be re-entrant, or else you must
+    // mutex it yourself
+    /*
+    the extents of the query could be accessed this way:
+    rpc.request.extents.x0
+    rpc.request.extents.y0
+    rpc.request.extents.x1
+    rpc.request.extents.y1
+    */
+    rpc.response.cell_size = 0.1; // 10cm grid
+    rpc.response.map.width = 600;
+    rpc.response.map.height = 600;
+    // copy over the buffer
   }
 };
 
