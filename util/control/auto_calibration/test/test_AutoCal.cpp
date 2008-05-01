ba4dfe21c6dc0a7701da7b35daabac622ed9d7d2
mmwise <mmwise@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-05-01 21:56:15 +0000

    added ros node stuff
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@280 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/control/auto_calibration/test/test_AutoCal.cpp
+++ util/control/auto_calibration/test/test_AutoCal.cpp
@@ -1,9 +1,23 @@
 #include "auto_calibration/AutoCal.h"
+#include "ros/node.h"
 
 using namespace std;
 
+class test : public ros::node
+{
+  test() : ros::node("test"){}
+};
+
 int main(int argc, char **argv)
 {
+  ros::init(argc, argv);
+  test T;
+  
+  T.setparam("junk", 89898);
+  int happy;
+  T.getparam("junk", happy);
+  
+  cout<< "happy "<<happy<<endl;
   EtherDrive e;
   if (!e.init("192.168.0.100")) {
     cout << "Could not initialize etherdrive." << endl;
@@ -11,30 +25,6 @@ int main(int argc, char **argv)
   }
   AutoCal robot(e);
   robot.RunAutoCal(argv[1]);
-  #if 0 
-  multimap<string, Info> paramMap;
-  Info headinfo1 = {0, 180, 0.0, 0.0, 0.0, 0.0, 0};
-  paramMap.insert(pair<string, Info>("head", headinfo1));   
-  Info headinfo2 = {1, 180, 0.0, 0.0, 0.0, 0.0, 0};
-  paramMap.insert(pair<string, Info>("head", headinfo2));
-  Info headinfo3 = {2, 180, 0.0, 0.0, 0.0, 0.0, 0};
-  paramMap.insert(pair<string, Info>("head", headinfo3));  
-  Info shoulderinfo1 = {0, 180.0, 0.0, 0.0, 90.0, -90.0, 1};
-  paramMap.insert(pair<string, Info>("shoulder", headinfo3));    
-     
-  cout << "Number of elements: " << paramMap.count(argv[1]) << endl;   
   
-  pair<multimap<string, Info>::iterator, multimap<string, Info>::iterator> ppp;
-
-   ppp = paramMap.equal_range(argv[1]);
-
-   // Loop through range of maps of key "b"
-   cout << endl << "Range of \"" << argv[1] << "\" elements:" << endl;
-   for (multimap<string, Info>::iterator it2 = ppp.first;
-       it2 != ppp.second;
-       ++it2)
-   {
-       cout << "  [" << (*it2).first << ", " << (*it2).second.motornum << "]" << endl;
-   }  
-   #endif
+ 
 }
