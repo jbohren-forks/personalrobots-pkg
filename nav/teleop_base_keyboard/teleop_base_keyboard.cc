7e47048ecb4b05a1573130e48116f3eff1cba817
gerkey <gerkey@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-05-12 01:42:11 +0000

    added keyboard-based teleoperation node
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@393 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ nav/teleop_base_keyboard/teleop_base_keyboard.cc
@@ -0,0 +1,216 @@
+#include <termios.h>
+#include <signal.h>
+
+#include <ros/node.h>
+#include <std_msgs/MsgBaseVel.h>
+
+#define KEYCODE_I 0x69
+#define KEYCODE_J 0x6a
+#define KEYCODE_K 0x6b
+#define KEYCODE_L 0x6c
+#define KEYCODE_Q 0x71
+#define KEYCODE_Z 0x7a
+#define KEYCODE_W 0x77
+#define KEYCODE_X 0x78
+#define KEYCODE_E 0x65
+#define KEYCODE_C 0x63
+#define KEYCODE_U 0x75
+#define KEYCODE_O 0x6F
+#define KEYCODE_M 0x6d
+#define KEYCODE_R 0x72
+#define KEYCODE_V 0x76
+#define KEYCODE_T 0x74
+#define KEYCODE_B 0x62
+
+#define KEYCODE_COMMA 0x2c
+#define KEYCODE_PERIOD 0x2e
+
+#define COMMAND_TIMEOUT_SEC 0.2
+
+// at full joystick depression you'll go this fast
+double max_speed = 0.500; // m/second
+double max_turn = 60.0*M_PI/180.0; // rad/second
+// should we continuously send commands?
+bool always_command = false;
+
+
+class TBK_Node : public ros::node
+{
+  private:
+    MsgBaseVel cmdvel;
+
+  public:
+    TBK_Node() : ros::node("tbk")
+    {
+      advertise<MsgBaseVel>("cmd_vel");
+    }
+    ~TBK_Node() { }
+    void keyboardLoop();
+    void stopRobot()
+    {
+      cmdvel.vx = cmdvel.vw = 0.0;
+      publish("cmd_vel", cmdvel);
+    }
+};
+
+TBK_Node* tbk;
+
+void
+quit(int sig)
+{
+  tbk->stopRobot();
+  ros::msg_destruct();
+  exit(0);
+}
+
+int
+main(int argc, char** argv)
+{
+  ros::init(argc,argv);
+
+  tbk = new TBK_Node();
+
+  signal(SIGINT,quit);
+
+  tbk->keyboardLoop();
+
+  return(0);
+}
+
+void
+TBK_Node::keyboardLoop()
+{
+  int kfd = 0;
+  char c;
+  double max_tv = max_speed;
+  double max_rv = max_turn;
+  struct termios cooked, raw;
+  bool dirty=false;
+
+  int speed=0;
+  int turn=0;
+
+  // get the console in raw mode
+  tcgetattr(kfd, &cooked);
+  memcpy(&raw, &cooked, sizeof(struct termios));
+  raw.c_lflag &=~ (ICANON | ECHO);
+  raw.c_cc[VEOL] = 1;
+  raw.c_cc[VEOF] = 2;
+  tcsetattr(kfd, TCSANOW, &raw);
+
+  puts("Reading from keyboard");
+  puts("---------------------------");
+  puts("Moving around:");
+  puts("   u    i    o");
+  puts("   j    k    l");
+  puts("   m    ,    .");
+  puts("");
+  puts("q/z : increase/decrease max speeds by 10%");
+  puts("w/x : increase/decrease only linear speed by 10%");
+  puts("e/c : increase/decrease only angular speed by 10%");
+  puts("");
+  puts("anything else : stop");
+  puts("---------------------------");
+
+  for(;;)
+  {
+    // get the next event from the keyboard
+    if(read(kfd, &c, 1) < 0)
+    {
+      perror("read():");
+      exit(-1);
+    }
+
+    switch(c)
+    {
+      case KEYCODE_I:
+        speed = 1;
+        turn = 0;
+        dirty = true;
+        break;
+      case KEYCODE_K:
+        speed = 0;
+        turn = 0;
+        dirty = true;
+        break;
+      case KEYCODE_O:
+        speed = 1;
+        turn = -1;
+        dirty = true;
+        break;
+      case KEYCODE_J:
+        speed = 0;
+        turn = 1;
+        dirty = true;
+        break;
+      case KEYCODE_L:
+        speed = 0;
+        turn = -1;
+        dirty = true;
+        break;
+      case KEYCODE_U:
+        turn = 1;
+        speed = 1;
+        dirty = true;
+        break;
+      case KEYCODE_COMMA:
+        turn = 0;
+        speed = -1;
+        dirty = true;
+        break;
+      case KEYCODE_PERIOD:
+        turn = 1;
+        speed = -1;
+        dirty = true;
+        break;
+      case KEYCODE_M:
+        turn = -1;
+        speed = -1;
+        dirty = true;
+        break;
+      case KEYCODE_Q:
+        max_tv += max_tv / 10.0;
+        max_rv += max_rv / 10.0;
+        if(always_command)
+          dirty = true;
+        break;
+      case KEYCODE_Z:
+        max_tv -= max_tv / 10.0;
+        max_rv -= max_rv / 10.0;
+        if(always_command)
+          dirty = true;
+        break;
+      case KEYCODE_W:
+        max_tv += max_tv / 10.0;
+        if(always_command)
+          dirty = true;
+        break;
+      case KEYCODE_X:
+        max_tv -= max_tv / 10.0;
+        if(always_command)
+          dirty = true;
+        break;
+      case KEYCODE_E:
+        max_rv += max_rv / 10.0;
+        if(always_command)
+          dirty = true;
+        break;
+    case KEYCODE_C:
+        max_rv -= max_rv / 10.0;
+        if(always_command)
+          dirty = true;
+        break;
+    default:
+      speed = 0;
+      turn = 0;
+      dirty = true;
+    }
+    if (dirty == true)
+    {
+      cmdvel.vx = speed * max_tv;
+      cmdvel.vw = turn * max_rv;
+
+      publish("cmd_vel",cmdvel);
+    }
+  }
+}
