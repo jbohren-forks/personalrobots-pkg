54515ea3b33d0c5078c39fdcfaaaf10fee94c400
gerkey <gerkey@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-05-27 22:30:36 +0000

    added better thread tear-down, plus explicit exit call
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@524 f5854215-dd47-0410-b2c4-cdd35faa7885

--- drivers/input/joy/joy.cpp
+++ drivers/input/joy/joy.cpp
@@ -22,6 +22,7 @@ public:
   int joy_fd;
   string joy_dev;
   int deadzone;
+  pthread_t joy_thread;
 
   Joy() : node("joy")
   {
@@ -29,19 +30,27 @@ public:
     param<int>("joy/deadzone", deadzone, 2000);
     printf("dev:%s\n", joy_dev.c_str());
     printf("deadzone:%d\n", deadzone);
+    advertise<MsgJoy>("joy");
+  }
+  void start()
+  {
     joy_fd = open(joy_dev.c_str(), O_RDONLY);
     if (joy_fd <= 0)
       log(FATAL, "couldn't open joystick %s.", joy_dev.c_str());
-    pthread_t joy_thread;
     pthread_create(&joy_thread, NULL, s_joy_func, this);
-
-    advertise("joy", joy_msg);
+  }
+  void stop()
+  {
+    pthread_cancel(joy_thread);
+    pthread_join(joy_thread,NULL);
+    close(joy_fd);
   }
   void joy_func()
   {
     js_event event;
     while (ok())
     {
+      pthread_testcancel();
       read(joy_fd, &event, sizeof(js_event));
       if (event.type & JS_EVENT_INIT)
         continue;
@@ -86,7 +95,11 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv);
   Joy joy;
+  joy.start();
   joy.spin();
+  joy.stop();
+  ros::fini();
+  exit(0);
   return 0;
 }
 
