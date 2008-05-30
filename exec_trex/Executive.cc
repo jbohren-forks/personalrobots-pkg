e38a59b4efb6fd2ad8e661dd2d402923e438bfd7
mcgann <mcgann@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-05-30 23:53:29 +0000

    More administrivia
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@580 f5854215-dd47-0410-b2c4-cdd35faa7885

--- exec_trex/Executive.cc
+++ exec_trex/Executive.cc
@@ -1,8 +1,83 @@
-///////////////////////////////////////////////////////////////////////////////
-// The trex demo node is a simple encapsulation of a Teleo-Reactive Executive 
-//
-// Copyright (C) 2008, Willow Garage
-//////////////////////////////////////////////////////////////////////////////
+/*
+ * wavefront_player
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <ORGANIZATION> nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+
+@mainpage
+
+@htmlinclude manifest.html
+
+@b exec_trex is a bare bones demonstration of using the TREX hybrid executive
+for planning and control. It uses the EUROPA-2 constraint-based temporal planning library
+to represent plan state, do planning, write models etc. Right now this is
+a crude integration which will evolve to include more generalized integration patterns to
+ROS nodes. Also, the build structure uses jam, and that allows builds for a variety
+of versions. For now we will use the default development build.
+
+<hr>
+
+@section usage Usage
+@verbatim
+$ exec_trex_g_rt cfgFile
+@endverbatim
+
+@param cfgFile Is the TREX configuration file which defines initial inputs and goals.
+
+@todo 
+
+@par Example
+
+@verbatim
+$ exec_trex_g_rt exec.cfg
+@endverbatim
+
+<hr>
+
+@section topic ROS topics
+
+Subscribes to (name/type):
+- @b "state"/Planner2DState : current planner state (e.g., goal reached, no
+path)
+
+Publishes to (name/type):
+- @b "goal"/Planner2DGoal : goal for the robot.
+
+@todo
+
+<hr>
+
+@section parameters ROS parameters
+
+- None
+
+ **/
 
 #include "Nddl.hh"
 #include "Executive.hh"
