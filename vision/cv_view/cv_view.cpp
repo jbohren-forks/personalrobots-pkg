fdd76ca5fc28fcacc439f480cdc339697d526d00
jleibs <jleibs@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-05-31 02:22:10 +0000

    Scale mono16 images for viewing and save last image... this will be cleaned up down the road.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@591 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/cv_view/cv_view.cpp
+++ vision/cv_view/cv_view.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <vector>
+#include "opencv/cxcore.h"
 #include "opencv/cv.h"
 #include "opencv/highgui.h"
 #include "ros/node.h"
@@ -23,11 +24,24 @@ public:
   void image_cb()
   {
     IplImage *cv_image;
+    IplImage *cv_image_sc;
+
     if (cv_bridge.to_cv(&cv_image))
     {
-      cvShowImage("cv_view", cv_image);
+
+    cv_image_sc = cvCreateImage(cvSize(cv_image->width, cv_image->height),
+				IPL_DEPTH_8U, 1);
+      //      printf("got cv image with dim: %d %d\n", cvGetSize(cv_image).width, cvGetSize(cv_image).height);
+    if (cv_image->depth == IPL_DEPTH_16U)
+      cvCvtScale(cv_image, cv_image_sc, 0.0625, 0);
+    else
+      cvCvtScale(cv_image, cv_image_sc, 1.0, 0);
+      
+      cvShowImage("cv_view", cv_image_sc);
+      cvSaveImage("last_img.png", cv_image_sc);
       cvWaitKey(3);
       cvReleaseImage(&cv_image);
+      cvReleaseImage(&cv_image_sc);
     }
   }
 };
