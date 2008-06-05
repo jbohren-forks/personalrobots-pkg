b4136d48188cec1951d2499087929faa2dd98f94
jleibs <jleibs@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-06-05 22:18:25 +0000

    Cleaning up some documentation for the urg_laser driver.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@660 f5854215-dd47-0410-b2c4-cdd35faa7885

--- drivers/laser/urg_driver/urg_laser.h
+++ drivers/laser/urg_driver/urg_laser.h
@@ -18,6 +18,14 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+/*! \mainpage
+ *
+ * This is a standalone urg driver, primarily for use with the Hokuyo
+ * TOP-URG.  Backwards compatibility with other urg devices is not yet
+ * complete.
+ *
+ */
+
 #include <stdio.h>
 #include <termios.h>
 
@@ -50,8 +58,6 @@ typedef struct urg_laser_config
   float max_range;
   //! Range Resolution [m]
   float range_res;
-  //! Scanning frequency [Hz]
-  float scanning_frequency;
 } urg_laser_config_t;
 
 
@@ -75,6 +81,7 @@ typedef struct urg_laser_scan
 /*!
  *  Except where specified otherwise, methods of this class return 0
  *  or positive values on success and negative values on failure.
+ *  All timeouts are in milliseconds or -1 for a blocking call.
  */
 class urg_laser
 {
@@ -114,7 +121,8 @@ public:
    *  This function allows commands to be sent directly to the URG.
    *  Possible commands can be found in the URG documentation.
    *
-   *  \param Command and arguments in a character array
+   *  \param cmd    Command and arguments in a character array
+   *  \param timeout Timeout in milliseconds.
    * 
    *  \return Status code returned from urg device.
    */
@@ -125,7 +133,8 @@ public:
    *  This function allows commands to be sent directly to the URG.
    *  Possible commands can be found in the URG documentation.
    *
-   *  \param Command and arguments in a character array
+   *  \param cmd    Command and arguments in a character array
+   *  \param timeout Timeout in milliseconds.
    * 
    *  \return Status code returned from urg device.
    */
@@ -139,26 +148,70 @@ public:
    *
    * \param curr_baud The current baud rate
    * \param new_baud  The desired baud rate
-   * \param timeout   A timeout in milliseconds
+   * \param timeout Timeout in milliseconds.
    *
    * \return          Returns 0 on success, -1 on failure
    */
   int change_baud(int curr_baud, int new_baud, int timeout = -1);
 
-  //! Get a single scan from the urg
+  //! Retrieve a single scan from the urg
+  /*!
+   * \param scan       A pointer to an urg_laser_scan_t which will be populated
+   * \param min_ang    Minimal angle of the scan
+   * \param max_ang    Maximum angle of the scan
+   * \param clustering Number of adjascent ranges to be clustered into a single measurement.
+   * \param timeout    Timeout in milliseconds.
+   *
+   * \return          Returns 0 on success, -1 on communications failure, passthrough of status from Hokuyo otherwise.
+   */
   int poll_scan(urg_laser_scan_t * scan, double min_ang, double max_ang, int clustering = 0, int timeout = -1);
 
-  int request_scans(bool intensity, double min_ang, double max_ang, int cluster = 0, int skip = 0, int num = 0, int timeout = -1);
+  //! Request a sequence of scans from the urg
+  /*!
+   *  This function will request a sequence of scans from the URG.  If
+   *  indefinite scans are requested, stop_scanning() must be called
+   *  to terminate scanning.  Service_scan() must be called repeatedly
+   *  to receive scans as they come in.
+   *
+   * \param intensity  Whether or not intensity data should be provided
+   * \param min_ang    Minimal angle of the scan
+   * \param max_ang    Maximum angle of the scan
+   * \param clustering Number of adjascent ranges to be clustered into a single measurement
+   * \param skip       Number of scans to skip between returning measurements
+   * \param num        Number of scans to return (0 for indefinite)
+   * \param timeout    Timeout in milliseconds.
+   *
+   * \return          Returns 0 on success, -1 on communications failure, passthrough of status from Hokuyo otherwise.
+   */
+  int request_scans(bool intensity, double min_ang, double max_ang, int clustering = 0, int skip = 0, int num = 0, int timeout = -1);
 
+  //! Retrieve a scan if they have been requested
+  /*!
+   * \param scan       A pointer to an urg_laser_scan_t which will be populated.
+   * \param timeout    Timeout in milliseconds.
+   *
+   * \return          Returns 0 on success, -1 on communications failure, passthrough of status from Hokuyo otherwise.
+   */
   int service_scan(urg_laser_scan_t * scan, int timeout = -1);
 
+
+  //! Stop retrieving scans
+  /*!
+   * \return          Returns 0 on success, -1 on communications failure, passthrough of status from Hokuyo otherwise.
+   */
   int stop_scanning();
 
   //! Get serial number from the URG
+  /*!
+   * \return Returns ID on success, -1 on failure.
+   */
   int get_ID();
 
   //! Get the SCIP version in use by the urg
   int get_SCIP_version() { return SCIP_version; }
+  /*!
+   * \return Returns the SCIP version in use by the URG.
+   */
   
 private:
   //! Query which version of SCIP the URG is using
@@ -173,12 +226,16 @@ private:
   //! Wrapper around write
   int urg_write(const char* msg);
 
+  //! Read a full line from the urg
   int urg_readline(char *buf, int len, int timeout = -1);
 
+  //! Search for a particular sequence and then read the rest of the line
   char* urg_readline_after(char *buf, int len, const char *str, int timeout = -1);
 
+  //! Compute the checksum of a given buffer
   int check_sum(const char* buf, int buf_len);
 
+  //! Read in a scan
   int read_data(urg_laser_scan_t* scan, bool has_intensity, int timout = -1);
 
   int SCIP_version;  
