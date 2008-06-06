21020364ac6b391529cec404de221ea9d9d1d8f6
hsujohnhsu <hsujohnhsu@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-06-06 17:34:29 +0000

    added path for dynamically linked library libpr2API.so in the shell scripts.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@681 f5854215-dd47-0410-b2c4-cdd35faa7885

--- simulators/rosgazebo/setup.bash
+++ simulators/rosgazebo/setup.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 export SIM_TOP=`rospack find gazebo`/gazebo-git
 export SIM_PLUGIN=`rospack find gazebo_plugin`
+export PR2API=`rospack find libpr2API`
 
+export LD_LIBRARY_PATH=$PR2API/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$SIM_PLUGIN/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$SIM_TOP/root/lib:$LD_LIBRARY_PATH
 export PATH=$SIM_TOP/root/bin:$PATH
