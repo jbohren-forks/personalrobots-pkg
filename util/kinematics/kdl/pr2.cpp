4651f76131cc5af62795408db8ac2f4daec36007
advaitjain <advaitjain@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-06-10 22:36:22 +0000

    pr2 using kdl, now need to link with rosgazebo.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@727 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/kinematics/kdl/pr2.cpp
+++ util/kinematics/kdl/pr2.cpp
@@ -1,34 +1,117 @@
-#include <pr2Core/pr2Core.h>
-#include <math.h>
 
-double a1x(0.1),a2x(0.5),a3x(0.59085),a4x(0.81455),a5x(0.81455);
-using namespace PR2;
-using namespace std;
+#include "kdl_kinematics.h"
 
 
-#include <kdl/chain.hpp>
-#include <kdl/chainfksolver.hpp>
-#include <kdl/chainfksolverpos_recursive.hpp>
-#include <kdl/chainfksolvervel_recursive.hpp>
-#include <kdl/chainiksolvervel_pinv.hpp>
-#include <kdl/chainiksolverpos_nr.hpp>
-#include <kdl/kinfam_io.hpp>
-#include <kdl/frames_io.hpp>
-#include <kdl/framevel_io.hpp>
-#include <stdio.h>
-#include <iostream>
-#include <math.h>
+PR2_kinematics::PR2_kinematics()
+{
+	//---------- create the chain for the PR2 ----------
+	Chain pr2_chain;
+	pr2_chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Vector(ARM_PAN_SHOULDER_PITCH_OFFSET.x,0.0,0.0))));
+	pr2_chain.addSegment(Segment(Joint(Joint::RotY),Frame(Vector(ARM_SHOULDER_PITCH_ROLL_OFFSET.x,0.0,0.0))));
+	pr2_chain.addSegment(Segment(Joint(Joint::RotX),Frame(Vector(ARM_SHOULDER_ROLL_ELBOW_PITCH_OFFSET.x,0.0,0.0))));
+	pr2_chain.addSegment(Segment(Joint(Joint::RotY),Frame(Vector(ELBOW_PITCH_ELBOW_ROLL_OFFSET.x,0.0,0.0))));
+	pr2_chain.addSegment(Segment(Joint(Joint::RotX),Frame(Vector(ELBOW_ROLL_WRIST_PITCH_OFFSET.x,0.0,0.0))));
+	pr2_chain.addSegment(Segment(Joint(Joint::RotY),Frame(Vector(WRIST_PITCH_WRIST_ROLL_OFFSET.x,0.0,0.0))));
+	pr2_chain.addSegment(Segment(Joint(Joint::RotX),Frame(Vector(WRIST_ROLL_GRIPPER_OFFSET.x,0.0,0.0))));
 
-#define RTOD(r) ((r)*180/M_PI)
-#define DTOR(d) ((d)*M_PI/180)
+	this->chain = &pr2_chain;
+	this->nJnts = pr2_chain.getNrOfJoints();
 
-using namespace KDL;
-using namespace std;
+	//------ Forward Position Kinematics --------------
+	this->fk_pos_solver = new ChainFkSolverPos_recursive(pr2_chain);
+
+	//------- IK
+	this->ik_vel_solver = new ChainIkSolverVel_pinv(pr2_chain);
+	this->ik_pos_solver = new ChainIkSolverPos_NR(*this->chain, *this->fk_pos_solver, *this->ik_vel_solver);
+}
+
+
+/*
+ * f - end effector frame (result of the fwd kinematics)
+ * returns True if ok, False if error.
+ */
+bool PR2_kinematics::FK(const JntArray &q, Frame &f)
+{
+	if (this->fk_pos_solver->JntToCart(q,f) >= 0)
+		return true;
+	else
+		return false;
+}
+
+bool PR2_kinematics::IK(const JntArray &q_init, const Frame &f, JntArray &q_out)
+{
+	if (this->ik_pos_solver->CartToJnt(q_init,f,q_out) >= 0)
+	{
+		angle_within_mod180(q_out, this->nJnts);
+		return true;
+	}
+	else
+		return false;
+}
+
+
+// returns a%b
+double modulus_double(double a, double b)
+{
+	int quo = a/b;
+	return a-b*quo;
+}
+
+double angle_within_mod180(double ang)
+{
+	double rem = modulus_double(ang, 2*M_PI);
+	if (rem>M_PI)
+		rem -= 2*M_PI;
+
+	return rem;
+}
+
+void angle_within_mod180(JntArray &q, int nJnts)
+{
+	for(int i=0;i<nJnts;i++)
+		q(i) = angle_within_mod180(q(i));
+}
 
 
 int main( int argc, char** argv )
 {
+	PR2_kinematics pr2_kin;
 
+	JntArray pr2_config = JntArray(pr2_kin.nJnts);
+	pr2_config(0) = 0.1, pr2_config(1) = -1, pr2_config(2)=0.3, pr2_config(3)=0.3;
+	pr2_config(4) = 0.2, pr2_config(5) = 0.5, pr2_config(6) = 0.0;
+	cout<<"Config of the arm:"<<pr2_config<<endl;
+
+	Frame f;
+	if (pr2_kin.FK(pr2_config,f))
+		cout<<"End effector transformation:"<<f<<endl;
+	else
+		cout<<"Could not compute Fwd Kin."<<endl;
+
+	JntArray q_init = JntArray(pr2_kin.nJnts);
+	q_init(0) = 0.1, q_init(1) = 0.0, q_init(2) = 0.0, q_init(3) = 0.0;
+	q_init(4) = 0.0, q_init(5) = 0.0, q_init(6) = 0.0;
+
+	JntArray q_out = JntArray(pr2_kin.nJnts);
+	if (pr2_kin.IK(q_init, f, q_out) == true)
+		cout<<"IK result:"<<q_out<<endl;
+	else
+		cout<<"Could not compute Inv Kin."<<endl;
+
+	//------ checking that IK returned a valid soln -----
+	Frame f_ik;
+	if (pr2_kin.FK(q_out,f_ik))
+		cout<<"End effector after IK:"<<f_ik<<endl;
+	else
+		cout<<"Could not compute Fwd Kin. (After IK)"<<endl;
+
+}
+
+
+/*
+
+int main( int argc, char** argv )
+{
 	//---------- create the chain for the PR2 ----------
 	Chain pr2_chain;
 	pr2_chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Vector(ARM_PAN_SHOULDER_PITCH_OFFSET.x,0.0,0.0))));
@@ -41,6 +124,10 @@ int main( int argc, char** argv )
 
 	int nJts = pr2_chain.getNrOfJoints();
 	JntArray pr2_config = JntArray(nJts);
+	
+	pr2_config(0) = 0.1, pr2_config(1) = -1, pr2_config(2)=0.3, pr2_config(3)=0.3;
+	pr2_config(4) = 0.2, pr2_config(5) = 0.5, pr2_config(6) = 0.0;
+	cout<<"Config of the arm:"<<pr2_config<<endl;
 
 	//------ Forward Position Kinematics --------------
 	ChainFkSolverPos_recursive fk_pr2 = ChainFkSolverPos_recursive(pr2_chain);
@@ -78,6 +165,7 @@ int main( int argc, char** argv )
 		printf("Error: could not calculate forward kinematics\n");
 
 }
+*/
 
 
 
