b5bea376489d754734ac2a536fc4b8eec9cdf46b
jleibs <jleibs@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-06-11 08:13:04 +0000

    Assorted changes to make tilting laser behave more nicely.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@735 f5854215-dd47-0410-b2c4-cdd35faa7885

--- drivers/robot/pr2/tilting_laser/src/tilting_laser/tilting_laser.cpp
+++ drivers/robot/pr2/tilting_laser/src/tilting_laser/tilting_laser.cpp
@@ -56,6 +56,7 @@ public:
   LaserImage image;
 
   PointCloudFloat32 full_cloud;
+  int full_cloud_cnt;
 
   int num_scans;
 
@@ -77,7 +78,7 @@ public:
 
   bool scan_received;
 
-  Tilting_Laser() : ros::node("tilting_laser"), sizes_ready(false), img_ind(-1), img_dir(1),last_ang(1000000), rate_err_down(0.0), rate_err_up(0.0), accum_angle(0.0), scan_received(false)
+  Tilting_Laser() : ros::node("tilting_laser"), full_cloud_cnt(0), sizes_ready(false), img_ind(-1), img_dir(1),last_ang(1000000), rate_err_down(0.0), rate_err_up(0.0), accum_angle(0.0), scan_received(false)
   {
     
     advertise<PointCloudFloat32>("cloud");
@@ -87,15 +88,14 @@ public:
     advertise<Image>("image");
     advertise<Actuator>("mot_cmd");
 
-    subscribe("scan", scans, &Tilting_Laser::scans_callback,10);
-    subscribe("mot",  encoder, &Tilting_Laser::encoder_callback,10);
+    subscribe("scan", scans, &Tilting_Laser::scans_callback,40);
+    subscribe("mot",  encoder, &Tilting_Laser::encoder_callback,40);
 
     param("tilting_laser/num_scans", num_scans, 400);
     param("tilting_laser/min_ang", min_ang, -1.3);
     param("tilting_laser/max_ang", max_ang, 0.6);
 
     last_motor_time = ros::Time::now();
-
   }
 
   virtual ~Tilting_Laser()
@@ -104,11 +104,11 @@ public:
   }
 
   void scans_callback() {
-
     scan_received = true;
 
     //TODO: Add check for hokuyo changing width!
-    if (!sizes_ready) {
+    if (!sizes_ready)
+    {
       image.range_img.height = num_scans;
       image.range_img.width = scans.get_ranges_size();
       image.range_img.set_data_size(2*image.range_img.height*image.range_img.width);
@@ -133,7 +133,6 @@ public:
     }
 
     cloud.set_pts_size(scans.get_ranges_size());
-
     cloud.set_chan_size(1);
     cloud.chan[0].name = "intensities";
     cloud.chan[0].set_vals_size(scans.get_ranges_size());
@@ -171,74 +170,83 @@ public:
     else
       ind = (num_scans - img_ind - 1);
 
+    if (img_ind >= 0)
+    {
 
-    NEWMAT::Matrix point(4,1);
-    NEWMAT::Matrix rot_point(4,1);
+      NEWMAT::Matrix point(4,1);
+      NEWMAT::Matrix rot_point(4,1);
 
-    for (uint32_t i = 0; i < scans.get_ranges_size(); i++) {
-      if (scans.ranges[i] < 20.0) {
-	point(1,1) = cos(scans.angle_min + i*scans.angle_increment) * scans.ranges[i];
-	point(2,1) = sin(scans.angle_min + i*scans.angle_increment) * scans.ranges[i];
-	point(3,1) = 0.0;
-	point(4,1) = 1.0;
-      } else {
-	point(1,1) = 0.0;
-	point(2,1) = 0.0;
-	point(3,1) = 0.0;
-	point(4,1) = 1.0;
-      }
+      int cnt = 0;
+
+      for (uint32_t i = 0; i < scans.get_ranges_size(); i++) {
+        bool valid = false;
+        if (scans.ranges[i] < scans.range_max && scans.ranges[i] > scans.range_min)
+          valid = true;
+        
+        if (valid) {
+          point(1,1) = cos(scans.angle_min + i*scans.angle_increment) * scans.ranges[i];
+          point(2,1) = sin(scans.angle_min + i*scans.angle_increment) * scans.ranges[i];
+          point(3,1) = 0.0;
+          point(4,1) = 1.0;
+        } else {
+          point(1,1) = 0.0;
+          point(2,1) = 0.0;
+          point(3,1) = 0.0;
+          point(4,1) = 1.0;
+        }
       
-      long long inc = scans.time_increment * i * 1000000000;
-      unsigned long long t = scans.header.stamp.to_ull() + inc;
-      rot_point = TR.getMatrix(1,3,t) * point;
+        long long inc = scans.time_increment * i * 1000000000;
+        unsigned long long t = scans.header.stamp.to_ull() + inc;
+        rot_point = TR.getMatrix(1,3,t) * point;
 
-      cloud.pts[i].x = rot_point(1,1);
-      cloud.pts[i].y = rot_point(2,1);
-      cloud.pts[i].z = rot_point(3,1);
-      cloud.chan[0].vals[i] = scans.intensities[i];
+        if (valid)
+        {
+          cloud.pts[i].x = rot_point(1,1);
+          cloud.pts[i].y = rot_point(2,1);
+          cloud.pts[i].z = rot_point(3,1);
+          cloud.chan[0].vals[i] = scans.intensities[i];
+          cnt++;
 
-      if (img_ind >= 0)
-      {
-        full_cloud.pts[img_ind * scans.get_ranges_size() + i].x = rot_point(1,1);
-        full_cloud.pts[img_ind * scans.get_ranges_size() + i].y = rot_point(2,1);
-        full_cloud.pts[img_ind * scans.get_ranges_size() + i].z = rot_point(3,1);
-	full_cloud.chan[0].vals[img_ind * scans.get_ranges_size() + i] = scans.intensities[i];
+          full_cloud.pts[img_ind * scans.get_ranges_size() + i].x = rot_point(1,1);
+          full_cloud.pts[img_ind * scans.get_ranges_size() + i].y = rot_point(2,1);
+          full_cloud.pts[img_ind * scans.get_ranges_size() + i].z = rot_point(3,1);
+          full_cloud.chan[0].vals[img_ind * scans.get_ranges_size() + i] = scans.intensities[i];
+          full_cloud_cnt++;
+        }
         
 	*(unsigned short*)(&(image.intensity_img.data[ind * 2 * image.intensity_img.width + image.intensity_img.width * 2 - 2 - 2*i])) = scans.intensities[i];
 	*(unsigned short*)(&(image.range_img.data[ind * 2 * image.range_img.width + image.range_img.width * 2 - 2 - 2*i])) = scans.ranges[i] * 1000.0;
 	image.horiz_angles[i] = scans.angle_min + i*scans.angle_increment;
       }
-    }
 
-    if (img_ind >= 0)
-    {
       image.vert_angles[ind] = ang;
+      cloud.set_pts_size(cnt);
+      cloud.chan[0].set_vals_size(cnt);
+      publish("cloud", cloud);
       img_ind++;
-    }
 
-    publish("cloud", cloud);
+      if (img_ind == num_scans) {
+        printf("Scan completed.\n");
+        
+        if (img_dir == 1)
+          rate_err_down -= (min_ang - ang) / (scans.scan_time * num_scans);
+        else
+          rate_err_up += (max_ang - ang) / (scans.scan_time * num_scans);
 
-    if (img_ind == num_scans) {
-      printf("Scan completed.\n");
 
-      double ang_err;
+        publish("laser_image", image);
+        publish("image", image.intensity_img);
+        full_cloud.set_pts_size(full_cloud_cnt);
+        full_cloud.chan[0].set_vals_size(full_cloud_cnt);
+        publish("full_cloud", full_cloud);
+        full_cloud.set_pts_size(scans.get_ranges_size()*num_scans);
+        full_cloud.chan[0].set_vals_size(scans.get_ranges_size()*num_scans);
+        publish("shutter",shutter);
+        full_cloud_cnt = 0;
 
-      if (img_dir == 1)
-      {
-	ang_err = min_ang - ang;
-	rate_err_down -= ang_err / (scans.scan_time * num_scans);
+        img_ind = -1;
+        sizes_ready = false;
       }
-      else
-      {
-	ang_err = max_ang - ang;
-	rate_err_up += ang_err / (scans.scan_time * num_scans);
-      }
-
-      publish("laser_image", image);
-      publish("image", image.intensity_img);
-      publish("full_cloud", full_cloud);
-      publish("shutter",shutter);
-      img_ind = -1;
     }
   }
 
@@ -260,7 +268,10 @@ public:
   void motor_control() {
 
     if (!scan_received)
+    {
+      last_motor_time = ros::Time::now();
       return;
+    }
 
     cmd.rel = false;
     cmd.valid = true;    
@@ -278,9 +289,16 @@ public:
 
     accum_angle += (t - last_motor_time).to_double() * rate;
 
-    double index = fabs(fmod( (accum_angle) / (2.0 * (ext_max_ang - ext_min_ang)), 1) * 2.0 - 1.0);
+    double index = fabs(fmod( (accum_angle) / (2.0 * (ext_max_ang - ext_min_ang + .1)), 1) * 2.0 - 1.0);
 
-    cmd.val = index * (ext_max_ang - ext_min_ang) + ext_min_ang;
+    cmd.val = index * (ext_max_ang - ext_min_ang + .1) + ext_min_ang -.05;
+
+    if (cmd.val > ext_max_ang)
+      cmd.val = ext_max_ang;
+
+    if (cmd.val < ext_min_ang)
+      cmd.val = ext_min_ang;
+
     publish("mot_cmd",cmd);
 
     last_motor_time = t;
