4ad8be756b7e3ef822453308afe8440166516cb0
advaitjain <advaitjain@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-06-11 18:01:46 +0000

    making a library for the kdl kinematics, similar to libKinematics.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@746 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ util/kinematics/kdl/include/kdl_kinematics.h
@@ -0,0 +1,47 @@
+
+#ifndef KDL_KINEMATICS_H
+#define KDL_KINEMATICS_H
+
+#include <kdl/chain.hpp>
+#include <kdl/chainfksolver.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chainfksolvervel_recursive.hpp>
+#include <kdl/chainiksolvervel_pinv.hpp>
+#include <kdl/chainiksolverpos_nr.hpp>
+#include <kdl/kinfam_io.hpp>
+#include <kdl/frames_io.hpp>
+#include <kdl/framevel_io.hpp>
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+
+#include <pr2Core/pr2Core.h>
+
+#define RTOD(r) ((r)*180/M_PI)
+#define DTOR(d) ((d)*M_PI/180)
+
+double modulus_double(double a, double b);
+double angle_within_mod180(double ang);
+void angle_within_mod180(KDL::JntArray &q, int nJnts);
+
+class PR2_kinematics
+{
+	private:
+		KDL::Chain *chain;
+		KDL::ChainFkSolverPos_recursive *fk_pos_solver;
+		KDL::ChainIkSolverVel_pinv *ik_vel_solver;
+		KDL::ChainIkSolverPos_NR *ik_pos_solver;
+	
+	public:
+		int nJnts;
+
+	public:
+		PR2_kinematics();
+		bool FK(const KDL::JntArray &q, KDL::Frame &f);
+		bool IK(const KDL::JntArray &q_init, const KDL::Frame &f, KDL::JntArray &q_out);
+
+};
+
+#endif
+
+
--- util/kinematics/kdl/test/pr2_kin_test.cpp
+++ util/kinematics/kdl/test/pr2_kin_test.cpp
@@ -1,76 +1,10 @@
 
 #include "kdl_kinematics.h"
 
+using namespace KDL;
+using namespace PR2;
+using namespace std;
 
-PR2_kinematics::PR2_kinematics()
-{
-	//---------- create the chain for the PR2 ----------
-	Chain pr2_chain;
-	pr2_chain.addSegment(Segment(Joint(Joint::RotZ),Frame(Vector(ARM_PAN_SHOULDER_PITCH_OFFSET.x,0.0,0.0))));
-	pr2_chain.addSegment(Segment(Joint(Joint::RotY),Frame(Vector(ARM_SHOULDER_PITCH_ROLL_OFFSET.x,0.0,0.0))));
-	pr2_chain.addSegment(Segment(Joint(Joint::RotX),Frame(Vector(ARM_SHOULDER_ROLL_ELBOW_PITCH_OFFSET.x,0.0,0.0))));
-	pr2_chain.addSegment(Segment(Joint(Joint::RotY),Frame(Vector(ELBOW_PITCH_ELBOW_ROLL_OFFSET.x,0.0,0.0))));
-	pr2_chain.addSegment(Segment(Joint(Joint::RotX),Frame(Vector(ELBOW_ROLL_WRIST_PITCH_OFFSET.x,0.0,0.0))));
-	pr2_chain.addSegment(Segment(Joint(Joint::RotY),Frame(Vector(WRIST_PITCH_WRIST_ROLL_OFFSET.x,0.0,0.0))));
-	pr2_chain.addSegment(Segment(Joint(Joint::RotX),Frame(Vector(WRIST_ROLL_GRIPPER_OFFSET.x,0.0,0.0))));
-
-	this->chain = &pr2_chain;
-	this->nJnts = pr2_chain.getNrOfJoints();
-
-	//------ Forward Position Kinematics --------------
-	this->fk_pos_solver = new ChainFkSolverPos_recursive(pr2_chain);
-
-	//------- IK
-	this->ik_vel_solver = new ChainIkSolverVel_pinv(pr2_chain);
-	this->ik_pos_solver = new ChainIkSolverPos_NR(*this->chain, *this->fk_pos_solver, *this->ik_vel_solver);
-}
-
-
-/*
- * f - end effector frame (result of the fwd kinematics)
- * returns True if ok, False if error.
- */
-bool PR2_kinematics::FK(const JntArray &q, Frame &f)
-{
-	if (this->fk_pos_solver->JntToCart(q,f) >= 0)
-		return true;
-	else
-		return false;
-}
-
-bool PR2_kinematics::IK(const JntArray &q_init, const Frame &f, JntArray &q_out)
-{
-	if (this->ik_pos_solver->CartToJnt(q_init,f,q_out) >= 0)
-	{
-		angle_within_mod180(q_out, this->nJnts);
-		return true;
-	}
-	else
-		return false;
-}
-
-
-// returns a%b
-double modulus_double(double a, double b)
-{
-	int quo = a/b;
-	return a-b*quo;
-}
-
-double angle_within_mod180(double ang)
-{
-	double rem = modulus_double(ang, 2*M_PI);
-	if (rem>M_PI)
-		rem -= 2*M_PI;
-
-	return rem;
-}
-
-void angle_within_mod180(JntArray &q, int nJnts)
-{
-	for(int i=0;i<nJnts;i++)
-		q(i) = angle_within_mod180(q(i));
-}
 
 
 int main( int argc, char** argv )
