ae337e4f94ab2c9e2705c2c5c068a4f0473796a8
gerkey <gerkey@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-06-12 02:58:37 +0000

    working on map service request
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@770 f5854215-dd47-0410-b2c4-cdd35faa7885

--- nav/nav_view/nav_view.cpp
+++ nav/nav_view/nav_view.cpp
@@ -45,18 +45,13 @@
 
 @section usage Usage
 @verbatim
-$ nav_view [map res] [standard ROS args]
+$ nav_view
 @endverbatim
-
-@param map An image file to load as an occupancy grid map. The lower-left pixel of the map is assigned the pose (0,0,0).
-@param res The resolution of the map, in meters, pixel.
-
-@todo Remove the map and res arguments in favor map retrieval via ROSRPC.
 
 @par Example
 
 @verbatim
-$ nav_view mymap.png 0.1 odom:=localizedpose
+$ nav_view
 @endverbatim
 
 @par GUI controls
@@ -95,37 +90,42 @@ Publishes to (name / type):
 #include <unistd.h>
 #include <assert.h>
 #include <math.h>
+
+// OpenGL
 #include <GL/gl.h>
 #include <SDL/SDL_image.h>
-#include "ros/node.h"
-//#include "std_msgs/MsgRobotBase2DOdom.h"
-#include "std_msgs/MsgParticleCloud2D.h"
-#include "std_msgs/MsgPlanner2DGoal.h"
-//#include "std_msgs/MsgPlanner2DState.h"
-#include "std_msgs/MsgPolyline2D.h"
-#include "std_msgs/MsgPose2DFloat32.h"
-#include "sdlgl/sdlgl.h"
 
+// roscpp and friends
+#include "ros/node.h"
 #include "rosTF/rosTF.h"
 #include "rostime/clock.h"
+
+// messages and services
+#include "std_msgs/ParticleCloud2D.h"
+#include "std_msgs/Planner2DGoal.h"
+#include "std_msgs/Polyline2D.h"
+#include "std_msgs/Pose2DFloat32.h"
+#include "std_srvs/StaticMap.h"
+#include "sdlgl/sdlgl.h"
 
 class NavView : public ros::node, public ros::SDLGL
 {
 public:
-  //MsgRobotBase2DOdom odom;
-  MsgParticleCloud2D cloud;
-  MsgPlanner2DGoal goal;
-  //MsgPlanner2DState pstate;
-  MsgPolyline2D pathline;
-  MsgPolyline2D laserscan;
-  MsgPose2DFloat32 initialpose;
+  std_msgs::ParticleCloud2D cloud;
+  std_msgs::Planner2DGoal goal;
+  std_msgs::Polyline2D pathline;
+  std_msgs::Polyline2D laserscan;
+  std_msgs::Pose2DFloat32 initialpose;
   float view_scale, view_x, view_y;
   SDL_Surface* map_surface;
   GLuint map_texture;
   double map_res;
+  int map_width, map_height;
   int gwidth, gheight;
   bool setting_theta;
   double gx,gy;
+
+  std_srvs::StaticMap::response map_resp;
 
   rosTFClient tf;
   ros::time::clock myClock;
@@ -134,18 +134,17 @@ public:
 	      view_scale(10), view_x(0), view_y(0),
 	      tf(*this,false)
   {
-    advertise<MsgPlanner2DGoal>("goal");
-    advertise<MsgPose2DFloat32>("initialpose");
-    //subscribe("state", pstate, &NavView::pstate_cb);
-    //subscribe("odom", odom, &NavView::odom_cb); //replaced with rosTF
-    subscribe("particlecloud", cloud, &NavView::odom_cb);
-    subscribe("gui_path", pathline, &NavView::odom_cb);
-    subscribe("gui_laser", laserscan, &NavView::odom_cb);
+    advertise<std_msgs::Planner2DGoal>("goal");
+    advertise<std_msgs::Pose2DFloat32>("initialpose");
+    subscribe("particlecloud", cloud, &NavView::generic_cb);
+    subscribe("gui_path", pathline, &NavView::generic_cb);
+    subscribe("gui_laser", laserscan, &NavView::generic_cb);
     gwidth = 1024;
     gheight = 768;
     init_gui(gwidth, gheight, "nav view");
     glClearColor(1.0,1.0,1.0,0);
     map_surface = NULL;
+    map_texture = 0;
     setting_theta = false;
   }
 
@@ -155,124 +154,18 @@ public:
       SDL_FreeSurface(map_surface);
   }
   
-  void load_map(const char* fname, double res);
+  bool load_map();
+  bool load_map_from_image(const char*, double);
+
   SDL_Surface* flip_vert(SDL_Surface* sfc);
 
-  void odom_cb()
+  void generic_cb()
   {
     request_render();
   }
 
-  /*
-  void pstate_cb()
-  {
-    printf("Planner state: %d %d %d (%.3f %.3f %.3f)\n", 
-           pstate.active,
-           pstate.valid,
-           pstate.done,
-           pstate.pos.x,
-           pstate.pos.y,
-           pstate.pos.th);
-  }
-  */
-  void render()
-  {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
-    glScalef(view_scale, view_scale, view_scale);
-    glTranslatef(view_x, view_y, 0);
-    
-    if(map_surface)
-    {
-      // Draw the map
-      glColor3f(1.0,1.0,1.0);
-      glBindTexture( GL_TEXTURE_2D, map_texture );
-      // Bind the texture to which subsequent calls refer to
-      glBegin( GL_QUADS );
-      //Top-left vertex (corner)
-      glTexCoord2i( 0, 0 );
-      glVertex3f( 0, 0, 0.0f );
+  void render();
 
-      //Bottom-left vertex (corner)
-      glTexCoord2i( 1, 0 );
-      glVertex3f( map_res*map_surface->w, 0, 0 );
-
-      //Bottom-right vertex (corner)
-      glTexCoord2i( 1, 1 );
-      glVertex3f( map_res*map_surface->w, map_res*map_surface->h, 0 );
-
-      //Top-right vertex (corner)
-      glTexCoord2i( 0, 1 );
-      glVertex3f( 0, map_res*map_surface->h, 0 );
-      glEnd();
-    }
-
-    const float robot_rad = 0.3;
-    try
-    {
-      libTF::TFPose2D robotPose;
-      robotPose.x = 0;
-      robotPose.y = 0;
-      robotPose.yaw = 0;
-      robotPose.frame = FRAMEID_ROBOT;
-      robotPose.time = 0;
-
-      libTF::TFPose2D mapPose = tf.transformPose2D(FRAMEID_MAP, robotPose);
-
-      glPushMatrix();
-      glTranslatef(mapPose.x, mapPose.y, 0);
-      glRotatef(mapPose.yaw * 180 / M_PI, 0, 0, 1);
-      glColor3f(0.2, 1.0, 0.4);
-      glBegin(GL_LINE_LOOP);
-      for (float f = 0; f < (float)(2*M_PI); f += 0.5f)
-        glVertex2f(robot_rad * cos(f), robot_rad * sin(f));
-      glEnd();
-      glBegin(GL_LINES);
-      glVertex2f(0,0);
-      glVertex2f(robot_rad,0);
-      glEnd();
-      glPopMatrix();
-    }
-    catch(libTF::TransformReference::LookupException& ex)
-    {
-    }
-    
-    cloud.lock();
-    for(unsigned int i=0;i<cloud.get_particles_size();i++)
-    {
-      glPushMatrix();
-      glTranslatef(cloud.particles[i].x, cloud.particles[i].y, 0);
-      glRotatef(cloud.particles[i].th * 180 / M_PI, 0, 0, 1);
-      glColor3f(1.0, 0.0, 0.0);
-      glBegin(GL_LINE_LOOP);
-      glVertex2f(0,0);
-      glVertex2f(robot_rad,0);
-      glVertex2f(0.75*robot_rad,-0.25*robot_rad);
-      glVertex2f(0.75*robot_rad,0.25*robot_rad);
-      glVertex2f(robot_rad,0);
-      glEnd();
-      glPopMatrix();
-    }
-    cloud.unlock();
-
-    pathline.lock();
-    glColor3f(pathline.color.r,pathline.color.g,pathline.color.b);
-    glBegin(GL_LINES);
-    for(unsigned int i=0;i<pathline.get_points_size();i++)
-      glVertex2f(pathline.points[i].x,pathline.points[i].y);
-    glEnd();
-    pathline.unlock();
-
-    pathline.lock();
-    glColor3f(laserscan.color.r,laserscan.color.g,laserscan.color.b);
-    glBegin(GL_POINTS);
-    for(unsigned int i=0;i<laserscan.get_points_size();i++)
-      glVertex2f(laserscan.points[i].x,laserscan.points[i].y);
-    glEnd();
-    pathline.unlock();
-
-    SDL_GL_SwapBuffers();
-  }
   void set_view_params(int width, int height)
   {
     glMatrixMode(GL_PROJECTION);
@@ -280,6 +173,7 @@ public:
     glOrtho(-width/2, width/2, -height/2, height/2, -1, 1);
     glMatrixMode(GL_MODELVIEW);
   }
+
   virtual void mouse_motion(int x, int y, int dx, int dy, int buttons)
   {
     if (buttons & SDL_BUTTON(1)) // left button: translate view
@@ -294,63 +188,8 @@ public:
       request_render();
     }
   }
-  virtual void mouse_button(int x, int y, int button, bool is_down) 
-  { 
-    if((button == 2) && !is_down) // middle mouse button
-    {
-      // If this is the first click, we're setting the position, next click
-      // will give direction
-      if(!setting_theta)
-      {
-        // Convert to conventional coords
-        gx = (x-gwidth/2.0)/view_scale-view_x;
-        gy = -((y-gheight/2.0)/view_scale+view_y);
 
-        setting_theta = true;
-      }
-      else
-      {
-        // Convert to conventional coords
-        double nx = (x-gwidth/2.0)/view_scale-view_x;
-        double ny = -((y-gheight/2.0)/view_scale+view_y);
-
-        // Compute orientation
-        double ga = atan2(ny-gy,nx-gx);
-        
-        // Check modifier keys
-        Uint8* keystate = SDL_GetKeyState(NULL);
-        // If shift is down, set initial pose
-        if(keystate[SDLK_LSHIFT] || keystate[SDLK_RSHIFT])
-        {
-          // Send out the pose
-          initialpose.x = gx;
-          initialpose.y = gy;
-          initialpose.th = ga;
-          printf("setting pose: %.3f %.3f %.3f\n",
-                 initialpose.x,
-                 initialpose.y,
-                 initialpose.th);
-          publish("initialpose", initialpose);
-        }
-        // No modifiers; set goal
-        else 
-        {
-          // Send out the goal
-          goal.goal.x = gx;
-          goal.goal.y = gy;
-          goal.goal.th = ga;
-          goal.enable = 1;
-          printf("setting goal: %.3f %.3f %.3f\n",
-                 goal.goal.x,
-                 goal.goal.y,
-                 goal.goal.th);
-          publish("goal", goal);
-        }
-
-        setting_theta = false;
-      }
-    }
-  }
+  virtual void mouse_button(int x, int y, int button, bool is_down);
 };
 
 void
@@ -361,34 +200,253 @@ quit(int sig)
   exit(0);
 }
 
-int main(int argc, char **argv)
+int 
+main(int argc, char **argv)
 {
-  char* fname=NULL;
-  double res=0.0;
-
   signal(SIGINT, quit);
   signal(SIGPIPE, SIG_IGN);
 
   ros::init(argc, argv);
   
-  // HACK: assume args are: image filename and resolution
-  if(argc >= 3)
-  {
-    fname = argv[1];
-    res = atof(argv[2]);
-  }
-
-
   NavView view;
-  if(fname)
-    view.load_map(fname,res);
+  //if(!view.load_map())
+  if(!view.load_map_from_image(argv[1],atof(argv[2])))
+    puts("WARNING: failed to load map");
   view.main_loop();
   ros::fini();
   return 0;
 }
 
+void 
+NavView::mouse_button(int x, int y, int button, bool is_down) 
+{ 
+  if((button == 2) && !is_down) // middle mouse button
+  {
+    // If this is the first click, we're setting the position, next click
+    // will give direction
+    if(!setting_theta)
+    {
+      // Convert to conventional coords
+      gx = (x-gwidth/2.0)/view_scale-view_x;
+      gy = -((y-gheight/2.0)/view_scale+view_y);
+
+      setting_theta = true;
+    }
+    else
+    {
+      // Convert to conventional coords
+      double nx = (x-gwidth/2.0)/view_scale-view_x;
+      double ny = -((y-gheight/2.0)/view_scale+view_y);
+
+      // Compute orientation
+      double ga = atan2(ny-gy,nx-gx);
+
+      // Check modifier keys
+      Uint8* keystate = SDL_GetKeyState(NULL);
+      // If shift is down, set initial pose
+      if(keystate[SDLK_LSHIFT] || keystate[SDLK_RSHIFT])
+      {
+        // Send out the pose
+        initialpose.x = gx;
+        initialpose.y = gy;
+        initialpose.th = ga;
+        printf("setting pose: %.3f %.3f %.3f\n",
+               initialpose.x,
+               initialpose.y,
+               initialpose.th);
+        publish("initialpose", initialpose);
+      }
+      // No modifiers; set goal
+      else 
+      {
+        // Send out the goal
+        goal.goal.x = gx;
+        goal.goal.y = gy;
+        goal.goal.th = ga;
+        goal.enable = 1;
+        printf("setting goal: %.3f %.3f %.3f\n",
+               goal.goal.x,
+               goal.goal.y,
+               goal.goal.th);
+        publish("goal", goal);
+      }
+
+      setting_theta = false;
+    }
+  }
+}
+
 void
-NavView::load_map(const char* fname, double res)
+NavView::render()
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+  glLoadIdentity();
+  glScalef(view_scale, view_scale, view_scale);
+  glTranslatef(view_x, view_y, 0);
+
+  if(map_texture)
+  {
+    // Draw the map
+    glColor3f(1.0,1.0,1.0);
+    glBindTexture( GL_TEXTURE_2D, map_texture );
+    // Bind the texture to which subsequent calls refer to
+    glBegin( GL_QUADS );
+    //Top-left vertex (corner)
+    glTexCoord2i( 0, 0 );
+    glVertex3f( 0, 0, 0.0f );
+
+    //Bottom-left vertex (corner)
+    glTexCoord2i( 1, 0 );
+    glVertex3f( map_res*map_width, 0, 0 );
+
+    //Bottom-right vertex (corner)
+    glTexCoord2i( 1, 1 );
+    glVertex3f( map_res*map_width, map_res*map_height, 0 );
+
+    //Top-right vertex (corner)
+    glTexCoord2i( 0, 1 );
+    glVertex3f( 0, map_res*map_height, 0 );
+    glEnd();
+  }
+
+  const float robot_rad = 0.3;
+  try
+  {
+    libTF::TFPose2D robotPose;
+    robotPose.x = 0;
+    robotPose.y = 0;
+    robotPose.yaw = 0;
+    robotPose.frame = FRAMEID_ROBOT;
+    robotPose.time = 0;
+
+    libTF::TFPose2D mapPose = tf.transformPose2D(FRAMEID_MAP, robotPose);
+
+    glPushMatrix();
+    glTranslatef(mapPose.x, mapPose.y, 0);
+    glRotatef(mapPose.yaw * 180 / M_PI, 0, 0, 1);
+    glColor3f(0.2, 1.0, 0.4);
+    glBegin(GL_LINE_LOOP);
+    for (float f = 0; f < (float)(2*M_PI); f += 0.5f)
+      glVertex2f(robot_rad * cos(f), robot_rad * sin(f));
+    glEnd();
+    glBegin(GL_LINES);
+    glVertex2f(0,0);
+    glVertex2f(robot_rad,0);
+    glEnd();
+    glPopMatrix();
+  }
+  catch(libTF::TransformReference::LookupException& ex)
+  {
+  }
+
+  cloud.lock();
+  for(unsigned int i=0;i<cloud.get_particles_size();i++)
+  {
+    glPushMatrix();
+    glTranslatef(cloud.particles[i].x, cloud.particles[i].y, 0);
+    glRotatef(cloud.particles[i].th * 180 / M_PI, 0, 0, 1);
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(0,0);
+    glVertex2f(robot_rad,0);
+    glVertex2f(0.75*robot_rad,-0.25*robot_rad);
+    glVertex2f(0.75*robot_rad,0.25*robot_rad);
+    glVertex2f(robot_rad,0);
+    glEnd();
+    glPopMatrix();
+  }
+  cloud.unlock();
+
+  pathline.lock();
+  glColor3f(pathline.color.r,pathline.color.g,pathline.color.b);
+  glBegin(GL_LINES);
+  for(unsigned int i=0;i<pathline.get_points_size();i++)
+    glVertex2f(pathline.points[i].x,pathline.points[i].y);
+  glEnd();
+  pathline.unlock();
+
+  pathline.lock();
+  glColor3f(laserscan.color.r,laserscan.color.g,laserscan.color.b);
+  glBegin(GL_POINTS);
+  for(unsigned int i=0;i<laserscan.get_points_size();i++)
+    glVertex2f(laserscan.points[i].x,laserscan.points[i].y);
+  glEnd();
+  pathline.unlock();
+
+  SDL_GL_SwapBuffers();
+}
+
+bool
+NavView::load_map()
+{
+  std_srvs::StaticMap::request  req;
+  std_srvs::StaticMap::response res;
+  puts("Requesting the map...");
+  if(ros::service::call("static_map", req, res))
+  {
+    puts("success");
+    map_resp = res;
+    map_res = map_resp.map.resolution;
+    map_width = map_resp.map.width;
+    map_height = map_resp.map.height;
+
+    printf("Received a %d X %d map @ %.3f m/pix\n",
+           map_resp.map.width,
+           map_resp.map.height,
+           map_resp.map.resolution);
+
+    // Expand it to be RGB data
+    unsigned char* pixels = 
+            new unsigned char[map_resp.map.width * map_resp.map.height * 4];
+    for(unsigned int i=0;i<map_resp.map.width*map_resp.map.height;i++)
+    {
+      unsigned char val;
+      if(map_resp.map.data[i] == 100)
+        val = 0;
+      else if(map_resp.map.data[i] == 0)
+        val = 255;
+      else
+        val = 127;
+
+      pixels[4*i] = val;
+      pixels[4*i+1] = val;
+      pixels[4*i+2] = val;
+      pixels[4*i+3] = 255;
+    }
+
+    GLint nOfColors = 4;
+    GLenum texture_format = GL_RGBA;
+
+    // Have OpenGL generate a texture object handle for us
+    glGenTextures( 1, &map_texture );
+
+    // Bind the texture object
+    glBindTexture( GL_TEXTURE_2D, map_texture );
+
+    // Set the texture's stretching properties
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                     GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D,
+                     GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+    // Edit the texture object's image data using
+    //the information SDL_Surface gives us
+    glTexImage2D( GL_TEXTURE_2D, 0, nOfColors,
+                  map_width, map_height, 0,
+                  texture_format,
+                  GL_UNSIGNED_BYTE,
+                  pixels );
+    return(true);
+  }
+  else
+  {
+    puts("Error!  Failed to get the map.");
+    return(false);
+  }
+}
+
+bool
+NavView::load_map_from_image(const char* fname, double res)
 {
   map_res = res;
   glEnable( GL_TEXTURE_2D ); 
@@ -398,8 +456,11 @@ NavView::load_map(const char* fname, double res)
   {
     printf("Warning: failed to load map from file \"%s\"\n",
            fname);
-    return;
+    return(false);
   }
+
+  map_width = temp->w;
+  map_height = temp->h;
 
   // Check that the image's width is a power of 2
   if((temp->w & (temp->w - 1)) != 0) {
@@ -434,7 +495,7 @@ NavView::load_map(const char* fname, double res)
   } else {
     printf("warning: the image is not truecolor..  this will probably break\n");
     // this error should not go unhandled
-    return;
+    return(false);
   }
 
 
@@ -457,6 +518,7 @@ NavView::load_map(const char* fname, double res)
                 texture_format,
                 GL_UNSIGNED_BYTE,
                 map_surface->pixels );
+  return(true);
 }
 
 
