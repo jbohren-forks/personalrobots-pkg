fd746e7438aca6f766a679e1f4456d0a2c22e33f
hsujohnhsu <hsujohnhsu@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-06-21 06:23:05 +0000

    miscellaneous helper functions.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@889 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ drivers/robot/pr2/pr2Core/include/pr2Core/pr2Misc.h
@@ -0,0 +1,130 @@
+#ifndef __PR2MISC_H__
+#define __PR2MISC_H__
+
+#include<math.h>
+namespace PR2
+{
+
+    /* --------------- utility functions ---------------- */
+    /* ---------------        TODO       ---------------- */
+    /* --------------- move to a library ---------------- */
+
+    point Rot2D(point p,double theta)
+    {
+       point q;
+       q.x = cos(theta)*p.x - sin(theta)*p.y;
+       q.y = sin(theta)*p.y + cos(theta)*p.x;
+       return q;
+    }
+
+    bool IsHead(PR2_MODEL_ID id)
+    {
+       if(id == HEAD)
+          return true;
+       return false;
+    }
+    bool IsGripperLeft(PR2_MODEL_ID id)
+    {
+       if(id == PR2_LEFT_GRIPPER)
+          return true;
+       return false;
+    }
+    bool IsGripperRight(PR2_MODEL_ID id)
+    {
+       if(id == PR2_RIGHT_GRIPPER)
+          return true;
+       return false;
+    }
+
+    bool IsHead(PR2_JOINT_ID id)
+    {
+       if(id >= JointStart[HEAD] && id <= JointEnd[HEAD])
+          return true;
+       return false;
+    }
+
+    bool IsGripperLeft(PR2_JOINT_ID id)
+    {
+       if (id >= JointStart[ARM_L_GRIPPER] && id <= JointEnd[ARM_L_GRIPPER])
+          return true;
+       return false;
+    }
+    bool IsGripperRight(PR2_JOINT_ID id)
+    {
+       if (id >= JointStart[ARM_R_GRIPPER] && id <= JointEnd[ARM_R_GRIPPER])
+          return true;
+       return false;
+    }
+
+    double GetMagnitude(double xl[], int num)
+    {
+       int ii;
+       double mag=0;
+       for(ii=0; ii < num; ii++)
+          mag += (xl[ii]*xl[ii]); 
+       return sqrt(mag);
+    }
+
+    double GetMagnitude(double x, double y)
+    {
+       return sqrt(x*x+y*y);
+    }
+
+
+    #define FROM_DEGREES(degrees)	(((double)(degrees))/180*M_PI)
+    /*
+     * normalize_angle_positive
+     *
+     * Normalizes the angle to be 0 circle to 1 circle
+     * It takes and returns native units.
+     */
+
+    double normalize_angle_positive(double angle)
+    {
+      return fmod(fmod(angle, FROM_DEGREES(360))+FROM_DEGREES(360), FROM_DEGREES(360));
+    }
+    /*
+     * normalize
+     *
+     * Normalizes the angle to be -1/2 circle to +1/2 circle
+     * It takes and returns native units.
+     *
+     */
+
+    double normalize_angle(double angle)
+    {
+      double a=normalize_angle_positive(angle);
+      if (a>FROM_DEGREES(180))
+          a-=FROM_DEGREES(360);
+      return(a);
+    }
+    /*
+     * shortest_angular_distance
+     *
+     * Given 2 angles, this returns the shortest angular
+     * difference.  The inputs and ouputs are of course native
+     * units.
+     *
+     * As an example, if native units are degrees, the result
+     * would always be -180 <= result <= 180.  Adding the result
+     * to "from" will always get you an equivelent angle to "to".
+     */
+
+    double shortest_angular_distance(double from, double to)
+    {
+      double result;
+      result=normalize_angle_positive(
+          normalize_angle_positive(to) - normalize_angle_positive(from));
+
+      if ( result > FROM_DEGREES(180) ) {  // If the result > 180,
+                                           // It's shorter the other way.
+          result=-(FROM_DEGREES(360)-result);   
+      }
+      return normalize_angle(result);
+    }
+
+
+
+}
+
+#endif
