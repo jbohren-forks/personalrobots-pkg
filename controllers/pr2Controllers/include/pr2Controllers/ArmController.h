f7b150be32dbf61fa983cf7f642e01186c179f9d
hsujohnhsu <hsujohnhsu@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-06-27 23:46:21 +0000

    updates to ArmController. work in progress.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1074 f5854215-dd47-0410-b2c4-cdd35faa7885

--- controllers/pr2Controllers/include/pr2Controllers/ArmController.h
+++ controllers/pr2Controllers/include/pr2Controllers/ArmController.h
@@ -1,9 +1,9 @@
 #pragma once
 /***************************************************/
-/*! \brief A PR2 Head controller
+/*! \brief A PR2 Arm controller
     
     This class implements controller loops for
-    PR2 Head Control
+    PR2 Arm Control
 
 */
 /***************************************************/
@@ -14,11 +14,13 @@
 //#include <string>
 //#include <libKDL/kdl_kinematics.h> // for kinematics using KDL -- util/kinematics/libKDL
 
+#include <iostream>
+
 #include <pr2Core/pr2Core.h>
 #include <libpr2HW/pr2HW.h>
 
 
-class HeadController : Controller
+class ArmController : Controller
 {
   public:
   
@@ -27,15 +29,15 @@ class HeadController : Controller
       *
       * \param 
       */
-    HeadController();
+    ArmController();
     
     /*!
       * \brief Destructor of Pid class.
       */       
-    ~HeadController( );
+    ~ArmController( );
 
     /*!
-      * \brief Set yaw and pitch of head in Local Head Frame
+      * \brief Set yaw and pitch of head in Local Arm Frame
       * 
       */       
     PR2::PR2_ERROR_CODE setAngularPos(double yaw , double pitch);
@@ -59,16 +61,6 @@ class HeadController : Controller
     PR2::PR2_ERROR_CODE setTraj(int numPts, double x[],double y[], double yaw[], double vx[], double vy[], double yawDot[]);
 
     /*!
-      * \brief Heading pose for the robot
-      *
-      * Robot assume a stationary rotation mode, and achieve heading in Global Frame
-      * yaw=0 implies +x-axis direction,
-      * +yaw implies counter-clockwise
-      *
-      */       
-    PR2::PR2_ERROR_CODE setHeading(double yaw);
-
-    /*!
       * \brief Set force (linear summation of all wheels) in global frame
       */       
     PR2::PR2_ERROR_CODE setForce(double fx, double fy);
@@ -87,7 +79,7 @@ class HeadController : Controller
     PR2::PR2_ERROR_CODE setParam(string label,double value);
 
   private:
-    PR2::PR2_CONTROL_MODE controlMode;      /**< Head controller control mode >*/
+    PR2::PR2_CONTROL_MODE controlMode;      /**< Arm controller control mode >*/
 };
 
 
