33335a4ef693d33129b879edfe5a3601104187bd
hsujohnhsu <hsujohnhsu@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-06-27 23:43:22 +0000

    updates to LaserScannerController - Pitching Hokuyo controller.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1073 f5854215-dd47-0410-b2c4-cdd35faa7885

--- controllers/pr2Controllers/include/pr2Controllers/LaserScannerController.h
+++ controllers/pr2Controllers/include/pr2Controllers/LaserScannerController.h
@@ -1,9 +1,9 @@
 #pragma once
 /***************************************************/
-/*! \brief A PR2 Head controller
+/*! \brief A PR2 Pitching Hokuyo Joint controller
     
     This class implements controller loops for
-    PR2 Head Control
+    PR2 Pitching Hokuyo Joint
 
 */
 /***************************************************/
@@ -14,11 +14,13 @@
 //#include <string>
 //#include <libKDL/kdl_kinematics.h> // for kinematics using KDL -- util/kinematics/libKDL
 
+#include <iostream>
+
 #include <pr2Core/pr2Core.h>
 #include <libpr2HW/pr2HW.h>
 
 
-class HeadController : Controller
+class LaserScannerController : Controller
 {
   public:
   
@@ -27,51 +29,30 @@ class HeadController : Controller
       *
       * \param 
       */
-    HeadController();
+    LaserScannerController();
     
     /*!
       * \brief Destructor of Pid class.
       */       
-    ~HeadController( );
+    ~LaserScannerController( );
 
     /*!
-      * \brief Set yaw and pitch of head in Local Head Frame
+      * \brief Set scanning profile for the pitching Hokuyo
       * 
+      * To be determined on how to implement
+      * For now, use sawtooth or sine wave
+      * Consider also, using setParam('profile','sawtooth')
+      *
       */       
-    PR2::PR2_ERROR_CODE setAngularPos(double yaw , double pitch);
+    PR2::PR2_ERROR_CODE setProfile(double *t, double *x);
 
     /*!
-      * \brief Drive robot on a course in the Robot Frame
+      * \brief Set pitch angle of the pitching Hokuyo joint
       * 
-      * Same as setCourse except the inputs are the x and y components of velocities.
+      * pitch=0 means home position of horizontal scan.
       *
       */       
-    PR2::PR2_ERROR_CODE setCourseXY(double vx, double vy);
-
-    /*!
-      * \brief Set target point in Global Frame
-      */
-    PR2::PR2_ERROR_CODE setTarget(double x,double y, double yaw, double vx, double vy, double yawDot);
-
-    /*!
-      * \brief Set target points (trajectory list) in Global Frame
-      */       
-    PR2::PR2_ERROR_CODE setTraj(int numPts, double x[],double y[], double yaw[], double vx[], double vy[], double yawDot[]);
-
-    /*!
-      * \brief Heading pose for the robot
-      *
-      * Robot assume a stationary rotation mode, and achieve heading in Global Frame
-      * yaw=0 implies +x-axis direction,
-      * +yaw implies counter-clockwise
-      *
-      */       
-    PR2::PR2_ERROR_CODE setHeading(double yaw);
-
-    /*!
-      * \brief Set force (linear summation of all wheels) in global frame
-      */       
-    PR2::PR2_ERROR_CODE setForce(double fx, double fy);
+    PR2::PR2_ERROR_CODE SetPosition(double pitch);
 
     /*!
       * \brief Set parameters for this controller
@@ -80,14 +61,20 @@ class HeadController : Controller
       * and maximum acceleration
       * constraints for this controller
       *
-      * e.g. setParam('maxVel',10);
-      *   or setParam('maxAcc',10);
+      * e.g. setParam('maxVel', 1.0);
+      *   or setParam('maxAcc', 1.0);
+      *   or setParam('profile','sawtooth');
+      *   or setParam('profile','sinewave');
+      *   or setParam('maxLim', 1.0);
+      *   or setParam('minLim',-1.0);
+      *   or setParam('rateHz'  , 1);
       *
       */
     PR2::PR2_ERROR_CODE setParam(string label,double value);
+    PR2::PR2_ERROR_CODE setParam(string label,string value);
 
   private:
-    PR2::PR2_CONTROL_MODE controlMode;      /**< Head controller control mode >*/
+    PR2::PR2_CONTROL_MODE controlMode;      /**< Pitching Hokuyo laser scanner controller control mode >*/
 };
 
 
--- controllers/pr2Controllers/src/LaserScannerController.cpp
+++ controllers/pr2Controllers/src/LaserScannerController.cpp
@@ -2,4 +2,40 @@
 #include <controller/LaserScannerController.h>
 
 
+LaserScannerController::LaserScannerController()
+{
+}
+    
+LaserScannerController::~LaserScannerController( )
+{
+}
+
+PR2::PR2_ERROR_CODE
+LaserScannerController::setProfile(double *t, double *x)
+{
+
+  return PR2::PR2_ALL_OK;
+}
+
+PR2::PR2_ERROR_CODE
+LaserScannerController::SetPosition(double pitch)
+{
+
+  return PR2::PR2_ALL_OK;
+}
+
+PR2::PR2_ERROR_CODE
+LaserScannerController::setParam(string label,double value)
+{
+
+  return PR2::PR2_ALL_OK;
+}
+
+PR2::PR2_ERROR_CODE
+LaserScannerController::setParam(string label,string value)
+{
+
+  return PR2::PR2_ALL_OK;
+}
+
 
