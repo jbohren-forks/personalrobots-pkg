ef3d51a1d1ac9fc267a3b81b0940e99ed578f614
jleibs <jleibs@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-06-27 08:55:08 +0000

    Even though libdc1934v2 is fairly clean.  Object oriented wrappers are just friendlier to use.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1051 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ drivers/cam/dc1394_cams/include/dc1394_cam/dc1394_cam.h
@@ -0,0 +1,128 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+* 
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+* 
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+* 
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+* 
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+#ifndef DC1394_CAM_H
+#define DC1394_CAM_H
+
+#include <stdexcept>
+#include "dc1394/dc1394.h"
+
+
+
+#define CHECK_READY() \
+  if (!dcRef) { \
+    char msg[256]; \
+    snprintf(msg, 256, "Tried to call %s before calling dc1394_cam::Cam::init()", __FUNCTION__); \
+    throw CamException(msg); \
+  }
+
+#define CHECK_ERR(fnc, amsg) \
+  { \
+  dc1394error_t err = fnc; \
+  if (err != DC1394_SUCCESS) { \
+    char msg[256]; \
+    snprintf(msg, 256, "%s: %s", dc1394_error_get_string(err), amsg);        \
+    throw CamException(msg); \
+  } \
+  }
+
+#define CHECK_ERR_CLEAN(fnc, amsg) \
+  { \
+  dc1394error_t err = fnc; \
+  if (err != DC1394_SUCCESS) { \
+    cleanup(); \
+    char msg[256]; \
+    snprintf(msg, 256, "%s: %s", dc1394_error_get_string(err), amsg);        \
+    throw CamException(msg); \
+  }\
+  }
+
+namespace dc1394_cam
+{
+
+  class CamException : public std::runtime_error
+  {
+  public:
+    CamException(const char* msg) : std::runtime_error(msg) {}
+  };
+  
+
+  void init();
+  void fini();
+  size_t numCams();
+  uint64_t getGuid(size_t i);
+
+  class Cam
+  {
+  private:
+    static dc1394_t* dcRef;
+
+    void cleanup();
+    bool started;
+
+  public:
+
+    static void init();
+
+    static void fini();
+
+    static size_t numCams();
+
+    static uint64_t getGuid(size_t i);
+
+    Cam(uint64_t guid,
+        dc1394speed_t speed = DC1394_ISO_SPEED_400,
+        dc1394video_mode_t video = DC1394_VIDEO_MODE_640x480_MONO8,
+        dc1394framerate_t fps = DC1394_FRAMERATE_30,
+        size_t bufferSize = 8);
+
+    ~Cam();
+
+    void start();
+
+    void stop();
+    
+    dc1394video_frame_t* getFrame(dc1394capture_policy_t policy = DC1394_CAPTURE_POLICY_WAIT);
+
+    void releaseFrame(dc1394video_frame_t* f);
+
+    dc1394camera_t* dcCam;
+
+
+  };
+
+};
+
+#endif
+
---
+++ drivers/cam/dc1394_cams/src/dc1394_cam/dc1394_cam.cpp
@@ -0,0 +1,201 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+* 
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+* 
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+* 
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+* 
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+#include "dc1394_cam/dc1394_cam.h"
+
+
+dc1394_t* dc1394_cam::Cam::dcRef = NULL;
+
+
+void
+dc1394_cam::init() {
+  Cam::init();
+}
+
+void
+dc1394_cam::Cam::init() {
+  if (dcRef == NULL)
+    dcRef = dc1394_new();
+}
+
+
+
+void
+dc1394_cam::fini()
+{
+  Cam::fini();
+}
+
+void
+dc1394_cam::Cam::fini()
+{
+  if (dcRef != NULL)
+    dc1394_free(dcRef);
+}
+
+
+
+size_t dc1394_cam::numCams()
+{
+  return Cam::numCams();
+}
+
+size_t dc1394_cam::Cam::numCams() {
+  CHECK_READY();
+
+  dc1394camera_list_t * list;
+  CHECK_ERR( dc1394_camera_enumerate(dcRef, &list), "Could not enumerate cameras" );
+
+  size_t num = list->num;
+
+  dc1394_camera_free_list (list);
+
+  return num;
+}
+
+
+uint64_t dc1394_cam::getGuid(size_t i)
+{
+  return Cam::getGuid(i);
+}
+
+uint64_t dc1394_cam::Cam::getGuid(size_t i)
+{
+  CHECK_READY();
+
+  dc1394camera_list_t * list;
+  CHECK_ERR( dc1394_camera_enumerate(dcRef, &list), "Could not enumerate cameras" );
+
+  if (i > list->num)
+    throw CamException("Tried to get Guid of non-existant camera");
+
+  uint64_t guid = list->ids[i].guid;
+
+  dc1394_camera_free_list (list);
+
+  return guid;
+}
+
+
+
+dc1394_cam::Cam::Cam(uint64_t guid, 
+                     dc1394speed_t speed, 
+                     dc1394video_mode_t video,
+                     dc1394framerate_t fps,
+                     size_t bufferSize)        : started(false), dcCam(NULL)
+{
+  CHECK_READY();
+
+  dcCam = dc1394_camera_new(dcRef, guid);
+
+  if (!dcCam)
+    throw CamException("Could not create camera");
+
+  CHECK_ERR_CLEAN( dc1394_reset_bus(dcCam), "Could not rset bus" );
+
+  CHECK_ERR_CLEAN( dc1394_video_set_iso_speed(dcCam, speed), "Could not set iso speed");
+  
+  CHECK_ERR_CLEAN( dc1394_video_set_mode(dcCam, video), "Could not set video mode");
+  
+  CHECK_ERR_CLEAN( dc1394_video_set_framerate(dcCam, fps), "Could not set framerate");
+
+  CHECK_ERR_CLEAN( dc1394_capture_setup(dcCam, bufferSize, DC1394_CAPTURE_FLAGS_DEFAULT), "Could not setup camera.");
+}
+
+
+
+dc1394_cam::Cam::~Cam()
+{
+  if (dcCam != NULL)
+    cleanup();
+}
+
+
+
+void
+dc1394_cam::Cam::start()
+{
+  CHECK_READY();
+
+  CHECK_ERR_CLEAN( dc1394_video_set_transmission(dcCam, DC1394_ON), "Could not start camera iso transmission");
+
+  started = true;
+}
+
+
+
+void
+dc1394_cam::Cam::stop()
+{
+  CHECK_READY();
+
+  CHECK_ERR_CLEAN( dc1394_video_set_transmission(dcCam, DC1394_OFF), "Could not stop camera iso transmission");
+
+  started = false;
+}
+
+
+dc1394video_frame_t*
+dc1394_cam::Cam::getFrame(dc1394capture_policy_t policy)
+{
+    CHECK_READY();
+
+    if (!started)
+      start();
+
+    dc1394video_frame_t* f;
+    CHECK_ERR_CLEAN( dc1394_capture_dequeue(dcCam, policy, &f), "Could not capture frame");
+    return f;
+}
+
+
+void
+dc1394_cam::Cam::releaseFrame(dc1394video_frame_t* f)
+{
+    CHECK_READY();
+    CHECK_ERR_CLEAN( dc1394_capture_enqueue(dcCam, f), "Could not releaseframe");
+}
+
+
+
+void
+dc1394_cam::Cam::cleanup() {
+  CHECK_READY();
+  dc1394_video_set_transmission(dcCam, DC1394_OFF);
+  dc1394_capture_stop(dcCam);
+  dc1394_camera_free(dcCam);
+  
+  dcCam = NULL;
+}
+
