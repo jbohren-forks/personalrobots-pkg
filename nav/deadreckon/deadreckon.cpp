ac6c8cbe88977f678c80931a6e98c089ff965742
morgan_quigley <morgan_quigley@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-06-30 19:23:52 +0000

    deadreckon package is usable now. it's really stupid, but hey, that's what dead reckoning is all about
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1111 f5854215-dd47-0410-b2c4-cdd35faa7885

--- nav/deadreckon/deadreckon.cpp
+++ nav/deadreckon/deadreckon.cpp
@@ -27,14 +27,131 @@
 // POSSIBILITY OF SUCH DAMAGE.
 /////////////////////////////////////////////////////////////////////////////
 
+#include <cmath>
 #include "ros/node.h"
-#include "std_msgs/String.h"
+#include "std_msgs/BaseVel.h"
+#include "std_msgs/RobotBase2DOdom.h"
+#include "deadreckon/DriveDeadReckon.h"
 using namespace std;
 using namespace ros;
+
+class DeadReckon : public ros::node
+{
+public:
+  std_msgs::BaseVel velMsg;
+  std_msgs::RobotBase2DOdom odomMsg;
+  double maxTV, maxRV, distEps, headEps, finalEps, tgtX, tgtY, tgtTh;
+  enum
+  {
+    DR_FACE_TGT,
+    DR_DRIVE_TO_TGT,
+    DR_FACE_FINAL_HEADING,
+    DR_IDLE
+  } drState;
+
+  DeadReckon() : node("DeadReckon"), drState(DR_IDLE), tgtX(0), tgtY(0),
+                 tgtTh(0)
+  {
+    param("drMaxTV", maxTV, 0.3);
+    param("drMaxRV", maxRV, 0.3);
+    param("drDistEps", distEps, 0.05);
+    param("drHeadEps", headEps, 0.1);
+    param("drFinalEps", finalEps, 0.05);
+    advertise_service("DriveDeadReckon", &DeadReckon::dr_cb);
+    subscribe("odom", odomMsg, &DeadReckon::odom_cb);
+    advertise<std_msgs::BaseVel>("cmd_vel");
+  }
+  bool dr_cb(deadreckon::DriveDeadReckon::request  &req,
+             deadreckon::DriveDeadReckon::response &res)
+  {
+    printf("handling dead reckon request: %f %f %f\n",
+           req.dist,
+           req.bearing,
+           req.finalRelHeading);
+    odomMsg.lock();
+    tgtX  = odomMsg.pos.x + req.dist * cos(odomMsg.pos.th + req.bearing);
+    tgtY  = odomMsg.pos.y + req.dist * sin(odomMsg.pos.th + req.bearing);
+    tgtTh = normalizeAngle(odomMsg.pos.th + req.finalRelHeading);
+    odomMsg.unlock();
+    drState = DR_FACE_TGT;
+    while (drState != DR_IDLE && ok())
+      usleep(100000);
+    res.status = "ok";
+    return (drState == DR_IDLE);
+  }
+  double normalizeAngle(double ang)
+  {
+    while (ang > M_PI)
+      ang -= 2 * M_PI;
+    while (ang < -M_PI)
+      ang += 2 * M_PI;
+    return ang;
+  }
+  inline double saturate(double val, double low, double high)
+  {
+    return (val < low ? low : (val > high ? high : val));
+  }
+  void odom_cb()
+  {
+    velMsg.vx = velMsg.vw = 0;
+    double dx = tgtX - odomMsg.pos.x, dy = tgtY - odomMsg.pos.y;
+    double distToGo = sqrt(dx*dx + dy*dy);
+    double bearing = normalizeAngle(atan2(dy, dx) - odomMsg.pos.th);
+    bool backup = false;
+    if (fabs(bearing) > M_PI/2)
+    {
+      backup = true;
+      bearing = normalizeAngle(bearing + M_PI);
+    }
+    double finalBearing = normalizeAngle(tgtTh - odomMsg.pos.th);
+    switch(drState)
+    {
+      case DR_FACE_TGT:
+        velMsg.vx = 0;
+        velMsg.vw = saturate(bearing, -maxRV, maxRV);
+        if (fabs(bearing) < headEps)
+        {
+          drState = DR_DRIVE_TO_TGT;
+          printf("driving to target\n");
+        }
+        break;
+      case DR_DRIVE_TO_TGT:
+        velMsg.vw = 0;
+        if (!backup)
+        {
+          velMsg.vx = saturate(distToGo, 0.2, maxTV);
+          if (distToGo > 0.3)
+            velMsg.vw = saturate(bearing, -maxRV, maxRV);
+        }
+        else
+          velMsg.vx = saturate(-distToGo, -0.2, -maxTV);
+        if (distToGo < distEps)
+        {
+          drState = DR_FACE_FINAL_HEADING;
+          printf("facing final heading\n");
+        }
+        break;
+      case DR_FACE_FINAL_HEADING:
+        velMsg.vx = 0;
+        velMsg.vw = saturate(finalBearing, -maxRV, maxRV);
+        if (fabs(finalBearing) < finalEps)
+        {
+          printf("going back to idle\n");
+          drState = DR_IDLE;
+        }
+        break;
+      case DR_IDLE:
+        velMsg.vx = velMsg.vw = 0;
+    }
+    publish("cmd_vel", velMsg);
+  }
+};
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv);
+  DeadReckon dr;
+  dr.spin();
   ros::fini();
   return 0;
 }
