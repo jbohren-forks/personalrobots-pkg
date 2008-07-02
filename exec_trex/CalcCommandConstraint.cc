dff1c61acf203cda5a6b21fcba511722bd59a319
tpratkanis <tpratkanis@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-02 20:04:37 +0000

    Added logging and playback, moved constraints out of EUROPA and into TREX.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1216 f5854215-dd47-0410-b2c4-cdd35faa7885

--- exec_trex/CalcCommandConstraint.cc
+++ exec_trex/CalcCommandConstraint.cc
@@ -1,18 +1,32 @@
 #include "CalcCommandConstraint.hh"
 #include "map_helpers.h"
 #include "WavefrontPlanner.hh"
+#include "Logger.hh"
 
-namespace EUROPA {
-
+namespace TREX {
   CalcCommandConstraint::CalcCommandConstraint(const LabelStr& name,
 					       const LabelStr& propagatorName,
 					       const ConstraintEngineId& constraintEngine,
 					       const std::vector<ConstrainedVariableId>& variables)
-    : Constraint(name, propagatorName, constraintEngine, variables)
-  {
+    : Constraint(name, propagatorName, constraintEngine, variables) {
+    m_logger = TREX::Logger::request();
+  }
+  CalcCommandConstraint::~CalcCommandConstraint() {
+    m_logger->release();
+  }
+
+  void CalcCommandConstraint::logData(const unsigned int cycles, const bool bd, 
+				      const double x_value, const double th_value) {
+    FILE* log = m_logger->getFile();
+    if (log) {
+      fprintf(log, "\t<CalcCommandConstraint time=\"%u\" value=\"%d\" x_v=\"%f\" th_v=\"%f\"/>\n",
+	      cycles, bd, x_value, th_value);
+    }
   }
   
   void CalcCommandConstraint::handleExecute() {
+    static unsigned int sl_cycles(0);
+    sl_cycles++;
     BoolDomain& boolDom = static_cast<BoolDomain&>(getCurrentDomain(m_variables[0]));
     check_error(!boolDom.isOpen());
    //  if(boolDom.isSingleton()) {
@@ -64,8 +78,9 @@ namespace EUROPA {
 						       m_variables[GOAL_X]->lastDomain().getSingletonValue(),  
 						       m_variables[GOAL_Y]->lastDomain().getSingletonValue()) < 0) {
       boolDom.set(false);
-      getCurrentDomain(m_variables[COM_VEL_X]).intersect(0.0,0.0);
-      getCurrentDomain(m_variables[COM_VEL_TH]).intersect(0.0,0.0);
+      getCurrentDomain(m_variables[COM_VEL_X]).set(0.0);
+      getCurrentDomain(m_variables[COM_VEL_TH]).set(0.0);
+      logData(sl_cycles, false, 0.0, 0.0);
       return;
     }
 
@@ -80,8 +95,9 @@ namespace EUROPA {
 							    m_variables[GOAL_TH]->lastDomain().getSingletonValue()) < 0) {
       boolDom.set(false);
       check_error(boolDom.isSingleton());
-      getCurrentDomain(m_variables[COM_VEL_X]).intersect(0.0,0.0);
-      getCurrentDomain(m_variables[COM_VEL_TH]).intersect(0.0,0.0);
+      getCurrentDomain(m_variables[COM_VEL_X]).set(0.0);
+      getCurrentDomain(m_variables[COM_VEL_TH]).set(0.0);
+      logData(sl_cycles, false, 0.0, 0.0);
       return;  
     }
 
@@ -96,8 +112,12 @@ namespace EUROPA {
       //		<< " cur_vel_th " << m_variables[COM_VEL_TH]->lastDomain().getSingletonValue()); 
     }
     //debugMsg("CalcCommandConstraint::handleExecute", "vx " << vx << " va " << va);
-    getCurrentDomain(m_variables[COM_VEL_X]).intersect(vx,vx);
-    getCurrentDomain(m_variables[COM_VEL_TH]).intersect(va,va);
+    getCurrentDomain(m_variables[COM_VEL_X]).set(vx);
+    getCurrentDomain(m_variables[COM_VEL_TH]).set(va);
+
+
+    logData(sl_cycles, true, vx, va);
+
     //debugMsg("CalcCommandConstraint::handleExecute", "BoolDom up " << boolDom.getUpperBound());
     //debugMsg("CalcCommandConstraint::handleExecute", "BoolDom low " << boolDom.getLowerBound());
   // debugMsg("CalcCommandConstraint::handleExecute", "Local x " << m_variables[LOCAL_X]->lastDomain().getSingletonValue()
