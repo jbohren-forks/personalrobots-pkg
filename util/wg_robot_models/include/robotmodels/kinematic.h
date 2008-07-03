706a7fe326b129a134c548abeefad14e82195b6f
isucan <isucan@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-03 23:31:55 +0000

    robot models -- a kinematic model that can be loaded from a parsed URDF model
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1281 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ util/wg_robot_models/include/robotmodels/kinematic.h
@@ -0,0 +1,186 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+* 
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+* 
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+* 
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+* 
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+#ifndef KINEMATIC_ROBOT_MODEL_
+#define KINEMATIC_ROBOT_MODEL_
+
+#include <urdf/URDF.h>
+#include <libTF/Pose3D.h>
+
+/** @htmlinclude ../../manifest.html
+
+    A class describing a kinematic robot model loaded from URDF */
+
+class KinematicModel
+{
+ public:
+    
+    struct Geometry
+    {
+	Geometry(void)
+	{
+	    type = UNKNOWN;
+	    size[0] = size[1] = size[2] = 0.0;
+	}
+	
+	enum 
+	    {
+		UNKNOWN, BOX, CYLINDER, SPHERE
+	    }  type;
+	double size[3];
+    };
+    
+    struct Joint;
+    struct Link;	
+    
+    struct Joint
+    {
+	Joint(void)
+	{
+	    before = after = NULL;
+	    usedParamStart = usedParamEnd = 0;
+	    axis[0] = axis[1] = axis[2] = 0.0;
+	    anchor[0] = anchor[1] = anchor[2] = 0.0;
+	    limit[0] = limit[1] = 0.0;
+	    type = UNKNOWN;
+	}
+	
+	~Joint(void)
+	{
+	    if (after)
+		delete after;
+	}
+	
+	/* the links that this joint connects */	    
+	Link         *before;
+	Link         *after;
+	
+	/* the range of indices in the parameter vector that
+	   needed to access information about the position of this
+	   joint */
+	unsigned int  usedParamStart;
+	unsigned int  usedParamEnd;
+	
+	/* relevant joint information */
+	enum
+	    {
+		UNKNOWN, FIXED, REVOLUTE, PRISMATIC, FLOATING
+	    }         type;
+	double        axis[3];
+	double        anchor[3];
+	double        limit[2];
+	
+	/* ----------------- Computed data -------------------*/
+	
+	/* the local transform (computed by forward kinematics) */
+	libTF::Pose3D trans;
+    };
+    
+    struct Link
+    {
+	Link(void)
+	{
+	    before = NULL;
+	}
+	
+	~Link(void)
+	{
+	    for (unsigned int i = 0 ; i < after.size() ; ++i)
+		delete after[i];
+	}
+	
+	/* joint that connects this link to the parent link */
+	Joint              *before;
+	
+	/* list of descending joints (each connects to a child link) */
+	std::vector<Joint*> after;
+	
+	/* the constant transform applied to the link (local) */
+	libTF::Pose3D       constTrans;
+	
+	/* the geometry of the link */
+	Geometry            geom;
+	
+	/* ----------------- Computed data -------------------*/
+	
+	/* the global transform for this link (computed by forward kinematics) */
+	libTF::Pose3D       globalTrans;
+	
+    };
+    
+    struct Robot
+    {
+	Robot(void)
+	{
+	    chain = NULL;
+	    stateDimension = 0;
+	}
+	
+	~Robot(void)
+	{
+	    if (chain)
+		delete chain;
+	}
+	
+	Joint       *chain;
+	unsigned int stateDimension;
+    };
+    
+    KinematicModel(URDF *model = NULL)
+    {
+	if (model)
+	    build(*model);
+    }
+    
+    virtual ~KinematicModel(void)
+    {
+	for (unsigned int i = 0 ; i < m_robots.size() ; ++i)
+	    delete m_robots[i];
+    }
+    
+    void build(URDF &model);
+    
+    unsigned int getRobotCount(void) const;
+    Robot* getRobot(unsigned int index) const;
+    
+ protected:
+    
+    std::vector<Robot*> m_robots;
+    
+    void buildChain(Link *parent, unsigned int *usedParams, Joint* joint, URDF::Link* urdfLink);
+    void buildChain(Joint *parent, unsigned int *usedParams, Link* link, URDF::Link* urdfLink);
+    
+};
+
+#endif
---
+++ util/wg_robot_models/src/robotmodels/kinematic.cpp
@@ -0,0 +1,142 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+* 
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+* 
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+* 
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+* 
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+#include <robotmodels/kinematic.h>
+
+void KinematicModel::build(URDF &model)
+{
+    for (unsigned int i = 0 ; i < model.getDisjointPartCount() ; ++i)
+    {
+	URDF::Link *link = model.getDisjointPart(i);
+	Robot *rb = new Robot();
+	rb->chain = new Joint();
+	buildChain(NULL, &rb->stateDimension, rb->chain, link);
+	m_robots.push_back(rb);
+    }
+}
+    
+unsigned int KinematicModel::getRobotCount(void) const
+{
+    return m_robots.size();
+}
+
+KinematicModel::Robot* KinematicModel::getRobot(unsigned int index) const
+{
+    return m_robots[index];
+}
+
+void KinematicModel::buildChain(Link *parent, unsigned int *usedParams, Joint* joint, URDF::Link* urdfLink)
+{
+    joint->usedParamStart = *usedParams;
+    joint->before = parent;
+    joint->after  = new Link();
+    
+    /* copy relevant data */
+    joint->limit[0] = urdfLink->joint->limit[0];
+    joint->limit[1] = urdfLink->joint->limit[1];
+    joint->axis[0] = urdfLink->joint->axis[0];
+    joint->axis[1] = urdfLink->joint->axis[1];
+    joint->axis[2] = urdfLink->joint->axis[2];
+    joint->anchor[0] = urdfLink->joint->anchor[0];
+    joint->anchor[1] = urdfLink->joint->anchor[1];
+    joint->anchor[2] = urdfLink->joint->anchor[2];
+    switch (urdfLink->joint->type)
+    {
+    case URDF::Link::Joint::FLOATING:
+	joint->type = Joint::FLOATING;
+	joint->usedParamEnd = joint->usedParamStart + 3;
+	break;
+    case URDF::Link::Joint::FIXED:
+	joint->type = Joint::FIXED; 
+	joint->usedParamEnd = joint->usedParamStart;
+	break;
+    case URDF::Link::Joint::REVOLUTE:
+	joint->type = Joint::REVOLUTE;
+	joint->usedParamEnd = joint->usedParamStart + 1;
+	break;
+    case URDF::Link::Joint::PRISMATIC:
+	joint->type = Joint::PRISMATIC;
+	joint->usedParamEnd = joint->usedParamStart + 1;
+	break;
+    default:
+	joint->type = Joint::UNKNOWN; 
+	joint->usedParamEnd = joint->usedParamStart;
+	break;
+    }
+    *usedParams = joint->usedParamEnd;	    
+    buildChain(joint, usedParams, joint->after, urdfLink);
+}
+
+void KinematicModel::buildChain(Joint *parent, unsigned int *usedParams, Link* link, URDF::Link* urdfLink)
+{
+    link->before = parent;
+    
+    /* copy relevant data */ 
+    switch (urdfLink->collision->geometry->type)
+    {
+    case URDF::Link::Geometry::UNKNOWN:
+	link->geom.type = Geometry::UNKNOWN; break;
+    case URDF::Link::Geometry::BOX:
+	link->geom.type = Geometry::BOX; break;
+    case URDF::Link::Geometry::SPHERE:
+	link->geom.type = Geometry::SPHERE; break;
+    case URDF::Link::Geometry::CYLINDER:
+	link->geom.type = Geometry::CYLINDER; break;
+    default:
+	break;
+    }
+    
+    /* copy the geometry of the link */
+    link->geom.size[0] = urdfLink->collision->geometry->size[0];
+    link->geom.size[1] = urdfLink->collision->geometry->size[1];
+    link->geom.size[2] = urdfLink->collision->geometry->size[2];
+    
+    /* compute the constant transform for this link */
+    double *xyz = urdfLink->xyz;
+    double *rpy = urdfLink->rpy;
+    link->constTrans.setFromEuler(xyz[0], xyz[1], xyz[2], rpy[2], rpy[1], rpy[0]);	    
+    
+    xyz = urdfLink->collision->xyz;
+    rpy = urdfLink->collision->rpy;
+    libTF::Pose3D auxTransf;
+    auxTransf.setFromEuler(xyz[0], xyz[1], xyz[2], rpy[2], rpy[1], rpy[0]);	    
+    link->constTrans.multiplyPose(auxTransf);
+    
+    for (unsigned int i = 0 ; i < urdfLink->children.size() ; ++i)
+    {
+	Joint *newJoint = new Joint();
+	buildChain(link, usedParams, newJoint, urdfLink->children[i]);
+	link->after.push_back(newJoint);
+    }	    
+}
