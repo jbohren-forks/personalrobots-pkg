40cd0e5a67f9b26353f841568f3c3b1565fe2c52
isucan <isucan@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-03 20:38:45 +0000

    emacs autoindent
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1259 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/transforms/libTF/src/Pose3D.cpp
+++ util/transforms/libTF/src/Pose3D.cpp
@@ -39,29 +39,29 @@
 using namespace libTF;
 
 Pose3D::Pose3D():
-  xt(0), yt(0),zt(0),
-  xr(1), yr(0), zr(0),w(0)
+    xt(0), yt(0),zt(0),
+    xr(1), yr(0), zr(0),w(0)
 {
 };
 
 
 ///Translation only constructor
 Pose3D::Pose3D(double xt, double yt, double zt):
-  xt(xt), yt(yt),zt(zt),
-  xr(1), yr(0), zr(0),w(0)
+    xt(xt), yt(yt),zt(zt),
+    xr(1), yr(0), zr(0),w(0)
 {
 }; 
 /// Quaternion only constructor
 Pose3D::Pose3D(double xr, double yr, double zr, double w):
-  xt(0), yt(0),zt(0),
-  xr(xr), yr(yr), zr(zr),w(w)
+    xt(0), yt(0),zt(0),
+    xr(xr), yr(yr), zr(zr),w(w)
 {
 }; 
-  /// Trans and Quat constructor
+/// Trans and Quat constructor
 Pose3D::Pose3D(double xt, double yt, double zt, 
 	       double xr, double yr, double zr, double w):
-  xt(xt), yt(yt), zt(zt),
-  xr(xr), yr(yr), zr(zr), w(w)
+    xt(xt), yt(yt), zt(zt),
+    xr(xr), yr(yr), zr(zr), w(w)
 {
 }; 
 
@@ -85,65 +85,65 @@ void Pose3D::setAxisAngle(double ax, double ay, double az, double angle)
 
 void Pose3D::setFromMatrix(const NEWMAT::Matrix& matIn)
 {
-  // math derived from http://www.j3d.org/matrix_faq/matrfaq_latest.html
-
-  //  std::cout <<std::endl<<"Matrix in"<<std::endl<<matIn;
-
-  const double * mat = matIn.Store();
-  //Get the translations
-  xt = mat[3];
-  yt = mat[7];
-  zt = mat[11];
-
-  //TODO ASSERT others are zero and one as they should be
-
-
-  double T  = 1 + mat[0] + mat[5] + mat[10];
-
-
-  //  If the trace of the matrix is greater than zero, then
-  //  perform an "instant" calculation.
-  //	      Important note wrt. rouning errors:
-
-  if ( T > 0.00000001 ) //to avoid large distortions!
+    // math derived from http://www.j3d.org/matrix_faq/matrfaq_latest.html
+    
+    //  std::cout <<std::endl<<"Matrix in"<<std::endl<<matIn;
+    
+    const double * mat = matIn.Store();
+    //Get the translations
+    xt = mat[3];
+    yt = mat[7];
+    zt = mat[11];
+    
+    //TODO ASSERT others are zero and one as they should be
+    
+    
+    double T  = 1 + mat[0] + mat[5] + mat[10];
+    
+    
+    //  If the trace of the matrix is greater than zero, then
+    //  perform an "instant" calculation.
+    //	      Important note wrt. rouning errors:
+    
+    if ( T > 0.00000001 ) //to avoid large distortions!
     {
-      double S = sqrt(T) * 2;
-      xr = ( mat[9] - mat[6] ) / S;
-      yr = ( mat[2] - mat[8] ) / S;
-      zr = ( mat[4] - mat[1] ) / S;
-      w = 0.25 * S;
+	double S = sqrt(T) * 2;
+	xr = ( mat[9] - mat[6] ) / S;
+	yr = ( mat[2] - mat[8] ) / S;
+	zr = ( mat[4] - mat[1] ) / S;
+	w = 0.25 * S;
     }
-  //If the trace of the matrix is equal to zero then identify
-  // which major diagonal element has the greatest value.
-  //  Depending on this, calculate the following:
-  else if ( mat[0] > mat[5] && mat[0] > mat[10] ) {// Column 0: 
+    //If the trace of the matrix is equal to zero then identify
+    // which major diagonal element has the greatest value.
+    //  Depending on this, calculate the following:
+    else if ( mat[0] > mat[5] && mat[0] > mat[10] ) {// Column 0: 
         double S  = sqrt( 1.0 + mat[0] - mat[5] - mat[10] ) * 2;
         xr = 0.25 * S;
         yr = (mat[1] + mat[4] ) / S;
         zr = (mat[8] + mat[2] ) / S;
         w = (mat[6] - mat[9] ) / S;
-      } else if ( mat[5] > mat[10] ) {// Column 1: 
+    } else if ( mat[5] > mat[10] ) {// Column 1: 
         double S  = sqrt( 1.0 + mat[5] - mat[0] - mat[10] ) * 2;
         xr = (mat[1] + mat[4] ) / S;
         yr = 0.25 * S;
         zr = (mat[6] + mat[9] ) / S;
         w = (mat[8] - mat[2] ) / S;
-      } else {// Column 2:
+    } else {// Column 2:
         double S  = sqrt( 1.0 + mat[10] - mat[0] - mat[5] ) * 2;
         xr = (mat[8] + mat[2] ) / S;
         yr = (mat[6] + mat[9] ) / S;
         zr = 0.25 * S;
         w = (mat[1] - mat[4] ) / S;
-      }
-
-  //  std::cout << "setFromMatrix" << xt  <<" "<< yt  <<" "<< zt  <<" "<< xr  <<" "<<yr <<" "<<zr <<" "<<w <<std::endl;
-  //std::cout << "asMatrix" <<std::endl<< asMatrix();
-
+    }
+    
+    //  std::cout << "setFromMatrix" << xt  <<" "<< yt  <<" "<< zt  <<" "<< xr  <<" "<<yr <<" "<<zr <<" "<<w <<std::endl;
+    //std::cout << "asMatrix" <<std::endl<< asMatrix();
+    
 };
 
 void Pose3D::setFromEuler(double _x, double _y, double _z, double _yaw, double _pitch, double _roll)
 {
-  setFromMatrix(Pose3D::matrixFromEuler(_x,_y,_z,_yaw,_pitch,_roll));
+    setFromMatrix(Pose3D::matrixFromEuler(_x,_y,_z,_yaw,_pitch,_roll));
 };
 
 void Pose3D::setFromEuler(Position &pos, Euler &euler)
@@ -153,237 +153,237 @@ void Pose3D::setFromEuler(Position &pos, Euler &euler)
 
 void Pose3D::setFromDH(double length, double alpha, double offset, double theta)
 {
-  setFromMatrix(Pose3D::matrixFromDH(length, alpha, offset, theta));
+    setFromMatrix(Pose3D::matrixFromDH(length, alpha, offset, theta));
 };
 
 
 NEWMAT::Matrix Pose3D::matrixFromEuler(double ax,
-			       double ay, double az, double yaw,
-			       double pitch, double roll)
+				       double ay, double az, double yaw,
+				       double pitch, double roll)
 {
-  NEWMAT::Matrix matrix(4,4);
-  double ca = cos(yaw);
-  double sa = sin(yaw);
-  double cb = cos(pitch);
-  double sb = sin(pitch);
-  double cg = cos(roll);
-  double sg = sin(roll);
-  double sbsg = sb*sg;
-  double sbcg = sb*cg;
-
-
-  double* matrix_pointer = matrix.Store();
-
-  matrix_pointer[0] =  ca*cb;
-  matrix_pointer[1] = (ca*sbsg)-(sa*cg);
-  matrix_pointer[2] = (ca*sbcg)+(sa*sg);
-  matrix_pointer[3] = ax;
-
-  matrix_pointer[4] = sa*cb;
-  matrix_pointer[5] = (sa*sbsg)+(ca*cg);
-  matrix_pointer[6] = (sa*sbcg)-(ca*sg);
-  matrix_pointer[7] = ay;
-
-  matrix_pointer[8] = -sb;
-  matrix_pointer[9] = cb*sg;
-  matrix_pointer[10] = cb*cg;
-  matrix_pointer[11] = az;
-
-  matrix_pointer[12] = 0.0;
-  matrix_pointer[13] = 0.0;
-  matrix_pointer[14] = 0.0;
-  matrix_pointer[15] = 1.0;
-
-  return matrix;
+    NEWMAT::Matrix matrix(4,4);
+    double ca = cos(yaw);
+    double sa = sin(yaw);
+    double cb = cos(pitch);
+    double sb = sin(pitch);
+    double cg = cos(roll);
+    double sg = sin(roll);
+    double sbsg = sb*sg;
+    double sbcg = sb*cg;
+    
+    
+    double* matrix_pointer = matrix.Store();
+    
+    matrix_pointer[0] =  ca*cb;
+    matrix_pointer[1] = (ca*sbsg)-(sa*cg);
+    matrix_pointer[2] = (ca*sbcg)+(sa*sg);
+    matrix_pointer[3] = ax;
+    
+    matrix_pointer[4] = sa*cb;
+    matrix_pointer[5] = (sa*sbsg)+(ca*cg);
+    matrix_pointer[6] = (sa*sbcg)-(ca*sg);
+    matrix_pointer[7] = ay;
+    
+    matrix_pointer[8] = -sb;
+    matrix_pointer[9] = cb*sg;
+    matrix_pointer[10] = cb*cg;
+    matrix_pointer[11] = az;
+    
+    matrix_pointer[12] = 0.0;
+    matrix_pointer[13] = 0.0;
+    matrix_pointer[14] = 0.0;
+    matrix_pointer[15] = 1.0;
+    
+    return matrix;
 };
 
 
 // Math from http://en.wikipedia.org/wiki/Robotics_conventions
 NEWMAT::Matrix Pose3D::matrixFromDH(double length,
-			    double alpha, double offset, double theta)
+				    double alpha, double offset, double theta)
 {
-  NEWMAT::Matrix matrix(4,4);
-  
-  double ca = cos(alpha);
-  double sa = sin(alpha);
-  double ct = cos(theta);
-  double st = sin(theta);
-  
-  double* matrix_pointer = matrix.Store();
-  
-  matrix_pointer[0] =  ct;
-  matrix_pointer[1] = -st*ca;
-  matrix_pointer[2] = st*sa;
-  matrix_pointer[3] = length * ct;
-  matrix_pointer[4] = st;
-  matrix_pointer[5] = ct*ca;
-  matrix_pointer[6] = -ct*sa;
-  matrix_pointer[7] = length*st;
-  matrix_pointer[8] = 0;
-  matrix_pointer[9] = sa;
-  matrix_pointer[10] = ca;
-  matrix_pointer[11] = offset;
-  matrix_pointer[12] = 0.0;
-  matrix_pointer[13] = 0.0;
-  matrix_pointer[14] = 0.0;
-  matrix_pointer[15] = 1.0;
-
-  return matrix;
+    NEWMAT::Matrix matrix(4,4);
+    
+    double ca = cos(alpha);
+    double sa = sin(alpha);
+    double ct = cos(theta);
+    double st = sin(theta);
+    
+    double* matrix_pointer = matrix.Store();
+    
+    matrix_pointer[0] =  ct;
+    matrix_pointer[1] = -st*ca;
+    matrix_pointer[2] = st*sa;
+    matrix_pointer[3] = length * ct;
+    matrix_pointer[4] = st;
+    matrix_pointer[5] = ct*ca;
+    matrix_pointer[6] = -ct*sa;
+    matrix_pointer[7] = length*st;
+    matrix_pointer[8] = 0;
+    matrix_pointer[9] = sa;
+    matrix_pointer[10] = ca;
+    matrix_pointer[11] = offset;
+    matrix_pointer[12] = 0.0;
+    matrix_pointer[13] = 0.0;
+    matrix_pointer[14] = 0.0;
+    matrix_pointer[15] = 1.0;
+    
+    return matrix;
 };
 
 
 Pose3D& Pose3D::operator=(const Pose3D & input)
 {
-  xt = input.xt;
-  yt = input.yt;
-  zt = input.zt;
-  xr = input.xr;
-  yr = input.yr;
-  zr = input.zr;
-  w  = input.w ;
-
-  return *this;
+    xt = input.xt;
+    yt = input.yt;
+    zt = input.zt;
+    xr = input.xr;
+    yr = input.yr;
+    zr = input.zr;
+    w  = input.w ;
+    
+    return *this;
 };
 
 
 Pose3D::Euler Pose3D::eulerFromMatrix(const NEWMAT::Matrix & matrix_in, unsigned int solution_number)
 {
-
-  Euler euler_out;
-  Euler euler_out2; //second solution
-  //get the pointer to the raw data
-  double* matrix_pointer = matrix_in.Store();
-
-  // Check that pitch is not at a singularity
-  if (fabs(matrix_pointer[8]) >= 1)
+    
+    Euler euler_out;
+    Euler euler_out2; //second solution
+    //get the pointer to the raw data
+    double* matrix_pointer = matrix_in.Store();
+    
+    // Check that pitch is not at a singularity
+    if (fabs(matrix_pointer[8]) >= 1)
     {
-      euler_out.pitch = - asin(matrix_pointer[8]);
-      euler_out2.pitch = M_PI - euler_out.pitch;
-
-      euler_out.roll = atan2(matrix_pointer[9]/cos(euler_out.pitch), 
-			     matrix_pointer[10]/cos(euler_out.pitch));
-      euler_out2.roll = atan2(matrix_pointer[9]/cos(euler_out2.pitch), 
-			      matrix_pointer[10]/cos(euler_out2.pitch));
-      
-      euler_out.yaw = atan2(matrix_pointer[4]/cos(euler_out.pitch), 
-			    matrix_pointer[1]/cos(euler_out.pitch));
-      euler_out2.yaw = atan2(matrix_pointer[4]/cos(euler_out2.pitch), 
-			     matrix_pointer[1]/cos(euler_out2.pitch));
+	euler_out.pitch = - asin(matrix_pointer[8]);
+	euler_out2.pitch = M_PI - euler_out.pitch;
+	
+	euler_out.roll = atan2(matrix_pointer[9]/cos(euler_out.pitch), 
+			       matrix_pointer[10]/cos(euler_out.pitch));
+	euler_out2.roll = atan2(matrix_pointer[9]/cos(euler_out2.pitch), 
+				matrix_pointer[10]/cos(euler_out2.pitch));
+	
+	euler_out.yaw = atan2(matrix_pointer[4]/cos(euler_out.pitch), 
+			      matrix_pointer[1]/cos(euler_out.pitch));
+	euler_out2.yaw = atan2(matrix_pointer[4]/cos(euler_out2.pitch), 
+			       matrix_pointer[1]/cos(euler_out2.pitch));
     }
-  else
+    else
     {
-      euler_out.yaw = 0;
-      euler_out2.yaw = 0;
-
-      // From difference of angles formula
-      double delta = atan2(matrix_pointer[1],matrix_pointer[2]);
-      if (matrix_pointer[8] > 0)  //gimbal locked up
+	euler_out.yaw = 0;
+	euler_out2.yaw = 0;
+	
+	// From difference of angles formula
+	double delta = atan2(matrix_pointer[1],matrix_pointer[2]);
+	if (matrix_pointer[8] > 0)  //gimbal locked up
 	{
-	  euler_out.pitch = M_PI / 2.0;
-	  euler_out2.pitch = M_PI / 2.0;
-	  euler_out.roll = euler_out.pitch + delta;
-	  euler_out2.roll = euler_out.pitch + delta;
+	    euler_out.pitch = M_PI / 2.0;
+	    euler_out2.pitch = M_PI / 2.0;
+	    euler_out.roll = euler_out.pitch + delta;
+	    euler_out2.roll = euler_out.pitch + delta;
 	}
-      else // gimbal locked down
+	else // gimbal locked down
 	{
-	  euler_out.pitch = -M_PI / 2.0;
-	  euler_out2.pitch = -M_PI / 2.0;
-	  euler_out.roll = -euler_out.pitch + delta;
-	  euler_out2.roll = -euler_out.pitch + delta;
+	    euler_out.pitch = -M_PI / 2.0;
+	    euler_out2.pitch = -M_PI / 2.0;
+	    euler_out.roll = -euler_out.pitch + delta;
+	    euler_out2.roll = -euler_out.pitch + delta;
 	}
     }
-  
-  if (solution_number == 1)
-    return euler_out;
-  else
-    return euler_out2;
+    
+    if (solution_number == 1)
+	return euler_out;
+    else
+	return euler_out2;
 };
 
 Pose3D::Position Pose3D::positionFromMatrix(const NEWMAT::Matrix & matrix_in)
 {
-
-  Position position;
-  //get the pointer to the raw data
-  double* matrix_pointer = matrix_in.Store();
-  //Pass through translations
-  position.x = matrix_pointer[3];
-  position.y = matrix_pointer[7];
-  position.z = matrix_pointer[11];
-  return position;
+    
+    Position position;
+    //get the pointer to the raw data
+    double* matrix_pointer = matrix_in.Store();
+    //Pass through translations
+    position.x = matrix_pointer[3];
+    position.y = matrix_pointer[7];
+    position.z = matrix_pointer[11];
+    return position;
 };
 
 void Pose3D::normalize()
 {
-  double mag = getMagnitude();
-  xr /= mag;
-  yr /= mag;
-  zr /= mag;
-  w /= mag;
+    double mag = getMagnitude();
+    xr /= mag;
+    yr /= mag;
+    zr /= mag;
+    w /= mag;
 };
 
 double Pose3D::getMagnitude()
 {
-  return sqrt(xr*xr + yr*yr + zr*zr + w*w);
+    return sqrt(xr*xr + yr*yr + zr*zr + w*w);
 };
 
 NEWMAT::Matrix Pose3D::asMatrix()
 {
-  
-  NEWMAT::Matrix outMat(4,4);
-  
-  double * mat = outMat.Store();
-
-  // math derived from http://www.j3d.org/matrix_faq/matrfaq_latest.html
-  double xx      = xr * xr;
-  double xy      = xr * yr;
-  double xz      = xr * zr;
-  double xw      = xr * w;
-  double yy      = yr * yr;
-  double yz      = yr * zr;
-  double yw      = yr * w;
-  double zz      = zr * zr;
-  double zw      = zr * w;
-  mat[0]  = 1 - 2 * ( yy + zz );
-  mat[1]  =     2 * ( xy - zw );
-  mat[2]  =     2 * ( xz + yw );
-  mat[4]  =     2 * ( xy + zw );
-  mat[5]  = 1 - 2 * ( xx + zz );
-  mat[6]  =     2 * ( yz - xw );
-  mat[8]  =     2 * ( xz - yw );
-  mat[9]  =     2 * ( yz + xw );
-  mat[10] = 1 - 2 * ( xx + yy );
-  mat[12]  = mat[13] = mat[14] = 0;
-  mat[3] = xt;
-  mat[7] = yt;
-  mat[11] = zt;
-  mat[15] = 1;
     
-
-  return outMat;
+    NEWMAT::Matrix outMat(4,4);
+    
+    double * mat = outMat.Store();
+    
+    // math derived from http://www.j3d.org/matrix_faq/matrfaq_latest.html
+    double xx      = xr * xr;
+    double xy      = xr * yr;
+    double xz      = xr * zr;
+    double xw      = xr * w;
+    double yy      = yr * yr;
+    double yz      = yr * zr;
+    double yw      = yr * w;
+    double zz      = zr * zr;
+    double zw      = zr * w;
+    mat[0]  = 1 - 2 * ( yy + zz );
+    mat[1]  =     2 * ( xy - zw );
+    mat[2]  =     2 * ( xz + yw );
+    mat[4]  =     2 * ( xy + zw );
+    mat[5]  = 1 - 2 * ( xx + zz );
+    mat[6]  =     2 * ( yz - xw );
+    mat[8]  =     2 * ( xz - yw );
+    mat[9]  =     2 * ( yz + xw );
+    mat[10] = 1 - 2 * ( xx + yy );
+    mat[12]  = mat[13] = mat[14] = 0;
+    mat[3] = xt;
+    mat[7] = yt;
+    mat[11] = zt;
+    mat[15] = 1;
+    
+    
+    return outMat;
 };
 
 NEWMAT::Matrix Pose3D::getInverseMatrix()
 {
-  return asMatrix().i();
+    return asMatrix().i();
 };
 
 Pose3D::Quaternion Pose3D::getQuaternion(void) const
 {
-  Quaternion quat;
-  quat.x = xr;
-  quat.y = yr;
-  quat.z = zr;
-  quat.w = w;
-  return quat;
+    Quaternion quat;
+    quat.x = xr;
+    quat.y = yr;
+    quat.z = zr;
+    quat.w = w;
+    return quat;
 };
 
 Pose3D::Position Pose3D::getPosition(void) const
 {
-  Position pos;
-  pos.x = xt;
-  pos.y = yt;
-  pos.z = zt;
-  return pos;
+    Position pos;
+    pos.x = xt;
+    pos.y = yt;
+    pos.z = zt;
+    return pos;
 };
 
 void Pose3D::setPosition(double x, double y, double z)
@@ -419,8 +419,8 @@ void Pose3D::setQuaternion(Quaternion &quat)
 void Pose3D::applyToPosition(Position &pos) const
 {
     double xx = 2.0 * xr * xr, xy = 2.0 * xr * yr, xz = 2.0 * xr * zr,                 
-	   xs = 2.0 * xr * w,  yy = 2.0 * yr * yr, yz = 2.0 * yr * zr,                 
-	   ys = 2.0 * yr * w,  zz = 2.0 * zr * zr, zs = 2.0 * zr * w;
+	xs = 2.0 * xr * w,  yy = 2.0 * yr * yr, yz = 2.0 * yr * zr,                 
+	ys = 2.0 * yr * w,  zz = 2.0 * zr * zr, zs = 2.0 * zr * w;
     
     double t0 = xt + pos.x * (1.0 - yy - zz) + pos.y * (xy - zs) + pos.z * (xz + ys);
     double t1 = yt + pos.x * (xy + zs) + pos.y * (1.0 - xx - zz) + pos.z * (yz - xs);
@@ -432,8 +432,8 @@ void Pose3D::applyToPosition(Position &pos) const
 void Pose3D::applyToPositions(std::vector<Position*> &posv) const
 {
     double xx = 2.0 * xr * xr, xy = 2.0 * xr * yr, xz = 2.0 * xr * zr,                 
-	   xs = 2.0 * xr * w,  yy = 2.0 * yr * yr, yz = 2.0 * yr * zr,                 
-	   ys = 2.0 * yr * w,  zz = 2.0 * zr * zr, zs = 2.0 * zr * w;
+	xs = 2.0 * xr * w,  yy = 2.0 * yr * yr, yz = 2.0 * yr * zr,                 
+	ys = 2.0 * yr * w,  zz = 2.0 * zr * zr, zs = 2.0 * zr * w;
     
     for (unsigned int i = 0 ; i < posv.size() ; ++i)
     {
