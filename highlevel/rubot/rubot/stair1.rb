0d0f52be84bdc8b5d9f0699a4ff0e42569bc7505
morgan_quigley <morgan_quigley@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-05 00:01:25 +0000

    script hackery
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1304 f5854215-dd47-0410-b2c4-cdd35faa7885

--- highlevel/rubot/rubot/stair1.rb
+++ highlevel/rubot/rubot/stair1.rb
@@ -11,6 +11,11 @@ class Stair1 < Rubot
     system "#{`rospack find mux`.strip}/switch mux:=velMuxSrv wavefrontVel"
     system "#{`rospack find wavefront_player`.strip}/query_wavefront #{x} #{y} #{th}"
   end
+  def opendoor turn_after
+    system "#{`rospack find mux`.strip}/switch mux:=velMuxSrv deadReckonVel"
+    system "echo \"hello\" |#{`rospack find roscpp_tutorials`.strip}/bin/stdin_query query:=openDoorSrv"
+    system "#{`rospack find deadreckon`.strip}/test_deadreckon_service 1 0 #{turn_after}"
+  end
   def borgscan
     puts "borgscanning"
     system "#{`rospack find mux`.strip}/switch mux:=finalVelMuxSrv __none"
@@ -18,6 +23,10 @@ class Stair1 < Rubot
     system 'ssh stair@stair1 "bash -i -c \"cd /home/stair/ros/ros-pkg/unported/vision/sharks/src/lonefleashark && ./patientshark\""'
     system "#{`rospack find mux`.strip}/switch mux:=finalVelMuxSrv activeVel"
     sleep 0.5
+  end
+  def move_arm location
+    puts "moving arm to [#{location}]"
+    system('ssh stair@stair1 "bash -i -c \"cd /home/stair/ros/ros-pkg/drivers/robot/katana/nodes/katana_client && ./katana_client ' + location + '\""')
   end
   def deadreckon(range, bearing, final_heading)
     puts "stair1 deadreckon to #{range}, #{bearing}, #{final_heading}"
