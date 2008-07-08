238ba946149761972d8c92907b544bb4b7c6aba0
david_li <david_li@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-08 00:42:58 +0000

    Added static angle conversion function
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1342 f5854215-dd47-0410-b2c4-cdd35faa7885

--- controllers/genericControllers/src/Controller.cpp
+++ controllers/genericControllers/src/Controller.cpp
@@ -2,6 +2,21 @@
 
 using namespace CONTROLLER;
 
+//Static function for angle conversion
+static double Controller::ModNPi2Pi(double angle)
+{
+   double theta = angle - ((int)(angle/(2*M_PI))*2*M_PI);
+   //double theta = fmod(angle,2*M_PI);
+   double result = theta;
+
+   if (theta > M_PI) 
+      result = theta - 2*M_PI;
+   if(theta < -M_PI)
+      result = theta + 2*M_PI;
+
+   return result;
+}
+
 Controller::Controller()
 {
 }
