774253460f2361486adaee63b4ba4793e1ebd3a8
david_li <david_li@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-10 19:45:48 +0000

    Added waveform generators
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1418 f5854215-dd47-0410-b2c4-cdd35faa7885

--- controllers/pr2Controllers/src/LaserScannerController.cpp
+++ controllers/pr2Controllers/src/LaserScannerController.cpp
@@ -1,5 +1,6 @@
 #include <pr2Controllers/LaserScannerController.h>
 
+
 using namespace CONTROLLER;
 
 LaserScannerController::LaserScannerController()
@@ -42,6 +43,65 @@ LaserScannerController::setParam(string label,string value)
 {
 
   return PR2::PR2_ALL_OK;
+}
+
+void LaserScannerController::GenerateSawtooth(double *&x, double *&t, double period, double amplitude, double dt, double offset, unsigned int numElements){
+	double lastPeakTime = 0.0; //Track the time of the last peak
+	double timeDifference= 0.0;
+	double currentTime = 0.0;
+	double rawValue = 0.0;
+
+	//Set up arrays
+	x = new double[numElements];
+	t = new double[numElements];
+
+	for (unsigned int i = 0;i<numElements;i++){
+		currentTime = i*dt;
+		t[i] = currentTime;
+		timeDifference = currentTime-lastPeakTime;
+		rawValue = amplitude - (timeDifference * amplitude)/period;
+		if(rawValue<0){
+			lastPeakTime = currentTime;
+			rawValue = amplitude;
+		}
+		 x[i] =  rawValue + offset; //Assume floor is 0
+	}
+}
+
+void LaserScannerController::GenerateSinewave(double *&x, double *&t, double period, double amplitude,double dt, double offset, unsigned int numElements){
+
+	double currentTime = 0.0;
+	//Set up arrays
+	x = new double[numElements];
+	t = new double[numElements];
+
+	for(unsigned int i = 0;i<numElements;i++){
+		currentTime = i*dt;
+		t[i] = currentTime;
+		x[i] = amplitude* sin(2*M_PI*currentTime/period) + offset;
+	}
+
+}
+
+
+void LaserScannerController::GenerateSquarewave(double *&x, double *&t, double period, double amplitude, double dt, double offset, unsigned int numElements){
+
+	double currentTime = 0.0;
+	double lastSwitchTime = 0.0;
+	//Set up arrays
+	x = new double[numElements];
+	t = new double[numElements];
+
+	for(unsigned int i = 0;i<numElements;i++){
+		currentTime = i*dt;
+		t[i] = currentTime;
+		if((currentTime-lastSwitchTime)>period){
+			lastSwitchTime = currentTime;
+			x[i] = offset;
+		}
+		else if((currentTime-lastSwitchTime) < (period/2))x[i] = offset; //Lower part of square wave
+		else x[i] = amplitude + offset;
+	}
 }
 
 
