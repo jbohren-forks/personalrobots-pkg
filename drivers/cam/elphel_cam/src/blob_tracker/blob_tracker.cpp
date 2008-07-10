66c6b0976191fd876b4d2cb2bd3f55a3adac1ed2
jsastra <jsastra@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-10 16:40:32 +0000

    Integrated some  OpenCV code from camshiftdemo.c to track but not quite working yet.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1401 f5854215-dd47-0410-b2c4-cdd35faa7885

--- drivers/cam/elphel_cam/src/blob_tracker/blob_tracker.cpp
+++ drivers/cam/elphel_cam/src/blob_tracker/blob_tracker.cpp
@@ -1,6 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// The axis_cam package provides a library that talks to Axis IP-based cameras
-// as well as ROS nodes which use these libraries
+// This package provides a library to take OpenCV images and track blobs.
 //
 // Copyright (C) 2008, Jimmy Sastra
 //                     
@@ -33,17 +32,24 @@
 #include "blob_tracker/blob_tracker.h"
 
 
+int foo1;
+
+
 IplImage *image = 0, *hsv = 0, *hue = 0, *mask = 0, *backproject = 0, *histimg = 0;
 CvHistogram *hist = 0;
 
+int backproject_mode = 0;
 int select_object = 0;
 int track_object = 0;
-
+int show_hist = 1;
 CvPoint origin;
 CvRect selection;
-
-
-
+CvRect track_window;
+CvBox2D track_box;
+CvConnectedComp track_comp;
+int hdims = 16;
+float hranges_arr[] = {0,180};
+float* hranges = hranges_arr;
 int vmin = 10, vmax = 256, smin = 30;
 
 // constructor
@@ -85,8 +91,24 @@ void on_mouse( int event, int x, int y, int flags, void* param )
             track_object = -1;
         break;
     }
-  printf("mouse event\n");
 
+}
+
+CvScalar hsv2rgb( float hue )
+{
+    int rgb[3], p, sector;
+    static const int sector_data[][3]=
+        {{0,2,1}, {1,2,0}, {1,0,2}, {2,0,1}, {2,1,0}, {0,1,2}};
+    hue *= 0.033333333333333333333333333333333f;
+    sector = cvFloor(hue);
+    p = cvRound(255*(hue - sector));
+    p ^= sector & 1 ? 255 : 0;
+
+    rgb[sector_data[sector][0]] = 255;
+    rgb[sector_data[sector][1]] = 0;
+    rgb[sector_data[sector][2]] = p;
+
+    return cvScalar(rgb[2], rgb[1], rgb[0],0);
 }
 
 void Blob_Tracker::init() {
@@ -96,27 +118,133 @@ void Blob_Tracker::init() {
       "\tb - switch to/from backprojection view\n"
       "\th - show/hide object histogram\n"
       "To initialize tracking, select the object with mouse\n" );
-printf("inside init\n");
  
   cvNamedWindow( "Histogram", 1 );
-  cvNamedWindow( "CamShiftDemo", 1 );
-  cvSetMouseCallback( "CamShiftDemo", on_mouse, 0 );
-printf("creating Trackbar\n");
-  cvCreateTrackbar( "Vmin", "CamShiftDemo", &vmin, 256, 0 );
-  cvCreateTrackbar( "Vmax", "CamShiftDemo", &vmax, 256, 0 );
-  cvCreateTrackbar( "Smin", "CamShiftDemo", &smin, 256, 0 );
+  cvNamedWindow( "BlobTracker", 1 );
+  cvSetMouseCallback( "BlobTracker", on_mouse, 0 );
+  cvCreateTrackbar( "Vmin", "BlobTracker", &vmin, 256, 0 );
+  cvCreateTrackbar( "Vmax", "BlobTracker", &vmax, 256, 0 );
+  cvCreateTrackbar( "Smin", "BlobTracker", &smin, 256, 0 );
 
 }
 
 void Blob_Tracker::processFrame(IplImage** cv_image) {
-printf("inside processFrame\n");
-    cvNamedWindow("blobtracker", CV_WINDOW_AUTOSIZE);
-      cvMoveWindow("blobtracker", 100, 100);
-printf("showing image\n");
-      cvShowImage("blobtracker", *cv_image);
-printf("wait key\n");
-      cvWaitKey(10);
-      cvReleaseImage(cv_image);
+  IplImage* frame = 0;
+  int i, bin_w, c;
+  frame = *cv_image;
+  if( !frame )
+    return;
+
+  if( !image )
+  {
+      /* allocate all the buffers */
+      image = cvCreateImage( cvGetSize(frame), 8, 3 );
+      image->origin = frame->origin;
+      hsv = cvCreateImage( cvGetSize(frame), 8, 3 );
+      hue = cvCreateImage( cvGetSize(frame), 8, 1 );
+      mask = cvCreateImage( cvGetSize(frame), 8, 1 );
+      backproject = cvCreateImage( cvGetSize(frame), 8, 1 );
+      hist = cvCreateHist( 1, &hdims, CV_HIST_ARRAY, &hranges, 1 );
+      histimg = cvCreateImage( cvSize(320,200), 8, 3 );
+      cvZero( histimg );
+  }
+
+  cvCopy( frame, image, 0 );
+  cvCvtColor( image, hsv, CV_BGR2HSV );
+
+  if( track_object )
+  {
+      int _vmin = vmin, _vmax = vmax;
+
+      cvInRangeS( hsv, cvScalar(0,smin,MIN(_vmin,_vmax),0),
+                  cvScalar(180,256,MAX(_vmin,_vmax),0), mask );
+      cvSplit( hsv, hue, 0, 0, 0 );
+
+      if( track_object < 0 )
+      {
+          float max_val = 0.f;
+          cvSetImageROI( hue, selection );
+          cvSetImageROI( mask, selection );
+          cvCalcHist( &hue, hist, 0, mask );
+          cvGetMinMaxHistValue( hist, 0, &max_val, 0, 0 );
+          cvConvertScale( hist->bins, hist->bins, max_val ? 255. / max_val : 0., 0 );
+          cvResetImageROI( hue );
+          cvResetImageROI( mask );
+          track_window = selection;
+          track_object = 1;
+
+          cvZero( histimg );
+          bin_w = histimg->width / hdims;
+          for( i = 0; i < hdims; i++ )
+          {
+              int val = cvRound( cvGetReal1D(hist->bins,i)*histimg->height/255 );
+              CvScalar color = hsv2rgb(i*180.f/hdims);
+              cvRectangle( histimg, cvPoint(i*bin_w,histimg->height),
+                           cvPoint((i+1)*bin_w,histimg->height - val),
+                           color, -1, 8, 0 );
+          }
+      }
+
+      cvCalcBackProject( &hue, backproject, hist );
+
+      cvAnd( backproject, mask, backproject, 0 );
+      cvCamShift( backproject, track_window,
+                  cvTermCriteria( CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 10, 1 ),
+                  &track_comp, &track_box );
+      track_window = track_comp.rect;
+      
+      if( backproject_mode )
+          cvCvtColor( backproject, image, CV_GRAY2BGR );
+      if( !image->origin )
+          track_box.angle = -track_box.angle;
+      cvEllipseBox( image, track_box, CV_RGB(255,0,0), 3, CV_AA, 0 );
+  }
+  
+  if( select_object && selection.width > 0 && selection.height > 0 )
+  {
+      cvSetImageROI( image, selection );
+      cvXorS( image, cvScalarAll(255), image, 0 );
+      cvResetImageROI( image );
+  }
+
+  cvShowImage( "BlobTracker", image );
+  cvShowImage( "Histogram", histimg );
+
+  c = cvWaitKey(10);
+  if( (char) c == 27 )
+      return;
+  switch( (char) c )
+  {
+  case 'b':
+      backproject_mode ^= 1;
+      break;
+  case 'c':
+      track_object = 0;
+      cvZero( histimg );
+      break;
+  case 'h':
+      show_hist ^= 1;
+      if( !show_hist )
+          cvDestroyWindow( "Histogram" );
+      else
+          cvNamedWindow( "Histogram", 1 );
+      break;
+  default:
+      ;
+  }
+  cvWaitKey(10);
+  cvReleaseImage(&image);
+  cvReleaseImage(&histimg);
+
+
+}
+
+void Blob_Tracker::showFrame(IplImage** cv_image) {
+  cvNamedWindow("BlobTracker", CV_WINDOW_AUTOSIZE);
+  cvMoveWindow("BlobTracker", 100, 100);
+  cvShowImage("BlobTracker", *cv_image);
+  cvWaitKey(10);
+  cvReleaseImage(cv_image);
 }
 
 
