e6e2e366b8315c8f498775ee74bc3785a5b4f1dc
ehberger <ehberger@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-10 23:01:03 +0000

    in-progress mechanism control infrastructure
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1430 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ mechanism/src/mechanism_controller.cpp
@@ -0,0 +1,54 @@
+
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2008, Eric Berger
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright notice, 
+//     this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright 
+//     notice, this list of conditions and the following disclaimer in the 
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of Stanford University nor the names of its 
+//     contributors may be used to endorse or promote products derived from 
+//     this software without specific prior written permission.
+//   
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// POSSIBILITY OF SUCH DAMAGE.
+//////////////////////////////////////////////////////////////////////////////
+
+MechanismController::MechanismController(HardwareInterface *hw){
+  this->hw = hw;
+  //Needs to learn pr2.xml filename or get it from param server
+  //Create robot model
+  //Set up transmissions, joints, and links
+  
+  //Set up ROS
+  //Subscribe to "n
+  //Advertise "new controller" service
+}
+
+void MechanismController::update(){
+  //Clear actuator commands
+  for(int i = 0; i < numTransmissions; i++){
+    r->transmission[i].propagatePosition();
+  }
+  //update KDL model with new joint position/velocities
+  //
+  //update controllers
+  for(int i = 0; i < numJoints; i++){
+    r->joint[i].enforceLimits();
+  }
+  for(int i = 0; i < numTransmissions; i++){
+    r->transmission[i].propagateEffort();
+  }
+}
