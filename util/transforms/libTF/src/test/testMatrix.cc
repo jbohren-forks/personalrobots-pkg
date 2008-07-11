5ee458fbc34612a63005fcc8855704191fb33d16
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-11 06:39:34 +0000

    more test cases using rand for inputs
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1475 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/transforms/libTF/src/test/testMatrix.cc
+++ util/transforms/libTF/src/test/testMatrix.cc
@@ -1,30 +1,44 @@
 #include "libTF/libTF.h"
 
+#include <sys/time.h>
+#include <cstdlib>
+
 int main(int argc, char ** argv)
 {
-  bool success = true;
+  //Seed random number generator with current microseond count
+  timeval temp_time_struct;
+  gettimeofday(&temp_time_struct,NULL);
+  srand(temp_time_struct.tv_usec);
+
+  bool success = true; //Flag of whether to say Success at the end
+
+  //Test many different premutations
   for (unsigned int j = 0; j < 3;j ++)
     {
       for (int i = -5; i < 5; i++)
 	{
 
 	  double yaw, pitch, roll;
-	  if (j ==0){
+	  if (j ==0){//yaw only
 	    yaw = 0.1*i ;
 	    roll = 0.0;
 	    pitch = 0.0;
 	  }
-	  else if (j == 1){
+	  else if (j == 1){ //pitch only
 	    yaw = 0.0;
 	    roll = 0.0;
 	    pitch = 0.1*i;
 	  }
-	  else {
+	  else if ( j== 3){ //roll only
 	    yaw = 0.0 ;
 	    roll = 0.1*i ;
 	    pitch = 0.0;
 	  }
-
+	  else { //random combinations
+	    yaw = ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
+	    pitch = ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
+	    roll = ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
+	  }
 
 	  	      
 	  NEWMAT::Matrix m = libTF::Pose3D::matrixFromEuler(0.0,
@@ -37,6 +51,7 @@ int main(int argc, char ** argv)
 	  libTF::Pose3D::Euler out = libTF::Pose3D::eulerFromMatrix(m);
 
 	  //TODO add +- 2PI checking/redundant angles checking
+	  // see if input is the same as output (accounting for floating point errors)
 	  if (fabs(out.yaw - yaw) > 0.001 || fabs(out.pitch - pitch) > 0.001 || fabs(out.roll -roll) > 0.0001)
 	    {
 
