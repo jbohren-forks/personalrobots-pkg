d269e235d7ace8c6449e4347e8e415ff8be2bb44
mmwise <mmwise@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-14 22:39:57 +0000

    fixed doxygen
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1561 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ controllers/genericControllers/doc.h
@@ -0,0 +1,8 @@
+/** 
+@mainpage
+@htmlinclude manifest.html
+
+Here are the controllers which are currently implemented:
+ \li \ref CONTROLLER::Pid "Pid"
+ \li \ref CONTROLLER::JointController "Joint Controller"
+**/
