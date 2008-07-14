b7c97e8d95cb1f2ba3c8004af8b1e05a621bf6b2
mmwise <mmwise@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-14 22:44:34 +0000

    removed Mod2pi fnc
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1564 f5854215-dd47-0410-b2c4-cdd35faa7885

--- controllers/genericControllers/src/Controller.cpp
+++ controllers/genericControllers/src/Controller.cpp
@@ -4,20 +4,6 @@
 
 using namespace CONTROLLER;
 
-//Static function for angle conversion
-double Controller::ModNPi2Pi(double angle)
-{
-   double theta = angle - ((int)(angle/(2*M_PI))*2*M_PI);
-   //double theta = fmod(angle,2*M_PI);
-   double result = theta;
-
-   if (theta > M_PI) 
-      result = theta - 2*M_PI;
-   if(theta < -M_PI)
-      result = theta + 2*M_PI;
-
-   return result;
-}
 
 Controller::Controller()
 {
