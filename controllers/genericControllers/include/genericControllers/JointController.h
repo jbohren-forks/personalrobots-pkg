5e7b89828722c52bb13f65cbea82d4f74cd0cfef
mmwise <mmwise@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-14 22:13:38 +0000

    added bsd
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1554 f5854215-dd47-0410-b2c4-cdd35faa7885

--- controllers/genericControllers/include/genericControllers/JointController.h
+++ controllers/genericControllers/include/genericControllers/JointController.h
@@ -1,7 +1,7 @@
 /////////////////////////////////////////////////////////////////////////////////////
 //Software License Agreement (BSD License)
 //
-//Copyright (c) 2008, Melonee Wise
+//Copyright (c) 2008, David Li, Melonee Wise, John Hsu
 //All rights reserved.
 //
 //Redistribution and use in source and binary forms, with or without
