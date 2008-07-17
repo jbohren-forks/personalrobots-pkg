efdd0f670a6d69b464068965ff0c5cf87d7acbd4
david_li <david_li@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-17 17:57:38 +0000

    Added functionality, works in simulator
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1698 f5854215-dd47-0410-b2c4-cdd35faa7885

--- controllers/pr2Controllers/include/pr2Controllers/LaserScannerController.h
+++ controllers/pr2Controllers/include/pr2Controllers/LaserScannerController.h
@@ -27,7 +27,10 @@ namespace CONTROLLER
   class LaserScannerController : Controller
   {
     public:
-    
+    //---------------------------------------------------------------------------------//
+    //CONSTRUCTION/DESTRUCTION CALLS
+    //---------------------------------------------------------------------------------//
+
       /*!
         * \brief Constructor.
         *
@@ -38,45 +41,23 @@ namespace CONTROLLER
       /*!
         * \brief Destructor.
         */       
-      ~LaserScannerController( );
+      ~LaserScannerController( ); 
 
-      /*!
-        * \brief Update controller
-        */       
-      void Update( );
 
-      /*!
-        * \brief Set scanning profile for the pitching Hokuyo
-        * 
-        * To be determined on how to implement
-        * For now, use sawtooth or sine wave
-        * Consider also, using setParam('profile','sawtooth')
-        *
-        */       
-      PR2::PR2_ERROR_CODE setProfile(double *&t, double *&x, int numElements);
-
-      /*!
-        * \brief Set parameters for this controller
-        *
-        * user can set maximum velocity
-        * and maximum acceleration
-        * constraints for this controller
-        *
-        * e.g. <br>
-        * <UL type="none">
-        * <LI> setParam('maxVel', 1.0);
-        * <LI> setParam('maxAcc', 1.0);
-        * <LI> setParam('profile','sawtooth');
-        * <LI> setParam('profile','sinewave');
-        * <LI> setParam('maxLim', 1.0);
-        * <LI> setParam('minLim',-1.0);
-        * <LI> setParam('rateHz'  , 1);
-        * </UL>
+        //TEMPORARY
+        /*! 
+        * \brief Temporary way to initialize limits and gains. Default argument for dt is 1 ms
         *
         */
-      PR2::PR2_ERROR_CODE setParam(std::string label,     double value);
-      PR2::PR2_ERROR_CODE setParam(std::string label,std::string value);
-  
+      void Init(double PGain, double IGain, double DGain, double IMax, double IMin, CONTROLLER_CONTROL_MODE mode, double time, double maxPositiveTorque, double maxNegativeTorque, double maxEffort, mechanism::Joint *joint);
+
+
+
+    //---------------------------------------------------------------------------------//
+    //AUTOMATIC PROFILE FUNCTIONS
+    //---------------------------------------------------------------------------------//
+
+     
         /*!
         * \brief Set automatic profile to sawtooth
         *
@@ -129,68 +110,18 @@ namespace CONTROLLER
         *
         */       
       void GenerateSquarewave(double *&x, double *&t, double period, double amplitude, double dt, double offset, unsigned int numElements);
+ 
+//---------------------------------------------------------------------------------//
+//MODE/ENABLE CALLS
+//---------------------------------------------------------------------------------//
+ 
+    /*!
+        * \brief Switches command mode type (Automatic, Torque, position, velocity control)
+        *  
+        */
+      void SetMode(CONTROLLER_CONTROL_MODE mode);
 
-
-      /*!
-      * \brief Give a torque command to be issue on update (if in torque mode)
-      *
-      * \param torque Torque command to issue
-      */
-    
-      CONTROLLER::CONTROLLER_ERROR_CODE SetTorqueCmd(double torque);
-
-	    /*!
-      * \brief Fetch the latest user issued torque command 
-      * 
-      * \param double* torque Pointer to value to change 
-      */ 
-      CONTROLLER::CONTROLLER_ERROR_CODE GetTorqueCmd(double *torque);
-    
-      /*!
-      * \brief Get the actual torque of the joint motor.
-      * 
-      * \param double* torque Pointer to value to change
-      */  
-      CONTROLLER::CONTROLLER_ERROR_CODE GetTorqueAct(double *torque);
-
-      /*!
-      * \brief Give set position of the joint for next update: revolute (angle) and prismatic (position)
-      * 
-      * \param double pos Position command to issue. Pos = 0 is home position of horizontal scan
-      */       
-      CONTROLLER::CONTROLLER_ERROR_CODE SetPosCmd(double pos);
-    
-      /*!
-      * \brief Get latest position command to the joint: revolute (angle) and prismatic (position).
-      * \param double* pos Pointer to value to change
-      */       
-      CONTROLLER::CONTROLLER_ERROR_CODE GetPosCmd(double *pos);
-    
-      /*!
-      * \brief Read the torque of the motor
-      * \param double* pos Pointer to value to change
-      */       
-      CONTROLLER::CONTROLLER_ERROR_CODE GetPosAct(double *pos);    
-    
-      /*!
-      * \brief Set velocity command to the joint to be issue next update
-      * \param double vel Velocity to issue next command
-      */
-      CONTROLLER::CONTROLLER_ERROR_CODE SetVelCmd(double vel);
-    
-      /*!
-      * \brief Get latest velocity command to the joint
-      * \param double* vel Pointer to value to change
-      */
-      CONTROLLER::CONTROLLER_ERROR_CODE GetVelCmd(double *vel);
-    
-      /*!
-      * \brief Get actual velocity of the joint
-      * \param double* vel Pointer to value to change
-      */
-      CONTROLLER::CONTROLLER_ERROR_CODE GetVelAct(double *vel);
-      
-        /*!
+    /*!
       * \brief Allow controller to send commands
       *      
       */
@@ -209,11 +140,140 @@ namespace CONTROLLER
       * 
       */
     
-      CONTROLLER::CONTROLLER_CONTROL_MODE GetMode(void);
+      CONTROLLER_CONTROL_MODE GetMode(void);
 
+        /*!
+        * \brief Return true if last command saturated the torque 
+        *
+        *  
+        */
+      bool CheckForSaturation(void);
+
+//---------------------------------------------------------------------------------//
+//TORQUE CALLS
+//---------------------------------------------------------------------------------//
+
+      /*!
+      * \brief Give a torque command to be issue on update (if in torque mode)
+      *
+      * \param torque Torque command to issue
+      */
+    
+
+      CONTROLLER_ERROR_CODE SetTorqueCmd(double torque);
+
+	    /*!
+      * \brief Fetch the latest user issued torque command 
+      * 
+      * \param double* torque Pointer to value to change 
+      */ 
+      CONTROLLER_ERROR_CODE GetTorqueCmd(double *torque);
+    
+      /*!
+      * \brief Get the actual torque of the joint motor.
+      * 
+      * \param double* torque Pointer to value to change
+      */  
+      CONTROLLER_ERROR_CODE GetTorqueAct(double *torque);
+
+//---------------------------------------------------------------------------------//
+//POSITION CALLS
+//---------------------------------------------------------------------------------//
+
+      /*!
+      * \brief Give set position of the joint for next update: revolute (angle) and prismatic (position)
+      * 
+      * \param double pos Position command to issue. Pos = 0 is home position of horizontal scan
+      */       
+      CONTROLLER_ERROR_CODE SetPosCmd(double pos);
+    
+      /*!
+      * \brief Get latest position command to the joint: revolute (angle) and prismatic (position).
+      * \param double* pos Pointer to value to change
+      */       
+      CONTROLLER_ERROR_CODE GetPosCmd(double *pos);
+    
+      /*!
+      * \brief Read the torque of the motor
+      * \param double* pos Pointer to value to change
+      */       
+      CONTROLLER_ERROR_CODE GetPosAct(double *pos);    
+ 
+//---------------------------------------------------------------------------------//
+//VELOCITY CALLS
+//---------------------------------------------------------------------------------//
+   
+      /*!
+      * \brief Set velocity command to the joint to be issue next update
+      * \param double vel Velocity to issue next command
+      */
+      CONTROLLER_ERROR_CODE SetVelCmd(double vel);
+    
+      /*!
+      * \brief Get latest velocity command to the joint
+      * \param double* vel Pointer to value to change
+      */
+      CONTROLLER_ERROR_CODE GetVelCmd(double *vel);
+    
+      /*!
+      * \brief Get actual velocity of the joint
+      * \param double* vel Pointer to value to change
+      */
+      CONTROLLER_ERROR_CODE GetVelAct(double *vel);
+      
+//---------------------------------------------------------------------------------//
+//UPDATE CALLS
+//---------------------------------------------------------------------------------//
+    /*!
+        * \brief Issues commands to joint based on control mode
+        *
+        * 
+        */
+
+      //Issues commands to the joint. Should be called at regular intervals
+       void Update();
+
+//---------------------------------------------------------------------------------//
+//PARAM SERVER CALLS
+//---------------------------------------------------------------------------------//
+
+      /*!
+        * \brief Set scanning profile for the pitching Hokuyo
+        * 
+        * To be determined on how to implement
+        * For now, use sawtooth or sine wave
+        * Consider also, using setParam('profile','sawtooth')
+        *
+        */       
+      PR2::PR2_ERROR_CODE setProfile(double *&t, double *&x, int numElements);
+
+      /*!
+        * \brief Set parameters for this controller
+        *
+        * user can set maximum velocity
+        * and maximum acceleration
+        * constraints for this controller
+        *
+        * e.g. <br>
+        * <UL type="none">
+        * <LI> setParam('maxVel', 1.0);
+        * <LI> setParam('maxAcc', 1.0);
+        * <LI> setParam('profile','sawtooth');
+        * <LI> setParam('profile','sinewave');
+        * <LI> setParam('maxLim', 1.0);
+        * <LI> setParam('minLim',-1.0);
+        * <LI> setParam('rateHz'  , 1);
+        * </UL>
+        *
+        */
+      PR2::PR2_ERROR_CODE setParam(std::string label,     double value);
+      PR2::PR2_ERROR_CODE setParam(std::string label,std::string value);
+
+    
         private:
-  
-      CONTROLLER::CONTROLLER_CONTROL_MODE controlMode; /**Allow different control modes for hokuyo>*/
+      int counter; 
+      bool enabled; /**< Track enabled stats>*/
+      CONTROLLER_CONTROL_MODE controlMode; /**<Allow different control modes for hokuyo>*/
 
       std::string name; /**<Namespace identifier for ROS>*/      
 
@@ -222,9 +282,9 @@ namespace CONTROLLER
       int profileIndex; /**<Track location in profile>*/
       int profileLength; /**<Number of points in one cycle>*/
 
-      double lastCycleStart; //**<Start of the last cycle>*/
+      double lastCycleStart; //**<Start of the last cycle>*/      
 
-      CONTROLLER::JointController lowerControl; /**< Lower level control done by JointController>*/
+      JointController*  lowerControl; /**< Lower level control done by JointController>*/
 
       double cmdPos; /**<Last commanded position>*/
       double cmdVel; /**<Last commanded velocity>*/
--- controllers/pr2Controllers/src/LaserScannerController.cpp
+++ controllers/pr2Controllers/src/LaserScannerController.cpp
@@ -1,88 +1,67 @@
 #include <pr2Controllers/LaserScannerController.h>
-
+#define DEMO
 
 using namespace CONTROLLER;
 
+ //---------------------------------------------------------------------------------//
+ //CONSTRUCTION/DESTRUCTION CALLS
+ //---------------------------------------------------------------------------------//
+
+
 LaserScannerController::LaserScannerController()
 {
+  controlMode = CONTROLLER_DISABLED;
 
 	profileX = NULL;
 	profileT = NULL; 
-
-	lowerControl.GetTime(&lastCycleStart); //Mark this time as beginning of cycle
+  counter = 0;
 }
     
 LaserScannerController::~LaserScannerController( )
 {
 	if(profileX!=NULL) delete[] profileX;
 	if(profileT!=NULL) delete[] profileT;
+
+  delete lowerControl;
 }
 
 /*
 LaserScannerController::LaserScannerController(Joint* joint, std::string name);
 	//Pass in joint* when we get it
-	lowerControl.Init(joint,name);
+	lowerControl->Init(joint,name);
   this->name = name;
 	//Set gains
 	
 	// Set control mode 
-	lowerControl.SetMode(CONTROLLER::CONTROLLER_POSITION);
+	lowerControl->SetMode(CONTROLLER_POSITION);
 	
 	//Enable controller
-	//lowerControl.EnableController();
+	//lowerControl->EnableController();
 	
 	
 }
 */
+ 
+void LaserScannerController::Init(double PGain, double IGain, double DGain, double IMax, double IMin, CONTROLLER_CONTROL_MODE mode, double time, double maxPositiveTorque, double maxNegativeTorque, double maxEffort, mechanism::Joint *joint){
+  
+  //If we're in automatic mode, want to pass down position control mode to lower controller
+  CONTROLLER_CONTROL_MODE newMode;
 
-void LaserScannerController::Update( )
-{
+  controlMode = mode;
+  if(mode==CONTROLLER_AUTOMATIC) {
+    newMode = CONTROLLER_POSITION;
+  }
+  else newMode = mode;
 
-	double currentTime;
-	lowerControl.GetTime(&currentTime);
-	//Check for automatic scan mode
-	if(controlMode == CONTROLLER::CONTROLLER_AUTOMATIC){
-		if(currentTime-lastCycleStart > profileT[profileIndex]){ //Check to see whether it's time for a new setpoint
-			lowerControl.SetPosCmd(profileX[profileIndex]); 
-			
-			//Advance time index. Reset if necessary
-			if(profileIndex == profileLength-1){
-				profileIndex = 0; //Restart profile
-				lastCycleStart = currentTime;
-			}
-			else profileIndex++;
-		}
-		//No new setpoint necessary
-	}
-	
-  //Instruct lower level controller to update
-	lowerControl.Update();
-	
+  lowerControl = new JointController();
+  lowerControl->Init(PGain,IGain,DGain,IMax,IMin,mode,time,maxPositiveTorque,maxNegativeTorque,maxEffort,joint);
+
+  EnableController();
 }
 
-PR2::PR2_ERROR_CODE LaserScannerController::setProfile(double *&t, double *&x, int numElements)
-{
-	profileX = x;
-	profileT = t;
-	profileLength = numElements; //Set length of profile
-	profileIndex = 0; //Start at beginning of profile
-  return PR2::PR2_ALL_OK;
-}
-
-PR2::PR2_ERROR_CODE
-LaserScannerController::setParam(std::string label,double value)
-{
-
-  return PR2::PR2_ALL_OK;
-}
-
-PR2::PR2_ERROR_CODE
-LaserScannerController::setParam(std::string label,std::string value)
-{
-
-  return PR2::PR2_ALL_OK;
-}
-
+ //---------------------------------------------------------------------------------//
+ //AUTOMATIC PROFILE FUNCTIONS
+ //---------------------------------------------------------------------------------//
 void LaserScannerController::SetSawtoothProfile(double period, double amplitude, double dt, double offset){
 
 	unsigned int elements = (unsigned int) (period/dt);
@@ -137,7 +116,6 @@ void LaserScannerController::GenerateSinewave(double *&x, double *&t, double per
 
 }
 
-
 void LaserScannerController::SetSquarewaveProfile(double period, double amplitude, double dt, double offset){
 	setParam("profile", "squarewave");
 	unsigned int elements = (unsigned int) (period/dt);
@@ -165,102 +143,208 @@ void LaserScannerController::GenerateSquarewave(double *&x, double *&t, double p
 		else x[i] = amplitude + offset;
 	}
 }
+ 
+PR2::PR2_ERROR_CODE LaserScannerController::setProfile(double *&t, double *&x, int numElements)
+{
+	profileX = x;
+	profileT = t;
+	profileLength = numElements; //Set length of profile
+	profileIndex = 0; //Start at beginning of profile
+  return PR2::PR2_ALL_OK;
+}
 
+//---------------------------------------------------------------------------------//
+//MODE/ENABLE CALLS
+//---------------------------------------------------------------------------------//
 
-//-
-//Torque
-//-
+//Set the controller control mode
+void LaserScannerController::SetMode(CONTROLLER_CONTROL_MODE mode){
+  //Record top level of control mode
+  controlMode = mode;
 
-
-CONTROLLER::CONTROLLER_ERROR_CODE LaserScannerController::SetTorqueCmd(double torque){
-	if(GetMode() != CONTROLLER::CONTROLLER_TORQUE)  //Make sure we're in torque command mode
-	return CONTROLLER::CONTROLLER_MODE_ERROR;
-	
-  return lowerControl.SetTorqueCmd(torque);
+  //Want the lower level controller to be set to position control if we're providing an automatic profile
+  if(mode==CONTROLLER_AUTOMATIC) {
+    lowerControl->SetMode(CONTROLLER_POSITION);
   }
-
-//Return current torque command
-CONTROLLER::CONTROLLER_ERROR_CODE
-LaserScannerController::GetTorqueCmd(double *torque)
-{
-  *torque = cmdTorque;
-  return CONTROLLER::CONTROLLER_ALL_OK; 
+  else{
+    lowerControl->SetMode(mode);
+  }
 }
 
-//Query motor for actual torque 
-CONTROLLER::CONTROLLER_ERROR_CODE
-LaserScannerController::GetTorqueAct(double *torque)
-{
-	return lowerControl.GetTorqueAct(torque);
-}
 
-//-
-//Position
-//-
-
-//Query mode, then set desired position 
-CONTROLLER::CONTROLLER_ERROR_CODE LaserScannerController::SetPosCmd(double pos)
-{
-	if(GetMode() != CONTROLLER::CONTROLLER_POSITION)  //Make sure we're in position command mode
-	return CONTROLLER::CONTROLLER_MODE_ERROR;
-	
-  return lowerControl.SetPosCmd(pos);
-
-	
-}
-
-//Return the current position command
-CONTROLLER::CONTROLLER_ERROR_CODE LaserScannerController::GetPosCmd(double *pos)
-{
-  *pos = cmdPos;
-	return CONTROLLER::CONTROLLER_ALL_OK;
-}
-
-//Query the joint for the actual position
-CONTROLLER::CONTROLLER_ERROR_CODE LaserScannerController::GetPosAct(double *pos)
-{
-  return	lowerControl.GetPosAct(pos);
-}
-
-//
-//-
-//Velocity
-//-
-//Check mode, then set the commanded velocity
-CONTROLLER::CONTROLLER_ERROR_CODE LaserScannerController::SetVelCmd(double vel)
-{
-	if(GetMode() != CONTROLLER::CONTROLLER_VELOCITY)  //Make sure we're in velocity command mode
-	return CONTROLLER::CONTROLLER_MODE_ERROR;
-
-  return  lowerControl.SetVelCmd(vel);
-}
-
-//Return the internally stored commanded velocity
-CONTROLLER::CONTROLLER_ERROR_CODE LaserScannerController::GetVelCmd(double *vel)
-{
-  return lowerControl.GetVelCmd(vel);
-}
-
-//Query our joint for velocity
-CONTROLLER::CONTROLLER_ERROR_CODE LaserScannerController::GetVelAct(double *vel)
-{
-//	lowerControl.GetVelAct(vel);
-	return CONTROLLER::CONTROLLER_ALL_OK;
-}
-
-CONTROLLER::CONTROLLER_CONTROL_MODE LaserScannerController::GetMode(void){
+CONTROLLER_CONTROL_MODE LaserScannerController::GetMode(void){
 	return controlMode;
 }
 
 
 //Allow controller to function
 void LaserScannerController::EnableController(){
-  lowerControl.EnableController();
+  enabled = true;
+  lowerControl->EnableController();
 }
 
 //Disable functioning. Set joint torque to zero.
 void LaserScannerController::DisableController(){
-  lowerControl.DisableController();   
+  enabled = false;
+  lowerControl->DisableController();   
+}
+
+//Check for saturation of last command
+bool LaserScannerController::CheckForSaturation(void){ 
+  return lowerControl->CheckForSaturation();
+}
+
+
+
+//---------------------------------------------------------------------------------//
+//TORQUE CALLS
+//---------------------------------------------------------------------------------//
+
+CONTROLLER_ERROR_CODE LaserScannerController::SetTorqueCmd(double torque){
+	if(controlMode != CONTROLLER_TORQUE)  //Make sure we're in torque command mode
+	return CONTROLLER_MODE_ERROR;
+	
+  return lowerControl->SetTorqueCmd(torque);
+  }
+
+//Return current torque command
+CONTROLLER_ERROR_CODE
+LaserScannerController::GetTorqueCmd(double *torque)
+{
+  *torque = cmdTorque;
+  return CONTROLLER_ALL_OK; 
+}
+
+//Query motor for actual torque 
+CONTROLLER_ERROR_CODE
+LaserScannerController::GetTorqueAct(double *torque)
+{
+	return lowerControl->GetTorqueAct(torque);
+}
+
+//---------------------------------------------------------------------------------//
+//POSITION CALLS
+//---------------------------------------------------------------------------------//
+
+//Query mode, then set desired position 
+CONTROLLER_ERROR_CODE LaserScannerController::SetPosCmd(double pos)
+{
+	if(controlMode != CONTROLLER_POSITION)  //Make sure we're in position command mode
+	return CONTROLLER_MODE_ERROR;
+	
+  return lowerControl->SetPosCmd(pos);
+
+	
+}
+
+//Return the current position command
+CONTROLLER_ERROR_CODE LaserScannerController::GetPosCmd(double *pos)
+{
+  *pos = cmdPos;
+	return CONTROLLER_ALL_OK;
+}
+
+//Query the joint for the actual position
+CONTROLLER_ERROR_CODE LaserScannerController::GetPosAct(double *pos)
+{
+  return	lowerControl->GetPosAct(pos);
+}
+
+ 
+//---------------------------------------------------------------------------------//
+//VELOCITY CALLS
+//---------------------------------------------------------------------------------//
+//Check mode, then set the commanded velocity
+CONTROLLER_ERROR_CODE LaserScannerController::SetVelCmd(double vel)
+{
+	if( controlMode != CONTROLLER_VELOCITY)  //Make sure we're in velocity command mode
+	return CONTROLLER_MODE_ERROR;
+
+  return  lowerControl->SetVelCmd(vel);
+}
+
+//Return the internally stored commanded velocity
+CONTROLLER_ERROR_CODE LaserScannerController::GetVelCmd(double *vel)
+{
+  return lowerControl->GetVelCmd(vel);
+}
+
+//Query our joint for velocity
+CONTROLLER_ERROR_CODE LaserScannerController::GetVelAct(double *vel)
+{
+//	lowerControl->GetVelAct(vel);
+	return CONTROLLER_ALL_OK;
+}
+      
+//---------------------------------------------------------------------------------//
+//UPDATE CALLS
+//---------------------------------------------------------------------------------//
+
+void LaserScannerController::Update( )
+{
+  if(!enabled) return; //Check for enabled
+ /* 	double currentTime;
+	lowerControl->GetTime(&currentTime);
+	//Check for automatic scan mode
+	if(controlMode == CONTROLLER_AUTOMATIC){
+		if(currentTime-lastCycleStart > profileT[profileIndex]){ //Check to see whether it's time for a new setpoint
+			lowerControl->SetPosCmd(profileX[profileIndex]); 
+			
+			//Advance time index. Reset if necessary
+			if(profileIndex == profileLength-1){
+				profileIndex = 0; //Restart profile
+				lastCycleStart = currentTime;
+			}
+			else profileIndex++;
+		}
+		//No new setpoint necessary
+	}
+	*/
+
+    if(controlMode == CONTROLLER_AUTOMATIC){
+			lowerControl->SetPosCmd(profileX[profileIndex]);
+      //Every x calls, advance profile Index
+      if(counter==1000){ //Hardcoded for now...
+       counter = 0;			
+		  	//Advance time index. Reset if necessary
+		  	if(profileIndex == profileLength-1){
+			  	profileIndex = 0; //Restart profile
+			  } else profileIndex++;
+      }
+      else counter++;
+		}
+		//No new setpoint necessary
+	
+ 
+   
+  /*
+  double torque;
+  //Print torque
+  lowerControl->GetTorqueCmd(&torque);
+  std::cout<<"Torque:"<<torque<<std::endl;
+ //Instruct lower level controller to update
+ */
+	lowerControl->Update();
+
+}
+
+//---------------------------------------------------------------------------------//
+//PARAM SERVER CALLS
+//---------------------------------------------------------------------------------//
+
+
+PR2::PR2_ERROR_CODE
+LaserScannerController::setParam(std::string label,double value)
+{
+
+  return PR2::PR2_ALL_OK;
+}
+
+PR2::PR2_ERROR_CODE
+LaserScannerController::setParam(std::string label,std::string value)
+{
+
+  return PR2::PR2_ALL_OK;
 }
 
 
