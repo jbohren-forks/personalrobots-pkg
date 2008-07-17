0787ea2f15f97b15f5675a46e45bb779ee25aeec
mmwise <mmwise@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-17 00:31:56 +0000

    testing commit
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1678 f5854215-dd47-0410-b2c4-cdd35faa7885

--- controllers/genericControllers/doc.h
+++ controllers/genericControllers/doc.h
@@ -2,6 +2,8 @@
 @mainpage
 @htmlinclude manifest.html
 
+test
+
 Here are the controllers which are currently implemented:
  \li \ref CONTROLLER::Pid "Pid"
  \li \ref CONTROLLER::JointController "Joint Controller"
