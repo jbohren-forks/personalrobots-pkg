24e59cad143276e58abd14c686d184c19dd87aff
mcgann <mcgann@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-20 22:11:07 +0000

    Restructured examples
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1819 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ exec_trex/ROSAdapter.cc
@@ -0,0 +1,90 @@
+#include "ROSAdapter.hh"
+#include "Agent.hh"
+#include "Observer.hh"
+#include "Object.hh"
+#include "Debug.hh"
+#include "Token.hh"
+#include "TokenVariable.hh"
+#include "Observer.hh"
+#include "ROSNode.hh"
+#include "Logger.hh"
+
+namespace TREX {
+
+  ROSAdapter::ROSAdapter(const LabelStr& agentName, const TiXmlElement& configData)
+    : Adapter(agentName, configData){
+  }
+
+  void ROSAdapter::handleInit(TICK initialTick, const std::map<double, ServerId>& serversByTimeline, 
+				 const ObserverId& observer){
+    m_observer = observer;
+    m_node = ROSNode::request();
+
+    // Wait till we get a message before starting the agent
+    while(!m_node->isInitialized() && m_node->ok()){
+      debugMsg("ROSAdapter:Create", "Waiting...");
+      sleep(1);
+    }
+
+  }
+
+  ROSAdapter::~ROSAdapter() {
+    m_node->release();
+  }
+
+  bool ROSAdapter::synchronize(){
+    debugMsg("ROSAdapter:synchronize", nameString() << "Checking..");
+    std::vector<Observation*> obsBuffer;
+    m_node->get_obs(obsBuffer);
+
+    // Because the VelCommander is an abstracted timeline we are generating a value for it
+    // that is hardwired
+    if(getCurrentTick() == 0 && Agent::instance()->getOwner("vcom") == getId()){
+      ObservationByValue* obs = new ObservationByValue("vcom", "VelCommander.Holds");
+      obs->push_back("cmd_x", new IntervalDomain(0.0));
+      obs->push_back("cmd_th", new IntervalDomain(0.0));
+      obsBuffer.push_back(obs);
+    }
+
+    for(std::vector<Observation*>::const_iterator it = obsBuffer.begin(); it != obsBuffer.end(); ++it){
+      Observation* obs = *it;
+      debugMsg("ROSAdapter:synchronize", nameString() << obs->toString());
+      m_observer->notify(*obs);
+      delete obs;
+    }
+
+    return true;
+  }
+
+  void ROSAdapter::handleNextTick(){}
+
+  void ROSAdapter::handleRequest(const TokenId& token){
+    // Constants for Timelines handled
+    static const LabelStr VELOCITY_COMMANDER("VelCommander");
+    static const LabelStr ARM_CONTROLLER("ArmController");
+
+    bool returnObservation = false;
+
+    if(token->getBaseObjectType() == VELOCITY_COMMANDER){
+      debugMsg("ROSAdapter:handleRequest", token->toString());
+      m_node->dispatchVel(token);
+      returnObservation = true;
+    }
+    else if(token->getBaseObjectType() == ARM_CONTROLLER) {
+      debugMsg("RCSArmAdapter:handleRequest", token->toString());
+      m_node->dispatchArm(token, getCurrentTick());
+
+      //assuming instantly active
+      if(token->getPredicateName() == LabelStr("ArmController.Active"))
+	returnObservation = true;
+    }
+
+    if(returnObservation){
+      ObservationByReference obs(token);
+      m_observer->notify(obs);
+    }
+  }
+
+  // Allocate a Factory
+  TeleoReactor::ConcreteFactory<ROSAdapter> l_ROSAdapter_Factory("ROS");
+}
