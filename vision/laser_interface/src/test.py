2cd5c7c24a912cb1068cd0e557e203dbc6cf6949
sfkwc <sfkwc@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-22 00:52:29 +0000

    more portable test case
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1882 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/laser_interface/src/test.py
+++ vision/laser_interface/src/test.py
@@ -1,11 +1,21 @@
+PKG = 'laser_interface'
+import sys, os, subprocess
+try:
+    rostoolsDir = (subprocess.Popen(['rospack', 'find', 'rostools'], stdout=subprocess.PIPE).communicate()[0] or '').strip()
+    sys.path.append(os.path.join(rostoolsDir,'src'))
+    import rostools.launcher
+    rostools.launcher.updateSysPath(sys.argv[0], PKG, bootstrapVersion="0.6")
+except ImportError:
+    print >> sys.stderr, "\nERROR: Cannot locate rostools"
+    sys.exit(1)  
+
 from pkg import *
 from std_msgs.msg import Point3DFloat64
 from std_msgs.msg import RobotBase2DOdom
-import camera as cam
 import sys
 
 def debug_me(p):
     print 'received', p.pos.x, p.pos.y, p.pos.th
 rospy.TopicSub('odom', RobotBase2DOdom, debug_me)
-rospy.ready(sys.argv[0])
+rospy.ready('test')
 rospy.spin()
--- vision/laser_interface/src/test2.py
+++ vision/laser_interface/src/test2.py
@@ -1,16 +1,27 @@
+PKG = 'laser_interface'
+import sys, os, subprocess
+try:
+    rostoolsDir = (subprocess.Popen(['rospack', 'find', 'rostools'], stdout=subprocess.PIPE).communicate()[0] or '').strip()
+    sys.path.append(os.path.join(rostoolsDir,'src'))
+    import rostools.launcher
+    rostools.launcher.updateSysPath(sys.argv[0], PKG, bootstrapVersion="0.6")
+except ImportError:
+    print >> sys.stderr, "\nERROR: Cannot locate rostools"
+    sys.exit(1)  
+
 from pkg import *
 from std_msgs.msg import Point3DFloat64
 from std_msgs.msg import RobotBase2DOdom
 from std_msgs.msg import Pose2DFloat32
-import camera as cam
 import sys
 import time
 
 pub = rospy.TopicPub('odom', RobotBase2DOdom)
 rospy.ready(sys.argv[0])
 
-while True:
-    odom = RobotBase2DOdom('header', Pose2DFloat32(1,2,3), Pose2DFloat32(1,2,4), 1)
+while not rospy.isShutdown():
+    odom = RobotBase2DOdom(None, Pose2DFloat32(1,2,3), Pose2DFloat32(1,2,4), 1)
+    pub.publish(odom)
     time.sleep(.5)
     print 'published'
 
