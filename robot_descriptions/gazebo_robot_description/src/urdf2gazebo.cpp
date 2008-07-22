cd3d7a9254673a1aed0b27f77bba3402e098aa02
isucan <isucan@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-22 01:09:23 +0000

    basic setup for conversion to gazebo
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1883 f5854215-dd47-0410-b2c4-cdd35faa7885

--- robot_descriptions/gazebo_robot_description/src/urdf2gazebo.cpp
+++ robot_descriptions/gazebo_robot_description/src/urdf2gazebo.cpp
@@ -1,7 +1,44 @@
+#include <cstdio>
+#include <cstdlib>
+#include <urdf/URDF.h>
 
+void convert(robot_desc::URDF &wgxml, TiXmlDocument &doc)
+{
+    
+}
+
+void usage(const char *progname)
+{
+    printf("\nUsage: %s wgDescription.xml Gazebo.model\n", progname);
+    printf("       where wgDescription.xml is the file containing a robot description in the Willow Garage format\n");
+    printf("       and Gazebo.model is the file where the Gazebo model should be written\n\n");
+}
 
 int main(int argc, char **argv)
 {
+    if (argc != 3)
+    {
+	usage(argv[0]);
+	exit(1);
+    }
+    
+    robot_desc::URDF wgxml;
 
+    if (!wgxml.loadFile(argv[1]))
+    {
+	printf("Unable to load robot model from %s\n", argv[1]);	
+	exit(2);
+    }
+    
+    TiXmlDocument doc;
+    
+    convert(wgxml, doc);
+    
+    if (!doc.SaveFile(argv[2]))
+    {
+	printf("Unable to save gazebo model in %s\n", argv[2]);	
+	exit(3);
+    }
+    
     return 0;
 }
