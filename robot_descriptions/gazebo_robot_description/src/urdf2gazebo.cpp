93f70f0cb7b2006ab9a197de0b45c6644f8648fd
isucan <isucan@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-22 19:58:34 +0000

    sending XML description to param server
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1910 f5854215-dd47-0410-b2c4-cdd35faa7885

--- robot_descriptions/gazebo_robot_description/src/urdf2gazebo.cpp
+++ robot_descriptions/gazebo_robot_description/src/urdf2gazebo.cpp
@@ -1,13 +1,36 @@
 #include <cstdio>
 #include <cstdlib>
-#include <urdf/URDF.h>
+#include <cmath>
+
 #include <vector>
+#include <string>
+#include <sstream>
+
+#include <urdf/URDF.h>
+
+std::string values2str(unsigned int count, const double *values, double (*conv)(double) = NULL)
+{
+    std::stringstream ss;
+    for (unsigned int i = 0 ; i < count ; i++)
+    {
+	if (i > 0)
+	    ss << " ";
+	ss << (conv ? conv(values[i]) : values[i]);
+    }
+    return ss.str();
+}
+
+double rad2deg(double v)
+{
+    return v * 180.0 / M_PI;
+}
 
 void convert(robot_desc::URDF &wgxml, TiXmlDocument &doc)
 {
     TiXmlDeclaration *decl = new TiXmlDeclaration("1.0", "", "");
     doc.LinkEndChild(decl);
     
+    /* create root element and define needed namespaces */
     TiXmlElement *root = new TiXmlElement("model:physical");
     root->SetAttribute("xmlns:model", "http://playerstage.sourceforge.net/gazebo/xmlschema/#model");
     root->SetAttribute("xmlns:sensor", "http://playerstage.sourceforge.net/gazebo/xmlschema/#sensor");
@@ -18,13 +41,14 @@ void convert(robot_desc::URDF &wgxml, TiXmlDocument &doc)
     root->SetAttribute("xmlns:interface", "http://playerstage.sourceforge.net/gazebo/xmlschema/#interface");
     doc.LinkEndChild(root);
     
+    /* set the transform for the whole model to identity */
     TiXmlElement *xyz      = new TiXmlElement("xyz");
-    TiXmlText    *text_xyz = new TiXmlText("0.0 0.0 0.0");
+    TiXmlText    *text_xyz = new TiXmlText("0 0 0");
     xyz->LinkEndChild(text_xyz);    
     root->LinkEndChild(xyz);
     TiXmlElement *rpy = new TiXmlElement("rpy");
-    TiXmlText    *text_rpy = new TiXmlText("0.0 0.0 0.0");
-    rpy->LinkEndChild(text_rpy);    
+    TiXmlText    *text_rpy = new TiXmlText("0 0 0");
+    rpy->LinkEndChild(text_rpy);
     root->LinkEndChild(rpy);    
 
     std::vector<robot_desc::URDF::Link*> links;
@@ -32,9 +56,135 @@ void convert(robot_desc::URDF &wgxml, TiXmlDocument &doc)
     
     for (unsigned int i = 0 ; i < links.size() ; ++i)
     {
-	TiXmlElement *elem = new TiXmlElement("body:box");
-	root->LinkEndChild(elem);	
-    }    
+	std::string type;
+	unsigned int nsize = 0;
+	switch (links[i]->collision->geometry->type)
+	{
+	case robot_desc::URDF::Link::Geometry::BOX:
+	    nsize = 3;
+	    type = "box";
+	    break;
+	case robot_desc::URDF::Link::Geometry::CYLINDER:
+	    nsize = 2;
+	    type = "cylinder";
+	    break;
+	case robot_desc::URDF::Link::Geometry::SPHERE:
+	    nsize = 1;
+	    type = "sphere";
+	    break;
+	default:
+	    nsize = 0;
+	    printf("Unknown body type: %d in link '%s'\n", links[i]->collision->geometry->type, links[i]->name.c_str());
+	    break;
+	}
+	if (!type.empty())
+	{
+	    /* create new body */
+	    TiXmlElement *elem     = new TiXmlElement("body:" + type);
+
+	    /* set body name */
+	    elem->SetAttribute("name", links[i]->name);
+	    
+	    /* set translation */
+	    TiXmlElement *xyz      = new TiXmlElement("xyz");
+	    TiXmlText    *text_xyz = new TiXmlText(values2str(3, links[i]->xyz));
+	    xyz->LinkEndChild(text_xyz);    
+	    elem->LinkEndChild(xyz);
+
+	    /* set rotation */
+	    TiXmlElement *rpy      = new TiXmlElement("rpy");
+	    TiXmlText    *text_rpy = new TiXmlText(values2str(3, links[i]->rpy, rad2deg));
+	    rpy->LinkEndChild(text_rpy);    
+	    elem->LinkEndChild(rpy);	    
+	    
+	    /* create geometry node */
+	    TiXmlElement *geom     = new TiXmlElement("geom:" + type);
+
+	    {		
+		/* set its name */
+		geom->SetAttribute("name", links[i]->collision->geometry->name);
+		
+		/* set geometry translation */
+		TiXmlElement *xyz      = new TiXmlElement("xyz");
+		TiXmlText    *text_xyz = new TiXmlText(values2str(3, links[i]->collision->xyz));
+		xyz->LinkEndChild(text_xyz);
+		geom->LinkEndChild(xyz);
+		
+		/* set geometry rotation */
+		TiXmlElement *rpy      = new TiXmlElement("rpy");
+		TiXmlText    *text_rpy = new TiXmlText(values2str(3, links[i]->collision->rpy, rad2deg));
+		rpy->LinkEndChild(text_rpy);    
+		geom->LinkEndChild(rpy);
+
+		/* set mass properties */
+		TiXmlElement *massMatrix      = new TiXmlElement("massMatrix");
+		TiXmlText    *text_massMatrix = new TiXmlText("true");
+		massMatrix->LinkEndChild(text_massMatrix);
+		geom->LinkEndChild(massMatrix);
+		TiXmlElement *mass            = new TiXmlElement("mass");
+		TiXmlText    *text_mass       = new TiXmlText(values2str(1, &links[i]->inertial->mass));
+		mass->LinkEndChild(text_mass);
+		geom->LinkEndChild(mass);
+ 		static const char tagList1[6][4] = {"ixx", "ixy", "ixz", "iyy", "iyz", "izz"};
+		for (int j = 0 ; j < 6 ; ++j)
+		{
+		    TiXmlElement *ie      = new TiXmlElement(tagList1[j]);
+		    TiXmlText    *text_ie = new TiXmlText(values2str(1, links[i]->inertial->inertia + j));
+		    ie->LinkEndChild(text_ie);
+		    geom->LinkEndChild(ie); 
+		}
+		static const char tagList2[3][3] = {"cx", "cy", "cz"};
+		for (int j = 0 ; j < 3 ; ++j)
+		{
+		    TiXmlElement *ie      = new TiXmlElement(tagList2[j]);
+		    TiXmlText    *text_ie = new TiXmlText(values2str(1, links[i]->inertial->com + j));
+		    ie->LinkEndChild(text_ie);
+		    geom->LinkEndChild(ie); 
+		}
+		
+		/* set additional data */
+		static const char tagList3[4][4] = {"kp", "kd", "mu1", "mu2"};
+		for (int j = 0 ; j < 4 ; ++j)
+		    if (links[i]->collision->data.hasDefaultValue(tagList3[j]))
+		    {
+			TiXmlElement *ie      = new TiXmlElement(tagList3[j]);
+			TiXmlText    *text_ie = new TiXmlText(links[i]->collision->data.getDefaultValue(tagList3[j]));
+			ie->LinkEndChild(text_ie);
+			geom->LinkEndChild(ie);
+		    }
+		
+		/* set geometry size */
+		TiXmlElement *size      = new TiXmlElement("size");
+		TiXmlText    *text_size = new TiXmlText(values2str(nsize, links[i]->collision->geometry->size));
+		size->LinkEndChild(text_size);
+		geom->LinkEndChild(size);
+		
+		/* create visual node */
+		TiXmlElement *visual = new TiXmlElement("visual");
+		{
+		    /* set geometry translation */
+		    TiXmlElement *xyz      = new TiXmlElement("xyz");
+		    TiXmlText    *text_xyz = new TiXmlText(values2str(3, links[i]->visual->xyz));
+		    xyz->LinkEndChild(text_xyz);
+		    geom->LinkEndChild(xyz);
+		    
+		    /* set geometry rotation */
+		    TiXmlElement *rpy      = new TiXmlElement("rpy");
+		    TiXmlText    *text_rpy = new TiXmlText(values2str(3, links[i]->visual->rpy, rad2deg));
+		    rpy->LinkEndChild(text_rpy);    
+		    geom->LinkEndChild(rpy); 
+		}
+		
+		geom->LinkEndChild(visual);
+	    }
+	    
+	    /* add geometry to body */
+	    elem->LinkEndChild(geom);	    
+	    
+	    /* add body to document */
+	    root->LinkEndChild(elem);
+	}	
+    }
 }
 
 void usage(const char *progname)
