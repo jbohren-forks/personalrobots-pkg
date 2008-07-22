c9a48f08c52fc8d6d947aae2bbcf4e0e96344c0b
isucan <isucan@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-22 01:43:22 +0000

    incremental update on model conversion
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1886 f5854215-dd47-0410-b2c4-cdd35faa7885

--- robot_descriptions/gazebo_robot_description/src/urdf2gazebo.cpp
+++ robot_descriptions/gazebo_robot_description/src/urdf2gazebo.cpp
@@ -1,10 +1,40 @@
 #include <cstdio>
 #include <cstdlib>
 #include <urdf/URDF.h>
+#include <vector>
 
 void convert(robot_desc::URDF &wgxml, TiXmlDocument &doc)
 {
+    TiXmlDeclaration *decl = new TiXmlDeclaration("1.0", "", "");
+    doc.LinkEndChild(decl);
     
+    TiXmlElement *root = new TiXmlElement("model:physical");
+    root->SetAttribute("xmlns:model", "http://playerstage.sourceforge.net/gazebo/xmlschema/#model");
+    root->SetAttribute("xmlns:sensor", "http://playerstage.sourceforge.net/gazebo/xmlschema/#sensor");
+    root->SetAttribute("xmlns:body", "http://playerstage.sourceforge.net/gazebo/xmlschema/#body");
+    root->SetAttribute("xmlns:geom", "http://playerstage.sourceforge.net/gazebo/xmlschema/#geom");
+    root->SetAttribute("xmlns:joint", "http://playerstage.sourceforge.net/gazebo/xmlschema/#joint");
+    root->SetAttribute("xmlns:controller", "http://playerstage.sourceforge.net/gazebo/xmlschema/#controller");
+    root->SetAttribute("xmlns:interface", "http://playerstage.sourceforge.net/gazebo/xmlschema/#interface");
+    doc.LinkEndChild(root);
+    
+    TiXmlElement *xyz      = new TiXmlElement("xyz");
+    TiXmlText    *text_xyz = new TiXmlText("0.0 0.0 0.0");
+    xyz->LinkEndChild(text_xyz);    
+    root->LinkEndChild(xyz);
+    TiXmlElement *rpy = new TiXmlElement("rpy");
+    TiXmlText    *text_rpy = new TiXmlText("0.0 0.0 0.0");
+    rpy->LinkEndChild(text_rpy);    
+    root->LinkEndChild(rpy);    
+
+    std::vector<robot_desc::URDF::Link*> links;
+    wgxml.getLinks(links);
+    
+    for (unsigned int i = 0 ; i < links.size() ; ++i)
+    {
+	TiXmlElement *elem = new TiXmlElement("body:box");
+	root->LinkEndChild(elem);	
+    }    
 }
 
 void usage(const char *progname)
