c68e689ae338f200a1aec22cf453649d616823e6
david_li <david_li@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-22 17:54:54 +0000

    Implemented joint control
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1899 f5854215-dd47-0410-b2c4-cdd35faa7885

--- controllers/pr2Controllers/include/pr2Controllers/ArmController.h
+++ controllers/pr2Controllers/include/pr2Controllers/ArmController.h
@@ -18,14 +18,31 @@
 
 #include <pr2Core/pr2Core.h>
 #include <genericControllers/Controller.h>
+#include <genericControllers/JointController.h>
+#include <mechanism_model/joint.h>
+
+
 
 namespace CONTROLLER
 {
   class ArmController : Controller
   {
-    public:
-    
-      /*!
+    public: 
+    enum ARM_JOINT_ID{
+      ARM_PAN         , 
+      ARM_SHOULDER_PITCH, 
+      ARM_SHOULDER_ROLL,
+      ARM_ELBOW_PITCH , 
+      ARM_ELBOW_ROLL  ,
+      ARM_WRIST_PITCH , 
+      ARM_WRIST_ROLL  ,
+      ARM_MAX_JOINTS
+    };
+
+//---------------------------------------------------------------------------------//
+//CONSTRUCTION/DESTRUCTION CALLS
+//---------------------------------------------------------------------------------//
+      /*! 
         * \brief Constructor.
         *
         * \param 
@@ -36,31 +53,107 @@ namespace CONTROLLER
         * \brief Destructor.
         */       
       ~ArmController( );
+  
+      //TEMPORARY
+        /*! 
+        * \brief Temporary way to initialize limits and gains for a single joint. JointNum is Identified with enum
+        *
+        */
+      void initJoint(int jointNum, double PGain, double IGain, double DGain, double IMax, double IMin, CONTROLLER_CONTROL_MODE mode, double time, double maxPositiveTorque, double maxNegativeTorque, double maxEffort, mechanism::Joint *joint);
 
-      /*!
-        * \brief Update controller
-        */       
-      void Update( );
+     //TEMPORARY
+        /*! 
+        * \brief Call after initializing individual joints to initialize arm as a whole
+        *         
+        */
+      CONTROLLER_ERROR_CODE initArm(CONTROLLER_CONTROL_MODE mode);
 
-      /*!
+
+
+//---------------------------------------------------------------------------------//
+//MODE/ENABLE CALLS
+//---------------------------------------------------------------------------------//
+ 
+    /*!
+        * \brief Switches command mode type (Automatic, Torque, position, velocity control)
+        *  
+        */
+      CONTROLLER_CONTROL_MODE setMode(CONTROLLER_CONTROL_MODE mode);
+
+    /*!
+      * \brief Allow controller to send commands
+      *      
+      */
+      CONTROLLER_CONTROL_MODE enableController(void);
+
+        /*!
+      * \brief Shut down controller.
+      * 
+      *       
+      */
+      CONTROLLER_CONTROL_MODE disableController(void);
+      
+
+        /*!
+      * \brief Return controller mode
+      * 
+      */
+    
+      CONTROLLER_CONTROL_MODE getMode(void);
+
+        /*!
+        * \brief Return true if last command saturated the torque 
+        *
+        *  
+        */
+      void checkForSaturation(bool* status[]);
+
+
+//---------------------------------------------------------------------------------//
+//HAND CALLS
+//---------------------------------------------------------------------------------//
+
+        /*!
         * 
         * \brief Set Cartesian position of Hand (end-effector) in Global Frame (Euler Angles)
         * 
         */       
-      PR2::PR2_ERROR_CODE setHandCartesianPosition   (double  x, double  y, double  z, double  roll, double  pitch, double  yaw);
+      CONTROLLER_ERROR_CODE setHandCartesianPosition   (double  x, double  y, double  z, double  roll, double  pitch, double  yaw);
 
-      PR2::PR2_ERROR_CODE getHandCartesianPositionCmd(double *x, double *y, double *z, double *roll, double *pitch, double *yaw);
-      PR2::PR2_ERROR_CODE getHandCartesianPositionAct(double *x, double *y, double *z, double *roll, double *pitch, double *yaw);
+        /*!
+        * 
+        * \brief Get commanded Cartesian position of Hand (end-effector) in Global Frame (Euler Angles)
+        * 
+        */      
+       CONTROLLER_ERROR_CODE getHandCartesianPositionCmd(double *x, double *y, double *z, double *roll, double *pitch, double *yaw);
 
-      /*!
+        /*!
+        * 
+        * \brief Get Cartesian position of Hand (end-effector) in Global Frame (Euler Angles)
+        * 
+        */      
+       CONTROLLER_ERROR_CODE getHandCartesianPositionAct(double *x, double *y, double *z, double *roll, double *pitch, double *yaw);
+
+        /*!
         * 
         * \brief Set orientation of Hand (end-effector) in Global Frame (Euler Angles)
         * 
         */       
-      PR2::PR2_ERROR_CODE setHandOrientation(double roll, double pitch, double yaw);
+       CONTROLLER_ERROR_CODE setHandOrientation(double roll, double pitch, double yaw);
 
-      PR2::PR2_ERROR_CODE getHandOrientationCmd(double *roll, double *pitch, double *yaw);
-      PR2::PR2_ERROR_CODE getHandOrientationAct(double *roll, double *pitch, double *yaw);
+        /*!
+        * 
+        * \brief Get commanded orientation of Hand (end-effector) in Global Frame (Euler Angles)
+        * 
+        */    
+       CONTROLLER_ERROR_CODE getHandOrientationCmd(double *roll, double *pitch, double *yaw);
+        
+        /*!
+        * 
+        * \brief Get orientation of Hand (end-effector) in Global Frame (Euler Angles)
+        * 
+        */    
+       CONTROLLER_ERROR_CODE getHandOrientationAct(double *roll, double *pitch, double *yaw);
 
       /*!
         * 
@@ -77,51 +170,88 @@ namespace CONTROLLER
         * </UL>
         *
         */       
-      PR2::PR2_ERROR_CODE setHandParam(std::string label, double value);
-      PR2::PR2_ERROR_CODE setHandParam(std::string label, std::string value);
+       CONTROLLER_ERROR_CODE setHandParam(std::string label, double value);
+       CONTROLLER_ERROR_CODE  setHandParam(std::string label, std::string value);
 
-      PR2::PR2_ERROR_CODE getHandParam(std::string label, double *value);
-      PR2::PR2_ERROR_CODE getHandParam(std::string label, std::string *value);
+       CONTROLLER_ERROR_CODE getHandParam(std::string label, double *value);
+       CONTROLLER_ERROR_CODE getHandParam(std::string label, std::string *value);
+
+//---------------------------------------------------------------------------------//
+//ARM JOINT POSITION CALLS
+//---------------------------------------------------------------------------------//
+      /*!
+        *
+        * \brief Set all arm joint positions 
+        *
+        */
+       CONTROLLER_ERROR_CODE setArmJointPosition(int numJoints, double angles[],double speed[]);
+  /*!
+        *
+        * \brief Get all commanded arm joint positions 
+        *
+        */
+
+       CONTROLLER_ERROR_CODE getArmJointPositionCmd(int *numJoints, double *angles[],double *speed[]);
+     /*!
+        *
+        * \brief Get all arm joint positions 
+        *
+        */
+      CONTROLLER_ERROR_CODE getArmJointPositionAct(int *numJoints, double *angles[],double *speed[]);
+
+//---------------------------------------------------------------------------------//
+//ARM JOINT TORQUE CALLS
+//---------------------------------------------------------------------------------//
 
       /*!
         *
-        * \brief Set Arm joint positions individually.
+        * \brief Set all arm joint torques 
         *
         */
-      PR2::PR2_ERROR_CODE setArmJointPosition(int numJoints, double angles[],double speed[]);
+       CONTROLLER_ERROR_CODE setArmJointTorque(int numJoints, double torque[]);
+       /*!
+        *
+        * \brief Get all commanded arm joint torques 
+        *
+        */
 
-      PR2::PR2_ERROR_CODE getArmJointPositionCmd(int *numJoints, double *angles[],double *speed[]);
-      PR2::PR2_ERROR_CODE getArmJointPositionAct(int *numJoints, double *angles[],double *speed[]);
+       CONTROLLER_ERROR_CODE getArmJointTorqueCmd(int *numJoints, double *torque[]);
+       /*!
+        *
+        * \brief Get all arm joint torques 
+        *
+        */
+
+       CONTROLLER_ERROR_CODE getArmJointTorqueAct(int *numJoints, double *torque[]);
+
+//---------------------------------------------------------------------------------//
+//ARM JOINT VELOCITY CALLS
+//---------------------------------------------------------------------------------//
 
       /*!
         *
-        * \brief Set Arm joint torques individually.
+        * \brief Set all arm joint speeds 
         *
         */
-      PR2::PR2_ERROR_CODE setArmJointTorque(int numJoints, double torque[]);
+       CONTROLLER_ERROR_CODE setArmJointSpeed(int numJoints, double speed[]);
+  
+        /*!
+        *
+        * \brief Get all commanded arm joint speeds 
+        *
+        */
 
-      PR2::PR2_ERROR_CODE getArmJointTorqueCmd(int *numJoints, double *torque[]);
-      PR2::PR2_ERROR_CODE getArmJointTorqueAct(int *numJoints, double *torque[]);
-
+       CONTROLLER_ERROR_CODE getArmJointSpeedCmd(int *numJoints, double *speed[]);
       /*!
         *
-        * \brief Set Arm joint speeds individually.
+        * \brief Get all arm joint speeds 
         *
         */
-      PR2::PR2_ERROR_CODE setArmJointSpeed(int numJoints, double speed[]);
+        CONTROLLER_ERROR_CODE getArmJointSpeedAct(int *numJoints, double *speed[]);
 
-      PR2::PR2_ERROR_CODE getArmJointSpeedCmd(int *numJoints, double *speed[]);
-      PR2::PR2_ERROR_CODE getArmJointSpeedAct(int *numJoints, double *speed[]);
-
-      /*!
-        *
-        * \brief Set arm joint maximum torques individually.
-        *
-        */
-      PR2::PR2_ERROR_CODE setArmJointMaxTorque(int numJoints, double maxTorque[]);
-
-      PR2::PR2_ERROR_CODE getArmJointMaxTorqueCmd(int *numJoints, double *maxTorque[]);
-      PR2::PR2_ERROR_CODE getArmJointMaxTorqueAct(int *numJoints, double *maxTorque[]);
+//---------------------------------------------------------------------------------//
+// GAZE POINT CALLS
+//---------------------------------------------------------------------------------//
 
       /*!
         *
@@ -130,14 +260,60 @@ namespace CONTROLLER
         * TODO:  global frame???
         *
         */
-      PR2::PR2_ERROR_CODE setArmCamGazePoint(double x, double y, double z);
+       CONTROLLER_ERROR_CODE setArmCamGazePoint(double x, double y, double z);
 
-      PR2::PR2_ERROR_CODE getArmCamGazePointCmd(double *x, double *y, double *z);
-      PR2::PR2_ERROR_CODE getArmCamGazePointAct(double *x, double *y, double *z);
+      /*!
+        *
+        * \brief Get commanded forearm camera gazepoints.
+        *
+        * TODO:  global frame???
+        *
+        */
+
+       CONTROLLER_ERROR_CODE getArmCamGazePointCmd(double *x, double *y, double *z);
+     
+      /*!
+        *
+        * \brief Get forearm camera gazepoints.
+        *
+        * TODO:  global frame???
+        *
+        */
+       CONTROLLER_ERROR_CODE getArmCamGazePointAct(double *x, double *y, double *z);
+
+//---------------------------------------------------------------------------------//
+// UPDATE CALLS
+//---------------------------------------------------------------------------------//
+
+
+      /*!
+        * \brief Update controller
+        */       
+      void Update( );
+
 
     private:
-      PR2::PR2_CONTROL_MODE controlMode;      /**< Arm controller control mode >*/
+      bool enabled;   /**<Is the arm controller active?>*/
+      CONTROLLER_CONTROL_MODE controlMode;      /**< Arm controller control mode >*/
+
+      std::string name; /**<Namespace identifier for ROS>*/      
+
+      JointController lowerControl[ARM_MAX_JOINTS]; /**< Lower level control done by JointControllers>*/
+
+      double cmdPos[6]; /**<Last commanded cartesian position>*/
+      double cmdVel[6]; /**<Last commanded cartesian velocity>*/
+
+      
   };
 }
 
+
+      /*!
+        *
+        * \brief Set arm joint maximum torques individually.
+        *
+        */
+     // PR2::PR2_ERROR_CODE setArmJointMaxTorque(int numJoints, double maxTorque[]);
+      //PR2::PR2_ERROR_CODE getArmJointMaxTorqueCmd(int *numJoints, double *maxTorque[]);
+      //PR2::PR2_ERROR_CODE getArmJointMaxTorqueAct(int *numJoints, double *maxTorque[]);
 
--- controllers/pr2Controllers/src/ArmController.cpp
+++ controllers/pr2Controllers/src/ArmController.cpp
@@ -1,142 +1,347 @@
 #include <pr2Controllers/ArmController.h>
 
 using namespace CONTROLLER;
+/***************************************************/
+/*! \class CONTROLLER::ArmController
+    \brief An overall arm controller
+    
+    This class implements controller loops for
+    the PR2 Arm
+
+    ASSUMES:
+     7 rotary joints
+
+    NOTES:
+      Need to init all joints separately
+      Modes: Position, Velocity, Torque (same as joint controllers)
+      
+
+    Parameters to be set by SetParam
+    PGain
+    IGain
+    DGain
+    IMax
+    IMin
+
+    Parameters fetched from joint
+    Time
+    SaturationEffort
+    MaxEffort
+
+    Steps to bring an ArmController online
+      1. Initialize all joints
+      2. InitArm
+    
+*/
+/***************************************************/
+
+//---------------------------------------------------------------------------------//
+//CONSTRUCTION/DESTRUCTION CALLS
+//---------------------------------------------------------------------------------//
 
 ArmController::ArmController()
 {
+  //Default construction of JointControllers should init them to blank controllers
 }
     
 ArmController::~ArmController( )
 {
 }
 
-void
-ArmController::Update( )
+void ArmController::initJoint(int jointNum, double PGain, double IGain, double DGain, double IMax, double IMin, CONTROLLER_CONTROL_MODE mode, double time, double maxPositiveTorque, double maxNegativeTorque, double maxEffort, mechanism::Joint *joint)
 {
-
+  lowerControl[jointNum].Init(PGain,  IGain,  DGain,  IMax,  IMin,  CONTROLLER::CONTROLLER_DISABLED,  time,  maxPositiveTorque,  maxNegativeTorque,  maxEffort,  joint); //Initialize joint, but keep in disabled state 
 }
 
-PR2::PR2_ERROR_CODE
+CONTROLLER_ERROR_CODE ArmController::initArm(CONTROLLER_CONTROL_MODE mode)
+{
+
+  //Reset commanded positions
+  for(int i =0 ;i<6;i++){
+    cmdPos[i] = 0.0;
+    cmdVel[i] = 0.0;
+  }
+
+  //Set mode
+  controlMode = mode;
+
+    for(int i =0;i<ARM_MAX_JOINTS;i++){
+      lowerControl[i].SetMode(mode); //Set underlying jointControllers to position control
+    }
+  
+  //Turn on the controller
+  enableController();
+
+  return CONTROLLER_ALL_OK;
+}
+
+//---------------------------------------------------------------------------------//
+//MODE/ENABLE CALLS
+//---------------------------------------------------------------------------------//
+CONTROLLER::CONTROLLER_CONTROL_MODE ArmController::setMode(CONTROLLER_CONTROL_MODE mode)
+{
+  for(int i = 0;i<ARM_MAX_JOINTS;i++){
+    lowerControl[i].SetMode(mode);
+  }
+  return CONTROLLER_MODE_SET;
+}
+
+CONTROLLER::CONTROLLER_CONTROL_MODE ArmController::enableController(void)
+{
+  enabled = true;
+ for(int i = 0;i<ARM_MAX_JOINTS;i++){
+    lowerControl[i].EnableController();
+  }
+  return CONTROLLER_ENABLED;
+}
+
+CONTROLLER::CONTROLLER_CONTROL_MODE  ArmController::disableController(void)
+{ 
+  enabled = false;
+ for(int i = 0;i<ARM_MAX_JOINTS;i++){
+    lowerControl[i].DisableController();
+  }
+  return CONTROLLER_DISABLED;
+
+}
+      
+    
+CONTROLLER::CONTROLLER_CONTROL_MODE  ArmController::getMode(void)
+{
+  return controlMode;
+}
+
+void  ArmController::checkForSaturation(bool* status[])
+{
+  for(int i= 0;i<ARM_MAX_JOINTS;i++){
+    *status[i] = lowerControl[i].CheckForSaturation();
+  }
+}
+
+
+//---------------------------------------------------------------------------------//
+//HAND CALLS
+//Require forward and inverse kinematics
+//---------------------------------------------------------------------------------//
+CONTROLLER_ERROR_CODE
 ArmController::setHandCartesianPosition(double x, double y, double z, double roll, double pitch, double yaw)
 {
 
-  return PR2::PR2_ALL_OK;
+  return CONTROLLER::CONTROLLER_ALL_OK;
 }
-PR2::PR2_ERROR_CODE
-ArmController::getHandCartesianPositionCmd(double *x, double *y, double *z, double *roll, double *pitch, double *yaw)
+CONTROLLER_ERROR_CODE ArmController::getHandCartesianPositionCmd(double *x, double *y, double *z, double *roll, double *pitch, double *yaw)
 {
 
-  return PR2::PR2_ALL_OK;
+  return CONTROLLER::CONTROLLER_ALL_OK;
 }
-PR2::PR2_ERROR_CODE
-ArmController::getHandCartesianPositionAct(double *x, double *y, double *z, double *roll, double *pitch, double *yaw)
+CONTROLLER_ERROR_CODE ArmController::getHandCartesianPositionAct(double *x, double *y, double *z, double *roll, double *pitch, double *yaw)
 {
 
-  return PR2::PR2_ALL_OK;
+  return CONTROLLER::CONTROLLER_ALL_OK;
 }
 
-PR2::PR2_ERROR_CODE
-ArmController::setHandOrientation(double roll, double pitch, double yaw)
+CONTROLLER_ERROR_CODE ArmController::setHandOrientation(double roll, double pitch, double yaw)
 {
 
-  return PR2::PR2_ALL_OK;
+  return CONTROLLER::CONTROLLER_ALL_OK;
 }
-PR2::PR2_ERROR_CODE
-ArmController::getHandOrientationCmd(double *roll, double *pitch, double *yaw)
+CONTROLLER_ERROR_CODE ArmController::getHandOrientationCmd(double *roll, double *pitch, double *yaw)
 {
 
-  return PR2::PR2_ALL_OK;
+  return CONTROLLER::CONTROLLER_ALL_OK;
 }
-PR2::PR2_ERROR_CODE
-ArmController::getHandOrientationAct(double *roll, double *pitch, double *yaw)
+CONTROLLER_ERROR_CODE ArmController::getHandOrientationAct(double *roll, double *pitch, double *yaw)
 {
 
-  return PR2::PR2_ALL_OK;
+  return CONTROLLER::CONTROLLER_ALL_OK;
 }
 
-PR2::PR2_ERROR_CODE
-ArmController::setHandParam(std::string label, double value)
+CONTROLLER_ERROR_CODE ArmController::setHandParam(std::string label, double value)
 {
 
-  return PR2::PR2_ALL_OK;
+  return CONTROLLER::CONTROLLER_ALL_OK;
 }
-PR2::PR2_ERROR_CODE
-ArmController::getHandParam(std::string label, double *value)
+CONTROLLER_ERROR_CODE ArmController::getHandParam(std::string label, double *value)
 {
 
-  return PR2::PR2_ALL_OK;
+  return CONTROLLER::CONTROLLER_ALL_OK;
 }
 
-PR2::PR2_ERROR_CODE
-ArmController::setHandParam(std::string label, std::string value)
+CONTROLLER_ERROR_CODE ArmController::setHandParam(std::string label, std::string value)
 {
 
-  return PR2::PR2_ALL_OK;
+  return CONTROLLER::CONTROLLER_ALL_OK;
 }
-PR2::PR2_ERROR_CODE
-ArmController::getHandParam(std::string label, std::string *value)
+CONTROLLER_ERROR_CODE ArmController::getHandParam(std::string label, std::string *value)
 {
 
-  return PR2::PR2_ALL_OK;
+  return CONTROLLER::CONTROLLER_ALL_OK;
 }
 
-PR2::PR2_ERROR_CODE
+//---------------------------------------------------------------------------------//
+//ARM JOINT POSITION CALLS
+//---------------------------------------------------------------------------------//
+CONTROLLER_ERROR_CODE
 ArmController::setArmJointPosition(int numJoints, double angles[],double speed[])
 {
-
-  return PR2::PR2_ALL_OK;
+  CONTROLLER_ERROR_CODE error = CONTROLLER_ALL_OK;
+  CONTROLLER_ERROR_CODE current;
+  if(controlMode!=CONTROLLER::CONTROLLER_POSITION) return CONTROLLER_MODE_ERROR; //TODO implement errors?
+  for (int i = 0;i<numJoints;i++){
+    current = lowerControl[i].SetPosCmd(angles[i]);
+    std::cout<<i<<":"<<current<<std::endl;
+    if(current!=CONTROLLER_ALL_OK) error = current;
+  }  
+  return error;
 }
-PR2::PR2_ERROR_CODE
-ArmController::getArmJointPositionCmd(int *numJoints, double *angles[],double *speed[])
+CONTROLLER_ERROR_CODE ArmController::getArmJointPositionCmd(int *numJoints, double *angles[],double *speed[])
 {
+  CONTROLLER_ERROR_CODE error = CONTROLLER_ALL_OK;
+  CONTROLLER_ERROR_CODE current;
 
-  return PR2::PR2_ALL_OK;
+   for (int i = 0;i<*numJoints;i++){
+    current = lowerControl[i].GetPosCmd(angles[i]); //If we find even a single error,return error for overall
+    if(current!=CONTROLLER_ALL_OK) error = current;
+  }  
+  return error;
 }
-PR2::PR2_ERROR_CODE
+CONTROLLER_ERROR_CODE
 ArmController::getArmJointPositionAct(int *numJoints, double *angles[],double *speed[])
 {
+ CONTROLLER_ERROR_CODE error = CONTROLLER_ALL_OK;
+ CONTROLLER_ERROR_CODE current;
 
-  return PR2::PR2_ALL_OK;
+  for (int i = 0;i<*numJoints;i++){
+    current = lowerControl[i].GetPosAct(angles[i]); //If we find even a single error,return error for overall
+    if(current!=CONTROLLER_ALL_OK) error = current;
+
+  }  
+  return error;
 }
 
-PR2::PR2_ERROR_CODE
+//---------------------------------------------------------------------------------//
+//ARM JOINT TORQUE CALLS
+//---------------------------------------------------------------------------------//
+CONTROLLER_ERROR_CODE
 ArmController::setArmJointTorque(int numJoints, double torque[])
 {
+  CONTROLLER_ERROR_CODE error = CONTROLLER_ALL_OK;
+  CONTROLLER_ERROR_CODE current;
+  if(controlMode!=CONTROLLER::CONTROLLER_TORQUE) return CONTROLLER_MODE_ERROR; //TODO implement errors?
+  for (int i = 0;i<numJoints;i++){
+    current = lowerControl[i].SetTorqueCmd(torque[i]); //If we find even a single error, complete the set position and return error for overall
+     if(current!=CONTROLLER_ALL_OK) error = current;
 
-  return PR2::PR2_ALL_OK;
+  }  
+  return error;
+
 }
-PR2::PR2_ERROR_CODE
-ArmController::getArmJointTorqueCmd(int *numJoints, double *torque[])
+CONTROLLER_ERROR_CODE ArmController::getArmJointTorqueCmd(int *numJoints, double *torque[])
 {
+  CONTROLLER_ERROR_CODE error = CONTROLLER_ALL_OK;
+  CONTROLLER_ERROR_CODE current;
 
-  return PR2::PR2_ALL_OK;
+   for (int i = 0;i<*numJoints;i++){
+    current = lowerControl[i].GetTorqueCmd(torque[i]); //If we find even a single error,return error for overall
+   if(current!=CONTROLLER_ALL_OK) error = current;
+
+  }  
+  return error;
+
 }
-PR2::PR2_ERROR_CODE
-ArmController::getArmJointTorqueAct(int *numJoints, double *torque[])
+CONTROLLER_ERROR_CODE ArmController::getArmJointTorqueAct(int *numJoints, double *torque[])
 {
+ CONTROLLER_ERROR_CODE error = CONTROLLER_ALL_OK;
+ CONTROLLER_ERROR_CODE current;
 
-  return PR2::PR2_ALL_OK;
+  for (int i = 0;i<*numJoints;i++){
+    current = lowerControl[i].GetTorqueAct(torque[i]); //If we find even a single error,return error for overall 
+   if(current!=CONTROLLER_ALL_OK) error = current;
+  }  
+  return error;
+
 }
 
-PR2::PR2_ERROR_CODE
+//---------------------------------------------------------------------------------//
+//ARM JOINT VELOCITY CALLS
+//---------------------------------------------------------------------------------//
+CONTROLLER_ERROR_CODE
 ArmController::setArmJointSpeed(int numJoints, double speed[])
 {
+   CONTROLLER_ERROR_CODE error = CONTROLLER_ALL_OK;
+  CONTROLLER_ERROR_CODE current;
+  if(controlMode!=CONTROLLER::CONTROLLER_VELOCITY) return CONTROLLER_MODE_ERROR; //TODO implement errors?
+  for (int i = 0;i<numJoints;i++){
+    current = lowerControl[i].SetVelCmd(speed[i]); //If we find even a single error, complete the set position and return error for overall
+    if(current!=CONTROLLER_ALL_OK) error = current;
+  }  
+  return error;
 
-  return PR2::PR2_ALL_OK;
 }
-PR2::PR2_ERROR_CODE
-ArmController::getArmJointSpeedCmd(int *numJoints, double *speed[])
+CONTROLLER_ERROR_CODE ArmController::getArmJointSpeedCmd(int *numJoints, double *speed[])
+{ 
+  CONTROLLER_ERROR_CODE error = CONTROLLER_ALL_OK;
+  CONTROLLER_ERROR_CODE current;
+
+   for (int i = 0;i<*numJoints;i++){
+    current = lowerControl[i].GetVelCmd(speed[i]); //If we find even a single error,return error for overall
+    if(current!=CONTROLLER_ALL_OK) error = current;
+  }  
+  return error;
+
+ }
+CONTROLLER_ERROR_CODE ArmController::getArmJointSpeedAct(int *numJoints, double *speed[])
+{ 
+  CONTROLLER_ERROR_CODE error = CONTROLLER_ALL_OK;
+  CONTROLLER_ERROR_CODE current;
+
+  for (int i = 0;i<*numJoints;i++){
+    current = lowerControl[i].GetVelAct(speed[i]); //If we find even a single error,return error for overall
+    if(current!=CONTROLLER_ALL_OK) error = current;
+  }  
+  return error;
+
+}
+
+//---------------------------------------------------------------------------------//
+// GAZE POINT CALLS
+//---------------------------------------------------------------------------------//
+CONTROLLER_ERROR_CODE
+ArmController::setArmCamGazePoint(double x, double y, double z)
 {
 
-  return PR2::PR2_ALL_OK;
+  return CONTROLLER::CONTROLLER_ALL_OK;
 }
-PR2::PR2_ERROR_CODE
-ArmController::getArmJointSpeedAct(int *numJoints, double *speed[])
+CONTROLLER_ERROR_CODE ArmController::getArmCamGazePointCmd(double *x, double *y, double *z)
 {
 
-  return PR2::PR2_ALL_OK;
+  return CONTROLLER::CONTROLLER_ALL_OK;
+}
+CONTROLLER_ERROR_CODE ArmController::getArmCamGazePointAct(double *x, double *y, double *z)
+{
+
+  return CONTROLLER::CONTROLLER_ALL_OK;
 }
 
+//---------------------------------------------------------------------------------//
+// UPDATE CALLS
+//---------------------------------------------------------------------------------//
+
+void
+ArmController::Update( )
+{
+  if(!enabled){
+    for(int i = 0;i<ARM_MAX_JOINTS;i++){    
+      lowerControl[i].DisableController();
+    }
+  } //Make sure controller is enabled. Otherwise, tell all jointcontrollers to shut down
+
+  for(int i = 0;i<ARM_MAX_JOINTS;i++){   
+    lowerControl[i].Update();
+  }
+}
+
+/*
 PR2::PR2_ERROR_CODE
 ArmController::setArmJointMaxTorque(int numJoints, double maxTorque[])
 {
@@ -155,23 +360,5 @@ ArmController::getArmJointMaxTorqueAct(int *numJoints, double *maxTorque[])
 
   return PR2::PR2_ALL_OK;
 }
-
-PR2::PR2_ERROR_CODE
-ArmController::setArmCamGazePoint(double x, double y, double z)
-{
-
-  return PR2::PR2_ALL_OK;
-}
-PR2::PR2_ERROR_CODE
-ArmController::getArmCamGazePointCmd(double *x, double *y, double *z)
-{
-
-  return PR2::PR2_ALL_OK;
-}
-PR2::PR2_ERROR_CODE
-ArmController::getArmCamGazePointAct(double *x, double *y, double *z)
-{
-
-  return PR2::PR2_ALL_OK;
-}
+*/
 
