261abd73a614a9aa194395f9d047cd2da9beb93f
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-22 18:32:46 +0000

    protecting service calls as per morgans statements in the meeting today. and adding caching of lookups which helps too
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1900 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/transforms/namelookup/include/namelookup/nameLookupClient.hh
+++ util/transforms/namelookup/include/namelookup/nameLookupClient.hh
@@ -4,29 +4,65 @@
 #include "ros/node.h"
 #include "namelookup/NameToNumber.h"
 #include <iostream>
+#include <map>
 
 class nameLookupClient 
 {
 public:
   nameLookupClient(ros::node &aNode): myNode(aNode)
   {
+    //printf("Constructed nameLoookupClient\n");
+    pthread_mutex_init(&protect_call, NULL);
   };
 
   int lookup(std::string str_in)
   {
+    static std::map<std::string, int> nameMap;  //This is here not a member because of initialization order issues.  
+    //    printf("Start lookup\n");
+    //    return lookupOnServer(str_in); // Using this instead exposes race condition in wavefron_ player
+    std::map<std::string, int>::iterator it;
+    it = nameMap.find(str_in);
+    if ( it == nameMap.end())
+      {
+        int value = lookupOnServer(str_in);
+        nameMap[str_in] = value;
+        //  printf("NOT FOUND\n");
+        return value;
+      }
+    else
+      {
+        //printf("FOUDN\n");
+        return      (*it).second;
+      }
+  }  
+      
+  
+  
+  
+private:
+  ros::node &myNode;
+  
+  pthread_mutex_t protect_call;
+
+  int lookupOnServer(std::string str_in)
+  {
     namelookup::NameToNumber::request req;
     namelookup::NameToNumber::response res;
     req.name = myNode.map_name(str_in);
+    //printf("About to lock\n");
+    pthread_mutex_lock(&protect_call);   
     if (ros::service::call("/nameToNumber", req, res))
-      return res.number;
+      {
+        pthread_mutex_unlock(&protect_call);   
+        return res.number;
+      }
     else 
-      return 0;
+      {
+        pthread_mutex_unlock(&protect_call);   
+        return 0;
+      }
   }
-
-private:
-  ros::node &myNode;
-
-
+  
 };
 
 
