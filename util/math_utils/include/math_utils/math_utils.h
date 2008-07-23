d41fbbed812df51f9a945732719a168fd4664e08
sachinchitta <sachinchitta@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-23 01:22:55 +0000

    Math utility functions
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1956 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ util/math_utils/include/math_utils/math_utils.h
@@ -0,0 +1,8 @@
+#ifndef MATH_UTIL_H
+#define MATH_UTIL_H
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define CLAMP(a, b, c) MIN(MAX((b), (a)), (c))
+
+#endif
