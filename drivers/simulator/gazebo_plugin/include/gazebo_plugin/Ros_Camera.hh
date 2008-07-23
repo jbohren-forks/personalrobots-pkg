643870ee21a8fbc9ee17fe706452951a1debef1a
hsujohnhsu <hsujohnhsu@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-23 08:39:47 +0000

    ros camera plugins for gazebo.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@1971 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ drivers/simulator/gazebo_plugin/include/gazebo_plugin/Ros_Camera.hh
@@ -0,0 +1,121 @@
+/*
+ *  Gazebo - Outdoor Multi-Robot Simulator
+ *  Copyright (C) 2003  
+ *     Nate Koenig & Andrew Howard
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+/*
+ * Desc: Actuator array controller for a Pr2 robot.
+ * Author: Nathan Koenig
+ * Date: 19 Sept 2007
+ * SVN: $Id$
+ */
+#ifndef ROS_CAMERA_HH
+#define ROS_CAMERA_HH
+
+#include <gazebo/Controller.hh>
+
+#include <ros/node.h>
+#include <std_msgs/Image.h>
+
+namespace gazebo
+{
+  class CameraIface;
+  class MonoCameraSensor;
+
+/// @addtogroup gazebo_controller
+/// @{
+/** \defgroup genericcamera generic camera
+
+  \brief Ros camera controller.
+  
+  This is a controller that collects data from a Camera Sensor and populates a libgazebo camera interfaace. This controller should only be used as a child of a camera sensor 
+
+  \verbatim
+  <model:physical name="camera_model">
+    <body:empty name="camera_body">
+      <sensor:camera name="camera_sensor">
+
+        <controller:generic_camera name="controller-name">
+          <interface:camera name="iface-name"/>
+        </controller:generic_camera>
+
+      </sensor:camera>
+    </body:empty>
+  </model:phyiscal>
+  \endverbatim
+ 
+\{
+*/
+
+/// \brief Ros camera controller.
+/// 
+/// This is a controller that simulates a generic camera
+class Ros_Camera : public Controller
+{
+  /// \brief Constructor
+  /// \param parent The parent entity, must be a Model or a Sensor
+  public: Ros_Camera(Entity *parent);
+
+  /// \brief Destructor
+  public: virtual ~Ros_Camera();
+
+  /// \brief Load the controller
+  /// \param node XML config node
+  /// \return 0 on success
+  protected: virtual void LoadChild(XMLConfigNode *node);
+
+  /// \brief Init the controller
+  /// \return 0 on success
+  protected: virtual void InitChild();
+
+  /// \brief Update the controller
+  /// \return 0 on success
+  protected: virtual void UpdateChild();
+
+  /// \brief Finalize the controller
+  /// \return 0 on success
+  protected: virtual void FiniChild();
+
+  /// \brief Put camera data to the iface
+  private: void PutCameraData();
+
+  /// The camera interface
+  private: CameraIface *cameraIface;
+
+  /// The parent sensor
+  private: MonoCameraSensor *myParent;
+
+  // pointer to ros node
+  private: ros::node *rosnode;
+
+  // ros message
+  private: std_msgs::Image image;
+
+  // topic name
+  private: std::string topicName;
+
+
+
+};
+
+/** /} */
+/// @}
+
+}
+#endif
+
---
+++ drivers/simulator/gazebo_plugin/src/Ros_Camera.cc
@@ -0,0 +1,191 @@
+/*
+ *  Gazebo - Outdoor Multi-Robot Simulator
+ *  Copyright (C) 2003
+ *     Nate Koenig & Andrew Howard
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+/*
+ * Desc: Actuator array controller for a Pr2 robot.
+ * Author: Sachin Chitta and John Hsu
+ * Date: 1 June 2008
+ * SVN info: $Id$
+ */
+
+
+#include <algorithm>
+#include <assert.h>
+
+#include <gazebo/Sensor.hh>
+#include <gazebo/Global.hh>
+#include <gazebo/XMLConfig.hh>
+#include <gazebo/Simulator.hh>
+#include <gazebo/gazebo.h>
+#include <gazebo/GazeboError.hh>
+#include <gazebo/ControllerFactory.hh>
+//#include "/u/johnhsu/projects/pr2/3rdparty/gazebo/gazebo-svn/server/sensors/camera/MonoCameraSensor.hh"
+#include "MonoCameraSensor.hh"
+#include <gazebo_plugin/Ros_Camera.hh>
+
+using namespace gazebo;
+
+GZ_REGISTER_DYNAMIC_CONTROLLER("ros_camera", Ros_Camera);
+
+////////////////////////////////////////////////////////////////////////////////
+// Constructor
+Ros_Camera::Ros_Camera(Entity *parent)
+    : Controller(parent)
+{
+  this->myParent = dynamic_cast<MonoCameraSensor*>(this->parent);
+
+  if (!this->myParent)
+    gzthrow("Ros_Camera controller requires a Camera Sensor as its parent");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Destructor
+Ros_Camera::~Ros_Camera()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Load the controller
+void Ros_Camera::LoadChild(XMLConfigNode *node)
+{
+  this->cameraIface = dynamic_cast<CameraIface*>(this->ifaces[0]);
+
+  if (!this->cameraIface)
+    gzthrow("Ros_Camera controller requires a CameraIface");
+
+  int argc;
+  char** argv = NULL;
+  // this only works for a single camera.
+  ros::init(argc,argv);
+
+
+  this->topicName = node->GetString("topicName","default_ros_camera",0); //read from xml file
+
+  rosnode = new ros::node(this->topicName);
+  rosnode->advertise<std_msgs::Image>(this->topicName);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Initialize the controller
+void Ros_Camera::InitChild()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Update the controller
+void Ros_Camera::UpdateChild()
+{
+
+  // do this first so there's chance for sensor to run 1 frame after activate
+  if (this->myParent->IsActive())
+    this->PutCameraData();
+
+  // activate if iface open
+  if (this->cameraIface->Lock(1))
+  {
+    if (this->cameraIface->GetOpenCount() > 0)
+      this->myParent->SetActive(true);
+    else
+      this->myParent->SetActive(false);
+
+    //std::cout << " camera open count " << this->cameraIface->GetOpenCount() << std::endl;
+    this->cameraIface->Unlock();
+  }
+  //std::cout << " camera     active " << this->myParent->IsActive() << std::endl;
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Finalize the controller
+void Ros_Camera::FiniChild()
+{
+  // TODO: will be replaced by global ros node eventually
+  delete rosnode;
+  ros::fini();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Put laser data to the interface
+void Ros_Camera::PutCameraData()
+{
+  CameraData *data = this->cameraIface->data;
+  const unsigned char *src;
+  unsigned char *dst;
+  Pose3d cameraPose;
+
+  this->cameraIface->Lock(1);
+
+  // Data timestamp
+  data->head.time = Simulator::Instance()->GetSimTime();
+
+  data->width = this->myParent->GetImageWidth();
+  data->height = this->myParent->GetImageHeight();
+  data->image_size = data->width * data->height * 3;
+
+  // GetFOV() returns radians
+  data->hfov = this->myParent->GetHFOV();
+  data->vfov = this->myParent->GetVFOV();
+
+  // Set the pose of the camera
+  cameraPose = this->myParent->GetWorldPose();
+  data->camera_pose.pos.x = cameraPose.pos.x;
+  data->camera_pose.pos.y = cameraPose.pos.y;
+  data->camera_pose.pos.z = cameraPose.pos.z;
+  data->camera_pose.roll = cameraPose.rot.GetRoll();
+  data->camera_pose.pitch = cameraPose.rot.GetPitch();
+  data->camera_pose.yaw = cameraPose.rot.GetYaw();
+
+  // Make sure there is room to store the image
+  assert (data->image_size <= sizeof(data->image));
+
+  // Copy the pixel data to the interface
+  src = this->myParent->GetImageData(0);
+  dst = data->image;
+
+  // TODO: can skip copy to Iface if Iface is not used
+  memcpy(dst, src, data->image_size);
+
+  this->cameraIface->Unlock();
+
+  // New data is available
+  this->cameraIface->Post();
+
+
+  // copy data into image
+  int    width            = this->myParent->GetImageWidth();
+  int    height           = this->myParent->GetImageHeight();
+  int    depth            = 3;
+
+  this->image.width       = width;
+  this->image.height      = height;
+  this->image.compression = "jpeg";
+  this->image.colorspace  = "bgr";
+
+  // on first pass, the sensor does not update after cameraIface is opened.
+  uint32_t       buf_size = (width) * (height) * (depth);
+
+  this->image.set_data_size(buf_size);
+  this->image.data        = (unsigned char*)src;
+
+  // publish to ros
+  rosnode->publish(this->topicName,this->image);
+
+}
+
