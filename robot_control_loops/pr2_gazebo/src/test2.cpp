993872ef07d1a14e39687f898c7453739c6db2d5
hsujohnhsu <hsujohnhsu@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-24 20:45:39 +0000

    changes corresponding to gazebo mechanism control.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@2075 f5854215-dd47-0410-b2c4-cdd35faa7885

--- robot_control_loops/pr2_gazebo/src/test2.cpp
+++ robot_control_loops/pr2_gazebo/src/test2.cpp
@@ -21,34 +21,25 @@
 #include <stdlib.h>
 #include <math.h>
 
-#include <pthread.h>
-
 // robot model
 #include <pr2Core/pr2Core.h>
-#include <mechanism_model/joint.h>
 
 // gazebo hardware, sensors
 #include <gazebo_hardware/gazebo_hardware.h>
 #include <gazebo_sensors/gazebo_sensors.h>
 
 // controller objects
-#include <pr2Controllers/ArmController.h>
-#include <pr2Controllers/HeadController.h>
-#include <pr2Controllers/SpineController.h>
-#include <pr2Controllers/BaseController.h>
-#include <pr2Controllers/LaserScannerController.h>
-#include <pr2Controllers/GripperController.h>
+#include <mechanism_control/gazebo_control.h>
 
 // ros node for controllers
 #include <rosControllers/RosJointController.h>
 
 // roscpp
-//#include <ros/node.h>
+#include <ros/node.h>
 
 #include <time.h>
 #include <signal.h>
 
-pthread_mutex_t simMutex[PR2::MAX_JOINTS]; //Mutex for sim R/W
 
 void finalize(int)
 {
@@ -57,7 +48,10 @@ void finalize(int)
   exit(-1);
 }
 
-
+#define TEST_ROS_CONTROLLER 0
+#if TEST_ROS_CONTROLLER
+#include <pthread.h>
+pthread_mutex_t simMutex[PR2::MAX_JOINTS]; //Mutex for sim R/W
 void *nonRealtimeLoop(void *rjc)
 {
   std::cout << "Started nonRT loop" << std::endl;
@@ -73,20 +67,28 @@ void *nonRealtimeLoop(void *rjc)
   }
 
 }
+#endif
 
 int 
 main(int argc, char** argv)
 {
 
+  // ros node stuff
+  ros::node *rosnode;
+  ros::init(argc,argv);
+  rosnode = new ros::node("gazebo_mechanism_node");
+
   double time;
   double torque;
 
+#if TEST_ROS_CONTROLLER
   // we need 2 threads, one for RT and one for nonRT
   pthread_t threads[2];
 
   for (int i = 0;i<PR2::MAX_JOINTS;i++){
     pthread_mutex_init(&(simMutex[i]), NULL);
   }
+#endif
 
   ros::init(argc,argv);
 
@@ -102,21 +104,21 @@ main(int argc, char** argv)
   /*   hardware info, eventually retrieve from param server (from xml files)             */
   /*                                                                                     */
   /***************************************************************************************/
-  int iB, iP;
-
   int numBoards    = 4;                 // 0 for head and 1 for arms, see pr2.model:Pr2_Actarray for now
   int numActuators = PR2::MAX_JOINTS-2; // total number of actuators for both boards, last 2 in pr2Core (BASE_6DOF and PR2_WORLD don't count)
 
-  int boardLookUp[] ={1,1,1,1,1,1,1,1,1,1,1,1, // casters
-                      1,                       // spine elevator
-                      1,1,1,1,1,1,1,           // left arm
-                      1,1,1,1,1,1,1,           // right arm
-                      2,                       // left gripper
-                      3,                       // right gripper
-                      0,0,0,0,0,0,0            // head y/p, laser p, left ptz p/t, right ptz p/t
-                      };  // 0 for head, 1 for caster+arm
+  int boardLookUp[] ={0,0,0,                   // head pitch, yaw, hokuyo pitch
+                      0,0,0,0,0,0,0,0,0,0,0,0, // casters
+                      0,                       // spine elevator
+                      0,0,0,0,0,0,0,           // left arm
+                      0,0,0,0,0,0,0,           // right arm
+                      1,                       // left gripper
+                      2,                       // right gripper
+                      3,3,4,4                  // left ptz p/t, right ptz p/t
+                      };  // 0=actarray, 1=left gripper, 2=right gripper, 3=left ptz, 4=right ptz
   //int numJointsLookUp[]   ={7, 28};  // joints per actuator array
   int portLookUp[] = {    // the way joints are listed in pr2.model
+      PR2::HEAD_YAW            , PR2::HEAD_PITCH          , PR2::HEAD_LASER_PITCH    ,
       PR2::CASTER_FL_STEER     , PR2::CASTER_FL_DRIVE_L   , PR2::CASTER_FL_DRIVE_R   ,
       PR2::CASTER_FR_STEER     , PR2::CASTER_FR_DRIVE_L   , PR2::CASTER_FR_DRIVE_R   ,
       PR2::CASTER_RL_STEER     , PR2::CASTER_RL_DRIVE_L   , PR2::CASTER_RL_DRIVE_R   ,
@@ -128,18 +130,16 @@ main(int argc, char** argv)
       PR2::ARM_R_PAN           , PR2::ARM_R_SHOULDER_PITCH, PR2::ARM_R_SHOULDER_ROLL ,
       PR2::ARM_R_ELBOW_PITCH   , PR2::ARM_R_ELBOW_ROLL    , PR2::ARM_R_WRIST_PITCH   ,
       PR2::ARM_R_WRIST_ROLL    ,
-      PR2::ARM_L_GRIPPER      - PR2::ARM_L_GRIPPER ,
-      PR2::ARM_R_GRIPPER      - PR2::ARM_R_GRIPPER ,
-      PR2::HEAD_YAW           - PR2::HEAD_YAW ,
-      PR2::HEAD_PITCH         - PR2::HEAD_YAW ,
-      PR2::HEAD_LASER_PITCH   - PR2::HEAD_YAW ,
-      PR2::HEAD_PTZ_L_PAN     - PR2::HEAD_YAW ,
-      PR2::HEAD_PTZ_L_TILT    - PR2::HEAD_YAW ,
-      PR2::HEAD_PTZ_R_PAN     - PR2::HEAD_YAW ,
-      PR2::HEAD_PTZ_R_TILT    - PR2::HEAD_YAW };
+      PR2::ARM_L_GRIPPER      - PR2::ARM_L_GRIPPER        ,
+      PR2::ARM_R_GRIPPER      - PR2::ARM_R_GRIPPER        ,
+      PR2::HEAD_PTZ_L_PAN     - PR2::HEAD_PTZ_L_PAN       ,
+      PR2::HEAD_PTZ_L_TILT    - PR2::HEAD_PTZ_L_PAN       ,
+      PR2::HEAD_PTZ_R_PAN     - PR2::HEAD_PTZ_L_PAN       ,
+      PR2::HEAD_PTZ_R_TILT    - PR2::HEAD_PTZ_L_PAN       };
 
   
   int jointId[]    = {    // numbering system
+      PR2::HEAD_YAW            , PR2::HEAD_PITCH          , PR2::HEAD_LASER_PITCH    ,
       PR2::CASTER_FL_STEER     , PR2::CASTER_FL_DRIVE_L   , PR2::CASTER_FL_DRIVE_R   ,
       PR2::CASTER_FR_STEER     , PR2::CASTER_FR_DRIVE_L   , PR2::CASTER_FR_DRIVE_R   ,
       PR2::CASTER_RL_STEER     , PR2::CASTER_RL_DRIVE_L   , PR2::CASTER_RL_DRIVE_R   ,
@@ -153,9 +153,6 @@ main(int argc, char** argv)
       PR2::ARM_R_WRIST_ROLL    ,
       PR2::ARM_L_GRIPPER       ,
       PR2::ARM_R_GRIPPER       ,
-      PR2::HEAD_YAW            ,
-      PR2::HEAD_PITCH          ,
-      PR2::HEAD_LASER_PITCH    ,
       PR2::HEAD_PTZ_L_PAN      ,
       PR2::HEAD_PTZ_L_TILT     ,
       PR2::HEAD_PTZ_R_PAN      ,
@@ -177,18 +174,6 @@ main(int argc, char** argv)
                      };
   /***************************************************************************************/
   /*                                                                                     */
-  /*                        initialize robot model                                       */
-  /*                                                                                     */
-  /*    in Robot                                                                         */
-  /*                                                                                     */
-  /***************************************************************************************/
-  mechanism::Joint* joint[PR2::MAX_JOINTS]; //Joint pointer array
-  for (int i = 0;i<PR2::MAX_JOINTS;i++){
-    joint[i] = new mechanism::Joint();
-  }
-
-  /***************************************************************************************/
-  /*                                                                                     */
   /*                        initialize hardware                                          */
   /*                                                                                     */
   /*   User initialized                                                                  */
@@ -197,80 +182,23 @@ main(int argc, char** argv)
   /*  single gazebo hardware object                                                      */
   /*                                                                                     */
   /***************************************************************************************/
-  // a single HardwareInterface objects with numActuators actuators
-  HardwareInterface *hi = new HardwareInterface(numActuators);
-  // hi is passed to h during construction of h,
   // mapping between actuators in h and actuators in hi defined by boardLookUp, portLookUp and jointId
-  GazeboHardware    *h  = new GazeboHardware(numBoards,numActuators, boardLookUp, portLookUp, jointId, etherIP, hostIP);
-  // connect to hardware (gazebo in this case)
-  h->init();
+  GazeboHardware         hardware(numBoards,numActuators, boardLookUp, portLookUp, jointId, etherIP, hostIP);
+  // MechanismControl
+  GazeboMechanismControl mechanismControl;
+  mechanismControl.init(hardware.hardwareInterface); // pass hardware interface to mechanism control
+  mechanismControl.baseController->setVelocity(0,0,0);
 
   // access to all the gazebo sensors
-  GazeboSensors    *s  = new GazeboSensors();
+  GazeboSensors         gazeboSensor;
 
-  /***************************************************************************************/
-  /*                                                                                     */
-  /*                            initialize controllers                                   */
-  /*                                                                                     */
-  /*         TODO: move to mechanism_control                                             */
-  /*                                                                                     */
-  /***************************************************************************************/
-  // stubs
-  controller::ArmController          myArm;
-  controller::HeadController         myHead;
-  controller::SpineController        mySpine;
-  controller::BaseController         myBase;
-  controller::LaserScannerController myLaserScanner;
-  controller::GripperController      myGripper;
 
-  // used here in this test
-  controller::JointController*       jc[PR2::MAX_JOINTS];  // One controller per joint
-
-  // initialize each jointController jc[i], associate with a joint joint[i]
-  for (int i = 0;i<PR2::MAX_JOINTS;i++){
-    jc[i]  = new controller::JointController();
-    jc[i]->init(1000,0,0,500,-500, controller::CONTROLLER_POSITION,time,-1000,1000,joint[i]);
-  }
-
-  //Explicitly initialize the controllers we wish to use. Don't forget to set the controllers to torque mode in the world file! 
-  jc[PR2::ARM_L_PAN]->init(100,0,0,500,-500, controller::CONTROLLER_POSITION,time,-100,100,joint[PR2::ARM_L_PAN]);
-  jc[PR2::ARM_L_SHOULDER_PITCH]->init(1000,0,0,500,-500, controller::CONTROLLER_POSITION,time,-1000,1000,joint[PR2::ARM_L_SHOULDER_PITCH]);
-  jc[PR2::ARM_L_SHOULDER_ROLL]->init(100,0,0,500,-500, controller::CONTROLLER_POSITION,time,-100,100,joint[PR2::ARM_L_SHOULDER_ROLL]);
-  jc[PR2::ARM_L_ELBOW_PITCH]->init(300,0,0,500,-500, controller::CONTROLLER_POSITION,time,-100,100,joint[PR2::ARM_L_ELBOW_PITCH]);
-  jc[PR2::ARM_L_ELBOW_ROLL]->init(100,0,0,500,-500, controller::CONTROLLER_POSITION,time,-100,100,joint[PR2::ARM_L_ELBOW_ROLL]);
-  jc[PR2::ARM_L_WRIST_PITCH]->init(100,0,0,500,-500, controller::CONTROLLER_POSITION,time,-100,100,joint[PR2::ARM_L_WRIST_PITCH]);
-  jc[PR2::ARM_L_WRIST_ROLL]->init(100,0,0,500,-500, controller::CONTROLLER_POSITION,time,-100,100,joint[PR2::ARM_L_WRIST_ROLL]);
-
-  jc[PR2::ARM_R_PAN]->init(100,0,0,500,-500, controller::CONTROLLER_POSITION,time,-100,100,joint[PR2::ARM_R_PAN]);
-  jc[PR2::ARM_R_SHOULDER_PITCH]->init(1000,0,0,500,-500, controller::CONTROLLER_POSITION,time,-1000,1000,joint[PR2::ARM_R_SHOULDER_PITCH]);
-  jc[PR2::ARM_R_SHOULDER_ROLL]->init(100,0,0,500,-500, controller::CONTROLLER_POSITION,time,-100,100,joint[PR2::ARM_R_SHOULDER_ROLL]);
-  jc[PR2::ARM_R_ELBOW_PITCH]->init(300,0,0,500,-500, controller::CONTROLLER_POSITION,time,-100,100,joint[PR2::ARM_R_ELBOW_PITCH]);
-  jc[PR2::ARM_R_ELBOW_ROLL]->init(100,0,0,500,-500, controller::CONTROLLER_POSITION,time,-100,100,joint[PR2::ARM_R_ELBOW_ROLL]);
-  jc[PR2::ARM_R_WRIST_PITCH]->init(100,0,0,500,-500, controller::CONTROLLER_POSITION,time,-100,100,joint[PR2::ARM_R_WRIST_PITCH]);
-  jc[PR2::ARM_R_WRIST_ROLL]->init(100,0,0,500,-500, controller::CONTROLLER_POSITION,time,-100,100,joint[PR2::ARM_R_WRIST_ROLL]);
-
-  //JointController::init(double PGain, double IGain, double DGain, double IMax,
-  //                      double IMin, CONTROLLER_CONTROL_MODE mode, double time,
-  //                      double minEffore,
-  //                      double maxEffort, mechanism::Joint *joint)
-
-  /***************************************************************************************/
-  /*                                                                                     */
-  /*                        initialize transmission                                      */
-  /*                                                                                     */
-  /*       TODO: in Robot                                                                */
-  /*                                                                                     */
-  /***************************************************************************************/
-  SimpleTransmission* st[PR2::MAX_JOINTS];
-  for (int i = 0;i<PR2::MAX_JOINTS;i++){
-    st[i] = new SimpleTransmission(joint[i],&hi->actuator[0],1,1,1);
-  }
-  
   //myPR2->hw.GetSimTime(&time);
-
+#if TEST_ROS_CONTROLLER
   /***************************************************************************************/
   /*                                                                                     */
   /*                            initialize ROS Gazebo Nodes                              */
+  /*        this should probably live inside each pr2Controller                          */
   /*                                                                                     */
   /***************************************************************************************/
   RosJointController*               rjc[PR2::MAX_JOINTS];  // One ros node per controller
@@ -285,17 +213,6 @@ main(int argc, char** argv)
     if (rjc[i]->advertiseSubscribeMessages() != 0)
       exit(-1);
   }
-
-
-  /***************************************************************************************/
-  /*                                                                                     */
-  /*                                on termination...                                    */
-  /*                                                                                     */
-  /***************************************************************************************/
-  signal(SIGINT,  (&finalize));
-  signal(SIGQUIT, (&finalize));
-  signal(SIGTERM, (&finalize));
-
   /***************************************************************************************/
   /*                                                                                     */
   /* Update ROS Gazebo Node                                                              */
@@ -311,6 +228,16 @@ main(int argc, char** argv)
       exit(-1);
     }
   }
+#endif
+
+  /***************************************************************************************/
+  /*                                                                                     */
+  /*                                on termination...                                    */
+  /*                                                                                     */
+  /***************************************************************************************/
+  signal(SIGINT,  (&finalize));
+  signal(SIGQUIT, (&finalize));
+  signal(SIGTERM, (&finalize));
 
   /***************************************************************************************/
   /*                                                                                     */
@@ -319,7 +246,6 @@ main(int argc, char** argv)
   /*                                                                                     */
   /***************************************************************************************/
   printf("Entering realtime loop\n");
-  double pos;
   while(1)
   { 
     //myPR2->hw.GetSimTime(&time);
@@ -329,44 +255,15 @@ main(int argc, char** argv)
     //   each controller will try to read new commands from shared memory with nonRT hooks,
     //   and skip update if locked by nonRT loop.
 
-    /*
-    myArm.Update();
-    myHead.Update();
-    mySpine.Update();
-    myBase.Update();
-    myLaserScanner.Update();
-    myGripper.Update();
-    */
-
     // get encoder counts from hardware
-    h->updateState();
+    hardware.updateState();
 
+    // update mechanism control
+    mechanismControl.update();
 
-    for (int i = 0;i<PR2::MAX_JOINTS;i++){
-      // setup joint state from encoder counts
-      st[i]->propagatePosition();
-      //update controller
-      if (pthread_mutex_trylock(&simMutex[i])==0)
-      {
-        jc[i]->update();
-        pthread_mutex_unlock(&simMutex[i]); //Unlock after we're done with r/w
-      }
-      // update command current from joint command
-      st[i]->propagateEffort();
-    }
-
-    //cout << "pos:: " << joint->position << ", eff:: " << joint->commandedEffort << endl;
-
-    // send command to hardware
-    h->sendCommand();
-
-    // refresh hardware
-    h->tick();
-
-
-    // wait for Gazebo time step
+    // wait for Gazebo time step, fix Wait() call or else...
     //myPR2->hw.ClientWait();
-    usleep(1000);
+    usleep(800);
 
   }
   
