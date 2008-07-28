877c2717b5a18249890aef0491c2492f732769ac
stuglaser <stuglaser@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-28 22:10:00 +0000

    SimpleTransmission now inherits from Transmission.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@2194 f5854215-dd47-0410-b2c4-cdd35faa7885

--- robot_models/robot_mechanism_model/include/mechanism_model/transmission.h
+++ robot_models/robot_mechanism_model/include/mechanism_model/transmission.h
@@ -2,27 +2,27 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Copyright (C) 2008, Eric Berger
 //
-// Redistribution and use in source and binary forms, with or without 
+// Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-//   * Redistributions of source code must retain the above copyright notice, 
+//   * Redistributions of source code must retain the above copyright notice,
 //     this list of conditions and the following disclaimer.
-//   * Redistributions in binary form must reproduce the above copyright 
-//     notice, this list of conditions and the following disclaimer in the 
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
 //     documentation and/or other materials provided with the distribution.
-//   * Neither the name of Stanford University nor the names of its 
-//     contributors may be used to endorse or promote products derived from 
+//   * Neither the name of Stanford University nor the names of its
+//     contributors may be used to endorse or promote products derived from
 //     this software without specific prior written permission.
-//   
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 //////////////////////////////////////////////////////////////////////////////
 
@@ -34,66 +34,61 @@
 
 namespace mechanism {
 
-  /*  class Transmission{
+class Transmission
+{
+public:
+  Transmission() {}
+  virtual ~Transmission() {}
 
-    public:
+  // Uses encoder data to fill out joint position and velocities
+  virtual void propagatePosition() = 0;
 
-    Transmission(){};
+  // Uses commanded joint efforts to fill out commanded motor currents
+  virtual void propagateEffort() = 0;
+};
 
-    virtual ~Transmission(){};
 
-    virtual void propagatePosition(); //Use encoder data to fill out joint position and velocities
+class SimpleTransmission : Transmission
+{
+public:
+  SimpleTransmission() {}
+  SimpleTransmission(Joint *joint, Actuator *actuator, double mechanicalReduction, double motorTorqueConstant, double ticksPerRadian);
+  ~SimpleTransmission() {}
 
-    virtual void propagateEffort(); //Use commanded joint efforts to fill out commanded motor currents
+  Actuator *actuator;
+  Joint *joint;
 
-  };
-  */
-  class SimpleTransmission{
+  double mechanicalReduction;
+  double motorTorqueConstant;
+  double pulsesPerRevolution;
 
-    public:
+  void propagatePosition();
+  void propagateEffort();
+};
 
-    SimpleTransmission(Joint *joint, Actuator *actuator, double mechanicalReduction, double motorTorqueConstant, double ticksPerRadian);
-    
-    SimpleTransmission(){};
+#if 0
+class CoupledTransmission : public Transmission{
 
-    ~SimpleTransmission(){};
+public:
 
-     Actuator *actuator;
+  void CoupledTranmission(Actuator *actuator, Joint *joint, double mechanicalReduction, double motorTorqueConstant);
 
-     Joint *joint;
+};
 
-     double mechanicalReduction;
+class NonlinearTransmission : public Transmission{
 
-     double motorTorqueConstant;
+public:
 
-     double pulsesPerRevolution;
+  NonlinearTransmission(Actuator *actuator, Joint *joint, double mechanicalReduction, double motorTorqueConstant);
 
-     void propagatePosition();
+  Actuator *actuator;
 
-     void propagateEffort();
-  };
-  /*
-  class CoupledTransmission : public Transmission{
+  Joint *joint;
 
-    public:
+  // ?? Lookup table
+};
+#endif
 
-     void CoupledTranmission(Actuator *actuator, Joint *joint, double mechanicalReduction, double motorTorqueConstant);
-
-  };
-
-  class NonlinearTransmission : public Transmission{
-
-    public:
-
-     NonlinearTransmission(Actuator *actuator, Joint *joint, double mechanicalReduction, double motorTorqueConstant);
-
-     Actuator *actuator;
-
-     Joint *joint;
-
-     // ?? Lookup table
-  };
-  */
-}
+} // namespace mechanism
 
 #endif
