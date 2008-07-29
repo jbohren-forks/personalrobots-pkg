b59f7f36472c1b7529b7c04fec58bc8c5e259b7f
jdchen <jdchen@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-29 18:23:13 +0000

    move test code to a separate project
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@2253 f5854215-dd47-0410-b2c4-cdd35faa7885

--- 3rdparty/opencv/wgext/3DPoseEstimation/include/Cv3DPoseEstimateSSE.h
+++ 3rdparty/opencv/wgext/3DPoseEstimation/include/Cv3DPoseEstimateSSE.h
@@ -1,13 +1,13 @@
-#ifndef WG3DPOSEESTIMATESSE_H_
-#define WG3DPOSEESTIMATESSE_H_
+#ifndef CV3DPOSEESTIMATESSE_H_
+#define CV3DPOSEESTIMATESSE_H_
 
-#include "CvTest3DPoseEstimate.h"
+#include "Cv3DPoseEstimate.h"
 
-class Cv3DPoseEstimateSSE : public CvTest3DPoseEstimate
+class Cv3DPoseEstimateSSE : public Cv3DPoseEstimate
 {
 public:
 	Cv3DPoseEstimateSSE();
 	virtual ~Cv3DPoseEstimateSSE();
 };
 
-#endif /*WG3DPOSEESTIMATESSE_H_*/
+#endif /*CV3DPOSEESTIMATESSE_H_*/
