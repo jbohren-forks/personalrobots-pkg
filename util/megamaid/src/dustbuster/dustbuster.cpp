95d6308777b6986f6c5afe4da39182c2bc0735a0
alexteichman <alexteichman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-07-30 18:48:47 +0000

    Sequence saving of logs with dustbuster.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@2327 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/megamaid/src/dustbuster/dustbuster.cpp
+++ util/megamaid/src/dustbuster/dustbuster.cpp
@@ -44,44 +44,99 @@ using namespace std;
 void doUnsubscribe(string name, ros::msg* m, void* n)
 {
   ((ros::node*)(n))->unsubscribe(name);
+  cout << "Got a " << name << " message." << endl;
 
-  if (((ros::node*)(n))->num_subscriptions() == 0)
+  if (((ros::node*)(n))->num_subscriptions() == 0) { 
+    cout << "Dustbuster is full." << endl;
     ((ros::node*)(n))->self_destruct();
+  }
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv);
-  if (argc <= 1)
-  {
-    printf("\nusage: dustbuster TOPIC1 [TOPIC2] ...\n\n");
+
+  string filename;
+  vector<std::string> topics;
+
+  // -- Vanilla usage case: just log a bunch of topics.
+  if(argc > 2 && !strcmp(argv[1], "--vanilla")) {
+
+    //Get the filename.
+    time_t t = ::time(NULL);
+    struct tm *tms = localtime(&t);
+    char logdir[500];
+    snprintf(logdir, sizeof(logdir), "%d-%02d-%02d-%02d-%02d-%02d",
+	     tms->tm_year+1900, tms->tm_mon+1, tms->tm_mday,
+	     tms->tm_hour     , tms->tm_min  , tms->tm_sec);
+    mkdir(logdir, 0755);
+    filename = std::string(logdir) + std::string("/topics.bag");
+    
+    //Get the topics.
+    for (int i = 2; i < argc; i++)
+      topics.push_back(argv[i]);
+  }
+
+  
+  // -- Dataset collection usage case.
+  else if(argc > 4 && !strcmp(argv[1], "--seq")) {
+
+    //Get the filename.
+    mkdir(argv[2], 0755);
+    string dirname = argv[2];
+    string filebase = argv[3];
+
+    int n=0;
+    char num[3];
+    FILE *fp;
+    while(1) {
+      sprintf(num, "%03d", n);
+      filename = dirname + string("/") + num + string("-") + filebase + string(".bag");
+      fp = fopen(filename.c_str(), "r");
+      if(fp == NULL) {
+	break;
+      }
+      fclose(fp);
+      if(n>999) {
+	cout << "too many logs" << endl;
+	return 1;
+      }
+      n++;
+    }
+
+    //Get the topics.
+    for (int i = 4; i < argc; i++)
+      topics.push_back(argv[i]);
+  }
+  
+  // -- Usage description case.
+  else {
+    printf("\nusage:\n\n"
+	   "dustbuster --vanilla TOPIC1 [TOPIC2] ...\n"
+	   "\t Saves to a timestamped directory.\n\n"
+	   "dustbuster --seq SAVEDIR FILENAME TOPIC1 [TOPIC2] ... \n"
+	   "\t For saving a sequence of logs.  FILENAME will have a number and a .bag appended to it.\n\n");
     return 1;
   }
 
-  time_t t = ::time(NULL);
-  struct tm *tms = localtime(&t);
-  char logdir[500];
-  snprintf(logdir, sizeof(logdir), "%d-%02d-%02d-%02d-%02d-%02d",
-           tms->tm_year+1900, tms->tm_mon+1, tms->tm_mday,
-           tms->tm_hour     , tms->tm_min  , tms->tm_sec);
-  mkdir(logdir, 0755);
+  //Output confirmation of logging and filename.
+  cout << "Logging topics ";
+  for(unsigned int i=0; i<topics.size(); i++) {
+    cout << "\"" << topics[i] << "\" ";
+  }
+  cout << " to file " << filename << endl;
 
-  vector<std::string> topics;
-  for (int i = 1; i < argc; i++)
-    topics.push_back(argv[i]);
 
+  //Setup the logger.
   ros::node n("dustbuster", ros::node::WRITE_LOG_FILE);  // Ros peer style usage.
-
   LogRecorder l(&n);
-
-
   ros::Time start = ros::Time::now();
 
-  if (l.open(std::string(logdir) + std::string("/topics.bag"), start))
+  if (l.open(filename, start))
   {
     for (vector<std::string>::iterator i = topics.begin(); i != topics.end(); i++)
     {
-      printf("vacuum up [%s]\n", i->c_str());
+      printf("dustbustering up [%s]\n", i->c_str());
       l.addTopic<AnyMsg>(*i, &doUnsubscribe, (void*)(&n));
     }
 
@@ -90,6 +145,7 @@ int main(int argc, char **argv)
     n.self_destruct();
   }
 
+  //Wait for messages to log.
   n.spin();
   ros::fini();
   return 0;
