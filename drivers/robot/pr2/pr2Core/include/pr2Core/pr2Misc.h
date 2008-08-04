c96179b4fcec2a3692f98fb4a808a68d8f30cdd8
isucan <isucan@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-04 23:44:45 +0000

    removing functions that are now in math_utils w/Tully
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@2526 f5854215-dd47-0410-b2c4-cdd35faa7885

--- drivers/robot/pr2/pr2Core/include/pr2Core/pr2Misc.h
+++ drivers/robot/pr2/pr2Core/include/pr2Core/pr2Misc.h
@@ -1,12 +1,14 @@
 #ifndef __PR2MISC_H__
 #define __PR2MISC_H__
 
-#include<math.h>
-#include<stdio.h>
+#include <cmath>
+#include <cstdio>
+#include <math_utils/angles.h>
 
 namespace PR2
 {
-
+    using namespace math_utils;
+    
     /* --------------- utility functions ---------------- */
     /* ---------------        TODO       ---------------- */
     /* --------------- move to a library ---------------- */
@@ -115,61 +117,6 @@ namespace PR2
     {
        return sqrt(x*x+y*y);
     }
-
-
-    #define FROM_DEGREES(degrees)	(((double)(degrees))/180*M_PI)
-    /*
-     * normalize_angle_positive
-     *
-     * Normalizes the angle to be 0 circle to 1 circle
-     * It takes and returns native units.
-     */
-
-    inline double normalize_angle_positive(double angle)
-    {
-      return fmod(fmod(angle, FROM_DEGREES(360))+FROM_DEGREES(360), FROM_DEGREES(360));
-    }
-    /*
-     * normalize
-     *
-     * Normalizes the angle to be -1/2 circle to +1/2 circle
-     * It takes and returns native units.
-     *
-     */
-
-    inline double normalize_angle(double angle)
-    {
-      double a=normalize_angle_positive(angle);
-      if (a>FROM_DEGREES(180))
-          a-=FROM_DEGREES(360);
-      return(a);
-    }
-    /*
-     * shortest_angular_distance
-     *
-     * Given 2 angles, this returns the shortest angular
-     * difference.  The inputs and ouputs are of course native
-     * units.
-     *
-     * As an example, if native units are degrees, the result
-     * would always be -180 <= result <= 180.  Adding the result
-     * to "from" will always get you an equivelent angle to "to".
-     */
-
-    inline double shortest_angular_distance(double from, double to)
-    {
-      double result;
-      result=normalize_angle_positive(
-          normalize_angle_positive(to) - normalize_angle_positive(from));
-
-      if ( result > FROM_DEGREES(180) ) {  // If the result > 180,
-                                           // It's shorter the other way.
-          result=-(FROM_DEGREES(360)-result);   
-      }
-      return normalize_angle(result);
-    }
-
-
 
 }
 
