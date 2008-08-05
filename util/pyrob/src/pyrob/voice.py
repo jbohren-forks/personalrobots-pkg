f50a09beadf9547edb289b143f39e6f858e79205
haidai <haidai@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-05 22:52:34 +0000

    added __init__ to make pyrob a package. made voice.say into a function instead of class method.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@2597 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/pyrob/src/pyrob/voice.py
+++ util/pyrob/src/pyrob/voice.py
@@ -1,10 +1,5 @@
 import os
 
-class Voice( ):
-    def __init__(self):
-        self.salutations = [ "hello", "top of the morning"]
-        pass
-
-    def say_text( self, mytex ):
-        os.system( 'echo "' + mytex + '" | festival --tts' )
+def say( self, mytex ):
+    os.system( 'echo "' + mytex + '" | festival --tts' )
 
