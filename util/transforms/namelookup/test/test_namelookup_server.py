8390c782509babd50b67f8162d09bd7bf900daf1
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-06 00:12:07 +0000

    trying out unittest for #366 (not working yet, not sure why)
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@2607 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ util/transforms/namelookup/test/test_namelookup_server.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the Willow Garage nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Revision $Id: gossipbot.py 1013 2008-05-21 01:08:56Z sfkwc $
+
+## Integration test for peer_subscribe_notify
+
+PKG = 'namelookup'
+NAME = 'namelookup_server_test'
+
+import rostools
+rostools.update_path(PKG)
+
+import sys, time
+import unittest
+
+import rospy, rostest
+from namelookup.srv import *
+
+class TestNameLookup(unittest.TestCase):
+
+    names = {}
+    previously_tried_names = {}
+    
+    def test_namelookup_server(self):
+        rospy.wait_for_service('/nameToNumber')
+        s = rospy.ServiceProxy('/nameToNumber', NameToNumber)
+
+        ## \todo change this to a random string generator
+        tests = [('asdf', 1), ('fdsa', 2), ('asdf', 1)]
+        for x, y in tests:
+            print "Requesting %s"%(x)
+            resp = s.call(NameToNumber(x))
+            if names.has_key(x):
+                self.assertEquals(resp.number,names[x], "two different values returned for the same string, previous value was %s vs. %s"%( names[x], resp.number))
+            else:
+                ## \todo assert that the name was not previously tested
+                names[x] = resp.number
+            print "%s lookus up to %s = %s"%(x, resp.number)            
+        
+if __name__ == '__main__':
+    rostest.run(PKG, NAME, TestNameLookup, sys.argv)
