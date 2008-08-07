8068bfcae2f169e1bcb48d7d30e241d17a69c104
tpratkanis <tpratkanis@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-07 23:33:29 +0000

    Changing name.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@2768 f5854215-dd47-0410-b2c4-cdd35faa7885

--- simulators/rosgazebo/test/poshack.cpp
+++ simulators/rosgazebo/test/poshack.cpp
@@ -8,14 +8,15 @@ using std::string;
 /**
  * This is a hack to get the position of the robot. TfPy is not here yet, so this is just to get the tests working.
  */
-class PosHack : public ros::node {
+class GroundTruthTransform : public ros::node {
 public:
   std_msgs::Point2DFloat32 msg;
   rosTFClient tf;
 
-  PosHack() : ros::node("poshack"), tf(*this, true) {
+  GroundTruthTransform() : ros::node("GroundTruthTransform"), tf(*this, false) {
     advertise<std_msgs::Point2DFloat32>("groundtruthposition");
   }
+
   void speak() {
     libTF::TFPose robotPose, global_pose;
     robotPose.x = 0;
@@ -27,18 +28,17 @@ public:
     robotPose.time = 0;
     try {
       robotPose.frame = tf.lookup("FRAMEID_ROBOT");
-      //      tf.lookup("base");
     } catch(libTF::TransformReference::LookupException& ex) {
-      std::cerr << "LookupException in lookup(\"FRAMEID_ROBOT\")!"<<ex.what() <<"\n";
-      std::cout << "LookupException in lookup(\"FRAMEID_ROBOT\")!"<<ex.what() <<"";
+      std::cerr << "LookupException in lookup(\"FRAMEID_ROBOT\"): " << ex.what() << "\n";
+      std::cout << "LookupException in lookup(\"FRAMEID_ROBOT\"): " << ex.what();
       return;
     } catch(libTF::Pose3DCache::ExtrapolateException& ex) {
-      std::cerr << "ExtrapolateException in lookup(\"FRAMEID_ROBOT\")!"<<ex.what() <<"\n";
-      std::cout << "ExtrapolateException in lookup(\"FRAMEID_ROBOT\")!"<<ex.what() <<"";
+      std::cerr << "ExtrapolateException in lookup(\"FRAMEID_ROBOT\"): " << ex.what() << "\n";
+      std::cout << "ExtrapolateException in lookup(\"FRAMEID_ROBOT\"): " << ex.what();
       return;
     } catch(libTF::TransformReference::ConnectivityException& ex) {
-      std::cerr << "ConnectivityException in lookup(\"FRAMEID_ROBOT\")!"<<ex.what() <<"\n";
-      std::cout << "ConnectivityException in lookup(\"FRAMEID_ROBOT\")!"<<ex.what() <<"";
+      std::cerr << "ConnectivityException in lookup(\"FRAMEID_ROBOT\"): " << ex.what() << "\n";
+      std::cout << "ConnectivityException in lookup(\"FRAMEID_ROBOT\"): " << ex.what();
       return;
     }
 
@@ -47,16 +47,16 @@ public:
       global_pose = this->tf.transformPose("FRAMEID_ODOM", robotPose);
     } catch(libTF::TransformReference::LookupException& ex) {
       std::cerr << tf.viewFrames();
-      std::cerr << "LookupException in transformPose(\"FRAMEID_ODOM\", robotPose)!"<<ex.what() <<"\n";
-      std::cout << "LookupException in transformPose(\"FRAMEID_ODOM\", robotPose)!"<<ex.what() <<"";
+      std::cerr << "LookupException in transformPose(\"FRAMEID_ODOM\", robotPose): " << ex.what() << "\n";
+      std::cout << "LookupException in transformPose(\"FRAMEID_ODOM\", robotPose): " << ex.what();
       return;
     } catch(libTF::Pose3DCache::ExtrapolateException& ex) {
-      std::cerr << "ExtrapolateException in transformPose(\"FRAMEID_ODOM\", robotPose)!"<<ex.what() <<"\n";
-      std::cout << "ExtrapolateException in transformPose(\"FRAMEID_ODOM\", robotPose)!"<<ex.what() <<"";
+      std::cerr << "ExtrapolateException in transformPose(\"FRAMEID_ODOM\", robotPose): " << ex.what() << "\n";
+      std::cout << "ExtrapolateException in transformPose(\"FRAMEID_ODOM\", robotPose): " << ex.what();
       return;
     } catch(libTF::TransformReference::ConnectivityException& ex) {
-      std::cerr << "ConnectivityException in transformPose(\"FRAMEID_ODOM\", robotPose)!"<<ex.what() <<"\n";
-      std::cout << "ConnectivityException in transformPose(\"FRAMEID_ODOM\", robotPose)!"<<ex.what() <<"";
+      std::cerr << "ConnectivityException in transformPose(\"FRAMEID_ODOM\", robotPose): " << ex.what() << "\n";
+      std::cout << "ConnectivityException in transformPose(\"FRAMEID_ODOM\", robotPose): " << ex.what();
       return;
     }
     msg.x = global_pose.x;
@@ -68,8 +68,8 @@ public:
 
 int main(int argc, char **argv) {
   ros::init(argc, argv);
-  std::cout << "Starting... Real\n";
-  PosHack t;
+  std::cout << "Starting...\n";
+  GroundTruthTransform t;
   while (t.ok())
   {
     usleep(100000);
