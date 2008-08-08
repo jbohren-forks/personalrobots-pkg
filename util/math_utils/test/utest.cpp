4c69bb56d9f44a455db59d0593d23b1037d82df2
gerkey <gerkey@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-08 19:04:14 +0000

    renamed and recased unit tests
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@2811 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/math_utils/test/utest.cpp
+++ util/math_utils/test/utest.cpp
@@ -4,7 +4,7 @@
 
 #define TEST_EXPRESSION(a) EXPECT_EQ((a), meval::EvaluateMathExpression(#a))
 
-TEST(MathExpressions, OperatorRecognition){
+TEST(MathExpressions, operatorRecognition){
   EXPECT_TRUE(meval::ContainsOperators("+"));
   EXPECT_TRUE(meval::ContainsOperators("-"));
   EXPECT_TRUE(meval::ContainsOperators("/"));
@@ -12,7 +12,7 @@ TEST(MathExpressions, OperatorRecognition){
   EXPECT_FALSE(meval::ContainsOperators("1234567890qwertyuiop[]asdfghjkl;'zxcvbnm,._=?8"));
 }
 
-TEST(MathExpressions, BasicOperations){
+TEST(MathExpressions, basicOperations){
   EXPECT_EQ(5, meval::EvaluateMathExpression("2+3"));
   EXPECT_EQ(5, meval::EvaluateMathExpression("2 + 3"));
   EXPECT_EQ(10, meval::EvaluateMathExpression("20/2"));
@@ -20,23 +20,23 @@ TEST(MathExpressions, BasicOperations){
   EXPECT_EQ(24, meval::EvaluateMathExpression("6 * 4"));
 }
 
-TEST(MathExpressions, ComplexOperations){
+TEST(MathExpressions, complexOperations){
   TEST_EXPRESSION(((3 + 4) / 2.0) + 10);
   TEST_EXPRESSION(7 * (1 + 2 + 3 - 2 + 3.4) / 12.7);
   TEST_EXPRESSION((1 + 2 + 3) - (8.0 / 10));
 }
 
-TEST(MathExpression, UnaryMinus){
+TEST(MathExpressions, UnaryMinus){
   TEST_EXPRESSION(-5);
 }
 
-TEST(MathExpressions, BadInput){
+TEST(MathExpressions, badInput){
   //TODO - figure out what good error behavior is and test for it properly
   //EXPECT_EQ(0, meval::EvaluateMathExpression("4.1.3 - 4.1"));
   //EXPECT_EQ(0, meval::EvaluateMathExpression("4.1.3"));
 }
 
-TEST(math_utils, BasicOperations){
+TEST(MathUtils, basicOperations){
   EXPECT_EQ(math_utils::clamp<int>(-10, 10, 20), 10);
   EXPECT_EQ(math_utils::clamp<int>(15, 10, 20), 15);
   EXPECT_EQ(math_utils::clamp<int>(25, 10, 20), 20);
