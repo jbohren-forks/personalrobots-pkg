2f0bf9b9019295c53e2578a463337c1aa1458334
mmwise <mmwise@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-11 22:28:23 +0000

    adding stub for david
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@2927 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ controllers/pr2_controllers/include/pr2_controllers/laser_scanner_controller.h
@@ -0,0 +1,137 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#ifndef LASER_SCANNER_CONTROLLER_H
+#define LASER_SCANNER_CONTROLLER_H
+
+#include <ros/node.h>
+
+#include <generic_controllers/controller.h>
+#include <generic_controllers/joint_position_controller.h>
+
+// Services
+#include <generic_controllers/SetCommand.h>
+#include <generic_controllers/GetCommand.h>
+
+namespace controller
+{
+
+class LaserScannerController : public Controller
+{
+public:
+  /*!
+   * \brief Default Constructor of the JointController class.
+   *
+   */
+  LaserScannerController();
+
+  /*!
+   * \brief Destructor of the JointController class.
+   */
+  ~LaserScannerController();
+
+  /*!
+   * \brief Functional way to initialize limits and gains.
+   *
+   */
+  void init(double p_gain, double i_gain, double d_gain, double windup, double time, mechanism::Joint *joint);
+  void initXml(mechanism::Robot *robot, TiXmlElement *config);
+
+  /*!
+   * \brief Give set position of the joint for next update: revolute (angle) and prismatic (position)
+   *
+   * \param double pos Position command to issue
+   */
+  void setCommand(double command);
+
+  /*!
+   * \brief Get latest position command to the joint: revolute (angle) and prismatic (position).
+   */
+  double getCommand();
+
+  /*!
+   * \brief Read the torque of the motor
+   */
+  double getActual();
+
+  /*!
+   * \brief Issues commands to the joint. Should be called at regular intervals
+   */
+
+  virtual void update();
+
+private:
+  /*!
+   * \brief Actually issue torque set command of the joint motor.
+   */
+  void setJointEffort(double torque);
+
+  mechanism::Joint* joint_; /*!< Joint we're controlling>*/
+  JointPositionController joint_position_controller_; /*!< Internal PID controller>*/
+  double last_time_; /*!< Last time stamp of update> */
+  double command_; /*!< Last commanded position> */
+  mechanism::Robot *robot_; /*!< Pointer to robot structure>*/
+};
+
+class LaserScannerControllerNode : public Controller
+{
+public:
+  /*!
+   * \brief Default Constructor
+   *
+   */
+  LaserScannerControllerNode();
+
+  /*!
+   * \brief Destructor
+   */
+  ~LaserScannerControllerNode();
+
+  void update();
+
+  void initXml(mechanism::Robot *robot, TiXmlElement *config);
+
+  // Services
+  bool setCommand(generic_controllers::SetCommand::request &req,
+                  generic_controllers::SetCommand::response &resp);
+
+  bool getCommand(generic_controllers::GetCommand::request &req,
+                  generic_controllers::GetCommand::response &resp);
+
+private:
+  LaserScannerController *c_;
+};
+}
+
+#endif /* JOINT_POSITION_CONTROLLER_H */
---
+++ controllers/pr2_controllers/src/laser_scanner_controller.cpp
@@ -0,0 +1,149 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+#include <algorithm>
+
+#include <pr2_controllers/laser_scanner_controller.h>
+#include <math_utils/angles.h>
+
+using namespace std;
+using namespace controller;
+
+ROS_REGISTER_CONTROLLER(LaserScannerController)
+
+LaserScannerController::LaserScannerController()
+{
+  // Initialize PID class
+  command_ = 0;
+}
+
+LaserScannerController::~LaserScannerController()
+{
+}
+
+void LaserScannerController::init(double p_gain, double i_gain, double d_gain, double windup, double time, mechanism::Joint *joint)
+{
+  joint_position_controller_.init( p_gain,  i_gain,  d_gain,  windup, time, joint);
+
+  command_= 0;
+  last_time_= time;
+  joint_ = joint;
+}
+
+void LaserScannerController::initXml(mechanism::Robot *robot, TiXmlElement *config)
+{
+  robot_ = robot;
+  TiXmlElement *elt = config->FirstChildElement("joint");
+  if (elt) {
+    // TODO: error check if xml attributes/elements are missing
+    double p_gain = atof(elt->FirstChildElement("pGain")->GetText());
+    double i_gain = atof(elt->FirstChildElement("iGain")->GetText());
+    double d_gain = atof(elt->FirstChildElement("dGain")->GetText());
+    double windup= atof(elt->FirstChildElement("windup")->GetText());
+    init(p_gain, i_gain, d_gain, windup, robot->hw_->current_time_, robot->getJoint(elt->Attribute("name")));
+  }
+}
+
+// Set the joint position command
+void LaserScannerController::setCommand(double command)
+{
+  command_ = command;
+}
+
+// Return the current position command
+double LaserScannerController::getCommand()
+{
+  return command_;
+}
+
+// Return the measured joint position
+double LaserScannerController::getActual()
+{
+  return joint_->position_;
+}
+
+void LaserScannerController::update()
+{
+  
+}
+
+void LaserScannerController::setJointEffort(double effort)
+{
+  joint_->commanded_effort_ = min(max(effort, -joint_->effort_limit_), joint_->effort_limit_);
+}
+
+ROS_REGISTER_CONTROLLER(LaserScannerControllerNode)
+LaserScannerControllerNode::LaserScannerControllerNode() 
+{
+  c_ = new LaserScannerController();
+}
+
+LaserScannerControllerNode::~LaserScannerControllerNode()
+{
+  delete c_;
+}
+
+void LaserScannerControllerNode::update()
+{
+  c_->update();
+}
+
+bool LaserScannerControllerNode::setCommand(
+  generic_controllers::SetCommand::request &req,
+  generic_controllers::SetCommand::response &resp)
+{
+  c_->setCommand(req.command);
+  resp.command = c_->getCommand();
+
+  return true;
+}
+
+bool LaserScannerControllerNode::getCommand(
+  generic_controllers::GetCommand::request &req,
+  generic_controllers::GetCommand::response &resp)
+{
+  resp.command = c_->getCommand();
+
+  return true;
+}
+
+void LaserScannerControllerNode::initXml(mechanism::Robot *robot, TiXmlElement *config)
+{
+  ros::node *node = ros::node::instance();
+  string prefix = config->Attribute("name");
+  
+  c_->initXml(robot, config);
+  node->advertise_service(prefix + "/set_command", &LaserScannerControllerNode::setCommand, this);
+  node->advertise_service(prefix + "/get_command", &LaserScannerControllerNode::getCommand, this);
+}
+
