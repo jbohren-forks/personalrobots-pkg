b2b0d7efb9cecde0a7d4ab272ab5c4c336943593
stuglaser <stuglaser@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-13 21:31:23 +0000

    Added mech.py, a script for using the services exposed by MechanismControlNode.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3012 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ mechanism/mechanism_control/mech.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+# Provides quick access to the services exposed by MechanismControlNode
+
+import rostools
+rostools.update_path('mechanism_control')
+
+import rospy, sys
+from mechanism_control.srv import *
+
+def print_usage(exit_code = 0):
+    print '''Commands:
+    lt  - List controller Types
+    lc  - List active controllers
+    sp <type> <name>  - Spawn a <type> controller, calling it <name> (config on stdin)
+    kl <name>  - Kills the controller named <name>'''
+    sys.exit(exit_code)
+
+def list_controller_types():
+    s = rospy.ServiceProxy('list_controller_types', ListControllerTypes)
+    resp = s.call(ListControllerTypesRequest())
+    for t in resp.types:
+        print t
+
+def list_controllers():
+    s = rospy.ServiceProxy('list_controllers', ListControllers)
+    resp = s.call(ListControllersRequest())
+    for c in resp.controllers:
+        print c
+
+def spawn_controller(args):
+    type, name = args[0], args[1]
+    s = rospy.ServiceProxy('spawn_controller', SpawnController)
+    resp = s.call(SpawnControllerRequest(type, name, sys.stdin.read()))
+    if resp.ok == 1:
+        print "Spawned %s successfully" % name
+    else:
+        print "Error when spawning", resp.ok
+
+def kill_controller(name):
+    s = rospy.ServiceProxy('kill_controller', KillController)
+    resp = s.call(KillControllerRequest(name))
+    if resp.ok == 1:
+        print "Killed %s successfully" % name
+    else:
+        print "Error when killing", resp.ok
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print_usage()
+
+    if sys.argv[1] == 'lt':
+        list_controller_types()
+    elif sys.argv[1] == 'lc':
+        list_controllers()
+    elif sys.argv[1] == 'sp':
+        spawn_controller(sys.argv[2:])
+    elif sys.argv[1] == 'kl':
+        kill_controller(sys.argv[2])
