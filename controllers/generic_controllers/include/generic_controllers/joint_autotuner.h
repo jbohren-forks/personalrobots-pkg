711b56a72b3898be52449fa913fbda659ed1f803
david_li <david_li@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-14 22:55:24 +0000

    First cut at a class to perform relay feedback experiment for autotuning
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3092 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ controllers/generic_controllers/include/generic_controllers/joint_autotuner.h
@@ -0,0 +1,194 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#pragma once
+
+/***************************************************/
+/*! \class controller::JointAutotuner
+    \brief Joint Position Controller
+    
+    This class closes the loop around positon using
+    a pid loop. 
+
+*/
+/***************************************************/
+
+#include <ros/node.h>
+#include <vector>
+#include <generic_controllers/controller.h>
+#include <generic_controllers/pid.h>
+
+// Services
+#include <generic_controllers/SetCommand.h>
+#include <generic_controllers/GetActual.h>
+
+namespace controller
+{
+
+class JointAutotuner : public Controller
+{
+
+  static const double NUMCYCLES = 5; /*!<Number of cycles that need to match for autotuner to read as stable>!*/
+  static const double AMPLITUDETOLERANCE = 0.05; //Allow 5% variance 
+  static const double PERIODTOLERANCE = 0.05; //Allow 5% variance 
+  static const double RELAYFRACTION = 0.05; //Use 5% of effort
+ enum AutoControlState
+  { 
+    POSITIVE_PEAK,NEGATIVE_PEAK, DONE
+  };
+public:
+  /*!
+   * \brief Default Constructor of the JointAutotuner class.
+   *
+   */
+  JointAutotuner();
+
+  /*!
+   * \brief Destructor of the JointAutotuner class.
+   */
+  ~JointAutotuner();
+
+  /*!
+   * \brief Functional way to initialize limits and gains.
+   * \param p_gain Proportional gain.
+   * \param i_gain Integral gain.
+   * \param d_gain Derivative gain.
+   * \param windup Intergral limit.
+   * \param time The current hardware time. 
+   * \param *joint The joint that is being controlled.
+   */
+  void init(double p_gain, double i_gain, double d_gain, double windup, double time,mechanism::Robot *robot, mechanism::Joint *joint);
+  void initXml(mechanism::Robot *robot, TiXmlElement *config);
+
+  /*!
+   * \brief Give set position of the joint for next update: revolute (angle) and prismatic (position)
+   *
+   * \param command 
+   */
+  void setCommand(double command);
+
+  /*!
+   * \brief Get latest position command to the joint: revolute (angle) and prismatic (position).
+   */
+  double getCommand();
+
+  /*!
+   * \brief Read the torque of the motor
+   */
+  double getActual();
+
+  /*!
+   * \brief Get latest time..
+   */
+  double getTime();
+
+
+  /*!
+   * \brief Issues commands to the joint. Should be called at regular intervals
+   */
+
+  virtual void update();
+
+  double p_gain_;
+  double i_gain_;
+  double d_gain_;
+
+  AutoControlState current_state_;
+  double amplitude_;
+  double last_amplitude_;
+  double period_;
+  double last_period_;
+
+  double positive_peak_;
+  double negative_peak_;
+
+  double relay_height_;
+  int successful_cycles_;
+
+  double cycle_start_time_;
+
+private:
+  /*!
+   * \brief Actually issue torque set command of the joint motor.
+   */
+  void setJointEffort(double torque);
+
+  mechanism::Joint* joint_;  /**< Joint we're controlling. */
+  Pid pid_controller_;       /**< Internal PID controller. */
+  double last_time_;         /**< Last time stamp of update. */
+  double command_;           /**< Last commanded position. */
+  mechanism::Robot *robot_;  /**< Pointer to robot structure. */
+};
+
+/***************************************************/
+/*! \class controller::JointAutotunerNode
+    \brief Joint Position Controller ROS Node
+    
+    This class closes the loop around positon using
+    a pid loop. 
+
+
+*/
+/***************************************************/
+
+class JointAutotunerNode : public Controller
+{
+public:
+  /*!
+   * \brief Default Constructor
+   *
+   */
+  JointAutotunerNode();
+
+  /*!
+   * \brief Destructor
+   */
+  ~JointAutotunerNode();
+
+  void update();
+
+  void initXml(mechanism::Robot *robot, TiXmlElement *config);
+
+  // Services
+  bool setCommand(generic_controllers::SetCommand::request &req,
+                  generic_controllers::SetCommand::response &resp);
+
+  bool getActual(generic_controllers::GetActual::request &req,
+                  generic_controllers::GetActual::response &resp);
+
+private:
+  JointAutotuner *c_;
+};
+}
+
---
+++ controllers/generic_controllers/src/joint_autotuner.cpp
@@ -0,0 +1,200 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+#include <algorithm>
+
+#include <generic_controllers/joint_autotuner.h>
+#include <math_utils/angles.h>
+
+using namespace std;
+using namespace controller;
+
+ROS_REGISTER_CONTROLLER(JointAutotuner)
+
+JointAutotuner::JointAutotuner()
+{
+  // Initialize PID class
+  pid_controller_.initPid(0, 0, 0, 0, 0);
+  command_ = 0;
+}
+
+JointAutotuner::~JointAutotuner()
+{
+}
+
+void JointAutotuner::init(double p_gain, double i_gain, double d_gain, double windup, double time,mechanism::Robot *robot, mechanism::Joint *joint)
+{
+  pid_controller_.initPid(p_gain, i_gain, d_gain, windup, -windup);
+  
+  robot_ = robot;
+  command_= 0;
+  last_time_= time;
+  joint_ = joint;
+
+  relay_height_ = RELAYFRACTION * joint_->effort_limit_;
+  
+}
+
+void JointAutotuner::initXml(mechanism::Robot *robot, TiXmlElement *config)
+{
+  
+  TiXmlElement *elt = config->FirstChildElement("joint");
+  if (elt) {
+    // TODO: error check if xml attributes/elements are missing
+    double p_gain = atof(elt->FirstChildElement("pGain")->GetText());
+    double i_gain = atof(elt->FirstChildElement("iGain")->GetText());
+    double d_gain = atof(elt->FirstChildElement("dGain")->GetText());
+    double windup= atof(elt->FirstChildElement("windup")->GetText());
+    init(p_gain, i_gain, d_gain, windup, robot->hw_->current_time_, robot, robot->getJoint(elt->Attribute("name")));
+  }
+}
+
+// Set the joint position command
+void JointAutotuner::setCommand(double command)
+{
+  command_ = command;
+}
+
+// Return the current position command
+double JointAutotuner::getCommand()
+{
+  return joint_->commanded_effort_;
+}
+
+// Return the measured joint position
+double JointAutotuner::getActual()
+{
+  return joint_->position_;
+}
+
+double JointAutotuner::getTime()
+{
+  return robot_->hw_->current_time_;
+}
+
+void JointAutotuner::update()
+{
+  double position = joint_->position_;
+  double time = getTime();
+  //Scan for transitions
+  if(current_state_==POSITIVE_PEAK && position<0) //Transition negative
+  {
+    current_state_=NEGATIVE_PEAK;
+  }
+  else if (current_state_==NEGATIVE_PEAK && position>0) //Transition to next period
+  {
+    current_state_ = POSITIVE_PEAK;
+    period_ = time-cycle_start_time_;
+    amplitude_ = (positive_peak_-negative_peak_)/2; //Record amplitude
+    if( (fabs(amplitude_-last_amplitude_) < AMPLITUDETOLERANCE*last_amplitude_) &&(fabs(period_-last_period_)<PERIODTOLERANCE*last_period_))//If the two peaks match closely
+    {
+     
+        successful_cycles_++; //increment successful cycles
+        if(successful_cycles_>=NUMCYCLES) current_state_ = DONE; //Done testing
+     }
+    else successful_cycles_ = 0; //Otherwise, reset if we're varying too much
+   
+    //Reset for next period
+    positive_peak_ = 0.0;
+    negative_peak_ = 0.0;
+    cycle_start_time_ = time;
+    last_period_ = period_;
+    last_amplitude_ = amplitude_;
+  }
+
+  //Update amplitude measures
+  if(current_state_ == POSITIVE_PEAK)
+  {
+    if(position > positive_peak_) positive_peak_ = position;
+  //If looking for positive peak, set positive h
+    setJointEffort(relay_height_);
+  }
+  else if(current_state_ == NEGATIVE_PEAK)
+  {    
+    if(position<negative_peak_) negative_peak_ = position;
+  //If looking for negative peak, set negative h
+    setJointEffort(-relay_height_);
+  }
+
+}
+
+void JointAutotuner::setJointEffort(double effort)
+{
+  joint_->commanded_effort_ = min(max(effort, -joint_->effort_limit_), joint_->effort_limit_);
+}
+
+ROS_REGISTER_CONTROLLER(JointAutotunerNode)
+JointAutotunerNode::JointAutotunerNode() 
+{
+  c_ = new JointAutotuner();
+}
+
+JointAutotunerNode::~JointAutotunerNode()
+{
+  delete c_;
+}
+
+void JointAutotunerNode::update()
+{
+  c_->update();
+}
+
+bool JointAutotunerNode::setCommand(
+  generic_controllers::SetCommand::request &req,
+  generic_controllers::SetCommand::response &resp)
+{
+  c_->setCommand(req.command);
+  resp.command = c_->getCommand();
+
+  return true;
+}
+
+bool JointAutotunerNode::getActual(
+  generic_controllers::GetActual::request &req,
+  generic_controllers::GetActual::response &resp)
+{
+  resp.command = c_->getActual();
+  resp.time = c_->getTime();
+  return true;
+}
+
+void JointAutotunerNode::initXml(mechanism::Robot *robot, TiXmlElement *config)
+{
+  ros::node *node = ros::node::instance();
+  string prefix = config->Attribute("name");
+  
+  c_->initXml(robot, config);
+  node->advertise_service(prefix + "/set_command", &JointAutotunerNode::setCommand, this);
+  node->advertise_service(prefix + "/get_actual", &JointAutotunerNode::getActual, this);
+}
+
