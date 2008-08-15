81c3913f16263cd1ca3a0e7a1ae3984478310c15
tpratkanis <tpratkanis@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-15 22:09:47 +0000

    Convertting exec_trex to jam. Created a script to copy all headers to a single directory and cut down the path lengths.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3145 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ 3rdparty/trex/copyIncludes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+function scanDirectory {
+    HEADERS="`ls $1/*.h 2> /dev/null` `ls $1/*.hh 2> /dev/null`"
+    for HEADER in $HEADERS
+    do
+	cp $HEADER $INCLUDE_DIR
+    done
+    FILES=`ls -l $1 | grep "^d" | awk '{ print $8 }'`
+    for FILE in $FILES
+    do
+	scanDirectory "$1/$FILE"
+    done
+    
+}
+
+rm -r include 2> /dev/null
+mkdir include
+INCLUDE_DIR="`pwd`/include"
+echo "Warning, this is not the real include directory. It will be deleted and recreated by make every run. Includes should be next to .cc files." > $INCLUDE_DIR/WARNING.txt
+scanDirectory `pwd`
+
+
