abc21c6af8b69bc7fcb49acb961f48dd39923c2d
tpratkanis <tpratkanis@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-15 22:09:42 +0000

    Jam to CMake conversion. CMake now does everything but the NDDL builds.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3143 f5854215-dd47-0410-b2c4-cdd35faa7885

--- exec_trex/installEnv.sh
+++ exec_trex/installEnv.sh
@@ -5,7 +5,3 @@
 echo "# This is an auto-generated file built using rospack" > env.jam
 echo "TREX_PKG_ROOT = `rospack find trex` ;" >> env.jam
 echo "" >> env.jam
-echo "PKG_CPP_FLAGS = `rospack export/cpp/cflags exec_trex` `pkg-config --cflags gdk-pixbuf-2.0` ;" >> env.jam
-echo "" >> env.jam
-echo "PKG_LINK_FLAGS = `rospack export/cpp/lflags exec_trex` `pkg-config --libs gdk-pixbuf-2.0` ;" >> env.jam
-echo "" >> env.jam
