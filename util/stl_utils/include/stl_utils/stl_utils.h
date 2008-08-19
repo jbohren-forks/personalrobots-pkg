313aed3f86d7b6d38623c5853a9c36ddcc1a51f1
stuglaser <stuglaser@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-19 21:39:56 +0000

    Added deleteValues function to stl_utils.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3270 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/stl_utils/include/stl_utils/stl_utils.h
+++ util/stl_utils/include/stl_utils/stl_utils.h
@@ -18,5 +18,19 @@ void deleteElements(C *c)
   }
 }
 
+template <class C>
+void deleteValues(C *c)
+{
+  typename C::iterator it;
+  for (it = c->begin(); it != c->end(); ++it)
+  {
+    if (it->second != NULL)
+    {
+      delete it->second;
+      it->second = NULL;
+    }
+  }
+}
+
 #endif
 
