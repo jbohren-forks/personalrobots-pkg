2a2634182c95d64b328446333703705256d9f5ef
sachinchitta <sachinchitta@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-21 08:07:41 +0000

    Added xml parsing tests
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3407 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ controllers/pr2_controllers/test/test_base_controller.cpp
@@ -0,0 +1,39 @@
+
+#include <pr2_controllers/base_controller.h>
+#include <mechanism_model/robot.h>
+
+#include <ros/node.h>
+
+int main( int argc, char** argv )
+{
+  mechanism::Robot *robot = new mechanism::Robot("r2d2");
+  controller::BaseController bc;
+
+  if(argc != 3)
+  {
+    printf("Usage: %s <robot_xml> <controller_xml>\n",argv[0]);
+    exit(-1);
+  }
+
+  ros::init(argc,argv);
+  ros::node *node = new ros::node("test_base_controller"); //Can be found by ros::node *ros::g_node for now.  Will eventually be superceded by ros::peer interface
+
+  printf("robot file:: %s, controller file:: %s\n",argv[1],argv[2]);
+  char *xml_robot_file = argv[1];
+  TiXmlDocument xml(xml_robot_file);   // Load robot description
+  xml.LoadFile();
+  TiXmlElement *root = xml.FirstChildElement("robot");
+  robot->initXml(root);
+  
+
+  char *xml_control_file = argv[2];
+  TiXmlDocument xml_control(xml_control_file);   // Load robot description
+  xml_control.LoadFile();
+  TiXmlElement *root_control = xml_control.FirstChildElement("robot");
+  TiXmlElement *root_controller = root_control->FirstChildElement("controller");
+
+  bc.initXml(robot,root_controller);
+  ros::fini();
+
+//void BaseController::initXml(mechanism::Robot *robot, TiXmlElement *config)
+}
