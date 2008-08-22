dfa158226daf3180132b021632f95fe708628107
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-22 19:58:00 +0000

    adding matrix quaternion test to gtest
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3487 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/libTF/test/pose3d_unittest.cpp
+++ util/libTF/test/pose3d_unittest.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 #include <libTF/Pose3D.h>
+#include <math_utils/angles.h>
+#include <sys/time.h>
+#include <cstdlib>
 
 using namespace libTF;
 
@@ -44,6 +47,52 @@ TEST(Pose3D, EulerConversions){
   testEulerConversion(0, 0, 3.14);
   testEulerConversion(0, 0, -3.14);
 }
+
+TEST(Pose3D, MatrixToQuaternionAndBack)
+{
+  //Seed random number generator with current microseond count
+  timeval temp_time_struct;
+  gettimeofday(&temp_time_struct,NULL);
+  srand(temp_time_struct.tv_usec);
+
+  libTF::Pose3D aPose;
+
+  int i_max = 100;
+
+  //Test many different premutations
+  for (int i = -i_max; i < i_max; i++)
+  {
+
+    double yaw, pitch, roll;
+    yaw = 2* M_PI * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
+    pitch = 2 * M_PI * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
+    roll = 2 *M_PI * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
+    
+      
+    NEWMAT::Matrix m = libTF::Pose3D::matrixFromEuler(0.0,
+                                                      0.0,
+                                                      0.0,
+                                                      yaw,
+                                                      pitch,
+                                                      roll);
+      
+    //          std::cout << m <<std::endl;
+    aPose.setFromMatrix(m);
+    m = aPose.asMatrix();
+          
+    libTF::Pose3D::Euler out = libTF::Pose3D::eulerFromMatrix(m);
+    libTF::Pose3D::Euler out2 = libTF::Pose3D::eulerFromMatrix(m,2);
+
+    //Test the difference between input and output accounting for 2Pi redundancy.  
+    bool difference = ((fabs(math_utils::modNPiBy2(out.yaw) - math_utils::modNPiBy2(yaw)) > 0.001 || fabs(math_utils::modNPiBy2(out.pitch) - math_utils::modNPiBy2(pitch)) > 0.001 || fabs(math_utils::modNPiBy2(out.roll) -math_utils::modNPiBy2(roll)) > 0.0001) &&
+                       (fabs(math_utils::modNPiBy2(out2.yaw) - math_utils::modNPiBy2(yaw)) > 0.001 || fabs(math_utils::modNPiBy2(out2.pitch) - math_utils::modNPiBy2(pitch)) > 0.001 || fabs(math_utils::modNPiBy2(out2.roll) -math_utils::modNPiBy2(roll)) > 0.0001));
+          
+    ASSERT_FALSE(difference);
+  }
+
+}
+
+
 
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
