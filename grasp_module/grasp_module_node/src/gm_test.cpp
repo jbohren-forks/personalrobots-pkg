d850f35108dbd3dab71aaf4cf4af749184f39add
cmatei18 <cmatei18@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-22 23:52:12 +0000

    Added license headers.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3518 f5854215-dd47-0410-b2c4-cdd35faa7885

--- grasp_module/grasp_module_node/src/gm_test.cpp
+++ grasp_module/grasp_module_node/src/gm_test.cpp
@@ -1,8 +1,45 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* author: Matei Ciocarlie */
+
 #include <ros/node.h>
 #include <smartScan.h>
 #include <fstream>
 #include "grasp_module/object_detector_srv.h"
 #include "grasp_module/grasp_planner_srv.h"
+
+/*! \file This little executable testst the behavior of the
+    grasp_module_node. Start the grasp_module_node, then fire this
+    guy. As an argument, pass it a file that can be read in by a
+    SmartScan::readFromFile(...)
+ */
 
 class Service : public ros::node
 {
@@ -21,11 +58,6 @@ public:
 	
 };
 
-/*! This little executable testst the behavior of the
-    grasp_module_node. Start the grasp_module_node, then fire this
-    guy. As an argument, pass it a file that can be read in by a
-    SmartScan::readFromFile(...)
- */
 int main(int argc, char **argv)
 {
 	if (argc < 2) {
