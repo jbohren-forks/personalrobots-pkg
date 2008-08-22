c61f14614b139065a944df61a42350881a8e51c5
isucan <isucan@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-22 02:14:19 +0000

    added license + few more functions
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3465 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/string_utils/src/string_utils.cpp
+++ util/string_utils/src/string_utils.cpp
@@ -1,3 +1,37 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ * 
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ * 
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
 #include <string_utils/string_utils.h>
 
 void string_utils::split(const std::string &s, std::vector<std::string> &t, const std::string &d)
@@ -37,18 +71,23 @@ void string_utils::split(const std::string &str, std::vector<std::string> &token
 
 std::string string_utils::trim(const std::string &str)
 {
-    std::string res = str;
-    
-    while (!res.empty() && (res[res.size() - 1] == ' ' || 
-			    res[res.size() - 1] == '\t' || 
-			    res[res.size() - 1] == '\n' || 
-			    res[res.size() - 1] == '\r'))
-	res.erase(res.size() - 1); 
-    
-    while (!res.empty() && (res[0] == ' ' || 
-			    res[0] == '\t' || 
-			    res[0] == '\n' || 
-			    res[0] == '\r'))
-	res.erase(0, 1); 
-    return res;
+    std::string tmp = str;
+    tmp = tmp.erase(tmp.find_last_not_of(" \t\r\n") + 1);
+    return tmp.erase(0, tmp.find_first_not_of(" \t\r\n"));
+}
+
+std::string string_utils::tolower(const std::string& source)
+{
+    std::string result = source;
+    for (unsigned int i = 0 ; i < result.size() ; i++)
+	result[i] = ::tolower(result[i]);
+    return result;    
+}
+
+std::string string_utils::toupper(const std::string& source)
+{
+    std::string result = source;
+    for (unsigned int i = 0 ; i < result.size() ; i++)
+	result[i] = ::toupper(result[i]);
+    return result;    
 }
