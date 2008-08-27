1ee9b347321daa7c09709da541c2ceed8ea1538f
stuglaser <stuglaser@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-27 23:43:56 +0000

    Created RealtimePublisher, a class for publishing messages over ROS from inside the realtime loop.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3724 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ util/misc_utils/include/misc_utils/realtime_publisher.h
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Publishing ROS messages is difficult, as the publish function is
+ * not realtime safe.  This class provides the proper locking so that
+ * you can call publish in realtime and a separate (non-realtime)
+ * thread will ensure that the message gets published over ROS.
+ *
+ * Author: Stuart Glaser
+ */
+#ifndef REALTIME_PUBLISHER_H
+#define REALTIME_PUBLISHER_H
+
+#include <pthread.h>
+#include <string>
+#include <rosthread/mutex.h>
+#include <rosthread/member_thread.h>
+
+template <class Msg>
+class RealtimePublisher
+{
+public:
+  RealtimePublisher(const std::string &topic, int queue_size)
+    : topic_(topic), node_(NULL), is_running_(false), keep_running_(false), thread_(NULL)
+  {
+    if ((node_ = ros::node::instance()) == NULL)
+    {
+      int argc = 0;  char **argv = NULL;
+      ros::init(argc, argv);
+      node_ = new ros::node("realtime_publisher", ros::node::DONT_HANDLE_SIGINT);
+    }
+
+    node_->advertise<Msg>(topic_, queue_size);
+
+    pthread_cond_init(&updated_cond_, NULL);
+    pthread_mutex_init(&msg_lock_, NULL);
+    keep_running_ = true;
+    thread_ = ros::thread::member_thread::startMemberFunctionThread<RealtimePublisher<Msg> >
+      (this, &RealtimePublisher::publishingLoop);
+  }
+
+  ~RealtimePublisher()
+  {
+  }
+
+  void stop()
+  {
+    keep_running_ = false;
+  }
+
+  void publish(const Msg &msg)
+  {
+    if (0 == pthread_mutex_trylock(&msg_lock_))
+    {
+      msg_ = msg;
+      msg_updated_ = true;
+      pthread_mutex_unlock(&msg_lock_);
+      pthread_cond_signal(&updated_cond_);
+    }
+  }
+
+  bool is_running() const { return is_running_; }
+
+  void publishingLoop()
+  {
+    is_running_ = true;
+    while (keep_running_)
+    {
+      // Locks msg_ and copies it
+      pthread_mutex_lock(&msg_lock_);
+      while (!msg_updated_)
+        pthread_cond_wait(&updated_cond_, &msg_lock_);
+      Msg outgoing(msg_);
+      msg_updated_ = false;
+      pthread_mutex_unlock(&msg_lock_);
+
+      // Sends the outgoing message
+      node_->publish(topic_, outgoing);
+    }
+    is_running_ = false;
+  }
+
+private:
+  std::string topic_;
+  ros::node *node_;
+  bool is_running_;
+  bool keep_running_;
+
+  pthread_t *thread_;
+
+  Msg msg_;
+  bool msg_updated_;
+  pthread_mutex_t msg_lock_;  // Protects msg_
+  pthread_cond_t updated_cond_;
+};
+
+#endif
