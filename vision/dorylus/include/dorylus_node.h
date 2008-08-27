f7721d6fd489a44155cc1f28b8a7e55daae3f6cb
alexteichman <alexteichman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-27 07:13:40 +0000

    Whoops, checked in before I should have.  This will keep the build working until I can fix.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3679 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/dorylus/include/dorylus_node.h
+++ vision/dorylus/include/dorylus_node.h
@@ -25,6 +25,11 @@ class Descriptor
     return false;
   }
 
+  virtual void display(const NEWMAT::Matrix& result)
+  {
+    cout << name << " has no display function yet." << endl;
+  }
+
 };
 
 
@@ -43,10 +48,57 @@ class FixedSpinMedium : public Descriptor
 {
 
  public:
+  float support_;
+  float pixelsPerMeter_;
+  int height_, width_;
+  scan_utils::Grid2D *psi_;
+
   FixedSpinMedium()
     {
       name = string("FixedSpinMedium");
+      support_ = .05;  //.1
+      pixelsPerMeter_ = 200;
+
+      psi_ = new scan_utils::Grid2D(ceil(2*support_*pixelsPerMeter_), ceil(support_*pixelsPerMeter_));
+      psi_->getSize(height_, width_);
     }
 
+  ~FixedSpinMedium()
+    {
+      delete psi_;
+    }
+    
+
   bool operator()(SmartScan &ss, const IplImage &img, float x, float y, float z, int row, int col, NEWMAT::Matrix** result, bool debug=false);
+  void display(const NEWMAT::Matrix& result);
+};
+
+
+class FixedSpinLarge : public Descriptor
+{
+
+ public:
+  float support_;
+  float pixelsPerMeter_;
+  int height_, width_;
+  scan_utils::Grid2D *psi_;
+
+  FixedSpinLarge()
+    {
+      name = string("FixedSpinLarge");
+      support_ = .20;
+      pixelsPerMeter_ = 50;
+
+      psi_ = new scan_utils::Grid2D(ceil(2*support_*pixelsPerMeter_), ceil(support_*pixelsPerMeter_));
+      psi_->getSize(height_, width_);
+    }
+
+  ~FixedSpinLarge()
+    {
+      delete psi_;
+    }
+    
+
+  bool operator()(SmartScan &ss, const IplImage &img, float x, float y, float z, int row, int col, NEWMAT::Matrix** result, bool debug=false);
+  void display(const NEWMAT::Matrix& result);
 };
--- vision/dorylus/src/dorylus_node.cpp
+++ vision/dorylus/src/dorylus_node.cpp
@@ -43,20 +43,23 @@ public:
 
 	for(unsigned int iDesc=0; iDesc<descriptors_.size(); iDesc++) {
 	  cout << "Computing " << descriptors_[iDesc]->name << endl;
-	  sls.getRandomPoint(&x, &y, &z, &row, &col);
+	  x=0; y=0; z=0;
+	  sls.getRandomPointFromImage(&x, &y, &z, &row, &col);
 
 	  (*descriptors_[iDesc])(ss, img, x, y, z, row, col, &result); 
 	  //cout << "nrows: " << tmp.Nrows() << endl;
 	  Matrix feat(result->Nrows(), nSamples); feat = 0.0;
 	  for(unsigned int iSample=0; iSample<nSamples; iSample++) {
-	    sls.getRandomPoint(&x, &y, &z, &row, &col, &ss); //Gets a point from the object.
-
+	    
+	    //if(!sls.getRandomPointFromImage(&x, &y, &z, &row, &col, obj.label)) { //Gets a point from the object.
+	      
 
 	    IplImage *copy;
 	    std_msgs::VisualizationMarker mark;
 	    if(debug) {
 	      publish("videre/cloud", sls.cloud_);
 	      copy = cvCloneImage(sls.left_);
+	      cvNamedWindow("left", 1);
 	      cvCircle(copy, cvPoint(row, col), 5, cvScalar(0,255,0), 1);
 	      cvShowImage("left", copy);
 	      cvWaitKey(50);
@@ -84,7 +87,7 @@ public:
 	    // -- Compute the descriptor.
 	    //If debug, this will wait.
 	    result = NULL;
-	    bool success = (*descriptors_[iDesc])(sls.ss_cloud_, img, x, y, z, row, col, &result, debug);
+	    (*descriptors_[iDesc])(sls.ss_cloud_, img, x, y, z, row, col, &result, debug);
 	    feat.Column(iSample+1) = *result;
 	    //TODO: Mark down which descriptors are successful.
 	    
@@ -122,58 +125,65 @@ private:
 
 
 bool FixedSpinMedium::operator()(SmartScan &ss, const IplImage &img, float x, float y, float z, int row, int col, Matrix** result, bool debug) {
-  //cout << "Computing " << name << endl;
-  float support = .20;
-  float pixelsPerMeter = 250;
-  int height, width;
 
-  scan_utils::Grid2D *psi = new scan_utils::Grid2D(ceil(2*support*pixelsPerMeter), ceil(support*pixelsPerMeter));
-  psi->getSize(height, width);
+  // -- Reset psi_.
+  for(int i=0; i<width_; i++) {
+    for(int j=0; j<height_; j++) {
+      psi_->setElement(j,i, 0.0);
+    }
+  }
+ 
 
-  Matrix* res = new Matrix(height*width,1); (*res) = 0.0;
-  bool si_success = ss.computeSpinImageFixedOrientationSmallv(*psi, x, y, z, support, pixelsPerMeter);
+  Matrix* res = new Matrix(height_*width_,1); (*res) = 0.0;
+  bool si_success = ss.computeSpinImageFixedOrientation(*psi_, x, y, z, support_, pixelsPerMeter_, string("-y"));
   if(!si_success) {
     *result = res;
     return false;
   }
 
-  cout << "si is " << height << " x " << width << endl;
-  for(int i=0; i<width; i++) {
-    for(int j=0; j<height; j++) {
-      (*res)(i*height+j+1,1) = (float)psi->getElement(j,i);
+  cout << "si is " << height_ << " x " << width_ << endl;
+  for(int i=0; i<width_; i++) {
+    for(int j=0; j<height_; j++) {
+      (*res)(i*height_+j+1,1) = (float)psi_->getElement(j,i);
     }
   }
 
-  if(debug) {
-    //Display the spin image with gnuplot.
-    char cmd[] = "echo \'set pm3d map; set size ratio -1; splot \"gnuplot_tmp\"\' | gnuplot -persist";
-    char filename[] = "gnuplot_tmp";
-    ofstream f(filename); 
-    if(!f.is_open()) {
-      cerr << "Could not open temporary file." << endl;
-    }
-
-    for(int h=height-1; h>-1; h--) {
-      for(int w=0; w<width; w++) {
-	f << psi->getElement(h, w) << endl;
-      }
-      f << endl;
-    }
-    // system("killall gnuplot");
-    f.close();
-    system(cmd);
-    remove(filename);
-
-    cout << "Press Enter to continue. . .\n";
-    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    system("killall gnuplot");
-  }
-  delete psi;
   // -- Normalize.
   Matrix div(1,1); div = 1/res->MaximumAbsoluteValue();
   *res = KP(*res, div);
   *result = res;
+
+  if(debug) {
+    display(*res);
+  }
+    
   return true;
+}
+
+
+void FixedSpinMedium::display(const Matrix& result) {
+  //Display the spin image with gnuplot.
+  char cmd[] = "echo \'set pm3d map; set size ratio -1; splot \"gnuplot_tmp\"\' | gnuplot -persist";
+  char filename[] = "gnuplot_tmp";
+  ofstream f(filename); 
+  if(!f.is_open()) {
+    cerr << "Could not open temporary file." << endl;
+  }
+
+  for(int h=height_-1; h>-1; h--) {
+    for(int w=0; w<width_; w++) {
+      //f << psi_->getElement(h, w) << endl;
+      f << result(h+w*height_+1, 1) << endl;
+    }
+    f << endl;
+  }
+  f.close();
+  system(cmd);
+  remove(filename);
+
+  cout << "Press Enter to continue. . .\n";
+  cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  system("killall gnuplot");
 }
 
 
@@ -182,12 +192,19 @@ int main(int argc, char **argv) {
   ros::init(argc, argv);
   DorylusNode d;
 
+  if(argc > 2 && !strcmp(argv[1], "--dataset")) {
+
   vector<string> datafiles;
-  for(int i=1; i<argc; i++) {
+  for(int i=2; i<argc; i++) {
     datafiles.push_back(string(argv[i]));
   }
 
   d.buildDataset(2, datafiles, string("savename.dd"), true);
+  }
+  else {
+    cout << "Usage: " << endl;
+    cout << "  dorylus_node --dataset [BAGFILES]" << endl;
+  }
 
   usleep(250000);
   ros::fini();
