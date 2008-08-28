24453cd252bcae1eb4b110c388d83efe8bc0e37d
hsujohnhsu <hsujohnhsu@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-28 17:35:27 +0000

    updates to use -Wl and rpath in ogre. clean up setup.bash for gazebo launch script.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3750 f5854215-dd47-0410-b2c4-cdd35faa7885

--- 3rdparty/gazebo/setup.bash
+++ 3rdparty/gazebo/setup.bash
@@ -1,17 +1,12 @@
 #!/bin/bash
 export GAZ_TOP=`rospack find gazebo`/gazebo
 export OGRE_TOP=`rospack find ogre`/ogre
-export CG_TOP=`rospack find Cg`/Cg
 export SIM_PLUGIN=`rospack find gazebo_plugin`
 export PR2MEDIA=`rospack find gazebo_robot_description`/world
 
 
 export LD_LIBRARY_PATH=$SIM_PLUGIN/lib
 export LD_LIBRARY_PATH=$GAZ_TOP/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$OGRE_TOP/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$OGRE_TOP/lib/OGRE:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$CG_TOP/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$SIM_PLUGIN/lib:$LD_LIBRARY_PATH
 export PATH=$GAZ_TOP/bin:$PATH
 
 export GAZEBO_RESOURCE_PATH=$PR2MEDIA
