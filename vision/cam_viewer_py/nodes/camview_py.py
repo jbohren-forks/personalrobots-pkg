a3cb48560c01a390449504d3c9b8328b6831e667
sfkwc <sfkwc@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-28 22:37:36 +0000

    cleaned up nodes to better conform to python style
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3767 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/cam_viewer_py/nodes/camview_py.py
+++ vision/cam_viewer_py/nodes/camview_py.py
@@ -1,12 +1,47 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the Willow Garage nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import rostools
-rostools.updatePath('cam_viewer_py')
+rostools.update_path('cam_viewer_py')
+
+import sys
+import time
+
 from std_msgs.msg import Image
 from std_msgs.msg import ImageArray
 import rospy
 import pyrob.util as ut
 import opencv.highgui as hg
-import sys
-import time
 
 def display_array(iar):
     left  = ut.ros2cv(iar.images[0])
@@ -21,81 +56,11 @@ def display_single(im):
     hg.cvShowImage('channel 1', imcv)
     hg.cvWaitKey(5)
 
-hg.cvNamedWindow('channel 1', 1)
-hg.cvNamedWindow('channel 2', 1)
-rospy.TopicSub('images', ImageArray, display_array)
-rospy.TopicSub('image', Image, display_single)
-rospy.ready(sys.argv[0])
-rospy.spin()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#bottle = message_bottle()
-#class message_bottle:
-#    def __init__(self):
-#        self.message = None
-#
-#    def callback(self, iar):
-#        l = iar.images[0]
-#        r = iar.images[1]
-#        #print l.width, l.height, l.compression, l.colorspace, len(l.data), l.data.__class__
-#        #print dir(l.data)
-#        self.message = iar
-#
-#    def get_message(self):
-#        return self.message
-#
-#    def empty(self):
-#        return self.message == None
-#
-#
-#
-#print 'hready'
-#while bottle.empty():
-#    time.sleep(.033)
-#    print 'waiting...'
-#    pass
-
+if __name__ == '__main__':
+    hg.cvNamedWindow('channel 1', 1)
+    hg.cvNamedWindow('channel 2', 1)
+    rospy.TopicSub('images', ImageArray, display_array)
+    rospy.TopicSub('image', Image, display_single)
+    rospy.ready('camview_py')
+    rospy.spin()
 
