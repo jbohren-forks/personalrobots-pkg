80e7499aaa26409f1c160f2f39c24fa4acb5ab0d
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-08-30 00:16:21 +0000

    r7830@zug (orig r298):  vernaza | 2008-08-26 15:07:53 -0700  Adding the ROS classifier node
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3842 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ vision/mrf_image_classifier/src/ros/mrf_image_classifier.cpp
@@ -0,0 +1,104 @@
+#include <cv.h>
+#include <highgui.h>
+#include <std_msgs/ImageArray.h>
+#include <ros/node.h>
+
+#include "classifier/NaryImageClassifier.hh"
+#include "display/LabelingViewer.hh"
+#include "image_utils/cv_bridge.h"
+
+class ROSImageClassifier : public ros::node
+{
+public:
+  /**
+     @param trainingFile Name of the file containing classifier state
+   */
+  ROSImageClassifier(const char* trainingFile, std::istream& objectsFile) :
+    ros::node("mrf_image_classifier"),
+    bridgeIn(NULL),
+    currFrame(NULL),
+    segmented(NULL),
+    cTree(NULL),
+    classifier(NULL),
+    objectSet(objectsFile)
+  {
+    // initialize classifier 
+    ifstream tFileStream(trainingFile);
+    cTree = BinaryClassTreeUtils::deserialize(tFileStream);
+    classifier = new NaryImageClassifier(cTree);
+  
+    subscribe("videre/images", images, 
+	      &ROSImageClassifier::processFrame, this, 1);
+  }
+
+  virtual ~ROSImageClassifier() {
+    delete bridgeIn;
+    delete cTree;
+    delete classifier;
+
+    if (currFrame != NULL)
+      cvReleaseImage(&currFrame);
+
+    if (segmented != NULL)
+      cvReleaseImage(&segmented);
+  }
+
+  void processFrame() {
+    if (bridgeIn == NULL)
+      bridgeIn = new CvBridge<std_msgs::Image>
+	(&images.images[1],
+	 CvBridge<std_msgs::Image>::CORRECT_BGR | 
+	 CvBridge<std_msgs::Image>::MAXDEPTH_8U
+	 );
+
+    if (currFrame != NULL)
+      cvReleaseImage(&currFrame);
+
+    bridgeIn->to_cv(&currFrame);
+      
+    if (segmented == NULL)
+      segmented = cvCreateImage(cvGetSize(currFrame), IPL_DEPTH_32S, 3);
+
+    vector<int> labeling;
+    vector<blobStat> blobStats;
+    classifier->evaluate(currFrame, segmented, labeling, blobStats);
+
+    LabelingViewer::viewLabeling("Segmented", 3, 
+				 currFrame, segmented, 
+				 objectSet,
+				 labeling, blobStats);
+
+    cvNamedWindow("testing", 0);
+    cvShowImage("testing", currFrame);
+    cvWaitKey(50);
+  }
+
+private:
+  CvBridge<std_msgs::Image> *bridgeIn;
+  std_msgs::ImageArray images;
+
+  IplImage *currFrame;
+  IplImage *segmented;
+  
+  BinaryClassifierTree* cTree;
+  NaryImageClassifier* classifier;
+  ObjectSet objectSet;
+};
+
+int main(int argc, char* argv[])
+{
+  ros::init(argc, argv);
+
+  assert(argc > 2);
+  char* trainingFile = argv[1];
+  char* objectsFile = argv[2];
+  
+  ifstream objStream(objectsFile);
+
+  ROSImageClassifier classifier(trainingFile, objStream);
+  
+  objStream.close();
+
+  while(1)
+    sleep(1);
+}
