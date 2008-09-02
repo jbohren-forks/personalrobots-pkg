65de66f7fd6eb3aca64f5dcea1a8677eeeac798c
jdchen <jdchen@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-09-02 18:29:06 +0000

    initial import
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3867 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ 3rdparty/opencv/wgext/3DPoseEstimation/include/CvPathRecon.h
@@ -0,0 +1,20 @@
+/*
+ * CvPathRecon.h
+ *
+ *  Created on: Sep 2, 2008
+ *      Author: jdchen
+ */
+
+#ifndef CVPATHRECON_H_
+#define CVPATHRECON_H_
+
+/**
+ * Reconstruction of trajectory from stereo pairs of videos
+ */
+class CvPathRecon {
+public:
+	CvPathRecon();
+	virtual ~CvPathRecon();
+};
+
+#endif /* CVPATHRECON_H_ */
---
+++ 3rdparty/opencv/wgext/3DPoseEstimation/src/CvPathRecon.cpp
@@ -0,0 +1,17 @@
+/*
+ * CvPathRecon.cpp
+ *
+ *  Created on: Sep 2, 2008
+ *      Author: jdchen
+ */
+
+#include "CvPathRecon.h"
+
+CvPathRecon::CvPathRecon() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CvPathRecon::~CvPathRecon() {
+	// TODO Auto-generated destructor stub
+}
