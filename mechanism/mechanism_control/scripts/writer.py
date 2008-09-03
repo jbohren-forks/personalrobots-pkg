2646c0983e7b8b05a1c0abbc42e4f005f8d55332
david_li <david_li@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-09-03 20:54:47 +0000

    Added a python script that dumps incoming messages to file
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@3901 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ mechanism/mechanism_control/scripts/writer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import rostools
+rostools.update_path('mechanism_control')
+
+import sys, traceback, logging, rospy
+from mechanism_control.msg import MechanismState
+
+print "File name is: %s" %sys.argv[1]
+filehandle = open(sys.argv[1],'w')
+
+NAME = 'joint_writer'
+
+def callback(data):
+    for a in data.actuator_states:
+      filehandle.write("%.4f " %a.timestamp)
+    for j in data.joint_states:
+      filehandle.write("%.4f " %j.position)
+      filehandle.write("%.4f " %j.velocity)
+      filehandle.write("%.4f " %j.commanded_effort)
+      filehandle.write("\n")
+   
+
+def listener_with_user_data():
+    rospy.TopicSub("/mechanism_state", MechanismState, callback)
+    rospy.ready(NAME, anonymous=True)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        listener_with_user_data()
+    except KeyboardInterrupt, e:
+        pass
+    print "exiting"
+
