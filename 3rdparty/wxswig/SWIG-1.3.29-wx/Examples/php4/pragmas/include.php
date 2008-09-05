dbcedfe612a26ee4e4adb82867eecaa08109fb5b
jfaustwg <jfaustwg@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-09-05 21:47:36 +0000

    Remove Examples dir from wxswig, since that's what took most of the space
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@4002 f5854215-dd47-0410-b2c4-cdd35faa7885

--- 3rdparty/wxswig/SWIG-1.3.29-wx/Examples/php4/pragmas/include.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-# This code is inserted into example.php
-echo "this is hi.php\n";
-
-
-?>
--- 3rdparty/wxswig/SWIG-1.3.29-wx/Examples/test-suite/tcl/profiletest_runme.tcl
+++
@@ -1,9 +0,0 @@
-catch { load ./profiletest.so profiletest}
-catch { load ./profiletest.dll profiletest}    ;# Windows
-
-set  a [new_A]
-set  b [new_B]
-
-for {set i 0} {$i < 1000000} {incr i 1} {
-    set a [B_fn $b $a]
-}
