43e8e53f89dbcfdf2f1e0fbf397d5321030f62d1
tpratkanis <tpratkanis@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-09-05 22:00:34 +0000

    Migrate tests to new directory.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@4004 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ highlevel/executive_trex/executive_trex_pr2/test/runtesttrex.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+cd `rospack find executive_trex_pr2`
+
+
+mkdir $1
+export TREX_LOG_DIR=$1
+sleep 5
+
+cd $2
+`rospack find executive_trex_pr2`/build/trex_fast `rospack find executive_trex_pr2`/cfg/pr2.cfg
