ff958fe905d1b3db8e2dda188565b59e64193e72
david_li <david_li@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-09-10 22:56:38 +0000

    Added functions to communicate with laser scanner controller
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@4158 f5854215-dd47-0410-b2c4-cdd35faa7885

--- controllers/generic_controllers/src/generic_controllers/controllers.py
+++ controllers/generic_controllers/src/generic_controllers/controllers.py
@@ -14,7 +14,8 @@ def print_usage(exit_code = 0):
     ls                         - List controllers
     set <controller> <command> - Set the controller's commanded value
     get <controller>           - Get the controller's commanded value
-    profile <controller> <mode> <period> <amplitude> <offset> - Set profile 3 for sawtooth 4 for sinewave'''
+    profile <controller> <upper turnaround offset> <lower turnaround offset> <upper decel buffer> <lower decel bufer>
+                               - Define how far away from joint limit to turn around. Buffers indicate how far from that point to start decelerating. Set to 0 to disable'''
     sys.exit(exit_code)
 
 def list_controllers():
@@ -33,10 +34,21 @@ def get_controller(controller):
     resp = s.call(GetActualRequest())
     print str(resp.time) + ": " + str(resp.command)
 
-def set_profile(controller, profile,period,amplitude,offset):
+def set_position(controller, command):
+    s = rospy.ServiceProxy(controller + '/set_position', SetPosition)
+    resp = s.call(SetPositionRequest(command))
+    print resp.command
+
+def get_position(controller):
+    s = rospy.ServiceProxy(controller + '/get_position', GetPosition)
+    resp = s.call(GetPositionRequest())
+    print str(resp.time) + ": " + str(resp.command)
+
+def set_profile(controller, upper_turnaround, lower_turnaround, upper_decel_buffer, lower_decel_buffer):
     s = rospy.ServiceProxy(controller + '/set_profile', SetProfile)
-    resp = s.call(SetProfileRequest(profile,period,amplitude,offset))
+    resp = s.call(SetProfileRequest(upper_turnaround, lower_turnaround, upper_decel_buffer, lower_decel_buffer))
     print str(resp.time)
+
 
 
 if __name__ == '__main__':
@@ -53,6 +65,10 @@ if __name__ == '__main__':
           print_usage()
         get_controller(sys.argv[2])
     elif sys.argv[1] == 'profile':
-        set_profile(sys.argv[2],int(sys.argv[3]),float(sys.argv[4]),float(sys.argv[5]),float(sys.argv[6]))
+        set_profile(sys.argv[2],float(sys.argv[3]),float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]))
+    elif sys.argv[1] == 'setPosition':
+        set_position(sys.argv[2],float(sys.argv[3]))
+    elif sys.argv[1] == 'getPosition':
+        get_position(sys.argv[2])
     else:
         print_usage(1)
