d6757a13733e092470a070e8cc8ef24eb9adff4d
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-09-10 21:06:33 +0000

    a new package for the new version of transforms, with a very simple bullet math test.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@4150 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ util/tf/simpletest.cpp
@@ -0,0 +1,27 @@
+#include "LinearMath/btQuaternion.h"
+#include "LinearMath/btTransform.h"
+
+#include <sys/time.h>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+  btQuaternion quat;
+  quat.setEuler(0,1,1);
+  cout << quat.x() << std::endl << quat.y() << std::endl << quat.z() << std::endl << quat.w() << std::endl;
+  quat.setEuler(0,1,0);
+  cout << quat.x() << std::endl << quat.y() << std::endl << quat.z() << std::endl << quat.w() << std::endl;
+  quat.setEuler(0,0,1);
+  cout << quat.x() << std::endl << quat.y() << std::endl << quat.z() << std::endl << quat.w() << std::endl;
+  quat.setEuler(0,1,2);
+  cout << quat.x() << std::endl << quat.y() << std::endl << quat.z() << std::endl << quat.w() << std::endl;
+
+
+  btTransform tran(quat, btVector3(btScalar(1), btScalar(1), btScalar(1)));
+
+  std::cout << sizeof(btScalar)<<std::endl;
+    
+return 0;
+};
