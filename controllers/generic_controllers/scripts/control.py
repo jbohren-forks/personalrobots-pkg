90ac865e1300fe675c1655adceaaba5cff5e0d28
stuglaser <stuglaser@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-09-17 00:59:46 +0000

    Moved the script for controlling joints into the scripts/ directory.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@4370 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ controllers/generic_controllers/scripts/control.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+import rostools
+rostools.update_path('mechanism_control')
+rostools.update_path('generic_controllers')
+
+
+import rospy, sys
+from mechanism_control.srv import *
+from generic_controllers.srv import *
+from generic_controllers import controllers
+
+def print_usage(exit_code = 0):
+    print '''Commands:
+    ls                         - List controllers
+    set <controller> <command> - Set the controller's commanded value
+    get <controller>           - Get the controller's commanded value
+    profile <controller> <upper turnaround offset> <lower turnaround offset> <upper decel buffer> <lower decel bufer>
+                               - Define how far away from joint limit to turn around. Buffers indicate how far from that point to start decelerating. Set to 0 to disable'''
+    sys.exit(exit_code)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print_usage()
+    if sys.argv[1] == 'ls':
+        controllers.list_controllers()
+    elif sys.argv[1] == 'set':
+        if len(sys.argv) != 4:
+          print_usage()
+        controllers.set_controller(sys.argv[2], float(sys.argv[3]))
+    elif sys.argv[1] == 'get':
+        if len(sys.argv) != 3:
+          print_usage()
+        controllers.get_controller(sys.argv[2])
+    elif sys.argv[1] == 'profile':
+        controllers.set_profile(sys.argv[2],float(sys.argv[3]),float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]))
+    elif sys.argv[1] == 'setPosition':
+        controllers.set_position(sys.argv[2],float(sys.argv[3]))
+    elif sys.argv[1] == 'getPosition':
+        controllers.get_position(sys.argv[2])
+    else:
+        print_usage(1)
--- controllers/generic_controllers/src/generic_controllers/controllers.py
+++ controllers/generic_controllers/src/generic_controllers/controllers.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python
-
 import rostools
 rostools.update_path('mechanism_control')
 rostools.update_path('generic_controllers')
@@ -8,15 +6,6 @@ rostools.update_path('generic_controllers')
 import rospy, sys
 from mechanism_control.srv import *
 from generic_controllers.srv import *
-
-def print_usage(exit_code = 0):
-    print '''Commands:
-    ls                         - List controllers
-    set <controller> <command> - Set the controller's commanded value
-    get <controller>           - Get the controller's commanded value
-    profile <controller> <upper turnaround offset> <lower turnaround offset> <upper decel buffer> <lower decel bufer>
-                               - Define how far away from joint limit to turn around. Buffers indicate how far from that point to start decelerating. Set to 0 to disable'''
-    sys.exit(exit_code)
 
 def list_controllers():
     s = rospy.ServiceProxy('list_controllers', ListControllers)
@@ -49,26 +38,3 @@ def set_profile(controller, upper_turnaround, lower_turnaround, upper_decel_buff
     resp = s.call(SetProfileRequest(upper_turnaround, lower_turnaround, upper_decel_buffer, lower_decel_buffer))
     print str(resp.time)
 
-
-
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        print_usage()
-    if sys.argv[1] == 'ls':
-        list_controllers()
-    elif sys.argv[1] == 'set':
-        if len(sys.argv) != 4:
-          print_usage()
-        set_controller(sys.argv[2], float(sys.argv[3]))
-    elif sys.argv[1] == 'get':
-        if len(sys.argv) != 3:
-          print_usage()
-        get_controller(sys.argv[2])
-    elif sys.argv[1] == 'profile':
-        set_profile(sys.argv[2],float(sys.argv[3]),float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]))
-    elif sys.argv[1] == 'setPosition':
-        set_position(sys.argv[2],float(sys.argv[3]))
-    elif sys.argv[1] == 'getPosition':
-        get_position(sys.argv[2])
-    else:
-        print_usage(1)
