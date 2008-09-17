5b434f67078d507fa4e02f2d6e6e159dfbc0b4e0
fedassa <fedassa@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-09-17 23:39:22 +0000

    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@4409 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/spacetime_stereo/sts_dynamic.cpp
+++ vision/spacetime_stereo/sts_dynamic.cpp
@@ -1,10 +1,4 @@
 
-//#############################################
-//####### SPACE-TIME STEREO code    ###########
-//####### author: Federico Tombari  ###########
-//####### tombari@willowgarage.com  ###########
-//#############################################
-
 #include <stdlib.h>
 #include <math.h>
 #include <ctype.h>
@@ -30,15 +24,18 @@
 #define SUBPIXEL 16
 #define FILTERS
 
-//Radius of spatial window
-#define RADIUS 2
-//Width of temporal window
-#define NIMAGES 40
+#define DYNAMIC
+//#define STATIC
 
-//Horopter (sensor range)
+#define RADIUS 2
 #define MAXDISP 100
 #define XOFFSET 50
+#define NIMAGES 40
 
+#define	PI1S  70
+#define	PI2S  15
+#define	PI1T  20
+#define	PI2T  10
 
 
 struct calib_params{
@@ -269,6 +266,201 @@ class SpacetimeStereoNode : public ros::node
 };
 
 
+
+
+inline int min3(int a, int b, int c){
+return (a<b) ? ((a<c)?a:c) : ((b<c)?b:c);
+}
+
+inline int min4(int a, int b, int c, int d){
+
+int t1 = (a<b)?a:b;
+int t2 = (c<d)?c:d;
+return (t1<t2)?t1:t2;
+
+}
+
+void stereo_SOst(stereo_params par){
+
+	int maxdisp = par.maxdisp;
+	int xoffset = par.xoffset;
+	
+	int r = par.radius;
+	int w = par.w;
+	int h = par.h;
+
+	unsigned char *L = par.L;
+	unsigned char *R = par.R;
+
+	int x,y,i,j,d;
+	int n = 2*r+1;
+	int dbest=0;
+
+	int sqn = n*n;
+	int costmin;
+
+	int **F = (int **)calloc(w, sizeof(int *));
+	int **B = (int **)calloc(w, sizeof(int *));
+	for(x=0; x<w; x++){
+		F[x] = (int *)calloc(maxdisp, sizeof(int));
+		B[x] = (int *)calloc(maxdisp, sizeof(int));
+	}
+	
+	int **acc = (int **)calloc(w, sizeof(int *));
+	int **V = (int **)calloc(w, sizeof(int*));
+	for(d=0; d<w; d++){
+		V[d] = (int *)calloc(maxdisp, sizeof(int)); 
+		acc[d] = (int *)calloc(maxdisp, sizeof(int)); 	
+	}
+	int *tempc = (int *)calloc(maxdisp, sizeof(int));
+
+	int **C = par.mins;
+
+	int pi = PI1S * sqn;
+	int pi2 = PI2S * sqn;
+
+	int pit = PI1T * sqn;
+	int pi2t = PI2T * sqn;
+	
+	//BEGIN SPATIAL 2-PASS DP
+	
+	//FIRST ROW
+	//STAGE 1 - init acc
+	for(d=0; d<maxdisp; d++){
+		for(i=maxdisp; i<maxdisp+n; i++){
+			V[i][d]=0;
+			for(j=0; j<n; j++)
+				V[i][d] += abs( L[j*w+i] - R[j*w+i-d-xoffset] );
+			//acc[d] += V[i][d];			
+		}		
+	}
+	
+	//STAGE 2: other positions
+	for(x=maxdisp+r+1; x<w-r; x++){
+		for(d=0; d<maxdisp; d++){
+			V[x+r][d]=0;
+			for(j=0; j<n; j++)
+				V[x+r][d] += abs( L[j*w + x+r] - R[ j*w + x+r-d-xoffset] );
+			//acc[d] = acc[d] + V[x+r][d] - V[x-r-1][d];
+		}//d
+	}//x
+	unsigned char *lp, *rp, *lpp, *rpp;
+	int ind1 = r+r+maxdisp;
+	
+	for(y=r+1; y<h-r; y++){
+	
+		//first position
+		for(d=0; d<maxdisp; d++){
+			acc[maxdisp+r][d] = 0;
+			for(i=maxdisp; i<maxdisp+n; i++){
+				V[i][d] = V[i][d] + abs( L[ (y+r)*w+i] - R[ (y+r)*w+i-d-xoffset] ) - abs( L[ (y-r-1)*w+i] - R[ (y-r-1)*w+i-d-xoffset] );
+				acc[maxdisp+r][d] += V[i][d];			
+			}	
+		}
+			
+		//compute ACC for all other positions
+		lp = (unsigned char *) L + (y+r)*w + ind1;
+		rp =  (unsigned char *) R + (y+r)*w + ind1 -xoffset;
+		lpp = (unsigned char *) L + (y-r-1)*w + ind1;
+		rpp = (unsigned char *) R + (y-r-1)*w + ind1 -xoffset;
+			
+		for(x=maxdisp+r+1; x<w-r; x++){
+				
+			lp++;
+			rp++;
+			lpp++;
+			rpp++;
+					
+			//int dbestprev = dbest;
+			for(d=0; d<maxdisp; d++){
+					
+				V[x+r][d] = V[x+r][d] + abs( *lp - *rp ) - abs( *lpp - *rpp ); 
+				rp--;
+				rpp--;
+				acc[x][d] = acc[x-1][d] + V[x+r][d] - V[x-r-1][d];
+			}//d
+			rp += maxdisp;
+			rpp += maxdisp;
+		}//x to compute ACC
+
+		//BEGIN FORWARD
+		//BORDO
+		for(d=0; d<maxdisp; d++)
+			F[maxdisp+r][d] = acc[maxdisp+r][d];
+	
+		for(x=maxdisp+r; x<w-r; x++){
+	
+			costmin = F[x-1][0];
+			dbest = 0;
+			for(d=1; d<maxdisp; d++)
+			if(F[x-1][d] < costmin){
+				costmin = F[x-1][d];
+				dbest = d;
+			}
+	
+			F[x][0] =  acc[x][0] - costmin + min3(F[x-1][0], F[x-1][1]+pi2, costmin+pi);
+			for(d=1; d<maxdisp-1; d++){
+				F[x][d] = acc[x][d] - costmin + min4(F[x-1][d], F[x-1][d-1]+pi2, F[x-1][d+1]+pi2, costmin+pi);
+			} //d
+			F[x][maxdisp-1] = acc[x][maxdisp-1] - costmin + min3(F[x-1][maxdisp-1], F[x-1][maxdisp-2]+pi2, costmin+pi);
+		}//x1
+		//END FORWARD
+		//BEGIN BACKWARD
+		//BORDO	
+		for(d=0; d<maxdisp; d++)
+			B[w-1-r][d] = acc[w-1-r][d];
+		
+		for(x=w-2-r; x>=maxdisp+r; x--){
+
+			costmin = B[x+1][0];
+			dbest = 0;
+			for(d=1; d<maxdisp; d++)
+			if(B[x+1][d] < costmin){
+				costmin = B[x+1][d];
+				dbest = d;
+			}
+	
+			B[x][0] =  acc[x][0] - costmin + min3(B[x+1][0], B[x+1][1]+pi2, costmin+pi);
+			for(d=1; d<maxdisp-1; d++){
+				B[x][d] = acc[x][d] - costmin + min4(B[x+1][d], B[x+1][d-1]+pi2, B[x+1][d+1]+pi2, costmin+pi);
+			} //d
+			B[x][maxdisp-1] = acc[x][maxdisp-1] - costmin + min3(B[x+1][maxdisp-1], B[x+1][maxdisp-2]+pi2, costmin+pi);
+			//END BACKWARD
+			//END SPATIAL 2-PASS DP
+
+			costmin = C[y*w+x][0];
+			dbest = 0;
+			for(d=1; d<maxdisp; d++)
+			if(C[y*w+x][d] < costmin){
+				costmin = C[y*w+x][d];
+				dbest = d;
+			}
+			
+			tempc[0] = F[x][0] + B[x][0] + min3(costmin+pit, C[y*w+x][d], C[y*w+x][d+1]+pi2t) - costmin;
+			for(d=1; d<maxdisp-1; d++)
+				tempc[d] = F[x][d] + B[x][d] + min4(costmin+pit, C[y*w+x][d], C[y*w+x][d-1]+pi2t, C[y*w+x][d+1]+pi2t) - costmin;
+			tempc[maxdisp-1] = F[x][maxdisp-1] + B[x][maxdisp-1] + min3(costmin+pit, C[y*w+x][d], C[y*w+x][d-1]+pi2t) - costmin;
+
+			for(d=0; d<maxdisp; d++)
+				C[y*w+x][d] = tempc[d];
+		}//x
+	}//y
+
+	for(x=0; x<w; x++){
+		free(acc[x]);
+		free(F[x]);
+		free(B[x]);
+		free(V[x]);
+	}
+	
+	free(F);
+	free(B);
+	free(V);
+	free(acc);
+	free(tempc);
+}
+
+
 void stereo_standard_sad(stereo_params par){
 
 int maxdisp = par.maxdisp;
@@ -365,6 +557,13 @@ free(acc);
 }
 
 
+//void SpacetimeStereoNode::compute_point_cloud(calib_params cpar, short int* disp, int w, int h){
+
+
+//}
+
+
+
 void SpacetimeStereoNode::spacetime_stereo(vector<IplImage*> left_frames, vector<IplImage*> right_frames, unsigned int nImages, short int* disp, string cal_string){
 
 	par.radius = RADIUS;
@@ -394,9 +593,14 @@ void SpacetimeStereoNode::spacetime_stereo(vector<IplImage*> left_frames, vector
 	disp = (short int *)malloc(w*h*sizeof(short int));
 	par.disp = disp;
 	
+	//Needed for averaging the intensities of the several stereo reference image	
 	unsigned char *L = (unsigned char *)malloc(w*h*sizeof(unsigned char));
 	unsigned char *R = (unsigned char *)malloc(w*h*sizeof(unsigned char));
+	//unsigned char *L, *R;
 
+	
+	//par.Lf = Leftf;
+	//par.Rf = Rightf;
 	par.L = L;
 	par.R = R;
 	
@@ -416,12 +620,21 @@ void SpacetimeStereoNode::spacetime_stereo(vector<IplImage*> left_frames, vector
 	int da, db;
 	#endif
 
+	
 	printf("Channel n: %d %d\n", left_frames[0]->nChannels, right_frames[0]->nChannels);
 	printf("Processing frame ");
 
-	for(framecount = 0; framecount < nImages; framecount ++){
+	int key = 'a';
+	//for(framecount = 0; framecount < nImages; framecount ++){
+	while(1){
 
-		printf("%d\r", framecount);
+// 		cvNamedWindow("L", 1);
+// 		cvNamedWindow("R", 1);
+// 		cvShowImage("L", left_frames[framecount]);
+// 		cvShowImage("R", right_frames[framecount]);
+// 		cvWaitKey(0);
+
+		printf("%d ", framecount);
 		fflush(stdout);
 
 		cvCvtColor(left_frames[framecount], Leftg, CV_BGR2GRAY);
@@ -431,12 +644,13 @@ void SpacetimeStereoNode::spacetime_stereo(vector<IplImage*> left_frames, vector
 		cv2array(Leftg, L);
 		cv2array(Rightg, R);
 		
-		stereo_standard_sad(par);
+		//stereo_standard_sad(par);
+		stereo_SOst(par);
 		
-	} //framecount
-	printf("\n");
+	//} //framecount
+	//printf("\n");
 
-	for(y=r; y<h-r; y++){
+		for(y=r; y<h-r; y++){
 		for(x=maxdisp+r+1; x<w-r; x++){
 			
 			costmin	= mins[y*w+x][0];
@@ -495,9 +709,107 @@ void SpacetimeStereoNode::spacetime_stereo(vector<IplImage*> left_frames, vector
 		if(par.unique == 1)
 			uniqueness_constraint_reducedrange(par, min_scores, y);
 		#endif
-	}//y
+		}//y
 
-	for(d=0; d<w*h;d++)
+		
+		//BEGIN COMPUTING POINT CLOUD
+		printf("Computing Point Cloud; w: %d h:%d ...", w, h);
+		
+		calib_params cpar;
+		
+		int position = cal_string.find("Tx");
+		string substring = cal_string.substr(position, cal_string.find("Ty") - position);
+		sscanf(substring.c_str(), "%*s %f", &cpar.tx);
+		
+		position = cal_string.find("proj");
+		substring = cal_string.substr(position, cal_string.find("rect")-position);
+		sscanf(substring.c_str(), "%*s \n %f %*f %f %*f \n %*f %f %f", &cpar.fx, &cpar.u0, &cpar.fy, &cpar.v0);
+		
+		cpar.feq =  (cpar.fx+cpar.fy) / 2;
+		
+		printf("\nReading Parameters: tx: %f, u0: %f, v0: %f, fx:%f, fy:%f\n", cpar.tx, cpar.u0, cpar.v0, cpar.fx, cpar.fy);
+		
+		std_msgs::PointCloudFloat32 ros_cloud;
+		
+		ros_cloud.set_chan_size(1);
+		ros_cloud.chan[0].name = "intensities";
+		
+		int i,j;
+		float xf,yf,zf;
+		
+		//int w = Left->width;
+		//int h = Left->height;
+		//int wsi = Left->widthStep;
+		//unsigned char *dL = ((unsigned char *)(Left->imageData));
+		
+		
+		int point_count = 0, tot_points=0;
+		for(j=0; j<h; j++)
+		for(i=0; i<w; i++)
+		if( disp[j*w+i] > 0) 
+			tot_points ++;
+		
+		ros_cloud.set_pts_size(tot_points);
+		ros_cloud.chan[0].set_vals_size(tot_points);
+		
+		for(j=0; j<h; j++)
+		for(i=0; i<w; i++)
+		if( disp[j*w+i] > 0) {
+			zf = (abs(cpar.tx) * cpar.feq) / ( XOFFSET + ( (disp[j*w+i]*1.0) / SUBPIXEL) ) ;
+			xf = ((i - cpar.u0) * zf) / cpar.fx;
+			yf = ((j - cpar.v0) * zf) / cpar.fy;
+			
+			ros_cloud.pts[point_count].x = xf/1000; //from millimiters to decimiters
+			ros_cloud.pts[point_count].y = yf/1000; //Why does this need to be flipped?
+			ros_cloud.pts[point_count].z = zf/1000; //Why does this need to be flipped?
+		
+			ros_cloud.chan[0].vals[point_count] = 16*255;
+			point_count++;
+		}
+		
+		//printf("DON");
+		
+		ros_cloud.header.frame_id = "FRAMEID_SMALLV";
+		publish("spacetime_stereo", ros_cloud);
+		
+		signal(SIGUSR1, toggle_wait);
+
+		IplImage *idisp = cvCreateImage(cvSize(w, h), 8, 3);
+		cvZero(idisp);
+		
+		for(int y=RADIUS; y<h-RADIUS; y++){
+		for(int x=MAXDISP+RADIUS+1; x<w-RADIUS; x++){
+			if(disp[y*w+x]-8 < 0){
+				((unsigned char *)(idisp->imageData))[y*idisp->widthStep+x*3] = 255;
+				((unsigned char *)(idisp->imageData))[y*idisp->widthStep+x*3+1] = 0;
+				((unsigned char *)(idisp->imageData))[y*idisp->widthStep+x*3+2] = 0;
+			}
+			else{
+				((unsigned char *)(idisp->imageData))[y*idisp->widthStep+x*3] = disp[y*w+x]/8;
+				((unsigned char *)(idisp->imageData))[y*idisp->widthStep+x*3+1] = disp[y*w+x]/8;
+				((unsigned char *)(idisp->imageData))[y*idisp->widthStep+x*3+2] = disp[y*w+x]/8;
+			}
+		}}
+
+		printf("Done; press q to quit, space-key to continue.. \n");
+
+		cvShowImage("Disp Image", idisp);
+		cvShowImage("Ref Image", left_frames[0]);
+		cvShowImage("Tar Image", right_frames[0]);
+		cvReleaseImage(&idisp);
+
+		wait = 1;
+		int key = 'a';
+		while(wait){
+		key = cvWaitKey(10);
+		if(key == ' ')
+			wait = 0;
+		if (key=='q')
+			exit(1);
+		}
+	}
+
+for(d=0; d<w*h;d++)
 		free(mins[d]);	
 	free(mins);
 
@@ -511,99 +823,13 @@ void SpacetimeStereoNode::spacetime_stereo(vector<IplImage*> left_frames, vector
 	#endif
 	printf("Finished computing stereo disparities \n");
 
-//BEGIN COMPUTING POINT CLOUD
-printf("Computing Point Cloud; w: %d h:%d ...", w, h);
-
-calib_params cpar;
-
-int position = cal_string.find("Tx");
-string substring = cal_string.substr(position, cal_string.find("Ty") - position);
-sscanf(substring.c_str(), "%*s %f", &cpar.tx);
-
-position = cal_string.find("proj");
-substring = cal_string.substr(position, cal_string.find("rect")-position);
-sscanf(substring.c_str(), "%*s \n %f %*f %f %*f \n %*f %f %f", &cpar.fx, &cpar.u0, &cpar.fy, &cpar.v0);
-
-cpar.feq =  (cpar.fx+cpar.fy) / 2;
-
-printf("\nReading Parameters: tx: %f, u0: %f, v0: %f, fx:%f, fy:%f\n", cpar.tx, cpar.u0, cpar.v0, cpar.fx, cpar.fy);
-
-std_msgs::PointCloudFloat32 ros_cloud;
-
-ros_cloud.set_chan_size(1);
-ros_cloud.chan[0].name = "intensities";
-
-int i,j;
-float xf,yf,zf;
-
-int point_count = 0, tot_points=0;
-for(j=0; j<h; j++)
-for(i=0; i<w; i++)
-if( disp[j*w+i] > 0) 
-	tot_points ++;
-
-ros_cloud.set_pts_size(tot_points);
-ros_cloud.chan[0].set_vals_size(tot_points);
-
-for(j=0; j<h; j++)
-for(i=0; i<w; i++)
-if( disp[j*w+i] > 0) {
-	zf = (abs(cpar.tx) * cpar.feq) / ( XOFFSET + ( (disp[j*w+i]*1.0) / SUBPIXEL) ) ;
-	xf = ((i - cpar.u0) * zf) / cpar.fx;
-	yf = ((j - cpar.v0) * zf) / cpar.fy;
-	
-	ros_cloud.pts[point_count].x = xf/1000; //from millimiters to decimiters
-	ros_cloud.pts[point_count].y = yf/1000; //Why does this need to be flipped?
-	ros_cloud.pts[point_count].z = zf/1000; //Why does this need to be flipped?
-
-	ros_cloud.chan[0].vals[point_count] = 16*255;
-	point_count++;
-}
-
-//printf("DON");
-
-ros_cloud.header.frame_id = "FRAMEID_SMALLV";
-publish("spacetime_stereo", ros_cloud);
-
-signal(SIGUSR1, toggle_wait);
-
-//#ifdef DISPLAY
-
-IplImage *idisp = cvCreateImage(cvSize(w, h), 8, 3);
-cvZero(idisp);
-
-for(int y=RADIUS; y<h-RADIUS; y++){
-for(int x=MAXDISP+RADIUS+1; x<w-RADIUS; x++){
-	if(disp[y*w+x]-8 < 0){
-		((unsigned char *)(idisp->imageData))[y*idisp->widthStep+x*3] = 255;
-		((unsigned char *)(idisp->imageData))[y*idisp->widthStep+x*3+1] = 0;
-		((unsigned char *)(idisp->imageData))[y*idisp->widthStep+x*3+2] = 0;
-	}
-	else{
-		((unsigned char *)(idisp->imageData))[y*idisp->widthStep+x*3] = disp[y*w+x]/8;
-		((unsigned char *)(idisp->imageData))[y*idisp->widthStep+x*3+1] = disp[y*w+x]/8;
-		((unsigned char *)(idisp->imageData))[y*idisp->widthStep+x*3+2] = disp[y*w+x]/8;
-	}
-}}
-
-printf("Done; press q to quit, space-key to continue.. \n");
-
-cvShowImage("Disp Image", idisp);
-//cvShowImage("Ref Image", left_frames[0]);
-//cvShowImage("Tar Image", right_frames[0]);
-cvReleaseImage(&idisp);
-
-wait = 1;
-int key = 'a';
-while(wait){
-	key = cvWaitKey(10);
-	if(key == ' ')
-		wait = 0;
-	if (key=='q')
-		exit(1);
-}
-
 system("killall -s USR1 lpg");
+
+//printf("E!\n");
+//m_rosNode->publish("full_cloud",ros_cloud);
+		
+	
+
 
 }
 
@@ -614,19 +840,31 @@ int main(int argc, char **argv){
 
 //#ifdef DISPLAY
 cvNamedWindow("Disp Image", 1);
-//cvNamedWindow("Ref Image", 1);
-//cvNamedWindow("Tar Image", 1);
+cvNamedWindow("Ref Image", 1);
+cvNamedWindow("Tar Image", 1);
+
 //#endif
 
-ros::init(argc, argv);
-SpacetimeStereoNode sts;
-sts.spin();
+	ros::init(argc, argv);
+	SpacetimeStereoNode sts;
+	sts.spin();
+
+
+	//ros::fini();
+// 	static uint32_t count = 0;
+// 	std::stringstream ss;
+// 	ss << "spacetime_stereo" << count++;
+// 	
+// 	m_rosNode->init(argc, argv);	
+// 	
+// 	m_rosNode = new ros::node( ss.str() );
+// 	m_rosNode->advertise<std_msgs::PointCloudFloat32>("full_cloud", 5);
 
 //#ifdef DISPLAY
 cvDestroyWindow("Disp Image");
-//cvDestroyWindow("Ref Image");
-//vDestroyWindow("Tar Image");
-//#endifc
+cvDestroyWindow("Ref Image");
+cvDestroyWindow("Tar Image");
+//#endif
 
 return 0;
 
