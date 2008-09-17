eea4d808eddda478c5f471cfc218ed13a6769cda
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-09-17 01:18:39 +0000

    very simple tf server
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@4372 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ util/tf/include/tf/server.h
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+/** \author Tully Foote */
+
+namespace tf
+{
+
+class TransformSender{
+public:
+  TransformSender(ros::node& anode):
+    node_(anode)
+  {
+    node_.advertise<rosTF::TransformArray>("TransformArray", 100);
+  };
+
+  sendTransform(const Stamped<Transform> & transform, const std::string& parent_id)
+  {
+    tfMessage message;
+    message.header.stamp = ros::Time(transform.stamp_);
+    message.header.frame_id = transform.frame_id_;
+    message.parent = parent_id;
+    TransformBtToMsg(transform.data_, message.transform);
+    node_.publish("/tf", message);
+  } 
+  
+  sendTransform(const btTransform & transform, const uint64_t & time, const std::string& frame_id, const std::string& parent_id)
+  {
+    tfMessage message;
+    message.header.stamp = ros::Time(time);
+    message.header.frame_id = frame_id;
+    message.parent = parent_id;
+    TransformBtToMsg(transform, message.transform);
+    node_.publish("/tf", message);
+  }
+  
+private:
+  ros::node & node_;
+
+}
+
+}
