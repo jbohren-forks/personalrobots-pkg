280c8c3cb449fc2cbcfa5bf1009c586513b4004a
garybradski <garybradski@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-09-18 21:45:18 +0000

    Axis cam shell that shows getting images, getting slider and keyboard commands, getting and setting ptz state
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@4438 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ sandbox/axis_cam2/build/CMakeFiles/axis_demo.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/u/bradski/ros/ros-pkg/sandbox/axis_cam2/src/axis_demo/axis_control_demo.cpp" "/u/bradski/ros/ros-pkg/sandbox/axis_cam2/build/CMakeFiles/axis_demo.dir/src/axis_demo/axis_control_demo.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
