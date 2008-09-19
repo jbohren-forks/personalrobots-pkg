2ce8aac475fc1ba86d9ecb8532dd5923429d6b3e
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-09-19 20:55:28 +0000

    adding bullet_unittest which is used to check things I have questions about in Bullet, such as euler conventions.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@4502 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ util/tf/test/bullet_unittest.cpp
@@ -0,0 +1,252 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <gtest/gtest.h>
+#include <sys/time.h>
+#include "LinearMath/btTransform.h"
+#include "LinearMath/btVector3.h"
+
+void seed_rand()
+{
+  //Seed random number generator with current microseond count
+  timeval temp_time_struct;
+  gettimeofday(&temp_time_struct,NULL);
+  srand(temp_time_struct.tv_usec);
+};
+
+
+TEST(Bullet, EulerConventions)
+{
+  double epsilon = 1e-6;
+  double yaw, pitch, roll;
+
+  /** Bullet Convention
+   * x  left
+   * y up
+   * z forward 
+   *
+   * yaw about y
+   * then
+   * pitch around x
+   * then
+   * roll around z 
+   */
+
+
+  //Yaw by 90 
+  yaw = M_PI/2;
+  pitch = 0;
+  roll = 0;
+  
+  btQuaternion q(0,0,0);
+  btVector3 v(1,0,0);
+  btTransform tr(btQuaternion(yaw, pitch,roll), btVector3(0,0,0));
+  
+  btVector3 v2 = tr * v;
+
+  printf("%f, %f, %f: yaw by %f -> %f, %f, %f\n", v.x(), v.y(), v.z(), yaw, v2.x(), v2.y(), v2.z());
+  EXPECT_NEAR(v2.x(), 0,epsilon); 
+  EXPECT_NEAR(v2.y(), 0,epsilon); 
+  EXPECT_NEAR(v2.z(), -1,epsilon); 
+                 
+  //Pitch by 90
+  yaw = 0;
+  pitch = M_PI/2;
+  roll = 0;
+  
+  q = btQuaternion(0,0,0);
+  v = btVector3(0,0,1);
+  tr = btTransform(btQuaternion(yaw, pitch,roll), btVector3(0,0,0));
+  
+  v2 = tr * v;
+
+  printf("%f, %f, %f: pitch by %f -> %f, %f, %f\n", v.x(), v.y(), v.z(), pitch, v2.x(), v2.y(), v2.z());
+  EXPECT_NEAR(v2.x(), 0,epsilon); 
+  EXPECT_NEAR(v2.y(), -1,epsilon); 
+  EXPECT_NEAR(v2.z(), 0,epsilon); 
+                 
+  //Roll by 90
+  yaw = 0;
+  pitch = 0;
+  roll = M_PI/2;
+  
+  q = btQuaternion(0,0,0);
+  v = btVector3(1,0,0);
+  tr = btTransform(btQuaternion(yaw, pitch,roll), btVector3(0,0,0));
+  
+  v2 = tr * v;
+
+  printf("%f, %f, %f: roll by %f -> %f, %f, %f\n", v.x(), v.y(), v.z(), roll, v2.x(), v2.y(), v2.z());
+  EXPECT_NEAR(v2.x(), 0,epsilon); 
+  EXPECT_NEAR(v2.y(), 1,epsilon); 
+  EXPECT_NEAR(v2.z(), 0,epsilon); 
+                 
+  //Yaw and Roll by 90
+  yaw = M_PI/2;
+  pitch = 0;
+  roll = M_PI/2;
+  
+  q = btQuaternion(0,0,0);
+  v = btVector3(1,0,0);
+  tr = btTransform(btQuaternion(yaw, pitch,roll), btVector3(0,0,0));
+  
+  v2 = tr * v;
+
+  printf("%f, %f, %f: yaw and roll by %f -> %f, %f, %f\n", v.x(), v.y(), v.z(), roll, v2.x(), v2.y(), v2.z());
+  EXPECT_NEAR(v2.x(), 0,epsilon); 
+  EXPECT_NEAR(v2.y(), 1,epsilon); 
+  EXPECT_NEAR(v2.z(), 0,epsilon); 
+
+  q = btQuaternion(0,0,0);
+  v = btVector3(0,1,0);
+  tr = btTransform(btQuaternion(yaw, pitch,roll), btVector3(0,0,0));
+  
+  v2 = tr * v;
+
+  printf("%f, %f, %f: yaw and roll by %f -> %f, %f, %f\n", v.x(), v.y(), v.z(), roll, v2.x(), v2.y(), v2.z());
+  EXPECT_NEAR(v2.x(), 0,epsilon); 
+  EXPECT_NEAR(v2.y(), 0,epsilon); 
+  EXPECT_NEAR(v2.z(), 1,epsilon); 
+
+  q = btQuaternion(0,0,0);
+  v = btVector3(0,0,1);
+  tr = btTransform(btQuaternion(yaw, pitch,roll), btVector3(0,0,0));
+  
+  v2 = tr * v;
+
+  printf("%f, %f, %f: yaw and roll by %f -> %f, %f, %f\n", v.x(), v.y(), v.z(), roll, v2.x(), v2.y(), v2.z());
+  EXPECT_NEAR(v2.x(), 1,epsilon); 
+  EXPECT_NEAR(v2.y(), 0,epsilon); 
+  EXPECT_NEAR(v2.z(), 0,epsilon); 
+
+  //Yaw and Pitch
+  yaw = M_PI/2;
+  pitch = M_PI/2;
+  roll = 0;
+  
+  q = btQuaternion(0,0,0);
+  v = btVector3(1,0,0);
+  tr = btTransform(btQuaternion(yaw, pitch,roll), btVector3(0,0,0));
+  
+  v2 = tr * v;
+
+  printf("%f, %f, %f: yaw and pitch by %f -> %f, %f, %f\n", v.x(), v.y(), v.z(), pitch, v2.x(), v2.y(), v2.z());
+  EXPECT_NEAR(v2.x(), 0,epsilon); 
+  EXPECT_NEAR(v2.y(), 0,epsilon); 
+  EXPECT_NEAR(v2.z(), -1,epsilon); 
+
+  q = btQuaternion(0,0,0);
+  v = btVector3(0,1,0);
+  tr = btTransform(btQuaternion(yaw, pitch,roll), btVector3(0,0,0));
+  
+  v2 = tr * v;
+
+  printf("%f, %f, %f: yaw and pitch by %f -> %f, %f, %f\n", v.x(), v.y(), v.z(), pitch, v2.x(), v2.y(), v2.z());
+  EXPECT_NEAR(v2.x(), 1,epsilon); 
+  EXPECT_NEAR(v2.y(), 0,epsilon); 
+  EXPECT_NEAR(v2.z(), 0,epsilon); 
+
+  q = btQuaternion(0,0,0);
+  v = btVector3(0,0,1);
+  tr = btTransform(btQuaternion(yaw, pitch,roll), btVector3(0,0,0));
+  
+  v2 = tr * v;
+
+  printf("%f, %f, %f: yaw and pitch by %f -> %f, %f, %f\n", v.x(), v.y(), v.z(), pitch, v2.x(), v2.y(), v2.z());
+  EXPECT_NEAR(v2.x(), 0,epsilon); 
+  EXPECT_NEAR(v2.y(), -1,epsilon); 
+  EXPECT_NEAR(v2.z(), 0,epsilon); 
+
+  //Pitch and Roll
+  yaw = 0;
+  pitch = M_PI/2;
+  roll = M_PI/2;
+  
+  q = btQuaternion(0,0,0);
+  v = btVector3(1,0,0);
+  tr = btTransform(btQuaternion(yaw, pitch,roll), btVector3(0,0,0));
+  
+  v2 = tr * v;
+
+  printf("%f, %f, %f: pitch and roll by %f -> %f, %f, %f\n", v.x(), v.y(), v.z(), pitch, v2.x(), v2.y(), v2.z());
+  EXPECT_NEAR(v2.x(), 0,epsilon); 
+  EXPECT_NEAR(v2.y(), 0,epsilon); 
+  EXPECT_NEAR(v2.z(), 1,epsilon); 
+
+  q = btQuaternion(0,0,0);
+  v = btVector3(0,1,0);
+  tr = btTransform(btQuaternion(yaw, pitch,roll), btVector3(0,0,0));
+  
+  v2 = tr * v;
+
+  printf("%f, %f, %f: pitch and roll by %f -> %f, %f, %f\n", v.x(), v.y(), v.z(), pitch, v2.x(), v2.y(), v2.z());
+  EXPECT_NEAR(v2.x(), -1,epsilon); 
+  EXPECT_NEAR(v2.y(), 0,epsilon); 
+  EXPECT_NEAR(v2.z(), 0,epsilon); 
+
+  q = btQuaternion(0,0,0);
+  v = btVector3(0,0,1);
+  tr = btTransform(btQuaternion(yaw, pitch,roll), btVector3(0,0,0));
+  
+  v2 = tr * v;
+
+  printf("%f, %f, %f: pitch and roll by %f -> %f, %f, %f\n", v.x(), v.y(), v.z(), pitch, v2.x(), v2.y(), v2.z());
+  EXPECT_NEAR(v2.x(), 0,epsilon); 
+  EXPECT_NEAR(v2.y(), -1,epsilon); 
+  EXPECT_NEAR(v2.z(), 0,epsilon); 
+};
+
+
+TEST(Bullet, Slerp)
+{
+
+  unsigned int runs = 100;
+  seed_rand();
+
+  btQuaternion q1, q2;
+  q1.setEuler(0,0,0);
+  
+  for (unsigned int i = 0 ; i < runs ; i++)
+  {
+    q2.setEuler(1.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX,
+                1.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX,
+                1.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX);
+    
+    
+    btQuaternion q3 = slerp(q1,q2,0.5);
+    
+    EXPECT_NEAR(q3.angle(q1), q2.angle(q3), 1e-5);
+  }
+
+}
+
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
