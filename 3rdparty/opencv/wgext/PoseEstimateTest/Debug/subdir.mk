0a5ca4f714a4d9282a32d2bf244bbd0bba99b5a3
jdchen <jdchen@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-10-02 22:47:59 +0000

    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@4967 f5854215-dd47-0410-b2c4-cdd35faa7885

--- 3rdparty/opencv/wgext/PoseEstimateTest/Debug/subdir.mk
+++ 3rdparty/opencv/wgext/PoseEstimateTest/Debug/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/opt/opencv/include -I"/wg/stor1/jdchen/workspace/ros-pkg/3rdparty/opencv/wgext/3DPoseEstimation/include" -I/u/jdchen/workspace/ros-pkg/3rdparty/VTK/vtk-install/include/vtk-5.0/ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	g++ -I/usr/local/include/opencv -I/usr/include/vtk-5.0 -I/opt/boost/include -I/opt/ -I../../3DPoseEstimation/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
