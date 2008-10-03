5e190f8eac11ae3deb9f913fa862ee7bbda6ce19
jamesbowman <jamesbowman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-10-03 20:15:14 +0000

    Move to ros package visual_odometry
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@5020 f5854215-dd47-0410-b2c4-cdd35faa7885

--- 3rdparty/opencv/wgext/PoseEstimateTest/Debug/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l3DPoseEstimation -lcxcore -lcv -lcvaux -lhighgui -lml
--- 3rdparty/opencv/wgext/PoseEstimateTest/Release/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l3DPoseEstimation -lwgdetect -lsignatures -lhighgui -lcvaux -lml -lcv -lcxcore -lvtkGraphics -lvtkRendering
