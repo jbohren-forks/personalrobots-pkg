547f555a66059f02787d5792c220690a5204f807
mmwise <mmwise@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-10-06 22:29:09 +0000

    adding a simple head_pan_tilt_controller without transform
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@5090 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ controllers/pr2_mechanism_controllers/include/pr2_mechanism_controllers/head_pan_tilt_controller.h
@@ -0,0 +1,197 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#pragma once
+
+#include <ros/node.h>
+#include <rosthread/mutex.h>
+
+#include <mechanism_model/controller.h>
+#include <mechanism_model/joint.h>
+#include <robot_mechanism_controllers/joint_position_controller.h>
+#include <robot_mechanism_controllers/joint_velocity_controller.h>
+#include <robot_mechanism_controllers/joint_effort_controller.h>
+
+// Services
+#include <pr2_mechanism_controllers/SetJointCmd.h>
+#include <pr2_mechanism_controllers/GetJointCmd.h>
+
+#include <pr2_mechanism_controllers/SetJointGains.h>
+#include <pr2_mechanism_controllers/GetJointGains.h>
+
+//Kinematics
+#include <robot_kinematics/robot_kinematics.h>
+
+// Math utils
+#include <math_utils/angles.h>
+
+// #include <libTF/Pose3D.h>
+// #include <urdf/URDF.h>
+
+namespace controller
+{
+
+// The maximum number of joints expected in an arm.
+static const int MAX_HEAD_JOINTS = 2;
+
+class HeadPanTiltController : public Controller
+{
+public:
+
+  /*!
+   * \brief Default Constructor of the JointController class.
+   *
+   */
+  HeadPanTiltController();
+
+  /*!
+   * \brief Destructor of the JointController class.
+   */
+  virtual ~HeadPanTiltController();
+
+  /*!
+   * \brief Functional way to initialize limits and gains.
+   *
+  */
+  bool initXml(mechanism::RobotState *robot, TiXmlElement *config);
+
+
+  /*!
+   * \brief Sets a goal for some joint, specified as a pair of (joint values, joint names)
+   *
+   * \param j_values the positions of the joints
+   * \param j_names the names of the joints
+  */
+  void setJointCmd(const std::vector<double> &j_values, const std::vector<std::string> & j_names);
+
+//  /**
+//   * @brief Overloaded method for convenience
+//   * @param req
+//   */
+//  void setJointCmd(pr2_mechanism_controllers::SetJointPosCmd::request &req);
+
+  void getJointCmd(pr2_mechanism_controllers::JointCmd & cmd) const;
+
+  /*!
+   * \brief Get latest position command to the joint: revolute (angle) and prismatic (position).
+  */
+//  void getJointPosCmd(pr2_mechanism_controllers::GetJointPosCmd::response &resp);
+
+//  void getCurrentConfiguration(std::vector<double> &);
+
+  /*!
+     * \brief Issues commands to the joint. Should be called at regular intervals
+  */
+  virtual void update(void); // Real time safe.
+
+  controller::JointPositionController* getJointPositionControllerByName(std::string name);
+
+  unsigned int num_joints_;
+
+private:
+
+  double last_time_;
+
+  std::vector<JointPositionController *> joint_position_controllers_;
+
+  // Goal of the joints
+  std::vector<double> set_pts_;
+
+  mechanism::Robot* robot_;
+
+  void updateJointControllers(void);
+
+  int getJointControllerByName(std::string name);
+
+
+};
+
+/** @class HeadPanTiltControllerNode
+ *  @brief ROS interface for the head controller.
+ *
+ *  This class provides a ROS interface for controlling the arm by setting position configurations. If offers several ways to control the head:
+ *  - through listening to ROS messages: this is specified in the XML configuration file by the following parameters:
+ *      <listen_topic name="the name of my message" />
+ *      (only one topic can be specified)
+ *  - through a non blocking service call: this service call can specify a single configuration as a target (and maybe multiple configuration in the future)
+ *  - through a blocking service call: this service can receive a list of position commands that will be followed one after the other
+ *
+ */
+class HeadPanTiltControllerNode : public Controller
+{
+  public:
+    /*!
+    * \brief Default Constructor
+    *
+    */
+    HeadPanTiltControllerNode();
+
+    /*!
+    * \brief Destructor
+    */
+    ~HeadPanTiltControllerNode();
+
+    void update();
+
+    bool initXml(mechanism::RobotState *robot, TiXmlElement *config);
+
+    /** @brief sets a command for all the joints managed by the controller at once
+     * This is a lightweight version of setJointPosHeadless when the caller knows the order of the joints.
+     * @note this service should not be used. use setJointPosHeadless instead
+     * This service returns immediately
+     * @param req an array of position
+     * @param resp The response is empty
+     * @return
+     */
+    bool setJointSrv(pr2_mechanism_controllers::SetJointCmd::request &req,
+                    pr2_mechanism_controllers::SetJointCmd::response &resp);
+
+    /** @brief service that returns the goal of the controller
+     * @note if you know the goal has been reached and you do not want to subscribe to the /mechanism_state topic, you can use it as a hack to get the position of the arm
+     * @param req
+     * @param resp the response, contains a JointPosCmd message with the goal of the controller
+     * @return
+     */
+    bool getJointCmd(pr2_mechanism_controllers::GetJointCmd::request &req,
+                        pr2_mechanism_controllers::GetJointCmd::response &resp);
+
+  private:
+    pr2_mechanism_controllers::JointCmd msg_;   //The message used by the ROS callback
+    HeadPanTiltController *c_;
+
+};
+
+}
+
+
---
+++ controllers/pr2_mechanism_controllers/src/head_pan_tilt_controller.cpp
@@ -0,0 +1,213 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+// Original version: Melonee Wise <mwise@willowgarage.com>
+
+#include "pr2_mechanism_controllers/head_pan_tilt_controller.h"
+
+using namespace controller;
+using namespace std;
+
+ROS_REGISTER_CONTROLLER(HeadPanTiltController);
+
+HeadPanTiltController::HeadPanTiltController():num_joints_(0),last_time_(0)
+{
+}
+
+HeadPanTiltController::~HeadPanTiltController()
+{
+  // Assumes the joint controllers are owned by this class.
+  for(unsigned int i=0; i < num_joints_;++i)
+  {
+    delete joint_position_controllers_[i];
+  }
+}
+
+bool HeadPanTiltController::initXml(mechanism::RobotState * robot, TiXmlElement * config)
+{
+  robot_ = robot->model_;
+  TiXmlElement *elt = config->FirstChildElement("controller");
+  while (elt)
+  {
+    JointPositionController * jpc = new JointPositionController();
+    std::cout<<elt->Attribute("type")<<elt->Attribute("name")<<std::endl;
+    assert(static_cast<std::string>(elt->Attribute("type")) == std::string("JointPositionController"));
+    joint_position_controllers_.push_back(jpc);
+    if(!jpc->initXml(robot, elt))
+      return false;
+
+    elt = elt->NextSiblingElement("controller");
+  }
+
+  num_joints_ = joint_position_controllers_.size();
+
+  fprintf(stderr,"HeadPanTiltController:: num_joints_: %d\n",num_joints_);
+
+  set_pts_.resize(num_joints_);
+  last_time_ = robot->hw_->current_time_;
+
+  return true;
+}
+
+void HeadPanTiltController::setJointCmd(const std::vector<double> &j_values, const std::vector<std::string> & j_names)
+{
+  assert(j_values.size() == j_names.size());
+  for(uint i = 0; i < j_values.size(); ++i)
+  {
+    const std::string & name = j_names[i];
+    const int id = getJointControllerByName(name);
+    if(id >= 0)
+    {
+      set_pts_[id] = j_values[i];
+    }
+  }
+}
+
+
+void HeadPanTiltController::getJointCmd(pr2_mechanism_controllers::JointCmd & cmd) const
+{
+  const unsigned int n = joint_position_controllers_.size();
+  cmd.set_names_size(n);
+  for(unsigned int i=0; i<n; ++i)
+      cmd.names[i] = joint_position_controllers_[i]->getJointName();
+
+  cmd.set_positions_vec(set_pts_);
+}
+
+
+controller::JointPositionController* HeadPanTiltController::getJointPositionControllerByName(std::string name)
+{
+  for(int i=0; i< (int) num_joints_; i++)
+  {
+    if(joint_position_controllers_[i]->getJointName() == name)
+    {
+      return joint_position_controllers_[i];
+    }
+  }
+    return NULL;
+}
+
+
+int HeadPanTiltController::getJointControllerByName(std::string name)
+{
+  for(int i=0; i< (int) num_joints_; i++)
+  {
+    if(joint_position_controllers_[i]->getJointName() == name)
+    {
+      return i;
+    }
+  }
+  return -1;
+}
+
+
+void HeadPanTiltController::update(void)
+{
+  for(unsigned int i=0; i < num_joints_;++i)
+  {
+   joint_position_controllers_[i]->setCommand(set_pts_[i]);
+  }
+
+  updateJointControllers();
+}
+
+
+
+void HeadPanTiltController::updateJointControllers(void)
+{
+  for(unsigned int i=0;i<num_joints_;++i)
+    joint_position_controllers_[i]->update();
+}
+
+
+//------ Head controller node --------
+
+ROS_REGISTER_CONTROLLER(HeadPanTiltControllerNode)
+
+HeadPanTiltControllerNode::HeadPanTiltControllerNode()
+  : Controller()
+{
+  std::cout<<"Controller node created"<<endl;
+  c_ = new HeadPanTiltController();
+}
+
+HeadPanTiltControllerNode::~HeadPanTiltControllerNode()
+{
+  delete c_;
+}
+
+void HeadPanTiltControllerNode::update()
+{
+  c_->update();
+}
+
+bool HeadPanTiltControllerNode::initXml(mechanism::RobotState * robot, TiXmlElement * config)
+{
+  std::cout<<"LOADING HEAD PAN TILT CONTROLLER NODE"<<std::endl;
+  ros::node * const node = ros::node::instance();
+  string prefix = config->Attribute("name");
+  std::cout<<"the prefix is "<<prefix<<std::endl;
+  
+  // Parses subcontroller configuration
+  if(c_->initXml(robot, config))
+  {
+    node->advertise_service(prefix + "/set_command_array", &HeadPanTiltControllerNode::setJointSrv, this);
+    node->advertise_service(prefix + "/get_command", &HeadPanTiltControllerNode::getJointCmd, this);
+
+    return true;
+  }
+  return false;
+}
+
+bool HeadPanTiltControllerNode::setJointSrv(pr2_mechanism_controllers::SetJointCmd::request &req,
+                                   pr2_mechanism_controllers::SetJointCmd::response &resp)
+{
+  std::vector<double> pos;
+  std::vector<std::string> names;
+
+  req.set_positions_vec(pos);
+  req.set_names_vec(names);
+
+  c_->setJointCmd(pos,names);
+  return true;
+}
+
+bool HeadPanTiltControllerNode::getJointCmd(pr2_mechanism_controllers::GetJointCmd::request &req,
+                    pr2_mechanism_controllers::GetJointCmd::response &resp)
+{
+  pr2_mechanism_controllers::JointCmd cmd;
+  c_->getJointCmd(cmd);
+  resp.command = cmd;
+  return true;
+}
