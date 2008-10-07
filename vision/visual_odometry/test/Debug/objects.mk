63a1ede9db94e22dd7a043c8af27f6e00e8c5864
jdchen <jdchen@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-10-07 23:54:28 +0000

    remove files generated by eclipse project
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@5128 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/visual_odometry/test/Debug/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lvisual_odometry -lcxcore -lcv -lcvaux -lhighgui -lml
