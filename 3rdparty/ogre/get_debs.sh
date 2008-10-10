a398860afbe08c6bd7883c8ea8aefd08bc5363fd
jfaustwg <jfaustwg@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-10-10 23:53:29 +0000

    Adding script to get the ogre debs from the website
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@5274 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ 3rdparty/ogre/get_debs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wget http://pr.willowgarage.com/downloads/debs-$1/libceguiogre-dev_1.4.9.dfsg1-1_all.deb
+wget http://pr.willowgarage.com/downloads/debs-$1/libceguiogrerenderer-1.4.9-dbg_1.4.9.dfsg1-1_i386.deb
+wget http://pr.willowgarage.com/downloads/debs-$1/libceguiogrerenderer-1.4.9_1.4.9.dfsg1-1_i386.deb
+wget http://pr.willowgarage.com/downloads/debs-$1/libogre-dev_1.4.9.dfsg1-1_all.deb
+wget http://pr.willowgarage.com/downloads/debs-$1/libogremain-1.4.9-dbg_1.4.9.dfsg1-1_i386.deb
+wget http://pr.willowgarage.com/downloads/debs-$1/libogremain-1.4.9_1.4.9.dfsg1-1_i386.deb
+wget http://pr.willowgarage.com/downloads/debs-$1/ogre-doc_1.4.9.dfsg1-1_all.deb
+wget http://pr.willowgarage.com/downloads/debs-$1/ogre-tools_1.4.9.dfsg1-1_i386.deb
+
