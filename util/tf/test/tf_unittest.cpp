9926915ad1c0a32c5f942a9f6e9c1cae47d9be98
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-10-17 08:17:45 +0000

    two more passed tests!
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@5481 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/tf/test/tf_unittest.cpp
+++ util/tf/test/tf_unittest.cpp
@@ -454,8 +454,96 @@ TEST(data, TransformStampedConversions)
   } 
 }
 
+TEST(tf, ListOneForward)
+{
+  unsigned int runs = 400;
+  double epsilon = 1e-6;
+  seed_rand();
+  
+  tf::Transformer mTR(true);
+  std::vector<double> xvalues(runs), yvalues(runs), zvalues(runs);
+  for ( unsigned int i = 0; i < runs ; i++ )
+  {
+    xvalues[i] = 10.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
+    yvalues[i] = 10.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
+    zvalues[i] = 10.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
 
-/** @todo Make this actually Assert something */
+    Stamped<btTransform> tranStamped(btTransform(btQuaternion(0,0,0), btVector3(xvalues[i],yvalues[i],zvalues[i])), 10 + i, "child",  "my_parent");
+    mTR.setTransform(tranStamped);
+  }
+
+  //  std::cout << mTR.allFramesAsString() << std::endl;
+  //  std::cout << mTR.chainAsString("child", 0, "my_parent2", 0, "my_parent2") << std::endl;
+
+  for ( unsigned int i = 0; i < runs ; i++ )
+
+  {
+    Stamped<btTransform> inpose (btTransform(btQuaternion(0,0,0), btVector3(0,0,0)), 10 + i, "child");
+
+    try{
+    Stamped<Pose> outpose;
+    outpose.data_.setIdentity(); //to make sure things are getting mutated
+    mTR.transformPose("my_parent",inpose, outpose);
+    EXPECT_NEAR(outpose.data_.getOrigin().x(), xvalues[i], epsilon);
+    EXPECT_NEAR(outpose.data_.getOrigin().y(), yvalues[i], epsilon);
+    EXPECT_NEAR(outpose.data_.getOrigin().z(), zvalues[i], epsilon);
+    }
+    catch (tf::TransformException & ex)
+    {
+      std::cout << "TransformExcepion got through!!!!! " << ex.what() << std::endl;
+      bool exception_improperly_thrown = true;
+      EXPECT_FALSE(exception_improperly_thrown);
+    }
+  }
+  
+}
+
+
+TEST(tf, ListOneInverse)
+{
+  unsigned int runs = 400;
+  double epsilon = 1e-6;
+  seed_rand();
+  
+  tf::Transformer mTR(true);
+  std::vector<double> xvalues(runs), yvalues(runs), zvalues(runs);
+  for ( unsigned int i = 0; i < runs ; i++ )
+  {
+    xvalues[i] = 10.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
+    yvalues[i] = 10.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
+    zvalues[i] = 10.0 * ((double) rand() - (double)RAND_MAX /2.0) /(double)RAND_MAX;
+
+    Stamped<btTransform> tranStamped(btTransform(btQuaternion(0,0,0), btVector3(xvalues[i],yvalues[i],zvalues[i])), 10 + i, "child",  "my_parent");
+    mTR.setTransform(tranStamped);
+  }
+
+  //  std::cout << mTR.allFramesAsString() << std::endl;
+  //  std::cout << mTR.chainAsString("child", 0, "my_parent2", 0, "my_parent2") << std::endl;
+
+  for ( unsigned int i = 0; i < runs ; i++ )
+
+  {
+    Stamped<btTransform> inpose (btTransform(btQuaternion(0,0,0), btVector3(0,0,0)), 10 + i, "my_parent");
+
+    try{
+    Stamped<btTransform> outpose;
+    outpose.data_.setIdentity(); //to make sure things are getting mutated
+    mTR.transformPose("child",inpose, outpose);
+    EXPECT_NEAR(outpose.data_.getOrigin().x(), -xvalues[i], epsilon);
+    EXPECT_NEAR(outpose.data_.getOrigin().y(), -yvalues[i], epsilon);
+    EXPECT_NEAR(outpose.data_.getOrigin().z(), -zvalues[i], epsilon);
+    }
+    catch (tf::TransformException & ex)
+    {
+      std::cout << "TransformExcepion got through!!!!! " << ex.what() << std::endl;
+      bool exception_improperly_thrown = true;
+      EXPECT_FALSE(exception_improperly_thrown);
+    }
+  }
+  
+}
+
+
 
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
