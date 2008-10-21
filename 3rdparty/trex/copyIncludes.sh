1a79c5ef710ab38c3d759bd2dcd516d161d53540
gerkey <gerkey@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-10-21 05:16:30 +0000

    replaced error-prone grepping of ls output with calls to find
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@5601 f5854215-dd47-0410-b2c4-cdd35faa7885

--- 3rdparty/trex/copyIncludes.sh
+++ 3rdparty/trex/copyIncludes.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 
-function scanDirectory {
-    HEADERS="`ls $1/*.h 2> /dev/null` `ls $1/*.hh 2> /dev/null`"
-    for HEADER in $HEADERS
-    do
-	cp $HEADER $INCLUDE_DIR
-    done
-    FILES=`ls -l $1 | grep "^d" | awk '{ print $8 }'`
-    for FILE in $FILES
-    do
-	scanDirectory "$1/$FILE"
-    done
-    
-}
+#function scanDirectory {
+#    HEADERS="`ls $1/*.h 2> /dev/null` `ls $1/*.hh 2> /dev/null`"
+#    for HEADER in $HEADERS
+#    do
+#	cp $HEADER $INCLUDE_DIR
+#    done
+#    FILES=`ls -l $1 | grep "^d" | awk '{ print $8 }'`
+#    for FILE in $FILES
+#    do
+#	scanDirectory "$1/$FILE"
+#    done
+#    
+#}
 
 cd `rospack find trex`
 
 rm -r include 2> /dev/null
-mkdir -p include
 INCLUDE_DIR="`pwd`/include"
+#scanDirectory `pwd`
+DIRS=`find \`pwd\` -type d`
+mkdir -p include
+for d in $DIRS; do
+  HEADERS=`find $d -maxdepth 1 -type f -regex ".*\.hh?$"`
+  if [ "$HEADERS" ]; then
+    cp $HEADERS $INCLUDE_DIR
+  fi
+done
 echo "Warning, this is not the real include directory. It will be deleted and recreated by make every run. Includes should be next to .cc files." > $INCLUDE_DIR/WARNING.txt
-scanDirectory `pwd`
-
-
