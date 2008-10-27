11a7071682f5bb2681ac4044ed18852e39caaf10
jamesbowman <jamesbowman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-10-27 17:53:48 +0000

    Unit test
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@5803 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ vision/videre_face_detection/test/directed.py
@@ -0,0 +1,33 @@
+import rostools
+rostools.update_path('videre_face_detection')
+import rostest
+import videre_face_detection
+
+import sys
+sys.path.append('lib')
+
+import Image
+import ImageChops
+import ImageDraw
+
+import random
+import time
+import math
+
+import os
+
+p = videre_face_detection.people()
+print p
+for (i, f) in enumerate(range(200,240)):
+  im = Image.open("/u/jamesb/Desktop/wheelchair640l/%06d.png" % f).convert("L")
+  cascade_file = "haarcascade_frontalface_alt.xml"
+  assert os.access(cascade_file, os.R_OK)
+  faces = p.detectAllFaces(im.tostring(), im.size[0], im.size[1], cascade_file, 1.0, None, None, True)
+
+  draw = ImageDraw.Draw(im)
+  for (x,y,w,h) in faces:
+    draw.line((x,y,x+w,y), fill=255)
+    draw.line((x,y+h,x+w,y+h), fill=255)
+    draw.line((x,y,x,y+h), fill=255)
+    draw.line((x+w,y,x+w,y+h), fill=255)
+  im.save("out%06d.tiff" % i)
