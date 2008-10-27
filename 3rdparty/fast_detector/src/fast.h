4b0455ed466c1ea8784ec24b1efd83006336da2b
pmihelich <pmihelich@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-10-27 22:45:05 +0000

    Added extern C guard to fast.h to make functions easily accessible from C++.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@5825 f5854215-dd47-0410-b2c4-cdd35faa7885

--- 3rdparty/fast_detector/src/fast.h
+++ 3rdparty/fast_detector/src/fast.h
@@ -1,6 +1,10 @@
 #ifndef FAST_H
 #define FAST_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct { int x, y; } xy; 
 typedef unsigned char byte;
 
@@ -9,4 +13,9 @@ xy*  fast_corner_detect_9(const byte* im, int xsize, int ysize, int barrier, int
 xy*  fast_corner_detect_10(const byte* im, int xsize, int ysize, int barrier, int* numcorners);
 xy*  fast_corner_detect_11(const byte* im, int xsize, int ysize, int barrier, int* numcorners);
 xy*  fast_corner_detect_12(const byte* im, int xsize, int ysize, int barrier, int* numcorners);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
