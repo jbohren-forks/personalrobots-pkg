e27a7c79b78fd83887a57d768a2f2705e6695e3f
eitanme <eitanme@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-10-28 23:28:54 +0000

    Script to reset motors automatically.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@5893 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ robot_descriptions/wg_robot_description/pr2_prototype1/motor_monitor.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import rostools
+rostools.update_path('wg_robot_description')
+
+import sys, traceback, logging, rospy, os
+from robot_msgs.msg import DiagnosticMessage
+
+NAME = 'motor_monitor'
+
+def callback(data):
+  for msg in data.status:
+    if msg.name == "EtherCAT Master":
+      if msg.message == "Motors halted":
+        file = open('reset_motors.log', 'a')
+        log_msg = "Resetting motors at time: %d.%d\n" % (data.header.stamp.secs, data.header.stamp.nsecs)
+        file.write(log_msg)
+        file.close()
+        os.system('rosrun pr2_etherCAT resetMotors.py')
+        print "Resetting motors"
+
+def listener_with_user_data():
+    rospy.TopicSub("/diagnostics", DiagnosticMessage, callback)
+    rospy.ready(NAME, anonymous=True)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        listener_with_user_data()
+    except KeyboardInterrupt, e:
+        pass
+    print "exiting"
