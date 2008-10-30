874142d27afbd03a96bbe9b89a0fd412e71e2c6b
mcgann <mcgann@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-10-30 20:06:15 +0000

    Monitors the executive and shutds down controllers when not active.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@6025 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ highlevel/executive_trex/executive_trex_pr2/src/WatchDog.cc
@@ -0,0 +1,58 @@
+#include <ros/node.h>
+#include <highlevel_controllers/Ping.h>
+#include <sys/time.h>
+
+namespace TREX{
+
+  class Watchdog: public ros::node {
+  public:
+    Watchdog(): ros::node("trex_watch_dog"), watchdogOK_(true), watchdogTimeLimit_(1.0){
+      param("executive/ping_frequency", watchdogTimeLimit_, watchdogTimeLimit_);
+      subscribe<highlevel_controllers::Ping>("executive/ping", pingMsg_, &Watchdog::pingCallback, 1);
+      advertise<highlevel_controllers::Ping>("highlevel_controllers/shutdown", 1);
+    }
+
+    void run(){
+      while(true){
+	if(!watchdogOK_){
+	  highlevel_controllers::Ping shutdownMsg;
+	  publish<highlevel_controllers::Ping>("highlevel_controllers/shutdown", shutdownMsg);
+	}
+
+	watchdogOK_ = false;
+	usleep((unsigned int) rint(watchdogTimeLimit_ * 1e6));
+      }
+    }
+
+    /**
+     * Callback will reset the watchdog
+     */
+    void pingCallback(){
+      watchdogOK_ = true;
+    }
+
+  private:
+    bool watchdogOK_;
+    double watchdogTimeLimit_;
+    highlevel_controllers::Ping pingMsg_;
+  };
+
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc,argv); 
+
+  TREX::Watchdog node;
+
+  try {
+    node.run();
+  }
+  catch(char const* e){
+    std::cout << e << std::endl;
+  }
+
+  ros::fini();
+
+  return(0);
+}
