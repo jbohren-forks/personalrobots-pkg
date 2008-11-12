5564a0a2564e41d79aec7b8993103ffb7b8ee1bc
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-11-12 05:32:06 +0000

    gcc4.3
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@6577 f5854215-dd47-0410-b2c4-cdd35faa7885

--- curltest/curltest.cpp
+++ curltest/curltest.cpp
@@ -28,6 +28,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
 // POSSIBILITY OF SUCH DAMAGE.
 
+#include <cstring>
 #include "curltest.h"
 
 #define RETURN_CURL_ERR(curl, code) \
