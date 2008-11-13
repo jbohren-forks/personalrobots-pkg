eb588377ca5cb23ddfe631b6f10c0b129348d141
sachinchitta <sachinchitta@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-11-13 21:31:49 +0000

    Added testing for zero trajectory
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@6695 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/trajectory/test/utestBlended.cpp
+++ util/trajectory/test/utestBlended.cpp
@@ -39,6 +39,42 @@ TEST(Trajectory, samplingAfterInstantiationWithoutTimeBlendedLinear){
 }
 
 
+TEST(Trajectory, samplingWithZeroInput){
+
+  Trajectory t(2);
+  Trajectory::TPoint b(2);
+
+  std::vector<double> time;
+
+  std::vector<double> a;
+  std::vector<double> d;
+
+  d.resize(2);
+
+  d[0] = 1;
+  d[1] = 1;
+
+  t.setMaxRates(d);
+  t.setMaxAcc(d);
+  t.setInterpolationMethod("blended_linear");
+
+  a.resize(8);
+  time.resize(4);
+  
+  for(int i=0; i< 8; i++)
+    a[i] = 0.0;
+
+  for(int j=0; j < 4; j++)
+   time[j] = double(j);
+
+  t.setTrajectory(a,time,4);
+  ROS_INFO("Setting trajectory done");
+  t.sample(b,1.5);
+  t.write(std::string("junk_zero.txt"),0.01);
+
+}
+
+
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
