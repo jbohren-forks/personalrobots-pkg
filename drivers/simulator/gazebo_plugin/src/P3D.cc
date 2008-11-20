4fc2407a3d6ca1df4f33cf145de076e3a770b5ee
hsujohnhsu <hsujohnhsu@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-11-20 23:42:10 +0000

    use the right get-pose call to get Gazebo coordinates rather than ODE coordinates.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@7091 f5854215-dd47-0410-b2c4-cdd35faa7885

--- drivers/simulator/gazebo_plugin/src/P3D.cc
+++ drivers/simulator/gazebo_plugin/src/P3D.cc
@@ -104,15 +104,19 @@ void P3D::InitChild()
 // Update the controller
 void P3D::UpdateChild()
 {
+  Pose3d pose;
   Quatern rot;
   Vector3 pos;
 
-  // apply xyz offsets
-  pos = this->myBody->GetPosition() + this->xyzOffsets;
+  // Get Pose/Orientation ///@todo: verify correctness
+  pose = this->myBody->GetPose(); // - this->myBody->GetCoMPose();
+
+  // apply xyz offsets and get position and rotation components
+  pos = pose.pos + this->xyzOffsets;
+  rot = pose.rot;
 
   // apply rpy offsets
   Vector3 rpyTotal;
-  rot = this->myBody->GetRotation();
   rpyTotal.x = this->rpyOffsets.x + rot.GetRoll();
   rpyTotal.y = this->rpyOffsets.y + rot.GetPitch();
   rpyTotal.z = this->rpyOffsets.z + rot.GetYaw();
