6679fc5610e94ae305074ef28726c35617ffafe8
mcgann <mcgann@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-11-25 23:03:31 +0000

    Opps - forgot this
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@7311 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ highlevel/executive_trex/executive_trex_pr2/include/Executive.hh
@@ -0,0 +1,87 @@
+#ifndef H_EXECUTIVE
+#define H_EXECUTIVE
+
+// roscpp
+#include <ros/node.h>
+
+// TREX
+#include "Clock.hh"
+#include "Agent.hh"
+#include "Debug.hh"
+#include "LogManager.hh"
+#include "Logger.hh"
+
+namespace TREX{
+  class Executive;
+  typedef EUROPA::Id<Executive> ExecutiveId;
+
+
+  class Executive : public ros::node
+  {
+  public:
+
+    /**
+     * Creates and instance of the singleton and adds a reference.
+     */
+    static ExecutiveId request();
+
+    /**
+     * Releases a reference to the singleton.
+     */
+    static void release();    
+
+    Executive();
+
+    ~Executive();
+
+    /**
+     * @brief Main execution leak
+     */
+    void run();
+
+    template<class T> 
+    void registerPublisher(const std::string &topic, size_t max_queue){
+      ros::node::advertise<T>(topic, max_queue);
+    }
+
+    template<class M, class T>
+    void registerSubscriber(const std::string &_topic, M &_msg, void (T::*fp)(), T* obj, int max_queue){
+      ros::node::subscribe(_topic, _msg, fp, obj, max_queue);
+    }
+    
+    template<class M>
+    void publishMsg(const std::string &_topic, M &msg){
+      ros::node::publish<M>(_topic, msg);
+    }
+    
+  private:
+    
+    /**
+     * Adds a reference
+     */
+    void addRef();
+    /**
+     * Deletes a reference.
+     */
+    bool decRef();
+
+    /**
+     * A watchdog loop to keep pump ping messages to a watchdog
+     */
+    void watchDogLoop();
+
+    static ExecutiveId s_id;
+    ExecutiveId m_id;
+    unsigned int m_refCount;
+    double watchDogCycleTime_; /*!< Duration of sleep interval between pings */
+
+    TREX::Clock* agent_clock_; /*!< The clock used to run the agent */
+    std::ofstream debug_file_; /*!< TREX debug output sent here */
+    TREX::LoggerId logger_; /*!< handles TREX logging */
+    TiXmlElement* input_xml_root_; /*!< Root to xml file used by all */
+    bool play_back_; /*! Will be true if node running off a playback log */
+  };
+}
+
+
+#endif
