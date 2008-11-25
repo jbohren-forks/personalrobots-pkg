b1a3fe683683ad2ae8528b2c6f91ba624f611581
mcgann <mcgann@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-11-25 20:09:45 +0000

    Deprecated in favor of locally defined launch files
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@7292 f5854215-dd47-0410-b2c4-cdd35faa7885

--- highlevel/executive_trex/executive_trex_pr2/test/runtesttrex.sh
+++ highlevel/executive_trex/executive_trex_pr2/test/runtesttrex.sh
@@ -9,4 +9,4 @@ export TREX_LOG_DIR=$1
 sleep 5
 
 cd $2
-`rospack find executive_trex_pr2`/bin/trexfast `rospack find executive_trex_pr2`/cfg/nav.cfg
+`rospack find executive_trex_pr2`/bin/trexfast `rospack find executive_trex_pr2`/cfg/$3.cfg
