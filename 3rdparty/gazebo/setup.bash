0633d7ff99bb44198489f228acbad63ed455569e
hsujohnhsu <hsujohnhsu@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-11-26 15:59:42 +0000

    switch gazebo to use boost.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@7341 f5854215-dd47-0410-b2c4-cdd35faa7885

--- 3rdparty/gazebo/setup.bash
+++ 3rdparty/gazebo/setup.bash
@@ -2,11 +2,12 @@
 export GAZ_TOP=`rospack find gazebo`/gazebo
 export OGRE_TOP=`rospack find ogre`/ogre
 export CG_TOP=`rospack find Cg`/Cg
+export BOOST_TOP=`rospack find boost`/boost
 export SIM_PLUGIN=`rospack find gazebo_plugin`
 export PR2MEDIA=`rospack find gazebo_robot_description`/world
 
 
-export LD_LIBRARY_PATH=$SIM_PLUGIN/lib:$GAZ_TOP/lib:$CG_TOP/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$SIM_PLUGIN/lib:$GAZ_TOP/lib:$CG_TOP/lib:$BOOST_TOP/lib:$LD_LIBRARY_PATH
 export PATH=$GAZ_TOP/bin:$PATH
 
 export GAZEBO_RESOURCE_PATH=$PR2MEDIA
