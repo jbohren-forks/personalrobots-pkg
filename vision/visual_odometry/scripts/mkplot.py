9a7497e32e6bda272e22f12d8e8ef482091acfea
jamesbowman <jamesbowman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-12-06 06:33:32 +0000

    rosrecord change
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@7725 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/visual_odometry/scripts/mkplot.py
+++ vision/visual_odometry/scripts/mkplot.py
@@ -38,6 +38,21 @@ import array
 import rosrecord
 import visualize
 
+
+def planar(x, y, z):
+  from scipy import optimize
+
+  def silly(sol, args):
+      a,b,c = sol
+      x,y,z = args
+      return sum((y - (a*x + b*z + c)) ** 2)
+
+  sol = [1.0, 1.0, 1.0]
+  sol = optimize.fmin(silly, sol, args=((x,y,z),))
+
+  a,b,c = sol
+  return sqrt(sum((y - (a*x + b*z + c)) ** 2) / len(x))
+
 #def imgAdapted(im):
 #    return Image.fromstring({'mono8' : 'L', 'rgb24' : 'RGB'}[im.colorspace], (im.width, im.height), im.data).convert("L")
 
@@ -60,7 +75,9 @@ filename = sys.argv[1]
 framecounter = 0
 oe_x = []
 oe_y = []
-for topic, msg in rosrecord.logplayer(filename):
+first_pair = None
+
+for topic, msg, t in rosrecord.logplayer(filename):
   if rospy.is_shutdown():
     break
 
@@ -72,8 +89,8 @@ for topic, msg in rosrecord.logplayer(filename):
     cam = camera.Camera((Fx, Fy, Tx, Clx, Crx, Cy))
 
     vos = [
-      VisualOdometer(cam, feature_detector = FeatureDetectorFast(), descriptor_scheme = DescriptorSchemeSAD(), sba = True),
-      #VisualOdometer(cam, feature_detector = FeatureDetectorFast(), descriptor_scheme = DescriptorSchemeSAD(), scavenge = True),
+      VisualOdometer(cam, feature_detector = FeatureDetectorFast(), descriptor_scheme = DescriptorSchemeSAD()),
+      VisualOdometer(cam, feature_detector = FeatureDetectorFast(), descriptor_scheme = DescriptorSchemeSAD(), scavenge = True),
       #VisualOdometer(cam, feature_detector = FeatureDetectorHarris(), descriptor_scheme = DescriptorSchemeSAD()),
       #VisualOdometer(cam, feature_detector = FeatureDetectorFast(), descriptor_scheme = DescriptorSchemeSAD(), inlier_error_threshold=1.0),
       #VisualOdometer(cam, feature_detector = FeatureDetector4x4(FeatureDetectorFast), descriptor_scheme = DescriptorSchemeSAD())
@@ -82,9 +99,10 @@ for topic, msg in rosrecord.logplayer(filename):
     vo_y = [ [] for i in vos]
     vo_u = [ [] for i in vos]
     vo_v = [ [] for i in vos]
+    trajectory = [ [] for i in vos]
 
   start,end = 941,1000
-  start,end = 0,1000
+  start,end = 0,20000
 
   if cam and topic.endswith("videre/images"):
     print framecounter
@@ -93,11 +111,14 @@ for topic, msg in rosrecord.logplayer(filename):
     if start <= framecounter and (framecounter % 1) == 0:
       imgR = imgAdapted(msg.images[0])
       imgL = imgAdapted(msg.images[1])
+      if not first_pair:
+        first_pair = (imgL, imgR)
 
       for i,vo in enumerate(vos):
         af = SparseStereoFrame(imgL, imgR)
         vo.handle_frame(af)
         x,y,z = vo.pose.xform(0,0,0)
+        trajectory[i].append((x,y,z))
         vo_x[i].append(x)
         vo_y[i].append(z)
         x1,y1,z1 = vo.pose.xform(0,0,1)
@@ -108,8 +129,17 @@ for topic, msg in rosrecord.logplayer(filename):
     oe_x.append(-msg.pose.position.y)
     oe_y.append(msg.pose.position.x)
 
+# Attempt to compute best possible end-to-end pose
+vo = VisualOdometer(cam, feature_detector = FeatureDetector4x4(FeatureDetectorHarris), scavenge = True)
+f0 = SparseStereoFrame(*first_pair)
+f1 = SparseStereoFrame(imgL, imgR)
+vo.handle_frame(f0)
+vo.handle_frame(f1)
+quality_pose = vo.pose
+
 colors = [ 'blue', 'red', 'black', 'magenta', 'cyan' ]
 for i in range(len(vos)):
+  vos[i].planarity = planar(numpy.array([x for (x,y,z) in trajectory[i]]), numpy.array([y for (x,y,z) in trajectory[i]]), numpy.array([z for (x,y,z) in trajectory[i]]))
   xs = numpy.array(vo_x[i])
   ys = numpy.array(vo_y[i])
   xs -= 4.5 * 1e-3
@@ -151,11 +181,13 @@ pylab.xlim(mid - 0.5 * r, mid + 0.5 * r)
 mid = sum(ylim) / 2
 pylab.ylim(mid - 0.5 * r, mid + 0.5 * r)
 pylab.legend()
-pylab.show()
+#pylab.show()
 
 for vo in vos:
   print vo.feature_detector.name(), vo.descriptor_scheme.__class__.__name__
   print "distance from start:", vo.pose.distance()
+  print "planarity", vo.planarity
+  print "pose", vo.pose.comparison(quality_pose)
   vo.summarize_timers()
   print vo.log_keyframes
   print
