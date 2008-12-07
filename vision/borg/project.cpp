6e77deae5da749d644ab9c8179c3b35abc4f82d8
morgan_quigley <morgan_quigley@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-12-07 23:50:31 +0000

    calibration and projection programs
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@7762 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ vision/borg/project.cpp
@@ -0,0 +1,28 @@
+#include <stdexcept>
+#include <cassert>
+#include <cstdio>
+#include "SDL/SDL.h"
+#include "ros/time.h"
+#include "borg.h"
+#include <vector>
+
+using namespace borg;
+using std::vector;
+
+int main(int argc, char **argv)
+{
+  if (argc == 1)
+    throw std::runtime_error("usage: project EXTRACTIONFILE");
+  Borg borg(0);
+  vector<Borg::SensedPoint> extraction;
+  borg.loadExtractionFile(argv[1], extraction);
+  vector<Borg::ProjectedPoint> projection;
+  borg.project(extraction, projection);
+  for (vector<Borg::ProjectedPoint>::iterator p = projection.begin();
+       p != projection.end(); ++p)
+    printf("%.3f %.3f %.3f 0 0 0 %.3f %.3f %.3f 0\n", 
+           p->x, p->y, p->z,
+           p->r / 255.0, 0.3 /*p->g / 255.0*/, p->b / 255.0);
+  return 0;
+}
+
