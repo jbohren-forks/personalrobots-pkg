2132033fc6553ae0d6122898b2f08391d7058f36
eitanme <eitanme@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-12-10 03:52:09 +0000

    Adding the node Brian wrote to send bogus_battery_state messages to tell the robot to recharge on the push of a joystick button
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@7885 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ highlevel/highlevel_controllers/src/joy_batt_sender.cpp
@@ -0,0 +1,86 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+* 
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+* 
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+* 
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+* 
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+#include "ros/node.h"
+#include "robot_msgs/BatteryState.h"
+#include "joy/Joy.h"
+
+class JoyBattSender : public ros::node
+{
+  public:
+    JoyBattSender() : ros::node("joy_batt_msg") 
+    {
+      param<int>("~button", button_, 7);
+      param<int>("~deadman_button", deadman_button_, 4);
+      advertise<robot_msgs::BatteryState>("bogus_battery_state", 2);
+      subscribe("joy", joy_msg_, &JoyBattSender::handleJoyMsg, 2);
+    }
+
+    void handleJoyMsg()
+    {
+      ROS_INFO("%d", joy_msg_.buttons.size());
+      if(joy_msg_.buttons[button_] && (joy_msg_.buttons[deadman_button_]))
+      {
+        // Fake battery message that says we're empty
+        robot_msgs::BatteryState s;
+        s.energy_remaining = 0.0;
+        s.energy_capacity = 1000.0;
+        s.power_consumption = -800.0;
+
+        publish("bogus_battery_state", s);
+
+        ROS_INFO("Published bogus battery message");
+      }
+    }
+
+  private:
+    joy::Joy joy_msg_;
+    int button_;
+    int deadman_button_;
+
+};
+
+int
+main(int argc, char** argv)
+{
+  ros::init(argc, argv);
+
+  JoyBattSender jbs;
+
+  jbs.spin();
+
+  ros::fini();
+
+  return 0;
+}
