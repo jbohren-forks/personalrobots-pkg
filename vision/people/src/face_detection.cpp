b6b1be249098f52d1d79ab080d440859eb00a486
pantofaru <pantofaru@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-12-10 04:20:34 +0000

    Now using the new image msgs
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@7888 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/people/src/face_detection.cpp
+++ vision/people/src/face_detection.cpp
@@ -38,13 +38,17 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 
 #include "ros/node.h"
-#include "std_msgs/ImageArray.h"
-#include "std_msgs/String.h"
-#include "image_utils/cv_bridge.h"
+
 #include "CvStereoCamModel.h"
 #include <robot_msgs/PositionMeasurement.h>
+#include "image_msgs/StereoInfo.h"
+#include "image_msgs/CamInfo.h"
+#include "image_msgs/Image.h"
+#include "image_msgs/CvBridge.h"
+#include "topic_synchronizer.h"
 
 #include "opencv/cxcore.h"
 #include "opencv/cv.h"
@@ -55,24 +59,22 @@
 
 using namespace std;
 
-// FaceDetector - Face detection using the videre cams. A wrapper around OpenCV's face detection, plus some usage of depth to restrict the search.
+// FaceDetector - A wrapper around OpenCV's face detection, plus some usage of depth to restrict the search.
 
 class FaceDetector: public ros::node {
 public:
   // Images and conversion
-  std_msgs::ImageArray image_msg_;
-  std_msgs::String cal_params_;
-  CvBridge<std_msgs::Image> *cv_bridge_left_;
-  CvBridge<std_msgs::Image> *cv_bridge_disp_;
-  bool built_bridge_;
+  image_msgs::Image limage_;
+  image_msgs::Image dimage_;
+  image_msgs::StereoInfo stinfo_;
+  image_msgs::CamInfo rcinfo_;
+  image_msgs::CvBridge lbridge_;
+  image_msgs::CvBridge dbridge_;
+  TopicSynchronizer<FaceDetector> sync_;
 
   // The left and disparity images.
   IplImage *cv_image_left_;
   IplImage *cv_image_disp_;
-
-  // Display copies of the images.
-  IplImage *cv_image_left_cpy_;
-  IplImage *cv_image_disp_cpy_;
 
   bool use_depth_;
   CvStereoCamModel *cam_model_;
@@ -86,14 +88,10 @@ public:
   ros::thread::mutex cv_mutex_;
 
   FaceDetector(const char *haar_filename, bool use_depth) : 
-    node("face_detector", ros::node::ANONYMOUS_NAME),
-    cv_bridge_left_(NULL),
-    cv_bridge_disp_(NULL),
-    built_bridge_(false),
+    ros::node("face_detector"),
+    sync_(this, &FaceDetector::image_cb_all, ros::Duration(0.05), &FaceDetector::image_cb_timeout),
     cv_image_left_(NULL),
     cv_image_disp_(NULL),
-    cv_image_left_cpy_(NULL), 
-    cv_image_disp_cpy_(NULL),
     use_depth_(use_depth),
     cam_model_(NULL),
     people_(NULL),
@@ -103,17 +101,16 @@ public:
   { 
 
     // OpenCV: pop up an OpenCV highgui window
-    cvNamedWindow("Disparity",CV_WINDOW_AUTOSIZE);
-    cvNamedWindow("Face Detection", CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("Face detector: Disparity",CV_WINDOW_AUTOSIZE);
+    cvNamedWindow("Face detector: Face Detection", CV_WINDOW_AUTOSIZE);
 
     people_ = new People();
 
-    // Subscribe to image
-    subscribe("videre/images", image_msg_, &FaceDetector::image_cb, 1);
-
-
-    // Subscribe to calibration parameters
-    subscribe("videre/cal_params", cal_params_, &FaceDetector::cal_params_cb, 1);
+    // Subscribe to the images and parameters
+    sync_.subscribe("dcam/left/image_rect_color",limage_,1);
+    sync_.subscribe("dcam/disparity",dimage_,1);
+    sync_.subscribe("dcam/stereo_info",stinfo_,1);
+    sync_.subscribe("dcam/right/cam_info",rcinfo_,1);
 
     // Advertise a position measure message.
     advertise<robot_msgs::PositionMeasurement>("face_detection/position_measurement",1);
@@ -126,48 +123,56 @@ public:
 
     cvReleaseImage(&cv_image_left_);
     cvReleaseImage(&cv_image_disp_);
-    cvReleaseImage(&cv_image_left_cpy_);
-    cvReleaseImage(&cv_image_disp_cpy_);
 
-    cvDestroyWindow("Face Detection");
+    cvDestroyWindow("Face detector: Face Detection");
+    cvDestroyWindow("Face detector: Disparity");
 
     delete cam_model_;
 
     delete people_;
 
-    if (built_bridge_) {
-      delete cv_bridge_left_;
-      delete cv_bridge_disp_;
-    }
+  }
 
-
+  /// The image callback when not all topics are sync'ed. Don't do anything, just wait for sync.
+  void image_cb_timeout(ros::Time t) {
+    if (limage_.header.stamp != t)
+      printf("Timed out waiting for left image\n");
+    if (dimage_.header.stamp != t)
+      printf("Timed out waiting for disparity image\n");
   }
 
   /// The image callback. For each new image, copy it, perform face detection, and draw the rectangles on the image.
-  void image_cb()
+  void image_cb_all(ros::Time t)
   {
 
     detect_++;
     if (detect_ % 2) {
       return;
     }
+
+    cv_mutex_.lock();
  
     CvSize im_size;
 
-    // Set up the cv bridges, should only run once.
-    if (!built_bridge_) {
-      cv_bridge_left_ = new CvBridge<std_msgs::Image>(&image_msg_.images[1],  CvBridge<std_msgs::Image>::CORRECT_BGR | CvBridge<std_msgs::Image>::MAXDEPTH_8U);
-      cv_bridge_disp_ = new CvBridge<std_msgs::Image>(&image_msg_.images[0],  CvBridge<std_msgs::Image>::CORRECT_BGR | CvBridge<std_msgs::Image>::MAXDEPTH_8U);
-      built_bridge_ = true;
+    // Convert the images to OpenCV format
+    if (lbridge_.fromImage(limage_,"bgr")) {
+      cv_image_left_ = lbridge_.toIpl();
     }
- 
-    // Convert the images to opencv format.
-    if (cv_image_left_) {
-      cvReleaseImage(&cv_image_left_);
-      cvReleaseImage(&cv_image_disp_);
+    if (dbridge_.fromImage(dimage_)) {
+      cv_image_disp_ = dbridge_.toIpl();
     }
-    cv_bridge_left_->to_cv(&cv_image_left_);
-    cv_bridge_disp_->to_cv(&cv_image_disp_);
+
+    // Convert the stereo calibration into a camera model.
+    if (cam_model_) {
+      delete cam_model_;
+    }
+    double Fx = rcinfo_.P[0];
+    double Fy = rcinfo_.P[5];
+    double Clx = rcinfo_.P[2];
+    double Crx = Clx;
+    double Cy = rcinfo_.P[6];
+    double Tx = -rcinfo_.P[3]/Fx;
+    cam_model_ = new CvStereoCamModel(Fx,Fy,Tx,Clx,Crx,Cy,1.0/stinfo_.dpp);
  
     if ( cv_image_left_ )  {
       im_size = cvGetSize(cv_image_left_);
@@ -178,7 +183,7 @@ public:
       // Get the average disparity in the middle half of the bounding box, and compute the face center in 3d. Publish the face center as a track point.
       if (cv_image_disp_ && cam_model_) {
 	int r, c, good_pix;
-	uchar* ptr;
+	ushort* ptr;
 	double avg_disp;
 	CvRect *one_face;
 	robot_msgs::PositionMeasurement pos;
@@ -189,7 +194,7 @@ public:
 	  good_pix = 0;
 	  avg_disp = 0;
 	  for (r = floor(one_face->y+0.25*one_face->height); r < floor(one_face->y+0.75*one_face->height); r++) {
-	    ptr = (uchar*)(cv_image_disp_->imageData + r*cv_image_disp_->widthStep);
+	    ptr = (ushort*)(cv_image_disp_->imageData + r*cv_image_disp_->widthStep);
 	    for (c = floor(one_face->x+0.25*one_face->width); c < floor(one_face->x+0.75*one_face->width); c++) {
 	      if (ptr[c] > 0) {
 		avg_disp += ptr[c];
@@ -202,7 +207,7 @@ public:
 	  cvmSet(uvd,0,1,one_face->y+one_face->height/2.0);
 	  cvmSet(uvd,0,2,avg_disp);
 	  cam_model_->dispToCart(uvd,xyz);
-	  pos.header.stamp = image_msg_.header.stamp;
+	  pos.header.stamp = limage_.header.stamp;
 	  pos.name = "face_detection";
 	  pos.object_id = -1;
 	  pos.pos.x = cvmGet(xyz,0,2);
@@ -217,64 +222,12 @@ public:
 	cvReleaseMat(&uvd);
 	cvReleaseMat(&xyz);
       }
+
+
+      cvShowImage("Face detector: Face Detection",cv_image_left_);
+      cvShowImage("Face detector: Disparity",cv_image_disp_);
  
-      // Copy all of the images you might want to display.
-      // This is necessary because OpenCV doesn't like multiple threads.
-      cv_mutex_.lock();
-
-      if (cv_image_left_cpy_ == NULL) {
-	cv_image_left_cpy_ = cvCreateImage(im_size,IPL_DEPTH_8U,3);
-      }
-      cvCopy(cv_image_left_, cv_image_left_cpy_);
-
-      if (cv_image_disp_cpy_==NULL) {
-	cv_image_disp_cpy_ = cvCreateImage(im_size,IPL_DEPTH_8U,1);
-      }
-      cvCopy(cv_image_disp_, cv_image_disp_cpy_);
-
       cv_mutex_.unlock();
-    }
-  }
-
-  // Calibration parameters callback
-  void cal_params_cb() {
-    parseCaliParams(cal_params_.data);
-  }
-
-  /// JD's small parser to pick up the projection matrix from the
-  /// calibration message. This should really be somewhere else, this code is copied in multiple files.
-  void parseCaliParams(const string& cal_param_str){
-
-    if (cam_model_==NULL) {
-      const string labelRightCamera("[right camera]");
-      const string labelRightCamProj("proj");
-      const string labelRightCamRect("rect");
-      // move the current position to the section of "[right camera]"
-      size_t rightCamSection = cal_param_str.find(labelRightCamera);
-      // move the current position to part of proj in the section of "[right camera]"
-      size_t rightCamProj = cal_param_str.find(labelRightCamProj, rightCamSection);
-      // get the position of the word "rect", which is also the end of the projection matrix
-      size_t rightCamRect = cal_param_str.find(labelRightCamRect, rightCamProj);
-      // the string after the word "proj" is the starting of the matrix
-      size_t matrix_start = rightCamProj + labelRightCamProj.length();
-      // get the sub string that contains the matrix
-      string mat_str = cal_param_str.substr(matrix_start, rightCamRect-matrix_start);
-      // convert the string to a double array of 12
-      stringstream sstr(mat_str);
-      double matdata[12];
-      for (int i=0; i<12; i++) {
-	sstr >> matdata[i];
-      }
-
-      //if (cam_model_ == NULL) {
-      double Fx  = matdata[0]; // 0,0
-      double Fy  = matdata[5]; // 1,1
-      double Crx = matdata[2]; // 0,2
-      double Cy  = matdata[6]; // 1,2
-      double Clx = Crx; // the same
-      double Tx  = - matdata[3]/Fx;
-      std::cout << "base length "<< Tx << std::endl;
-      cam_model_ = new CvStereoCamModel(Fx, Fy, Tx, Clx, Crx, Cy, 0.25);
     }
   }
 
@@ -285,12 +238,6 @@ public:
 
 	// Display all of the images.
 	cv_mutex_.lock();
-	if (cv_image_left_cpy_)
-	  cvShowImage("Face Detection",cv_image_left_cpy_);
-	if (cv_image_disp_cpy_)
-	  cvShowImage("Disparity",cv_image_disp_cpy_);
-
-	cv_mutex_.unlock();
 
 	// Get user input and allow OpenCV to refresh windows.
 	int c = cvWaitKey(2);
@@ -298,6 +245,9 @@ public:
 	// Quit on ESC, "q" or "Q"
 	if((c == 27)||(c == 'q')||(c == 'Q'))
 	  quit_ = true;
+
+	cv_mutex_.unlock();
+	usleep(10000);
 
     }
     return true;
@@ -316,9 +266,7 @@ int main(int argc, char **argv)
     cerr << "Path to cascade file required.\n" << endl;
     return 0;
   }
-  char *haar_filename = argv[1]; //"./cascades/haarcascade_frontalface_alt.xml";
-  //char haar_filename[] = "./cascades/haarcascade_profileface.xml";
-  //char haar_filename[] = "./cascades/haarcascade_upperbody.xml";
+  char *haar_filename = argv[1]; 
   FaceDetector fd(haar_filename, use_depth);
  
   fd.spin();
