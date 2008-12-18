33454894d417263cb9566279995accb414bba517
eitanme <eitanme@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-12-18 17:30:30 +0000

    Adding a missing try-catch for a transform.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@8334 f5854215-dd47-0410-b2c4-cdd35faa7885

--- nav/amcl_player/amcl_player.cc
+++ nav/amcl_player/amcl_player.cc
@@ -479,9 +479,13 @@ AmclNode::ProcessMessage(QueuePointer &resp_queue,
 
     // subtracting base to odom from map to base and send map to odom instead
     tf::Stamped<tf::Pose> odom_to_map;
-    this->tfL->transformPose(odom_frame_id,tf::Stamped<tf::Pose> (btTransform(btQuaternion(pdata->pos.pa, 0, 0), 
+    try {
+      this->tfL->transformPose(odom_frame_id,tf::Stamped<tf::Pose> (btTransform(btQuaternion(pdata->pos.pa, 0, 0), 
                                                                        btVector3(pdata->pos.px, pdata->pos.py, 0.0)).inverse(), 
-                                                           t, "base_link"),odom_to_map);
+    }                                                  t, "base_link"),odom_to_map);
+    catch(tf::TransformException e){
+      return(0);
+    }
     this->tf->sendTransform(tf::Stamped<tf::Transform> (tf::Transform(tf::Quaternion( odom_to_map.getRotation() ),
                                                                       tf::Point(      odom_to_map.getOrigin() ) ),
                                                         t, "map",odom_frame_id));
