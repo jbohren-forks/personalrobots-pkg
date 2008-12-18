b2c360d26400221a6ea0933c3904f2a144635aee
stuglaser <stuglaser@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-12-18 01:15:30 +0000

    Xacro can now run shell commands
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@8305 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/xacro/xacro.py
+++ util/xacro/xacro.py
@@ -29,7 +29,7 @@
 # Author: Stuart Glaser
 
 
-import os.path, sys
+import os.path, sys, os
 from xml.dom.minidom import parse, parseString
 import xml.dom
 import re
@@ -301,6 +301,33 @@ def eval_expr(lex, symbols):
 
 
 def eval_text(text, symbols):
+    def handle_expr(s):
+        lex = QuickLexer(IGNORE = r"\s+",
+                         NUMBER = r"(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?",
+                         SYMBOL = r"[a-zA-Z_]\w*",
+                         OP = r"[\+\-\*/^]",
+                         LPAREN = r"\(",
+                         RPAREN = r"\)")
+        lex.lex(s)
+        return eval_expr(lex, symbols)
+    def handle_shell(s):
+        (_, fin) = os.popen2(s)
+        return fin.read().rstrip()
+
+    results = []
+    lex = QuickLexer(EXPR = r"\$\{[^\}]*\}",
+                     SHELL = r"\$\([^\)]*\)",
+                     TEXT = r"[^\$]+")
+    lex.lex(text)
+    while lex.peek():
+        if lex.peek()[0] == lex.EXPR:
+            results.append(handle_expr(lex.next()[1][2:-1]))
+        elif lex.peek()[0] == lex.SHELL:
+            results.append(handle_shell(lex.next()[1][2:-1]))
+        elif lex.peek()[0] == lex.TEXT:
+            results.append(lex.next()[1])
+    return ''.join(map(str, results))
+
     results = []
     pieces = re.split(r'\$\{([^\}]*)\}', text)
     for i in range(len(pieces)):
