7588906e73b7649c6d6601e3b71913bc888cc877
pantofaru <pantofaru@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-12-24 02:17:57 +0000

    comments
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@8565 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/people/src/stereo_face_color_tracker.cpp
+++ vision/people/src/stereo_face_color_tracker.cpp
@@ -5,7 +5,7 @@
  *
  * Software License Agreement (BSD License)
  * 
- *  Copyright (c) 2008, Caroline Pantofaru
+ *  Copyright (c) 2008, Willow Garage, Inc
  *  All rights reserved.
  * 
  *  Redistribution and use in source and binary forms, with or without
