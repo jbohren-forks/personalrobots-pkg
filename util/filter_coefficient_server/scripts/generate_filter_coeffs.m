300a094c9e7101e7e865962d46567289cde30615
mmwise <mmwise@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-12-29 23:36:50 +0000

    adding correct path for roslaunching
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@8657 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/filter_coefficient_server/scripts/generate_filter_coeffs.m
+++ util/filter_coefficient_server/scripts/generate_filter_coeffs.m
@@ -1,6 +1,10 @@
 #!/usr/bin/env octave
 
+[status,rosoctpath] = system('rospack find filter_coefficient_server');
+rosoctpath = strtrim(rosoctpath);
+addpath(fullfile(rosoctpath, '/scripts'));
 startup;
+
 
 __rosoct_unadvertise_service('generate_filter_coeffs');
 suc = rosoct_advertise_service('generate_filter_coeffs',@filter_coefficient_server_Filter,@filterserv);
