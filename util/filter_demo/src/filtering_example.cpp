9de6991b46119059ca70dd37f4962d20c2d0cd81
mmwise <mmwise@f5854215-dd47-0410-b2c4-cdd35faa7885>
2008-12-30 21:13:07 +0000

    cleaning up code
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@8702 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/filter_demo/src/filtering_example.cpp
+++ util/filter_demo/src/filtering_example.cpp
@@ -47,13 +47,18 @@ public:
   ExampleFilter() : ros::node("filtering_example")
   {
   }
-  bool call_srv(std::string name, std::vector<std::string> args, std::vector<double> &a, std::vector<double> &b, std::vector<std::vector<double> > &in, std::vector<std::vector<double> > &out)
+  bool call_srv(std::string name, std::vector<std::string> args)
   {
     filter_coefficient_server::Filter::request  req;
     filter_coefficient_server::Filter::response res;
     req.name = name;
     req.args = args;
-
+    std::vector<double> a;
+    std::vector<double> b;
+    std::vector<std::vector<double> > in;
+    std::vector<std::vector<double> > out;
+    std::vector<double> temp(2);
+    
     //hack for wait for service
     while (!ros::service::call("generate_filter_coeffs", req, res))
     {
@@ -62,15 +67,14 @@ public:
     
     if (ros::service::call("generate_filter_coeffs", req, res))
     {
+
+      std::ofstream outfile("output.txt");
       for(uint32_t i=0; i<res.a.size();i++)
       {
         a.push_back(res.a[i]);
         b.push_back(res.b[i]);
       }
       
-      std::vector<double> temp(2);
-      std::ofstream outfile("output.txt");  
-        
       //create a filter using the coeffs
       filters::TransferFunctionFilter<double> filter(b,a,2);
       
@@ -89,8 +93,6 @@ public:
     }
     else
     {
-      a.push_back(0);
-      b.push_back(0);
       return false;
     }
   }
@@ -99,26 +101,16 @@ public:
 int main(int argc, char **argv)
 {
   ros::init(argc, argv);
-  if (argc < 3)
-  {
-    printf("usage: generate_filter_coeffs_client name args\n");
-    printf("usage: generate_filter_coeffs_client butter 2 .1\n");
-    printf("usage: generate_filter_coeffs_client butter 2 .1 high\n");
-    printf("usage: generate_filter_coeffs_client butter 2 .1 .5 stop\n");
-    return 1;
-  }
+
   ExampleFilter a;
-  std::vector<double> tf_a;
-  std::vector<double> tf_b;
-  std::vector<std::vector<double> > input;
-  std::vector<std::vector<double> > output;
+  
   std::vector<std::string> args;
   for(int i=2; i<argc; i++)
   {
     args.push_back(argv[i]);
   }
 
-  if (a.call_srv(argv[1], args, tf_a, tf_b, input, output))
+  if (a.call_srv(argv[1], args))
   {
   }
   else
