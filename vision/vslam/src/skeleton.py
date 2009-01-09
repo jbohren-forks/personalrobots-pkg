cf4c6176024f768a4e99c85c19ae6f8bf0e63aa6
jamesbowman <jamesbowman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-01-09 23:18:47 +0000

    Restrict frames to every 15.  Use new Calonder binding.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@9148 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/vslam/src/skeleton.py
+++ vision/vslam/src/skeleton.py
@@ -31,8 +31,8 @@ class Skeleton:
     else:
       self.vt = None
     self.place_ids = []
+    self.vo = VisualOdometer(cam, feature_detector = FeatureDetectorFast(), descriptor_scheme = DescriptorSchemeCalonder())
     #self.vo = VisualOdometer(cam)
-    self.vo = VisualOdometer(cam, feature_detector = FeatureDetectorStar(), descriptor_scheme = DescriptorSchemeCalonder())
     self.node_kp = {}
     self.node_descriptors = {}
     self.node_matcher = {}
@@ -48,6 +48,11 @@ class Skeleton:
     elif not(this in self.nodes):
       byid = [ (f.id, f) for f in self.nodes ]
       prev = max(byid)[1]
+
+      # Ignore the node if there are less than 15 frames since the previous node
+      if (this.id - prev.id) < 15:
+        return
+
       relpose = ~prev.pose * this.pose
 
       self.nodes.add(this)
@@ -55,7 +60,7 @@ class Skeleton:
       self.timer['toro add'].start()
       self.pg.addIncrementalEdge(prev.id, this.id, relpose.xform(0,0,0), relpose.euler())
       self.timer['toro add'].stop()
-      #print "added node at", this.pose.xform(0,0,0), "in graph as", self.newpose(this).xform(0,0,0)
+      #print "added node at", this.pose.xform(0,0,0), "in graph as", self.newpose(this.id).xform(0,0,0)
 
       self.memoize_node_kp_d(this)
       this_d = self.node_descriptors[this.id]
@@ -85,8 +90,8 @@ class Skeleton:
     #self.pg.recomputeAllTransformations()
     #self.pg.save("render5.graph")
 
-  def newpose(self, f):
-    xyz,euler = self.pg.vertex(f.id)
+  def newpose(self, id):
+    xyz,euler = self.pg.vertex(id)
     return from_xyz_euler(xyz, euler)
 
   def place_find(self, lf, descriptors, count = 10):
@@ -114,7 +119,6 @@ class Skeleton:
     while not self.termcrit(count, self.pg.error()):
       self.pg.iterate()
       count += 1
-    this.pose = self.newpose(this)
     self.timer['toro opt'].stop()
 
   def my_frame(self, id):
@@ -128,15 +132,51 @@ class Skeleton:
       self.node_kp[af.id] = nf.kp
       self.node_descriptors[af.id] = nf.descriptors
       self.node_matcher[af.id] = nf.matcher
+
+      if 0:
+        pylab.imshow(numpy.fromstring(af.lf.tostring(), numpy.uint8).reshape(480,640), cmap=pylab.cm.gray)
+        pylab.scatter([x for (x,y,d) in nf.kp], [y for (x,y,d) in nf.kp], label = 'f0 kp', c = 'red')
+        pylab.show()
+        Image.fromstring("L", (640,480), af.lf.tostring()).save("star.png")
     self.timer['descriptors'].stop()
 
   def PE(self, af0, af1):
+    if 0:
+      (f0,f1) = (self.my_frame(af0.id), self.my_frame(af1.id))
+      pairs = self.vo.temporal_match(f0, f1)
+      for (a,b) in pairs:
+        pylab.plot([ f0.kp[a][0], f1.kp[b][0] ], [ f0.kp[a][1], f1.kp[b][1] ])
+      pylab.imshow(numpy.fromstring(af0.lf.tostring(), numpy.uint8).reshape(480,640), cmap=pylab.cm.gray)
+      pylab.scatter([x for (x,y,d) in f0.kp], [y for (x,y,d) in f0.kp], label = 'f0 kp', c = 'red')
+      pylab.scatter([x for (x,y,d) in f1.kp], [y for (x,y,d) in f1.kp], label = 'f1 kp', c = 'green')
+      pylab.legend()
+      pylab.show()
     return self.vo.proximity(self.my_frame(af0.id), self.my_frame(af1.id))
 
+  def correct_frame_pose(self, id):
+    if id in [ f.id for f in self.nodes ]:
+      return self.newpose(id)
+    else:
+      return None
+
+  def drawable(self):
+
+    pts = dict([ (f,self.newpose(f.id).xform(0,0,0)) for f in self.nodes ])
+    nodepts = pts.values()
+    edges = []
+    for (f0,f1) in self.edges:
+      p0 = pts[f0]
+      p1 = pts[f1]
+      edges.append((p0, p1))
+    return (nodepts, edges)
+
   def plot(self, color):
-    pts = dict([ (f,self.newpose(f).xform(0,0,0)) for f in self.nodes ])
+    pts = dict([ (f,self.newpose(f.id).xform(0,0,0)) for f in self.nodes ])
     nodepts = pts.values()
     pylab.scatter([x for (x,y,z) in nodepts], [z for (x,y,z) in nodepts], c = color, label = 'after SGD')
+    for (f,(x,y,z)) in pts.items():
+      pylab.annotate('%d' % f.id, (float(x), float(z)))
+
     for (f0,f1) in self.edges:
       p0 = pts[f0]
       p1 = pts[f1]
