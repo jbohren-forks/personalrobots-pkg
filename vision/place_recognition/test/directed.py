d7c143b0a6c0be272108062ee5bd190e4848d76c
jamesbowman <jamesbowman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-01-09 00:40:18 +0000

    Perf improvements
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@9094 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/place_recognition/test/directed.py
+++ vision/place_recognition/test/directed.py
@@ -1,8 +1,9 @@
 import rostools
-rostools.update_path('star_detector')
+rostools.update_path('place_recognition')
 import rostest
 import pylab, numpy
 import Image
+import starfeature
 
 import sys
 sys.path.append('lib')
@@ -25,8 +26,24 @@ if 1:
   for i in ims:
     vt.add(i)
     
+  def kp_d(frame):
+    sd = starfeature.star_detector(frame.size[0], frame.size[1], 5, 10.0, 10.0)
+    kp = [ (x,y) for (x,y,s,r) in sd.detect(frame.rawdata) ]
+    cl = calonder.classifier()
+    cl.read('/u/mihelich/ros/ros-pkg/vision/calonder_descriptor/src/test/land50_cs.trees.old')
+
+    descriptors = []
+    im = Image.fromstring("L", frame.size, frame.rawdata)
+    matcher = calonder.BruteForceMatcher(176)
+    for (x,y) in kp:
+      patch = im.crop((x-16,y-16,x+16,y+16))
+      sig = cl.getSignature(patch.tostring(), patch.size[0], patch.size[1])
+      descriptors.append(sig)
+    return descriptors
+
   #M = vt.query(ims)
-  M = [ vt.topN(i) for i in ims[:100] ]
+  print "Do query"
+  M = [ vt.topN(i, kp_d(i), 10) for i in ims[:100] ]
 
   pylab.pcolor(numpy.array(M))
   pylab.colorbar()
