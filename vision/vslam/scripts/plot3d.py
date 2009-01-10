c9ca900748a280c1411e840f95a85d987b76a85a
jdchen <jdchen@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-01-10 02:54:50 +0000

    update for plotting 3d curves
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@9172 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/vslam/scripts/plot3d.py
+++ vision/vslam/scripts/plot3d.py
@@ -2,12 +2,15 @@ import pickle
 import pprint
 
 # load pickled files
-trajs_file = open('trajs.pkl', 'rb')
-gt_file    = open('gt.pkl',    'rb')
+trajs_filename = 'trajs.pkl'
+gt_filename    = 'gt.pkl'
+print "loading ",  trajs_filename, "and", gt_filename
+trajs_file = open(trajs_filename, 'rb')
+gt_file    = open(gt_filename,    'rb')
 
 trajectory = pickle.load(trajs_file)
 gt         = pickle.load(gt_file)
-#pprint.pprint(trajectory)
+pprint.pprint(trajectory)
 
 trajs_file.close()
 
@@ -28,21 +31,45 @@ from enthought.tvtk.api import tvtk
 import numpy
 
 # The numpy array data.
-points = array(trajectory[0])
-numpts = len(trajectory[0])
+st_trj_points = array(trajectory[0])
+trj_numpts = len(trajectory[0])
 
-points = array(gt)
-numpts = len(gt)
+st_gt_points  = array(gt)
+gt_numpts = len(gt)
 
-col0 = numpy.arange(0,numpts-1,1)
-col1 = numpy.arange(1,numpts,1)
-lines=array([col0, col1]).transpose()
+if st_trj_points.shape[1]==3:
+  # Old format. No time stamp
+  trj_points = st_trj_points
+else:
+  trj_points = st_trj_points[:,1:4]
+
+if st_gt_points.shape[1]==3:
+  # Old format. No time stamp
+  gt_points = st_gt_points
+else:
+  gt_points = st_gt_points[:,1:4]
+  
+
+col0 = numpy.arange(0,trj_numpts-1,1)
+col1 = numpy.arange(1,trj_numpts,1)
+trj_lines=array([col0, col1]).transpose()
+
+col0 = numpy.arange(0,gt_numpts-1,1)
+col1 = numpy.arange(1,gt_numpts,1)
+gt_lines=array([col0, col1]).transpose()
 
 
 # The TVTK dataset.
-mesh = tvtk.PolyData(points=points, lines=lines)
-mesh.point_data.scalars = numpy.ones(numpts)
-mesh.point_data.scalars.name = 'scalars'
+trj_curve = tvtk.PolyData(points=trj_points, lines=trj_lines)
+# assign temperature in sequence so that we now the "direction" of the curve
+trj_curve.point_data.scalars = numpy.arange(0,trj_numpts,1)
+trj_curve.point_data.scalars.name = 'temperature'
+
+
+gt_curve = tvtk.PolyData(points=gt_points, lines=gt_lines)
+# assign temperature in sequence so that we now the "direction" of the curve
+gt_curve.point_data.scalars = numpy.arange(0, gt_numpts, 1)
+gt_curve.point_data.scalars.name = 'temperature'
 
 # Uncomment the next two lines to save the dataset to a VTK XML file.
 #w = tvtk.XMLPolyDataWriter(input=mesh, file_name='polydata.vtp')
@@ -55,8 +82,14 @@ def view():
     from enthought.mayavi.modules.surface import Surface
 
     mayavi.new_scene()
-    src = VTKDataSource(data = mesh)
-    mayavi.add_source(src)
+    trj_src = VTKDataSource(data = trj_curve)
+    mayavi.add_source(trj_src)
+    mayavi.add_module(Outline())
+    s = Surface()
+    mayavi.add_module(s)
+
+    gt_src = VTKDataSource(data = gt_curve)
+    mayavi.add_source(gt_src)
     mayavi.add_module(Outline())
     s = Surface()
     mayavi.add_module(s)
