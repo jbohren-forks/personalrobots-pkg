573bb535c8e99142b4d81b912c6ec5b469dfe06c
bhaskarama <bhaskarama@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-01-12 16:16:37 +0000

    Construction of roadmap from topological map (partial)
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@9234 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ world_models/topological_map/include/topological_map/gridcell.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef TOPOLOGICAL_MAP_GRIDCELL_H
+#define TOPOLOGICAL_MAP_GRIDCELL_H
+
+#include <utility>
+#include <set>
+#include <iostream>
+
+
+namespace topological_map
+{
+
+// Vertex descriptions
+struct GridCell
+{
+  int first;
+  int second;
+
+  GridCell(int f=0, int s=0) : first(f), second(s) {}
+  int heuristicDistanceTo (const GridCell& p) const;
+
+};
+ 
+ std::ostream& operator<< (std::ostream& str, const GridCell& g);
+ int operator< (const GridCell& c, const GridCell& c2);
+
+ typedef std::set<GridCell> Region;
+
+} // Namespace topological_map
+
+#endif
