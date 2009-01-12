595f2806302b913d0bb76dd311bbda04af410660
jfaustwg <jfaustwg@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-01-12 23:01:18 +0000

    Deleting directory that should have gone away in a merge
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/branches/josh@9275 f5854215-dd47-0410-b2c4-cdd35faa7885

--- util/filter_coeffiecient_server/scripts/startup.m
+++
@@ -1,9 +0,0 @@
-more off;
-
-[status,rosoctpath] = system('rospack find rosoct');
-rosoctpath = strtrim(rosoctpath);
-addpath(fullfile(rosoctpath, 'octave'));
-
-rosoct_add_srvs('filter_generator');
-
-rosoct('shutdown');
