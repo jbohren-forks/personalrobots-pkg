704fcd751297b5e0b2e75e4c70a142a4fcbc86f8
pantofaru <pantofaru@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-01-13 17:43:49 +0000

    The display options are now none, local, and remote. local --> draw the image and the boxes from the node. remote --> publish the rectangles (as lines) and let stereo_view_pixel_info display them.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@9338 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/people/src/face_detection.cpp
+++ vision/people/src/face_detection.cpp
@@ -1,37 +1,37 @@
 /**********************************************************************
-*
-* Software License Agreement (BSD License)
-* 
-*  Copyright (c) 2008, Willow Garage, Inc.
-*  All rights reserved.
-* 
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-* 
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of the Willow Garage nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-* 
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
+ *
+ * Software License Agreement (BSD License)
+ * 
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ * 
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
 
 #include <stdio.h>
 #include <iostream>
@@ -47,6 +47,8 @@
 #include "image_msgs/CamInfo.h"
 #include "image_msgs/Image.h"
 #include "image_msgs/CvBridge.h"
+#include "image_msgs/ColoredLine.h"
+#include "image_msgs/ColoredLines.h"
 #include "topic_synchronizer.h"
 #include "tf/transform_listener.h"
 
@@ -83,7 +85,7 @@ public:
   // The left and disparity images.
   IplImage *cv_image_left_; /**< Left image. */
   IplImage *cv_image_disp_; /**< Disparity image. */
-  bool do_display_; /**< True/false display face detections. */
+  string do_display_; /**< True/false display face detections. */
   IplImage *cv_image_disp_out_; /**< Display image. */
 
   bool use_depth_; /**< True/false use depth information. */
@@ -106,7 +108,7 @@ public:
   boost::mutex cv_mutex_, pos_mutex_;
 
   // ros::node("face_detector", ros::node::ANONYMOUS_NAME ),
-  FaceDetector(std::string node_name, const char *haar_filename, double reliability, bool use_depth, bool do_display, bool external_init) : 
+  FaceDetector(std::string node_name, const char *haar_filename, double reliability, bool use_depth, string do_display, bool external_init) : 
     ros::node(node_name ),
     sync_(this, &FaceDetector::image_cb_all, ros::Duration().fromSec(0.05), &FaceDetector::image_cb_timeout),
     cv_image_left_(0),
@@ -124,7 +126,7 @@ public:
     tf(*this)
   { 
     
-    if (do_display_) {
+    if (do_display_ == "local") {
       // OpenCV: pop up an OpenCV highgui window
       cvNamedWindow("Face detector: Disparity",CV_WINDOW_AUTOSIZE);
       cvNamedWindow("Face detector: Face Detection", CV_WINDOW_AUTOSIZE);
@@ -145,6 +147,9 @@ public:
 
     // Advertise a position measure message.
     advertise<robot_msgs::PositionMeasurement>("people_tracker_measurements",1);
+    // Advertise the rectangles to draw if stereo_view is running.
+    advertise<image_msgs::ColoredLines>("lines_to_draw",1);
+    // Subscribe to filter measurements.
     if (external_init_) {
       subscribe<robot_msgs::PositionMeasurement>("people_tracker_filter",pos_,&FaceDetector::pos_cb,1);
     }
@@ -158,7 +163,7 @@ public:
       cvReleaseImage(&cv_image_disp_out_); cv_image_disp_out_ = 0;
     }
 
-    if (do_display_) {
+    if (do_display_ == "local") {
       cvDestroyWindow("Face detector: Face Detection");
       cvDestroyWindow("Face detector: Disparity");
     }
@@ -192,6 +197,7 @@ public:
    * If unsynced stereo msgs are received, do nothing. 
    */
   void image_cb_timeout(ros::Time t) {
+    cout << "In timeout" << endl;
   }
 
   /*! 
@@ -204,6 +210,8 @@ public:
    */
   void image_cb_all(ros::Time t)
   {
+    ros::Time startt, endt;
+    startt = t.now();
 
     // Update face positions from the filter.
     robot_msgs::PositionMeasurement filter_pos;
@@ -237,8 +245,11 @@ public:
       pos_list_.pop();
     } 
     pos_lock.unlock();
+    // End filter face position update
 
-    boost::mutex::scoped_lock lock(cv_mutex_);
+    if (do_display_ == "local") {
+      cv_mutex_.lock();
+    }
 
     // Kill anyone who hasn't had a filter update in a given time.
     people_->killIfFilterUpdateTimeout(limage_.header.stamp);
@@ -251,7 +262,7 @@ public:
       cv_image_disp_ = dbridge_.toIpl();
     }
     else {
-      lock.unlock();
+      cv_mutex_.unlock();
       return;
     }
 
@@ -263,92 +274,182 @@ public:
     double Tx = -rcinfo_.P[3]/Fx;
     cam_model_ = new CvStereoCamModel(Fx,Fy,Tx,Clx,Crx,Cy,1.0/(double)dispinfo_.dpp);
  
-    if ( cv_image_left_ )  {
-      im_size = cvGetSize(cv_image_left_);
-      vector<CvRect> faces_vector = people_->detectAllFaces(cv_image_left_, haar_filename_, 1.0, cv_image_disp_, cam_model_, true);
+    im_size = cvGetSize(cv_image_left_);
+    vector<Box2D3D> faces_vector = people_->detectAllFaces(cv_image_left_, haar_filename_, 1.0, cv_image_disp_, cam_model_);
+    image_msgs::ColoredLines all_cls;
+    vector<image_msgs::ColoredLine> lines(faces_vector.size());
+    
 
-      // Get the average (median) disparity in the middle half of the bounding box, and compute the face center in 3d. 
-      // Publish the face center as a track point.
-      if (cv_image_disp_ && cam_model_) {
-	int r, c, good_pix;
-	ushort* ptr;
-	double avg_disp;
-	CvRect *one_face;
-	robot_msgs::PositionMeasurement pos;
-	CvMat *uvd = cvCreateMat(1,3,CV_32FC1);
-	CvMat *xyz = cvCreateMat(1,3,CV_32FC1);
-	for (uint iface = 0; iface < faces_vector.size(); iface++) {
-	  one_face = &faces_vector[iface];
-	  good_pix = 0;
-	  avg_disp = 0;
-
-	  // Get the median disparity in the middle half of the bounding box.
-	  avg_disp = cvMedianNonZeroElIn2DArr(cv_image_disp_,
-					      floor(one_face->y+0.25*one_face->height),floor(one_face->y+0.75*one_face->height),
-					      floor(one_face->x+0.25*one_face->width), floor(one_face->x+0.75*one_face->width)); 
-
-	  cvmSet(uvd,0,0,one_face->x+one_face->width/2.0);
-	  cvmSet(uvd,0,1,one_face->y+one_face->height/2.0);
-	  cvmSet(uvd,0,2,avg_disp);
-	  cam_model_->dispToCart(uvd,xyz);
+    // Get the average (median) disparity in the middle half of the bounding box, and compute the face center in 3d. 
+    // Publish the face center as a track point.
+    if (cv_image_disp_ && cam_model_) {
+      Box2D3D *one_face;
+      robot_msgs::PositionMeasurement pos;
+      for (uint iface = 0; iface < faces_vector.size(); iface++) {
+	one_face = &faces_vector[iface];
 	  
+	if (one_face->status != "bad") {
 	  bool do_publish = true;
 	  std::string id = "-1";
 	  if (external_init_) {
 	    // Check if this person's face is close enough to one of the previously known faces and associate it.
 	    // If not, don't publish it.
 	    // If yes, set it's new position and publish it.
-	    int close_person = people_->findPersonFaceLTDist3D(FACE_DIST, cvmGet(xyz,0,0), cvmGet(xyz,0,1), cvmGet(xyz,0,2));
-	    printf("close person %d \n", close_person);
+	    int close_person = people_->findPersonFaceLTDist3D(FACE_DIST, one_face->center3d.val[0], one_face->center3d.val[1], one_face->center3d.val[2]);
 	    if (close_person < 0) {
-	      //do_publish = false;
 	      id = "";
+	      cout << "Closest person: unknown" << endl;
 	    }
 	    else {
 	      id = people_->getID(close_person);
-	      people_->setFaceCenter3D(cvmGet(xyz,0,0), cvmGet(xyz,0,1), cvmGet(xyz,0,2), close_person);
+	      people_->setFaceCenter3D(one_face->center3d.val[0], one_face->center3d.val[1], one_face->center3d.val[2], close_person);
+	      cout << "Closest person: " << id << endl;
 	    }
 	  }
 
+	  // Publish the face center.
 	  if (do_publish) {
 	    pos.header.stamp = limage_.header.stamp;
 	    pos.name = node_name_;
 	    pos.object_id = id;
-	    pos.pos.x = cvmGet(xyz,0,2);
-	    pos.pos.y = -1.0*cvmGet(xyz,0,0);
-	    pos.pos.z = -1.0*cvmGet(xyz,0,1);
+	    pos.pos.x = one_face->center3d.val[2]; 
+	    pos.pos.y = -1.0*one_face->center3d.val[0];
+	    pos.pos.z = -1.0*one_face->center3d.val[1]; 
 	    pos.header.frame_id = limage_.header.frame_id;//"stereo_link";
 	    pos.reliability = reliability_;
 	    pos.initialization = 0;
-	    pos.covariance[0] = 0.04;
-	    pos.covariance[1] = 0.0;
-	    pos.covariance[2] = 0.0;
-	    pos.covariance[3] = 0.0;
-	    pos.covariance[4] = 0.04;
-	    pos.covariance[5] = 0.0;
-	    pos.covariance[6] = 0.0;
-	    pos.covariance[7] = 0.0;
-	    pos.covariance[8] = 0.04;
+	    pos.covariance[0] = 0.04; pos.covariance[1] = 0.0;  pos.covariance[2] = 0.0;
+	    pos.covariance[3] = 0.0;  pos.covariance[4] = 0.04; pos.covariance[5] = 0.0;
+	    pos.covariance[6] = 0.0;  pos.covariance[7] = 0.0;  pos.covariance[8] = 0.04;
 	    publish("people_tracker_measurements",pos);
 	  }
 	}
-
-	cvReleaseMat(&uvd);
-	cvReleaseMat(&xyz);	
-      
-	if (do_display_) {
-	  if (!cv_image_disp_out_) {
-	    cv_image_disp_out_ = cvCreateImage(im_size,IPL_DEPTH_8U,1);
+	
+	// Draw an appropriately colored rectangle on the display image. 
+	if (do_display_ != "none") { 
+	  CvScalar color;
+	  if (one_face->status == "good") {
+	    color = cvScalar(0,255,0);
 	  }
-	  cvCvtScale(cv_image_disp_,cv_image_disp_out_,4.0/dispinfo_.dpp);
+	  else if (one_face->status == "unknown") {
+	    color = cvScalar(255,0,0);
+	  }
+	  else {
+	    color = cvScalar(0,0,255);
+	  }
 
-	  cvShowImage("Face detector: Face Detection",cv_image_left_);
-	  cvShowImage("Face detector: Disparity",cv_image_disp_out_);
-	}
-      }
-      
+	  if (do_display_ == "local") {
+	    cvRectangle(cv_image_left_, 
+			cvPoint(one_face->box2d.x,one_face->box2d.y), 
+			cvPoint(one_face->box2d.x+one_face->box2d.width, one_face->box2d.y+one_face->box2d.height), color, 4);
+	  }
+	  else if (do_display_ == "remote") {
+	    //image_msgs::ColoredLine l;
+	    //all_cls.lines.push_back(l);
+	    //all_cls.lines.push_back(l);
+	    //all_cls.lines.push_back(l);
+	    //all_cls.lines.push_back(l);
+
+	    // 	  all_cls.lines[4*iface].r = color.val[2]; all_cls.lines[4*iface+1].r = all_cls.lines[4*iface].r; 
+	    // 	  all_cls.lines[4*iface+2].r = all_cls.lines[4*iface].r; all_cls.lines[4*iface+3].r = all_cls.lines[4*iface].r;
+	    // 	  all_cls.lines[4*iface].g = color.val[1]; all_cls.lines[4*iface+1].g = all_cls.lines[4*iface].g; 
+	    // 	  all_cls.lines[4*iface+2].g = all_cls.lines[4*iface].g; all_cls.lines[4*iface+3].g = all_cls.lines[4*iface].g;
+	    // 	  all_cls.lines[4*iface].b = color.val[0]; all_cls.lines[4*iface+1].b = all_cls.lines[4*iface].b; 
+	    // 	  all_cls.lines[4*iface+2].b = all_cls.lines[4*iface].b; all_cls.lines[4*iface+3].b = all_cls.lines[4*iface].b;
+
+	    // 	  all_cls.lines[4*iface].x0 = one_face->box2d.x; 
+	    // 	  all_cls.lines[4*iface].x1 = one_face->box2d.x + one_face->box2d.width;
+	    // 	  all_cls.lines[4*iface].y0 = one_face->box2d.y; 
+	    // 	  all_cls.lines[4*iface].y1 = one_face->box2d.y;
+
+	    // 	  all_cls.lines[4*iface+1].x0 = one_face->box2d.x; 
+	    // 	  all_cls.lines[4*iface+1].x1 = one_face->box2d.x;
+	    // 	  all_cls.lines[4*iface+1].y0 = one_face->box2d.y; 
+	    // 	  all_cls.lines[4*iface+1].y1 = one_face->box2d.y + one_face->box2d.height;
+
+	    // 	  all_cls.lines[4*iface+2].x0 = one_face->box2d.x; 
+	    // 	  all_cls.lines[4*iface+2].x1 = one_face->box2d.x + one_face->box2d.width;
+	    // 	  all_cls.lines[4*iface+2].y0 = one_face->box2d.y + one_face->box2d.height; 
+	    // 	  all_cls.lines[4*iface+2].y1 = one_face->box2d.y + one_face->box2d.height;
+
+	    // 	  all_cls.lines[4*iface+3].x0 = one_face->box2d.x + one_face->box2d.width; 
+	    // 	  all_cls.lines[4*iface+3].x1 = one_face->box2d.x + one_face->box2d.width;
+	    // 	  all_cls.lines[4*iface+3].y0 = one_face->box2d.y; 
+	    // 	  all_cls.lines[4*iface+3].y1 = one_face->box2d.y + one_face->box2d.height;
+
+	    // 	  all_cls.lines[4*iface].header.stamp = limage_.header.stamp;
+	    // 	  all_cls.lines[4*iface+1].header.stamp = limage_.header.stamp;
+	    // 	  all_cls.lines[4*iface+2].header.stamp = limage_.header.stamp;
+	    // 	  all_cls.lines[4*iface+3].header.stamp = limage_.header.stamp;
+	    // 	  all_cls.lines[4*iface].header.frame_id = limage_.header.frame_id;
+	    // 	  all_cls.lines[4*iface+1].header.frame_id = limage_.header.frame_id;
+	    // 	  all_cls.lines[4*iface+2].header.frame_id = limage_.header.frame_id;
+	    // 	  all_cls.lines[4*iface+3].header.frame_id = limage_.header.frame_id;
+	  
+	  
+	    lines[4*iface].r = color.val[2]; lines[4*iface+1].r = lines[4*iface].r; 
+	    lines[4*iface+2].r = lines[4*iface].r; lines[4*iface+3].r = lines[4*iface].r;
+	    lines[4*iface].g = color.val[1]; lines[4*iface+1].g = lines[4*iface].g; 
+	    lines[4*iface+2].g = lines[4*iface].g; lines[4*iface+3].g = lines[4*iface].g;
+	    lines[4*iface].b = color.val[0]; lines[4*iface+1].b = lines[4*iface].b; 
+	    lines[4*iface+2].b = lines[4*iface].b; lines[4*iface+3].b = lines[4*iface].b;
+
+	    lines[4*iface].x0 = one_face->box2d.x; 
+	    lines[4*iface].x1 = one_face->box2d.x + one_face->box2d.width;
+	    lines[4*iface].y0 = one_face->box2d.y; 
+	    lines[4*iface].y1 = one_face->box2d.y;
+
+	    lines[4*iface+1].x0 = one_face->box2d.x; 
+	    lines[4*iface+1].x1 = one_face->box2d.x;
+	    lines[4*iface+1].y0 = one_face->box2d.y; 
+	    lines[4*iface+1].y1 = one_face->box2d.y + one_face->box2d.height;
+
+	    lines[4*iface+2].x0 = one_face->box2d.x; 
+	    lines[4*iface+2].x1 = one_face->box2d.x + one_face->box2d.width;
+	    lines[4*iface+2].y0 = one_face->box2d.y + one_face->box2d.height; 
+	    lines[4*iface+2].y1 = one_face->box2d.y + one_face->box2d.height;
+
+	    lines[4*iface+3].x0 = one_face->box2d.x + one_face->box2d.width; 
+	    lines[4*iface+3].x1 = one_face->box2d.x + one_face->box2d.width;
+	    lines[4*iface+3].y0 = one_face->box2d.y; 
+	    lines[4*iface+3].y1 = one_face->box2d.y + one_face->box2d.height;
+
+	    lines[4*iface].header.stamp = limage_.header.stamp;
+	    lines[4*iface+1].header.stamp = limage_.header.stamp;
+	    lines[4*iface+2].header.stamp = limage_.header.stamp;
+	    lines[4*iface+3].header.stamp = limage_.header.stamp;
+	    lines[4*iface].header.frame_id = limage_.header.frame_id;
+	    lines[4*iface+1].header.frame_id = limage_.header.frame_id;
+	    lines[4*iface+2].header.frame_id = limage_.header.frame_id;
+	    lines[4*iface+3].header.frame_id = limage_.header.frame_id;
+	  }
+	}	
+      } // End for iface
     }
-    lock.unlock();
+    // Display
+    if (do_display_ == "remote") {
+      all_cls.header.stamp = limage_.header.stamp;
+      all_cls.label = node_name_;
+      all_cls.header.frame_id = limage_.header.frame_id;
+      all_cls.lines = lines;
+      publish("lines_to_draw",all_cls);
+    }
+    else if (do_display_ == "local") {
+      if (!cv_image_disp_out_) {
+	cv_image_disp_out_ = cvCreateImage(im_size,IPL_DEPTH_8U,1);
+      }
+      cvCvtScale(cv_image_disp_,cv_image_disp_out_,4.0/dispinfo_.dpp);
+      cvShowImage("Face detector: Face Detection",cv_image_left_);
+      cvShowImage("Face detector: Disparity",cv_image_disp_out_);
+    }
+
+    if (do_display_=="local") {
+      cv_mutex_.unlock();
+    }
+
+    endt = t.now();
+    ros::Duration diff = endt-startt;
+    printf("Image callback duration = %fsec\n", diff.toSec());
   }
 
 
@@ -358,17 +459,19 @@ public:
   bool spin() {
     while (ok() && !quit_) {
 
-      // Display all of the images.
-      boost::mutex::scoped_lock lock(cv_mutex_);
+      if (do_display_ == "local") {
+	// Display all of the images.
+	boost::mutex::scoped_lock lock(cv_mutex_);
 
-      // Get user input and allow OpenCV to refresh windows.
-      int c = cvWaitKey(2);
-      c &= 0xFF;
-      // Quit on ESC, "q" or "Q"
-      if((c == 27)||(c == 'q')||(c == 'Q'))
-	quit_ = true;
+	// Get user input and allow OpenCV to refresh windows.
+	int c = cvWaitKey(2);
+	c &= 0xFF;
+	// Quit on ESC, "q" or "Q"
+	if((c == 27)||(c == 'q')||(c == 'Q'))
+	  quit_ = true;
 
-      lock.unlock(); 
+	lock.unlock(); 
+      }
       usleep(10000);
 
     }
@@ -383,7 +486,7 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv);
   bool use_depth = true;
-  bool do_display = true;
+  string do_display = "none";
   bool external_init = true;
   double reliability = 0.8;
 
@@ -395,7 +498,7 @@ int main(int argc, char **argv)
   if (argc >= 4) {
     reliability = atof(argv[3]);
     if (argc >= 5) {
-      do_display = atoi(argv[4]);
+      do_display = argv[4];
       if (argc >= 6) {
 	external_init = atoi(argv[5]);
       }
