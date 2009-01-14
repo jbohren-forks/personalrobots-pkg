ca4c15d77a192c5c9314bcb8f2a42e28f1a95e0c
morgan_quigley <morgan_quigley@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-01-14 23:51:05 +0000

    update irobot create driver so it actually does something
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@9466 f5854215-dd47-0410-b2c4-cdd35faa7885

--- drivers/robot/irobot_create/include/irobot_create/irobot_create.h
+++ drivers/robot/irobot_create/include/irobot_create/irobot_create.h
@@ -3,18 +3,94 @@
 
 #include "rosconsole/rosconsole.h"
 #include "serial_port/lightweightserial.h"
+#include <time.h>
+#include <cstdio>
+#include <iostream>
+#include <unistd.h>
 
+
+using namespace std;
 class IRobotCreate
 {
 public:
   IRobotCreate();
   ~IRobotCreate();
 
-  bool getEncoders(double &x, double &y, double &th);
-  bool setVelocity(double x_vel, double w_vel); // linear, angular velocities
-
-  LightweightSerial *serial_port;
+	double getReadings(int & left, int  &right); //distance traveled in mm, angle turned in degree
+//	double getAngle(); //in degree
+	bool setVelocity(double x_vel, double w_vel); // linear, angular velocities	
+	bool setWheels(double l_vel, double r_vel); //left and right wheel velocities
+	bool Start();
+    LightweightSerial *serial_port;
+private:
+  void Wait();		//20 us
+  void Wait(double useconds);
+  void toOpcode(int decimal, int & high, int & low); //convert int to 2 uint8_t
+  int toDecimal(int decimal); //convert 2 uint8_t to int
+	
 };
+//1 byte = 8 bits
+#define CREATE_OPCODE_START            128
+#define CREATE_OPCODE_BAUD             129
+#define CREATE_OPCODE_SAFE             131
+#define CREATE_OPCODE_FULL             132
+#define CREATE_OPCODE_SPOT             134
+#define CREATE_OPCODE_COVER            135
+#define CREATE_OPCODE_DEMO             136
+#define CREATE_OPCODE_DRIVE            137
+#define CREATE_OPCODE_MOTORS           138
+#define CREATE_OPCODE_LEDS             139
+#define CREATE_OPCODE_SONG             140
+#define CREATE_OPCODE_PLAY             141
+#define CREATE_OPCODE_SENSORS          142
+#define CREATE_OPCODE_COVERDOCK        143
+#define CREATE_OPCODE_PWM_MOTORS       144
+#define CREATE_OPCODE_DRIVE_WHEELS     145
+#define CREATE_OPCODE_DIGITAL_OUTPUTS  147
+#define CREATE_OPCODE_STREAM           148
+#define CREATE_OPCODE_QUERY_LIST       149
+#define CREATE_OPCODE_DO_STREAM        150
+#define CREATE_OPCODE_SEND_IR_CHAR     151
+#define CREATE_OPCODE_SCRIPT           152
+#define CREATE_OPCODE_PLAY_SCRIPT      153
+#define CREATE_OPCODE_SHOW_SCRIPT      154
+#define CREATE_OPCODE_WAIT_TIME        155
+#define CREATE_OPCODE_WAIT_DISTANCE    156
+#define CREATE_OPCODE_WAIT_ANGLE       157
+#define CREATE_OPCODE_WAIT_EVENT       158
+
+
+#define CREATE_DELAY_MODECHANGE_MS      5000
+
+#define CREATE_MODE_OFF                  0
+#define CREATE_MODE_PASSIVE              1
+#define CREATE_MODE_SAFE                 2
+#define CREATE_MODE_FULL                 3
+
+#define CREATE_TVEL_MAX_MM_S           500     
+#define CREATE_RADIUS_MAX_MM          2000
+
+#define CREATE_SENSOR_PACKET_SIZE       26
+
+#define CREATE_CHARGING_NOT              0
+#define CREATE_CHARGING_RECOVERY         1
+#define CREATE_CHARGING_CHARGING         2
+#define CREATE_CHARGING_TRICKLE          3
+#define CREATE_CHARGING_WAITING          4
+#define CREATE_CHARGING_ERROR            5
+#define CREATE_SENSOR_BUMPER           7
+#define CREATE_SENSOR_DISTANCE		19
+#define CREATE_SENSOR_ANGLE		20
+#define CREATE_REQUESTED_VELOCITY		39
+#define CREATE_REQUESTED_RADIUS		40
+#define CREATE_REQUESTED_RVELOCITY		41
+#define CREATE_REQUESTED_LVELOCITY		42
+
+#define CREATE_AXLE_LENGTH            0.258
+
+#define CREATE_DIAMETER 0.33
+
+#define CREATE_BUMPER_XOFFSET 0.05
 
 #endif
 
--- drivers/robot/irobot_create/irobot_create.cpp
+++ drivers/robot/irobot_create/irobot_create.cpp
@@ -1,15 +1,14 @@
 #include "rosconsole/rosconsole.h"
 #include "irobot_create/irobot_create.h"
-#include <cstdlib>
 
 IRobotCreate::IRobotCreate()
 {
-  // see if the IROBOT_CREATE_PORT variable is defined
+  // see if the IROBOT_CREATE_OPPORT variable is defined
   const char *default_port = "/dev/ttyUSB0";
   const char *port_env = getenv("IROBOT_CREATE_PORT");
   const char *serial_port_str = (port_env ? port_env : default_port);
   ROS_DEBUG("irobotcreate ctor, serial_port_str = [%s]", serial_port_str);
-  serial_port = new LightweightSerial(serial_port_str, 115200);
+  serial_port = new LightweightSerial(serial_port_str, 57600);
 }
 
 IRobotCreate::~IRobotCreate()
@@ -20,18 +19,243 @@ IRobotCreate::~IRobotCreate()
   serial_port = NULL;
   ROS_DEBUG("irobotcreate dtor complete");
 }
-
-bool IRobotCreate::getEncoders(double &x, double &y, double &th)
-{
-  x = 0;
-  y = 0; 
-  th = 0;
-  return true;
+void IRobotCreate::Wait(double useconds) {
+/*
+	time_t start,end;  
+        time (&start);
+	while	(true) {
+		time (&end);
+		double dif = difftime (end, start);
+		if (dif >=seconds) break;
+	}
+*/
+	usleep(useconds);
 }
 
+void IRobotCreate::Wait() {
+	usleep(CREATE_DELAY_MODECHANGE_MS);
+}
+
+bool IRobotCreate::Start() {
+
+//	Wait();
+	serial_port->write((uint8_t)CREATE_OPCODE_START);
+	Wait();
+	
+//	int usleep(useconds_t useconds);
+
+//	Wait(1.0);
+	serial_port->write((uint8_t)CREATE_OPCODE_FULL);
+	Wait();
+//	Wait(1.0);
+	ROS_DEBUG("irobotcreate started");
+//	serial_port->write((uint8_t)CREATE_OPCODE_COVER);usleep(20);
+//	Wait(1.0);
+//	ROS_DEBUG("here?");
+	return true;
+}
+
+
+double IRobotCreate::getReadings(int &left, int &right)
+{
+	uint8_t input1, input2;
+//	serial_port->read(&input1);
+//	Wait();
+	
+	serial_port->write((uint8_t)CREATE_OPCODE_QUERY_LIST);
+	Wait();
+	uint8_t number = 5;
+	serial_port->write((uint8_t)number);	Wait();
+	serial_port->write((uint8_t)CREATE_REQUESTED_VELOCITY); Wait();
+	serial_port->write((uint8_t)CREATE_REQUESTED_RADIUS); Wait();
+	serial_port->write((uint8_t)CREATE_SENSOR_DISTANCE); Wait();
+	serial_port->write((uint8_t)CREATE_SENSOR_ANGLE); Wait();
+	serial_port->write((uint8_t)CREATE_SENSOR_BUMPER); Wait();
+
+	serial_port->read(&input1);
+	Wait();
+	serial_port->read(&input2);
+	Wait();
+	ROS_DEBUG("velocity requested = %d %d mm/s", input1, input2);
+	ROS_DEBUG("velocity requested = %d mm/s",  toDecimal(input1 * 256+ input2));
+	
+	serial_port->read(&input1);
+	Wait();
+	serial_port->read(&input2);
+	Wait();
+	ROS_DEBUG("radius requested = %d %d mm", input1, input2);
+	ROS_DEBUG("radius requested = %d mm",  toDecimal(input1 * 256+ input2));
+	
+	serial_port->read(&input1);
+	Wait();
+	serial_port->read(&input2);
+	Wait();
+	ROS_DEBUG("distance traveled = %d %d mm", input1, input2);	
+	ROS_DEBUG("distance traveled = %d mm",  toDecimal(input1 * 256+ input2));
+
+	serial_port->read(&input1);
+	Wait();
+	serial_port->read(&input2);
+	Wait();
+	ROS_DEBUG("angle changed = %d %d degree", input1, input2);	
+	ROS_DEBUG("angle changed = %d degree", toDecimal(input1 * 256+ input2));
+
+	serial_port->read(&input1);
+	Wait();
+	left =input1 &1;
+	right = (input1>>1) & 1;
+	ROS_DEBUG("bumper %d", input1);
+	ROS_DEBUG("bumper right %d left %d", left, right);
+	
+/*
+//	serial_port->write((uint8_t)CREATE_OPCODE_SENSORS);
+//	Wait();
+	serial_port->write((uint8_t)CREATE_SENSOR_DISTANCE);
+//	uint8_t input1, input2;
+	serial_port->read(&input1);
+	Wait();
+	serial_port->read(&input2);
+	Wait();
+	ROS_DEBUG("distance traveled = %d %d mm", input1, input2);	
+	ROS_DEBUG("distance traveled = %d mm",  toDecimal(input1 * 256+ input2));
+*/	return 1.0;
+
+}
+
+int IRobotCreate::toDecimal(int decimal) {
+	int bin[16]; int sign = 1;
+/*	ROS_DEBUG("%d",decimal);
+	sprintf( hex, "%08x", decimal);
+	ROS_DEBUG("%s",hex);
+	int i = atoi(hex);
+	ROS_DEBUG("%d",i);
+	sprintf(dec, "%08x", i);
+	ROS_DEBUG("%s",dec);
+	int j = atoi(dec);
+	ROS_DEBUG("%d",j);
+	*/
+//	ROS_DEBUG("%d",decimal);
+//	itoa(decimal, bin, 2);	
+	for (int i = 15; i >=0; i--) {
+		bin[i] = decimal % 2;
+		decimal = decimal / 2;
+//		cout << ' ' << bin[i];
+	}
+	if (bin[0] == 1) {
+		int pos = 15; sign = -1;
+		while (bin[pos] == 0 && pos>=0) pos--;
+		for (int i = 0; i< pos; i++) 
+			bin[i] = 1 - bin[i];
+	}
+	int sum = 0; int factor = 1;
+	for (int i = 15; i >= 0; i--) {
+//		cout << ' ' << bin[i];
+		sum +=bin[i] * factor;
+		factor *=2;
+	}
+	return sum*sign;
+}
+/*
+double IRobotCreate::getAngle()
+{
+	serial_port->write((uint8_t)CREATE_OPCODE_SENSORS);
+	Wait();
+	serial_port->write((uint8_t)CREATE_SENSOR_ANGLE);
+	uint8_t input1, input2;
+	serial_port->read(&input1);
+	Wait();
+	serial_port->read(&input2);
+	Wait();
+	ROS_DEBUG("angle changed = %d %d degree", input1, input2);	
+	ROS_DEBUG("angle changed = %d degree", toDecimal(input1 * 256+ input2));
+  return 1.0;
+}
+*/
 bool IRobotCreate::setVelocity(double x_vel, double w_vel)
 {
-  ROS_DEBUG("setting velocity to %f, %f", x_vel, w_vel);
-  return true;
+//convert linear (m/s) and angular velocity (rad/s) into velocity (mm/s) and radius (mm)
+
+	int velocity = ((int)(x_vel * 1000.0));
+	if (velocity > CREATE_TVEL_MAX_MM_S) velocity = CREATE_TVEL_MAX_MM_S;
+		else if (velocity <-CREATE_TVEL_MAX_MM_S) velocity = -CREATE_TVEL_MAX_MM_S;
+	
+	int radius;
+	if (w_vel == 0.0) radius = CREATE_RADIUS_MAX_MM;
+	else radius = ((int)(x_vel / w_vel * 1000.0));
+	if (radius > CREATE_RADIUS_MAX_MM) radius = CREATE_RADIUS_MAX_MM;
+		else if (radius <-CREATE_RADIUS_MAX_MM) radius = -CREATE_RADIUS_MAX_MM;
+
+	int high1, low1, high2, low2;
+	toOpcode(velocity, high1, low1);
+	toOpcode(radius, high2, low2);
+	
+	
+	serial_port->write((uint8_t)CREATE_OPCODE_DRIVE);
+	Wait();
+	serial_port->write((uint8_t)high1);
+	Wait();
+	serial_port->write((uint8_t)low1);
+	Wait();
+	serial_port->write((uint8_t)high2);
+	Wait();
+	serial_port->write((uint8_t)low2);
+	Wait();
+
+
+//	serial_port->write((uint8_t)CREATE_OPCODE_COVER);
+//	Wait();
+	ROS_DEBUG("setting velocity to %d mm/s, radius %d mm", velocity, radius);
+	ROS_DEBUG("setting velocity to code [%d][%d], [%d][%d]", high1, low1, high2, low2);
+	return true;
 }
 
+bool IRobotCreate::setWheels(double l_vel, double r_vel)
+{
+//left and right wheel velocities (in mm/s)
+	int left = l_vel *1000; int right = r_vel*1000;
+	if (left > CREATE_TVEL_MAX_MM_S) left= CREATE_TVEL_MAX_MM_S;
+	else if (left < -CREATE_TVEL_MAX_MM_S) left= -CREATE_TVEL_MAX_MM_S;
+	if (right > CREATE_TVEL_MAX_MM_S) right= CREATE_TVEL_MAX_MM_S;
+	else if (right < -CREATE_TVEL_MAX_MM_S) right= -CREATE_TVEL_MAX_MM_S;
+	
+	int high1, low1, high2, low2;
+	toOpcode(right, high1, low1);
+	toOpcode(left, high2, low2);
+	
+	
+	serial_port->write((uint8_t)CREATE_OPCODE_DRIVE_WHEELS);
+	Wait();
+	serial_port->write((uint8_t)high1);
+	Wait();
+	serial_port->write((uint8_t)low1);
+	Wait();
+	serial_port->write((uint8_t)high2);
+	Wait();
+	serial_port->write((uint8_t)low2);
+	Wait();
+
+
+//	serial_port->write((uint8_t)CREATE_OPCODE_COVER);
+//	Wait();
+	ROS_DEBUG("setting velocity to left %d mm/s, right %d mm/s", left, right);
+	ROS_DEBUG("setting velocity to code [%d][%d], [%d][%d]", high1, low1, high2, low2);
+	return true;
+}
+void IRobotCreate::toOpcode(int decimal, int & high, int & low) {
+	char hex[8];
+	sprintf( hex, "%08x", decimal);
+//	for (int i = 0; i < 8; i++ ) {
+//		printf ("%c", hex[i]);
+//	}
+	int first, second;
+	if (hex[4]>='0' && hex[4]<='9')	{ first = hex[4] - '0'; }
+		else if (hex[4]>='a' && hex[4]<='f')  { first = hex[4] - 'a' +10; }
+	if (hex[5]>='0' && hex[5]<='9')	{ second = hex[5] - '0'; }
+		else if (hex[5]>='a' && hex[5]<='f')  { second = hex[5] - 'a' +10; }
+	high = first *16 + second;
+	if (hex[6]>='0' && hex[6]<='9')	{ first = hex[6] - '0'; }
+		else if (hex[6]>='a' && hex[6]<='f')  { first = hex[6] - 'a' +10; }
+	if (hex[7]>='0' && hex[7]<='9')	{ second = hex[7] - '0'; }
+		else if (hex[7]>='a' && hex[7]<='f')  { second = hex[7] - 'a' +10; }
+	low = first *16 + second;
+}
