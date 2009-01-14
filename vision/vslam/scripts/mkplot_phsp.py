66c862e9013ea8c45e1c332cff32d81e91974bc4
jdchen <jdchen@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-01-14 06:32:40 +0000

    lots of stuffs - pickling skeletons
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@9405 f5854215-dd47-0410-b2c4-cdd35faa7885

--- vision/vslam/scripts/mkplot_phsp.py
+++ vision/vslam/scripts/mkplot_phsp.py
@@ -89,6 +89,26 @@ if 0:
 else:
   skipto = None
   start,end = 0, 10000
+  
+keyframe_list_bag5 = [0, 158, 169, 194, 229, 251, 260, 268, 278, 288, 359,
+                      #370, # insert another one 
+                      387, 
+                      #400, # insert another one
+                      433, 503, 517, 608, 642, 668, 698, 719, 740, 767, 795, 821, 842, 880, 893, 928, 970, 1002, 1040, 1105, 1144, 1200, 1286, 1330, 1356, 1388, 1418, 1453, 1482, 1508, 1525, 1553, 1570, 1590, 1612, 1634, 1647, 1672, 1718, 1749, 1765, 1782, 1800, 1828, 1877, 1917, 1975, 2012, 2097, 2115, 2133, 2174, 2275, 
+                      2304, 2342,
+                      2350, # add this frame to remove a kink
+                      2358, 2381, 
+                      2435, 2468, 2488, 2502, 2541, 2555,2766,2767]
+keyframe_list_bag5t = [0,2012, 2097, 2115, 2133, 2174, 2275, 2304, 2342, 
+                      # 2358, 2381, 
+                      2435, 2468, 
+                      2488, 2502, 
+                      2541, 2555,
+                      2766, 2767]
+#keyframe_list_bag5 = [0, 2555, 2766, 2767]
+keyframe_list_bag6 = [0, 192, 278, 287, 327, 357, 394, 439, 469, 504, 529, 593, 636, 677, 711, 773, 834, 888, 940, 980, 999, 1038, 1057, 1090, 1112, 1133, 1216, 1254, 1279, 1323, 1370, 1396, 1424, 1494, 1566, 1631, 1673, 1695, 1724, 1751, 1768, 1781, 1795, 1830, 1847, 1980, 2009, 2030, 2063, 2073, 2112, 2138, 2165, 2188, 2220, 2242, 2260, 2281, 2291, 2310, 2360, 2387, 2428, 2437, 2480, 2505, 2527, 2573, 2590, 2614, 2643, 2658, 2666, 2677, 2720, 2759, 2783, 2802, 2838, 2867, 2890, 2934, 2956, 2975, 2996, 3019, 3046, 3059, 3097, 3133, 3152, 3182, 3215, 3245, 3286, 3312, 3343, 3368, 3390, 3430, 3480, 3508, 3526, 3560, 3584, 3624, 3639, 3680, 3718, 3754, 3787, 3809, 3821, 3840, 3864, 3939, 3965, 3984, 3997, 4042, 4066, 4092, 4111, 4129, 4164, 4174, 4180, 4187, 4206, 4227, 4249, 4261, 4302, 4338, 4381, 4396, 4409, 4624, 4625]
+keyframe_list = keyframe_list_bag5
+print 'set of keyframe set in bag5', len(keyframe_list)
 
 print "starting loop"
 f = open(filename)
@@ -104,12 +124,16 @@ for topic, msg, timeStamp in rosrecord.logplayer(filename):
     if not cam:
       cam = camera.StereoCamera(msg.right_info)
       vos = [
-        VisualOdometer(cam, scavenge = True, 
+        VisualOdometer(cam, 
+                       # scavenge = True, 
+                       scavenge = False, 
                        #feature_detector = FeatureDetectorFast(), 
                        feature_detector = FeatureDetectorHarris(), 
                        inlier_error_threshold = 3.0, 
-                       sba = (10,10,10),
-                       # sba = None,
+                       # sba = (1,100,10),
+                       # sba = (1,1,10),
+                       # sba = (100,10,10),
+                       sba = None,
                        inlier_thresh = 100,
                        position_keypoint_thresh = 0.2, angle_keypoint_thresh = 0.15)
       ]
@@ -119,22 +143,31 @@ for topic, msg, timeStamp in rosrecord.logplayer(filename):
       vo_v = [ [] for i in vos]
       trajectory = [ [] for i in vos]
       stampedTrajectory = [ [] for i in vos]
+      # jdc turning off skeletoning
       skel = Skeleton(cam)
+      # skel = None
       oe_x = []
       oe_y = []
       oe_home = None
       stampedGroundTruth = []
     if framecounter == end:
       break
-    if start <= framecounter and (framecounter % 1) == 0:
+    # jdc debugging 
+    if start <= framecounter and (framecounter % 1) == 0 and (keyframe_list != [] and framecounter in keyframe_list):
       for i,vo in enumerate(vos):
-        af = SparseStereoFrame(dcamImage(msg.left_image), dcamImage(msg.right_image))
+        imgL = dcamImage(msg.left_image)
+        imgR = dcamImage(msg.right_image)
+        if not first_pair:
+          first_pair = (imgL, imgR)
+        af = SparseStereoFrame(imgL, imgR)
+        # af = SparseStereoFrame(dcamImage(msg.left_image), dcamImage(msg.right_image))
+        
         
         # jdc debugging
         Image.fromstring("L", af.size, af.rawdata).save("/tmp/mkplot-left.png")   
              
         vo.handle_frame(af)
-        if i == 0:
+        if i == 0 and skel:
           skel.add(vo.keyframe)
         x,y,z = vo.pose.xform(0,0,0)
         trajectory[i].append((x,y,z))
@@ -178,7 +211,7 @@ print "There are", len([t for t in vo.tracks if t.alive]), "live tracks"
 print "There are", len(set([t.p[-1] for t in vo.tracks if t.alive])), "unique endpoints on live tracks"
 
 quality_pose = Pose()
-if 0:
+if 1:
   # Attempt to compute best possible end-to-end pose
   vo = VisualOdometer(cam, feature_detector = FeatureDetector4x4(FeatureDetectorHarris), scavenge = True)
   f0 = SparseStereoFrame(*first_pair)
@@ -219,8 +252,9 @@ for i in range(len(vos)):
 
 pylab.plot(oe_x, oe_y, c = 'green', label = ground_truth_label)
 
-#skel.optimize()
-skel.plot('blue')
+if skel:
+  #skel.optimize()
+  skel.plot('blue')
 
 xlim = pylab.xlim()
 ylim = pylab.ylim()
@@ -235,7 +269,9 @@ pylab.legend()
 pylab.savefig("foo.png", dpi=200)
 pylab.show()
 
-#pickling
+#
+# pickling
+#
 output_trajectory = open('trajs.pkl','wb')
 output_gt         = open('gt.pkl', 'wb')
 pickle.dump(stampedTrajectory,  output_trajectory)
@@ -248,7 +284,6 @@ stampedKeyFrameTrajectory = [ [] for i in vos]
 for i,vo in enumerate(vos):
   k=0;
   for keyframeId in vo.log_keyframes:
-    print 'keyframe id', keyframeId,i,k
     stampedKeyFrameTrajectory[i].append(stampedTrajectory[i][keyframeId])
     k+=1
     
@@ -256,6 +291,23 @@ output_keyframe_trajectory = open('keyframe_trajs.pkl','wb')
 pickle.dump(stampedKeyFrameTrajectory, output_keyframe_trajectory)
 output_keyframe_trajectory.close()
 
+# pickling the skeleton
+if 1 and skel:
+  pts = dict([ (f,skel.newpose(f.id).xform(0,0,0)) for f in skel.nodes ])
+  nodepts = pts.values()
+
+  skel_edges = []
+  for (f0,f1) in skel.edges:
+    p0 = pts[f0]
+    p1 = pts[f1]
+    skel_edges.append([p0, p1])
+  output_skeleton_nodes = open('skeleton_nodes.pkl','wb')
+  output_skeleton_edges = open('skeleton_edges.pkl','wb')
+  pickle.dump(nodepts, output_skeleton_nodes)
+  pickle.dump(skel_edges, output_skeleton_edges)
+  output_skeleton_edges.close()
+  output_skeleton_nodes.close()
+    
 for vo in vos:
   print vo.name()
   print "distance from start:", vo.pose.distance()
