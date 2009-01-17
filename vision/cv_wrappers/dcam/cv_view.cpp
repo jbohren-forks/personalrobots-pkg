f74d0b9c36a9512febbab7258c158fcac5b1473b
sfkwc <sfkwc@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-01-17 01:41:11 +0000

    new cv_wrappers package to replace cv_*. Currently blacklisted until realtime_thread issues are resolved
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@9605 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ vision/cv_wrappers/dcam/cv_view.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <vector>
+#include "opencv/cxcore.h"
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
+#include "ros/node.h"
+#include "image_msgs/Image.h"
+#include "image_msgs/CvBridge.h"
+
+using namespace std;
+using namespace ros;
+
+class CvView : public node
+{
+public:
+  image_msgs::Image img;
+  image_msgs::CvBridge bridge;
+
+  CvView() : node("cv_view")
+  { 
+    cvNamedWindow("cv_view", CV_WINDOW_AUTOSIZE);
+    subscribe("image", img, &CvView::image_cb, 1);
+  }
+  void image_cb()
+  {
+    bridge.fromImage(img);
+
+    cvShowImage("cv_view", bridge.toIpl());
+
+    cvWaitKey(5);
+  }
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv);
+  CvView view;
+  view.spin();
+  ros::fini();
+  return 0;
+}
+
---
+++ vision/cv_wrappers/minimal/cv_view.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <vector>
+#include "opencv/cxcore.h"
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
+#include "ros/node.h"
+#include "std_msgs/Image.h"
+#include "image_utils/cv_bridge.h"
+
+using namespace std;
+using namespace ros;
+
+class CvView : public node
+{
+public:
+  std_msgs::Image image_msg;
+  CvBridge<std_msgs::Image> cv_bridge;
+
+  CvView() : node("cv_view"), cv_bridge(&image_msg, CvBridge<std_msgs::Image>::CORRECT_BGR)
+  { 
+    cvNamedWindow("cv_view", CV_WINDOW_AUTOSIZE);
+    subscribe("image", image_msg, &CvView::image_cb, 1);
+  }
+  void image_cb()
+  {
+    IplImage *cv_image;
+
+    if (cv_bridge.to_cv(&cv_image))
+    {
+      cvShowImage("cv_view", cv_image);
+      cvWaitKey(3);
+      cvReleaseImage(&cv_image);
+    }
+  }
+};
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv);
+  CvView view;
+  view.spin();
+  ros::fini();
+  return 0;
+}
+
