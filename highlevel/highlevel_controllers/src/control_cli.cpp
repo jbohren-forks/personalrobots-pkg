54ad2d74a932d80e433ce5b18623b65bcbe887e3
tpratkanis <tpratkanis@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-01-20 19:36:44 +0000

    Start the renaming and moving of the source code files in highlevel_controllers.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@9793 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ highlevel/highlevel_controllers/src/control_cli.cpp
@@ -0,0 +1,278 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+// Author Tony Pratkanis (pratkanis@willowgarage.com)
+
+
+#include <ros/node.h>
+#include <pr2_msgs/MoveArmGoal.h>
+#include <robot_msgs/MechanismState.h>
+#include <highlevel_controllers/RechargeGoal.h>
+#include <robot_srvs/PlanNames.h>
+#include <robot_msgs/BatteryState.h>
+#include <boost/thread.hpp>
+
+
+using namespace std;
+
+class ControlCLI : public ros::Node {
+public:
+  ControlCLI() : ros::Node("CommandLineInterface") {
+    dead = false;
+    advertise<pr2_msgs::MoveArmGoal>("left_arm_goal", 1);
+    advertise<pr2_msgs::MoveArmGoal>("right_arm_goal", 1);
+    advertise<pr2_msgs::MoveArmGoal>("right_arm_goal", 1);
+    advertise<highlevel_controllers::RechargeGoal>("recharge_goal", 1);
+    advertise<robot_msgs::BatteryState>("bogus_battery_state", 1);
+    advertise<robot_msgs::MechanismState>("mechanism_state", 1);
+
+    // Set default values such that the robot is connected and fully charged
+    batteryState_.power_consumption = -800;
+    batteryState_.energy_remaining = 1000;
+    batteryState_.energy_capacity = 1000;
+
+    // Start a thread to publish batteryState at 10 Hz 
+    publishLoop = boost::thread(boost::bind(&ControlCLI::publishingLoop, this));
+
+    // Start the console
+    runCLI();
+  }
+  ~ControlCLI() {
+
+  }
+  bool alive() { return !dead; }
+private:
+  boost::thread publishLoop;
+  bool dead;
+
+  void publishingLoop(){
+    while (true) {
+      publish("bogus_battery_state", batteryState_);
+      usleep(100000);
+    }
+  }
+
+  void fillNamesLeftArm(std::vector<std::string> &names) {
+    names.push_back("l_shoulder_pan_joint");
+    names.push_back("l_shoulder_lift_joint");
+    names.push_back("l_upper_arm_roll_joint");
+    names.push_back("l_elbow_flex_joint");
+    names.push_back("l_forearm_roll_joint");
+    names.push_back("l_wrist_flex_joint");
+    names.push_back("l_wrist_roll_joint"); 
+  }
+
+  void fillNamesRightArm(std::vector<std::string> &names) {
+    names.push_back("r_shoulder_pan_joint");
+    names.push_back("r_shoulder_lift_joint");
+    names.push_back("r_upper_arm_roll_joint");
+    names.push_back("r_elbow_flex_joint");
+    names.push_back("r_forearm_roll_joint");
+    names.push_back("r_wrist_flex_joint");
+    names.push_back("r_wrist_roll_joint");
+  }
+
+  float enterValue(float min, float max) {
+    char buff[256];
+    while(1) {
+      memset(buff, 0, 256);
+      float value = min - 1;
+      fgets(buff, 256, stdin);
+      if (buff[0] != '\n') {
+	if (sscanf(buff, "%f", &value) && value < min || value > max) {
+	  printf("Please enter a value in the range [%f, %f]\n", min, max);
+	} else {
+	  return value;
+	}
+      }
+    }
+    return 0;
+  }
+
+  void runCLI() {
+    printf("Type:\nI\tInitialize States\nP\tActivate Recharge.\nB\tSet Battery State\nN\tPrint Joint Names\nQ\tQuit\nS\tHand Wave\nL\tLeft Arm\nR\tRight Arm\n");
+    char c = '\n';
+
+    while (c == '\n' || c == '\r') {
+      c = getchar();
+    }
+
+    if (c == 'B') {
+      double consumption(0.0), remaining(0.0), capacity(0.0);
+      printf("Enter Power Consumption:\n");
+      consumption = enterValue(-10000, 10000);
+      printf("Enter Remaining Energy:\n");
+      remaining = enterValue(-10000, 10000);
+      printf("Enter Capacity:\n");
+      capacity = enterValue(0, 10000);
+      printf("Setting battery state to:<%f, %f, %f>.\n", consumption, remaining, capacity);
+      batteryState_.lock();
+      batteryState_.power_consumption = consumption;
+      batteryState_.energy_remaining = remaining;
+      batteryState_.energy_capacity = capacity;
+      batteryState_.unlock();
+    } else if (c == 'P') {
+      double recharge_level(0.0);
+      printf("Enter recharge level as a percentage of maximum capacity:\n");
+      recharge_level = enterValue(0, 1);
+      recharge_level = std::max(0.0, std::min(recharge_level, 1.0));
+      printf("Sending recharge goal:%f\n", recharge_level);
+      highlevel_controllers::RechargeGoal goal;
+      goal.enable = 1;
+      goal.recharge_level = recharge_level;
+      publish("recharge_goal", goal);
+    } else if (c == 'N') {
+      printf("Joint names (number of parameters):\n");
+
+      robot_srvs::PlanNames::request namesReq;
+      robot_srvs::PlanNames::response names;
+      if (ros::service::call("plan_joint_state_names", namesReq, names)) {
+        for (unsigned int i = 0; i < names.get_names_size(); i++) {
+          printf("%s: %d\n", names.names[i].c_str(), names.num_values[i]);
+        }
+      } else {
+        printf("Error in service call.\n");
+      }
+
+
+    } else if (c == 'I') {
+      robot_msgs::MechanismState mechanismState;
+      std::vector<std::string> names;
+
+      fillNamesLeftArm(names);
+      fillNamesRightArm(names);
+
+      mechanismState.set_joint_states_size(names.size());
+      for (unsigned int i = 0; i < names.size(); i++) {
+        mechanismState.joint_states[i].position = 0;
+        mechanismState.joint_states[i].name = names[i];
+      }
+      printf("Publishing states.\n");
+      publish("mechanism_state", mechanismState);
+
+    } else if (c == 'S') {
+      std::vector<std::string> names;
+
+      fillNamesRightArm(names);
+
+      pr2_msgs::MoveArmGoal goalMsg;
+      goalMsg.enable = 1;
+      goalMsg.set_configuration_size(names.size());
+
+      for (unsigned int i = 0; i < names.size(); i++) {
+	goalMsg.configuration[i].name = names[i];
+	goalMsg.configuration[i].position = 0;
+      }
+
+      goalMsg.configuration[0].position = -1;
+
+
+      printf("Publishing:");
+      for (unsigned int i = 0; i < names.size(); i++) {
+        printf(" %f", goalMsg.configuration[i].position);
+      }
+      printf("\n");
+
+      publish("right_arm_goal", goalMsg);
+
+    } else if (c == 'L' || c == 'R') {
+      printf("%s Arm\n", c == 'L' ? "Left" : "Right");
+
+      std::vector<std::string> names;
+
+      if (c == 'L') {
+        fillNamesLeftArm(names);
+      } else {
+        fillNamesRightArm(names);
+      }
+
+      pr2_msgs::MoveArmGoal goalMsg;
+      goalMsg.enable = 1;
+      goalMsg.set_configuration_size(names.size());
+
+      for (unsigned int i = 0; i < names.size(); i++) {
+        printf("Enter the angle for joint %s in radians:\n",
+               names[i].c_str());
+	goalMsg.configuration[i].name = names[i];
+	goalMsg.configuration[i].position = enterValue(-2 * M_PI, 2 * M_PI);
+
+      }
+      printf("Publishing:");
+      for (unsigned int i = 0; i < names.size(); i++) {
+        printf(" %f", goalMsg.configuration[i].position);
+      }
+      printf("\n");
+
+      publish(c == 'L' ? "left_arm_goal" : "right_arm_goal", goalMsg);
+
+    } else if (c == 'Q') {
+      dead = true;
+      return;
+    } else {
+      printf("Rejected command.\n");
+    }
+    runCLI();
+  }
+
+  robot_msgs::BatteryState batteryState_;
+  robot_msgs::BatteryState batteryMsg_;;
+};
+
+
+
+
+int
+main(int argc, char** argv)
+{
+
+  ros::init(argc,argv);
+
+  ControlCLI node;
+  while (node.ok() && node.alive()) {
+    usleep(100);
+  }
+  ros::fini();
+
+  return(0);
+}
+
+
+
+
+
+
+
+
+
+
---
+++ highlevel/highlevel_controllers/src/recharge_controller.cpp
@@ -0,0 +1,276 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ * 
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ * 
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/**
+ * @mainpage
+ *
+ * @htmlinclude manifest.html
+ *
+ * @b recharge is a node which will handle plugging in and unpluggin the robot, assuming the plug location
+ * is within the immediate vicinity.
+ *
+ * <hr>
+ *
+ *  @section usage Usage
+ *  @verbatim
+ *  $ recharge
+ *  @endverbatim
+ *
+ * <hr>
+ *
+ * @section topic ROS topics
+ *
+ * Subscribes to (name/type):
+ * - @b 
+ *
+ * Publishes to (name / type):
+ * - @b 
+ *
+ *  <hr>
+ *
+ * @section parameters ROS parameters
+ *
+ * - None
+ **/
+
+#include <HighlevelController.hh>
+#include <highlevel_controllers/RechargeGoal.h>
+#include <highlevel_controllers/RechargeState.h>
+#include <robot_msgs/BatteryState.h>
+#include <cstdlib>
+
+namespace highlevel_controllers {
+
+  class RechargeController : public HighlevelController<RechargeState, RechargeGoal> {
+
+  public:
+
+    /**
+     * @brief Constructor
+     */
+    RechargeController(const std::string& stateTopic, const std::string& goalTopic);
+
+    virtual ~RechargeController();
+
+
+  private:
+    enum MailType {UNPLUG, PLUG};
+
+    /**
+     * HighlevelController interface
+     */
+    virtual void updateGoalMsg();
+    virtual void updateStateMsg();
+    virtual bool makePlan();
+    virtual bool goalReached();
+    virtual bool dispatchCommands();
+
+    void batteryStateCallback();
+
+    void sendMail(MailType type);
+    bool charged() const;
+    bool connected() const;
+
+    // Mail parameters
+    std::string m_addresses, m_pluginSubject, m_unplugSubject, m_pluginBody, m_unplugBody, m_mailClient;
+    
+    robot_msgs::BatteryState batteryStateMsg_;
+    bool pluginNotified_;
+    bool unplugNotified_;
+    unsigned int connectionCount_; // Use to make sure we connect to a socket at least once. Handly for forcing a rest
+  };
+
+  RechargeController::RechargeController(const std::string& stateTopic, const std::string& goalTopic)
+    : HighlevelController<RechargeState, RechargeGoal>("recharge_controller", stateTopic, goalTopic),
+      m_addresses("mcgann@willowgarage.com"), m_pluginSubject("Robot Needs to Be Plugged In"), 
+      m_unplugSubject("Robot Needs to Be Unplugged"), m_pluginBody("Hello, could you please plug me in?\nThanks, PR2"),
+      m_unplugBody("Hello, could you please unplug me?\nThanks, PR2"), m_mailClient("mailx -s"),
+      pluginNotified_(false), unplugNotified_(false), connectionCount_(0){
+
+    param("recharge/email_addresses", m_addresses, m_addresses);
+    param("recharge/subject_plugin", m_pluginSubject, m_pluginSubject);
+    param("recharge/subject_unplug", m_unplugSubject, m_unplugSubject);
+    param("recharge/body_plugin", m_pluginBody, m_pluginBody);
+    param("recharge/body_unplug", m_unplugBody, m_unplugBody);
+    param("recharge/mail_client", m_mailClient,  m_mailClient);
+    param("recharge/body_unplug", m_unplugBody, m_unplugBody);
+    param("recharge/mail_client", m_mailClient,  m_mailClient);
+
+    if (m_addresses == "") {
+      ROS_INFO("There are no email addresses in the param server. Opening the text file.\n");
+      FILE* email = fopen("email_file.txt", "r");
+      if (email) {
+	char buff[1024];
+	fgets(buff, 1020, email);
+	for (int i = 0; i < 1024; i++) {
+	  if (buff[i] == '\r' || buff[i] == '\n') { 
+	    buff[i] = 0; 
+	    break;
+	  }
+	}
+	m_addresses = buff;
+	fclose(email);
+      } else {
+	ROS_INFO("Could not open email alert file: email_file.txt\n");
+      }
+    }
+
+    ROS_INFO("Emails: %s\n", m_addresses.c_str());
+
+
+    // We will listen to battery state messages to monitor for transitions. We only care about the latest one
+    subscribe("battery_state", batteryStateMsg_, &RechargeController::batteryStateCallback, 1);
+
+    lock();
+    stateMsg.recharge_level = 0.0;
+    stateMsg.goal_recharge_level = 0.0;
+    unlock();
+  }
+
+  RechargeController::~RechargeController(){}
+
+  void RechargeController::batteryStateCallback(){
+    // Set to initalized (benign if already true)
+    initialize();
+
+    // If the power consumption is positive, then energy is flowing into the battery, so it must be plugged in.
+    if(connected() && !isActive())
+      activate();
+
+    // If connected, we can reset notification to plug in. We also clear it to be releasable, meaning
+    if(connected()){
+      pluginNotified_ = false;
+      connectionCount_++;
+    }
+
+    // If !connected, we can reset notification to unplug
+    if(!connected() )
+      unplugNotified_ = false;
+  }
+
+  void RechargeController::sendMail(MailType type) {
+    std::string subject = "Error: bad mail.", body = "Bad, bug in recharge controller.";
+    if (type == UNPLUG) {
+      subject = m_unplugSubject;
+      body = m_unplugBody;
+    } else if (type == PLUG) {
+      subject = m_pluginSubject;
+      body = m_pluginBody;
+    }
+    
+    ROS_INFO("Sending mail...\n");
+    std::string command = "echo \"";
+    command += body + "\" | " + m_mailClient + " \"";
+    command += subject + "\" \"";
+    for (unsigned int i = 0; i <  m_addresses.length(); i++) {
+      if (m_addresses[i] == ' ') {
+	command += "\" \"";
+      } else {
+	command += m_addresses[i];
+      }
+    }
+    command += "\"";
+    system(command.c_str());
+    ROS_INFO("Mail command sent: %s\n", command.c_str());
+  }
+  
+  void RechargeController::updateGoalMsg(){
+    stateMsg.lock();
+    stateMsg.goal_recharge_level = goalMsg.recharge_level;
+    pluginNotified_ = false;
+    unplugNotified_ = false;
+    connectionCount_ = 0;
+    stateMsg.unlock();
+  }
+
+  void RechargeController::updateStateMsg(){
+    batteryStateMsg_.lock();
+    stateMsg.recharge_level = (batteryStateMsg_.energy_remaining / batteryStateMsg_.energy_capacity);
+    batteryStateMsg_.unlock();
+  }
+
+  bool RechargeController::makePlan(){ return true; }
+
+  /**
+   * @brief When the state machine has transitioned back into an inactive state, we think we are done
+   */
+  bool RechargeController::goalReached(){return !connected() && charged() && connectionCount_ > 0;}
+
+  /**
+   * @brief Commands involve sending mail to plug and unplug, as well as waiting to charge.
+   */
+  bool RechargeController::dispatchCommands(){
+
+    if(!connected() && (connectionCount_ == 0 || !charged()) && !pluginNotified_){
+      sendMail(PLUG);
+      pluginNotified_ = true;
+      ROS_DEBUG("Requested help to plug in.");
+    }
+    else if(charged() && connected() && !unplugNotified_){
+      sendMail(UNPLUG);
+      unplugNotified_ = true;
+      ROS_DEBUG("Requested help to unplug.");
+    }
+
+    return true;
+  }
+
+  bool RechargeController::charged() const{
+    return stateMsg.recharge_level >= stateMsg.goal_recharge_level;
+  }
+
+  /**
+   * If published power consumption is positive, then we must be connected to a power source
+   */
+  bool RechargeController::connected() const {
+    return batteryStateMsg_.power_consumption > -5;
+  }
+}
+
+int
+main(int argc, char** argv)
+{
+  ros::init(argc,argv);
+  try{
+    highlevel_controllers::RechargeController node("recharge_state", "recharge_goal");
+    node.run();
+  }
+  catch(...){
+    ROS_DEBUG("Caught expection running node. Cleaning up.\n");
+  }
+
+  ros::fini();
+
+  return(0);
+}
