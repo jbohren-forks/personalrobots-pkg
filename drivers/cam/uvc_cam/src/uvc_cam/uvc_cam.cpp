d0e1e86a208342778d0bbd0ac29101671ce2ecf0
morgan_quigley <morgan_quigley@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-01-21 10:59:58 +0000

    package is starting to become useable. you can write avi files using the 'dump' command, or watch (uncompressed) streams using 'view'. the 'dump' command uses the mjpeg mode, which saves a lot of usb bandwidth so that you can run multiple cameras on the same usb host. of course the quality is garbage compared to a decent firewire camera, but hey, they're dirt cheap.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@9871 f5854215-dd47-0410-b2c4-cdd35faa7885

--- drivers/cam/uvc_cam/src/uvc_cam/uvc_cam.cpp
+++ drivers/cam/uvc_cam/src/uvc_cam/uvc_cam.cpp
@@ -13,7 +13,10 @@
 using std::string;
 using namespace uvc_cam;
 
-Cam::Cam(const char *_device) : device(_device)
+static const unsigned WIDTH = 640, HEIGHT = 480, FPS = 30;
+
+Cam::Cam(const char *_device, mode_t _mode)
+: mode(_mode), device(_device), rgb_frame(NULL)
 {
   if ((fd = open(_device, O_RDWR)) == -1)
     throw std::runtime_error("couldn't open " + device);
@@ -87,10 +90,12 @@ Cam::Cam(const char *_device) : device(_device)
   if (errno != EINVAL)
     throw std::runtime_error("error enumerating frame formats");
   fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-  static const unsigned WIDTH = 640, HEIGHT = 480, FPS = 15;
   fmt.fmt.pix.width = WIDTH;
   fmt.fmt.pix.height = HEIGHT;
-  fmt.fmt.pix.pixelformat = 'Y' | ('U' << 8) | ('Y' << 16) | ('V' << 24);
+  if (mode == MODE_RGB) // grab in yuyv, we'll convert later
+    fmt.fmt.pix.pixelformat = 'Y' | ('U' << 8) | ('Y' << 16) | ('V' << 24);
+  else
+    fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG; 
   fmt.fmt.pix.field = V4L2_FIELD_ANY;
   if ((ret = ioctl(fd, VIDIOC_S_FMT, &fmt)) < 0)
     throw std::runtime_error("couldn't set format");
@@ -101,6 +106,52 @@ Cam::Cam(const char *_device) : device(_device)
   streamparm.parm.capture.timeperframe.denominator = FPS;
   if ((ret = ioctl(fd, VIDIOC_S_PARM, &streamparm)) < 0)
     throw std::runtime_error("unable to set framerate");
+  v4l2_queryctrl queryctrl;
+  memset(&queryctrl, 0, sizeof(queryctrl));
+  uint32_t i = V4L2_CID_BASE;
+  while (i != V4L2_CID_LAST_EXTCTR)
+  {
+    queryctrl.id = i;
+    if ((ret = ioctl(fd, VIDIOC_QUERYCTRL, &queryctrl)) == 0 &&
+        !(queryctrl.flags & V4L2_CTRL_FLAG_DISABLED))
+    {
+      printf("  %s (id = %d): %d to %d\n", queryctrl.name, queryctrl.id, 
+             queryctrl.minimum, queryctrl.maximum);
+      if (queryctrl.type == V4L2_CTRL_TYPE_MENU)
+      {
+        v4l2_querymenu querymenu;
+        memset(&querymenu, 0, sizeof(querymenu));
+        querymenu.id = queryctrl.id;
+        querymenu.index = 0;
+        while (ioctl(fd, VIDIOC_QUERYMENU, &querymenu) == 0)
+        {
+          printf("    %d: %s\n", querymenu.index, querymenu.name);
+          querymenu.index++;
+        }
+      }
+
+    }
+    else if (errno != EINVAL)
+      throw std::runtime_error("couldn't query control");
+    i++;
+    if (i == V4L2_CID_LAST_NEW)
+      i = V4L2_CID_CAMERA_CLASS_BASE_NEW;
+    else if (i == V4L2_CID_CAMERA_CLASS_LAST)
+      i = V4L2_CID_PRIVATE_BASE_OLD;
+    else if (i == V4L2_CID_PRIVATE_LAST)
+      i = V4L2_CID_BASE_EXTCTR;
+  }
+
+  set_control(V4L2_CID_EXPOSURE_AUTO_NEW, 1);
+  set_control(V4L2_CID_EXPOSURE_ABSOLUTE_NEW, 300);
+
+/*
+  v4l2_jpegcompression v4l2_jpeg;
+  if (ioctl(fd, VIDIOC_G_JPEGCOMP, &v4l2_jpeg) < 0)
+    throw std::runtime_error("no jpeg compression iface exposed");
+  printf("jpeg quality: %d\n", v4l2_jpeg.quality);
+*/
+
   memset(&rb, 0, sizeof(rb));
   rb.count = NUM_BUFFER;
   rb.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
@@ -126,7 +177,7 @@ Cam::Cam(const char *_device) : device(_device)
     if (mem[i] == MAP_FAILED)
       throw std::runtime_error("couldn't map buffer");
   }
-  unsigned buf_len = buf.length;
+  buf_length = buf.length;
   for (unsigned i = 0; i < NUM_BUFFER; i++)
   {
     memset(&buf, 0, sizeof(buf));
@@ -140,11 +191,10 @@ Cam::Cam(const char *_device) : device(_device)
     if (ioctl(fd, VIDIOC_QBUF, &buf) < 0)
       throw std::runtime_error("unable to queue buffer");
   }
-  buf.length = buf_len;
   int type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
   if (ioctl(fd, VIDIOC_STREAMON, &type) < 0)
     throw std::runtime_error("unable to start capture");
-
+  rgb_frame = new unsigned char[WIDTH * HEIGHT * 3];
 }
 
 Cam::~Cam()
@@ -157,6 +207,9 @@ Cam::~Cam()
     if (munmap(mem[i], buf_length) < 0)
       perror("failed to unmap buffer");
   close(fd);
+  if (rgb_frame)
+    delete[] rgb_frame;
+  rgb_frame = NULL;
 }
 
 void Cam::enumerate()
@@ -233,7 +286,13 @@ void Cam::enumerate()
   closedir(d);
 }
 
-int Cam::grab(unsigned char **frame)
+// saturate input into [0, 255]
+inline unsigned char sat(float f)
+{
+  return (unsigned char)( f >= 255 ? 255 : (f < 0 ? 0 : f)); 
+}
+
+int Cam::grab(unsigned char **frame, uint32_t &bytes_used)
 {
   *frame = NULL;
   int ret = 0;
@@ -243,8 +302,14 @@ int Cam::grab(unsigned char **frame)
   FD_SET(fd, &rdset);
   timeout.tv_sec = 1;
   timeout.tv_usec = 0;
+  bytes_used = 0;
   ret = select(fd + 1, &rdset, NULL, NULL, &timeout);
-  if (ret <= 0)
+  if (ret == 0)
+  {
+    printf("select timeout in grab\n");
+    return -1;
+  }
+  else if (ret < 0)
   {
     perror("couldn't grab image");
     return -1;
@@ -256,17 +321,52 @@ int Cam::grab(unsigned char **frame)
   buf.memory = V4L2_MEMORY_MMAP;
   if (ioctl(fd, VIDIOC_DQBUF, &buf) < 0)
     throw std::runtime_error("couldn't dequeue buffer");
-  // assume yuyv and bayer for now
-  *frame = (unsigned char *)mem[buf.index];
+  bytes_used = buf.bytesused;
+  if (mode == MODE_RGB)
+  {
+    unsigned char *pyuv = (unsigned char *)mem[buf.index];
+    *frame = rgb_frame;
+    // yuyv is 2 bytes per pixel. step through every pixel pair.
+    unsigned char *prgb = rgb_frame;
+    for (unsigned i = 0; i < WIDTH * HEIGHT * 2; i += 4)
+    {
+      /*
+      *prgb++ = sat(pyuv[i]+1.402f  *(pyuv[i+3]-128));
+      *prgb++ = sat(pyuv[i]-0.34414f*(pyuv[i+1]-128)-0.71414f*(pyuv[i+3]-128));
+      *prgb++ = sat(pyuv[i]+1.772f  *(pyuv[i+1]-128));
+      *prgb++ = sat(pyuv[i+2]+1.402f*(pyuv[i+3]-128));
+      *prgb++ = sat(pyuv[i+2]-0.34414f*(pyuv[i+1]-128)-0.71414f*(pyuv[i+3]-128));
+      *prgb++ = sat(pyuv[i+2]+1.772f*(pyuv[i+1]-128));
+      */
+      *prgb++ = sat(pyuv[i]+1.772f  *(pyuv[i+1]-128));
+      *prgb++ = sat(pyuv[i]-0.34414f*(pyuv[i+1]-128)-0.71414f*(pyuv[i+3]-128));
+      *prgb++ = sat(pyuv[i]+1.402f  *(pyuv[i+3]-128));
+      *prgb++ = sat(pyuv[i+2]+1.772f*(pyuv[i+1]-128));
+      *prgb++ = sat(pyuv[i+2]-0.34414f*(pyuv[i+1]-128)-0.71414f*(pyuv[i+3]-128));
+      *prgb++ = sat(pyuv[i+2]+1.402f*(pyuv[i+3]-128));
+    }
+  }
+  else
+  {
+    //if (bytes_used > 100)
+      *frame = (unsigned char *)mem[buf.index];
+  }
   return buf.index;
-  //tmp = new unsigned char[640 * 480 * 3];
-  //bayer_to_rgb24(mem[buf.index], tmp, 640, 480, 
 }
 
-void Cam::release(int buf_idx)
+void Cam::release(unsigned buf_idx)
 {
-  if (buf_idx >= 0 && buf_idx < NUM_BUFFER)
+  if (buf_idx < NUM_BUFFER)
     if (ioctl(fd, VIDIOC_QBUF, &buf) < 0)
       throw std::runtime_error("couldn't requeue buffer");
+}
+
+void Cam::set_control(uint32_t id, int val)
+{
+  v4l2_control c;
+  c.id = id;
+  c.value = val;
+  if (ioctl(fd, VIDIOC_S_CTRL, &c) < 0)
+    throw std::runtime_error("unable to set control");
 }
 
--- drivers/cam/uvc_cam/src/view/view.cpp
+++ drivers/cam/uvc_cam/src/view/view.cpp
@@ -1,8 +1,11 @@
 #include <unistd.h>
 #include <cstdio>
 #include <cassert>
+#include <ros/time.h>
 #include "uvc_cam/uvc_cam.h"
 #include "SDL/SDL.h"
+
+const unsigned WIDTH = 640, HEIGHT = 480;
 
 int main(int argc, char **argv)
 {
@@ -19,20 +22,30 @@ int main(int argc, char **argv)
     return 1;
   }
   atexit(SDL_Quit);
-  SDL_Surface *surf = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+  SDL_Surface *surf = SDL_SetVideoMode(WIDTH, HEIGHT, 24, SDL_HWSURFACE);
   assert(surf);
   SDL_WM_SetCaption("hello world", "hello world");
+  ros::Time t_prev(ros::Time::now());
+  int count = 0;
   for (bool done = false; !done;)
   {
     unsigned char *frame = NULL;
-    int buf_idx = cam.grab(&frame);
+    uint32_t bytes_used;
+    int buf_idx = cam.grab(&frame, bytes_used);
+    if (count++ % 30 == 0)
+    {
+      ros::Time t(ros::Time::now());
+      ros::Duration d(t - t_prev);
+      printf("%.1f fps\n", 30.0 / d.to_double());
+      t_prev = t;
+    }
     if (frame)
     {
-      memcpy(surf->pixels, frame, 640*480*3);
+      memcpy(surf->pixels, frame, WIDTH*HEIGHT*3);
       cam.release(buf_idx);
     }
     usleep(1000);
-    SDL_UpdateRect(surf, 0, 0, 640, 480);
+    SDL_UpdateRect(surf, 0, 0, WIDTH, HEIGHT);
     SDL_Event event;
     while (SDL_PollEvent(&event))
     {
