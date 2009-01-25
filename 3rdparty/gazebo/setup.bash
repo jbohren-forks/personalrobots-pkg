fd9b0c0cae8578be4d5b5d3ddc35c7a4d5d11f62
hsujohnhsu <hsujohnhsu@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-01-25 03:26:38 +0000

    update boost flags for gazebo.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@10151 f5854215-dd47-0410-b2c4-cdd35faa7885

--- 3rdparty/gazebo/setup.bash
+++ 3rdparty/gazebo/setup.bash
@@ -2,7 +2,7 @@
 export GAZ_TOP=`rospack find gazebo`/gazebo
 export OGRE_TOP=`rospack find ogre`/ogre
 export CG_TOP=`rospack find Cg`/Cg
-export BOOST_TOP=`rospack find boost`/boost
+export BOOST_TOP=`rosboost-cfg --root`
 export SIM_PLUGIN=`rospack find gazebo_plugin`
 export PR2MEDIA=`rospack find gazebo_robot_description`/world
 
--- 3rdparty/gazebo/setup.tcsh
+++ 3rdparty/gazebo/setup.tcsh
@@ -2,7 +2,7 @@
 setenv GAZ_TOP `rospack find gazebo`/gazebo
 setenv OGRE_TOP `rospack find ogre`/ogre
 setenv CG_TOP `rospack find Cg`/Cg
-setenv BOOST_TOP `rospack find boost`/boost
+setenv BOOST_TOP `rosboost-cfg --root`
 setenv SIM_PLUGIN `rospack find gazebo_plugin`
 setenv PR2MEDIA `rospack find gazebo_robot_description`/world
 
