a58303c66a78c84823afb40baa918c8e27d1e018
tpratkanis <tpratkanis@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-02-06 00:47:25 +0000

    Re-activate arm controller in trex.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@10636 f5854215-dd47-0410-b2c4-cdd35faa7885

--- highlevel/executive_trex/executive_trex_pr2/src/ArmControllerAdapter.cc
+++ highlevel/executive_trex/executive_trex_pr2/src/ArmControllerAdapter.cc
@@ -18,40 +18,28 @@ namespace TREX {
   protected:
     
     void fillActiveObservationParameters(ObservationByValue* obs){
-      ROS_FATAL("Disabled, pending update to new MoveArm interface");
-      ROS_BREAK();
-      /*
       for(unsigned int i = 0; i < stateMsg.get_goal_size(); i++){
 	unsigned int ind;
 	if(rosIndex(stateMsg.goal[i].name, ind))
 	   obs->push_back(nddlNames()[ind], new IntervalDomain(stateMsg.goal[i].position));
       }
-      */
     }
 
     void fillInactiveObservationParameters(ObservationByValue* obs){  
-      ROS_FATAL("Disabled, pending update to new MoveArm interface");
-      ROS_BREAK();
-      /*
       for(unsigned int i = 0; i < stateMsg.get_configuration_size(); i++){
 	unsigned int ind;
 	if(rosIndex(stateMsg.configuration[i].name, ind))
 	   obs->push_back(nddlNames()[ind], new IntervalDomain(stateMsg.configuration[i].position));
       }
-      */
     }
 
     void fillRequestParameters(pr2_msgs::MoveArmGoal& goalMsg, const TokenId& goalToken){
-      ROS_FATAL("Disabled, pending update to new MoveArm interface");
-      ROS_BREAK();
-      /*
       goalMsg.set_configuration_size(nddlNames().size());
       for(unsigned int i = 0; i<nddlNames().size(); i++){
 	const IntervalDomain& dom = goalToken->getVariable(nddlNames()[i])->lastDomain();
 	goalMsg.configuration[i].name = rosNames()[i];
 	goalMsg.configuration[i].position = (dom.isSingleton() ? dom.getSingletonValue() : (dom.getUpperBound() + dom.getLowerBound()) / 2);
       }
-      */
     }
   };  
 
