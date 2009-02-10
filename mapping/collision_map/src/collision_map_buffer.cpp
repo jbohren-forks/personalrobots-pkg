99efa3fdd6dd3e1f3f56892e27e88aeb6923a622
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-02-10 08:43:31 +0000

    fixing ambiguous namespace
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@10915 f5854215-dd47-0410-b2c4-cdd35faa7885

--- mapping/collision_map/src/collision_map_buffer.cpp
+++ mapping/collision_map/src/collision_map_buffer.cpp
@@ -107,7 +107,7 @@ class CollisionMapperBuffer : public ros::Node
     tf::TransformListener tf_;
 
     // Parameters
-    Point leaf_width_, robot_max_;
+    std_msgs::Point leaf_width_, robot_max_;
     int min_nr_points_;
     string end_effector_frame_l_, end_effector_frame_r_;
 
@@ -122,7 +122,7 @@ class CollisionMapperBuffer : public ros::Node
     // Internal parameters
     string cloud_frame_;
     PoseStamped gripper_orientation_link_;
-    Point32 min_object_b_, max_object_b_;
+    std_msgs::Point32 min_object_b_, max_object_b_;
     bool subtract_object_;
     int m_id_;
 
