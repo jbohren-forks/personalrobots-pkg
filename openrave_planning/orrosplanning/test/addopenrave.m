450ccda68f5a4ac83d43a676a5d6484c03e31091
rdiankov <rdiankov@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-02-17 06:09:30 +0000

    removed octave iktest program
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@11243 f5854215-dd47-0410-b2c4-cdd35faa7885

--- openrave_planning/orrosplanning/test/addopenrave.m
+++
@@ -1,9 +0,0 @@
-function addopenrave()
-
-[status,orprefix] = system('openrave-config --prefix');
-orprefix = strtrim(orprefix);
-if( exist('OCTAVE_VERSION') ~= 0 )
-    addpath(fullfile(orprefix,'share','openrave','octave'));
-else
-    addpath(fullfile(orprefix,'share','openrave','matlab'));
-end
