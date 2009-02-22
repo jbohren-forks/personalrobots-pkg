0d06d7ee58f32e9cb71a96195a7441b26cfd9092
sachinchitta <sachinchitta@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-02-22 12:54:15 +0000

    adding node for using sbpl planner with the pr2 arm, needs extensive testing and debugging
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@11536 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ motion_planning/planning_research/sbpl_arm_planner_node/include/sbpl_arm_planner_node/sbpl_arm_planner_node.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2008, Maxim Likhachev
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the University of Pennsylvania nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __SBPL_ARM_PLANNER_NODE_H_
+#define __SBPL_ARM_PLANNER_NODE_H_
+
+#include <iostream>
+#include "../../src/headers.h"
+#include <robot_msgs/JointTraj.h>
+#include <robot_msgs/JointTrajPoint.h>
+#include <ros/node.h>
+#include <robot_msgs/Point.h>
+#include <robarm3d/PlanPathSrv.h>
+
+namespace sbpl_arm_planner_node
+{
+#define VERBOSE 1
+#define MAX_RUNTIME 60.0
+
+   class SBPLArmPlannerNode : public ros::Node
+   {
+     public:
+
+      SBPLArmPlannerNode(std::string node_name);
+
+      ~SBPLArmPlannerNode();
+
+      bool planPath(SBPLAArmPlannerNode::PlanPathSrv::Request &req, SBPLArmPlannerNode::PlanPathSrv::Response &resp);
+
+     private:
+
+      double allocated_time_;
+
+      bool forward_search_;
+
+      bool search_mode_;
+
+      int num_joints_;
+    
+      bool initializePlanner();
+
+      int plan(const robot_msgs::JointTrajPoint &start, const std_msgs::Pose &goal, robot_msgs::JointTraj &arm_path);
+
+      MDPConfig mdp_cfg_;
+
+      EnvironmentROBARM env_pr2_arm_;
+
+      ARAPlanner planner_;
+
+   };
+}
+
+#endif
