6cf5c303dce3c49e7bd5c890630e81bdbb243f39
stuglaser <stuglaser@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-02-24 23:56:33 +0000

    Added list joints to mech.py
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@11700 f5854215-dd47-0410-b2c4-cdd35faa7885

--- mechanism/mechanism_control/scripts/mech.py
+++ mechanism/mechanism_control/scripts/mech.py
@@ -6,9 +6,19 @@ roslib.load_manifest('mechanism_control')
 
 import rospy, sys
 from mechanism_control import mechanism
+from robot_msgs.msg import MechanismState
+
+class Tracker:
+  def __init__(self, topic, Msg):
+    self.sub = rospy.Subscriber(topic, Msg, self.callback)
+    self.msg = None #Msg()
+
+  def callback(self, msg):
+    self.msg = msg
 
 def print_usage(exit_code = 0):
     print '''Commands:
+    lj  - List joints and actuators
     lt  - List controller Types
     lc  - List active controllers
     sp  - Spawn a controller using the xml passed over stdin
@@ -37,5 +47,20 @@ if __name__ == '__main__':
             mechanism.kill_controller(c)
     elif sys.argv[1] == 'shutdown':
         mechanism.shutdown()
+    elif sys.argv[1] == 'lj':
+        track_mech = Tracker('/mechanism_state', MechanismState)
+        print "Waiting for mechanism state message..."
+        rospy.init_node('mech', anonymous=True)
+        while not track_mech.msg:
+            time.sleep(0.01)
+            if rospy.is_shutdown():
+                sys.exit(0)
+        msg = track_mech.msg
+        print 'Actuators:'
+        for i in range(len(msg.actuator_states)):
+            print "  %3d %s" % (i, msg.actuator_states[i].name)
+        print 'Joints:'
+        for i in range(len(msg.joint_states)):
+            print "  %3d %s" % (i, msg.joint_states[i].name)
     else:
         print_usage(1)
