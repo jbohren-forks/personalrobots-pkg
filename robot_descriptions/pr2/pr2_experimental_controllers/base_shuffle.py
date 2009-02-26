c49222be7ba4c35ed5c592f79421e786ffcf6586
wattsk <wattsk@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-02-26 00:17:32 +0000

    Shuffle works
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@11780 f5854215-dd47-0410-b2c4-cdd35faa7885

--- robot_descriptions/pr2/pr2_experimental_controllers/base_shuffle.py
+++ robot_descriptions/pr2/pr2_experimental_controllers/base_shuffle.py
@@ -47,13 +47,11 @@ roslib.load_manifest('mechanism_bringup')
 roslib.load_manifest('mechanism_control')
 import rospy
 from std_msgs.msg import *
-from robot_msgs.msg import *
+from robot_msgs.msg import PoseDot
 
 from robot_mechanism_controllers.srv import *
 from robot_mechanism_controllers import controllers
 from mechanism_control import mechanism
-
-
 
 def main():
     usage = "base_shuffle.py <dist>;  Moves base in y dir side to side."
@@ -61,47 +59,58 @@ def main():
     if len(sys.argv) < 2:
         print usage
         sys.exit(1)
-    distance = sys.argv[1]
+    #distance = sys.argv[1]
+    distance = 0.5
 
     rospy.wait_for_service('spawn_controller')
     rospy.init_node('shuffle', anonymous=True)
     
-    xml_for_base = open('/ros/ros-pkg/robot_descriptions/pr2/pr2_default_controllers/bas_controller.xml')
+    path = roslib.packages.get_pkg_dir("pr2_default_controllers")
+    xml_for_base = open(path + "/base_controller.xml")
 
     mechanism.spawn_controller(xml_for_base.read())
 
-    # Publishes velocity every 0.1s, calculates number of publishes
-    num_publishes = distance * 10 * 2
+    # Publishes velocity every 0.05s, calculates number of publishes
+    num_publishes = int(distance * 20 * 2)
     
+    cmd_vel = PoseDot()
+    cmd_vel.vel.vx = float(0)
+    cmd_vel.vel.vy = float(0)
+    cmd_vel.vel.vz = float(0)
+    cmd_vel.ang_vel.vx = float(0)
+    cmd_vel.ang_vel.vy = float(0)
+    cmd_vel.ang_vel.vz = float(0)
+
+    base_vel = rospy.Publisher('cmd_vel', PoseDot)
+
     try:
-        cmd_vel = PoseDot()
-        cmd_vel.vel.vx = 0
-        cmd_vel.ang_vel.vz = 0
         while not rospy.is_shutdown():
             # Set velocity 
-            cmd_vel.vel.vy = -0.5
-            for i in range(0,num_publishes):
-                rospy.Publisher('/base_controller/cmd_vel', cmd_vel) 
+            cmd_vel.vel.vy = float(0.5)
+            for i in range(0, num_publishes): # Change to int num_publishes
+                base_vel.publish(cmd_vel)
                 if rospy.is_shutdown():
                     break 
-                sleep(0.1)
+                sleep(0.05)
                 
             if rospy.is_shutdown():
                 break
 
-            cmd_vel.vel.vy = -0.5
+            cmd_vel.vel.vy = float(-0.5)
             for i in range(0, num_publishes):
-                rospy.Publisher('/base_controller/cmd_vel', cmd_vel) 
-
+                base_vel.publish(cmd_vel)
                 if rospy.is_shutdown():
                     break
-                sleep(0.1)
-                
+                sleep(0.05)
+    except Exception, e:
+        print "Caught exception!"
+        print e
+        e
     finally:
         # Set velocity = 0
-        cmd_vel.vel.vy = 0.0
-        rospy.Publisher('/base_controller/cmd_vel', cmd_vel) 
+        cmd_vel.vel.vy = float(0)
+        base_vel.publish(cmd_vel)
         mechanism.kill_controller('base_controller')
-
+        
 if __name__ == '__main__':
     main()
