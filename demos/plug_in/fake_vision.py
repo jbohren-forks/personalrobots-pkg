c728d890f9a85dbf1b585763c3e420f08e301b21
stuglaser <stuglaser@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-02-26 01:04:17 +0000

    Added Python scripts for assisting plugging in integration.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@11785 f5854215-dd47-0410-b2c4-cdd35faa7885

---
+++ demos/plug_in/fake_vision.py
@@ -0,0 +1,93 @@
+#! /usr/bin/env python
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Willow Garage, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import roslib
+roslib.load_manifest('pr2_mechanism_controllers')
+roslib.load_manifest('std_msgs')
+roslib.load_manifest('rospy')
+
+import rospy
+from std_msgs.msg import *
+from robot_msgs.msg import *
+from tf.msg import tfMessage
+from math import *
+from time import sleep
+
+def xyz(x, y, z):
+  p = Point()
+  p.x, p.y, p.z = x, y, z
+  return p
+
+def ypr(y, p, r):
+  q = Quaternion()
+  cosY = cos(y / 2.)
+  sinY = sin(y / 2.)
+  cosP = cos(p / 2.)
+  sinP = sin(p / 2.)
+  cosR = cos(r / 2.)
+  sinR = sin(r / 2.)
+  q.x = cosR*cosP*cosY + sinR*sinP*sinY
+  q.y = sinR*cosP*cosY + cosR*sinP*sinY
+  q.z = cosR*sinP*cosY + sinR*cosP*sinY
+  q.w = cosR*cosP*sinY + sinR*sinP*cosY
+  return q
+
+
+class Tracker:
+  def __init__(self, topic, Msg):
+    self.sub = rospy.Subscriber(topic, Msg, self.callback)
+    self.msg = Msg()
+
+  def callback(self, msg):
+    self.msg = msg
+
+mechanism_state = Tracker('/mechanism_state', MechanismState)
+def last_time():
+  global mechanism_state
+  return mechanism_state.msg.header.stamp
+
+
+rospy.init_node('plug', anonymous=True)
+
+pub_outlet = rospy.Publisher('/outlet_pose', PoseStamped)
+pub_plug = rospy.Publisher('/plug_pose', PoseStamped)
+def send():
+  op = PoseStamped()
+  op.header.stamp = last_time()
+  op.header.frame_id = 'torso_lift_link'
+  op.pose.position = xyz(0.7, -0.4, -0.2)
+  op.pose.orientation = ypr(0,0,0)
+
+  pp = PoseStamped()
+  pp.header.stamp = last_time()
+  pp.header.frame_id = 'torso_lift_link'
+  pp.pose.position = xyz(0.7, -0.4, -0.2)
+  pp.pose.orientation = ypr(0,0,0)
+
+  pub_outlet.publish(op)
+  pub_plug.publish(pp)
---
+++ demos/plug_in/plug_in.py
@@ -0,0 +1,105 @@
+#! /usr/bin/env python
+# Copyright (c) 2008, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Willow Garage, Inc. nor the names of its
+#       contributors may be used to endorse or promote products derived from
+#       this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# Author: Stuart Glaser
+
+import sys, time
+
+import roslib
+roslib.load_manifest('plug_in')
+import rospy
+from robot_msgs.msg import *
+from robot_srvs.srv import *
+
+CONTROLLER = 'arm_constraint'
+
+class Tracker:
+    def __init__(self, topic, Msg):
+        self.sub = rospy.Subscriber(topic, Msg, self.callback)
+        self.msg = None
+
+    def callback(self, msg):
+        self.msg = msg
+
+mechanism_state = Tracker('/mechanism_state', MechanismState)
+def last_time():
+    global mechanism_state
+    if mechanism_state.msg:
+        return mechanism_state.msg.header.stamp
+    return 0
+
+
+
+def main():
+    rospy.init_node('plug_in')
+
+    track_outlet_pose = Tracker('/outlet_pose', PoseStamped)
+    track_plug_pose = Tracker('/plug_pose', PoseStamped)
+
+    # Waits for an estimate of the outlet pose
+    print "Waiting for outlet pose..."
+    outlet_pose = None
+    msg = None
+    while not msg:
+        msg = track_outlet_pose.msg
+        if rospy.is_shutdown(): return
+        time.sleep(0.1)
+    outlet_pose = msg
+    print "Found outlet"
+
+    # Waits for an estimate of the plug pose
+    print "Waiting for plug pose..."
+    plug_pose = None
+    msg = None
+    while not msg:
+        msg = track_plug_pose.msg
+        if rospy.is_shutdown(): return
+        time.sleep(0.1)
+    plug_pose = msg
+    print "Found plug"
+
+    print "Please start the constraint controller..."
+
+    # Sets the tool frame
+    rospy.wait_for_service("/%s/set_tool_frame" % CONTROLLER)
+    if rospy.is_shutdown(): return
+    #set_tool_frame = rospy.ServiceProxy("/%s/set_tool_frame" % CONTROLLER, SetPoseStamped)
+    # TODO: Actually set the tool frame
+    print "Tool frame set"
+
+    # Publishes commands
+    time.sleep(1.0)
+    print "Starting to publish"
+    pub_command = rospy.Publisher("/%s/outlet_pose" % CONTROLLER, PoseStamped)
+    msg = PoseStamped()
+    while not rospy.is_shutdown():
+        outlet_pose.header.stamp = last_time()
+        pub_command.publish(outlet_pose)
+        time.sleep(1.0)
+
+if __name__ == '__main__': main()
