48f4e4c7e0bda012996d408867d69361f011adf0
pmihelich <pmihelich@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-03-06 00:31:12 +0000

    prosilica_cam: Restructured source into proper directory structure.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@12215 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: f37fd3f18e6792187fd6ea501c75904ec5d9d86e

---
+++ drivers/cam/prosilica_cam/src/libprosilica/prosilica.h
@@ -0,0 +1,123 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+* 
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+* 
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+* 
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+* 
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+#ifndef PROSILICA_H
+#define PROSILICA_H
+
+#include <stdexcept>
+#include <boost/function.hpp>
+#include <boost/thread.hpp>
+
+// PvApi.h is blissfully unaware of the usual detection macros
+// TODO: do this properly
+#define _LINUX
+#define _x86
+#include <PvApi.h>
+
+namespace prosilica {
+
+struct ProsilicaException : public std::runtime_error
+{
+  ProsilicaException(const char* msg) : std::runtime_error(msg) {}
+};
+
+void init();                // initializes API
+void fini();                // releases internal resources
+size_t numCameras();        // number of cameras found
+uint64_t getGuid(size_t i); // camera ids
+
+enum AcquisitionMode
+{
+  Continuous,
+  //FixedRate,
+  Triggered,
+  None
+};
+
+enum AutoSetting
+{
+  Manual,
+  Auto,
+  AutoOnce
+};
+
+class Camera
+{
+public:
+  static const size_t DEFAULT_BUFFER_SIZE = 8;
+  
+  Camera(unsigned long guid, size_t bufferSize = DEFAULT_BUFFER_SIZE);
+  Camera(const char* ip_address, size_t bufferSize = DEFAULT_BUFFER_SIZE);
+
+  ~Camera();
+
+  //! Must be used before calling start() in a non-triggered mode.
+  void setFrameCallback(boost::function<void (tPvFrame*)> callback);
+  //! Start capture.
+  void start(AcquisitionMode mode = Continuous);
+  //! Stop capture.
+  void stop();
+  //! Capture a single frame from the camera. Must be called after
+  //! start(Triggered).
+  tPvFrame* grab(unsigned long timeout_ms = PVINFINITE);
+
+  void setExposure(unsigned int val, AutoSetting isauto = Manual);
+  void setGain(unsigned int val, AutoSetting isauto = Manual);
+  void setWhiteBalance(unsigned int blue, unsigned int red,
+                       AutoSetting isauto = Manual);
+
+  unsigned long guid();
+  
+  //! Data must have size <= USER_MEMORY_SIZE.
+  static const size_t USER_MEMORY_SIZE = 512;
+  void writeUserMemory(const char* data, size_t size);
+  void readUserMemory(char* data, size_t size);
+  
+private:
+  tPvHandle handle_; // handle to open camera
+  tPvFrame* frames_; // array of frame buffers
+  tPvUint32 frameSize_; // bytes per frame
+  size_t bufferSize_; // number of frame buffers
+  AcquisitionMode mode_;
+  boost::function<void (tPvFrame*)> userCallback_;
+  boost::mutex frameMutex_;
+
+  void setup();
+  
+  static void frameDone(tPvFrame* frame);
+};
+
+} // namespace prosilica
+
+#endif
