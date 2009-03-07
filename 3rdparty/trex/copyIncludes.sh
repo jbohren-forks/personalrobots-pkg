6a04970afd8917482766ca1c387ac812a559bd18
tpratkanis <tpratkanis@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-03-07 03:03:10 +0000

    Upgrade to copyincludes for bug 1009. This makes more robust to platform changes, but is still a hack.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@12271 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: f178b7da09a33ec2e05e43b333a5e9aa4a7ed596

--- 3rdparty/trex/copyIncludes.sh
+++ 3rdparty/trex/copyIncludes.sh
@@ -18,14 +18,26 @@
 cd `rospack find trex`
 
 rm -r include 2> /dev/null
+mkdir include
 INCLUDE_DIR="`pwd`/include"
-#scanDirectory `pwd`
-DIRS=`find \`pwd\` -type d`
-mkdir -p include
-for d in $DIRS; do
-  HEADERS=`find $d -maxdepth 1 -type f -regex ".*\.hh?$"`
-  if [ "$HEADERS" ]; then
-    cp $HEADERS $INCLUDE_DIR
-  fi
-done
 echo "Warning, this is not the real include directory. It will be deleted and recreated by make every run. Includes should be next to .cc files." > $INCLUDE_DIR/WARNING.txt
+#scanDirectory `pwd`
+
+PACKAGES="TREX PLASMA PlanWorks"
+
+
+for PACKAGE in $PACKAGES; do
+    cd `rospack find trex`/$PACKAGE
+    mkdir $INCLUDE_DIR/$PACKAGE
+    DIRS=`find \`pwd\` -type d`
+    mkdir -p include
+    for d in $DIRS; do
+	HEADERS=`find $d -maxdepth 1 -type f -regex ".*\.hh?$"`
+	if [ "$HEADERS" ]; then
+	    cp -p $HEADERS $INCLUDE_DIR/$PACKAGE/
+	fi
+    done
+    echo "Warning, this is not the real include directory. It will be deleted and recreated by make every run. Includes should be next to .cc files." > $INCLUDE_DIR/$PACKAGE/WARNING.txt
+done
+
+
