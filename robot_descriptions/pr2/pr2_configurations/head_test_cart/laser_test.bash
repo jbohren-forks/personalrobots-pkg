e83983c4e3282ed44a6fc4815ccb6318a07c3082
wattsk <wattsk@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-03-10 04:32:14 +0000

    Fix to head test cart
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@12330 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: c7cf13a89d0980259764ee74458664246464d55d

--- robot_descriptions/pr2/pr2_configurations/head_test_cart/laser_test.bash
+++ robot_descriptions/pr2/pr2_configurations/head_test_cart/laser_test.bash
@@ -1,2 +1,3 @@
 #! /bin/bash
-~/head_tree/ros-pkg/controllers/pr2_mechanism_controllers/scripts/control_laser.py laser_controller sine 0.25 0.95 0.25
+`rospack find pr2_mechanism_controllers`/scripts/control_laser.py laser_controller sine 0.25 0.95 0.25
+#~/head_tree/ros-pkg/controllers/pr2_mechanism_controllers/scripts/control_laser.py laser_controller sine 0.25 0.95 0.25
