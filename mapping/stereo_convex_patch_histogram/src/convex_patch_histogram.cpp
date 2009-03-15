4f40d52f49a31dd1170ce29f20485480a56a2433
veedee <veedee@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-03-15 18:39:37 +0000

    propset
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@12525 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 7ae3708b2bf321e9328391cfe3660dcf27c48e7f

--- mapping/stereo_convex_patch_histogram/src/convex_patch_histogram.cpp
+++ mapping/stereo_convex_patch_histogram/src/convex_patch_histogram.cpp
@@ -24,7 +24,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: convex_patch_histogram.cpp 9113 2009-01-09 05:37:03Z veedee $
+ * $Id$
  *
  */
 
