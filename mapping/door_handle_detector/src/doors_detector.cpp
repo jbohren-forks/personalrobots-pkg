619a262e30bfa5ffdb7171ae5ffee5f602c4dae9
veedee <veedee@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-03-20 17:34:58 +0000

    propset
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@12756 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 75959be8ef19e75fa3bb673265d7b8690f201f0f

--- mapping/door_handle_detector/src/doors_detector.cpp
+++ mapping/door_handle_detector/src/doors_detector.cpp
@@ -24,7 +24,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: doors_detector.cpp 9895 2009-01-21 21:05:27Z tfoote $
+ * $Id$
  *
  */
 
