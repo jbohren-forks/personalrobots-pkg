d680cf6a5c609ad321f73750a67ae0e727282c01
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-03-22 23:37:46 +0000

    changing default filter to no filter
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@12822 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 3f2d95dd784c3ec197e42e06eda23cc2cab8ce43

--- util/laser_scan/src/generic_laser_filter_node.cpp
+++ util/laser_scan/src/generic_laser_filter_node.cpp
@@ -32,7 +32,7 @@
 #include "laser_scan/median_filter.h"
 #include "laser_scan/intensity_filter.h"
 
-static std::string median_filter_xml = "<filter type=\"MedianFilter\" name=\"median_test_5\"> <params number_of_observations=\"5\"/></filter>";
+static std::string median_filter_xml = "<!-- NO FILTER DEFINED-->";
 
 
 class MedianFilterNode 
