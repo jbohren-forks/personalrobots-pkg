4e3afa50c90ea5e2e700b43009fa74e44a27905f
mcgann <mcgann@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-03-24 13:21:07 +0000

    Keep deprecated stuff around for a while
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@12887 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 0a0a0535e85081f5f691ee23d23b1ae2992de9d1

---
+++ highlevel/executive_trex/executive_trex_pr2/deprecated/RechargeControllerAdapter.cc
@@ -0,0 +1,43 @@
+#include "ROSControllerAdapter.hh"
+#include "IntervalDomain.hh"
+#include "BoolDomain.hh"
+#include "Token.hh"
+#include <robot_actions/RechargeGoal.h>
+#include <robot_actions/RechargeState.h>
+
+namespace TREX {
+
+  class RechargeControllerAdapter: public ROSControllerAdapter<robot_actions::RechargeState, robot_actions::RechargeGoal> {
+  public:
+
+    RechargeControllerAdapter(const LabelStr& agentName, const TiXmlElement& configData)
+      : ROSControllerAdapter<robot_actions::RechargeState, robot_actions::RechargeGoal>(agentName, configData){
+    }
+
+    virtual ~RechargeControllerAdapter(){}
+
+  protected:
+    
+
+    void fillActiveObservationParameters(ObservationByValue* obs){
+    }
+
+    void fillInactiveObservationParameters(ObservationByValue* obs){
+    }
+
+    void fillRequestParameters(robot_actions::RechargeGoal& goalMsg, const TokenId& goalToken){
+      const IntervalDomain& x = goalToken->getVariable("x")->lastDomain();
+      const IntervalDomain& y = goalToken->getVariable("y")->lastDomain();
+      const IntervalDomain& th = goalToken->getVariable("th")->lastDomain();
+      const IntervalDomain& recharge_level = goalToken->getVariable("recharge_level")->lastDomain();
+
+      goalMsg.pose.position.x = (x.isSingleton()  ? x.getSingletonValue() : (x.getLowerBound() + x.getUpperBound()) / 2);
+      goalMsg.pose.position.y = (y.isSingleton()  ? y.getSingletonValue() : (y.getLowerBound() + y.getUpperBound()) / 2);
+      goalMsg.pose.orientation.w = (th.isSingleton()  ? th.getSingletonValue() : (th.getLowerBound() + th.getUpperBound()) / 2);
+      goalMsg.recharge_level = (recharge_level.isSingleton()  ? recharge_level.getSingletonValue() : (recharge_level.getLowerBound() + recharge_level.getUpperBound()) / 2);
+    }
+  };  
+
+  // Allocate Factory
+  TeleoReactor::ConcreteFactory<RechargeControllerAdapter> RechargeControllerAdapter_Factory("RechargeControllerAdapter");
+}
