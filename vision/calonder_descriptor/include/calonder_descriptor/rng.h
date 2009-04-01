11298ede887e64f473163d6b9bf8e6c62c621abf
pmihelich <pmihelich@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-04-01 05:01:18 +0000

    calonder_descriptor: Removed optional GSL code from rng.h due to licensing.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@13221 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 7b1f444ebe63fa87ddd7a974ee6d64979b9f442d

--- vision/calonder_descriptor/include/calonder_descriptor/rng.h
+++ vision/calonder_descriptor/include/calonder_descriptor/rng.h
@@ -1,31 +1,21 @@
 #ifndef FEATURES_RNG_H
 #define FEATURES_RNG_H
 
-#ifdef HAVE_GSL
-#include <gsl/gsl_rng.h>
-#include <gsl/gsl_randist.h>
-#else
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int.hpp>
 #include <boost/random/uniform_real.hpp>
 #include <boost/random/normal_distribution.hpp>
 #include <boost/random.hpp>
-#endif
 
 namespace features {
 
 /*!
   A pseudo-random number generator usable with std::random_shuffle.
-  If HAVE_GSL is defined, uses GSL as a backend, otherwise Boost.Random.
  */
 class Rng
 {
 public:
-#ifdef HAVE_GSL
-  typedef unsigned long int_type;
-#else
   typedef uint32_t int_type;
-#endif
   
   Rng(int_type seed = 0);
 
@@ -46,55 +36,10 @@ public:
   double gaussian(double sigma);
 
 private:
-#ifdef HAVE_GSL
-  gsl_rng* state_;
-#else
   typedef boost::mt19937 engine_type;
   engine_type engine_;
-#endif
 };
 
-#ifdef HAVE_GSL
-/* Implementation using GSL backend */
-
-inline Rng::Rng(int_type seed)
-{
-  state_ = gsl_rng_alloc(gsl_rng_taus);
-  Rng::seed(seed);
-}
-
-inline Rng::~Rng()
-{
-  gsl_rng_free(state_);
-}
-
-inline void Rng::seed(int_type seed)
-{
-  gsl_rng_set(state_, seed);
-}
-
-inline Rng::int_type Rng::operator()()
-{
-  return gsl_rng_get(state_);
-}
-
-inline Rng::int_type Rng::operator()(int_type N)
-{
-  return gsl_rng_uniform_int(state_, N);
-}
-
-inline double Rng::uniform(double a, double b)
-{
-  return gsl_ran_flat(state_, a, b);
-}
-
-inline double Rng::gaussian(double sigma)
-{
-  return gsl_ran_gaussian_ziggurat(state_, sigma);
-}
-
-#else
-/* Implementation using Boost.Random backend */
 
 inline Rng::Rng(int_type seed)
   : engine_(seed)
@@ -131,8 +76,6 @@ inline double Rng::gaussian(double sigma)
   return normal_sampler();  
 }
 
-#endif
-  
 } // namespace features
 
 #endif
