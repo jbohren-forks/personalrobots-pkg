1fdac66e070e2b1e66aeecfc76083d944ca50d92
jamesbowman <jamesbowman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-04-10 00:18:37 +0000

    Picture map first cut
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@13719 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: cc6ca4501c07389884bfca9c4b0ff4c85e548dc4

---
+++ vision/vslam/nodes/picmap.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2009, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the Willow Garage nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import roslib
+roslib.load_manifest('vslam')
+
+import sys
+import time
+import getopt
+
+from math import *
+
+from stereo import DenseStereoFrame, SparseStereoFrame
+from visualodometer import VisualOdometer, Pose, DescriptorSchemeCalonder, DescriptorSchemeSAD, FeatureDetectorFast, FeatureDetector4x4, FeatureDetectorStar, FeatureDetectorHarris
+from vis import Vis
+from picturemap import Picture, PictureMap
+import pytf_swig
+
+import image_msgs.msg
+import robot_msgs.msg
+import tf.msg
+import deprecated_msgs.msg
+
+import camera
+import rospy
+import Image
+
+import time
+import math
+
+import vslam.msg
+
+def py_transform_from_transform_stamped(transform):
+    rot = transform.transform.rotation
+    tr = transform.transform.translation
+    ptf = pytf_swig.pyTransform()
+    ptf.qx = rot.x
+    ptf.qy = rot.y
+    ptf.qz = rot.z
+    ptf.qw = rot.w
+    ptf.x  = tr.x
+    ptf.y  = tr.y
+    ptf.z  = tr.z
+    ptf.frame_id = transform.header.frame_id
+    ptf.parent_id = transform.parent_id
+    ptf.stamp = transform.header.stamp.to_seconds()
+    return ptf
+
+class PicmapNode:
+  def __init__(self, args):
+    rospy.init_node('picmap_server')
+    stereo_cam = camera.Camera((389.0, 389.0, 89.23 * 1e-3, 323.42, 323.42, 274.95))
+
+    self.vo = None
+    self.transformer = pytf_swig.pyTransformer()
+    self.transformer.setExtrapolationLimit(0.0)
+
+    #self.pub = rospy.Publisher("/picmap_pose", vslam.msg.Picmap)
+
+    rospy.TopicSub('/stereo/raw_stereo', image_msgs.msg.RawStereo, self.handle_raw_stereo, queue_size=1, buff_size=7000000)
+    rospy.TopicSub('/amcl_pose', robot_msgs.msg.PoseWithCovariance, self.handle_amcl_pose)
+    rospy.TopicSub('/tf_message', tf.msg.tfMessage, self.handle_tf)
+
+  def handle_tf(self, msg):
+    print "incoming transform", min([ t.header.stamp.to_seconds() for t in msg.transforms ])
+    for transform in msg.transforms:
+      ptf = py_transform_from_transform_stamped(transform)
+      self.transformer.setTransform(ptf)
+
+  def handle_raw_stereo(self, msg):
+    print "incoming picture  ", msg.header.stamp.to_seconds()
+    size = (msg.left_info.width, msg.left_info.height)
+    cam = camera.StereoCamera(msg.right_info)
+    if self.vo == None:
+      self.fd = FeatureDetectorFast(300)
+      self.ds = DescriptorSchemeCalonder()
+      self.vo = VisualOdometer(cam, scavenge = False,
+                          inlier_error_threshold = 3.0, sba = None,
+                          inlier_thresh = 100,
+                          position_keypoint_thresh = 0.2, angle_keypoint_thresh = 0.15)
+      self.keys = set()
+      self.v = Vis('raw stereo')
+      self.pm = PictureMap(self.ds)
+
+    pair = [Image.fromstring("L", size, i.uint8_data.data) for i in [ msg.left_image, msg.right_image ]]
+    af = SparseStereoFrame(pair[0], pair[1], feature_detector = self.fd, descriptor_scheme = self.ds)
+    af.t = msg.header.stamp.to_seconds()
+    self.v.show(msg.left_image.uint8_data.data, [])
+
+    self.vo.handle_frame(af)
+
+    k = self.vo.keyframe
+    if (not (k.id in self.keys)):
+      self.keys.add(k.id)
+      picture = Picture(k.features(), k.descriptors())
+      self.pm.newpic(k.t, cam, picture, True)
+
+    picture = Picture(af.features(), af.descriptors())
+    self.pm.newpic(msg.header.stamp.to_seconds(), cam, picture, False)
+
+    self.pm.resolve(self.transformer)
+
+  def handle_amcl_pose(self, msg):
+    if self.vo:
+      happy = max([msg.covariance[0], msg.covariance[7]]) < 0.003
+      print "picmap node got amcl", msg.covariance[0], msg.covariance[7], "happy", happy
+      self.pm.newLocalization(msg.header.stamp.to_seconds(), happy)
+
+def main(args):
+  rms = PicmapNode(args)
+  rospy.spin()
+
+if __name__ == '__main__':
+  main(sys.argv)
---
+++ vision/vslam/src/picturemap.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2009, Willow Garage, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the Willow Garage nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import roslib
+roslib.load_manifest('vslam')
+import rostest
+import rospy
+
+import vop
+
+import sys
+
+import visual_odometry as VO
+import Image as Image
+import ImageChops as ImageChops
+import ImageDraw as ImageDraw
+import ImageFilter as ImageFilter
+
+import random
+import unittest
+import math
+import pickle
+import time
+import place_recognition
+
+import numpy
+
+from visualodometer import DescriptorSchemeCalonder, Pose
+from pe import PoseEstimator
+from skeleton import Skeleton
+from reader import reader
+
+from tf import transformations
+
+def getpose(transformer, parent_frame, frame, ttime):
+    ptf = transformer.getTransform(parent_frame, frame, ttime)
+    p = Pose(transformations.rotation_matrix_from_quaternion((ptf.qx, ptf.qy, ptf.qz, ptf.qw))[:3,:3], numpy.array((ptf.x, ptf.y, ptf.z)))
+    return p
+
+class Timeline:
+  def __init__(self, length):
+    self.q = []
+    self.length = length
+
+  def add(self, time, datum):
+    self.q.append((time, datum))
+    while len(self.q) > self.length:
+      oldest = min(self.q)
+      self.q.remove(oldest)
+
+  def query(self, time):
+    earlier = [ (t,d) for (t,d) in self.q if t < time ]
+    later = [ (t,d) for (t,d) in self.q if t > time ]
+    if earlier != [] and later != []:
+      return (max(earlier)[1], min(later)[1])
+    else:
+      return None
+
+  def __iter__(self):
+    return self.q.__iter__()
+
+  def __delitem__(self, i):
+    self.q.remove(i)
+
+class Picture:
+  def __init__(self, keypoints, descriptors, pose = None):
+    self.keypoints = keypoints
+    self.descriptors = descriptors
+    self.pose = pose
+
+class PictureMap:
+  """ Picture map """
+
+  def __init__(self, ds):
+    self.cameras = []
+    self.pics = []
+    filename = '/u/mihelich/images/holidays/holidays.tree'
+    self.vt = place_recognition.load(filename)
+
+    self.pe = PoseEstimator()
+
+    self.clear()
+
+    self.ds = ds
+
+    self.verbose = 1
+
+  def clear(self):
+    self.localizationOK = Timeline(10)
+    self.observations = Timeline(30)
+
+  def newLocalization(self, t, l):
+    self.localizationOK.add(t, l)
+
+  def isVerbosity(self, n):
+    return n <= self.verbose
+
+  def newpic(self, when, cam, picture, iskey):
+    if self.isVerbosity(1):
+      print "Adding picture", when
+    self.observations.add(when, (cam, picture, iskey))
+
+  def find(self, descriptors):
+    """ Search the map for a camera's frame """
+    r = self.vt.topN(None, descriptors, len(self.pics), False)
+    best = min([ (d,i) for (i,d) in enumerate(r) ])[1]
+    return best
+
+  def resolve(self, transformer):
+    if self.isVerbosity(1):
+      print "resolver"
+      print "getLatestCommonTime", transformer.getLatestCommonTime('map', 'stereo_link')
+      print "obs earliest       ", min(self.observations)[0]
+      print "obs latest         ", max(self.observations)[0]
+    for (t,(cam,picture,iskey)) in self.observations:
+      # Add keys to the map if they are well-localized and TF can transform them
+      if iskey and transformer.canTransform('map', 'stereo_link', t):
+        loc = self.localizationOK.query(t)
+        if loc != None:
+          if loc == (True,True):
+            picture.pose = getpose(transformer, 'map', 'stereo_link', t)
+            if not(cam in self.cameras):
+              self.cameras.append(cam)
+            ci = self.cameras.index(cam)
+            self.pics.append((ci, picture))
+            self.vt.add(None, picture.descriptors)
+            if 1 <= self.verbose:
+              print "Added image to picture map"
+          del self.observations[(t,(cam,picture,iskey))]
+
+    for (t,(cam,picture,iskey)) in self.observations:
+      # If localization is failing, look up the frame and return its pose
+      if not iskey and transformer.canTransform('stereo_link', 'base_link', t):
+        loc = self.localizationOK.query(t)
+        if loc != None:
+          if (loc[0] == False or loc[1] == False):
+            kp = picture.keypoints
+            descriptors = picture.descriptors
+
+            best = self.find(descriptors)
+            match_cam = self.cameras[self.pics[best][0]]
+            match_pic = self.pics[best][1]
+
+            pairs = self.ds.match0(match_pic.keypoints, match_pic.descriptors, kp, descriptors)
+            pairs = [(b,a) for (a,b,d) in pairs]
+            (inl,R,S) = self.pe.estimateC(match_cam, match_pic.keypoints, cam, kp, pairs)
+            if inl > 100:
+              r33 = numpy.mat(numpy.array(R).reshape(3,3))
+              rel_pose = Pose(r33, numpy.array(S))
+              cam90 = Pose(numpy.array([[ 0, 0, 1 ], [ -1, 0, 0 ], [ 0, -1, 0 ]]), numpy.array([0, 0, 0]))
+              rel_pose = cam90 * rel_pose * ~cam90
+
+              # Figure out camera's pose in map
+
+              m2s = match_pic.pose
+              cam_in_map = (m2s * rel_pose)
+              base_in_map = cam_in_map * getpose(transformer, 'stereo_link', 'base_link', t)
+              r = (t, base_in_map)
+            else:
+              r = None
+            del self.observations[(t,(cam,picture,iskey))]
+            return r
+    return None
+
+  def __getstate__(self):
+    return (self.cameras, self.pics)
+
+  def __setstate__(self, st):
+    self.__init__()
+    (self.cameras, self.pics) = st
+    for p in self.pics:
+      self.vt.add(None, p[1].descriptors)
+
