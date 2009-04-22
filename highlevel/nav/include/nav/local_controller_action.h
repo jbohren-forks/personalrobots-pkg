9f6b63a251b74099da22068815f3e6d3aefd5e93
eitanme <eitanme@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-04-22 18:51:24 +0000

    Adding a local controller action
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@14228 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 03f17ca228705970c9433c2c3bcb993155019747

---
+++ highlevel/nav/include/nav/local_controller_action.h
@@ -0,0 +1,114 @@
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Eitan Marder-Eppstein
+*********************************************************************/
+#ifndef NAV_MOVE_BASE_ACTION_H_
+#define NAV_MOVE_BASE_ACTION_H_
+#include <robot_actions/action.h>
+#include <robot_actions/action_runner.h>
+#include <robot_actions/MoveBaseStateNew.h>
+#include <robot_msgs/PoseStamped.h>
+#include <ros/node.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h>
+#include <base_local_planner/trajectory_planner_ros.h>
+#include <vector>
+#include <string>
+
+namespace nav {
+  /**
+   * @class LocalControllerAction
+   * @brief A class adhering to the robot_actions::Action interface that moves the robot base to a goal location.
+   */
+  class LocalControllerAction : public robot_actions::Action<robot_msgs::PoseStamped, robot_msgs::PoseStamped> {
+    public:
+      /**
+       * @brief  Constructor for the actions
+       * @param ros_node A reference to the ros node used 
+       * @param tf A reference to a TransformListener
+       * @return 
+       */
+      LocalControllerAction(ros::Node& ros_node, tf::TransformListener& tf);
+
+      /**
+       * @brief  Destructor - Cleans up
+       */
+      virtual ~LocalControllerAction();
+
+      /**
+       * @brief  Runs whenever a new goal is sent to the move_base
+       * @param goal The goal to pursue 
+       * @param feedback Feedback that the action gives to a higher-level monitor, in this case, the position of the robot
+       * @return The result of the execution, ie: Success, Preempted, Aborted, etc.
+       */
+      virtual robot_actions::ResultStatus execute(const robot_msgs::PoseStamped& goal, robot_msgs::PoseStamped& feedback);
+
+    private:
+      /**
+       * @brief  Sleeps for the remainder of a cycle
+       * @param  start The start time of the cycle
+       * @param  cycle_time The desired cycle time
+       * @param  actual Will be set to the actual cycle time of the loop
+       * @return True if the desired cycle time is met, false otherwise
+       */
+      bool sleepLeftover(ros::Time start, ros::Duration cycle_time, ros::Duration& actual);
+
+      /**
+       * @brief  Get the current pose of the robot in the specified frame
+       * @param  frame The frame to get the pose in
+       * @param  pose The pose returned
+       */
+      void getRobotPose(std::string frame, tf::Stamped<tf::Pose>& pose);
+
+      /**
+       * @brief  Resets the costmaps to the static map outside a given window
+       */
+      void resetCostMaps();
+
+      ros::Node& ros_node_;
+      tf::TransformListener& tf_;
+      base_local_planner::TrajectoryPlannerROS* tc_;
+      costmap_2d::Costmap2DROS* controller_cost_map_ros_;
+      costmap_2d::Costmap2D controller_cost_map_;
+
+      std::vector<robot_msgs::Point> footprint_;
+      std::string robot_base_frame_;
+
+      double controller_frequency_;
+
+  };
+};
+#endif
+
---
+++ highlevel/nav/src/local_controller_action.cpp
@@ -0,0 +1,224 @@
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Eitan Marder-Eppstein
+*********************************************************************/
+#include <nav/local_controller_action.h>
+
+using namespace base_local_planner;
+using namespace costmap_2d;
+using namespace robot_actions;
+
+namespace nav {
+  LocalControllerAction::LocalControllerAction(ros::Node& ros_node, tf::TransformListener& tf) : 
+    Action<robot_msgs::PoseStamped, robot_msgs::PoseStamped>(ros_node.getName()), ros_node_(ros_node), tf_(tf),
+    tc_(NULL), controller_cost_map_ros_(NULL) {
+
+    //get some parameters that will be global to the move base node
+    ros_node_.param("~base_local_planner/robot_base_frame", robot_base_frame_, std::string("base_link"));
+    ros_node_.param("~controller_frequency", controller_frequency_, 20.0);
+
+    //for comanding the base
+    ros_node_.advertise<robot_msgs::PoseDot>("cmd_vel", 1);
+
+    double inscribed_radius, circumscribed_radius;
+    //we'll assume the radius of the robot to be consistent with what's specified for the costmaps
+    ros_node_.param("~base_local_planner/costmap/inscribed_radius", inscribed_radius, 0.325);
+    ros_node_.param("~base_local_planner/costmap/circumscribed_radius", circumscribed_radius, 0.46);
+
+    //create the ros wrapper for the controller's cost_map... and initializer a pointer we'll use with the underlying map
+    controller_cost_map_ros_ = new Costmap2DROS(ros_node_, tf_, std::string("base_local_planner"));
+    controller_cost_map_ros_->getCostMapCopy(controller_cost_map_);
+
+    robot_msgs::Point pt;
+    //create a square footprint
+    pt.x = inscribed_radius + .01;
+    pt.y = -1 * (inscribed_radius + .01);
+    footprint_.push_back(pt);
+    pt.x = -1 * (inscribed_radius + .01);
+    pt.y = -1 * (inscribed_radius + .01);
+    footprint_.push_back(pt);
+    pt.x = -1 * (inscribed_radius + .01);
+    pt.y = inscribed_radius + .01;
+    footprint_.push_back(pt);
+    pt.x = inscribed_radius + .01;
+    pt.y = inscribed_radius + .01;
+    footprint_.push_back(pt);
+
+    //give the robot a nose
+    pt.x = circumscribed_radius;
+    pt.y = 0;
+    footprint_.push_back(pt);
+
+    //create a trajectory controller
+    tc_ = new TrajectoryPlannerROS(ros_node_, tf_, controller_cost_map_, footprint_, &controller_cost_map_);
+  }
+
+  LocalControllerAction::~LocalControllerAction(){
+    if(tc_ != NULL)
+      delete tc_;
+
+    if(controller_cost_map_ros_ != NULL)
+      delete controller_cost_map_ros_;
+  }
+
+  void LocalControllerAction::getRobotPose(std::string frame, tf::Stamped<tf::Pose>& pose){
+    tf::Stamped<tf::Pose> robot_pose;
+    robot_pose.setIdentity();
+    robot_pose.frame_id_ = robot_base_frame_;
+    robot_pose.stamp_ = ros::Time();
+
+    try{
+      tf_.transformPose(frame, robot_pose, pose);
+    }
+    catch(tf::LookupException& ex) {
+      ROS_ERROR("No Transform available Error: %s\n", ex.what());
+      return; // kind of pointless unless there's more code added below this try catch block
+    }
+    catch(tf::ConnectivityException& ex) {
+      ROS_ERROR("Connectivity Error: %s\n", ex.what());
+      return;
+    }
+    catch(tf::ExtrapolationException& ex) {
+      ROS_ERROR("Extrapolation Error: %s\n", ex.what());
+    }
+  }
+
+  robot_actions::ResultStatus LocalControllerAction::execute(const robot_msgs::PoseStamped& goal, robot_msgs::PoseStamped& feedback){
+    ros::Duration cycle_time = ros::Duration(1.0 / controller_frequency_);
+    while(!isPreemptRequested()){
+      struct timeval start, end;
+      double start_t, end_t, t_diff;
+      gettimeofday(&start, NULL);
+
+      //get the start time of the loop
+      ros::Time start_time = ros::Time::now();
+
+      //update feedback to correspond to our current position
+      tf::Stamped<tf::Pose> global_pose;
+      getRobotPose(goal.header.frame_id, global_pose);
+      tf::PoseStampedTFToMsg(global_pose, feedback);
+
+      //push the feedback out
+      update(feedback);
+
+      //make sure to update the cost_map we'll use for this cycle
+      controller_cost_map_ros_->getCostMapCopy(controller_cost_map_);
+
+      //check that the observation buffers for the costmap are current
+      if(!controller_cost_map_ros_->isCurrent()){
+        ROS_WARN("Sensor data is out of date, we're not going to allow commanding of the base for safety");
+        continue;
+      }
+
+
+      bool valid_control = false;
+      robot_msgs::PoseDot cmd_vel;
+      //pass plan to controller
+      std::vector<robot_msgs::PoseStamped> global_plan;
+      robot_msgs::PoseStamped robot_start;
+      tf::PoseStampedTFToMsg(global_pose, robot_start);
+      global_plan.push_back(robot_start);
+      global_plan.push_back(goal);
+      tc_->updatePlan(global_plan);
+      //get observations for the non-costmap controllers
+      std::vector<Observation> observations;
+      controller_cost_map_ros_->getMarkingObservations(observations);
+      valid_control = tc_->computeVelocityCommands(cmd_vel, observations);
+
+      //give the base the velocity command
+      ros_node_.publish("cmd_vel", cmd_vel);
+
+      //check for success
+      if(tc_->goalReached())
+        return robot_actions::SUCCESS;
+
+
+      //if we don't have a valid control... we'll abort
+      if(!valid_control){
+        return robot_actions::ABORTED;
+      }
+
+      gettimeofday(&end, NULL);
+      start_t = start.tv_sec + double(start.tv_usec) / 1e6;
+      end_t = end.tv_sec + double(end.tv_usec) / 1e6;
+      t_diff = end_t - start_t;
+      ROS_DEBUG("Full control cycle: %.9f Valid control: %d, Vel Cmd (%.2f, %.2f, %.2f)", t_diff, valid_control, cmd_vel.vel.vx, cmd_vel.vel.vy, cmd_vel.ang_vel.vz);
+
+      ros::Duration actual;
+      //sleep the remainder of the cycle
+      if(!sleepLeftover(start_time, cycle_time, actual))
+        ROS_WARN("Controll loop missed its desired cycle time of %.4f... the loop actually took %.4f seconds", cycle_time.toSec(), actual.toSec());
+    }
+    return robot_actions::PREEMPTED;
+  }
+
+  bool LocalControllerAction::sleepLeftover(ros::Time start, ros::Duration cycle_time, ros::Duration& actual){
+    ros::Time expected_end = start + cycle_time;
+    ros::Time actual_end = ros::Time::now();
+    ///@todo: because durations don't handle subtraction properly right now
+    ros::Duration sleep_time = ros::Duration((expected_end - actual_end).toSec()); 
+
+    //set the actual amount of time the loop took
+    actual = actual_end - start;
+
+    if(sleep_time < ros::Duration(0.0)){
+      return false;
+    }
+
+    sleep_time.sleep();
+    return true;
+  }
+
+  void LocalControllerAction::resetCostMaps(){
+    controller_cost_map_ros_->resetMapOutsideWindow(5.0, 5.0);
+  }
+
+};
+
+int main(int argc, char** argv){
+  ros::init(argc, argv);
+  ros::Node ros_node("local_controller_node");
+  tf::TransformListener tf(ros_node, true, ros::Duration(10));
+  
+  nav::LocalControllerAction move_base(ros_node, tf);
+  robot_actions::ActionRunner runner(20.0);
+  runner.connect<robot_msgs::PoseStamped, MoveBaseStateNew, robot_msgs::PoseStamped>(move_base);
+  runner.run();
+
+  ros_node.spin();
+
+  return(0);
+
+}
