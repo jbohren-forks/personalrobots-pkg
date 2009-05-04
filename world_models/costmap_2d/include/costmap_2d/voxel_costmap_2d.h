91eee4a5a5a062b346f5d847f411328c4ccd5174
eitanme <eitanme@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-05-04 19:03:18 +0000

    Adding a Voxel Grid version of the costmap
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@14837 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 3bf6da6ac89d6df31ba5a46a8c2e3c4eee4d17e4

---
+++ world_models/costmap_2d/include/costmap_2d/voxel_costmap_2d.h
@@ -0,0 +1,157 @@
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Eitan Marder-Eppstein
+*********************************************************************/
+#ifndef COSTMAP_VOXEL_COSTMAP_2D_H_
+#define COSTMAP_VOXEL_COSTMAP_2D_H_
+
+#include <vector>
+#include <queue>
+#include <costmap_2d/costmap_2d.h>
+#include <costmap_2d/observation.h>
+#include <costmap_2d/cell_data.h>
+#include <costmap_2d/cost_values.h>
+#include <voxel_grid/voxel_grid.h>
+#include <robot_msgs/PointCloud.h>
+#include <boost/thread.hpp>
+
+namespace costmap_2d {
+  /**
+   * @class VoxelCostmap2D
+   * @brief A 2D costmap provides a mapping between points in the world and their associated "costs".
+   */
+  class VoxelCostmap2D : public Costmap2D {
+    public:
+      /**
+       * @brief  Constructor for a voxel grid based costmap
+       * @param  cells_size_x The x size of the map in cells
+       * @param  cells_size_y The y size of the map in cells
+       * @param  cells_size_z The z size of the map in cells... up to 32 cells max
+       * @param  xy_resolution The horizontal resolution of the map in meters/cell
+       * @param  z_resolution The vertical resolution of the map in meters/cell
+       * @param  origin_x The x origin of the map
+       * @param  origin_y The y origin of the map
+       * @param  origin_z The z origin of the map
+       * @param  inscribed_radius The inscribed radius of the robot
+       * @param  circumscribed_radius The circumscribed radius of the robot
+       * @param  inflation_radius How far out to inflate obstacles
+       * @param  obstacle_range The maximum range at which obstacles will be put into the costmap
+       * @param  max_obstacle_height The maximum height of obstacles that will be considered
+       * @param  raytrace_range The maximum distance we'll raytrace out to
+       * @param  weight The scaling factor for the cost function. Should be 0 < weight <= 1. Lower values reduce effective cost.
+       * @param  static_data Data used to initialize the costmap
+       * @param  lethal_threshold The cost threshold at which a point in the static data is considered a lethal obstacle
+       * @param  unknown_threshold The maximum number of unknown voxel cells that can exist in a column considered as free space
+       * @param  mark_threshold The maximum number of marked voxel cells that can exist in a column considered as free space
+       */
+      VoxelCostmap2D(unsigned int cells_size_x, unsigned int cells_size_y, unsigned int cells_size_z,
+          double xy_resolution, double z_resolution, double origin_x, double origin_y, double origin_z_ = 0.0, double inscribed_radius = 0.0,
+          double circumscribed_radius = 0.0, double inflation_radius = 0.0, double obstacle_range = 0.0,
+          double max_obstacle_height = 0.0, double raytrace_range = 0.0, double weight = 25.0,
+          const std::vector<unsigned char>& static_data = std::vector<unsigned char>(0), unsigned char lethal_threshold = 0,
+          unsigned int unknown_threshold = 0, unsigned int mark_threshold = 0);
+
+      /**
+       * @brief  Destructor
+       */
+      ~VoxelCostmap2D();
+
+      /**
+       * @brief  Revert to the static map outside of a specified window centered at a world coordinate
+       * @param wx The x coordinate of the center point of the window in world space (meters)
+       * @param wy The y coordinate of the center point of the window in world space (meters)
+       * @param w_size_x The x size of the window in meters
+       * @param w_size_y The y size of the window in meters
+       */
+      void resetMapOutsideWindow(double wx, double wy, double w_size_x, double w_size_y);
+
+      /**
+       * @brief  Move the origin of the costmap to a new location.... keeping data when it can
+       * @param  new_origin_x The x coordinate of the new origin
+       * @param  new_origin_y The y coordinate of the new origin
+       */
+      void updateOrigin(double new_origin_x, double new_origin_y);
+
+      //void getPoints(robot_msgs::PointCloud& cloud);
+
+    private:
+      /**
+       * @brief  Insert new obstacles into the cost map
+       * @param obstacles The point clouds of obstacles to insert into the map 
+       * @param inflation_queue The queue to place the obstacles into for inflation
+       */
+      void updateObstacles(const std::vector<Observation>& observations, std::priority_queue<CellData>& inflation_queue);
+
+      /**
+       * @brief  Clear freespace based on any number of observations
+       * @param clearing_observations The observations used to raytrace 
+       */
+      void raytraceFreespace(const std::vector<Observation>& clearing_observations);
+
+      /**
+       * @brief  Clear freespace from an observation
+       * @param clearing_observation The observation used to raytrace 
+       */
+      void raytraceFreespace(const Observation& clearing_observation);
+
+      inline bool worldToMap3D(double wx, double wy, double wz, unsigned int& mx, unsigned int& my, unsigned int& mz){
+        if(wx < origin_x_ || wy < origin_y_ || wz < origin_z_)
+          return false;
+        mx = (int) ((wx - origin_x_) / xy_resolution_);
+        my = (int) ((wy - origin_y_) / xy_resolution_);
+        mz = (int) ((wz - origin_z_) / z_resolution_);
+        return true;
+      }
+
+      inline void mapToWorld3D(unsigned int mx, unsigned int my, unsigned int mz, double& wx, double& wy, double& wz){
+        //returns the center point of the cell
+        wx = origin_x_ + (mx + 0.5) * xy_resolution_;
+        wy = origin_y_ + (my + 0.5) * xy_resolution_;
+        wz = origin_z_ + (mz + 0.5) * z_resolution_;
+      }
+
+      inline double dist(double x0, double y0, double z0, double x1, double y1, double z1){
+        return sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0) + (z1 - z0) * (z1 - z0));
+      }
+
+    protected:
+      voxel_grid::VoxelGrid voxel_grid_;
+      double xy_resolution_, z_resolution_, origin_z_;
+      unsigned int unknown_threshold_, mark_threshold_;
+
+  };
+};
+
+#endif
---
+++ world_models/costmap_2d/src/voxel_costmap_2d.cpp
@@ -0,0 +1,345 @@
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Eitan Marder-Eppstein
+*********************************************************************/
+#include <costmap_2d/voxel_costmap_2d.h>
+
+using namespace std;
+using namespace robot_msgs;
+
+namespace costmap_2d{
+  VoxelCostmap2D::VoxelCostmap2D(unsigned int cells_size_x, unsigned int cells_size_y, unsigned int cells_size_z,
+      double xy_resolution, double z_resolution, double origin_x, double origin_y, double origin_z, double inscribed_radius,
+      double circumscribed_radius, double inflation_radius, double obstacle_range,
+      double max_obstacle_height, double raytrace_range, double weight,
+      const std::vector<unsigned char>& static_data, unsigned char lethal_threshold, unsigned int unknown_threshold, unsigned int mark_threshold) 
+    : Costmap2D(cells_size_x, cells_size_y, xy_resolution, origin_x, origin_y, inscribed_radius, circumscribed_radius,
+        inflation_radius, obstacle_range, max_obstacle_height, raytrace_range, weight, static_data, lethal_threshold),
+    voxel_grid_(cells_size_x, cells_size_y, cells_size_z), xy_resolution_(xy_resolution), z_resolution_(z_resolution),
+    origin_z_(origin_z), unknown_threshold_(unknown_threshold), mark_threshold_(mark_threshold)
+  {
+  }
+
+  VoxelCostmap2D::~VoxelCostmap2D(){}
+
+  void VoxelCostmap2D::resetMapOutsideWindow(double wx, double wy, double w_size_x, double w_size_y){
+    ROS_ASSERT_MSG(w_size_x >= 0 && w_size_y >= 0, "You cannot specify a negative size window");
+
+    double start_point_x = wx - w_size_x / 2;
+    double start_point_y = wy - w_size_y / 2;
+    double end_point_x = start_point_x + w_size_x;
+    double end_point_y = start_point_y + w_size_y;
+
+    //check start bounds
+    start_point_x = max(origin_x_, start_point_x);
+    start_point_y = max(origin_y_, start_point_y);
+
+    //check end bounds
+    end_point_x = min(origin_x_ + metersSizeX(), end_point_x);
+    end_point_y = min(origin_y_ + metersSizeY(), end_point_y);
+
+    unsigned int start_x, start_y, end_x, end_y;
+
+    //check for legality just in case
+    if(!worldToMap(start_point_x, start_point_y, start_x, start_y) || !worldToMap(end_point_x, end_point_y, end_x, end_y))
+      return;
+
+    ROS_ASSERT(end_x > start_x && end_y > start_y);
+    unsigned int cell_size_x = end_x - start_x;
+    unsigned int cell_size_y = end_y - start_y;
+
+    //we need a map to store the obstacles in the window temporarily
+    unsigned char* local_map = new unsigned char[cell_size_x * cell_size_y];
+    unsigned int* local_voxel_map = new unsigned int[cell_size_x * cell_size_y];
+
+    //copy the local window in the costmap to the local map
+    unsigned char* costmap_cell = &costmap_[getIndex(start_x, start_y)];
+    unsigned char* local_map_cell = local_map;
+    unsigned int* voxel_grid_cell = &(voxel_grid_.getData()[getIndex(start_x, start_y)]);
+    unsigned int* local_voxel = local_voxel_map;
+    for(unsigned int y = 0; y < cell_size_y; ++y){
+      for(unsigned int x = 0; x < cell_size_x; ++x){
+        *local_map_cell = *costmap_cell;
+        *local_voxel = *voxel_grid_cell;
+        local_map_cell++;
+        costmap_cell++;
+        local_voxel++;
+        voxel_grid_cell++;
+      }
+      costmap_cell += size_x_ - cell_size_x;
+      voxel_grid_cell += size_x_ - cell_size_x;
+    }
+
+    //now we'll reset the costmap to the static map
+    memcpy(costmap_, static_map_, size_x_ * size_y_ * sizeof(unsigned char));
+
+    //the voxel grid will just go back to being unknown
+    voxel_grid_.reset();
+
+    //now we want to copy the local map back into the costmap
+    costmap_cell = &costmap_[getIndex(start_x, start_y)];
+    local_map_cell = local_map;
+    voxel_grid_cell = &(voxel_grid_.getData()[getIndex(start_x, start_y)]);
+    local_voxel = local_voxel_map;
+    for(unsigned int y = 0; y < cell_size_y; ++y){
+      for(unsigned int x = 0; x < cell_size_x; ++x){
+        *costmap_cell = *local_map_cell;
+        local_map_cell++;
+        costmap_cell++;
+        local_voxel++;
+        voxel_grid_cell++;
+      }
+      costmap_cell += size_x_ - cell_size_x;
+      voxel_grid_cell += size_x_ - cell_size_x;
+    }
+
+    //clean up
+    delete[] local_map;
+    delete[] local_voxel;
+  }
+
+  void VoxelCostmap2D::updateObstacles(const vector<Observation>& observations, priority_queue<CellData>& inflation_queue){
+    //place the new obstacles into a priority queue... each with a priority of zero to begin with
+    for(vector<Observation>::const_iterator it = observations.begin(); it != observations.end(); ++it){
+      const Observation& obs = *it;
+
+      const PointCloud& cloud =obs.cloud_;
+
+      for(unsigned int i = 0; i < cloud.pts.size(); ++i){
+        //if the obstacle is too high or too far away from the robot we won't add it
+        if(cloud.pts[i].z > max_obstacle_height_)
+          continue;
+
+        //compute the squared distance from the hitpoint to the pointcloud's origin
+        double sq_dist = (cloud.pts[i].x - obs.origin_.x) * (cloud.pts[i].x - obs.origin_.x)
+          + (cloud.pts[i].y - obs.origin_.y) * (cloud.pts[i].y - obs.origin_.y)
+          + (cloud.pts[i].z - obs.origin_.z) * (cloud.pts[i].z - obs.origin_.z);
+
+        //if the point is far enough away... we won't consider it
+        if(sq_dist >= sq_obstacle_range_)
+          continue;
+
+        //now we need to compute the map coordinates for the observation
+        unsigned int mx, my, mz;
+        if(!worldToMap3D(cloud.pts[i].x, cloud.pts[i].y, cloud.pts[i].z, mx, my, mz))
+          continue;
+
+        //mark the cell in the voxel grid
+        voxel_grid_.markVoxel(mx, my, mz);
+        unsigned int index = getIndex(mx, my);
+
+        //push the relevant cell index back onto the inflation queue
+        enqueue(index, mx, my, mx, my, inflation_queue);
+      }
+    }
+  }
+
+  void VoxelCostmap2D::raytraceFreespace(const std::vector<Observation>& clearing_observations){
+    for(unsigned int i = 0; i < clearing_observations.size(); ++i){
+      raytraceFreespace(clearing_observations[i]);
+    }
+  }
+
+  void VoxelCostmap2D::raytraceFreespace(const Observation& clearing_observation){
+    if(clearing_observation.cloud_.pts.size() == 0)
+      return;
+
+    unsigned int sensor_x, sensor_y, sensor_z;
+    double ox = clearing_observation.origin_.x;
+    double oy = clearing_observation.origin_.y;
+    double oz = clearing_observation.origin_.z;
+    
+    if(!worldToMap3D(ox, oy, oz, sensor_x, sensor_y, sensor_z))
+      return;
+
+    //we can pre-compute the enpoints of the map outside of the inner loop... we'll need these later
+    double map_end_x = origin_x_ + metersSizeX();
+    double map_end_y = origin_y_ + metersSizeY();
+
+    for(unsigned int i = 0; i < clearing_observation.cloud_.pts.size(); ++i){
+      double wpx = clearing_observation.cloud_.pts[i].x;
+      double wpy = clearing_observation.cloud_.pts[i].y;
+      double wpz = clearing_observation.cloud_.pts[i].z;
+
+      double distance = dist(ox, oy, oz, wpx, wpy, wpz);
+      double scaling_fact = raytrace_range_ / distance;
+      scaling_fact = scaling_fact > 1.0 ? 1.0 : scaling_fact;
+      wpx = scaling_fact * (wpx - ox) + ox;
+      wpy = scaling_fact * (wpy - oy) + oy;
+      wpz = scaling_fact * (wpz - oz) + oz;
+
+      double a = wpx - ox;
+      double b = wpy - oy;
+      double c = wpz - oz;
+      //we can only raytrace to a maximum z height
+      if(wpz >= max_obstacle_height_){
+        //we know we want the vector's z value to be max_z
+        double t = (max_obstacle_height_ - .01 - oz) / c;
+        wpx = ox + a * t;
+        wpy = oy + b * t;
+        wpz = oz + c * t;
+      }
+      //and we can only raytrace down to the floor
+      else if(wpz < 0.0){
+        //we know we want the vector's z value to be 0.0
+        double t = (0.0 - oz) / c;
+        wpx = ox + a * t;
+        wpy = oy + b * t;
+        wpz = oz + c * t;
+      }
+
+      //the minimum value to raytrace from is the origin
+      if(wpx < origin_x_){
+        double t = (origin_x_ - ox) / a;
+        wpx = origin_x_;
+        wpy = oy + b * t;
+        wpz = oz + c * t;
+      }
+      if(wpy < origin_y_){
+        double t = (origin_y_ - oy) / b;
+        wpx = ox + a * t;
+        wpy = origin_y_;
+        wpz = oz + c * t;
+      }
+
+      //the maximum value to raytrace to is the end of the map
+      if(wpx > map_end_x){
+        double t = (map_end_x - ox) / a;
+        wpx = map_end_x;
+        wpy = oy + b * t;
+        wpz = oz + c * t;
+      }
+      if(wpy > map_end_y){
+        double t = (map_end_y - oy) / b;
+        wpx = ox + a * t;
+        wpy = map_end_y;
+        wpz = oz + c * t;
+      }
+
+      unsigned int point_x, point_y, point_z;
+      if(worldToMap3D(wpx, wpy, wpz, point_x, point_y, point_z)){
+        unsigned int cell_raytrace_range = cellDistance(raytrace_range_);
+        voxel_grid_.clearVoxelLineInMap(sensor_x, sensor_y, sensor_z, point_x, point_y, point_z, costmap_, unknown_threshold_, mark_threshold_, cell_raytrace_range);
+      }
+    }
+  }
+
+
+  void VoxelCostmap2D::updateOrigin(double new_origin_x, double new_origin_y){
+    //project the new origin into the grid
+    int cell_ox, cell_oy;
+    cell_ox = int((new_origin_x - origin_x_) / resolution_);
+    cell_oy = int((new_origin_y - origin_y_) / resolution_);
+
+    //compute the associated world coordinates for the origin cell
+    //beacuase we want to keep things grid-aligned
+    double new_grid_ox, new_grid_oy;
+    new_grid_ox = origin_x_ + cell_ox * resolution_;
+    new_grid_oy = origin_y_ + cell_oy * resolution_;
+
+    //To save casting from unsigned int to int a bunch of times
+    int size_x = size_x_;
+    int size_y = size_y_;
+
+    //we need to compute the overlap of the new and existing windows
+    int lower_left_x, lower_left_y, upper_right_x, upper_right_y;
+    lower_left_x = min(max(cell_ox, 0), size_x);
+    lower_left_y = min(max(cell_oy, 0), size_y);
+    upper_right_x = min(max(cell_ox + size_x, 0), size_x);
+    upper_right_y = min(max(cell_oy + size_y, 0), size_y);
+
+    unsigned int cell_size_x = upper_right_x - lower_left_x;
+    unsigned int cell_size_y = upper_right_y - lower_left_y;
+
+    //we need a map to store the obstacles in the window temporarily
+    unsigned char* local_map = new unsigned char[cell_size_x * cell_size_y];
+    unsigned int* local_voxel_map = new unsigned int[cell_size_x * cell_size_y];
+
+    //copy the local window in the costmap to the local map
+    unsigned char* costmap_cell = &costmap_[getIndex(lower_left_x, lower_left_y)];
+    unsigned char* local_map_cell = local_map;
+    unsigned int* voxel_grid_cell = &(voxel_grid_.getData()[getIndex(lower_left_x, lower_left_y)]);
+    unsigned int* local_voxel = local_voxel_map;
+    for(unsigned int y = 0; y < cell_size_y; ++y){
+      for(unsigned int x = 0; x < cell_size_x; ++x){
+        *local_map_cell = *costmap_cell;
+        local_map_cell++;
+        costmap_cell++;
+        local_voxel++;
+        voxel_grid_cell++;
+      }
+      costmap_cell += size_x_ - cell_size_x;
+      voxel_grid_cell += size_x_ - cell_size_x;
+    }
+
+    //now we'll set the costmap to be completely unknown
+    memset(costmap_, NO_INFORMATION, size_x_ * size_y_ * sizeof(unsigned char));
+
+    //the voxel grid will just go back to being unknown
+    voxel_grid_.reset();
+
+    //update the origin with the appropriate world coordinates
+    origin_x_ = new_grid_ox;
+    origin_y_ = new_grid_oy;
+
+    //compute the starting cell location for copying data back in
+    int start_x = lower_left_x - cell_ox;
+    int start_y = lower_left_y - cell_oy;
+
+    //now we want to copy the overlapping information back into the map, but in its new location
+    costmap_cell = &costmap_[getIndex(start_x, start_y)];
+    local_map_cell = local_map;
+    voxel_grid_cell = &(voxel_grid_.getData()[getIndex(start_x, start_y)]);
+    local_voxel = local_voxel_map;
+    for(unsigned int y = 0; y < cell_size_y; ++y){
+      for(unsigned int x = 0; x < cell_size_x; ++x){
+        *costmap_cell = *local_map_cell;
+        local_map_cell++;
+        costmap_cell++;
+        local_voxel++;
+        voxel_grid_cell++;
+      }
+      costmap_cell += size_x_ - cell_size_x;
+      voxel_grid_cell += size_x_ - cell_size_x;
+    }
+
+    //make sure to clean up
+    delete[] local_map;
+    delete[] local_voxel_map;
+
+  }
+
+
+};
