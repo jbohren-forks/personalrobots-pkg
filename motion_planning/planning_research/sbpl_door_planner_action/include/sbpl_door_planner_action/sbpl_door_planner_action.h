80158034f4ed19c8473d53564fa31e3af188dc23
sachinchitta <sachinchitta@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-05-05 00:58:56 +0000

    Robot action for planning a path through a closed door.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@14867 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 69917b83948367fce01ee21f80b1d69b0d61e789

---
+++ motion_planning/planning_research/sbpl_door_planner_action/include/sbpl_door_planner_action/sbpl_door_planner_action.h
@@ -0,0 +1,150 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ * 
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ * 
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#include <iostream> 
+
+/** ROS **/
+#include <ros/node.h>
+#include <boost/thread.hpp>
+
+/** TF **/
+#include <tf/tf.h>
+
+/** Messages needed for trajectory control and collision map**/
+#include <robot_msgs/Pose.h>
+#include <robot_msgs/JointTraj.h>
+#include <robot_msgs/JointTrajPoint.h>
+#include <robot_msgs/CollisionMap.h>
+
+// Costmap used for the map representation
+#include <costmap_2d/costmap_2d_ros.h>
+
+// MPGlue and sbpl headers
+#include <mpglue/sbpl_planner.h>
+#include <mpglue/sbpl_environment.h>
+#include <mpglue/plan.h>
+#include <sfl/util/strutil.hpp>
+#include <sbpl/headers.h>
+
+#include <robot_actions/action.h>
+#include <robot_actions/action_runner.h>
+#include <pr2_robot_actions/DoorActionState.h>
+#include <pr2_robot_actions/Pose2D.h>
+#include <robot_msgs/Door.h>
+#include <door_handle_detector/door_functions.h>
+
+typedef struct
+{
+    robot_msgs::Door door;
+    double door_thickness;
+    double arm_min_workspace_angle;
+    double arm_max_workspace_angle;
+    double arm_min_workspace_radius;
+    double arm_max_workspace_radius;
+    double door_angle_discretization_interval;
+    robot_msgs::Point32 shoulder;
+}DoorEnvProperties;
+
+class SBPLDoorPlanner : public robot_actions::Action<robot_msgs::Door, robot_msgs::Door>
+{
+  public:
+  SBPLDoorPlanner(ros::Node& ros_node, tf::TransformListener& tf);
+
+  virtual ~SBPLDoorPlanner();
+      
+  /**
+   * @brief  Runs whenever a new goal is sent to the move_base
+   * @param goal The goal to pursue 
+   * @param feedback Feedback that the action gives to a higher-level monitor, in this case, the position of the robot
+   * @return The result of the execution, ie: Success, Preempted, Aborted, etc.
+   */
+  virtual robot_actions::ResultStatus execute(const robot_msgs::Door& goal, robot_msgs::Door& feedback);
+
+  private:
+
+  ros::Node &ros_node_;
+  tf::TransformListener &tf_;
+  
+  boost::shared_ptr<mpglue::CostmapAccessor> cm_access_;
+  boost::shared_ptr<mpglue::IndexTransform> cm_index_;
+  boost::shared_ptr<mpglue::SBPLEnvironment> env_;
+  boost::shared_ptr<mpglue::SBPLPlannerWrap> pWrap_;
+
+  double planner_time_limit_; /* The amount of time given to the planner to find a plan */
+  double allocated_time_;
+
+  bool use_cost_map_;
+  bool forward_search_;
+
+  std::string planner_type_;
+  std::string plan_stats_file_;
+  
+  costmap_2d::Costmap2DROS *cost_map_ros_; /**< manages the cost map for us */
+  costmap_2d::Costmap2D cost_map_;        /**< local copy of the costmap underlying cost_map_ros_ */
+  
+  friend struct cm_getter;
+  struct cm_getter: public mpglue::costmap_2d_getter {
+      cm_getter(SBPLDoorPlanner * spn): spn_(spn) {}
+      virtual costmap_2d::Costmap2D const * operator () () { return &spn_->cost_map_; }
+      SBPLDoorPlanner * spn_;
+  };
+  cm_getter cm_getter_;	                   /**< for mpglue to get at our costmap instance */
+  
+  bool initializePlannerAndEnvironment(const robot_msgs::Door &door);
+  
+  bool plan(const pr2_robot_actions::Pose2D &start, const pr2_robot_actions::Pose2D &goal, robot_msgs::JointTraj &path);
+
+  std::vector<robot_msgs::Point> footprint_;
+
+  boost::recursive_mutex lock_; /*!< Lock for access to class members in callbacks */
+
+  DoorEnvProperties door_env_;
+
+  std::string global_frame_, robot_base_frame_;
+
+  bool updateGlobalPose();
+
+  pr2_robot_actions::Pose2D getPose2D(const tf::Stamped<tf::Pose> &pose);
+
+  pr2_robot_actions::Pose2D global_pose_2D_;
+
+  pr2_robot_actions::Pose2D goal_;
+
+  tf::Stamped<tf::Pose> global_pose_;
+
+  bool removeDoor();
+
+  void publishPath(const robot_msgs::JointTraj &path, std::string topic, double r, double g, double b, double a);
+
+};
---
+++ motion_planning/planning_research/sbpl_door_planner_action/src/sbpl_door_planner_action.cpp
@@ -0,0 +1,316 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sbpl_door_planner_action/sbpl_door_planner_action.h>
+#include <ros/node.h>
+
+using namespace std;
+using namespace ros;
+using namespace robot_actions;
+
+SBPLDoorPlanner::SBPLDoorPlanner(ros::Node& ros_node, tf::TransformListener& tf) : 
+  Action<robot_msgs::Door, robot_msgs::Door>(ros_node.getName()), ros_node_(ros_node), tf_(tf),
+  cost_map_ros_(NULL),cm_getter_(this)
+{
+  ros_node_.param("~allocated_time", allocated_time_, 1.0);
+  ros_node_.param("~forward_search", forward_search_, true);
+
+  ros_node_.param("~planner_type", planner_type_, string("ARAPlanner"));
+  ros_node_.param("~plan_stats_file", plan_stats_file_, string("/tmp/move_base_sbpl.log"));
+
+  double inscribed_radius, circumscribed_radius;
+  //we'll assume the radius of the robot to be consistent with what's specified for the costmaps
+  ros_node_.param("~costmap/inscribed_radius", inscribed_radius, 0.325);
+  ros_node_.param("~costmap/circumscribed_radius", circumscribed_radius, 0.46);
+  ros_node_.param("~global_frame", global_frame_, std::string("odom_combined"));
+  ros_node_.param("~robot_base_frame", robot_base_frame_, std::string("base_link"));
+
+  cost_map_ros_ = new costmap_2d::Costmap2DROS(ros_node_,tf_,std::string(" "));
+
+  ros_node_.param<double>("~door_thickness", door_env_.door_thickness, 0.05);
+  ros_node_.param<double>("~arm/min_workspace_angle",   door_env_.arm_min_workspace_angle, -M_PI);
+  ros_node_.param<double>("~arm/max_workspace_angle",   door_env_.arm_max_workspace_angle, M_PI);
+  ros_node_.param<double>("~arm/min_workspace_radius",  door_env_.arm_min_workspace_radius, 0.0);
+  ros_node_.param<double>("~arm/max_workspace_radius",  door_env_.arm_max_workspace_radius, 1.1);
+  ros_node_.param<double>("~arm/discretization_angle",  door_env_.door_angle_discretization_interval, 0.01);
+
+  double shoulder_x, shoulder_y;
+  ros_node_.param<double>("~arm/shoulder/x",shoulder_x, 0.0);
+  ros_node_.param<double>("~arm/shoulder/y",shoulder_y, -0.188);
+  door_env_.shoulder.x = shoulder_x;
+  door_env_.shoulder.y = shoulder_y;
+
+//  ros_node_.advertise<visualization_msgs::Polyline>("~robot_footprint", 1);
+  ros_node_.advertise<visualization_msgs::Polyline>("~global_plan", 1);
+
+  robot_msgs::Point pt;
+  //create a square footprint
+  pt.x = inscribed_radius;
+  pt.y = -1 * (inscribed_radius);
+  footprint_.push_back(pt);
+  pt.x = -1 * (inscribed_radius);
+  pt.y = -1 * (inscribed_radius);
+  footprint_.push_back(pt);
+  pt.x = -1 * (inscribed_radius);
+  pt.y = inscribed_radius;
+  footprint_.push_back(pt);
+  pt.x = inscribed_radius_;
+  pt.y = inscribed_radius_;
+  footprint_.push_back(pt);
+};
+
+SBPLDoorPlanner::~SBPLDoorPlanner()
+{
+//  ros_node_.unadvertise("~robot_footprint");
+  ros_node_.unadvertise("~global_plan");
+
+  if(cost_map_ros_ != NULL)
+    delete cost_map_ros_;
+}
+
+void PrintUsage(char *argv[])
+{
+  printf("USAGE: %s <cfg file>\n", argv[0]);
+}
+
+bool SBPLDoorPlanner::initializePlannerAndEnvironment(const robot_msgs::Door &door)
+{
+  door_env_.door = door;
+  // First set up the environment
+  try 
+  {
+    // We're throwing int exit values if something goes wrong, and
+    // clean up any new instances in the catch clause. The sentry
+    // gets destructed when we go out of scope, so unlock() gets
+    // called no matter what.
+    // sentry<SBPLPlannerNode> guard(this);  
+    cm_access_.reset(mpglue::createCostmapAccessor(&cm_getter_));
+    cm_index_.reset(mpglue::createIndexTransform(&cm_getter_));
+    
+    FILE *env_config_fp;
+    std::string filename = "sbpl_env_cfg_tmp.txt";
+    std::string env_config_string;
+    ros::Node::instance()->param<std::string>("~env_config_file", env_config_string, " ");
+    env_config_fp = fopen(filename.c_str(),"wt");
+    fprintf(env_config_fp,"%s",env_config_string.c_str());
+    fclose(env_config_fp);
+
+    double nominalvel_mpersecs, timetoturn45degsinplace_secs;
+    ros::Node::instance()->param("~nominalvel_mpersecs", nominalvel_mpersecs, 0.4);
+    ros::Node::instance()->param("~timetoturn45degsinplace_secs", timetoturn45degsinplace_secs, 0.6);
+    // Could also sanity check the other parameters...
+    env_.reset(mpglue::SBPLEnvironment::createXYThetaDoor(cm_access_, cm_index_, footprint_, nominalvel_mpersecs,timetoturn45degsinplace_secs, filename, 0, door));
+	
+    boost::shared_ptr<SBPLPlanner> sbplPlanner;
+    if ("ARAPlanner" == planner_type_)
+    {
+      sbplPlanner.reset(new ARAPlanner(env_->getDSI(), forward_search_));
+    }
+    else if ("ADPlanner" == planner_type_)
+    {
+      sbplPlanner.reset(new ADPlanner(env_->getDSI(), forward_search_));
+    }
+    else 
+    {
+      ROS_ERROR("in MoveBaseSBPL ctor: invalid planner_type_ \"%s\",use ARAPlanner or ADPlanner",planner_type_.c_str());
+      throw int(5);
+    }
+    pWrap_.reset(new mpglue::SBPLPlannerWrap(env_, sbplPlanner));
+  }
+  catch (int ii) 
+  {
+    exit(ii);
+  }
+  return true;
+}
+
+bool SBPLDoorPlanner::removeDoor()
+{
+  const std::vector<robot_msgs::Point> door_polygon = door_handle_detector::getPolygon(door_env_.door,door_env_.door_thickness); 
+  if(cost_map_.setConvexPolygonCost(door_polygon,costmap_2d::FREE_SPACE))
+    return true;
+  return false;
+}
+
+bool SBPLDoorPlanner::plan(const pr2_robot_actions::Pose2D &start, const pr2_robot_actions::Pose2D &goal, robot_msgs::JointTraj &path)
+{
+  ROS_INFO("[replan] getting fresh copy of costmap");
+  lock_.lock();
+  cost_map_ros_->getCostmapCopy(cost_map_);
+  lock_.unlock();
+  
+  // XXXX this is where we would cut out the doors in our local copy
+  removeDoor();
+  
+  ROS_INFO("[replan] replanning...");
+  try {
+    // Update costs
+    for (mpglue::index_t ix(cm_access_->getXBegin()); ix < cm_access_->getXEnd(); ++ix) {
+      for (mpglue::index_t iy(cm_access_->getYBegin()); iy < cm_access_->getYEnd(); ++iy) {
+	mpglue::cost_t cost;
+	if (cm_access_->getCost(ix, iy, &cost)) { // always succeeds though
+	  // Note that ompl::EnvironmentWrapper::UpdateCost() will
+	  // check if the cost has actually changed, and do nothing
+	  // if it hasn't.  It internally maintains a list of the
+	  // cells that have actually changed, and this list is what
+	  // gets "flushed" to the planner (a couple of lines
+	  // further down).
+	  env_->UpdateCost(ix, iy, cost);
+	}
+      }
+    }
+    
+    // Tell the planner about the changed costs. Again, the called
+    // code checks whether anything has really changed before
+    // embarking on expensive computations.
+    pWrap_->flushCostChanges(true);
+		
+    // Assume the robot is constantly moving, so always set start.
+    // Maybe a bit inefficient, but not as bad as "changing" the
+    // goal when it hasn't actually changed.
+    pWrap_->setStart(start.x, start.y, start.th);	
+    pWrap_->setGoal(goal.x, goal.y, goal.th);
+	
+    // BTW if desired, we could call pWrap_->forcePlanningFromScratch(true)...
+	
+    // Invoke the planner, updating the statistics in the process.
+    // The returned plan might be empty, but it will not contain a
+    // null pointer.  On planner errors, the createPlan() method
+    // throws a std::exception.
+    boost::shared_ptr<mpglue::waypoint_plan_t> plan(pWrap_->createPlan());
+	
+    if (plan->empty()) 
+    {
+      ROS_ERROR("No plan found\n");
+      return false;
+    }
+    
+    path.points.clear();	// just paranoid
+    mpglue::PlanConverter::convertToJointTraj(plan.get(), &path);
+    return true;
+  }
+  catch (std::runtime_error const & ee) {
+    ROS_ERROR("runtime_error in makePlan(): %s\n", ee.what());
+  }
+      
+  return false;
+}
+
+robot_actions::ResultStatus SBPLDoorPlanner::execute(const robot_msgs::Door& door, robot_msgs::Door& feedback)
+{
+  robot_msgs::JointTraj path;
+  if(!updateGlobalPose())
+  {
+    return robot_actions::ABORTED;
+  }
+
+  if(!initializePlannerAndEnvironment(door))
+  {
+    return robot_actions::ABORTED;
+  }
+
+  if(!isPreemptRequested())
+  {
+    if(!plan(global_pose_2D_, goal_, path))
+    {
+      return robot_actions::ABORTED;      
+    }
+  }
+
+  if(!isPreemptRequested())
+  {
+    publishPath(path,"global_plan",0,1,0,0);
+  }
+  return robot_actions::SUCCESS;
+}
+
+void SBPLDoorPlanner::publishPath(const robot_msgs::JointTraj &path, std::string topic, double r, double g, double b, double a)
+{
+    visualization_msgs::Polyline gui_path_msg;
+    gui_path_msg.header.frame_id = global_frame_;
+
+    //given an empty path we won't do anything
+    if(path.get_points_size() > 0){
+      // Extract the plan in world co-ordinates, we assume the path is all in the same frame
+      gui_path_msg.header.stamp = door_env_.door.header.stamp;
+      gui_path_msg.set_points_size(path.get_points_size());
+      for(unsigned int i=0; i < path.get_points_size(); i++){
+        gui_path_msg.points[i].x = path.points[i].positions[0];
+        gui_path_msg.points[i].y = path.points[i].positions[1];
+        gui_path_msg.points[i].z = path.points[i].positions[2];
+      }
+    }
+
+    gui_path_msg.color.r = r;
+    gui_path_msg.color.g = g;
+    gui_path_msg.color.b = b;
+    gui_path_msg.color.a = a;
+
+    ros_node_.publish("~" + topic, gui_path_msg);
+}
+
+
+bool SBPLDoorPlanner::updateGlobalPose()
+{
+  tf::Stamped<tf::Pose> robot_pose;
+  robot_pose.setIdentity();
+  robot_pose.frame_id_ = robot_base_frame_;
+  robot_pose.stamp_ = ros::Time();
+
+  try
+  {
+    tf_.transformPose(global_frame_, robot_pose, global_pose_);
+  }
+  catch(tf::LookupException& ex) {
+    ROS_ERROR("No Transform available Error: %s\n", ex.what());
+    return false;
+  }
+  catch(tf::ConnectivityException& ex) {
+    ROS_ERROR("Connectivity Error: %s\n", ex.what());
+    return false;
+  }
+  catch(tf::ExtrapolationException& ex) {
+    ROS_ERROR("Extrapolation Error: %s\n", ex.what());
+    return false;
+  }
+  global_pose_2D_ = getPose2D(global_pose_);
+  return true;
+}
+
+pr2_robot_actions::Pose2D SBPLDoorPlanner::getPose2D(const tf::Stamped<tf::Pose> &pose)
+{
+  pr2_robot_actions::Pose2D tmp_pose;
+  double useless_pitch, useless_roll, yaw;
+  pose.getBasis().getEulerZYX(yaw, useless_pitch, useless_roll);
+  tmp_pose.x = pose.getOrigin().x();
+  tmp_pose.y = pose.getOrigin().y();
+  tmp_pose.th = yaw;
+  return tmp_pose;
+}
+
