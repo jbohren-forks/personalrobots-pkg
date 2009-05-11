bdc7a94d20e5a670b1d6cc434f7f45fa23c338c5
stuglaser <stuglaser@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-05-11 17:05:27 +0000

    The stow plug action moves the gripper up (and away from the hokuyo) after releasing the plug.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@15163 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: c03863d7a5ea5d18908e99e2969c9f8f40c9d512

--- highlevel/plugs/plugs_core/src/action_stow_plug.cpp
+++ highlevel/plugs/plugs_core/src/action_stow_plug.cpp
@@ -43,7 +43,7 @@ StowPlugAction::StowPlugAction() :
   robot_actions::Action<robot_msgs::PlugStow, std_msgs::Empty>("stow_plug"),
   action_name_("stow_plug"),
   node_(ros::Node::instance()),
-  gripper_controller_("r_gripper_position_controller"),  
+  gripper_controller_("r_gripper_position_controller"),
   arm_controller_("r_arm_cartesian_trajectory_controller")
 {
 
@@ -77,7 +77,7 @@ robot_actions::ResultStatus StowPlugAction::execute(const robot_msgs::PlugStow& 
   plug_stow_ = plug_stow;
 
   reset();
-  
+
   moveToStow();
   releasePlug();
   return waitForDeactivation(feedback);
@@ -85,12 +85,12 @@ robot_actions::ResultStatus StowPlugAction::execute(const robot_msgs::PlugStow& 
 
 void StowPlugAction::reset()
 {
-  
+
   last_grasp_value_ = 0.0;
   grasp_count_ = 0;
   gripper_cmd_.data = 0.04;
 
-  req_pose_.pose.header.frame_id = plug_stow_.header.frame_id; 
+  req_pose_.pose.header.frame_id = plug_stow_.header.frame_id;
   req_pose_.pose.pose.position.x = plug_stow_.plug_centroid.x;
   req_pose_.pose.pose.position.y = plug_stow_.plug_centroid.y;
   req_pose_.pose.pose.position.z = plug_stow_.plug_centroid.z + 0.15;
@@ -104,7 +104,7 @@ void StowPlugAction::reset()
 }
 
 void StowPlugAction::moveToStow()
-{ 
+{
   req_pose_.pose.header.stamp = ros::Time();
   if (!ros::service::call(arm_controller_ + "/move_to", req_pose_, res_pose_))
   {
@@ -113,7 +113,7 @@ void StowPlugAction::moveToStow()
     deactivate(robot_actions::ABORTED, empty_);
     return;
   }
-  
+
   if (isPreemptRequested())
   {
     ROS_DEBUG("%s: preempted.", action_name_.c_str());
@@ -136,7 +136,7 @@ void StowPlugAction::moveToStow()
 
 void StowPlugAction::releasePlug()
 {
-  
+
   if (isPreemptRequested())
   {
     ROS_DEBUG("%s: preempted.", action_name_.c_str());
@@ -146,7 +146,7 @@ void StowPlugAction::releasePlug()
   node_->publish(gripper_controller_ + "/set_command", gripper_cmd_);
 
   // DO U REALLY WANT TO SUBSCRIBE EACH TIME? WHAT HAPPENS IF U DO NOT?
-  node_->subscribe(gripper_controller_ + "/state", controller_state_msg_, &StowPlugAction::checkGrasp, this, 1); 
+  node_->subscribe(gripper_controller_ + "/state", controller_state_msg_, &StowPlugAction::checkGrasp, this, 1);
 }
 
 void StowPlugAction::checkGrasp()
@@ -159,9 +159,9 @@ void StowPlugAction::checkGrasp()
     deactivate(robot_actions::PREEMPTED, std_msgs::Empty());
     return;
   }
-  
+
   node_->publish(gripper_controller_ + "/set_command", gripper_cmd_);
-  
+
   // Make sure that the gripper has stopped moving
   if(last_grasp_value_ < controller_state_msg_.process_value)
   {
@@ -186,8 +186,19 @@ void StowPlugAction::checkGrasp()
     }
     else
     {
-      ROS_INFO("%s: succeeded.", action_name_.c_str());
       node_->unsubscribe(gripper_controller_ + "/state");
+
+      req_pose_.pose.pose.position.z = plug_stow_.plug_centroid.z + 0.07;
+      req_pose_.pose.header.stamp = ros::Time();
+      if (!ros::service::call(arm_controller_ + "/move_to", req_pose_, res_pose_))
+      {
+        ROS_ERROR("%s: Failed to move arm.", action_name_.c_str());
+        ROS_DEBUG("%s: aborted.", action_name_.c_str());
+        deactivate(robot_actions::ABORTED, empty_);
+        return;
+      }
+
+      ROS_INFO("%s: succeeded.", action_name_.c_str());
       deactivate(robot_actions::SUCCESS, std_msgs::Empty());
       return;
     }
