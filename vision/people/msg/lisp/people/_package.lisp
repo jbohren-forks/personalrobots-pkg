44ba4a76dd0bec8cbfe34c792e5deaf0b83cd5c5
pantofaru <pantofaru@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-05-12 17:13:16 +0000

    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@15246 f5854215-dd47-0410-b2c4-cdd35faa7885
    Former-commit-id: 952bccae87e99ea126144bbf9ed4b01d65b50575

--- vision/people/msg/lisp/people/_package.lisp
+++
@@ -1,8 +0,0 @@
-(defpackage people
-  (:use cl
-        roslisp)
-  (:export
-   "<POSITIONMEASUREMENT>"
-  ))
-
-(roslisp:load-if-necessary "/u/pantofaru/Install/ros/ros-pkg/vision/people/msg/lisp/people/_package_PositionMeasurement.lisp")
--- vision/people/msg/lisp/people/_package_PositionMeasurement.lisp
+++
@@ -1,9 +0,0 @@
-(in-package people)
-(export '(HEADER-VAL
-          NAME-VAL
-          OBJECT_ID-VAL
-          POS-VAL
-          RELIABILITY-VAL
-          COVARIANCE-VAL
-          INITIALIZATION-VAL
-))
