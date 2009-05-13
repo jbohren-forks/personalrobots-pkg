c9cfe29e9f217670b80673a22aa54aa410f3c2c7
eitanme <eitanme@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-05-13 20:34:13 +0000

    Getting rid of a warning
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@15342 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: ed24ff2f6b6e6b5c8fc2516f5458894fd47382da

--- world_models/costmap_2d/src/voxel_costmap_2d.cpp
+++ world_models/costmap_2d/src/voxel_costmap_2d.cpp
@@ -251,7 +251,7 @@ namespace costmap_2d{
 
       double point_x, point_y, point_z;
       if(worldToMap3DFloat(wpx, wpy, wpz, point_x, point_y, point_z)){
-        unsigned int cell_raytrace_range = cellDistance(raytrace_range_);
+        //unsigned int cell_raytrace_range = cellDistance(raytrace_range_);
         //voxel_grid_.markVoxelLine(sensor_x, sensor_y, sensor_z, point_x, point_y, point_z);
         voxel_grid_.markVoxelLine(point_x, point_y, point_z, sensor_x, sensor_y, sensor_z);
         //voxel_grid_.clearVoxelLineInMap(sensor_x, sensor_y, sensor_z, point_x, point_y, point_z, costmap_, unknown_threshold_, mark_threshold_, cell_raytrace_range);
