efe3025d7a80d206a2bf81312e0a2432cf4d5244
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-05-13 04:00:21 +0000

    adding a second test case for multi targeted notifier
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@15297 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: d613b1466a0bb4d9096a2ba4ed9896f280ea8943

--- common/tf/test/test_notifier.cpp
+++ common/tf/test/test_notifier.cpp
@@ -379,14 +379,21 @@ TEST(MessageNotifier, setTargetFrame)
 TEST(MessageNotifier, setMultipleTargetFrame)
 {
 	Notification n(1);
+	Notification n2(1);
 	Counter<tf::tfMessage> c("tf_message", 1); /// \todo Switch this to tf_message once rosTF goes away completely
 	MessageNotifier<robot_msgs::PointStamped>* notifier = new MessageNotifier<robot_msgs::PointStamped>(g_tf, g_node, boost::bind(&Notification::notify, &n, _1), "test_message", "frame9", 1);
+	MessageNotifier<robot_msgs::PointStamped>* notifier2 = new MessageNotifier<robot_msgs::PointStamped>(g_tf, g_node, boost::bind(&Notification::notify, &n2, _1), "test_message", "frame9", 1);
 	std::auto_ptr<MessageNotifier<robot_msgs::PointStamped> > notifier_ptr(notifier);
+	std::auto_ptr<MessageNotifier<robot_msgs::PointStamped> > notifier_ptr2(notifier2);
         std::vector<std::string> target_frames;
         target_frames.resize(2);
         target_frames[0] = "frame1000";
         target_frames[1] = "frame10";
 	notifier->setTargetFrame(target_frames);
+
+        target_frames[0] = "frame1000";
+        target_frames[1] = "frame10000";
+	notifier2->setTargetFrame(target_frames);
 
 
 	ros::Duration().fromSec(0.2).sleep();
@@ -421,6 +428,21 @@ TEST(MessageNotifier, setMultipleTargetFrame)
 	}
 
 	EXPECT_EQ(1, n.count_);
+	EXPECT_EQ(0, n2.count_); //n2 is waiting for 10000 to be available
+
+	g_broadcaster->sendTransform(btTransform(btQuaternion(0,0,0), btVector3(1,2,3)), stamp, "frame10000", "frame1000");
+	{
+		boost::xtime xt;
+		boost::xtime_get(&xt, boost::TIME_UTC);
+		xt.sec += 10;
+
+		boost::timed_mutex::scoped_timed_lock lock(n2.mutex_, xt);
+
+		EXPECT_EQ(true, lock.owns_lock());
+	}
+
+	EXPECT_EQ(1, n2.count_);
+
 }
 
 
