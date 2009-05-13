5272c49fc01f84e4b9dc4f159cc9e9ae8ff7773c
vijaypradeep <vijaypradeep@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-05-13 01:59:54 +0000

    CvBridge now converts from IPL to Ros Images
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@15288 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 6b7483f1de2d1739906e2128b4334954d544cc68

--- 3rdparty/opencv_latest/include/opencv_latest/CvBridge.h
+++ 3rdparty/opencv_latest/include/opencv_latest/CvBridge.h
@@ -1,13 +1,13 @@
 /*********************************************************************
 * Software License Agreement (BSD License)
-* 
+*
 *  Copyright (c) 2008, Willow Garage, Inc.
 *  All rights reserved.
-* 
+*
 *  Redistribution and use in source and binary forms, with or without
 *  modification, are permitted provided that the following conditions
 *  are met:
-* 
+*
 *   * Redistributions of source code must retain the above copyright
 *     notice, this list of conditions and the following disclaimer.
 *   * Redistributions in binary form must reproduce the above
@@ -17,7 +17,7 @@
 *   * Neither the name of the Willow Garage nor the names of its
 *     contributors may be used to endorse or promote products derived
 *     from this software without specific prior written permission.
-* 
+*
 *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -51,8 +51,8 @@ namespace image_msgs
     void reallocIfNeeded_(IplImage** img, CvSize sz, int depth, int channels)
     {
       if ((*img) != 0)
-        if ((*img)->width     != sz.width  || 
-            (*img)->height    != sz.height || 
+        if ((*img)->width     != sz.width  ||
+            (*img)->height    != sz.height ||
             (*img)->depth     != depth     ||
             (*img)->nChannels != channels)
         {
@@ -63,7 +63,7 @@ namespace image_msgs
       if (*img == 0)
       {
         *img = cvCreateImage(sz, depth, channels);
-      }      
+      }
     }
 
   public:
@@ -79,7 +79,7 @@ namespace image_msgs
         cvReleaseImageHeader(&rosimg_);
 	rosimg_ = 0;
       }
-      
+
       if (cvtimg_) {
         cvReleaseImage(&cvtimg_);
 	cvtimg_ = 0;
@@ -150,6 +150,61 @@ namespace image_msgs
       if (channels == -1)
         channels = img_->nChannels;
       reallocIfNeeded_(img, cvGetSize(img_), depth, channels);
+    }
+
+    /**
+     * Converts an openCV IPL Image into a ROS Image that can be sent 'over the wire'.
+     * Note that this hasn't been rigorously tested
+     * \param source The original Ipl Image that we want to copy from
+     * \param dest The ROS Image message that we want to copy to
+     */
+    static bool fromIpltoRosImage(const IplImage* source, image_msgs::Image& dest)
+    {
+      switch(source->nChannels)
+      {
+        case 1: dest.encoding = "mono"; break;
+        case 3: dest.encoding = "rgb";  break;
+        case 4: dest.encoding = "rgba"; break;
+        default:
+          ROS_ERROR("unknown image format\n");
+          return false;
+      }
+      switch(source->depth)
+      {
+        case IPL_DEPTH_8U : dest.depth = "uint8";   fillImageHelperCV(dest.uint8_data,   source); break;
+        case IPL_DEPTH_8S : dest.depth = "int8";    fillImageHelperCV(dest.int8_data,    source); break;
+        case IPL_DEPTH_16U: dest.depth = "uint16";  fillImageHelperCV(dest.uint16_data,  source); break;
+        case IPL_DEPTH_16S: dest.depth = "int16";   fillImageHelperCV(dest.int16_data,   source); break;
+        case IPL_DEPTH_32S: dest.depth = "int32";   fillImageHelperCV(dest.int32_data,   source); break;
+        case IPL_DEPTH_32F: dest.depth = "float32"; fillImageHelperCV(dest.float32_data, source); break;
+        case IPL_DEPTH_64F: dest.depth = "float64"; fillImageHelperCV(dest.float64_data, source); break;
+        default:
+          ROS_ERROR("unsupported depth %d\n", source->depth);
+          return false;
+      }
+      return true;
+    }
+
+  private:
+    /**
+     * Helper method used by fromIplToRosImage in order to populate the images
+     */
+    template <typename T>
+    static void fillImageHelperCV(T& m, const IplImage* frame)
+    {
+        m.layout.dim.resize(3);
+        m.layout.dim.resize(3);
+        m.layout.dim[0].label  = "height";
+        m.layout.dim[0].size   = frame->height;
+        m.layout.dim[0].stride = frame->widthStep*frame->height/sizeof(m.data[0]);
+        m.layout.dim[1].label  = "width";
+        m.layout.dim[1].size   = frame->width;
+        m.layout.dim[1].stride = frame->widthStep/sizeof(m.data[0]);
+        m.layout.dim[2].label  = "channel";
+        m.layout.dim[2].size   = frame->nChannels;
+        m.layout.dim[2].stride = frame->nChannels*sizeof(m.data[0]);
+        m.data.resize(frame->widthStep*frame->height/sizeof(m.data[0]));
+        memcpy((char*)(&m.data[0]), frame->imageData, m.data.size()*sizeof(m.data[0]));
     }
   };
 }
