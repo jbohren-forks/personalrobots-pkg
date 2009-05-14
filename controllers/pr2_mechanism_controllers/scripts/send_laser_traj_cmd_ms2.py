013e582daeaff47c30d7f043dfa2f998956475e9
vijaypradeep <vijaypradeep@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-05-14 03:53:40 +0000

    Made sawtooth laser profile for milestone
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@15390 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 6754bf835fd280a9dc6e341a8c610c4f36d45135

--- controllers/pr2_mechanism_controllers/scripts/send_laser_traj_cmd_ms2.py
+++ controllers/pr2_mechanism_controllers/scripts/send_laser_traj_cmd_ms2.py
@@ -28,11 +28,15 @@ if __name__ == '__main__':
     cmd = LaserTrajCmd()
     controller   =    sys.argv[1]
     cmd.header   =    roslib.msg.Header(None, None, None)
-    cmd.profile  = "blended_linear"
-    cmd.pos      = [.4, -.3, .4]
-    cmd.time     = [0.0, 5.0, 7.5]
-    cmd.max_rate =  .4   # Ignores param
-    cmd.max_accel=  .5   # ignores param
+    cmd.profile  = "linear"
+    #cmd.pos      = [1.0, .26, -.26, -.7,   -.7,   -.26,   .26,   1.0, 1.0]
+    d = .025
+    #cmd.time     = [0.0, 0.4,  1.0, 1.1, 1.1+d,  1.2+d, 1.8+d, 2.2+d, 2.2+2*d]
+    
+    cmd.pos = [1.0, -.7, 1.0]
+    cmd.time= [0.0, 1.8, 2.025]
+    cmd.max_rate =  5   # Ignores param
+    cmd.max_accel=  5   # ignores param
 
     print 'Sending Command to %s: ' % controller
     print '  Profile Type: %s' % cmd.profile
