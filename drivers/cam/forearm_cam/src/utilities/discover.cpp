7ab443512327de606ea3cca5023897c3af2d256e
blaisegassend <blaisegassend@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-05-23 00:25:05 +0000

    Upgraded discover utility so that it can automatically look on all interfaces.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@16005 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: c0eceeae254a033bf8054f83c6218739c0ef07f2

--- drivers/cam/forearm_cam/src/utilities/discover.cpp
+++ drivers/cam/forearm_cam/src/utilities/discover.cpp
@@ -37,6 +37,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <vector>
+
+#include <net/if.h>
 
 #include "pr2lib.h"
 #include "host_netutil.h"
@@ -55,17 +58,17 @@ int discover(const std::string &if_name)
   int retval = system(rp_str.c_str());
   if (retval == -1 || !WIFEXITED(retval) || WEXITSTATUS(retval))
   {
-    fprintf(stderr, "Unable to set rp_filter to 0 on interface. Camera discovery is likely to fail.\n");
+    fprintf(stderr, "Unable to set rp_filter to 0 on interface %s. Camera discovery is likely to fail.\n", if_name.c_str());
   }
 
   // Discover any connected cameras, wait for 0.5 second for replies
   if( pr2Discover(if_name.c_str(), &camList, NULL, SEC_TO_USEC(0.5)) == -1) {
-    fprintf(stderr, "Discover error.\n");
+    fprintf(stderr, "Discover error on interface %s.\n", if_name.c_str());
     return -1;
   }
 
   if (pr2CamListNumEntries(&camList) == 0) {
-    printf("No cameras found\n");
+    fprintf(stderr, "No cameras found on interface %s\n", if_name.c_str());
     return 0;
   }
 
@@ -76,22 +79,51 @@ int discover(const std::string &if_name)
     uint8_t *ip = (uint8_t *) &camera->ip;
     char pcb_rev = 0x0A + (0x0000000F & camera->hw_version);
     int hdl_rev = 0x00000FFF & (camera->hw_version>>4);
-    printf("Found camera with S/N #%u, MAC: %02x:%02x:%02x:%02x:%02x:%02x, IP: %i.%i.%i.%i, PCB rev %X : HDL rev %3X : FW rev %3X\n", 
-        camera->serial, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], ip[0], ip[1], ip[2], ip[3], 
+    printf("Found camera with S/N #%u, MAC: %02x:%02x:%02x:%02x:%02x:%02x, iface: %s IP: %i.%i.%i.%i, PCB rev %X : HDL rev %3X : FW rev %3X\n", 
+        camera->serial, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], if_name.c_str(), ip[0], ip[1], ip[2], ip[3], 
         pcb_rev, hdl_rev, camera->fw_version);
   }
 
   return 0;
 }
 
+std::vector<std::string> getInterfaces()
+{
+  std::vector<std::string> ifaces;
+
+  struct if_nameindex *if_list = if_nameindex();
+  
+  if(if_list == NULL)
+    return ifaces;
+
+  for(int i = 0; if_list[i].if_index != 0; i++)
+  {
+    ifaces.push_back(if_list[i].if_name);
+  }
+
+  return ifaces;
+}
+
+int discoverall()
+{
+  int ret = 0;
+  
+  std::vector<std::string> interfaces = getInterfaces();
+
+  for (std::vector<std::string>::iterator iface = interfaces.begin(); iface != interfaces.end(); iface++)
+    if (discover(*iface))
+      ret = -1;
+  
+  return ret;
+}
 
 int main(int argc, char **argv)
 {
-  if (argc != 2)
-  {
-    fprintf(stderr, "usage: discover <interface>\n");
-    return 1;
-  }
-
-  return discover(argv[1]);
+  if (argc == 2)
+    return discover(argv[1]);
+  else if (argc == 1)
+    return discoverall();
+    
+  fprintf(stderr, "usage: discover <interface>\n");
+  return 1;
 } 
