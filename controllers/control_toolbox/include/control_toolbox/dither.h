653070cb6008bbe8cc3825103a41a7a9b1a22f1f
wattsk <wattsk@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-05-27 06:00:35 +0000

    Adding dither to control toolbox
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@16150 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: c180699e314401465d4c6a203f1db24cee41192d

---
+++ controllers/control_toolbox/include/control_toolbox/dither.h
@@ -0,0 +1,82 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+#pragma once
+
+#include <cstdlib>
+#include <ctime>
+
+namespace control_toolbox {
+/***************************************************/
+/*! \class Dither
+
+
+
+*/
+/***************************************************/
+
+class Dither
+{
+public:
+
+  /*!
+   * \brief Constructor
+   */
+  Dither(double seed);
+
+  /*!
+   * \brief Destructor.
+   */
+  ~Dither();
+
+  /*!
+   * \brief Get next Gaussian white noise point
+   */
+  double update();
+
+  /*!
+   * \brief Intializes everything and calculates the constants for the sweep.
+   *
+    * \param amplitude The amplitude of the dither, \f$A\f$.
+   */
+  void init(double amplitude);
+
+private:
+  double amplitude_;                        /**< Amplitude of the sweep. */
+  double s_;
+  double x_;
+  long idum; // Random seed
+
+  double uni();
+};
+}
