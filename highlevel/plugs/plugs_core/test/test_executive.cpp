5f6bb2f4f675694ca139d803e4f4e2f09404a876
stuglaser <stuglaser@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-05-28 22:45:48 +0000

    Made a test script for the full plugging in
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/branches/milestone2@16304 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: d0775b426c954dbdcf8b63a0afac9fc9299f7d14

--- highlevel/plugs/plugs_core/test/test_executive.cpp
+++ highlevel/plugs/plugs_core/test/test_executive.cpp
@@ -1,13 +1,13 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
- * 
+ *
  *  Copyright (c) 2008, Willow Garage, Inc.
  *  All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
  *  are met:
- * 
+ *
  *   * Redistributions of source code must retain the above copyright
  *     notice, this list of conditions and the following disclaimer.
  *   * Redistributions in binary form must reproduce the above
@@ -17,7 +17,7 @@
  *   * Neither the name of Willow Garage nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -43,29 +43,33 @@
 #include <ros/node.h>
 #include <robot_actions/action_client.h>
 
+#include <safety_core/action_detect_plug_on_base.h>
+
 // Msgs
 #include <robot_msgs/PlugStow.h>
 #include <std_msgs/Empty.h>
+#include <std_msgs/Int32.h>
 
 // State Msgs
 #include <robot_actions/NoArgumentsActionState.h>
-#include <pr2_robot_actions/MoveAndGraspPlugState.h>
+#include <nav_robot_actions/MoveBaseState.h>
 #include <pr2_robot_actions/DetectOutletState.h>
+#include <pr2_robot_actions/DetectPlugOnBaseState.h>
+#include <pr2_robot_actions/MoveAndGraspPlugState.h>
 #include <pr2_robot_actions/StowPlugState.h>
 #include <pr2_robot_actions/SwitchControllersState.h>
-#include <pr2_robot_actions/DetectPlugOnBaseState.h>
+#include <pr2_robot_actions/PlugInState.h>
 
 // Actions
-#include <safety_core/action_detect_plug_on_base.h>
-#include <safety_core/action_tuck_arms.h>
-#include <plugs_core/action_untuck_arms.h>
-#include <plugs_core/action_move_and_grasp_plug.h>
-#include <plugs_core/action_detect_outlet_fine.h>
 #include <plugs_core/action_detect_outlet_coarse.h>
+#include <plugs_core/action_detect_outlet_fine.h>
 #include <plugs_core/action_localize_plug_in_gripper.h>
+#include <plugs_core/action_move_and_grasp_plug.h>
 #include <plugs_core/action_plug_in.h>
 #include <plugs_core/action_stow_plug.h>
 #include <plugs_core/action_unplug.h>
+#include <plugs_core/action_untuck_arms.h>
+#include <safety_core/action_tuck_arms.h>
 
 using namespace ros;
 using namespace std;
@@ -83,36 +87,62 @@ int
   ros::Node node("test_executive");
   boost::thread_group threads_;
 
-  
+
 
   pr2_robot_actions::SwitchControllers switchlist;
   std_msgs::Empty empty;
-  robot_msgs::PlugStow plug_stow; 
+  robot_msgs::PlugStow plug_stow;
   robot_msgs::PointStamped point;
   robot_msgs::PoseStamped pose;
-  
+
   point.header.frame_id = "torso_lift_link";
   point.point.x=0;
   point.point.y=0;
   point.point.z=0;
 
-  Duration timeout_short = Duration().fromSec(3.0);
-  Duration timeout_medium = Duration().fromSec(40.0);
-  Duration timeout_long = Duration().fromSec(70.0);
+  Duration switch_timeout = Duration(4.0);
 
-  robot_actions::ActionClient<std_msgs::Empty, robot_actions::NoArgumentsActionState, std_msgs::Empty> tuck_arm("safety_tuck_arms");
-  robot_actions::ActionClient<pr2_robot_actions::SwitchControllers, pr2_robot_actions::SwitchControllersState,  std_msgs::Empty> switch_controllers("switch_controllers");
-  robot_actions::ActionClient<std_msgs::Empty, robot_actions::NoArgumentsActionState, std_msgs::Empty> untuck_arm("plugs_untuck_arms");
-  robot_actions::ActionClient<std_msgs::Empty, pr2_robot_actions::DetectPlugOnBaseState, robot_msgs::PlugStow> detect_plug_on_base("detect_plug_on_base");
-  robot_actions::ActionClient<robot_msgs::PlugStow, pr2_robot_actions::MoveAndGraspPlugState, std_msgs::Empty> move_and_grasp_plug("move_and_grasp_plug");
-  robot_actions::ActionClient<robot_msgs::PointStamped, pr2_robot_actions::DetectOutletState, robot_msgs::PoseStamped> detect_outlet_fine("detect_outlet_fine");
-  robot_actions::ActionClient<std_msgs::Empty, robot_actions::NoArgumentsActionState, std_msgs::Empty> localize_plug_in_gripper("localize_plug_in_gripper");
-  robot_actions::ActionClient<std_msgs::Empty, robot_actions::NoArgumentsActionState, std_msgs::Empty> plug_in("plug_in");
-  robot_actions::ActionClient<std_msgs::Empty, robot_actions::NoArgumentsActionState, std_msgs::Empty> unplug("unplug");
-  robot_actions::ActionClient< robot_msgs::PlugStow, pr2_robot_actions::StowPlugState, std_msgs::Empty> stow_plug("stow_plug");
+  robot_actions::ActionClient<std_msgs::Empty, robot_actions::NoArgumentsActionState, std_msgs::Empty>
+    tuck_arm("safety_tuck_arms");
+  robot_actions::ActionClient<pr2_robot_actions::SwitchControllers, pr2_robot_actions::SwitchControllersState,  std_msgs::Empty>
+    switch_controllers("switch_controllers");
+  robot_actions::ActionClient<std_msgs::Empty, robot_actions::NoArgumentsActionState, std_msgs::Empty>
+    untuck_arm("plugs_untuck_arms");
+  robot_actions::ActionClient<std_msgs::Empty, pr2_robot_actions::DetectPlugOnBaseState, robot_msgs::PlugStow>
+    detect_plug_on_base("detect_plug_on_base");
+  robot_actions::ActionClient<robot_msgs::PlugStow, pr2_robot_actions::MoveAndGraspPlugState, std_msgs::Empty>
+    move_and_grasp_plug("move_and_grasp_plug");
+  robot_actions::ActionClient<robot_msgs::PointStamped, pr2_robot_actions::DetectOutletState, robot_msgs::PoseStamped>
+    detect_outlet_fine("detect_outlet_fine");
+  robot_actions::ActionClient<std_msgs::Empty, robot_actions::NoArgumentsActionState, std_msgs::Empty>
+    localize_plug_in_gripper("localize_plug_in_gripper");
+  robot_actions::ActionClient<std_msgs::Int32, pr2_robot_actions::PlugInState, std_msgs::Empty>
+    plug_in("plug_in");
+  robot_actions::ActionClient<std_msgs::Empty, robot_actions::NoArgumentsActionState, std_msgs::Empty>
+    unplug("unplug");
+  robot_actions::ActionClient< robot_msgs::PlugStow, pr2_robot_actions::StowPlugState, std_msgs::Empty>
+    stow_plug("stow_plug");
+  robot_actions::ActionClient<robot_msgs::PointStamped, pr2_robot_actions::DetectOutletState, robot_msgs::PoseStamped>
+    detect_outlet_coarse("detect_outlet_coarse");
+  robot_actions::ActionClient<robot_msgs::PoseStamped, nav_robot_actions::MoveBaseState, robot_msgs::PoseStamped>
+    move_base_local("move_base_local");
 
+  Duration(1.0).sleep();
 
-  timeout_short.sleep();
+  tuck_arm.preempt();
+  switch_controllers.preempt();
+  untuck_arm.preempt();
+  detect_plug_on_base.preempt();
+  move_and_grasp_plug.preempt();
+  detect_outlet_fine.preempt();
+  localize_plug_in_gripper.preempt();
+  plug_in.preempt();
+  unplug.preempt();
+  stow_plug.preempt();
+  detect_outlet_coarse.preempt();
+  move_base_local.preempt();
+
+  Duration(2.0).sleep();
 
   switchlist.start_controllers.clear();  switchlist.stop_controllers.clear();
   switchlist.stop_controllers.push_back("laser_tilt_controller");
@@ -122,23 +152,66 @@ int
   switchlist.stop_controllers.push_back("r_arm_cartesian_pose_controller");
   switchlist.stop_controllers.push_back("r_arm_cartesian_twist_controller");
   switchlist.stop_controllers.push_back("r_arm_hybrid_controller");
-  if (switch_controllers.execute(switchlist, empty, timeout_medium) != robot_actions::SUCCESS) return -1;
+  switchlist.stop_controllers.push_back("r_arm_joint_trajectory_controller");
+  switchlist.stop_controllers.push_back("head_controller");
+  if (switch_controllers.execute(switchlist, empty, switch_timeout) != robot_actions::SUCCESS) return -10;
 
 
   // tuck arm
   switchlist.start_controllers.clear();  switchlist.stop_controllers.clear();
   switchlist.start_controllers.push_back("r_arm_joint_trajectory_controller");
-  if (switch_controllers.execute(switchlist, empty, timeout_short) != robot_actions::SUCCESS) return -1;
-  if (tuck_arm.execute(empty, empty, timeout_medium) != robot_actions::SUCCESS) return -1;
+  if (switch_controllers.execute(switchlist, empty, switch_timeout) != robot_actions::SUCCESS) return -20;
+  if (tuck_arm.execute(empty, empty, Duration(20.0)) != robot_actions::SUCCESS) return -30;
+
+
+  // Executes detect outlet (coarse)
+  switchlist.start_controllers.clear();  switchlist.stop_controllers.clear();
+  switchlist.start_controllers.push_back("head_controller");
+  if (switch_controllers.execute(switchlist, empty, switch_timeout) != robot_actions::SUCCESS) return -37;
+  robot_msgs::PointStamped guess;
+  guess.header.frame_id = "base_link";
+  guess.point.x = 2.0;
+  guess.point.y = 0.0;
+  guess.point.z = 0.4;
+  robot_msgs::PoseStamped coarse_outlet_pose_msg;
+  int tries = 0;
+  while (detect_outlet_coarse.execute(guess, coarse_outlet_pose_msg, Duration(300.0)) != robot_actions::SUCCESS)
+  {
+    ++tries;
+    if (tries > 5)
+      return -40;
+    Duration(1.0).sleep();
+  }
+
+  // Determines the desired base position
+  tf::Pose coarse_outlet_pose;
+  tf::PoseMsgToTF(coarse_outlet_pose_msg.pose, coarse_outlet_pose);
+
+  tf::Pose desi_offset(tf::Quaternion(0,0,0), tf::Vector3(-0.5, 0.25, 0.0));
+  tf::Pose target = coarse_outlet_pose * desi_offset;
+
+  robot_msgs::PoseStamped target_msg;
+  target_msg.header.frame_id = coarse_outlet_pose_msg.header.frame_id;
+  tf::PoseTFToMsg(target, target_msg.pose);
+
+  // Executes move base
+  if (move_base_local.execute(target_msg, target_msg, Duration(500.0)) != robot_actions::SUCCESS) return -50;
+
+  // detect outlet fine
+  robot_msgs::PointStamped outlet_pt;
+  outlet_pt.header.frame_id = coarse_outlet_pose_msg.header.frame_id;
+  outlet_pt.header.stamp = coarse_outlet_pose_msg.header.stamp;
+  outlet_pt.point = coarse_outlet_pose_msg.pose.position;
+  if (detect_outlet_fine.execute(outlet_pt, pose, Duration(60.0)) != robot_actions::SUCCESS) return -70;
 
   // untuck arm
-   if (untuck_arm.execute(empty, empty, timeout_medium) != robot_actions::SUCCESS) return -1;
+  if (untuck_arm.execute(empty, empty, Duration(30.0)) != robot_actions::SUCCESS) return -80;
 
   // detect plug on base
   switchlist.start_controllers.clear();  switchlist.stop_controllers.clear();
   switchlist.start_controllers.push_back("laser_tilt_controller");
-  if (switch_controllers.execute(switchlist, empty, timeout_short) != robot_actions::SUCCESS) return -1;
-  if (detect_plug_on_base.execute(empty, plug_stow, timeout_long) != robot_actions::SUCCESS) return -1;
+  if (switch_controllers.execute(switchlist, empty, switch_timeout) != robot_actions::SUCCESS) return -90;
+  if (detect_plug_on_base.execute(empty, plug_stow, Duration(120.0)) != robot_actions::SUCCESS) return -100;
 
   // move and grasp plug
   switchlist.start_controllers.clear();  switchlist.stop_controllers.clear();
@@ -148,14 +221,11 @@ int
   switchlist.start_controllers.push_back("r_arm_cartesian_pose_controller");
   switchlist.start_controllers.push_back("r_arm_cartesian_twist_controller");
   switchlist.start_controllers.push_back("r_arm_cartesian_wrench_controller");
-  if (switch_controllers.execute(switchlist, empty, timeout_short) != robot_actions::SUCCESS) return -1;
-  if (move_and_grasp_plug.execute(plug_stow, empty, timeout_long) != robot_actions::SUCCESS) return -1;
+  if (switch_controllers.execute(switchlist, empty, switch_timeout) != robot_actions::SUCCESS) return -110;
+  if (move_and_grasp_plug.execute(plug_stow, empty, Duration(120.0)) != robot_actions::SUCCESS) return -120;
 
-  // detect outlet fine
-  if (detect_outlet_fine.execute(point, pose, timeout_long) != robot_actions::SUCCESS) return -1;
-  
   // localize plug in gripper
-  if (localize_plug_in_gripper.execute(empty, empty, timeout_long) != robot_actions::SUCCESS) return -1;
+  if (localize_plug_in_gripper.execute(empty, empty, Duration(60.0)) != robot_actions::SUCCESS) return -130;
 
   // plug in
   switchlist.start_controllers.clear();  switchlist.stop_controllers.clear();
@@ -164,23 +234,25 @@ int
   switchlist.stop_controllers.push_back("r_arm_cartesian_pose_controller");
   switchlist.stop_controllers.push_back("r_arm_cartesian_twist_controller");
   switchlist.start_controllers.push_back("r_arm_hybrid_controller");
-  if (switch_controllers.execute(switchlist, empty, timeout_short) != robot_actions::SUCCESS) return -1;
-  //if (plug_in.execute(empty, empty, timeout_long) != robot_actions::SUCCESS) return -1;
+  if (switch_controllers.execute(switchlist, empty, switch_timeout) != robot_actions::SUCCESS) return -140;
+  std_msgs::Int32 outlet_id; outlet_id.data = 0;
+  pr2_robot_actions::PlugInState plug_in_state;
+  if (plug_in.execute(outlet_id, empty, Duration(10000.0)) != robot_actions::SUCCESS) return -150;
 
-  timeout_short.sleep();
+  Duration(8.0).sleep();
 
-  //unplug
-  // if (unplug.execute(empty, empty, timeout_long) != robot_actions::SUCCESS) return -1;
+  // unplug
+  if (unplug.execute(empty, empty, Duration(60.0)) != robot_actions::SUCCESS) return -160;
 
-  //stow plug
+  // stow plug
    switchlist.start_controllers.clear();  switchlist.stop_controllers.clear();
   switchlist.stop_controllers.push_back("r_arm_hybrid_controller");
   switchlist.start_controllers.push_back("r_arm_cartesian_trajectory_controller");
   switchlist.start_controllers.push_back("r_arm_cartesian_pose_controller");
   switchlist.start_controllers.push_back("r_arm_cartesian_twist_controller");
   switchlist.start_controllers.push_back("r_arm_cartesian_wrench_controller");
-  if (switch_controllers.execute(switchlist, empty, timeout_short) != robot_actions::SUCCESS) return -1;
-  if (stow_plug.execute(plug_stow, empty, timeout_long) != robot_actions::SUCCESS) return -1;
+  if (switch_controllers.execute(switchlist, empty, switch_timeout) != robot_actions::SUCCESS) return -170;
+  if (stow_plug.execute(plug_stow, empty, Duration(60.0)) != robot_actions::SUCCESS) return -180;
 
 
   // stop remaining controllers
@@ -191,7 +263,9 @@ int
   switchlist.stop_controllers.push_back("r_arm_cartesian_wrench_controller");
   switchlist.stop_controllers.push_back("r_arm_cartesian_pose_controller");
   switchlist.stop_controllers.push_back("r_arm_cartesian_twist_controller");
-  if (switch_controllers.execute(switchlist, empty, timeout_medium) != robot_actions::SUCCESS) return -1;
+  if (switch_controllers.execute(switchlist, empty, switch_timeout) != robot_actions::SUCCESS) return -1;
+
+  printf("DONE!\n");
 
   return (0);
 }
