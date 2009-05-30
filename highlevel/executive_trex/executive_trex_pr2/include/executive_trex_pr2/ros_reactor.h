210e4ebbf226f64b0db64cd3af86b4f7958dfe11
mcgann <mcgann@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-05-30 00:51:36 +0000

    A working ros service call for requesting goals
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/branches/milestone2@16434 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 9571dbfa01b5f11a319c52d994ea7dec8a6d8408

--- highlevel/executive_trex/executive_trex_pr2/include/executive_trex_pr2/ros_reactor.h
+++ highlevel/executive_trex/executive_trex_pr2/include/executive_trex_pr2/ros_reactor.h
@@ -23,9 +23,37 @@ namespace executive_trex_pr2 {
 
     bool executeGoals(ExecuteGoals::Request &req, ExecuteGoals::Response &resp);
 
+  protected:
+
+    /**
+     * @brief Used to hook up observer for dispatch of observations and servers for dispatch of goals
+     */
+    virtual void handleInit(TICK initialTick, const std::map<double, ServerId>& serversByTimeline, const ObserverId& observer);
+
+    /**
+     * @brief Must adress new tick and conduct appropriate propagation and dispatch
+     */
+    virtual void handleTickStart();
+
+    /**
+     * @brief Handle synchronization. If it fails it is a system error.
+     */
+    virtual bool synchronize();
+
+    /**
+     * @brief Return true if there are flaws to resolve
+     */
+    virtual bool hasWork();
+
+    /**
+     * @brief Step the reactor to resolve flaws
+     */
+    virtual void resume();
+
   private:
     ros::NodeHandle node_handle_;
     ros::ServiceServer service_server_;
+    boost::recursive_mutex lock_;
   };
 }
 #endif
--- highlevel/executive_trex/executive_trex_pr2/test/ros_reactor_test.cpp
+++ highlevel/executive_trex/executive_trex_pr2/test/ros_reactor_test.cpp
@@ -5,6 +5,10 @@ int main(int argc, char** argv){
   ros::init(argc, argv);
   ros::Node n("trex_test_client");
   executive_trex_pr2::ExecuteGoals::Request req;
+  req.outlet_ids.push_back(1);
+  req.outlet_ids.push_back(4);
+  req.outlet_ids.push_back(26);
+  req.outlet_ids.push_back(27);
   executive_trex_pr2::ExecuteGoals::Response resp;
   ROS_ASSERT(ros::service::call("execute_goals", req, resp));
 }
