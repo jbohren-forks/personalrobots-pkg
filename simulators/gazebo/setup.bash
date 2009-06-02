bab2e8eb068c629e21c5e7807dcb4e646d006274
natepak <natepak@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-02 21:10:16 +0000

    Updated the gazebo Makefile to handle cases where the system has audio video libraries installed
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@16601 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 0ba1a830e8cf4da610965110c5bb3abb4b600fad

--- simulators/gazebo/setup.bash
+++ simulators/gazebo/setup.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 export GAZ_TOP=`rospack find gazebo`/gazebo
+export ODE_TOP=`rospack find opende`/opende
 export OGRE_TOP=`rospack find ogre`/ogre
 export CG_TOP=`rospack find Cg`/Cg
 export BOOST_TOP=`rosboost-cfg --root`
@@ -8,11 +9,12 @@ export PR2MEDIA=`rospack find gazebo_robot_description`
 export GAZMEDIA=`rospack find gazebo`/gazebo/share/gazebo
 
 
-export LD_LIBRARY_PATH=$SIM_PLUGIN/lib:$GAZ_TOP/lib:$CG_TOP/lib:$BOOST_TOP/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$OGRE_TOP/lib:$SIM_PLUGIN/lib:$GAZ_TOP/lib:$CG_TOP/lib:$BOOST_TOP/lib:$ODE_TOP/lib:$LD_LIBRARY_PATH
 export PATH=$GAZ_TOP/bin:$PATH
 
-export GAZEBO_RESOURCE_PATH=$PR2MEDIA:$GAZMEDIA
-export OGRE_RESOURCE_PATH=$OGRE_TOP/lib/OGRE
+#export GAZEBO_RESOURCE_PATH=$PR2MEDIA:$GAZMEDIA
+#export OGRE_RESOURCE_PATH=$OGRE_TOP/lib/OGRE
+#export MC_RESOURCE_PATH=$PR2MEDIA
 
 echo
 echo Current GAZ_TOP is set to $GAZ_TOP
