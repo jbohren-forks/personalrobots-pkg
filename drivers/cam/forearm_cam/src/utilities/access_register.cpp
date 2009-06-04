72722d42495c6ff60e8bcab0ace510fee1f22dbd
blaisegassend <blaisegassend@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-04 03:02:03 +0000

    Added some features to stress-test the flash access functionality.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@16701 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 13d4673bf391c2d396d725a2b581eadbca4d4415

--- drivers/cam/forearm_cam/src/utilities/access_register.cpp
+++ drivers/cam/forearm_cam/src/utilities/access_register.cpp
@@ -5,10 +5,31 @@
 #include <cstring>
 #include <cassert>
 #include <unistd.h>
+  
+int sensorread(IpCamList *camera, uint8_t reg)
+{
+  uint16_t val;
+
+  if ( pr2SensorRead( camera, reg, &val ) != 0) {
+    fprintf(stderr, "Could not get register.");
+    return -1;
+  }
+
+  return val;
+}
+
+int sensorwrite(IpCamList *camera, uint8_t reg, uint16_t val)
+{
+  if ( pr2SensorWrite( camera, reg, val ) != 0) {
+    fprintf(stderr, "Could not set register.");
+    return -1;
+  }
+  return 0;
+}
 
 int main(int argc, char** argv)
 {
-  if (argc != 5 && argc != 6) {
+  if (argc < 4 || argc > 7) {
     fprintf(stderr, "Usage: %s <interface> <IP address> <serial number> <register> [<value>]\nAssumes that the IP is already configured.", argv[0]);
     return 0;
   }
@@ -17,14 +38,15 @@ int main(int argc, char** argv)
   int sn = atoi(argv[3]);
   uint16_t val;
   uint8_t reg;
-  sscanf(argv[4], "%hhx", &reg);
-  if (argc == 6)
+  if (argc >= 5)
+    sscanf(argv[4], "%hhx", &reg);
+  if (argc >= 6)
     sscanf(argv[5], "%hx", &val);
 
   // Create a new IpCamList to hold the camera list
   IpCamList camList;
   pr2CamListInit(&camList);
-
+  
   // Discover any connected cameras, wait for 0.5 second for replies
   if( pr2Discover(if_name, &camList, ip_address, SEC_TO_USEC(0.5)) == -1) {
     fprintf(stderr, "Discover error\n");
@@ -44,29 +66,66 @@ int main(int argc, char** argv)
   }
   IpCamList* camera = pr2CamListGetEntry(&camList, index);
 
-  uint16_t oldval;
-  if ( pr2SensorRead( camera, reg, &oldval ) != 0) {
-    fprintf(stderr, "Could not get register. Is ARP set? Is %s accessible from %s?", ip_address, if_name);
-    return -1;
-  }
-
-  if (argc == 6)
+  if (!strcmp(argv[4], "readtst"))
   {
-    if ( pr2SensorWrite( camera, reg, val ) != 0) {
-      fprintf(stderr, "Could not set register. Is ARP set? Is %s accessible from %s?", ip_address, if_name);
-      return -1;
+    int count = 0;
+    if (argc >= 6)
+      sscanf(argv[5], "%hhx", &reg);
+    while (1)
+    {
+      int oldval = sensorread(camera, reg);
+      fprintf(stderr, "count %i reg %02x read: %04x\n", count++, reg, oldval);
     }
-  
-    uint16_t newval;
-    if ( pr2SensorRead( camera, reg, &newval ) != 0) {
-      fprintf(stderr, "Could not get register. Is ARP set? Is %s accessible from %s?", ip_address, if_name);
-      return -1;
+  }
+  else if (!strcmp(argv[4], "writetst"))
+  {
+    int count = 0;
+    while (1)
+    {
+      if (argc >= 7)
+      {
+        sscanf(argv[5], "%hhx", &reg);
+        sscanf(argv[6], "%hx", &val);
+      }
+      else
+      {
+        reg = 0;
+        val = 0x1313;
+      }
+      sensorwrite(camera, reg, val);
+      fprintf(stderr, "count %i reg %02x val %04x\n", count++, reg, val);
     }
-
-    fprintf(stderr, "Reg %02x was: %04x set: %04x read: %04x\n", reg, oldval, val, newval);
+  }
+  else if (argc >= 5)
+  {
+    uint16_t oldval;
+    oldval = sensorread(camera, reg);
+    if (argc == 6)
+    {
+      sensorwrite(camera, reg, val);
+      usleep(200000);
+      int newval = sensorread(camera, reg);
+      fprintf(stderr, "Reg %02x was: %04x set: %04x read: %04x\n", reg, oldval, val, newval);
+    }
+    else
+      fprintf(stderr, "Reg %02x read: %04x\n", reg, oldval);
   }
   else
-    fprintf(stderr, "Reg %02x read: %04x\n", reg, oldval);
+  {
+    for (int i = 0; i < 256; i++)
+    {
+      if (i % 16 == 0)
+        fprintf(stderr, "%02x:  ", i); 
+      
+      if (i % 16 == 8)
+        fprintf(stderr, "- "); 
+        
+      fprintf(stderr, "%04x ", sensorread(camera, i));
+      
+      if ((i + 1) % 16 == 0)
+        fprintf(stderr, "\n"); 
+    }
+  }
 
   return 0;
 }
