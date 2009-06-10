b207456aaf0e7e6b735e92dc0753809ea5c5ac6c
jeking <jeking@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-10 03:13:57 +0000

    changes made just prior to paper submission
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@16910 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 3dbaa1cfb4005092c0b4dd00ccd1e45f24de9200

--- motion_planning/planning_research/dynamic_planning/scripts/run_planner.sh
+++ motion_planning/planning_research/dynamic_planning/scripts/run_planner.sh
@@ -1,9 +1,9 @@
-EXPECTED_ARGS=1
+EXPECTED_ARGS=2
 E_BADARGS=65
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-    echo 'Usage: run_planner.sh [environment file]'
+    echo 'Usage: run_planner.sh [environment file] [optimize]'
     exit $E_BADARGS
 fi
 
@@ -12,7 +12,7 @@ solutionfile=logs/$(date +%Y-%m-%d-%H-%M-%S)-solution.log
 
 echo Running planning with config file - $1
 
-bin/test_nav3ddyn $1 > $logfile
+bin/test_nav3ddyn $1 $2 > $logfile
 
 grep 'Printing actions to file' $logfile
 grep 'Time to precompute actions' $logfile
@@ -28,14 +28,16 @@ RETURN_VAL=$?
 
 if [ $RETURN_VAL -ne $EXPECTED_RETURN ]
 then
-    grep 'Computing Successors' $logfile > $solutionfile
-    grep 'Adding goal state to list' $logfile >> $solutionfile
-    grep 'Original Coords' $logfile >> $solutionfile
-
+#    grep 'Computing Successors' $logfile > $solutionfile
+#    grep 'Adding goal state to list' $logfile >> $solutionfile
+#    grep 'Original Coords' $logfile >> $solutionfile
+    grep 'Action selected' $logfile > $solutionfile
     echo Solution info output to file - $solutionfile
 
 else
     echo Failed to find a solution
 fi
+
+grep 'Number of cells checked' $logfile > /dev/null
 
 echo Done
