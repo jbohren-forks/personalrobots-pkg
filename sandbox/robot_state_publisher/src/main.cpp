deaa7a7ad52fe5e01acdb6e6a7ce2690bf6805d3
stuglaser <stuglaser@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-17 23:22:22 +0000

    Fixed up includes
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@17256 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 26fe033402ebfaceb02b4e781a2db69f1454bbc3

--- sandbox/robot_state_publisher/src/main.cpp
+++ sandbox/robot_state_publisher/src/main.cpp
@@ -1,13 +1,13 @@
 /*********************************************************************
 * Software License Agreement (BSD License)
-* 
+*
 *  Copyright (c) 2008, Willow Garage, Inc.
 *  All rights reserved.
-* 
+*
 *  Redistribution and use in source and binary forms, with or without
 *  modification, are permitted provided that the following conditions
 *  are met:
-* 
+*
 *   * Redistributions of source code must retain the above copyright
 *     notice, this list of conditions and the following disclaimer.
 *   * Redistributions in binary form must reproduce the above
@@ -17,7 +17,7 @@
 *   * Neither the name of the Willow Garage nor the names of its
 *     contributors may be used to endorse or promote products derived
 *     from this software without specific prior written permission.
-* 
+*
 *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -37,6 +37,7 @@
 #include <kdl/tree.hpp>
 #include <ros/ros.h>
 #include <kdl_parser/tree_parser.hpp>
+#include <robot_msgs/MechanismState.h>
 #include "robot_state_publisher/robot_state_publisher.h"
 
 using namespace std;
@@ -73,7 +74,7 @@ public:
   };
 
 private:
-  void callbackMechState(const MechanismStateConstPtr& state)
+  void callbackMechState(const robot_msgs::MechanismStateConstPtr& state)
   {
     // get joint positions from state message
     map<string, double> joint_positions;
