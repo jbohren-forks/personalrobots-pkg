190baf9934ad11b5df55acf041f35fe547ffde1d
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-17 21:18:27 +0000

    adding permuter class
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@17243 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 0d8beed0adbdc198be80b78064ccdec3c5bc3519

---
+++ common/tf/include/tf/option_permutations.h
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2009, Willow Garage, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+/** \author Tully Foote */
+
+#ifndef TF_OPTIONPERMUTATIONS_H
+#define TF_OPTIONPERMUTATIONS_H
+
+namespace tf
+{
+
+class OptionBase
+{
+public:
+  virtual void reset() =0;
+  virtual bool step() =0;
+};
+
+
+template<class T>
+class Option : public OptionBase
+{
+public:
+  Option(const std::vector<T>& options, T* output)
+  {
+    options_ = options;
+    output_ = output;
+    reset();
+  }
+  
+  void reset(){
+    current_element_ = options_.begin();
+    *output_ = *current_element_;
+  };
+  
+  bool step()
+  {
+    current_element_++;
+    if (current_element_ == options_.end())
+      return false;
+    *output_ = *current_element_;
+    return true;
+  };
+
+private:
+  std::vector<T> options_;
+  T* output_;
+  typedef typename std::vector<T>::iterator vecTit;
+  vecTit current_element_;
+
+};
+
+
+class Permuter
+{
+public:
+  template<class T>
+  void addOption(Option<T> *option)
+  {
+    options_.push_back(static_cast<OptionBase*> (option));
+    reset();
+  };
+
+
+
+  void reset(){
+    for (unsigned int level= 0; level < options_.size(); level++)
+      options_[level]->reset();
+  };
+
+  bool step()
+  {
+    // base case just iterating
+    for (unsigned int level= 0; level < options_.size(); level++)
+    {
+      if(options_[level]->step())
+      {
+        printf("stepping level %d returning true \n", level);
+        return true;
+      }
+      else
+      {
+        printf("reseting level %d\n", level);
+        options_[level]->reset();
+      }
+    }
+    return false;
+  };
+
+
+  void clearAll()
+  {
+    options_.clear();
+  };
+
+private:
+  std::vector<OptionBase*> options_;
+  
+};
+
+
+}
+
+#endif //TF_OPTIONPERMUTATIONS_H
---
+++ common/tf/test/test_permuter.cpp
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <gtest/gtest.h>
+
+#include "tf/option_permutations.h"
+
+using namespace tf;
+
+
+double epsilon = 1e-9;
+
+TEST(tf, Option)
+{
+  std::vector<double> vals;
+  vals.push_back(1.0);
+  vals.push_back(2.0);
+  vals.push_back(3.0);
+  vals.push_back(4.0);
+  double value = 0;
+  
+  Option<double> op(vals, &value);
+
+  for ( unsigned int i = 0; i < vals.size(); i++)
+  {
+    EXPECT_NEAR(vals[i], value, epsilon);
+    EXPECT_TRUE(op.step());
+  };
+    EXPECT_FALSE(op.step());
+  
+  
+}
+
+
+TEST(Permuter, OneOption)
+{
+  double epsilon = 1e-9;
+  tf::Permuter permuter;
+  std::vector<double> vals;
+  vals.push_back(1.0);
+  vals.push_back(2.0);
+  vals.push_back(3.0);
+  vals.push_back(4.0);
+
+
+  double value = 0;
+  Option<double> op(vals, &value);
+
+  permuter.addOption(&op);
+  for ( unsigned int i = 0; i < vals.size(); i++)
+  {
+    EXPECT_NEAR(vals[i], value, epsilon);
+    if (i < vals.size() -1)
+      EXPECT_TRUE(permuter.step());
+    else
+      EXPECT_FALSE(permuter.step());
+  };
+
+}
+
+TEST(Permuter, TwoOptions)
+{
+  double epsilon = 1e-9;
+  tf::Permuter permuter;
+  std::vector<double> vals;
+  vals.push_back(1.0);
+  vals.push_back(2.0);
+  vals.push_back(3.0);
+  vals.push_back(4.0);
+
+
+  double value = 0;
+  Option<double> op(vals, &value);
+  
+  std::vector<double> vals2;
+  vals2.push_back(9.0);
+  vals2.push_back(8.0);
+  vals2.push_back(7.0);
+  vals2.push_back(6.0);
+
+  double value2;
+  Option<double> op2(vals2, &value2);
+
+  permuter.addOption(&op);
+  permuter.addOption(&op2);
+  for ( unsigned int j = 0; j < vals2.size(); j++)
+    for ( unsigned int i = 0; i < vals.size(); i++)
+    {
+      printf("%f?=%f %f?=%f\n", value, vals[i], value2, vals2[j]); 
+      EXPECT_NEAR(vals[i], value, epsilon);
+      EXPECT_NEAR(vals2[j], value2, epsilon);
+      if (i == vals.size() -1 && j == vals2.size() -1)
+        EXPECT_FALSE(permuter.step());
+      else
+        EXPECT_TRUE(permuter.step());
+    };
+
+}
+
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
