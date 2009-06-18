a0bac3bafa82ce6a4840d1a7148a85cca8b08943
alexteichman <alexteichman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-18 23:29:06 +0000

    Adding a patch descriptor.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@17320 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 285bf2f41a584bd52b710ec58e9e0b2016b1c363

--- vision/stanleyi/stanleyi.cpp
+++ vision/stanleyi/stanleyi.cpp
@@ -10,8 +10,9 @@
 #include <errno.h>
 #include <vector>
 #include <string>
+#include "descriptors.h"
 
-
+using namespace NEWMAT;
 using namespace std;
 
 
@@ -86,28 +87,117 @@ public:
   void viewLabels(string bagfile);
 };
 
+void test_watershed(IplImage *img) {
+  int seed_step = 10;
+
+  cout << img->depth << " " << img->nChannels << endl;
+    
+
+  // -- Create a grid of seed points.
+  IplImage* markers = cvCreateImage( cvGetSize(img), IPL_DEPTH_32S, 1 );
+  cvZero(markers);
+  int label = 0;
+  for(int r=0; r<markers->height; r++) {
+    long* ptr = (long*)(markers->imageData + r * markers->widthStep);
+    for(int c=0; c<markers->width; c++) {
+      if(c%seed_step==0 && r%seed_step==0) {
+	*ptr = label;
+	label++;
+      }
+      ptr++;
+
+    }
+  }
+
+
+  double t = (double)cvGetTickCount();
+  cvWatershed( img, markers );
+  t = (double)cvGetTickCount() - t;
+  printf( "exec time = %gms\n", t/(cvGetTickFrequency()*1000.) );
+  
+
+  // -- Display the results.
+  CvMat* color_tab;
+  color_tab = cvCreateMat( 1, label, CV_8UC3 );
+  CvRNG rng = cvRNG(-1);
+  for(int i = 0; i < label; i++ )
+    {
+      uchar* ptr = color_tab->data.ptr + i*3;
+      ptr[0] = (uchar)(cvRandInt(&rng)%180 + 50);
+      ptr[1] = (uchar)(cvRandInt(&rng)%180 + 50);
+      ptr[2] = (uchar)(cvRandInt(&rng)%180 + 50);
+    }
+  
+  // paint the watershed image
+  IplImage* wshed = cvCloneImage( img );
+  IplImage* img_gray = cvCloneImage( img );
+  cvZero( wshed );
+  IplImage* marker_mask = cvCreateImage( cvGetSize(img), 8, 1 );
+  cvCvtColor( img, marker_mask, CV_BGR2GRAY );
+  cvCvtColor( marker_mask, img_gray, CV_GRAY2BGR );
+
+  for(int i = 0; i < markers->height; i++ )
+    for(int j = 0; j < markers->width; j++ )
+      {
+	int idx = CV_IMAGE_ELEM( markers, int, i, j );
+	uchar* dst = &CV_IMAGE_ELEM( wshed, uchar, i, j*3 );
+	if( idx == -1 )
+	  dst[0] = dst[1] = dst[2] = (uchar)255;
+	else if( idx <= 0 || idx > label )
+	  dst[0] = dst[1] = dst[2] = (uchar)0; // should not get here
+	else
+	  {
+	    uchar* ptr = color_tab->data.ptr + (idx-1)*3;
+	    dst[0] = ptr[0]; dst[1] = ptr[1]; dst[2] = ptr[2];
+	  }
+      }
+
+  cvAddWeighted( wshed, 0.5, img_gray, 0.5, 0, wshed );
+  cvNamedWindow("Wshed", CV_WINDOW_AUTOSIZE);
+  cvShowImage( "Wshed", wshed );
+
+
+
+  cvShowImage("Feature", markers);
+  cvWaitKey(0);
+}  
+  
+void test_patch(IplImage* img) {
+  Patch p(100, "intensity", .2);
+  Matrix* r = NULL;
+  p.compute(img, 300, 100, r,  true);
+  if(r!=NULL) {
+    cout << *r << endl;
+  }
+}
+
 void Stanleyi::viewLabels(string bagfile) 
 {
 
   cvNamedWindow("Image", CV_WINDOW_AUTOSIZE);
   cvNamedWindow("Mask", CV_WINDOW_AUTOSIZE);
+  cvNamedWindow("Feature", CV_WINDOW_AUTOSIZE);
+
   lp_.open(bagfile, ros::Time());
-  while(lp_.nextMsg()) 
-    {
-      if (img_bridge_.fromImage(img_msg_, "bgr"))
-	{
-	  img_ = img_bridge_.toIpl();
-	  mask_ = findLabelMask(img_msg_.header.stamp.toSec());
-	  if(mask_ != NULL) {
-	    cvShowImage("Image", img_);
-	    cvShowImage("Mask", mask_);
-	    cvWaitKey(0);
-	  }
-	}
-      
-      if(mask_ != NULL)
-	cvReleaseImage(&mask_);
+  while(lp_.nextMsg()) {
+    if (img_bridge_.fromImage(img_msg_, "bgr")) {
+      img_ = img_bridge_.toIpl();
+ 
+      //seg
+      //      if(mask_ != NULL)
+      //	cvReleaseImage(&mask_);
+      mask_ = findLabelMask(img_msg_.header.stamp.toSec());
+
+      if(mask_ != NULL) {
+	cvShowImage("Image", img_);
+	cvShowImage("Mask", mask_);
+	test_watershed(img_);
+	test_patch(img_);
+	
+	cvWaitKey(0);
+      }
     }
+  }
 }
   
 int main(int argc, char** argv) 
