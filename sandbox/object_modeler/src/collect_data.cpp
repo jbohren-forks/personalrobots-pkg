f10d7d0af7d7204e72584b40b02bbdc8623e7c28
thibaux <thibaux@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-18 16:19:12 +0000

    Now it turns around a point and keeps looking at it
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@17284 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 67e832fe6733011a487419cc882ada39f4166c89

--- sandbox/object_modeler/src/collect_data.cpp
+++ sandbox/object_modeler/src/collect_data.cpp
@@ -38,39 +38,94 @@
 #include <robot_msgs/PoseStamped.h>
 #include <robot_actions/action_client.h>
 #include <nav_robot_actions/MoveBaseState.h>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <LinearMath/btQuaternion.h>
+
+using namespace std;
+
+typedef robot_msgs::PoseStamped PS;
+
+void print_usage() {
+  ROS_INFO("collect_data filename\n"
+	   "filename contains poses, ie (point, orientation) where point is\n"
+	   "a triplet x,y,z and orientation is a quaternion x,y,z,w.\n"
+	   "The first pose is the target (the object to look at), the\n"
+	   "subsequent poses are positions of the robot. The robot will\n"
+	   "keep looking at the target");
+}
+
+
+void read_poses_from_file(vector<robot_msgs::Point> &result, const string &filename) {
+  std::ifstream goal_file(filename.c_str());
+  if (goal_file.is_open()) {
+    string line;
+    while (!goal_file.eof()) {
+      getline(goal_file, line);
+      istringstream line_stream(line);
+      robot_msgs::Point p;
+      robot_msgs::Quaternion q;
+      line_stream >> p.x >> p.y >> p.z >> q.x >> q.y >> q.z >> q.w;
+      result.push_back(p);
+    }
+    goal_file.close();
+  }
+}
+
+robot_msgs::Quaternion direction(robot_msgs::Point position, robot_msgs::Point target) {
+  double vec_x = target.x - position.x;
+  double vec_y = target.y - position.y;
+  double yaw = atan2(vec_y, vec_x);
+  robot_msgs::Quaternion result;
+  btQuaternion dir(yaw, 0, 0);
+  result.x = dir.getX();
+  result.y = dir.getY();  // WARNING! MAY NOT USE THE SAME CONVENTION
+  result.z = dir.getZ();
+  result.w = dir.getW();
+  return result;
+}
+
+
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "object_modeler");
   //ros::NodeHandle n;
   
-  robot_actions::ActionClient<robot_msgs::PoseStamped, nav_robot_actions::MoveBaseState, robot_msgs::PoseStamped> move_client("move_base");
+  robot_actions::ActionClient<PS, nav_robot_actions::MoveBaseState, PS> move_client("move_base");
 
-  robot_msgs::Point goal_pos;
-  goal_pos.x = 0;
-  goal_pos.y = 0;
-  goal_pos.z = 0;
-  robot_msgs::Quaternion goal_orient;
-  goal_orient.x = 0;
-  goal_orient.y = 0;
-  goal_orient.z = 0;
-  goal_orient.w = 0;
-  robot_msgs::Pose goal_pose;
-  goal_pose.position = goal_pos;
-  goal_pose.orientation = goal_orient;
-  robot_msgs::PoseStamped goal_pose_stamped;
-  goal_pose_stamped.pose = goal_pose;
-  robot_msgs::PoseStamped feedback;
-  robot_actions::ResultStatus result = move_client.execute(goal_pose_stamped, feedback, ros::Duration(60));
-  switch (result) {
-  case robot_actions::SUCCESS:
-    ROS_INFO("Move successful");
-    break;
-  case robot_actions::ABORTED:
-    ROS_INFO("Move aborted");
-    break;
-  case robot_actions::PREEMPTED:
-    ROS_INFO("Move preempted");
-    break;
+  vector<robot_msgs::Point> goal_points;
+  read_poses_from_file(goal_points, "poses.txt");
+  if (goal_points.size() < 2) {
+    print_usage();
+    exit(0);
+  }
+
+  robot_msgs::Point target = goal_points[0];
+
+  typedef vector<robot_msgs::Point>::const_iterator I;
+  for (I i = goal_points.begin() + 1; i != goal_points.end(); i++) {
+    robot_msgs::Pose goal_pose;
+    goal_pose.position = *i;
+    goal_pose.orientation = direction(*i, target);
+    PS goal_pose_stamped;
+    goal_pose_stamped.pose = goal_pose;
+    PS feedback;
+    robot_actions::ResultStatus result = move_client.execute(goal_pose_stamped, feedback, ros::Duration(30));
+    switch (result) {
+    case robot_actions::SUCCESS:
+      ROS_INFO("Move successful");
+      break;
+    case robot_actions::ABORTED:
+      ROS_INFO("Move aborted");
+      break;
+    case robot_actions::PREEMPTED:
+      ROS_INFO("Move preempted");
+      break;
+    }    
   }
 }
