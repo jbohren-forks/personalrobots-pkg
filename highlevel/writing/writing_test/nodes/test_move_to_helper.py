5efdc4f3b3d99582dc0e2f75d4afb484e94e11cc
jamesbowman <jamesbowman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-29 20:52:18 +0000

    Python Test Executive test_move_to_helper
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@17883 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: ee9b9115a0b6e7d14078ce08decbd5e6221cb4e0

---
+++ highlevel/writing/writing_test/nodes/test_move_to_helper.py
@@ -0,0 +1,147 @@
+#! /usr/bin/python
+
+#***********************************************************
+#* Software License Agreement (BSD License)
+#*
+#*  Copyright (c) 2009, Willow Garage, Inc.
+#*  All rights reserved.
+#*
+#*  Redistribution and use in source and binary forms, with or without
+#*  modification, are permitted provided that the following conditions
+#*  are met:
+#*
+#*   * Redistributions of source code must retain the above copyright
+#*     notice, this list of conditions and the following disclaimer.
+#*   * Redistributions in binary form must reproduce the above
+#*     copyright notice, this list of conditions and the following
+#*     disclaimer in the documentation and/or other materials provided
+#*     with the distribution.
+#*   * Neither the name of the Willow Garage nor the names of its
+#*     contributors may be used to endorse or promote products derived
+#*     from this software without specific prior written permission.
+#*
+#*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#*  POSSIBILITY OF SUCH DAMAGE.
+#***********************************************************
+
+import roslib
+roslib.load_manifest('writing_test')
+import rospy
+
+import Queue
+import time
+
+from robot_actions.msg import ActionStatus
+import robot_msgs.msg
+from std_msgs.msg import Empty
+
+import python_actions
+
+if __name__ == '__main__':
+
+  try:
+
+    import pr2_robot_actions.msg
+    import robot_actions.msg
+    import nav_robot_actions.msg
+
+    rospy.init_node("test_move_to_helper")
+    switch_controllers = python_actions.ActionClient("switch_controllers",
+                                                     pr2_robot_actions.msg.SwitchControllers,
+                                                     pr2_robot_actions.msg.SwitchControllersState, 
+                                                     Empty)
+    tuck_arm = python_actions.ActionClient("safety_tuck_arms",
+                                           Empty,
+                                           robot_actions.msg.NoArgumentsActionState,
+                                           Empty)
+    find_helper = python_actions.ActionClient("find_helper",
+                                              Empty,
+                                              pr2_robot_actions.msg.FindHelperState,
+                                              robot_msgs.msg.PoseStamped)
+
+    move_base_local = python_actions.ActionClient("move_base_local",
+                                                  robot_msgs.msg.PoseStamped,
+                                                  nav_robot_actions.msg.MoveBaseState,
+                                                  robot_msgs.msg.PoseStamped)
+
+    start_tilt_laser = python_actions.ActionClient("set_laser_tilt",
+                                        Empty,
+                                        robot_actions.msg.NoArgumentsActionState,
+                                        Empty)
+    track_helper = python_actions.ActionClient("track_helper",
+                         robot_msgs.msg.PoseStamped,
+                         pr2_robot_actions.msg.TrackHelperState,
+                         Empty)
+    w = python_actions.ActionClient("generate_text_trajectory",
+                                    pr2_robot_actions.msg.TextGoal,
+                                    pr2_robot_actions.msg.GenerateTextTrajectoryState,
+                                    robot_msgs.msg.Path)
+    time.sleep(2)
+
+    # Preempt all of the actions
+    switch_controllers.preempt()
+    tuck_arm.preempt()
+    find_helper.preempt()
+    move_base_local.preempt()
+
+    time.sleep(2)
+
+    class Abort(Exception):
+      pass
+    class Preempt(Exception):
+      pass
+
+    switchlist = pr2_robot_actions.msg.SwitchControllers()
+    switchlist.stop_controllers = [ "r_arm_joint_trajectory_controller", "head_controller", "laser_tilt_controller"]
+    switchlist.start_controllers = []
+    as,_ = switch_controllers.execute(switchlist, 4.0)
+    if as != python_actions.SUCCESS:
+      sys.exit(-1);
+
+    if 0:
+      state, path = w.execute(pr2_robot_actions.msg.TextGoal("klak", robot_msgs.msg.Point(10,20,0), 30))
+      print "EXECUTE returned", state #, path
+      #for p in path.poses:
+      #  print p.pose.position
+
+    switchlist.stop_controllers = []
+    switchlist.start_controllers = [ "r_arm_joint_trajectory_controller", "head_controller", "laser_tilt_controller"]
+    as,_ = switch_controllers.execute(switchlist, 4.0)
+    if as != python_actions.SUCCESS:
+      sys.exit(-1);
+
+    as,_ = tuck_arm.execute(Empty(), 20.0)
+    if as != python_actions.SUCCESS:
+      sys.exit(-1);
+
+    as,helper_p = find_helper.execute(Empty(), 200.0)
+    if as != python_actions.SUCCESS:
+      sys.exit(-1);
+
+    as,_ = start_tilt_laser.execute(Empty(), 20.0)
+    if as != python_actions.SUCCESS:
+      sys.exit(-1);
+
+    as,_ = move_base_local(helper_p, 500.0)
+    if as != python_actions.SUCCESS:
+      sys.exit(-1);
+
+    as,_ = track_helper(helper_p, 500.0)
+    if as != python_actions.SUCCESS:
+      sys.exit(-1);
+
+  except KeyboardInterrupt, e:
+
+    pass
+
+  print "exiting"
