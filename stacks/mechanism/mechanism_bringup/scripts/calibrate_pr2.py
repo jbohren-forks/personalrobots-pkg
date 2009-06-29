25c2a9d23c89a7e36cfc8c6f3f485fe43477c0ad
wattsk <wattsk@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-29 22:17:36 +0000

    Changed name of calibrate_arms to calibrate_pr2 and changed sequence. Renamed calibration controllers in pr2_default_controllers for each robot
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@17904 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 8f3e32d57acffb2ca90902e4a072a81645cc7fed

--- stacks/mechanism/mechanism_bringup/scripts/calibrate_pr2.py
+++ stacks/mechanism/mechanism_bringup/scripts/calibrate_pr2.py
@@ -34,9 +34,7 @@
 
 # Author: Kevin Watts
 
-# Calibrate the arm sequentially
-# Wrist -> Elbow roll -> Elbow flex -> Upperarm Roll -> 
-# Shoulder Lift -> Shoulder Pan
+# Calibrates the PR-2 in a safe sequence
 
 import roslib
 import copy
@@ -48,7 +46,6 @@ from time import sleep
 roslib.load_manifest('mechanism_bringup')
 import rospy
 from std_msgs.msg import *
-import std_srvs.srv
 from robot_srvs.srv import *
 
 from robot_mechanism_controllers.srv import *
@@ -78,9 +75,6 @@ def slurp(filename):
     return stuff
 
 def calibrate(config):
-    #spawn_controller = rospy.ServiceProxy('spawn_controller', SpawnController)
-    #kill_controller = rospy.ServiceProxy('kill_controller', KillController)
-
     # Spawns the controllers
     resp = spawn_controller(SpawnControllerRequest(config,1))
 
@@ -101,7 +95,7 @@ def calibrate(config):
             if name in waiting_for:
                 waiting_for.remove(name)
         for name in waiting_for:
-            rospy.Subscriber("/%s/calibrated" % name, Empty, calibrated, name)
+            rospy.Subscriber("%s/calibrated" % name, Empty, calibrated, name)
 
         # Waits until all the controllers have calibrated
         while waiting_for and not rospy.is_shutdown():
@@ -151,37 +145,32 @@ def hold_joint(name, p, i, d, iClamp, holding):
     # If launched, add to list of holding controllers and return true 
     for i in range(1,4):
         try:
-            resp = spawn_controller(xml_for_hold(name, p, i, d, iClamp))
+            resp = spawn_controller(xml_for_hold(name, p, i, d, iClamp), 1)
             if resp.ok[0] != 0:
                 holding.append(resp.name[0])
                 return True
         except:
-            print "Failed to spawn holding controller %s on try %d" % (name, i)
+            rospy.logerr("Failed to spawn holding controller %s on try %d" % (name, i))
 
     return False
 
 def set_controller(controller, command):
-    #    try:
-    #rospy.init_node('control', anonymous = True)
-    #finally:
-    pub = rospy.Publisher('/' + controller + '/set_command', Float64,
+    pub = rospy.Publisher(controller + '/set_command', Float64,
                               SendMessageOnSubscribe(Float64(command)))
 
 def calibrate_imu():
-    print "Calibrating IMU"
+    rospy.logout("Calibrating IMU")
     try:
-        rospy.wait_for_service('/imu/calibrate', 5)
-        calibrate = rospy.ServiceProxy('/imu/calibrate', std_srvs.srv.Empty)
+        rospy.wait_for_service('imu/calibrate', 5)
+        calibrate = rospy.ServiceProxy('imu/calibrate', std_srvs.srv.Empty)
         calibrate(timeout=20) # This should take 10 seconds
         return True
     except:
-        print "IMU calibration failed: %s"%sys.exc_info()[0]
+        rospy.logout("IMU calibration failed: %s"%sys.exc_info()[0])
         return False
 
 if __name__ == '__main__':
     rospy.wait_for_service('spawn_controller')
-    #   if  rospy.is_shutdown(): 
-    #    return
 
     rospy.init_node('calibration', anonymous=True)
 
@@ -191,14 +180,17 @@ if __name__ == '__main__':
     # Hold joints after they're calibrated.
     # Can set desired joint position by using name + /set_command service
     # for joint position controllers
-    
-    # Wrist and forearm calibrated with forearm_calibrator in arm_defs.xml
-    
+
     imustatus = calibrate_imu()
     if not imustatus:
-        print "IMU Calibration failed."
-        
-    print "Calibrating shoulder pan" 
+        rospy.logerr("IMU Calibration failed.")
+    
+    # Check sign on torso lift controller
+    calibrate(xml_for_cal("torso_lift", -10, 2000000, 0, 10000, 12000))
+    hold_joint("torso_lift", 1000000, 0.0, 10000, 1000000, holding)
+    set_controller("torso_lift_controller", float(0.08))
+    sleep(0.5)
+
     calibrate(xml_for_cal("r_shoulder_pan", 1.0, 7, 0.5, 0, 1.0))
     hold_joint("r_shoulder_pan", 60, 10, 5, 4, holding)
     set_controller("r_shoulder_pan_controller", float(-0.7))
@@ -207,46 +199,43 @@ if __name__ == '__main__':
     hold_joint("l_shoulder_pan", 60, 10, 5, 4, holding)
     set_controller("l_shoulder_pan_controller", float(0.7))
 
-    print "Calibrating elbow flex"
     calibrate(xml_for_cal("r_elbow_flex", -1.0, 6, 0.2, 0, 1) + "\n" + xml_for_cal("l_elbow_flex", -1.0, 6, 0.2, 0, 1))
     hold_joint("r_elbow_flex", 100, 20, 10, 2, holding)
     hold_joint("l_elbow_flex", 100, 20, 10, 2, holding)
 
-    set_controller("r_elbow_flex_controller", float(3.0))
-    set_controller("l_elbow_flex_controller", float(3.0))
+    set_controller("r_elbow_flex_controller", float(-2.0))
+    set_controller("l_elbow_flex_controller", float(-2.0))
     
-    print "Calibrating right upperarm roll"
     upperarm_roll_name = "r_upper_arm_roll"
     calibrate(xml_for_cal(upperarm_roll_name, 1.0, 6, 0.2, 0, 2))
     hold_joint(upperarm_roll_name, 25, 2, 1.0, 1.0, holding)
     set_controller("r_upper_arm_roll_controller", float(0.0))
 
-    print "Calibrating left upperarm roll" 
     upperarm_roll_name = "l_upper_arm_roll"
     calibrate(xml_for_cal(upperarm_roll_name, 1.0, 6, 0.2, 0, 2))
     hold_joint(upperarm_roll_name, 25, 2, 1.0, 1.0, holding)
     set_controller("l_upper_arm_roll_controller", float(0.0))
 
-    print "Calibrating shoulder lift"
     shoulder_lift_name = "r_shoulder_lift"
     calibrate(xml_for_cal("r_shoulder_lift", -1.0, 9, 1.0, 0, 6) + "\n" + xml_for_cal("l_shoulder_lift", -1.0, 9, 1.0, 0, 6))
 
     hold_joint("r_shoulder_lift", 60, 10, 5, 4, holding)
     hold_joint("l_shoulder_lift", 60, 10, 5, 4, holding)
     
-    set_controller("r_shoulder_lift_controller", float(3.0))
-    set_controller("l_shoulder_lift_controller", float(3.0))
+    set_controller("r_shoulder_lift_controller", float(1.0))
+    set_controller("l_shoulder_lift_controller", float(1.0))
 
-
-
-    sleep(0.5)
-    print "Calibrating rest of robot"
-
+    rospy.logerr('Setting torso lift controller')
+    torso_pub = rospy.Publisher('torso_lift_controller/set_command', Float64)
+    torso_pub.publish(Float64(float(0.0)))
+    sleep(1.5)
+    
     # Calibrate other controllers given
     xml = ''
     
     if len(sys.argv) > 1:
         xacro_cmd = roslib.packages.get_pkg_dir('xacro', True) + '/xacro.py'
+
         xmls = [os.popen2(xacro_cmd + " %s" % f)[1].read() for f in rospy.myargv()[1:]]
 
         # Poor man's xml splicer
@@ -257,7 +246,7 @@ if __name__ == '__main__':
     else:
         print "Reading from stdin..."
         xml = sys.stdin.read()
-
+        
     try:
         calibrate(xml)
     finally:
@@ -268,10 +257,10 @@ if __name__ == '__main__':
                     kill_controller(name)
                     break # Go to next controller if no exception
                 except:
-                    print "Failed to kill controller %s on try %d" % (name, i)
+                    rospy.logerr("Failed to kill controller %s on try %d" % (name, i))
     
     if not imustatus:
         print "Mechanism calibration complete, but IMU calibration failed."
         sys.exit(2)
     
-    print "Calibration complete"
+    rospy.logout("Calibration complete")
