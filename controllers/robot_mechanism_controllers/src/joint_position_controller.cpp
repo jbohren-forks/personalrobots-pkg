93b4267fb45cbc69fc3804eb58952715245a936d
mmwise <mmwise@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-29 23:27:09 +0000

    the state published was incorrect, it was publishing velocity instead of position
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@17916 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 892828b06185758532cc9ac562d0cd393e173023

--- controllers/robot_mechanism_controllers/src/joint_position_controller.cpp
+++ controllers/robot_mechanism_controllers/src/joint_position_controller.cpp
@@ -189,7 +189,7 @@ void JointPositionController::update()
     if(controller_state_publisher_ && controller_state_publisher_->trylock())
     {
       controller_state_publisher_->msg_.set_point = command_;
-      controller_state_publisher_->msg_.process_value = joint_state_->velocity_;
+      controller_state_publisher_->msg_.process_value = joint_state_->position_;
       controller_state_publisher_->msg_.error = error;
       controller_state_publisher_->msg_.time_step = dt_;
 
