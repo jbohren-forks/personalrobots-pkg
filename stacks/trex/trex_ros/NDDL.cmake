fba1be877435cd4a57c7effdfe8e5a54450453d4
tpratkanis <tpratkanis@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-30 18:14:52 +0000

    Remove NDDL builds again.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@17983 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: ea89c2be9c0ca025bcaaaa12526becc739c77228

---
+++ stacks/trex/trex_ros/NDDL.cmake
@@ -0,0 +1,18 @@
+cmake_minimum_required(VERSION 2.4.6)
+
+# To build the executive's config. This is currently not used.
+macro(create_nddl_config)
+    if(NOT ROSPACK_NDDL_PATH)
+        _rospack_invoke(${PROJECT_NAME} ${_prefix} ROSPACK_NDDL_PATH export --lang=nddl --attrib=iflags)
+        set(ROSPACK_NDDL_PATH ${${_prefix}_ROSPACK_NDDL_PATH})
+    endif(NOT ROSPACK_NDDL_PATH)
+    string(REPLACE "-I" ";" _nddl_file_path ${ROSPACK_NDDL_PATH})
+    file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/temp_nddl_gen.cfg 
+    	       "<configuration>\n"
+	       " <binding nddl=\"Object\" cpp=\"Object\"/>\n"
+	       " <binding nddl=\"Timeline\" cpp=\"Timeline\"/>\n"
+  	       " <include path=\"${_nddl_file_path}\"/>\n"
+	       "</configuration>\n")
+endmacro(create_nddl_config)
+
+
