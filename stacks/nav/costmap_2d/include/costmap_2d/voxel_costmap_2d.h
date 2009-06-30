463ea1d457e11e69f9dc0ec2e92c3a6a4ddf3a8b
eitanme <eitanme@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-30 17:57:44 +0000

    Updating documentation and fixing a typo in parameter list for the voxel costmap
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@17980 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 33e685bae2dfd67bee2cfd1ca763891a387d3138

--- stacks/nav/costmap_2d/include/costmap_2d/voxel_costmap_2d.h
+++ stacks/nav/costmap_2d/include/costmap_2d/voxel_costmap_2d.h
@@ -77,7 +77,7 @@ namespace costmap_2d {
        * @param  mark_threshold The maximum number of marked voxel cells that can exist in a column considered as free space
        */
       VoxelCostmap2D(unsigned int cells_size_x, unsigned int cells_size_y, unsigned int cells_size_z,
-          double xy_resolution, double z_resolution, double origin_x, double origin_y, double origin_z_ = 0.0, double inscribed_radius = 0.0,
+          double xy_resolution, double z_resolution, double origin_x, double origin_y, double origin_z = 0.0, double inscribed_radius = 0.0,
           double circumscribed_radius = 0.0, double inflation_radius = 0.0, double obstacle_range = 0.0,
           double raytrace_range = 0.0, double weight = 25.0,
           const std::vector<unsigned char>& static_data = std::vector<unsigned char>(0), unsigned char lethal_threshold = 0,
