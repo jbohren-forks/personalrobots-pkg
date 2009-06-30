b6d1c51f5d723663625a44f8bd86906cf813ee96
alexteichman <alexteichman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-06-30 22:47:15 +0000

    Making mask conversion work with new file names, and easier to use while I'm at it.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@18020 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 539c8e84c4d4096dd89a8aa1bed00fe7a475a08f

--- vision/cv_mech_turk/src/convert_session_results_to_mask.cpp
+++ vision/cv_mech_turk/src/convert_session_results_to_mask.cpp
@@ -10,20 +10,37 @@
 #include <cv.h>
 #include <cxcore.h>
 #include <highgui.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
 
 using namespace std;
 // Read a file into a vector
 void help(char *program) {
-  cout << "\n" << program << " <[path/]filelist> "<<"<xml_dir>" << " <base_dir>" << " <widthxheight>" << " [scale_factor]" << endl;
-  cout << "     filelist      file should be a list of the xml files with no path information\n" << endl;
-  cout << "     xml_dir       is where the xml files, listed in filelist reside\n" << endl;
-  cout << "     base_dir      is the directory that contains the original image sub-directories\n" << endl; 
+  cout << "\n" << program << " <session_results_dir> " << " <widthxheight>" << " [scale_factor]" << endl;
+  cout << "     session_results_dir      e.g. results/single-object-s\n" << endl;
   cout << "     widthxheight  for example: 640x480\n" << endl;
   cout << "     scale_factor  If images was downsampled prior to submit_img.py, upsample with this, DEFAULT: 1.0\n\n" << endl;
 }
 
 //SUPPORT FUNCTIONS
+bool getdir (string dir, vector<string> &files)
+{
+  DIR *dp;
+  struct dirent *dirp;
+  if((dp  = opendir(dir.c_str())) == NULL) {
+    cout << "Error opening " << dir << endl;
+    return false;
+  }
+
+  while ((dirp = readdir(dp)) != NULL) {
+    files.push_back(string(dirp->d_name));
+  }
+  closedir(dp);
+  return true;
+}
+
 int xy_pairs_from_polygon_string(string s, vector<float> &x, vector<float> &y, CvRect &r, float image_scaling = 1.0)
 {
 	x.clear(); y.clear();
@@ -93,14 +110,13 @@ void image_mask_out(const char *path_filename, int width, int height, vector<flo
 int main(int argc, char* argv[])
 {
   //Command line stuff
-  if (argc < 5) { help(argv[0]); return 1; }
-  std::vector<std::string> file;
-  std::string line;
-  file.clear();
-  std::ifstream infile (argv[1], std::ios_base::in);
-  std::string xml_dir = argv[2];
-  std::string base_dir = argv[3];
-  std::string x_by_y = argv[4];
+  if (argc < 3) { help(argv[0]); return 1; }
+  std::vector<std::string> xml_files;
+  xml_files.clear();
+  string dir = argv[1];
+  std::string xml_dir = dir + "/annotations";
+  std::string base_dir = dir + "/images";
+  std::string x_by_y = argv[2];
   string::size_type xmark = x_by_y.find("x");
   if(xmark == string::npos) { cout << "Error in param " << x_by_y << ", widthxheight (example 640x480)" << endl; return -1;} 
   int width = atoi((x_by_y.substr(0,xmark)).c_str());
@@ -111,88 +127,95 @@ int main(int argc, char* argv[])
   cout << "Width = " << width << ", height= " << height << "; scale = " << image_scaling << endl;
 
   //Get the list of xml files ...
-  while (getline(infile, line, '\n'))
-  {
-    file.push_back (line);
-  }
-  infile.close();
-  std::cout << "Read " << file.size() << " lines.\n";
-  
+  getdir(xml_dir, xml_files);
+
   //Parse into directories and files by looking for the "__" separator
   vector<string> img_dir,img_file;
   string::size_type beg1,len1,beg2,len2;
   int i;
-  for(i=0; i<(int)file.size(); ++i)
-  {
-  	cout << i << ": "; //<< " From " << file[i] << endl;
-  		beg1 = 0;
-  		if((len1 = file[i].find("__")) == string::npos) break; //protect against not finding anything
-  		len1 -= beg1;
-  		beg2 = len1 + beg1 + 2;
-  		if((len2 = file[i].find(".xml")) == string::npos) break;
-  		len2 -= beg2;
-  		img_dir.push_back(file[i].substr(beg1,len1));
-  		img_file.push_back(file[i].substr(beg2,len2));
-  		cout << img_dir[i] << ", " << img_file[i] << endl;
+  for(i=0; i<(int)xml_files.size(); ++i)
+    {
+
+      int sz = xml_files[i].size();
+      string base_name = xml_files[i].substr(0, sz - 4);
+      if(xml_files[i].compare(".") == 0 || xml_files[i].compare("..") == 0)
+	continue;
+
+      cout << "Found annotation for " << base_name << endl;
+
+      //   		beg1 = 0;
+      //   		if((len1 = xml_files[i].find("__")) == string::npos) break; //protect against not finding anything
+      //   		len1 -= beg1;
+      //   		beg2 = len1 + beg1 + 2;
+      //   		if((len2 = xml_files[i].find(".xml")) == string::npos) break;
+      //   		len2 -= beg2;
+      //   		img_dir.push_back(xml_files[i].substr(beg1,len1));
+      //   		img_file.push_back(xml_files[i].substr(beg2,len2));
+      //   		cout << img_dir[i] << ", " << img_file[i] << endl;
+
+      //Now attack the xml files themselves
+      string::size_type idx1,idx2,idxlen;
+      std::ifstream xml_file;
+      std::ofstream out_file;
+      int swidth = (int)((float)width*image_scaling + 0.5); //Scaled width and height
+      int sheight = (int)((float)height*image_scaling + 0.5);
+
+      string rect_dir = dir + "/CvRects/";
+      string mask_dir = dir + "/masks/";
+      string xml_file_path =  dir + "/annotations/" + base_name + ".xml";
+      string file_out =  rect_dir + base_name + "_CvRects.csv"; //For writing rectangles
+      string Imask_out = mask_dir + base_name + "_Mask"; //Base name for writing out image masks
+      mkdir(rect_dir.c_str(), S_IRWXO | S_IRWXU);
+      mkdir(mask_dir.c_str(), S_IRWXO | S_IRWXU);
+      cout << "Opening " << xml_files[i] << endl;
+      cout << "   rectangles will be stored in: " << file_out << endl;
+      cout << "   mask will be stored in: " << Imask_out << endl;
+      xml_file.open(xml_file_path.c_str(), ifstream::in);
+      out_file.open(file_out.c_str());
+      string xml_strings, polygon;
+      //Read the xml file into one big string
+      string line;
+      while(getline(xml_file, line, '\n'))
+	{
+	  xml_strings += line;
 	}
-	int num_files = i;
-	
-	//Now attack the xml files themselves
-  	string::size_type idx1,idx2,idxlen;
- 	std::ifstream xml_file;
- 	std::ofstream out_file;
- 	int swidth = (int)((float)width*image_scaling + 0.5); //Scaled width and height
- 	int sheight = (int)((float)height*image_scaling + 0.5);
-  	for(i=0; i<num_files; ++i) // For each xml file
-  	{
-  		string file_name = xml_dir + "/" + img_dir[i] + "__" + img_file[i] + ".xml"; //dir + recreate file string
-  		string file_out =  base_dir + "/" + img_dir[i] + "/" + img_file[i] + "_CvRects.csv"; //For writing rectangles
-  		string Imask_out = base_dir + "/" + img_dir[i] + "/" + img_file[i] + "_Mask"; //Base name for writing out image masks
-  		cout << "Opening " << file_name << ", rectangles and masks will be stored in: " << file_out << endl;
-  		xml_file.open(file_name.c_str(), ifstream::in);
-  		out_file.open(file_out.c_str());
-  		string xml_strings, polygon;
-  		//Read the xml file into one big string
-  		while(getline(xml_file, line, '\n'))
-  		{
-  			xml_strings += line;
-		}
-		//Parse out all polygon strings in this xml file
-		vector<vector<float> > X,Y;
-		vector<CvRect> R;
-		vector<float> x,y;
-		CvRect r;
-		idx2 = 0;
-		while(1)
-		{
-			if((idx1 = xml_strings.find("<polygon",idx2)) == string::npos) break;
-			if((idx2 = xml_strings.find("</polygon",idx1 + 8)) == string::npos) break;
-			idx2 += 9;
-			idxlen = idx2 - idx1;
-			if(xy_pairs_from_polygon_string(xml_strings.substr(idx1,idxlen), x, y, r, image_scaling) > 0) {
-				X.push_back(x); Y.push_back(y); R.push_back(r);
-			}
-		}
-		cout << "  * Found " << X.size() << " strings" << endl;
-		//RECORD IMAGE MASKS AND BOUNDING BOXES FOR EACH RECTANGLE
-		int polies = (int)X.size();
-		cout << "   MASKS:" << endl;
-		for(int xx = 0; xx<polies; ++xx)
-		{
-			out_file << "xywh" << xx << ", " << R[xx].x << ", " << R[xx].y << ", " << R[xx].width << ", " << R[xx].height << "," << endl;
-			std::stringstream numb;
-			numb << xx;
-			string img_out = Imask_out + "_" + numb.str() + ".png";
-			cout << "   " << img_out << endl;   
-			image_mask_out(img_out.c_str(), swidth, sheight, X[xx], Y[xx]);
-		}
-		xml_strings.clear();
-		xml_file.close();
-		out_file.close();
+      //Parse out all polygon strings in this xml file
+      vector<vector<float> > X,Y;
+      vector<CvRect> R;
+      vector<float> x,y;
+      CvRect r;
+      idx2 = 0;
+      while(1)
+	{
+	  if((idx1 = xml_strings.find("<polygon",idx2)) == string::npos) break;
+	  if((idx2 = xml_strings.find("</polygon",idx1 + 8)) == string::npos) break;
+	  idx2 += 9;
+	  idxlen = idx2 - idx1;
+	  if(xy_pairs_from_polygon_string(xml_strings.substr(idx1,idxlen), x, y, r, image_scaling) > 0) {
+	    X.push_back(x); Y.push_back(y); R.push_back(r);
+	  }
 	}
-  	
-	cout << "\nDONE\n" << endl;  	
+      cout << "  * Found " << X.size() << " strings" << endl;
+      //RECORD IMAGE MASKS AND BOUNDING BOXES FOR EACH RECTANGLE
+      int polies = (int)X.size();
+      cout << "   MASKS:" << endl;
+      for(int xx = 0; xx<polies; ++xx)
+	{
+	  out_file << "xywh" << xx << ", " << R[xx].x << ", " << R[xx].y << ", " << R[xx].width << ", " << R[xx].height << "," << endl;
+	  std::stringstream numb;
+	  numb << xx;
+	  string img_out = Imask_out + "_" + numb.str() + ".png";
+	  cout << "   " << img_out << endl;   
+	  image_mask_out(img_out.c_str(), swidth, sheight, X[xx], Y[xx]);
+	}
+      xml_strings.clear();
+      xml_file.close();
+      out_file.close();
+    }
+  
+  cout << "\nDONE\n" << endl;  	
   return 0;
 }
+
 
 
