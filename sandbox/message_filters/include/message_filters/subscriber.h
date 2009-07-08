71c1f82cefb060e79a1d1041ba94c4810e9ce78a
jfaustwg <jfaustwg@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-07-08 21:14:53 +0000

    Check for empty topic in subscribe()
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@18497 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 0cb3e2e44705dd1bbe7ff97cad4553952d39d9be

--- sandbox/message_filters/include/message_filters/subscriber.h
+++ sandbox/message_filters/include/message_filters/subscriber.h
@@ -116,11 +116,14 @@ public:
   {
     unsubscribe();
 
-    ros::SubscribeOptions ops;
-    ops.init<M>(topic, queue_size, boost::bind(&Subscriber<M>::cb, this, _1));
-    ops.callback_queue = callback_queue;
-    ops.transport_hints = transport_hints;
-    sub_ = nh.subscribe(ops);
+    if (!topic.empty())
+    {
+      ros::SubscribeOptions ops;
+      ops.init<M>(topic, queue_size, boost::bind(&Subscriber<M>::cb, this, _1));
+      ops.callback_queue = callback_queue;
+      ops.transport_hints = transport_hints;
+      sub_ = nh.subscribe(ops);
+    }
   }
 
   /**
