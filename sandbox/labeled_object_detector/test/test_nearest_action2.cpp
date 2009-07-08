ae037b4da753b3a021eff1c19b94ac9bda190604
syrnick <syrnick@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-07-08 22:19:14 +0000

    Whiteboard detection action.
    To launch :
    1. roslaunch labeled_object_detector/launch/whiteboard_detector_1.launch
    2. labeled_object_detector/launch/whiteboard_detector_2a.launch
    
    It should report ~36K points
    
    3. roslaunch launch/whiteboard_detector_laser.xml
    
    4. run test/test_nearest_action2
    
    It will activate the action with the amcl pose, so move the robot around. 
    
    You should see the whiteboards in the green room in rviz.
    
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@18504 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 99bcf7d447ec844b4997161e98a3f8bad4ee0e8c

---
+++ sandbox/labeled_object_detector/test/test_nearest_action2.cpp
@@ -0,0 +1,105 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+
+//include <labeled_object_detector/planar_object_detector_action.h>
+#include <labeled_object_detector/PoseStampedState.h>
+#include <robot_actions/action_client.h>
+#include <robot_actions/action.h>
+#include <ros/ros.h>
+#include <robot_msgs/PointStamped.h>
+#include <robot_msgs/PoseStamped.h>
+#include <robot_msgs/PoseWithCovariance.h>
+#include <gtest/gtest.h>
+
+using namespace robot_msgs;
+using namespace labeled_object_detector;
+
+int count=0;
+
+void on_pose(robot_actions::ActionClient<PoseStamped, PoseStampedState, PoseStamped> &client,PoseWithCovarianceConstPtr pose)
+{
+  ROS_INFO_STREAM("pose "<<count <<","<<pose);
+
+  PoseStamped goal;
+  goal.pose=pose->pose;
+  goal.header=pose->header;
+  PoseStamped feedback;
+  robot_actions::ResultStatus result = client.execute(goal, feedback, ros::Duration(5));      
+  count++;
+}
+
+/* ---[ */
+int
+  main (int argc, char** argv)
+{
+  ros::init (argc, argv, "action_test");
+
+  robot_actions::ActionClient<PoseStamped, PoseStampedState, PoseStamped> client("detect_nearest_object");
+  //ros::Duration duration(10);
+  //duration.sleep();
+  
+  ros::NodeHandle n_;
+  ros::Subscriber s=n_.subscribe<PoseWithCovariance>("amcl_pose",1,boost::bind(&on_pose,client,_1));
+
+  ros::spin();
+
+  /*
+  PoseStamped goal;
+  PoseStamped feedback;
+  // Start the action; it must finish in 1 second
+
+  goal.header.stamp.sec=1246990627;
+  goal.header.stamp.nsec=471105000;
+  goal.header.frame_id=std::string("map");
+  goal.pose.position.x=0;
+  goal.pose.position.y=0;
+  goal.pose.position.z=0;
+  goal.pose.orientation.x=0;
+  goal.pose.orientation.y=0;
+  goal.pose.orientation.z=0;
+  goal.pose.orientation.w=1;
+
+  robot_actions::ResultStatus result = client.execute(goal, feedback, ros::Duration(10));
+  //ASSERT_EQ(result, robot_actions::PREEMPTED);
+  */
+  // Start the action; it must finish in 0.0001 second
+  /*result = client.execute(g, f, ros::Duration().fromSec(0.0001));
+  ASSERT_EQ(result, robot_actions::PREEMPTED);
+  */
+
+  return (0);
+}
+/* ]--- */
+
