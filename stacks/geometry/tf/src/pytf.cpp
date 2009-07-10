5cd365bb280a61e3e569d0efcfa0ab5fd3a6df31
jamesbowman <jamesbowman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-07-10 00:05:54 +0000

    New Python bindings, currenty disabled
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@18601 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: e591ea018a5142ceff0a2b07184f2b52766ea87f

--- stacks/geometry/tf/src/pytf.cpp
+++ stacks/geometry/tf/src/pytf.cpp
@@ -2,6 +2,9 @@
 
 #include "tf/tf.h"
 
+static PyObject *tf_error = NULL;
+static PyObject *pModulerospy = NULL;
+
 struct transformer_t {
   PyObject_HEAD
   tf::Transformer *t;
@@ -10,11 +13,11 @@ struct transformer_t {
 static PyTypeObject transformer_Type = {
   PyObject_HEAD_INIT(&PyType_Type)
   0,                               /*size*/
-  "tf.transformer",                /*name*/
+  "_tfX.Transformer",                /*name*/
   sizeof(transformer_t),           /*basicsize*/
 };
 
-static PyObject *PyObject_StealAttrString(PyObject* o, const char *name)
+static PyObject *PyObject_BorrowAttrString(PyObject* o, const char *name)
 {
     PyObject *r = PyObject_GetAttrString(o, name);
     if (r != NULL)
@@ -22,38 +25,162 @@ static PyObject *PyObject_StealAttrString(PyObject* o, const char *name)
     return r;
 }
 
-static PyObject *mkTransformer(PyObject *self, PyObject *args)
+static int rostime_converter(PyObject *obj, ros::Time *rt)
+{
+  PyObject *tsr = PyObject_CallMethod(obj, (char*)"to_seconds", NULL);
+  if (tsr == NULL) {
+    PyErr_SetString(PyExc_TypeError, "time must have a to_seconds method, e.g. rospy.Time or rospy.Duration");
+    return 0;
+  } else {
+    (*rt).fromSec(PyFloat_AsDouble(tsr));
+    Py_DECREF(tsr);
+    return 1;
+  }
+}
+
+static int rosduration_converter(PyObject *obj, ros::Duration *rt)
+{
+  PyObject *tsr = PyObject_CallMethod(obj, (char*)"to_seconds", NULL);
+  if (tsr == NULL) {
+    PyErr_SetString(PyExc_TypeError, "time must have a to_seconds method, e.g. rospy.Time or rospy.Duration");
+    return 0;
+  } else {
+    (*rt).fromSec(PyFloat_AsDouble(tsr));
+    Py_DECREF(tsr);
+    return 1;
+  }
+}
+
+static int Transformer_init(PyObject *self, PyObject *args, PyObject *kw)
 {
   int interpolating = 1;
-  PyObject *py_cache_time = NULL;
-
-  if (!PyArg_ParseTuple(args, "|iO", &interpolating, &py_cache_time))
-    return NULL;
-
-  transformer_t *r = PyObject_NEW(transformer_t, &transformer_Type);
-
   ros::Duration cache_time;
-  if (py_cache_time != NULL) {
-    PyObject *tsr = PyObject_CallMethod(py_cache_time, "to_seconds", NULL);
-    if (tsr == NULL) {
-       PyErr_SetString(PyExc_TypeError, "'cache_time' must have a to_seconds method, e.g. rospy.Duration");
-       return NULL;
-    }
-    cache_time.fromSec(PyFloat_AsDouble(tsr));
-    Py_DECREF(tsr);
-  } else {
-    cache_time.fromSec(tf::Transformer::DEFAULT_CACHE_TIME);
-  }
 
-  r->t = new tf::Transformer(interpolating, cache_time);
+  cache_time.fromSec(tf::Transformer::DEFAULT_CACHE_TIME);
 
-  return (PyObject*)r;
+  if (!PyArg_ParseTuple(args, "|iO&", &interpolating, rosduration_converter, &cache_time))
+    return -1;
+
+  ((transformer_t*)self)->t = new tf::Transformer(interpolating, cache_time);
+
+  return 0;
+}
+
+static PyObject *allFramesAsDot(PyObject *self, PyObject *args)
+{
+  tf::Transformer *t = ((transformer_t*)self)->t;
+  return PyString_FromString(t->allFramesAsDot().c_str());
 }
 
 static PyObject *allFramesAsString(PyObject *self, PyObject *args)
 {
   tf::Transformer *t = ((transformer_t*)self)->t;
   return PyString_FromString(t->allFramesAsString().c_str());
+}
+
+static PyObject *canTransform(PyObject *self, PyObject *args, PyObject *kw)
+{
+  tf::Transformer *t = ((transformer_t*)self)->t;
+  char *target_frame, *source_frame;
+  ros::Time time;
+  static char *keywords[] = { "target_frame", "source_frame", "time", NULL };
+
+  if (!PyArg_ParseTupleAndKeywords(args, kw, "ssO&", keywords, &target_frame, &source_frame, rostime_converter, &time))
+    return NULL;
+  return PyBool_FromLong(t->canTransform(target_frame, source_frame, time));
+}
+
+static PyObject *canTransformFull(PyObject *self, PyObject *args, PyObject *kw)
+{
+  tf::Transformer *t = ((transformer_t*)self)->t;
+  char *target_frame, *source_frame, *fixed_frame;
+  ros::Time target_time, source_time;
+  static char *keywords[] = { "target_frame", "target_time", "source_frame", "source_time", "fixed_frame", NULL };
+
+  if (!PyArg_ParseTupleAndKeywords(args, kw, "sO&sO&s", keywords,
+                        &target_frame,
+                        rostime_converter,
+                        &target_time,
+                        &source_frame,
+                        rostime_converter,
+                        &source_time,
+                        &fixed_frame))
+    return NULL;
+  return PyBool_FromLong(t->canTransform(target_frame, target_time, source_frame, source_time, fixed_frame));
+}
+
+static PyObject *asListOfStrings(std::vector< std::string > los)
+{
+  PyObject *r = PyList_New(los.size());
+  size_t i;
+  for (i = 0; i < los.size(); i++) {
+    PyList_SetItem(r, i, PyString_FromString(los[i].c_str()));
+  }
+  return r;
+}
+
+static PyObject *chain(PyObject *self, PyObject *args, PyObject *kw)
+{
+  tf::Transformer *t = ((transformer_t*)self)->t;
+  char *target_frame, *source_frame, *fixed_frame;
+  ros::Time target_time, source_time;
+  std::vector< std::string > output;
+  static char *keywords[] = { "target_frame", "target_time", "source_frame", "source_time", "fixed_frame", NULL };
+
+  if (!PyArg_ParseTupleAndKeywords(args, kw, "sO&sO&s", keywords,
+                        &target_frame,
+                        rostime_converter,
+                        &target_time,
+                        &source_frame,
+                        rostime_converter,
+                        &source_time,
+                        &fixed_frame))
+    return NULL;
+
+  t->chainAsVector(target_frame, target_time, source_frame, source_time, fixed_frame, output);
+  return asListOfStrings(output);
+}
+
+static PyObject *getLatestCommonTime(PyObject *self, PyObject *args, PyObject *kw)
+{
+  tf::Transformer *t = ((transformer_t*)self)->t;
+  char *source, *dest;
+  std::string error_string;
+  ros::Time time;
+
+  if (!PyArg_ParseTuple(args, "ss", &source, &dest))
+    return NULL;
+  int r = t->getLatestCommonTime(source, dest, time, &error_string);
+  if (r == 0) {
+    PyObject *rospy_time = PyObject_GetAttrString(pModulerospy, "Time");
+    PyObject *args = Py_BuildValue("ii", time.sec, time.nsec);
+    PyObject *ob = PyObject_CallObject(rospy_time, args);
+    Py_DECREF(args);
+    Py_DECREF(rospy_time);
+    return ob;
+  } else {
+    PyErr_SetString(tf_error, error_string.c_str());
+    return NULL;
+  }
+}
+
+static PyObject *lookupTransform(PyObject *self, PyObject *args, PyObject *kw)
+{
+  tf::Transformer *t = ((transformer_t*)self)->t;
+  char *target_frame, *source_frame;
+  ros::Time time;
+  static char *keywords[] = { "target_frame", "source_frame", "time", NULL };
+
+  if (!PyArg_ParseTupleAndKeywords(args, kw, "ssO&", keywords, &target_frame, &source_frame, rostime_converter, &time))
+    return NULL;
+  tf::Stamped< btTransform > transform;
+  t->lookupTransform(target_frame, source_frame, time, transform);
+  // return Py_BuildValue("(ddd)(dddd)", 
+  btVector3 origin = transform.getOrigin();
+  btQuaternion rotation = transform.getRotation();
+  return Py_BuildValue("(ddd)(dddd)",
+      origin.x(), origin.y(), origin.z(),
+      rotation.x(), rotation.y(), rotation.z(), rotation.w());
 }
 
 static PyObject *setTransform(PyObject *self, PyObject *args)
@@ -65,27 +192,22 @@ static PyObject *setTransform(PyObject *self, PyObject *args)
   if (!PyArg_ParseTuple(args, "O|s", &py_transform, &authority))
     return NULL;
   tf::Stamped< btTransform > transform;
-  PyObject *header = PyObject_StealAttrString(py_transform, "header");
-  transform.frame_id_ = PyString_AsString(PyObject_StealAttrString(header, "frame_id"));
-  transform.parent_id_ = PyString_AsString(PyObject_StealAttrString(py_transform, "parent_id"));
-  PyObject *tsr = PyObject_CallMethod(PyObject_StealAttrString(header, "stamp"), "to_seconds", NULL);
-  if (tsr == NULL) {
-     PyErr_SetString(PyExc_TypeError, "'stamp' must have a to_seconds method, e.g. rospy.Time");
-     return NULL;
-  }
-  transform.stamp_ = ros::Time().fromSec(PyFloat_AsDouble(tsr));
-  Py_DECREF(tsr);
+  PyObject *header = PyObject_BorrowAttrString(py_transform, "header");
+  transform.frame_id_ = PyString_AsString(PyObject_BorrowAttrString(header, "frame_id"));
+  transform.parent_id_ = PyString_AsString(PyObject_BorrowAttrString(py_transform, "parent_id"));
+  if (rostime_converter(PyObject_BorrowAttrString(header, "stamp"), &transform.stamp_) != 1)
+    return NULL;
 
-  PyObject *mtransform = PyObject_StealAttrString(py_transform, "transform");
-  PyObject *translation = PyObject_StealAttrString(mtransform, "translation");
-  double tx = PyFloat_AsDouble(PyObject_StealAttrString(translation, "x"));
-  double ty = PyFloat_AsDouble(PyObject_StealAttrString(translation, "y"));
-  double tz = PyFloat_AsDouble(PyObject_StealAttrString(translation, "z"));
-  PyObject *rotation = PyObject_StealAttrString(mtransform, "rotation");
-  double qx = PyFloat_AsDouble(PyObject_StealAttrString(rotation, "x"));
-  double qy = PyFloat_AsDouble(PyObject_StealAttrString(rotation, "y"));
-  double qz = PyFloat_AsDouble(PyObject_StealAttrString(rotation, "z"));
-  double qw = PyFloat_AsDouble(PyObject_StealAttrString(rotation, "w"));
+  PyObject *mtransform = PyObject_BorrowAttrString(py_transform, "transform");
+  PyObject *translation = PyObject_BorrowAttrString(mtransform, "translation");
+  double tx = PyFloat_AsDouble(PyObject_BorrowAttrString(translation, "x"));
+  double ty = PyFloat_AsDouble(PyObject_BorrowAttrString(translation, "y"));
+  double tz = PyFloat_AsDouble(PyObject_BorrowAttrString(translation, "z"));
+  PyObject *rotation = PyObject_BorrowAttrString(mtransform, "rotation");
+  double qx = PyFloat_AsDouble(PyObject_BorrowAttrString(rotation, "x"));
+  double qy = PyFloat_AsDouble(PyObject_BorrowAttrString(rotation, "y"));
+  double qz = PyFloat_AsDouble(PyObject_BorrowAttrString(rotation, "z"));
+  double qw = PyFloat_AsDouble(PyObject_BorrowAttrString(rotation, "w"));
 
   transform.setData(btTransform(
     btQuaternion(btScalar(qx), btScalar(qy), btScalar(qz), btScalar(qw)),
@@ -94,30 +216,73 @@ static PyObject *setTransform(PyObject *self, PyObject *args)
   Py_RETURN_NONE;
 }
 
+static PyObject *clear(PyObject *self, PyObject *args)
+{
+  tf::Transformer *t = ((transformer_t*)self)->t;
+  t->clear();
+  Py_RETURN_NONE;
+}
+
+static PyObject *frameExists(PyObject *self, PyObject *args)
+{
+  tf::Transformer *t = ((transformer_t*)self)->t;
+  char *frame_id_str;
+  if (!PyArg_ParseTuple(args, "s", &frame_id_str))
+    return NULL;
+  return PyBool_FromLong(t->frameExists(frame_id_str));
+}
+
+static PyObject *getFrameStrings(PyObject *self, PyObject *args)
+{
+  tf::Transformer *t = ((transformer_t*)self)->t;
+  std::vector< std::string > ids;
+  t->getFrameStrings(ids);
+  return asListOfStrings(ids);
+}
+
 static struct PyMethodDef transformer_methods[] =
 {
+  {"allFramesAsDot", allFramesAsDot, METH_VARARGS},
   {"allFramesAsString", allFramesAsString, METH_VARARGS},
   {"setTransform", setTransform, METH_VARARGS},
+  {"canTransform", (PyCFunction)canTransform, METH_KEYWORDS},
+  {"canTransformFull", (PyCFunction)canTransformFull, METH_KEYWORDS},
+  {"chain", (PyCFunction)chain, METH_KEYWORDS},
+  {"clear", (PyCFunction)clear, METH_KEYWORDS},
+  {"frameExists", (PyCFunction)frameExists, METH_VARARGS},
+  {"getFrameStrings", (PyCFunction)getFrameStrings, METH_VARARGS},
+  {"getLatestCommonTime", (PyCFunction)getLatestCommonTime, METH_VARARGS},
+  {"lookupTransform", (PyCFunction)lookupTransform, METH_VARARGS},
   {NULL,          NULL}
 };
 
 static PyMethodDef module_methods[] = {
-  {"Transformer", mkTransformer, METH_VARARGS},
+  // {"Transformer", mkTransformer, METH_VARARGS},
   {NULL, NULL, NULL},
 };
 
 extern "C" void init_tfX()
 {
-  PyObject *m, *d;
+  PyObject *item, *m, *d;
+
+#if PYTHON_API_VERSION >= 1007
+  tf_error = PyErr_NewException((char*)"tf.error", NULL, NULL);
+#else
+  tf_error = PyString_FromString("tf.error");
+#endif
+
+  pModulerospy = PyImport_Import(item= PyString_FromString("rospy")); Py_DECREF(item);
 
   transformer_Type.tp_alloc = PyType_GenericAlloc;
   transformer_Type.tp_new = PyType_GenericNew;
+  transformer_Type.tp_init = Transformer_init;
   transformer_Type.tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE;
   transformer_Type.tp_methods = transformer_methods;
   if (PyType_Ready(&transformer_Type) != 0)
     return;
 
   m = Py_InitModule("_tfX", module_methods);
+  PyModule_AddObject(m, "Transformer", (PyObject *)&transformer_Type);
   d = PyModule_GetDict(m);
+  PyDict_SetItemString(d, "error", tf_error);
 }
-
--- stacks/geometry/tf/test/bringup.py
+++ stacks/geometry/tf/test/bringup.py
@@ -2,19 +2,67 @@ import roslib
 roslib.load_manifest('tf')
 import rostest
 import rospy
+import numpy
+import rostest
+import unittest
 
-import tf.msg
+import tf.transformations
 import robot_msgs.msg
 
-import _tfX as tf
+import _tfX
 
-#help(rospy.Duration())
-d = rospy.Duration(3)
-print "d.secs", d.secs, d.nsecs
-t = tf.Transformer(True, rospy.Duration(10.0))
-m = robot_msgs.msg.TransformStamped()
-m.header.frame_id = "THISFRAME"
-m.parent_id = "PARENT"
-m.transform.rotation.w = 1.0
-t.setTransform(m)
-print "allFramesAsString = \"%s\"" % t.allFramesAsString()
+class TestDirected(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def expect_exception(self, func, exception):
+       tripped = False
+       try:
+           func()
+       except exception:
+           tripped = True
+       self.assert_(tripped)
+
+    class SubClass(_tfX.Transformer):
+        pass
+
+    def test_smoke(self):
+        t = _tfX.Transformer(True, rospy.Duration(10.0))
+        self.common(t)
+
+    def test_subclass(self):
+        class TransformerSubclass(_tfX.Transformer):
+            def extra(self):
+              return 77
+        t = TransformerSubclass(True, rospy.Duration(10.0))
+        self.common(t)
+        self.assert_(t.extra() == 77)
+
+    def common(self, t):
+        m = robot_msgs.msg.TransformStamped()
+        m.header.frame_id = "THISFRAME"
+        m.parent_id = "PARENT"
+        m.transform.translation.y = 5.0
+        m.transform.rotation = robot_msgs.msg.Quaternion(*tf.transformations.quaternion_from_euler(0, 0, 0))
+        t.setTransform(m)
+        afs = t.allFramesAsString()
+        self.assert_(len(afs) != 0)
+        self.assert_("PARENT" in afs)
+        self.assert_("THISFRAME" in afs)
+        self.assert_(t.getLatestCommonTime("THISFRAME", "PARENT").to_seconds() == 0)
+        for ti in [3, 5, 10, 11, 19, 20, 21]:
+            m.header.stamp.secs = ti
+            t.setTransform(m)
+            self.assert_(t.getLatestCommonTime("THISFRAME", "PARENT").to_seconds() == ti)
+
+        # Verify that getLatestCommonTime with nonexistent frames raises a _tfX.error
+        self.expect_exception(lambda: t.getLatestCommonTime("XXX", "YYY"), _tfX.error)
+
+if __name__ == '__main__':
+    if 0:
+        rostest.unitrun('tf', 'directed', TestDirected)
+    else:
+        suite = unittest.TestSuite()
+        suite.addTest(TestDirected('test_smoke'))
+        unittest.TextTestRunner(verbosity=2).run(suite)
