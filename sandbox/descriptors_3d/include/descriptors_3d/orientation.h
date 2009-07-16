e30c9118d8c3814379de5813989e1a95f07ef642
danmuno <danmuno@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-07-16 00:03:50 +0000

    updated comments
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@18914 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 4a401dd3b0c92adafdaff7a5f1320a4672f606c3

--- sandbox/descriptors_3d/include/descriptors_3d/orientation.h
+++ sandbox/descriptors_3d/include/descriptors_3d/orientation.h
@@ -3,7 +3,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2009, Daniel Munoz
+ *  Copyright (c) 2009, Willow Garage
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
