9f12ab67b53cf60681d485213d5fb8b1fde39238
eitanme <eitanme@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-07-18 00:39:08 +0000

    Version of move base that uses the new action tools
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@19157 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: da3c1c4b87dd64da3a1542e2ac34250592259630

---
+++ stacks/nav/move_base/include/move_base/move_base.h
@@ -0,0 +1,199 @@
+/*********************************************************************
+*
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*
+* Author: Eitan Marder-Eppstein
+*********************************************************************/
+#ifndef NAV_MOVE_BASE_ACTION_H_
+#define NAV_MOVE_BASE_ACTION_H_
+#include <ros/ros.h>
+
+#include <action_tools/action_server.h>
+#include <action_tools/action_client.h>
+#include <move_base/MoveBaseGoal.h>
+#include <move_base/MoveBaseResult.h>
+
+#include <nav_robot_actions/base_local_planner.h>
+#include <nav_robot_actions/base_global_planner.h>
+#include <robot_msgs/PoseStamped.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h>
+#include <vector>
+#include <string>
+#include <nav_srvs/Plan.h>
+#include <visualization_msgs/Marker.h>
+#include <robot_msgs/PoseDot.h>
+
+namespace move_base {
+  //typedefs to help us out with the action server so that we don't hace to type so much
+  typedef action_tools::ActionServer<MoveBaseGoal, robot_msgs::PoseStamped,
+          MoveBaseResult, robot_msgs::PoseStamped> MoveBaseActionServer;
+
+  typedef action_tools::ActionClient<MoveBaseGoal, robot_msgs::PoseStamped,
+          MoveBaseResult, robot_msgs::PoseStamped> MoveBaseActionClient;
+
+  typedef MoveBaseActionServer::GoalHandle GoalHandle;
+
+  enum MoveBaseState {
+    PLANNING,
+    CONTROLLING,
+    CLEARING
+  };
+
+  enum ClearingState {
+    CONSERVATIVE_RESET,
+    IN_PLACE_ROTATION_1,
+    EXECUTE_ROTATE_1,
+    IN_PLACE_ROTATION_2,
+    EXECUTE_ROTATE_2,
+    AGGRESSIVE_RESET,
+    ABORT
+  };
+
+  /**
+   * @class MoveBase
+   * @brief A class that uses the action_tools::ActionServer interface that moves the robot base to a goal location.
+   */
+  class MoveBase {
+    public:
+      /**
+       * @brief  Constructor for the actions
+       * @param name The name of the action
+       * @param tf A reference to a TransformListener
+       */
+      MoveBase(std::string name, tf::TransformListener& tf);
+
+      /**
+       * @brief  Destructor - Cleans up
+       */
+      virtual ~MoveBase();
+
+      /**
+       * @brief  Performs a control cycle
+       * @param goal A reference to the goal to pursue
+       * @param global_plan A reference to the global plan being used
+       */
+      void executeCycle(robot_msgs::PoseStamped& goal, std::vector<robot_msgs::PoseStamped>& global_plan);
+
+      /**
+       * @brief  The main loop of the MoveBase action
+       */
+      void runLoop();
+
+    private:
+      /**
+       * @brief  A service call that can be made when the action is inactive that will return a plan
+       * @param  req The goal request
+       * @param  resp The plan request
+       * @return True if planning succeeded, false otherwise
+       */
+      bool planService(nav_srvs::Plan::Request &req, nav_srvs::Plan::Response &resp);
+
+      /**
+       * @brief  Make a new global plan
+       * @param  goal The goal to plan to
+       * @param  plan Will be filled in with the plan made by the planner
+       * @return  True if planning succeeds, false otherwise
+       */
+      bool makePlan(const robot_msgs::PoseStamped& goal, std::vector<robot_msgs::PoseStamped>& plan);
+
+      /**
+       * @brief  Publish a goal to the visualizer
+       * @param  goal The goal to visualize
+       */
+      void publishGoal(const robot_msgs::PoseStamped& goal);
+
+      /**
+       * @brief  Resets the costmaps to the static map outside a given window
+       * @param size_x The x size of the window
+       * @param size_y The y size of the window
+       */
+      void resetCostmaps(double size_x, double size_y);
+
+      /**
+       * @brief  Clears obstacles within a window around the robot
+       * @param size_x The x size of the window
+       * @param size_y The y size of the window
+       */
+      void clearCostmapWindows(double size_x, double size_y);
+
+      /**
+       * @brief  Publishes a velocity command of zero to the base
+       */
+      void publishZeroVelocity();
+
+      /**
+       * @brief  Perform an in-place rotation of the base to attempt to clear out space
+       */
+      bool rotateRobot();
+
+      bool set180RotationGoal();
+
+      /**
+       * @brief  Reset the state of the move_base action and send a zero velocity command to the base
+       */
+      void resetState();
+
+      void goalCB(const robot_msgs::PoseStamped::ConstPtr& goal);
+
+      ros::NodeHandle ros_node_;
+      tf::TransformListener& tf_;
+
+      MoveBaseActionServer as_;
+      MoveBaseActionClient ac_;
+
+      nav_robot_actions::BaseLocalPlanner* tc_;
+      costmap_2d::Costmap2DROS* planner_costmap_ros_, *controller_costmap_ros_;
+
+      nav_robot_actions::BaseGlobalPlanner* planner_;
+      std::string robot_base_frame_, global_frame_;
+
+      tf::Stamped<tf::Pose> global_pose_;
+      double controller_frequency_, inscribed_radius_, circumscribed_radius_;
+      double planner_patience_, controller_patience_;
+      double conservative_reset_dist_, clearing_radius_;
+      ros::Publisher vis_pub_, vel_pub_, position_pub_;
+      ros::Subscriber goal_sub_;
+      ros::ServiceServer make_plan_srv_;
+      bool shutdown_costmaps_;
+
+      MoveBaseState state_;
+      ClearingState clearing_state_;
+
+      ros::Time last_valid_plan_, last_valid_control_;
+      boost::thread* run_loop_;
+      
+  };
+};
+#endif
+
