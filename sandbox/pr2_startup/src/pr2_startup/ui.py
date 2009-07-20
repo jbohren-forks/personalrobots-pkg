0166f675dbf71cc200f2a4e1931bfff75329a2d4
mmwise <mmwise@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-07-20 20:58:06 +0000

    working version of the pr2_startup gui
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@19221 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 527266bc8fe2ddfbe35ef1c13bf01882b0e6dabc

--- sandbox/pr2_startup/src/pr2_startup/ui.py
+++ sandbox/pr2_startup/src/pr2_startup/ui.py
@@ -50,12 +50,17 @@ import wx
 import time
 from wx import xrc
 
+
 import wg_hardware_roslaunch.roslaunch_caller as roslaunch_caller
 
 class PR2StartUpAppFrame(wx.Frame):
   def __init__(self, parent):
     wx.Frame.__init__(self, parent, wx.ID_ANY, "PR2StartUp")
     self.Bind(wx.EVT_CLOSE, self.OnExit)
+    
+    self._robots = { 'PRE' : 'pre.launch', 
+                     'PRF' : 'prf.launch', 
+                     'PRG' : 'prg.launch'}
     
     
     self._core_launcher = None
@@ -64,6 +69,7 @@ class PR2StartUpAppFrame(wx.Frame):
     self._dashboard_launcher = None
     self._rviz_launcher = None
     self._rxgraph_launcher = None
+    self._robot_launch = None
 
     self._mutex = threading.Lock()
     
@@ -71,17 +77,22 @@ class PR2StartUpAppFrame(wx.Frame):
     
     self._xrc = xrc.XmlResource(xrc_path)
     self._real_panel = self._xrc.LoadPanel(self, 'PR2StartUpPanel')
-  
+    
+
     self._real_panel.Bind(wx.EVT_BUTTON, self.LaunchCore, id=xrc.XRCID('m_button7'))
     self._real_panel.Bind(wx.EVT_BUTTON, self.LaunchRobot, id=xrc.XRCID('m_button8'))
     self._real_panel.Bind(wx.EVT_BUTTON, self.LaunchJoystick, id=xrc.XRCID('m_button9'))
     self._real_panel.Bind(wx.EVT_BUTTON, self.LaunchDashboard, id=xrc.XRCID('m_button10'))
     self._real_panel.Bind(wx.EVT_BUTTON, self.LaunchRviz, id=xrc.XRCID('m_button11'))
     self._real_panel.Bind(wx.EVT_BUTTON, self.LaunchRxgraph, id=xrc.XRCID('m_button12'))
+    self._real_panel.Bind(wx.EVT_CHOICE, self.ChooseRobot, id=xrc.XRCID('m_choice1'))
 
     self.core_status = xrc.XRCCTRL(self._real_panel, 'm_textCtrl4')
     self.robot_status = xrc.XRCCTRL(self._real_panel, 'm_textCtrl5')
     self.joystick_status = xrc.XRCCTRL(self._real_panel, 'm_textCtrl6')
+    self.robot_choice = xrc.XRCCTRL(self._real_panel, 'm_choice1')
+    self.robot_choice.SetItems(self._robots)
+
     self.core_status.SetEditable(False)
     self.robot_status.SetEditable(False)
     self.joystick_status.SetEditable(False)
@@ -102,9 +113,14 @@ class PR2StartUpAppFrame(wx.Frame):
   def LaunchRobot(self, event):
     print "launching robot" 
     if self._robot_launcher == None:
-      script = roslib.packages.get_pkg_dir('pr2_alpha') + self.hostname +'.launch'
-      self._robot_launcher = self.launch_script(script, None)
-      self.robot_status.SetBackgroundColour("Light Green")
+      if self._robot_launch == None:
+        self._help_popup = wx.MessageDialog(self._real_panel, 'Please select a robot', 'caption', wx.OK)  
+        self._help_popup.ShowModal()
+      else:
+        script = roslib.packages.get_pkg_dir('pr2_alpha') +'/'+ self._robot_launch
+        self._robot_launcher = self.launch_script(script, None)
+        self._robot_launcher.spin_once()
+        self.robot_status.SetBackgroundColour("Light Green")
     else:
       self._robot_launcher.shutdown()
       self._robot_launcher = None
@@ -113,8 +129,9 @@ class PR2StartUpAppFrame(wx.Frame):
   def LaunchJoystick(self, event):
     print "launching joystick"
     if self._joystick_launcher == None:
-      script = roslib.packages.get_pkg_dir('pr2_alpha') + 'teleop_joystick.launch'
-      self._robot_launcher = self.launch_script(script, None)
+      script = roslib.packages.get_pkg_dir('pr2_alpha') +'/'+ 'teleop_joystick.launch'
+      self._joystick_launcher = self.launch_script(script, None)
+      self._joystick_launcher.spin_once()
       self.joystick_status.SetBackgroundColour("Light Green")
     else:
       self._joystick_launcher.shutdown()
@@ -151,9 +168,10 @@ class PR2StartUpAppFrame(wx.Frame):
       self._rxgraph_launcher.shutdown()
       self._rxgraph_launcher = None
 
+  def ChooseRobot(self, event):
+    self._robot_launch = self._robots[self.robot_choice.GetStringSelection()]
 
-  def launch_script(self, script, process_listener_object = None):
-     
+  def launch_script(self, script, process_listener_object = None):  
     f = open(script, 'r')
     launch_xml = f.read()
     f.close()
@@ -179,7 +197,7 @@ class PR2StartUpApp(wx.App):
   def OnInit(self):
     
     self._frame = PR2StartUpAppFrame(None)
-    self._frame.SetSize(wx.Size(300,300))
+    self._frame.SetSize(wx.Size(300,350))
     self._frame.Layout()
     self._frame.Centre()
     self._frame.Show(True)
