4c5057c7e9dd3b97e4371427cec08f1e07d04387
rphilippsen <rphilippsen@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-07-27 23:19:07 +0000

    more unit tests
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@19768 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 78465ce9cc7c847f50844d7dedb9bfedca64f370

--- motion_planning/mpglue/test/mpglue_test.cpp
+++ motion_planning/mpglue/test/mpglue_test.cpp
@@ -40,129 +40,190 @@ using namespace std;
 using namespace boost;
 
 
-static shared_ptr<costmap_2d::Costmap2D> createCostmap2D()
-{
-  static double const bbx0_(-1);
-  static double const bby0_(-1);
-  static double const bbx1_(1);
-  static double const bby1_(1);
-  static double const costmap_resolution(0.1);
-  static double const costmap_inscribed_radius(0.15);
-  static double const costmap_circumscribed_radius(0.25);
-  static double const costmap_inflation_radius(0.35);
-  double const origin_x(bbx0_);
-  double const origin_y(bby0_);
-  unsigned int const
-    cells_size_x(1+static_cast<unsigned int>(ceil((bbx1_ - bbx0_) / costmap_resolution)));
-  unsigned int const
-    cells_size_y(1+static_cast<unsigned int>(ceil((bby1_ - bby0_) / costmap_resolution)));
-  double const obstacle_range(8); // whatever
-  double const max_obstacle_height(1); // whatever
-  double const raytrace_range(8);	   // whatever
-  double const weight(1); // whatever: documentation says 0<w<=1 but default is 25???
-  vector<unsigned char> static_data(cells_size_x * cells_size_y);
-  unsigned char const lethal_threshold(1);
+namespace mpglue_test {
   
-  shared_ptr<costmap_2d::Costmap2D> cm2d(new costmap_2d::Costmap2D(cells_size_x,
-								   cells_size_y,
-								   costmap_resolution,
-								   origin_x,
-								   origin_y,
-								   costmap_inscribed_radius,
-								   costmap_circumscribed_radius,
-								   costmap_inflation_radius,
-								   obstacle_range,
-								   max_obstacle_height,
-								   raytrace_range,
-								   weight,
-								   static_data,
-								   lethal_threshold));
-  return cm2d;
+  class Factory {
+  public:
+    string const name;
+    explicit Factory(string const & _name): name(_name) {}
+    virtual ~Factory() {}
+    virtual shared_ptr<mpglue::Costmapper> createCostmapper() = 0;
+  };
+  
+  
+  class LocalFactory: public Factory {
+  public:
+    LocalFactory(): Factory("local") {}
+    virtual shared_ptr<mpglue::Costmapper> createCostmapper() {
+      static double const bbx0_(-1);
+      static double const bby0_(-1);
+      static double const bbx1_(1);
+      static double const bby1_(1);
+      static double const costmap_resolution(0.1);
+      static double const costmap_inscribed_radius(0.15);
+      static double const costmap_circumscribed_radius(0.25);
+      static double const costmap_inflation_radius(0.35);
+      double const origin_x(bbx0_);
+      double const origin_y(bby0_);
+      unsigned int const
+	cells_size_x(1+static_cast<unsigned int>(ceil((bbx1_ - bbx0_) / costmap_resolution)));
+      unsigned int const
+	cells_size_y(1+static_cast<unsigned int>(ceil((bby1_ - bby0_) / costmap_resolution)));
+      double const obstacle_range(8); // whatever
+      double const max_obstacle_height(1); // whatever
+      double const raytrace_range(8);	   // whatever
+      double const weight(1); // whatever: documentation says 0<w<=1 but default is 25???
+      vector<unsigned char> static_data(cells_size_x * cells_size_y);
+      unsigned char const lethal_threshold(1);
+      shared_ptr<costmap_2d::Costmap2D>
+	cm2d(new costmap_2d::Costmap2D(cells_size_x,
+				       cells_size_y,
+				       costmap_resolution,
+				       origin_x,
+				       origin_y,
+				       costmap_inscribed_radius,
+				       costmap_circumscribed_radius,
+				       costmap_inflation_radius,
+				       obstacle_range,
+				       max_obstacle_height,
+				       raytrace_range,
+				       weight,
+				       static_data,
+				       lethal_threshold));
+      return mpglue::createCostmapper(cm2d);
+    }
+  };
+  
+  
+  class SFLFactory: public Factory {
+  public:
+    SFLFactory(): Factory("sfl") {}
+    virtual shared_ptr<mpglue::Costmapper> createCostmapper() {
+      static double const origin_x(-2);
+      static double const origin_y(3);
+      static double const resolution(0.05);
+      static double const inscribed_radius(0.2);
+      static double const circumscribed_radius(0.25);
+      static double const inflation_radius(0.3);
+      static mpglue::index_t const ix_end(100);
+      static mpglue::index_t const iy_end(75);
+      static double const origin_th(-0.032);
+      static mpglue::index_t const ix_begin(-10);
+      static mpglue::index_t const iy_begin(32);
+      static int const obstacle_cost(87);
+      return mpglue::createCostmapper(mpglue::sfl_costmapper_params(origin_x,
+								    origin_y,
+								    origin_th,
+								    resolution,
+								    inscribed_radius,
+								    circumscribed_radius,
+								    inflation_radius,
+								    ix_begin,
+								    iy_begin,
+								    ix_end,
+								    iy_end,
+								    obstacle_cost));
+    }
+  };
+  
+  
+  class CM2DFactory: public Factory {
+  public:
+    CM2DFactory(): Factory("cm2d") {}
+    virtual shared_ptr<mpglue::Costmapper> createCostmapper() {
+      static double const origin_x(-2);
+      static double const origin_y(3);
+      static double const resolution(0.05);
+      static double const inscribed_radius(0.2);
+      static double const circumscribed_radius(0.25);
+      static double const inflation_radius(0.3);
+      static mpglue::index_t const ix_end(100);
+      static mpglue::index_t const iy_end(75);
+      static double const obstacle_range(7);
+      static double const max_obstacle_height(3.2);
+      static double const raytrace_range(5.7);
+      static double const weight(4.9);
+      return mpglue::createCostmapper(mpglue::cm2d_costmapper_params(origin_x,
+								     origin_y,
+								     resolution,
+								     inscribed_radius,
+								     circumscribed_radius,
+								     inflation_radius,
+								     ix_end,
+								     iy_end,
+								     obstacle_range,
+								     max_obstacle_height,
+								     raytrace_range,
+								     weight));
+    }
+  };
+  
+  
+  class FactoryList {
+  public:
+    typedef list<Factory*> factory_list_t;
+    typedef factory_list_t::iterator iterator;
+    
+    FactoryList() {
+      factory_list.push_back(new LocalFactory());
+      factory_list.push_back(new SFLFactory());
+      factory_list.push_back(new CM2DFactory());
+    }
+    
+    ~FactoryList() {
+      for (iterator ii(begin()); ii != end(); ++ii)
+	delete *ii;
+    }
+    
+    inline iterator begin() { return factory_list.begin(); }
+    inline iterator end() { return factory_list.end(); }
+    
+    factory_list_t factory_list;
+  };
+  
+}
+
+using namespace mpglue_test;
+
+
+TEST (costmapper, factory)
+{
+  shared_ptr<Factory> factory;
+  factory.reset(new LocalFactory());
+  factory.reset(new SFLFactory());
+  factory.reset(new CM2DFactory());
+  FactoryList fl;
 }
 
 
 TEST (costmapper, creation)
 {
-  static double const origin_x(-2);
-  static double const origin_y(3);
-  static double const resolution(0.05);
-  static double const inscribed_radius(0.2);
-  static double const circumscribed_radius(0.25);
-  static double const inflation_radius(0.3);
-  static mpglue::index_t const ix_end(100);
-  static mpglue::index_t const iy_end(75);
-  {
-    mpglue::costmapper_params const pp(origin_x,
-				       origin_y,
-				       resolution,
-				       inscribed_radius,
-				       circumscribed_radius,
-				       inflation_radius,
-				       ix_end,
-				       iy_end);
-    shared_ptr<mpglue::Costmapper> cm(createCostmapper(&pp));
-    EXPECT_TRUE (cm) << "mpglue::createCostmap2D(mpglue::costmapper_params const &) failed";
-  }
-  {
-    static double const origin_th(-0.032);
-    static mpglue::index_t const ix_begin(-10);
-    static mpglue::index_t const iy_begin(32);
-    static int const obstacle_cost(87);
-    shared_ptr<mpglue::Costmapper>
-      cm(createCostmapper(mpglue::sfl_costmapper_params(origin_x,
-							origin_y,
-							origin_th,
-							resolution,
-							inscribed_radius,
-							circumscribed_radius,
-							inflation_radius,
-							ix_begin,
-							iy_begin,
-							ix_end,
-							iy_end,
-							obstacle_cost)));
-    EXPECT_TRUE (cm) << "mpglue::createCostmap2D(mpglue::sfl_costmapper_params const &) failed";
-  }
-  {
-    static double const obstacle_range(7);
-    static double const max_obstacle_height(3.2);
-    static double const raytrace_range(5.7);
-    static double const weight(4.9);
-    shared_ptr<mpglue::Costmapper>
-      cm(createCostmapper(mpglue::cm2d_costmapper_params(origin_x,
-							 origin_y,
-							 resolution,
-							 inscribed_radius,
-							 circumscribed_radius,
-							 inflation_radius,
-							 ix_end,
-							 iy_end,
-							 obstacle_range,
-							 max_obstacle_height,
-							 raytrace_range,
-							 weight)));
-    EXPECT_TRUE (cm) << "mpglue::createCostmap2D(mpglue::cm2d_costmapper_params const &) failed";
-  }
-  {
-    shared_ptr<costmap_2d::Costmap2D> cm2d(createCostmap2D());
-    EXPECT_TRUE (cm2d) << "in-test createCostmap2D() failed";
-    shared_ptr<mpglue::Costmapper> cm(mpglue::createCostmapper(cm2d));
-    EXPECT_TRUE (cm) << "mpglue::createCostmapper() failed on in-test createCostmap2D()";
+  FactoryList fl;
+  for (FactoryList::iterator ii(fl.begin()); ii != fl.end(); ++ii) {
+    Factory * factory(*ii);
+    ASSERT_TRUE (factory) << "NULL factory";
+    shared_ptr<mpglue::Costmapper> cm(factory->createCostmapper());
+    EXPECT_TRUE (cm) << "factory \"" << factory->name << "\" failed";
   }
 }
 
 
 TEST (costmapper, addition)
 {
-  shared_ptr<costmap_2d::Costmap2D> cm2d(createCostmap2D());
-  ASSERT_TRUE (cm2d);
-  shared_ptr<mpglue::Costmapper> cm(mpglue::createCostmapper(cm2d));
-  ASSERT_TRUE (cm);
   mpglue::index_collection_t added_obstacle_indices;
   added_obstacle_indices.insert(mpglue::index_pair(2, 3));
-  EXPECT_EQ (added_obstacle_indices.size(), size_t(1));
-  size_t const count(cm->updateObstacles(&added_obstacle_indices, 0, &cerr));
-  EXPECT_GT (count, size_t(0));
+  ASSERT_EQ (added_obstacle_indices.size(), size_t(1)) << "empty index_collection";
+  FactoryList fl;
+  for (FactoryList::iterator ii(fl.begin()); ii != fl.end(); ++ii) {
+    Factory * factory(*ii);
+    ASSERT_TRUE (factory) << "NULL factory";
+    shared_ptr<mpglue::Costmapper> cm(factory->createCostmapper());
+    EXPECT_TRUE (cm) << "factory \"" << factory->name << "\" failed";
+    if ( ! cm)
+      continue;
+    size_t const count(cm->updateObstacles(&added_obstacle_indices, 0, &cerr));
+    EXPECT_GT (count, size_t(0));
+  }
 }
 
 
