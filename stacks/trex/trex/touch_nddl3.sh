f952adebd68d78aeb100a19b7f7c1a4926743342
tpratkanis <tpratkanis@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-04 17:09:04 +0000

    Try to get svn to set execuatable permisions here.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@20642 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: c64904cd80775a9d9729eac64eada52694dcb3dd

--- stacks/trex/trex/touch_nddl3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#this hack shell script goes in and touches two files, nddl3.g and nddl3tree.g. This is done as a work around to a PLASMA build issue.
-echo "Touching the NDDL3*.g files."
-touch `rospack find trex`/PLASMA/src/PLASMA/NDDL/base/antlr/NDDL3.g 
-touch `rospack find trex`/PLASMA/src/PLASMA/NDDL/base/antlr/NDDL3Tree.g 
-
-
