df62dbe4c7830cb2a13df9c2ed27b67eeac9282e
danmuno <danmuno@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-04 01:20:22 +0000

    initial commit of ros node
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@20596 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 7b435b7ce750aa51c11a0ec536307e4ce17b69a0

---
+++ sandbox/functional_m3n_ros/src/m3n_learning_node.cpp
@@ -0,0 +1,211 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+
+#include <functional_m3n_ros/m3n_learning_node.h>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+
+M3NParams GLOBAL_m3n_params;
+
+M3NParams& getM3NParameters(unsigned int nbr_clique_sets)
+{
+  vector<float> robust_potts_params(nbr_clique_sets, -1.0);
+
+  // ----------------------------------------------
+  // Define learning parameters
+  RegressionTreeWrapperParams regression_tree_params;
+  regression_tree_params.max_tree_depth_factor = 0.2; // was 0.4
+  GLOBAL_m3n_params.setLearningRate(0.4);
+  GLOBAL_m3n_params.setNumberOfIterations(6);
+  GLOBAL_m3n_params.setRegressorRegressionTrees(regression_tree_params);
+  GLOBAL_m3n_params.setInferenceRobustPotts(robust_potts_params);
+
+  return GLOBAL_m3n_params;
+}
+
+using namespace std;
+using namespace robot_msgs;
+using namespace m3n;
+
+
+LearningNode::LearningNode()
+{
+  rf_creator_ = boost::shared_ptr<RandomFieldCreator>(new  RandomFieldCreator());
+
+  cloud_sub_ = n_.subscribe<PointCloud>("training_cloud",100,boost::bind(&LearningNode::cloudCallback, this, _1));
+
+  n_.param(std::string("~model_file_path"),model_file_path_,std::string("NONE"));
+  if(model_file_path_=="NONE")
+  {
+    ROS_ERROR("Model file path is not specified");
+    throw "ERR";
+  }
+
+  //model_pub_ = n_.advertise<ModelReference>("final_model_reference",100);
+
+  learn_svc_ = n_.advertiseService(std::string("learn"), &LearningNode::learn,this);
+
+  ROS_INFO("Learning node init done.");
+}
+
+
+
+void LearningNode::cloudCallback(const PointCloudConstPtr& the_cloud)
+{
+  ROS_INFO_STREAM("Got cloud "<< the_cloud->header.stamp << "@" << the_cloud->header.frame_id );
+  training_clouds_.push_back(the_cloud);
+}
+
+
+
+
+int getAnnotationsChannel(const robot_msgs::PointCloud& pt_cloud)
+{
+  int chan_annotation_idx=-1;
+  for(unsigned int iC=0;iC<pt_cloud.chan.size();iC++)
+  {
+
+    const robot_msgs::ChannelFloat32& chan = pt_cloud.chan[iC];
+
+    //if (chan.name == "predictions" || ( (  chan.name.at(0)=='a'  ) && (  chan.name.at(1)=='n'  ) && chan.name.at(2)=='n'  ) ))
+    if (( (  chan.name.at(0)=='a'  ) && (  chan.name.at(1)=='n'  ) && (  chan.name.at(2)=='n'  ) ))
+    {
+      chan_annotation_idx=iC;
+      break;
+    }
+  }
+  return chan_annotation_idx;
+}
+
+bool LearningNode::learn(functional_m3n_ros::Learn::Request  &req,
+                         functional_m3n_ros::Learn::Response &res )
+{
+    ROS_INFO("Received learning command, starting to learn");
+
+
+    boost::filesystem::path model_base_path(model_file_path_);
+    if(~boost::filesystem::exists(model_base_path))
+    {
+      boost::filesystem::create_directory(model_base_path);
+    }
+
+    boost::filesystem::path model_path=model_base_path /= req.model_name;
+    if(~boost::filesystem::exists(model_path))
+    {
+      boost::filesystem::create_directory(model_path);
+    }
+
+    boost::filesystem::path model_filename=model_path /= "f_m3n";
+
+    std::string model_filename_str=model_filename.string();
+
+    ROS_INFO_STREAM("Starting to learn model "<< model_filename_str);
+
+    int num_clouds=training_clouds_.size();
+
+    std::vector<boost::shared_ptr<RandomField> > rf_all;
+    rf_all.resize(num_clouds);
+    std::vector<const RandomField*> training_rfs;
+    training_rfs.resize(num_clouds);
+
+    int iCloud=0;
+    for(std::deque<robot_msgs::PointCloudConstPtr>::iterator cloud_it=training_clouds_.begin();cloud_it!=training_clouds_.end();cloud_it++,iCloud++)
+    {
+
+      ROS_INFO("Extracting cloud features");
+
+      int chan_annotation_idx=getAnnotationsChannel(*(*cloud_it));
+      if(chan_annotation_idx==-1)
+      {
+        ROS_ERROR("Annotations channel is missing");
+        iCloud--;
+        continue;
+      }
+      boost::shared_ptr<RandomField> rf=rf_creator_->createRandomField(*(*cloud_it),(*cloud_it)->chan[chan_annotation_idx].vals);
+      rf_all[iCloud]=rf; //mem management
+      training_rfs[iCloud]=rf.get(); //for training
+    }
+
+    ROS_INFO_STREAM("Extracted features from "<< iCloud <<" clouds. Total "<< num_clouds <<" clouds.");
+    rf_all.resize(iCloud);
+    training_rfs.resize(iCloud);
+
+
+    if(iCloud==0)
+    {
+      ROS_ERROR("No clouds to train");
+      return false;
+    }
+    // ----------------------------------------------------------
+    // Train M3N model
+    ROS_INFO("Starting to train...");
+    M3NModel m3n_model;
+
+    learning_parameters_ = getM3NParameters(training_rfs[0]->getNumberOfCliqueSets());
+
+    if (m3n_model.train(training_rfs, learning_parameters_) < 0)
+    {
+      ROS_ERROR("Failed to train M3N model");
+      return false;
+    }
+    ROS_INFO("Successfully trained M3n model");
+    m3n_model.saveToFile(model_filename_str);
+    
+
+    res.model_type="functional_m3n";
+    res.model_reference=model_filename_str;
+
+    return true;
+
+}
+
+
+
+
+/* ---[ */
+int
+  main (int argc, char** argv)
+{
+  ros::init (argc, argv, "m3n_learning");
+
+  LearningNode m3n_learning;
+
+  ros::spin();
+
+  return (0);
+}
+/* ]--- */
+
+
---
+++ sandbox/functional_m3n_ros/src/m3n_prediction_node.cpp
@@ -0,0 +1,178 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+
+#include <functional_m3n_ros/m3n_prediction_node.h>
+
+using namespace std;
+using namespace robot_msgs;
+using namespace m3n;
+
+#include "object_names/Float2Name.h"
+#include "object_names/Name2Color.h"
+
+
+PredictionNode::PredictionNode()
+{
+
+  rf_creator_ = boost::shared_ptr<RandomFieldCreator>(new  RandomFieldCreator());
+
+  cloud_sub_ = n_.subscribe<PointCloud>("cloud",100,boost::bind(&PredictionNode::cloudCallback, this, _1));
+
+  n_.param(std::string("~model_file"),model_file_name_,std::string("NONE"));
+  if(model_file_name_=="NONE")
+  {
+    ROS_ERROR("Model file is not specified");
+    throw "ERR";
+  }
+
+  n_.param( std::string("~use_color"), use_colors_, false);
+
+  cloud_pub_ = n_.advertise<PointCloud>("predictions_cloud",100);
+
+
+  if (m3n_model2.loadFromFile(model_file_name_) < 0)  
+  {
+    ROS_ERROR("couldnt load model");
+    throw "ERR";
+  }
+  ROS_INFO("Init done.");
+}
+
+
+
+void PredictionNode::cloudCallback(const PointCloudConstPtr& the_cloud)
+{
+
+    ROS_INFO("Received point cloud, starting detection");
+    //cloud_ = the_cloud;
+
+    boost::shared_ptr<RandomField> rf=rf_creator_->createRandomField(*the_cloud);
+
+
+    map<unsigned int, unsigned int> inferred_labels;
+    if (m3n_model2.infer(*rf, inferred_labels) < 0)
+    {
+      ROS_ERROR("could not do inference");
+      return;
+    }
+
+
+    PointCloud cloud_out=*the_cloud;
+
+    unsigned int num_in=the_cloud->pts.size();
+    unsigned int nC_in=the_cloud->chan.size();
+    unsigned int free_channel=the_cloud->chan.size();
+
+    int chan_RGB_id;
+    map<int, std_msgs::ColorRGBA> label_colors;
+    if(use_colors_)
+    {
+      chan_RGB_id=cloud_geometry::getChannelIndex(cloud_out,"rgb");
+      if(chan_RGB_id==-1)
+      {
+        chan_RGB_id = free_channel;
+       free_channel++;
+      }
+      const vector<unsigned int> labels=m3n_model2.getTrainingLabels();
+      for(int i=0;i<labels.size();i++)
+      {
+        object_names::Float2Name::Request req1;
+        object_names::Float2Name::Response resp1;
+        req1.id=labels[i];
+        
+        ros::service::call("float_to_name",req1,resp1);
+
+        object_names::Name2Color::Request req2;
+        object_names::Name2Color::Response resp2;
+        req2.name=resp1.name;
+        
+        ros::service::call("name_to_color",req2,resp2);
+
+        label_colors[labels[i]]=resp2.color;
+      }
+    }
+
+    unsigned int chan_predictions_id=free_channel;
+    free_channel++;
+    unsigned int nCout=free_channel;
+    cloud_out.chan.resize(nCout);
+
+    cloud_out.chan[chan_predictions_id].name="predicted_label";
+    cloud_out.chan[chan_predictions_id].vals.resize(num_in);
+
+    if(use_colors_ && chan_RGB_id>=nC_in)
+    {
+      cloud_out.chan[chan_RGB_id].name="rgb";
+      cloud_out.chan[chan_RGB_id].vals.resize(num_in);
+    }
+
+
+    for(unsigned int iPt=0;iPt<num_in;iPt++)
+    {
+      cloud_out.chan[chan_predictions_id].vals[iPt]=inferred_labels[iPt];
+
+      if(use_colors_)
+      {
+        const std_msgs::ColorRGBA& color=label_colors[inferred_labels[iPt]];
+        int r=int(round(color.r*255));
+        int g=int(round(color.g*255));
+        int b=int(round(color.b*255));
+        int rgb = (r << 16) | (g << 8) | b;
+        cloud_out.chan[chan_RGB_id].vals[iPt] = *reinterpret_cast<float*>(&rgb);
+
+      }
+    }        
+
+    cloud_pub_.publish(cloud_out);
+}
+
+
+
+
+/* ---[ */
+int
+  main (int argc, char** argv)
+{
+  ros::init (argc, argv, "m3n_prediction");
+
+
+  PredictionNode m3n_prediction;
+
+  ros::spin();
+
+  return (0);
+}
+/* ]--- */
+
