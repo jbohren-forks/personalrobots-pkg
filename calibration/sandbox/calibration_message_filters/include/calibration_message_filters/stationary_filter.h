fa5583c2073ca1d32d21322fa1056b4b7214f07e
vijaypradeep <vijaypradeep@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-04 22:12:11 +0000

    Forgot to add source file
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@20714 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 78d7d003a1cd0b9e87e4550309a11156fa1bca11

---
+++ calibration/sandbox/calibration_message_filters/include/calibration_message_filters/stationary_filter.h
@@ -0,0 +1,116 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+#ifndef CALIBRATION_MESSAGE_FILTERS_STATIONARY_FILTER_H_
+#define CALIBRATION_MESSAGE_FILTERS_STATIONARY_FILTER_H_
+
+#include "message_filters/simple_filter.h"
+
+#include "calibration_message_filters/stationary_elem.h"
+#include "calibration_message_filters/joint_tolerance.h"
+#include "mechanism_msgs/MechanismState.h"
+
+namespace calibration_message_filters
+{
+
+/**
+ * Given a 'SandwichElem' that is tagged with MechanismState, this filter determines
+ * if the element was stationary during the interval.  The callback is called only
+ * if the elem was in fact stationary.
+ */
+template <class M>
+class StationaryFilter
+{
+public:
+  typedef boost::shared_ptr< SandwichElem<M, mechanism_msgs::JointStates> > SandConstPtr;   // Shared pointer to the sandwich elem
+  typedef boost::function<void(const StationaryElem<M>&)> Callback;
+  typedef boost::signal<Callback> Signal;
+
+  StationaryFilter()
+  {
+
+  }
+
+  template<class F>
+  StationaryFilter(F& f, std::vector<JointTolerance> joint_tol)
+  {
+    connectInput(f);
+    setJointTolerances(joint_tol);
+  }
+
+  ~StationaryFilter()
+  {
+    incoming_connection_.disconnect();
+  }
+
+  void connectInput(F& f)
+  {
+    incoming_connection_ = f.connect(boost::bind(&StationaryFilter<M>::callback, this));
+  }
+
+  void registerCallback(const Callback& callback)
+  {
+    boost::mutex::scoped_lock lock(signal_mutex_);
+    return Connection(boost::bind(&StationaryFilter<M>::disconnect, this), signal_.connect(callback));
+  }
+
+  void setJointTolerances(std::vector<JointTolerance> joint_tol)
+  {
+    joint_tol_ = joint_tol;
+  }
+
+  void callback(const SandConstPtr& msg)
+  {
+
+
+  }
+
+private:
+  void disconnect(const Connection& c)
+  {
+    boost::mutex::scoped_lock lock(signal_mutex_);
+    c.getBoostConnection().disconnect();
+  }
+
+  std::vector<JointTolerance> joint_tol_;
+  boost::mutex data_mutex_;
+
+  Connection incoming_connection_;
+  Signal signal_;
+  boost::mutex signal_mutex_;
+};
+
+}
+
+#endif // CALIBRATION_MESSAGE_FILTERS_STATIONARY_FILTER_H_
---
+++ calibration/sandbox/calibration_message_filters/test/joint_states_deflater_unittest.cpp
@@ -0,0 +1,119 @@
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2008, Willow Garage, Inc.
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Willow Garage nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+
+#include <gtest/gtest.h>
+
+#include "calibration_message_filters/joint_states_deflater.h"
+
+using namespace std;
+using namespace calibration_message_filters;
+using namespace mechanism_msgs;
+
+
+static const double eps = 1e-10;
+
+TEST(JointStatesDeflator, easy1)
+{
+  JointStatesDeflater deflater;
+
+  vector<string> joint_names;
+  joint_names.resize(2);
+  joint_names[0] = "A";
+  joint_names[1] = "C";
+  deflater.setDeflationJointNames(joint_names);
+
+  JointStatesPtr joint_states;
+  DeflatedMsg<JointStates> deflated_msg;
+
+  // ***********************************************
+
+  joint_states.reset(new JointStates);
+  joint_states->joints.resize(3);
+  joint_states->joints[0].name = "A";
+  joint_states->joints[1].name = "B";
+  joint_states->joints[2].name = "C";
+  joint_states->joints[0].position = 1.0;
+  joint_states->joints[1].position = 2.0;
+  joint_states->joints[2].position = 3.0;
+
+  deflater.deflate(joint_states, deflated_msg);
+
+  EXPECT_EQ(deflated_msg.deflated_.size(), (unsigned int) 2);
+  EXPECT_NEAR(deflated_msg.deflated_[0], 1.0, eps);
+  EXPECT_NEAR(deflated_msg.deflated_[1], 3.0, eps);
+
+  // ***********************************************
+
+  joint_states.reset(new JointStates);
+  joint_states->joints.resize(3);
+  joint_states->joints[0].name = "C";
+  joint_states->joints[1].name = "A";
+  joint_states->joints[2].name = "B";
+  joint_states->joints[0].position = 4.0;
+  joint_states->joints[1].position = 5.0;
+  joint_states->joints[2].position = 6.0;
+
+  deflater.deflate(joint_states, deflated_msg);
+
+  EXPECT_EQ(deflated_msg.deflated_.size(), (unsigned int) 2);
+  EXPECT_NEAR(deflated_msg.deflated_[0], 5.0, eps);
+  EXPECT_NEAR(deflated_msg.deflated_[1], 4.0, eps);
+
+  // ***********************************************
+
+  joint_states.reset(new JointStates);
+  joint_states->joints.resize(4);
+  joint_states->joints[0].name = "D";
+  joint_states->joints[1].name = "C";
+  joint_states->joints[2].name = "B";
+  joint_states->joints[3].name = "A";
+  joint_states->joints[0].position = 7.0;
+  joint_states->joints[1].position = 8.0;
+  joint_states->joints[2].position = 9.0;
+  joint_states->joints[3].position = 10.0;
+
+  deflater.deflate(joint_states, deflated_msg);
+
+  EXPECT_EQ(deflated_msg.deflated_.size(), (unsigned int) 2);
+  EXPECT_NEAR(deflated_msg.deflated_[0], 10.0, eps);
+  EXPECT_NEAR(deflated_msg.deflated_[1], 8.0, eps);
+
+}
+
+
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
