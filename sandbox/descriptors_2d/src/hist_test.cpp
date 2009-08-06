a94c2fb0c9d69087573df5a0786b97c2a083ad9c
alexteichman <alexteichman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-06 18:50:40 +0000

    Adding an rgb hist test
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@20919 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 71916850ca7f8569c32b63dd4319e337d7c2e2d7

--- sandbox/descriptors_2d/src/hist_test.cpp
+++ sandbox/descriptors_2d/src/hist_test.cpp
@@ -6,6 +6,37 @@
 
 using namespace std;
 
+
+void rgbTest(IplImage* img) {
+  // -- Get RGB
+  IplImage* r = cvCreateImage( cvGetSize(img), 8, 1 );
+  IplImage* g = cvCreateImage( cvGetSize(img), 8, 1 );
+  IplImage* b = cvCreateImage( cvGetSize(img), 8, 1 );
+  cvSplit(img, b, g, r, 0);
+
+  // -- Setup hist.
+  int num_bins = 8;
+  float range[] = {0, 255}; 
+  float* ranges[] = {range, range};
+  int sizes[] = {num_bins, num_bins};
+  CvHistogram* hist = cvCreateHist(2, sizes, CV_HIST_ARRAY, ranges, 1);
+  IplImage* imgs[] = {r, g};
+
+  // -- Compute.
+  cvCalcHist(imgs, hist, 0, NULL);
+
+  // -- Output.
+  cout << "RGB" << endl;
+  for(int i=0; i<num_bins; i++) {
+    for(int j=0; j<num_bins; j++) {
+      cout << cvQueryHistValue_2D(hist, i, j) << " ";
+    }
+  }
+  cout << endl;
+}
+  
+
+
 int main(int argc, char** argv) {
   // -- Get img.
   if(argc == 1) { 
@@ -28,18 +59,18 @@ int main(int argc, char** argv) {
 
   // -- Setup hist.
   int num_bins = 8;
-  float huerange[] = {0, 180}; //hsv
-  float satrange[] = {0, 255}; //hsv
-  float* ranges[] = {huerange, satrange}; //hue and sat.
+  float huerange[] = {0, 180}; 
+  float satrange[] = {0, 255}; 
+  float* ranges[] = {huerange, satrange};
   int sizes[] = {num_bins, num_bins};
   CvHistogram* hist = cvCreateHist(2, sizes, CV_HIST_ARRAY, ranges, 1);
   IplImage* imgs[] = {hue, sat};
 
   // -- Compute.
-  cvCalcHist(imgs, hist, 0, 0);
+  cvCalcHist(imgs, hist, 0, NULL);
 
   // -- Output.
-  cout << "cv hist (before norm): " << endl;
+  cout << "HSV" << endl;
   for(int i=0; i<num_bins; i++) {
     for(int j=0; j<num_bins; j++) {
       cout << cvQueryHistValue_2D(hist, i, j) << " ";
@@ -47,4 +78,8 @@ int main(int argc, char** argv) {
   }
   cout << endl;
 
+  rgbTest(img);
+
+  return 0;
 }
+
