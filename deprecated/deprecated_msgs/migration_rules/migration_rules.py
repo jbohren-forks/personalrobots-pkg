2bee0995c8a2c6b0dd6771102e25ff5de1eadbbc
jleibs <jleibs@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-08 17:19:58 +0000

    Adding VOPose migration rule.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@21172 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 1af99f0c80a20ea98b1c5eef584bb88841aa1158

---
+++ deprecated/deprecated_msgs/migration_rules/migration_rules.py
@@ -0,0 +1,95 @@
+class update_robot_msgs_VOPose_0b686775e7d79f53073ac33f28f73022(MessageUpdateRule):
+	old_type = "robot_msgs/VOPose"
+	old_full_text = """
+Header header
+robot_msgs/Pose pose
+int32 inliers
+
+================================================================================
+MSG: roslib/Header
+#Standard metadata for higher-level flow data types
+#sequence ID: consecutively increasing ID 
+uint32 seq
+#Two-integer timestamp that is expressed as:
+# * stamp.secs: seconds (stamp_secs) since epoch
+# * stamp.nsecs: nanoseconds since stamp_secs
+# time-handling sugar is provided by the client library
+time stamp
+#Frame this data is associated with
+# 0: no frame
+# 1: global frame
+string frame_id
+
+================================================================================
+MSG: robot_msgs/Pose
+Point position
+Quaternion orientation
+
+================================================================================
+MSG: robot_msgs/Point
+float64 x
+float64 y
+float64 z
+
+================================================================================
+MSG: robot_msgs/Quaternion
+# xyz - vector rotation axis, w - scalar term (cos(ang/2))
+float64 x
+float64 y
+float64 z
+float64 w
+"""
+
+	new_type = "deprecated_msgs/VOPose"
+	new_full_text = """
+Header header
+geometry_msgs/Pose pose
+int32 inliers
+
+================================================================================
+MSG: roslib/Header
+#Standard metadata for higher-level flow data types
+#sequence ID: consecutively increasing ID 
+uint32 seq
+#Two-integer timestamp that is expressed as:
+# * stamp.secs: seconds (stamp_secs) since epoch
+# * stamp.nsecs: nanoseconds since stamp_secs
+# time-handling sugar is provided by the client library
+time stamp
+#Frame this data is associated with
+# 0: no frame
+# 1: global frame
+string frame_id
+
+================================================================================
+MSG: geometry_msgs/Pose
+Point position
+Quaternion orientation
+
+================================================================================
+MSG: geometry_msgs/Point
+float64 x
+float64 y
+float64 z
+
+================================================================================
+MSG: geometry_msgs/Quaternion
+# xyz - vector rotation axis, w - scalar term (cos(ang/2))
+float64 x
+float64 y
+float64 z
+float64 w
+"""
+
+	order = 0
+	migrated_types = [
+		("Header","Header"),
+                ("Pose", "geometry_msgs/Pose")]
+
+	valid = False
+
+	def update(self, old_msg, new_msg):
+		self.migrate(old_msg.header, new_msg.header)
+                self.migrate(old_msg.pose, new_msg.pose)
+		new_msg.inliers = old_msg.inliers
+
