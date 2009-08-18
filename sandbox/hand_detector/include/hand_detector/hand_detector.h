e8f188cc49b48a6d41c3ae64a8e164169e4fe23e
alexteichman <alexteichman@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-18 01:46:49 +0000

    Adding functions to test Romains hand dataset.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@22092 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 43611892ac1ce2c4176a9c6468ec40fa672e8d12

--- sandbox/hand_detector/include/hand_detector/hand_detector.h
+++ sandbox/hand_detector/include/hand_detector/hand_detector.h
@@ -11,30 +11,33 @@
 
 class HandDetector {
  public:
+  string experiment_;
+  
   HandDetector();
-  HandDetector(std::string classifier_filename);
-  //! Collect features on the even images.
-  DorylusDataset* collectDataset(std::string results_dir, size_t num_samples);
+  //! Collect features on the even images from polygons.
+  DorylusDataset* collectDatasetFromPolygons(std::string results_dir, size_t num_samples);
+  //! Collect features on the even images from directories of positive and negative examples.
+  //! @param only_odd If null, get all data.  If true, only get data from odd numbered images.
+  DorylusDataset* collectDatasetFromDirs(std::string positives_dir, std::string negatives_dir, size_t num_samples, bool* only_odd = NULL);
   //! Train a boosting classifier. 
   Dorylus* train(std::string dataset_filename, int max_secs=0, int max_wcs=2000, int num_candidates=10);
-  //! Get results on the odd images.
-  void test(std::string results_dir);
-  //! Visualize results on the odd images.
-  void visualize(std::string classifier_filename, std::string results_dir, size_t num_samples);
+  //! Get classification results on the odd images.
+  void test(std::string classifier_filename, std::string results_dir, size_t num_samples);
+  //! Get classification results on the odd images for aligned 128x128 data.
+  void testOnDirs(std::string classifier_filename, std::string positives_dir, std::string negatives_dir, size_t num_samples);
   void showLabels(std::string results_dir);
   //! Sets the HandDetector debug_ flag as well as debugging flags for all the descriptors.
   void setDebug(bool debug);
-
+  
  private:
   //! Boosting classifier.
   Dorylus d_;
   vector<ImageDescriptor*> descriptors_;
   bool debug_; 
- 
   
+  //! @param plabel If not null, forces the label of all features collected to be the value of *plabel.  polys can be empty in this case.
   void collectRandomFeatures(IplImage* img, int num_samples, const cv::Vector< cv::Vector<cv::Point> >& polys, 
-			     cv::Vector<cv::KeyPoint>* keypoints, std::vector<object*>* objects);
-  
+			     cv::Vector<cv::KeyPoint>* keypoints, std::vector<object*>* objects, int* plabel = NULL);
 };
 
 std::vector<ImageDescriptor*> setupImageDescriptors();
