faaa49be019fd7d6f1e4a91f05235a46a25b6bc6
sachinchitta <sachinchitta@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-19 01:00:12 +0000

    changed door tracker to node handle, added a service to find the nearest door in the database
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@22202 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 17e7a0dffc773321293d35e580b925ac20c73d57

--- mapping/door_tracker/src/door_database.cpp
+++ mapping/door_tracker/src/door_database.cpp
@@ -45,19 +45,16 @@
 #include <ros/node.h>
 // ROS messages
 
-#include <geometry_msgs/Point32.h>
+#include <geometry_msgs/Point.h>
 #include <door_msgs/Door.h>
 #include <visualization_msgs/Marker.h>
-
-#include <tf/transform_listener.h>
-#include <tf/message_notifier.h>
-
 #include <angles/angles.h>
 
 #include <sys/time.h>
 
 #include <door_tracker/DoorDatabaseObject.h>
 #include <door_tracker/CoeffArray.h>
+#include <door_tracker/DoorQuery.h>
 
 #include <Eigen/Array>
 #include <Eigen/SVD>
@@ -72,11 +69,7 @@ class DoorDatabase
 {
   public:
 
-    ros::Node *node_;
-
-    tf::MessageNotifier<door_msgs::Door>* message_notifier_;
-
-    tf::TransformListener *tf_;
+    ros::NodeHandle node_handle_;
 
     door_msgs::Door door_msg_;
 
@@ -84,52 +77,56 @@ class DoorDatabase
 
     std::vector<DoorDatabaseObject> database_;
 
+    ros::Publisher viz_pub_;
+
+    ros::Subscriber door_sub_;
+
+    ros::ServiceServer door_srv_; 
+
     int hinge_number_;
 
     /*** Parameters to be updated from the param server ***/
     std::string door_msg_topic_;
+    std::string door_service_name_;
     std::string door_database_frame_;
+
     int min_angles_per_door_;
     double angle_difference_threshold_;
     double door_point_distance_threshold_;
 
-    DoorDatabase():message_notifier_(NULL)
+    DoorDatabase()
     {
-      node_ = ros::Node::instance();
-      tf_ = new tf::TransformListener(*node_);
+      node_handle_.param<std::string>("~p_door_msg_topic_", door_msg_topic_,"/door_tracker_node/door_message");
+      node_handle_.param<std::string>("~door_database_frame", door_database_frame_,"map");
+      node_handle_.param<int>("~p_min_angles_per_door",min_angles_per_door_, 4);
+      node_handle_.param<double>("~p_angle_difference_threshold",angle_difference_threshold_,M_PI/12.0);
+      node_handle_.param<double>("~p_door_point_distance_threshold",door_point_distance_threshold_,0.25);
 
-      node_->param<std::string>("~p_door_msg_topic_", door_msg_topic_,"/door_tracker_node/door_message");
-      node_->param<std::string>("~door_database_frame", door_database_frame_,"map");
-      node_->param<int>("~p_min_angles_per_door",min_angles_per_door_, 4);
-      node_->param<double>("~p_angle_difference_threshold",angle_difference_threshold_,M_PI/12.0);
-      node_->param<double>("~p_door_point_distance_threshold",door_point_distance_threshold_,0.25);
-
-      node_->advertise<visualization_msgs::Marker>( "visualization_marker", 0 );
+      viz_pub_ = node_handle_.advertise<visualization_msgs::Marker>( "visualization_marker",1);
 
       double tmp; int tmp2;
 
-      node_->param("~p_door_frame_p1_x", tmp, 0.5); door_msg_.frame_p1.x = tmp;
-      node_->param("~p_door_frame_p1_y", tmp, -0.5); door_msg_.frame_p1.y = tmp;
-      node_->param("~p_door_frame_p2_x", tmp, 0.5); door_msg_.frame_p2.x = tmp;
-      node_->param("~p_door_frame_p2_y", tmp, 0.5); door_msg_.frame_p2.y = tmp;
-      node_->param("~p_door_hinge" , tmp2, -1); door_msg_.hinge = tmp2;
-      node_->param("~p_door_rot_dir" , tmp2, -1); door_msg_.rot_dir = tmp2;
-      door_msg_.header.frame_id = "base_link";
+      node_handle_.param("~p_door_frame_p1_x", tmp, 0.5); door_msg_.frame_p1.x = tmp;
+      node_handle_.param("~p_door_frame_p1_y", tmp, -0.5); door_msg_.frame_p1.y = tmp;
+      node_handle_.param("~p_door_frame_p2_x", tmp, 0.5); door_msg_.frame_p2.x = tmp;
+      node_handle_.param("~p_door_frame_p2_y", tmp, 0.5); door_msg_.frame_p2.y = tmp;
+      node_handle_.param("~p_door_hinge" , tmp2, -1); door_msg_.hinge = tmp2;
+      node_handle_.param("~p_door_rot_dir" , tmp2, -1); door_msg_.rot_dir = tmp2;
+      node_handle_.param<std::string>("~door_service_name", door_service_name_,"~door_service");
 
-      node_->subscribe(door_msg_topic_,door_msg_, &DoorDatabase::doorMsgCallBack,this,1);
-//    message_notifier_ = new tf::MessageNotifier<door_msgs::Door> (tf_, node_,  boost::bind(&DoorDatabase::doorMsgCallBack, this, _1), door_msg_topic_.c_str (), door_database_frame_, 1);
+      door_msg_.header.frame_id = "base_link";
+      door_sub_ = node_handle_.subscribe(door_msg_topic_,1,&DoorDatabase::doorMsgCallBack,this);
+      door_srv_ = node_handle_.advertiseService(door_service_name_,&DoorDatabase::doorQuery,this);
       hinge_number_ = 0;
     };
 
     ~DoorDatabase()
     {
-      node_->unadvertise("visualization_marker");
-      delete message_notifier_;
     }
 
-//    void doorMsgCallBack(const tf::MessageNotifier<door_msgs::Door>::MessagePtr& door)
-    void doorMsgCallBack()
+    void doorMsgCallBack(const door_msgs::DoorConstPtr &door_msg_in)
     {
+      door_msgs::Door door_msg = *door_msg_in;
       updateDatabase(door_msg_);
     }
 
@@ -154,7 +151,7 @@ class DoorDatabase
         lhs(i,0) = db.coeff[i].data[0]*cos(db.angles[i])-db.coeff[i].data[0]+db.coeff[i].data[1]*sin(db.angles[i]);
         lhs(i,1) = -db.coeff[i].data[0]*sin(db.angles[i])-db.coeff[i].data[1]+db.coeff[i].data[1]*cos(db.angles[i]);
         lhs(i,2) = -x1*db.coeff[i].data[0]*cos(db.angles[i]) + y1*db.coeff[i].data[0]*sin(db.angles[i])-y1*db.coeff[i].data[1]*cos(db.angles[i])-x1*db.coeff[i].data[1]*sin(db.angles[i]);
-        ROS_INFO(" Matrix elements are: %f %f %f %f",lhs(i,0),lhs(i,1),lhs(i,2),db.angles[i]);
+        ROS_DEBUG(" Matrix elements are: %f %f %f %f",lhs(i,0),lhs(i,1),lhs(i,2),db.angles[i]);
       }
 
       VectorXf rhs;
@@ -165,13 +162,13 @@ class DoorDatabase
       }
 
       VectorXf sol = VectorXf::Zero(3);
-      ROS_INFO("lhs is a %d x 3 matrix, rhs is a %d vector",(int) db.angles.size(), (int) db.angles.size());
+      ROS_DEBUG("lhs is a %d x 3 matrix, rhs is a %d vector",(int) db.angles.size(), (int) db.angles.size());
       lhs.svd().solve(rhs,&sol);
       hinge.x = sol(0);
       hinge.y = sol(1);
       hinge.z = 0;
 
-      ROS_INFO("Setting hinge at: %f %f %f",hinge.x,hinge.y,hinge.z);
+      ROS_DEBUG("Setting hinge at: %f %f %f",hinge.x,hinge.y,hinge.z);
       publishPoint(hinge,HINGE_POINT_CONST+hinge_number_,db.door.header.frame_id);
       hinge_number_++;
       return hinge;
@@ -204,7 +201,7 @@ class DoorDatabase
 
       int index = database.size()-1;
       database[index].door = door;
-      database[index].door.hinge = 0;
+      database[index].door.hinge = door_msgs::Door::HINGE_P1;
 
       double dx = door.door_p2.x - door.door_p1.x;
       double dy = door.door_p2.y - door.door_p1.y;
@@ -213,10 +210,9 @@ class DoorDatabase
       database[index].set_angles_size(1);
       database[index].angles[0] = door_angle;
       database[index].set_coeff_size(1);
-      database[index].coeff[0];
       database[index].coeff[0] = generateLinearCoeff(door.door_p1,door.door_p2);
-      ROS_INFO("Adding new door to database: p1: %f, %f, %f, p2: %f, %f, %f",door.door_p1.x,door.door_p1.y,door.door_p1.z,door.door_p2.x,door.door_p2.y,door.door_p2.z);
-      ROS_INFO("There are %d candidate doors in the database now.",index+1);
+      ROS_DEBUG("Adding new door to database: p1: %f, %f, %f, p2: %f, %f, %f",door.door_p1.x,door.door_p1.y,door.door_p1.z,door.door_p2.x,door.door_p2.y,door.door_p2.z);
+      ROS_DEBUG("There are %d candidate doors in the database now.",index+1);
     }
 
     double distance(const geometry_msgs::Point32 &d1, const geometry_msgs::Point32 &d2)
@@ -230,13 +226,12 @@ class DoorDatabase
       double dx = door.door_p2.x - door.door_p1.x;
       double dy = door.door_p2.y - door.door_p1.y;
 
-      if(index_p2 == 1)
+      if(index_p2 == door_msgs::Door::HINGE_P2)
       {
         dx = -dx;
         dy = -dy;
       }
       double new_door_angle = atan2(dy,dx);
-
 
       bool different_angle = true;
 
@@ -266,14 +261,14 @@ class DoorDatabase
         }
 
         db.angles.resize(db.angles.size()+1);
-        ROS_INFO("Door has a different angle: %f \nNumber of angles for door is now %d", new_door_angle, (int) db.angles.size());
+        ROS_DEBUG("Door has a different angle: %f \nNumber of angles for door is now %d", new_door_angle, (int) db.angles.size());
         db.coeff.resize(db.coeff.size()+1);
         db.angles[db.angles.size()-1] = new_door_angle;
         db.coeff[db.coeff.size()-1] = generateLinearCoeff(door.door_p1,door.door_p2);
         if((int) db.angles.size() == min_angles_per_door_)
         {
           db.door.frame_p1 = findHingePosition(db);
-          db.door.hinge = 0;
+          db.door.hinge = door_msgs::Door::HINGE_P1;
 //          publishDoors();
         }
       }
@@ -317,7 +312,7 @@ class DoorDatabase
       marker.color.g = 1.0;
 //      ROS_DEBUG("Publishing line between: p1: %f %f %f, p2: %f %f %f",marker.points[0].x,marker.points[0].y,marker.points[0].z,marker.points[1].x,marker.points[1].y,marker.points[1].z);
 
-      node_->publish( "visualization_marker", marker );
+      viz_pub_.publish(marker);
     }
 
     void publishDoors()
@@ -327,7 +322,7 @@ class DoorDatabase
         if((int) database_[i].angles.size() == min_angles_per_door_)
         {
           publishPoint(database_[i].door.frame_p1,i+HINGE_POINT_CONST,database_[i].door.header.frame_id);
-          ROS_INFO("Publishing hinge point (%f,%f,%f) in frame id %s",database_[i].door.frame_p1.x,database_[i].door.frame_p1.y,database_[i].door.frame_p1.z,database_[i].door.header.frame_id.c_str());
+          ROS_DEBUG("Publishing hinge point (%f,%f,%f) in frame id %s",database_[i].door.frame_p1.x,database_[i].door.frame_p1.y,database_[i].door.frame_p1.z,database_[i].door.header.frame_id.c_str());
         }
       }
     }
@@ -336,49 +331,71 @@ class DoorDatabase
     {
       if(distance(door1.door_p1,door2.door_p1) < door_point_distance_threshold_)
       {
-        hinge_index_p1 = 0;
-        hinge_index_p2 = 0;
+        hinge_index_p1 = door_msgs::Door::HINGE_P1;
+        hinge_index_p2 = door_msgs::Door::HINGE_P1;
         return true;
       }
       else if(distance(door1.door_p1,door2.door_p2) < door_point_distance_threshold_)
       {
-        hinge_index_p1 = 0;
-        hinge_index_p2 = 1;
+        hinge_index_p1 = door_msgs::Door::HINGE_P1;
+        hinge_index_p2 = door_msgs::Door::HINGE_P2;
         return true;
       }
       else if(distance(door1.door_p2,door2.door_p1) < door_point_distance_threshold_)
       {
-        hinge_index_p1 = 1;
-        hinge_index_p2 = 0;
+        hinge_index_p1 = door_msgs::Door::HINGE_P2;
+        hinge_index_p2 = door_msgs::Door::HINGE_P1;
         return true;
       }
       else if(distance(door1.door_p2,door2.door_p2) < door_point_distance_threshold_)
       {
-        hinge_index_p1 = 1;
-        hinge_index_p2 = 1;
+        hinge_index_p1 = door_msgs::Door::HINGE_P2;
+        hinge_index_p2 = door_msgs::Door::HINGE_P2;
         return true;
       }
       return false;
     }
 
+    bool findNearestDoor(const door_msgs::Door &door_in, door_msgs::Door &door)
+    {   
+      int h1,h2;
+      if(database_.empty())
+        return false;
+      std::vector<std::pair<double,int> > distance_pair;
+      for(int i=0; i < (int) database_.size(); i++)
+      {
+        distance_pair.push_back(std::make_pair(distance(database_[i].door.frame_p1,door_in.frame_p1),i));
+        distance_pair.push_back(std::make_pair(distance(database_[i].door.frame_p1,door_in.frame_p2),i));
+        distance_pair.push_back(std::make_pair(distance(database_[i].door.frame_p2,door_in.frame_p1),i));
+        distance_pair.push_back(std::make_pair(distance(database_[i].door.frame_p2,door_in.frame_p2),i));
+      }
+      std::sort(distance_pair.begin(),distance_pair.end());
+      if(sameDoor(door_in,database_[0].door,h1,h2))
+      {
+        door = database_[0].door;
+        return true;
+      }
+      return false;
+    }
 
+    bool doorQuery(door_tracker::DoorQuery::Request &request, door_tracker::DoorQuery::Response &response)
+    {
+      door_msgs::Door door;
+      if(findNearestDoor(request.door,door))
+      {
+        response.door = door;
+        return true;
+      }
+      return false;
+    }
 };
 
 
-/* ---[ */
-int
-  main (int argc, char** argv)
+int main (int argc, char** argv)
 {
-  ros::init (argc, argv);
-
-  ros::Node n("door_database");
-
+  ros::init (argc, argv, "door_database");
   DoorDatabase p;
-
   ROS_INFO("Waiting for tracker to finish");
-
-  n.spin ();
+  ros::spin();
   return (0);
 }
-/* ]--- */
-
