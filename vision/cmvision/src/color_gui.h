289824f525336eb225d72796c0faec78cfd8653d
natepak <natepak@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-19 23:02:45 +0000

    Color selection GUI for creating a color.txt file for cmvision is now working
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@22348 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: c26788a6ada50a704159940bd166b63b3f9f910e

--- vision/cmvision/src/color_gui.h
+++ vision/cmvision/src/color_gui.h
@@ -7,6 +7,18 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
+#include <FL/Fl_Value_Output.H>
+#include <FL/Fl_Output.H>
+#include <FL/Fl_Button.H>
+
+#include "opencv/cv.h"
+
+#include "cmvision.h"
+
+namespace boost
+{
+  class mutex;
+}
 
 class ColorGui : public Fl_Window
 {
@@ -22,9 +34,35 @@ class ColorGui : public Fl_Window
   /// \brief The image callback
   public: void imageCB(const sensor_msgs::ImageConstPtr& msg);
 
+  /// \brief Handle a mouse click
+  public:void HandleMouseRelease();
+
+  /// \brief Handle events
+  public: int handle(int event);
+
+  /// \brief Reset the color selection
+  public: static void ResetCB( Fl_Widget *w, void *data);
+
   private: ros::NodeHandle *nodeHandle;
+  private: ros::Subscriber subscriber;
+  private: int mousePosX, mousePosY;
 
   private: Fl_Box *imageBox;
+
+  private: int width, height;
+  private: unsigned char *rgbImage;
+  private: unsigned char *uyvyImage;
+
+  private: Fl_Value_Output *rgbOutput[3];
+  private: Fl_Value_Output *yuvOutput[3];
+
+  private: Fl_Button *resetButton;
+
+  private: Fl_Output *yuvRangeOutput[3];
+
+  private: boost::mutex *mutex;
+
+  private: CMVision *vision;
 };
 
 #endif
