0b580b468c225606cba1bc82a0e96a90f9e82cfa
mmwise <mmwise@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-20 02:17:32 +0000

    adding a callback based tutorial
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@22391 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 6782cb2752a5b3f9b5e1607f6c4297537e9eb4e6

--- stacks/common/actionlib_tutorials/src/actionlib_tutorials/gen_number.py
+++ stacks/common/actionlib_tutorials/src/actionlib_tutorials/gen_number.py
@@ -9,7 +9,7 @@ def gen_number():
 
     while not rospy.is_shutdown():
         pub.publish(Float32(random.normalvariate(5, 1)))
-        rospy.sleep(1.0)
+        rospy.sleep(0.5)
 
 if __name__ == '__main__':
   try:
