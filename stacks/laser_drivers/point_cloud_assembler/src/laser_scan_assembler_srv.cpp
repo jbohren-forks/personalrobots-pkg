ab9977544463f3bf3828aae744f590922c71649a
tfoote <tfoote@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-20 17:33:51 +0000

    fixing for filters API change
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/branches/filters_dynamic_loading@22434 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 5fe96d9b530cf316fee49fd26f0eb3b165ecd23e

--- stacks/laser_drivers/point_cloud_assembler/src/laser_scan_assembler_srv.cpp
+++ stacks/laser_drivers/point_cloud_assembler/src/laser_scan_assembler_srv.cpp
@@ -61,7 +61,7 @@ namespace point_cloud_assembler
 class LaserScanAssemblerSrv : public BaseAssemblerSrv<sensor_msgs::LaserScan>
 {
 public:
-  LaserScanAssemblerSrv() : BaseAssemblerSrv<sensor_msgs::LaserScan>("laser_scan_assembler")
+  LaserScanAssemblerSrv() : BaseAssemblerSrv<sensor_msgs::LaserScan>("laser_scan_assembler"), filter_chain_("filters", "filters::FilterBase<sensor_msgs::LaserScan>")
   {
     // ***** Set Laser Projection Method *****
     ros::Node::instance()->param("~ignore_laser_skew", ignore_laser_skew_, true) ;
@@ -70,7 +70,7 @@ public:
     std::string filter_xml;
     node_.param("~filters", filter_xml, std::string("<filters><!--Filter Parameter Not Set--></filters>"));
     ROS_DEBUG("Got parameter'~filters' as: %s\n", filter_xml.c_str());
-    filter_chain_.configureFromXMLString(1, filter_xml);
+    filter_chain_.configureFromXMLString(filter_xml);
   }
 
   ~LaserScanAssemblerSrv()
