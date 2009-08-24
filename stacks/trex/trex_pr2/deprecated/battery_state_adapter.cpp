279324dfe9e7598335e6f7ee91c6491133eef0cd
wg_cmeyers <wg_cmeyers@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-24 19:13:27 +0000

    Changed BatteryState to PowerState
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@22728 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 6059dacc0cb1d17430e49778aa340dbb4536654e

--- stacks/trex/trex_pr2/deprecated/battery_state_adapter.cpp
+++ stacks/trex/trex_pr2/deprecated/battery_state_adapter.cpp
@@ -1,21 +1,21 @@
 #include "trex_ros/ros_state_adapter.h"
 #include "Domains.hh"
-#include <pr2_msgs/BatteryState.h>
+#include <pr2_msgs/PowerState.h>
 
 namespace TREX {
 
-  class BatteryStateAdapter: public ROSStateAdapter<pr2_msgs::BatteryState> {
+  class BatteryStateAdapter: public ROSStateAdapter<pr2_msgs::PowerState> {
   public:
     BatteryStateAdapter(const LabelStr& agentName, const TiXmlElement& configData)
-      : ROSStateAdapter<pr2_msgs::BatteryState> ( agentName, configData) {
+      : ROSStateAdapter<pr2_msgs::PowerState> ( agentName, configData) {
     }
 
     virtual ~BatteryStateAdapter(){}
 
   private:
     void fillObservationParameters(ObservationByValue* obs){
-      obs->push_back("energy_capacity", new IntervalDomain(stateMsg.energy_capacity));
-      obs->push_back("energy_remaining", new IntervalDomain(std::min(stateMsg.energy_remaining, stateMsg.energy_capacity)));
+      obs->push_back("time_remaining", new IntervalDomain(stateMsg.time_remaining));
+      obs->push_back("AC_present", new IntervalDomain(stateMsg.AC_present));
       obs->push_back("power_consumption",new IntervalDomain(stateMsg.power_consumption));
     }
   };
