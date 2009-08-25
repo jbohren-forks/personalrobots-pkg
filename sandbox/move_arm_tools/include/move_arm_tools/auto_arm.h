9c75e21bf63126b41fc20818e9341d91941958d3
bcohen1 <bcohen1@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-25 10:32:11 +0000

    first commit of move_arm_tools. it contains a modified of arm_cmd_line that allows for unsafe arm movement and it also contains a python gui
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@22824 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: acd0707cb84c73583ac1343c2675df085b9fe3cd

---
+++ sandbox/move_arm_tools/include/move_arm_tools/auto_arm.h
@@ -0,0 +1,109 @@
+
+
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <move_arm/MoveArmAction.h>
+#include <move_arm/ActuateGripperAction.h>
+
+#include <mapping_msgs/AttachedObject.h>
+
+#include <planning_environment/monitors/kinematic_model_state_monitor.h>
+#include <motion_planning_msgs/KinematicPath.h>
+#include <manipulation_msgs/JointTraj.h>
+#include <manipulation_srvs/IKService.h>
+#include <pr2_mechanism_controllers/TrajectoryStart.h>
+
+#include <boost/thread/thread.hpp>
+#include <boost/algorithm/string.hpp>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <move_arm_tools/ArmCtrlCmd.h>
+#include <motion_planning_msgs/GetMotionPlan.h>
+
+class MoveArmTools
+{
+	public:
+		
+		MoveArmTools(){};
+		
+		~MoveArmTools();
+		
+		bool init(std::string arm);
+		
+	private:
+		
+		ros::NodeHandle nh_;
+		
+		std::string group_;
+		
+		std::string planner_service_;
+
+		ros::Publisher view_;
+		
+		ros::Publisher pubAttach_;
+		
+		ros::ServiceServer arm_ctrl_service_;
+
+		std::map<std::string, move_arm::MoveArmGoal> goals_;
+    
+		std::vector<std::string> names_;
+		
+		planning_environment::RobotModels *rm_;
+		
+		ros::ServiceClient arm_ctrl_;
+		
+		ros::ServiceClient clientPlan_;
+		
+		actionlib::SimpleActionClient<move_arm::MoveArmAction> *move_arm_;
+		
+		actionlib::SimpleActionClient<move_arm::ActuateGripperAction> *gripper_;
+		    
+		boost::thread th_;
+		
+		tf::TransformListener tf_;
+		
+		planning_environment::KinematicModelStateMonitor *km_;
+
+		double allowed_time_;
+	
+		/** ------ */
+		
+		bool callback(move_arm_tools::ArmCtrlCmd::Request &req, move_arm_tools::ArmCtrlCmd::Response &res);
+				
+		int goToPlan(ros::NodeHandle &nh, planning_environment::KinematicModelStateMonitor &km, move_arm::MoveArmGoal &goal, const std::vector<std::string> &names);
+		
+		void printHelp(void);
+
+		void printJoints(const planning_environment::KinematicModelStateMonitor &km, const std::vector<std::string> &names);
+
+		void printPose(const btTransform &p);
+
+		void goalToState(const move_arm::MoveArmGoal &goal, planning_models::StateParams &sp);
+
+		btTransform effPosition(const planning_environment::KinematicModelStateMonitor &km, const move_arm::MoveArmGoal &goal);
+
+		void printConfig(const move_arm::MoveArmGoal &goal);
+		
+		void printConfigs(const std::map<std::string, move_arm::MoveArmGoal> &goals);
+
+		void setupGoal(const std::vector<std::string> &names, move_arm::MoveArmGoal &goal);
+
+		void setupGoalEEf(const std::string &link, const std::vector<double> &pz, move_arm::MoveArmGoal &goal);
+
+		void getIK(bool r, ros::NodeHandle &nh, planning_environment::KinematicModelStateMonitor &km, move_arm::MoveArmGoal &goal, planning_models::StateParams &sp, const std::vector<std::string> &names, double x, double y, double z);
+
+		void goToIK(ros::NodeHandle &nh,  planning_environment::KinematicModelStateMonitor &km, const std::vector<std::string> &names, double x, double y, double z);
+
+		void diffConfig(const planning_environment::KinematicModelStateMonitor &km, move_arm::MoveArmGoal &goal);
+
+		void viewState(ros::Publisher &view, const planning_environment::KinematicModelStateMonitor &km, const planning_models::StateParams &st);
+
+		void setConfigJoint(const unsigned int pos, const double value, move_arm::MoveArmGoal &goal);
+		
+		void setConfig(const planning_models::StateParams *_sp, const std::vector<std::string> &names, move_arm::MoveArmGoal &goal);
+				
+		void spinThread(void);
+};
