8a29d216379c8f095217238de30ef373ed7b4c4e
eitanme <eitanme@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-26 17:23:04 +0000

    Updating to be nicer with a cleaner actionlib API
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@22976 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 05d0966dc385c0d7f8bb652f3611c49fa20c33ef

--- stacks/navigation/tutorials/simple_navigation_goals/src/simple_navigation_goals.cpp
+++ stacks/navigation/tutorials/simple_navigation_goals/src/simple_navigation_goals.cpp
@@ -41,33 +41,26 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
-#include <boost/thread.hpp>
-
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
-
-void spinThread(){
-  ros::spin();
-}
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "simple_navigation_goals");
 
-  ros::NodeHandle n;
+  //tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
 
-  boost::thread spin_thread = boost::thread(boost::bind(&spinThread));
-
-  MoveBaseClient ac("move_base");
-
-  //give some time for connections to register
-  sleep(2.0);
+  //wait for the action server to come up
+  while(!ac.waitForActionServerToStart(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
 
   move_base_msgs::MoveBaseGoal goal;
 
-  //we'll send a goal to the robot to move 2 meters forward
+  //we'll send a goal to the robot to move 1 meter forward
   goal.target_pose.header.frame_id = "base_link";
   goal.target_pose.header.stamp = ros::Time::now();
 
-  goal.target_pose.pose.position.x = 2.0;
+  goal.target_pose.pose.position.x = 1.0;
   goal.target_pose.pose.orientation.w = 1.0;
 
   ROS_INFO("Sending goal");
@@ -76,9 +69,9 @@ int main(int argc, char** argv){
   ac.waitForGoalToFinish();
 
   if(ac.getTerminalState() == actionlib::TerminalState::SUCCEEDED)
-    ROS_INFO("Horray, the base moved 2 meters forward");
+    ROS_INFO("Horray, the base moved 1 meter forward");
   else
-    ROS_INFO("The base failed to move forward 2 meters for some reason");
+    ROS_INFO("The base failed to move forward 1 meter for some reason");
 
   return 0;
 }
