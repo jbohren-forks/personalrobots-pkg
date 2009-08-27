559103b3bc15f96a1c3e5aeeb470d87409d95f19
sachinchitta <sachinchitta@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-08-27 09:09:52 +0000

    commiting new primitive generation with higher costs on turning
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@23128 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: 60467bdb688bbd022799e255b7be8b3df5df7f88

--- stacks/motion_planning/sbpl/matlab/mprim/genmprim_unicycleplussidewaysplusbackturnplusdiag.m
+++ stacks/motion_planning/sbpl/matlab/mprim/genmprim_unicycleplussidewaysplusbackturnplusdiag.m
@@ -47,10 +47,10 @@ if UNICYCLE_MPRIM_16DEGS == 1
 
     %multipliers (multiplier is used as costmult*cost)
     forwardcostmult = 1;
-    backwardcostmult = 5;
+    backwardcostmult = 1;
     forwardandturncostmult = 20; % 3;
     sidestepcostmult = 2;
-    turninplacecostmult = 1;
+    turninplacecostmult = 20;
     forwarddiagcostmult = 1; %move forward slightly to the left/right without changing heading
 
     %note, what is shown x,y,theta *changes* (that is, dx,dy,dtheta and not absolute numbers)
