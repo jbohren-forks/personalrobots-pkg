2d1d834811a5e90a39dfdd3372af87acf6c6aae5
mcgann <mcgann@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-09-01 15:18:15 +0000

    No longer required
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@23535 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: ee7e690c235058ff3808f9066339dc0ca688b178

--- sandbox/trex_temp_proj/src/adapter_utilities.cpp
+++
@@ -1,8 +0,0 @@
-#include "trex_pr2/pr2_adapter_utilities.h"
-#include "trex_ros/adapter_utilities.h"
-#include <tf/transform_listener.h>
-
-namespace trex_temp_proj {
-  
-  
-}
--- sandbox/trex_temp_proj/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "Assembly.hh"
-#include "trex_ros/executive.h"
-
-int main(int argc, char** argv) {
-  return trexMain(argc, argv);
-}
