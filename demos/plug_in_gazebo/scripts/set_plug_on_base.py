2f0411f2a88d7080ae40e86cb20e7fa4d650a57a
hsujohnhsu <hsujohnhsu@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-09-01 21:09:41 +0000

    update plug / holder.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@23600 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: bbf5917c25182bdfa104641aff572603d7ba3fa4

--- demos/plug_in_gazebo/scripts/set_plug_on_base.py
+++ demos/plug_in_gazebo/scripts/set_plug_on_base.py
@@ -103,7 +103,7 @@ holder_position_initialized = False
 gripper_position_initialized = False
 
 def  plugP3DInput(p3d):
-    global force,torque,holder_p,holder_e, holder_position_initialized, force,torque,plug_p,plug_e, plug_position_initialized, in_holder_range, holder_cutoff, linear_gain_p, angular_gain_p, timeout, update_rate, gripper_p, gripper_q, gripper_e, in_gripper_range, gripper_cutoff, gripper_position_initialized
+    global force,torque,holder_p,holder_e, holder_position_initialized, force,torque,plug_p,plug_e, plug_position_initialized, in_holder_range, holder_cutoff, linear_gain_p, angular_gain_p, timeout, update_rate, gripper_p, gripper_q, gripper_e, in_gripper_range, gripper_cutoff, gripper_position_initialized, plug_q, holder_q, gripper_q
 
     plug_p = [p3d.pose.pose.position.x, p3d.pose.pose.position.y, p3d.pose.pose.position.z]
     plug_q = [p3d.pose.pose.orientation.x, p3d.pose.pose.orientation.y, p3d.pose.pose.orientation.z, p3d.pose.pose.orientation.w]
@@ -112,7 +112,7 @@ def  plugP3DInput(p3d):
     plug_position_initialized = True
 
 def  holderP3DInput(p3d):
-    global force,torque,holder_p,holder_e, holder_position_initialized, force,torque,plug_p,plug_e, plug_position_initialized, in_holder_range, holder_cutoff, linear_gain_p, angular_gain_p, timeout, update_rate, gripper_p, gripper_q, gripper_e, in_gripper_range, gripper_cutoff, gripper_position_initialized
+    global force,torque,holder_p,holder_e, holder_position_initialized, force,torque,plug_p,plug_e, plug_position_initialized, in_holder_range, holder_cutoff, linear_gain_p, angular_gain_p, timeout, update_rate, gripper_p, gripper_q, gripper_e, in_gripper_range, gripper_cutoff, gripper_position_initialized, plug_q, holder_q, gripper_q
     holder_p = [p3d.pose.pose.position.x, p3d.pose.pose.position.y, p3d.pose.pose.position.z]
     holder_q = [p3d.pose.pose.orientation.x, p3d.pose.pose.orientation.y, p3d.pose.pose.orientation.z, p3d.pose.pose.orientation.w]
     holder_e = tft.euler_from_quaternion(holder_q)
@@ -120,7 +120,7 @@ def  holderP3DInput(p3d):
     holder_position_initialized = True
 
 def  gripperP3DInput(p3d):
-    global force,torque,holder_p,holder_e, holder_position_initialized, force,torque,plug_p,plug_e, plug_position_initialized, in_holder_range, holder_cutoff, linear_gain_p, angular_gain_p, timeout, update_rate, gripper_p, gripper_q, gripper_e, in_gripper_range, gripper_cutoff, gripper_position_initialized
+    global force,torque,holder_p,holder_e, holder_position_initialized, force,torque,plug_p,plug_e, plug_position_initialized, in_holder_range, holder_cutoff, linear_gain_p, angular_gain_p, timeout, update_rate, gripper_p, gripper_q, gripper_e, in_gripper_range, gripper_cutoff, gripper_position_initialized, plug_q, holder_q, gripper_q
     gripper_p = [p3d.pose.pose.position.x, p3d.pose.pose.position.y, p3d.pose.pose.position.z]
     gripper_q = [p3d.pose.pose.orientation.x, p3d.pose.pose.orientation.y, p3d.pose.pose.orientation.z, p3d.pose.pose.orientation.w]
     gripper_e = tft.euler_from_quaternion(gripper_q)
@@ -128,7 +128,7 @@ def  gripperP3DInput(p3d):
     gripper_position_initialized = True
 
 def main():
-    global force,torque,holder_p,holder_e, holder_position_initialized, force,torque,plug_p,plug_e, plug_position_initialized, in_holder_range, holder_cutoff, linear_gain_p, angular_gain_p, timeout, update_rate, gripper_p, gripper_q, gripper_e, in_gripper_range, gripper_cutoff, gripper_position_initialized
+    global force,torque,holder_p,holder_e, holder_position_initialized, force,torque,plug_p,plug_e, plug_position_initialized, in_holder_range, holder_cutoff, linear_gain_p, angular_gain_p, timeout, update_rate, gripper_p, gripper_q, gripper_e, in_gripper_range, gripper_cutoff, gripper_position_initialized, plug_q, holder_q, gripper_q
 
     # get goal from commandline
     for i in range(0,len(sys.argv)):
@@ -161,11 +161,20 @@ def main():
     while not rospy.is_shutdown() and time.time() < timeout_t:
       if holder_position_initialized and plug_position_initialized and gripper_position_initialized:
         holder_linear_offset  = [ holder_p[0] - plug_p[0], holder_p[1] - plug_p[1], holder_p[2] - plug_p[2] ]
-        holder_angular_offset = [ holder_e[0] - plug_e[0], holder_e[1] - plug_e[1], holder_e[2] - plug_e[2] ]
-        gripper_linear_offset  = [ gripper_p[0] - plug_p[0], gripper_p[1] - plug_p[1], gripper_p[2] - plug_p[2] ]
-        gripper_angular_offset = [ gripper_e[0] - plug_e[0], gripper_e[1] - plug_e[1], gripper_e[2] - plug_e[2] ]
+        tmpiq = tft.quaternion_inverse(plug_q)
+        tmpdq = tft.quaternion_multiply(holder_q,tmpiq)
+        holder_angular_offset = tft.euler_from_quaternion(tmpdq)
         holder_dist = (holder_linear_offset[0]*holder_linear_offset[0] + holder_linear_offset[1]*holder_linear_offset[1] + holder_linear_offset[2]*holder_linear_offset[2])
+
+        tmpdq = tft.quaternion_multiply(plug_q,gripper_q)
+        tmpiq = tft.quaternion_inverse(tmpdq)
+        tmpe = tft.euler_from_quaternion(tmpiq)
+        gripper_linear_offset  = [ gripper_p[0] - plug_p[0], gripper_p[1] - plug_p[1], gripper_p[2] - plug_p[2] ]
+        gripper_angular_offset = [ tmpe[0], tmpe[1], tmpe[2] ]
         gripper_dist = (gripper_linear_offset[0]*gripper_linear_offset[0] + gripper_linear_offset[1]*gripper_linear_offset[1] + gripper_linear_offset[2]*gripper_linear_offset[2])
+
+        #print "plug_q ",plug_q
+        #print "holder_q ",holder_q
         if holder_dist <= holder_cutoff*holder_cutoff and holder_dist < gripper_dist:
           in_holder_range = True
           in_gripper_range = False
@@ -174,17 +183,17 @@ def main():
             in_holder_range = False
             in_gripper_range = True
         if in_holder_range:
-          linear_error  = [ linear_gain_p*holder_linear_offset[0]/abs(holder_linear_offset[0]*holder_linear_offset[0]*holder_linear_offset[0]+0.001), \
-                            linear_gain_p*holder_linear_offset[1]/abs(holder_linear_offset[1]*holder_linear_offset[1]*holder_linear_offset[1]+0.001), \
-                            linear_gain_p*holder_linear_offset[2]/abs(holder_linear_offset[2]*holder_linear_offset[2]*holder_linear_offset[2]+0.001) ]
-          angular_error  = [ angular_gain_p*holder_angular_offset[0]/abs(holder_angular_offset[0]*holder_angular_offset[0]*holder_angular_offset[0]+0.001), \
-                             angular_gain_p*holder_angular_offset[1]/abs(holder_angular_offset[1]*holder_angular_offset[1]*holder_angular_offset[1]+0.001), \
-                             angular_gain_p*holder_angular_offset[2]/abs(holder_angular_offset[2]*holder_angular_offset[2]*holder_angular_offset[2]+0.001) ]
+          linear_error  = [ linear_gain_p*holder_linear_offset[0]/max(abs(holder_linear_offset[0]*holder_linear_offset[0]*holder_linear_offset[0]),0.01), \
+                            linear_gain_p*holder_linear_offset[1]/max(abs(holder_linear_offset[1]*holder_linear_offset[1]*holder_linear_offset[1]),0.01), \
+                            linear_gain_p*holder_linear_offset[2]/max(abs(holder_linear_offset[2]*holder_linear_offset[2]*holder_linear_offset[2]),0.01) ]
+          angular_error  = [ angular_gain_p*holder_angular_offset[0]/max(abs(holder_angular_offset[0]*holder_angular_offset[0]*holder_angular_offset[0]),0.1), \
+                             angular_gain_p*holder_angular_offset[1]/max(abs(holder_angular_offset[1]*holder_angular_offset[1]*holder_angular_offset[1]),0.1), \
+                             angular_gain_p*holder_angular_offset[2]/max(abs(holder_angular_offset[2]*holder_angular_offset[2]*holder_angular_offset[2]),0.1) ]
         else:
           if in_gripper_range:
-            linear_error  = [ linear_gain_p*gripper_linear_offset[0]/abs(gripper_linear_offset[0]*gripper_linear_offset[0]*gripper_linear_offset[0]+0.001), \
-                              linear_gain_p*gripper_linear_offset[1]/abs(gripper_linear_offset[1]*gripper_linear_offset[1]*gripper_linear_offset[1]+0.001), \
-                              linear_gain_p*gripper_linear_offset[2]/abs(gripper_linear_offset[2]*gripper_linear_offset[2]*gripper_linear_offset[2]+0.001) ]
+            linear_error  = [ linear_gain_p*gripper_linear_offset[0]/max(abs(gripper_linear_offset[0]*gripper_linear_offset[0]*gripper_linear_offset[0]),0.01), \
+                              linear_gain_p*gripper_linear_offset[1]/max(abs(gripper_linear_offset[1]*gripper_linear_offset[1]*gripper_linear_offset[1]),0.01), \
+                              linear_gain_p*gripper_linear_offset[2]/max(abs(gripper_linear_offset[2]*gripper_linear_offset[2]*gripper_linear_offset[2]),0.01) ]
             angular_error  = [0,0,0]
           else:
             linear_error  = [0,0,0]
