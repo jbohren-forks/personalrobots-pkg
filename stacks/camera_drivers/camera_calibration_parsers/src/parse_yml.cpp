e0be19cb5563d4fe2887621076911c7c216a8f28
pmihelich <pmihelich@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-09-01 23:55:24 +0000

    camera_calibration_parsers: Now use yaml-cpp instead of opencv for yaml parsing.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@23615 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: b58a58600239d14cc2d5a31fe4f61240ebe6bc48

--- stacks/camera_drivers/camera_calibration_parsers/src/parse_yml.cpp
+++ stacks/camera_drivers/camera_calibration_parsers/src/parse_yml.cpp
@@ -1,6 +1,8 @@
 #include "camera_calibration_parsers/parse_yml.h"
-#include <opencv/cv.h>
+#include <yaml-cpp/yaml.h>
+#include <fstream>
 #include <ctime>
+#include <cassert>
 
 namespace camera_calibration_parsers {
 
@@ -17,16 +19,51 @@ static const double R_default[9] = {1.0, 0.0, 0.0,
                                     0.0, 1.0, 0.0,
                                     0.0, 0.0, 1.0};
 
-// @todo: use yaml-cpp to get rid of opencv dependency
+struct SimpleMatrix
+{
+  int rows;
+  int cols;
+  double* data;
+
+  SimpleMatrix(int rows, int cols, double* data)
+    : rows(rows), cols(cols), data(data)
+  {}
+};
+
+YAML::Emitter& operator << (YAML::Emitter& out, const SimpleMatrix& m)
+{
+  out << YAML::BeginMap;
+  out << YAML::Key << "rows" << YAML::Value << m.rows;
+  out << YAML::Key << "cols" << YAML::Value << m.cols;
+  //out << YAML::Key << "dt"   << YAML::Value << "d"; // OpenCV data type specifier
+  out << YAML::Key << "data" << YAML::Value;
+  out << YAML::Flow;
+  out << YAML::BeginSeq;
+  for (int i = 0; i < m.rows*m.cols; ++i)
+    out << m.data[i];
+  out << YAML::EndSeq;
+  out << YAML::EndMap;
+  return out;
+}
+
+void operator >> (const YAML::Node& node, SimpleMatrix& m)
+{
+  int rows, cols;
+  node["rows"] >> rows;
+  assert(rows == m.rows);
+  node["cols"] >> cols;
+  assert(cols == m.cols);
+  const YAML::Node& data = node["data"];
+  for (int i = 0; i < rows*cols; ++i)
+    data[i] >> m.data[i];
+}
+
 bool writeCalibrationYml(const std::string& file_name, const std::string& camera_name,
                          int width, int height,
                          const double* K, const double* D,
                          const double* R, const double* P)
 {
-  CvFileStorage* fs = cvOpenFileStorage(file_name.c_str(), 0, CV_STORAGE_WRITE);
-  if (!fs)
-    return false;
-
+  // Set up default matrices if needed
   if (!D) D = D_default;
   if (!R) R = R_default;
   double P_default[12];
@@ -37,24 +74,39 @@ bool writeCalibrationYml(const std::string& file_name, const std::string& camera
     }
     P = P_default;
   }
-  
+
+  YAML::Emitter out;
+  out << YAML::BeginMap;
+
+  // Calibration time
+  // FIXME: this breaks yaml-cpp on reading for some reason
+#if 0
   time_t raw_time;
   time( &raw_time );
-  cvWriteString(fs, "save_time", asctime( localtime(&raw_time) ));
-  cvWriteInt(fs, WIDTH_YML_NAME, width);
-  cvWriteInt(fs, HEIGHT_YML_NAME, height);
-  
-  cvWriteString(fs, CAM_YML_NAME, camera_name.c_str());
-  const CvMat K_mat = cvMat(3, 3, CV_64FC1, const_cast<double*>(K));
-  cvWrite(fs, K_YML_NAME, &K_mat);
-  const CvMat D_mat = cvMat(1, 5, CV_64FC1, const_cast<double*>(D));
-  cvWrite(fs, D_YML_NAME, &D_mat);
-  const CvMat R_mat = cvMat(3, 3, CV_64FC1, const_cast<double*>(R));
-  cvWrite(fs, R_YML_NAME, &R_mat);
-  const CvMat P_mat = cvMat(3, 4, CV_64FC1, const_cast<double*>(P));
-  cvWrite(fs, P_YML_NAME, &P_mat);
+  out << YAML::Key << "calibration_time";
+  out << YAML::Value << asctime(localtime(&raw_time));
+#endif
 
-  cvReleaseFileStorage(&fs);
+  // Image dimensions
+  out << YAML::Key << WIDTH_YML_NAME << YAML::Value << width;
+  out << YAML::Key << HEIGHT_YML_NAME << YAML::Value << height;
+  
+  // Camera name and intrinsics
+  out << YAML::Key << CAM_YML_NAME << YAML::Value << camera_name;
+  out << YAML::Key << K_YML_NAME << YAML::Value << SimpleMatrix(3, 3, const_cast<double*>(K));
+  out << YAML::Key << D_YML_NAME << YAML::Value << SimpleMatrix(1, 5, const_cast<double*>(D));
+  out << YAML::Key << R_YML_NAME << YAML::Value << SimpleMatrix(3, 3, const_cast<double*>(R));
+  out << YAML::Key << P_YML_NAME << YAML::Value << SimpleMatrix(3, 4, const_cast<double*>(P));
+
+  out << YAML::EndMap;
+
+  // Write to file
+  FILE* file = fopen(file_name.c_str(), "w");
+  if (!file)
+    return false;
+  fprintf(file, "%s\n", out.c_str());
+  fclose(file);
+  
   return true;
 }
 
@@ -62,58 +114,37 @@ bool readCalibrationYml(const std::string& file_name, std::string& camera_name,
                         int &width, int &height,
                         double* K, double* D, double* R, double* P)
 {
-  CvFileStorage* fs = cvOpenFileStorage(file_name.c_str(), 0, CV_STORAGE_READ);
-  if (!fs)
-    return false;
+  std::ifstream fin(file_name.c_str());
+  YAML::Parser parser(fin);
+  if (!parser)
+    printf("Parser not OK!\n");
+  YAML::Node doc;
+  parser.GetNextDocument(doc);
 
-  const char* name = cvReadStringByName(fs, NULL, CAM_YML_NAME, NULL);
-  if (name)
-    camera_name = name;
-
-  width = cvReadIntByName(fs, NULL, WIDTH_YML_NAME);
-  height = cvReadIntByName(fs, NULL, HEIGHT_YML_NAME);
-  if (width == 0 || height == 0)
-    return false;
-
-  CvMat* K_mat = (CvMat*)cvReadByName(fs, 0, K_YML_NAME);
-  if (!K_mat)
-    return false;
-  memcpy(K, K_mat->data.db, 9*sizeof(double));
-
+  try {
+    doc[CAM_YML_NAME] >> camera_name;
+  } catch(...) {
+    camera_name = "unknown";
+  }
+  doc[WIDTH_YML_NAME] >> width;
+  doc[HEIGHT_YML_NAME] >> height;
+  
+  SimpleMatrix K_(3, 3, K);
+  doc[K_YML_NAME] >> K_;
+  
   if (D) {
-    CvMat* D_mat = (CvMat*)cvReadByName(fs, 0, D_YML_NAME);
-    if (D_mat) {
-      memcpy(D, D_mat->data.db, 5*sizeof(double));
-      cvReleaseMat(&D_mat);
-    } else {
-      // Default to zeros
-      memcpy(D, D_default, 5*sizeof(double));
-    }
+    SimpleMatrix D_(1, 5, D);
+    doc[D_YML_NAME] >> D_;
   }
 
   if (R) {
-    CvMat* R_mat = (CvMat*)cvReadByName(fs, 0, R_YML_NAME);
-    if (R_mat) {
-      memcpy(R, R_mat->data.db, 9*sizeof(double));
-      cvReleaseMat(&R_mat);
-    } else {
-      // Default to identity matrix
-      memcpy(R, R_default, 9*sizeof(double));
-    }
+    SimpleMatrix R_(3, 3, R);
+    doc[R_YML_NAME] >> R_;
   }
 
   if (P) {
-    CvMat* P_mat = (CvMat*)cvReadByName(fs, 0, P_YML_NAME);
-    if (P_mat) {
-      memcpy(P, P_mat->data.db, 12*sizeof(double));
-      cvReleaseMat(&P_mat);
-    } else {
-      // Default to K augmented with column of zeros
-      for (int i = 0; i < 3; ++i) {
-        memcpy(P + 4*i, K + 3*i, 3*sizeof(double));
-        P[4*i + 3] = 0.0;
-      }
-    }
+    SimpleMatrix P_(3, 4, P);
+    doc[P_YML_NAME] >> P_;
   }
   
   return true;
