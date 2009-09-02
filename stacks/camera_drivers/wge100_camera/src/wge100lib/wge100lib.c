f90e905ad7148b65f7a9c34ad3dde375b08d60c0
blaisegassend <blaisegassend@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-09-02 17:26:09 +0000

    Fixed a segfault in the wge100 node.
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@23699 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: e84f9dd4ec0a5cc1f382b52ef24868214024788c

--- stacks/camera_drivers/wge100_camera/src/wge100lib/wge100lib.c
+++ stacks/camera_drivers/wge100_camera/src/wge100lib/wge100lib.c
@@ -386,7 +386,7 @@ int wge100Discover(const char *ifName, IpCamList *ipCamList, const char *ipAddre
      for (numif = 0, curif = ifaces; curif; curif = curif->ifa_next)
      {
        //fprintf(stderr, "Adding %s to discover list.\n", curif->ifa_name);
-       if (curif->ifa_addr->sa_family == AF_INET)
+       if (curif->ifa_addr && curif->ifa_addr->sa_family == AF_INET)
          ifNames[numif++] = curif->ifa_name;
     }
   }
