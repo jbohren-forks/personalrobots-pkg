d36157eff449e8209fef5d527684527e925040af
eitanme <eitanme@f5854215-dd47-0410-b2c4-cdd35faa7885>
2009-09-04 21:44:40 +0000

    Updating for new version of ros::Rate
    git-svn-id: https://svn.code.sf.net/p/personalrobots/code/pkg/trunk@23878 f5854215-dd47-0410-b2c4-cdd35faa7885
    
    Former-commit-id: c2a493a0c6105379618a727b19a4fde6804b70de

--- stacks/navigation/costmap_2d/src/costmap_2d_publisher.cpp
+++ stacks/navigation/costmap_2d/src/costmap_2d_publisher.cpp
@@ -68,8 +68,11 @@ namespace costmap_2d {
         new_data_ = false;
         publishCostmap();
       }
-      if(!r.sleep())
-        ROS_WARN("Map publishing loop missed its desired rate of %.4f the actual time the loop took was %.4f sec", frequency, r.cycleTime().toSec());
+
+      r.sleep();
+      //make sure to sleep for the remainder of our cycle time
+      if(r.cycleTime() > ros::Duration(1 / frequency))
+        ROS_WARN("Map update loop missed its desired rate of %.4fHz... the loop actually took %.4f seconds", frequency, r.cycleTime().toSec());
     }
   }
 
--- stacks/navigation/costmap_2d/src/costmap_2d_ros.cpp
+++ stacks/navigation/costmap_2d/src/costmap_2d_ros.cpp
@@ -444,8 +444,11 @@ namespace costmap_2d {
       end_t = end.tv_sec + double(end.tv_usec) / 1e6;
       t_diff = end_t - start_t;
       ROS_DEBUG("Map update time: %.9f", t_diff);
-      if(!r.sleep())
-        ROS_WARN("Map update loop missed its desired rate of %.4f the actual time the loop took was %.4f sec", frequency, r.cycleTime().toSec());
+
+      r.sleep();
+      //make sure to sleep for the remainder of our cycle time
+      if(r.cycleTime() > ros::Duration(1 / frequency))
+        ROS_WARN("Map update loop missed its desired rate of %.4fHz... the loop actually took %.4f seconds", frequency, r.cycleTime().toSec());
     }
   }
 
